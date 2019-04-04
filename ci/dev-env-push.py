b3fc3d3cb69a9a0adf303dd754b3a192e85d8b87
Florian Klink <florian.klink+da@tweag.io>
2019-04-04 17:44:29 +0200

    ci/dev-env-push: revert f-string usage
    This might be run with a python3 different from the one provided by
    dev-env, and not support f-strings yet.

--- ci/dev-env-push.py
+++ ci/dev-env-push.py
@@ -43,8 +43,8 @@ def log_cmd(cmd):
 # doesn't exist.
 def get_secret(workdir, key):
     value = os.environ.pop(key, None)
-    if value is None or value == f'$({key})':
-        log(f'could not find secret {key}')
+    if value is None or value == '$({})'.format(key):
+        log('could not find secret {}'.format(key))
         sys.exit(1)
 
     filepath = os.path.join(workdir, key)
