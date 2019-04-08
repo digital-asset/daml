31efe043a8b4251e0a64b8d2153d969c5efcc81a
Neil Mitchell <neil.mitchell@digitalasset.com>
2019-04-08 19:04:55 +0100

    Generate the dylib's on Mac

--- rattle/Main.hs
+++ rattle/Main.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE ViewPatterns #-}
 
 module Main(main) where
 
 import Rattle
 import System.IO.Extra
+import System.Info.Extra
 import System.Process.Extra
 import System.FilePattern.Directory
 import System.FilePath
@@ -25,21 +27,28 @@ main = rattle $ do
 
 
 buildHaskellLibrary :: Bool -> [String] -> FilePath -> IO [String]
-buildHaskellLibrary useSrc deps source = do
+buildHaskellLibrary useSrc (nubOrd -> deps) source = do
     print ("buildHaskellPackage",useSrc,deps,source)
     let addSrc x = if useSrc then x </> "src" else x
     let name = takeFileName source
     files <- getDirectoryFiles (addSrc source) ["**/*.hs"]
     let modules = map (intercalate "." . splitDirectories . dropExtension) files
-    cmd_ "ghc" ["-hidir=.rattle/haskell" </> name, "-odir=.rattle/haskell" </> name]
+    cmd_ "ghc"
+        [flag ++ "=.rattle/haskell" </> name | flag <- ["-outputdir","-odir","-hidir","-stubdir"]]
         ["-i" ++ addSrc source]
+        "-dynosuf=dyn_o -dynhisuf=dyn_hi"
+        ["-static"] ["-dynamic-too" | not isWindows]
         ["-package-db=.rattle/haskell" </> d </> "pkg.db" | d <- deps]
         modules ["-this-unit-id=" ++ name]
         (map ("-X"++) haskellExts) haskellFlags
-    cmd_ "ar -r" [".rattle/haskell" </> name </> "libHS" ++ name ++ ".a"]
+    cmd_ "ar -r -s" [".rattle/haskell" </> name </> "libHS" ++ name ++ ".a"]
         [".rattle/haskell" </> name </> x -<.> "o" | x <- files]
-    cmd_ "ld -x -r -o" [".rattle/haskell" </> name </> "HS" ++ name ++ ".o"]
-        [".rattle/haskell" </> name </> x -<.> "o" | x <- files]
+    if isWindows then
+        cmd_ "ld -x -r -o" [".rattle/haskell" </> name </> "HS" ++ name ++ ".o"]
+            [".rattle/haskell" </> name </> x -<.> "o" | x <- files]
+    else
+        cmd_ "ghc -shared -dynamic" ["-dynload deploy"] "-o" [".rattle/haskell" </> name </> "libHS" ++ name ++ ".dylib"]
+            [".rattle/haskell" </> name </> x -<.> "dyn_o" | x <- files]
     unlessM (doesDirectoryExist $ ".rattle/haskell" </> name </> "pkg.db") $
         cmd_ "ghc-pkg init" [".rattle/haskell" </> name </> "pkg.db"]
     keys <- map (drop 4) . filter (not . isInfixOf "haskeline") . filter (not . isInfixOf "ghc-lib") . lines <$>
@@ -89,4 +98,5 @@ haskellFlags =
     ,"-threaded", "-rtsopts"
     -- run on two cores, disable idle & parallel GC
     ,"-with-rtsopts=-N2 -qg -I0"
+    ,"-v"
     ]
