aaef06266c4679f81c86ab5d7f1a4925df970e95
Moritz Kiefer <moritz.kiefer@digitalasset.com>
2019-04-08 15:13:33 +0200

    Add copyright headers

--- daml-assistant/integration-tests/src/Main.hs
+++ daml-assistant/integration-tests/src/Main.hs
@@ -1,3 +1,5 @@
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
 module Main (main) where
 
 import Control.Concurrent
