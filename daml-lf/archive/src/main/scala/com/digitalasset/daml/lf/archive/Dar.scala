737fc06bfee2cdc989875bf1017ef1de189a655c
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-04-09 12:52:30 -0400

    Implement Functor for Dar, #109 (#305)

--- daml-lf/archive/src/main/scala/com/digitalasset/daml/lf/archive/Dar.scala
+++ daml-lf/archive/src/main/scala/com/digitalasset/daml/lf/archive/Dar.scala
@@ -3,6 +3,15 @@
 
 package com.digitalasset.daml.lf
 
+import scalaz.Functor
+
 case class Dar[A](main: A, dependencies: List[A]) {
-  def all: List[A] = main :: dependencies
+  lazy val all: List[A] = main :: dependencies
+}
+
+object Dar {
+  implicit val darFunctor: Functor[Dar] = new Functor[Dar] {
+    override def map[A, B](fa: Dar[A])(f: A => B): Dar[B] =
+      Dar[B](main = f(fa.main), dependencies = fa.dependencies.map(f))
+  }
 }
