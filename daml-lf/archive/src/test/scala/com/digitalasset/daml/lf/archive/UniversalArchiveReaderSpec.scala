a8e32f6735febcb6c7e64c6aca4fae530ac6ecc6
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-04-10 10:35:59 -0400

    Add Universal Archive Reader to parse DARs and DALFs, #109 (#359)
    Clean up DAR Reader errors.

---
+++ daml-lf/archive/src/test/scala/com/digitalasset/daml/lf/archive/UniversalArchiveReaderSpec.scala
@@ -0,0 +1,41 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.daml.lf
+
+import java.io.File
+
+import org.scalatest.{FlatSpec, Inside, Matchers}
+
+import scala.util.{Success, Try}
+
+class UniversalArchiveReaderSpec extends FlatSpec with Matchers with Inside {
+
+  private val darFile = new File("daml-lf/archive/DarReaderTest.dar")
+
+  private val dalfFile = new File("daml-lf/archive/DarReaderTest.dalf")
+
+  behavior of classOf[UniversalArchiveReader[_]].getSimpleName
+
+  it should "parse a DAR file" in {
+    assertSuccess(UniversalArchiveReader().readArchive(darFile))
+  }
+
+  it should "parse a DALF file" in {
+    assertSuccess(UniversalArchiveReader().readArchive(dalfFile))
+  }
+
+  it should "parse a DAR file and return language version" in {
+    assertSuccess(UniversalArchiveReaderWithVersion().readArchive(darFile))
+  }
+
+  it should "parse a DALF file and return language version" in {
+    assertSuccess(UniversalArchiveReaderWithVersion().readArchive(dalfFile))
+  }
+
+  private def assertSuccess[A](value: Try[Dar[A]]): Unit = {
+    inside(value) {
+      case Success(Dar(_, _)) =>
+    }
+  }
+}
