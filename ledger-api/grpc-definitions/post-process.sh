cbf08d22393b0de8066a8f0041c87158af26aaa6
Beth Aitman <beth.aitman@digitalasset.com>
2019-04-10 15:20:55 +0200

    Try to fix process script

--- ledger-api/grpc-definitions/post-process.sh
+++ ledger-api/grpc-definitions/post-process.sh
@@ -5,7 +5,7 @@
 
 # Fix links to external google protobuf documentation
 re='s,:ref:`(\.?google\.protobuf\.[^<>`\n]*) <(.*)>`, `\1 <https://developers.google.com/protocol-buffers/docs/reference/google.protobuf#\2>`__,g'
-sed -r "$re" -i docs_rst_mmd.md
+sed -r "$re" -i docs.rst
 # Fix links to external google rpc documentation
 re='s,:ref:`(\.?google\.rpc\.[^<>`\n]*) <(.*)>`, `\1 <https://cloud.google.com/tasks/docs/reference/rpc/google.rpc#\2>`__,g'
-sed -r "$re" -i docs_rst_mmd.md
+sed -r "$re" -i docs.rst
