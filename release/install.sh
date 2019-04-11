78d77d0a2e80014b867941d5e295ac71741d9ef2
Fran <francisco.mota@digitalasset.com>
2019-04-11 16:49:02 +0200

    Add a daml-sdk-head script. (#404)

--- release/install.sh
+++ release/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-DIR="$(cd $(dirname $(readlink -f "$BASH_SOURCE[0]")); pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 "$DIR/daml/daml" install "$DIR" --activate $@
