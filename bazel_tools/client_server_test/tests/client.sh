e73ad8b95704fcf3af5b6d7aa5e6a6e6209305ed
Jussi Mäki <jussi.maki@digitalasset.com>
2019-04-11 17:31:54 +0200

    Add client_server_test bazel rule (#365)
    * Add client_server_test bazel rule
    
    This adds a generic mechanism for constructing a test that launches
    a TCP server and runs a test-suite against it. The rule orchestrates
    passing the port number from the server to the client via a temporary
    file and takes care of killing the server when the client process exits.
    
    Still to be done is figuring out a good way to pass additional arguments
    to the server and client programs (where these arguments could be other
    bazel rules).
    
    * Add missing copyright headers
    
    * Add support for arguments to client_server_test
    
    Note that this does not support passing in labels (":my_data_file"),
    nor does it support "$(location :my_data_file)" string macros.
    
    * Add data attribute to client_server_test
    
    * Add support for location expansion in client_server_test args
    
    * client_server_test: Address code review
    
    * client_server_test: Bump waiting for port file write to 5s

---
+++ bazel_tools/client_server_test/tests/client.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+foobar=0
+port=""
+inputFile=""
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --target-port) port="$2"; shift 2;;
+    --target-port=*) port="${1#*=}"; shift 1;;
+    --foobar) foobar=1; shift 1;;
+    -*) echo "unknown option: $1" >&2; exit 1;;
+    *) inputFile="$1"; shift 1;;
+  esac
+done
+
+if [ "$foobar" -ne 1 ]; then
+  echo "--foobar not passed in"
+  exit 1
+fi
+
+if [[ -z "$port" || -z "$inputFile" ]]; then
+  echo "target port or input file not specified"
+  exit 1
+fi
+
+exec 5<> /dev/tcp/localhost/$port
+cat $inputFile >&5
+cat <&5
