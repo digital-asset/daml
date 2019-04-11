59d71bbab44f994ad50d040f9e5b3fd499097fb0
Jussi Maki <jussi.maki@digitalasset.com>
2019-04-11 11:52:42 +0200

    ledger/damlonx: scalafmt

--- ledger/api-server-damlonx/src/main/scala/com/daml/ledger/api/server/damlonx/services/DamlOnXCommandCompletionService.scala
+++ ledger/api-server-damlonx/src/main/scala/com/daml/ledger/api/server/damlonx/services/DamlOnXCommandCompletionService.scala
@@ -129,10 +129,8 @@ object DamlOnXCommandCompletionService {
   def create(indexService: IndexService)(
       implicit ec: ExecutionContext,
       mat: Materializer,
-      esf: ExecutionSequencerFactory): CommandCompletionServiceValidation
-    with BindableService
-    with AutoCloseable
-    with CommandCompletionServiceLogging = {
+      esf: ExecutionSequencerFactory)
+    : CommandCompletionServiceValidation with BindableService with AutoCloseable with CommandCompletionServiceLogging = {
     val impl = new DamlOnXCommandCompletionService(indexService)
 
     val ledgerId = Await.result(indexService.getLedgerId(), 5.seconds)
