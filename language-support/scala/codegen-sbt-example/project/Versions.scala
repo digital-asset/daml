d9c20ef3ee352b18554cdbc0d8a79bd4acc84a96
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-04-16 14:29:40 -0400

    Add support for `DA.sdkVersion` system property.
    To override the default sdkVersion:
    ```
    > sbt -DDA.sdkVersion=100.12.6 compile
    ```

--- language-support/scala/codegen-sbt-example/project/Versions.scala
+++ language-support/scala/codegen-sbt-example/project/Versions.scala
@@ -2,10 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 object Versions {
-  lazy val sdkVersion: String = "100.12.6"
+  val sdkVersion: String = sdkVersionFromSysProps().getOrElse("100.12.6")
+
+  println(s"DA sdkVersion: $sdkVersion")
 
   lazy val detectedOs: String = sys.props("os.name") match {
     case "Mac OS X" => "osx"
     case _ => "linux"
   }
+
+  private def sdkVersionFromSysProps(): Option[String] =
+    sys.props.get("DA.sdkVersion").map(_.toString)
 }
