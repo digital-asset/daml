8655a792c35f8944a8a61d58204c9655216ab58d
Robert Autenrieth <31539813+rautenrieth-da@users.noreply.github.com>
2019-04-16 13:20:59 +0200

    Navigator: Print important messages to stderr (#522)
    * Navigator: Print important messages to stderr
    
    All messages that users need to act on are now printed to
    stderr, in addition to the log file.
    
    Otherwise, it is confusing to users why they have to start
    Navigator twice.
    
    Fixes #112

--- navigator/backend/src/main/scala/com/digitalasset/navigator/store/platform/PlatformStore.scala
+++ navigator/backend/src/main/scala/com/digitalasset/navigator/store/platform/PlatformStore.scala
@@ -31,6 +31,7 @@ import com.digitalasset.navigator.ApplicationInfo
 import io.grpc.netty.{GrpcSslContexts, NettyChannelBuilder}
 import io.grpc.{ManagedChannel, Status}
 import io.netty.handler.ssl.SslContext
+import org.slf4j.LoggerFactory
 
 import scala.util.{Failure, Random, Success, Try}
 import scala.concurrent.Future
@@ -97,6 +98,8 @@ class PlatformStore(
     .nextLong()
     .toHexString
 
+  private[this] def userFacingLogger = LoggerFactory.getLogger("user-facing-logs")
+
   import PlatformStore._
 
   // ----------------------------------------------------------------------------------------------
@@ -125,7 +128,9 @@ class PlatformStore(
 
     case Connected(Failure(e)) =>
       // Connection failed even after several retries - not sure how to recover from this
-      log.error("Giving up. Please fix any issues and restart this application.")
+      val message = s"Permanently failed to connect to the ledger at $platformHost:$platformPort. " +
+        "Please fix any issues and restart this application."
+      userFacingLogger.error(message)
       context.become(failed(StateFailed(e)))
 
     case GetApplicationStateInfo =>
