86617b57e7a2d47824a6346dd14a1e7a7ee8e3c6
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-04-17 07:22:12 +0200

    Add reference for passing -j 200 to Bazel

--- build.sh
+++ build.sh
@@ -20,6 +20,8 @@ ARTIFACT_DIRS="${BUILD_ARTIFACTSTAGINGDIRECTORY:-$PWD}"
   # Bazel also limits cache downloads by -j so increasing this to a ridiculous value
   # helps. Bazel separately controls the number of jobs using CPUs so this should not
   # overload machines.
+  # This also appears to be what Google uses internally, see
+  # https://github.com/bazelbuild/bazel/issues/6394#issuecomment-436234594.
   bazel build -j 200 //... --experimental_execution_log_file "$ARTIFACT_DIRS/build_execution${execution_log_postfix}.log"
 )
 bazel test -j 200 //... --experimental_execution_log_file "$ARTIFACT_DIRS/test_execution${execution_log_postfix}.log"
