a9b00ae8d5155d85014b040a01a2dc0af99c7bd1
Francesco Mazzoli <francesco.mazzoli@daml.com>
2019-04-18 14:55:46 +0200

    bump the perf test timeout

--- ledger/sandbox-perf/src/perf/lib/scala/com/digitalasset/platform/sandbox/perf/TestHelper.scala
+++ ledger/sandbox-perf/src/perf/lib/scala/com/digitalasset/platform/sandbox/perf/TestHelper.scala
@@ -50,7 +50,7 @@ trait TestHelper {
     entityName = "ListUtil")
 
   val setupTimeout = 30.seconds
-  val perfTestTimeout = 5.minutes
+  val perfTestTimeout = 30.minutes
 
   val transactionFilter = TransactionFilter(Map(party -> Filters()))
 
