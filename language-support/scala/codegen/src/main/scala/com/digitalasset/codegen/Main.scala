678ecf104da23f5bb1ddf9f4f90e60b3b63c8d23
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-04-18 12:24:21 -0400

    Improving Scala Codegen error reporting (code review)

--- language-support/scala/codegen/src/main/scala/com/digitalasset/codegen/Main.scala
+++ language-support/scala/codegen/src/main/scala/com/digitalasset/codegen/Main.scala
@@ -10,6 +10,9 @@ import ch.qos.logback.classic.Level
 import com.digitalasset.daml.lf.codegen.conf.Conf
 import com.typesafe.scalalogging.StrictLogging
 import org.slf4j.{Logger, LoggerFactory}
+import scalaz.Cord
+
+import scala.collection.breakOut
 
 object Main extends StrictLogging {
 
@@ -24,34 +27,43 @@ object Main extends StrictLogging {
         CodeGen.generateCode(dars, packageName, outputDir.toFile, CodeGen.Novel)
       case None =>
         throw new IllegalArgumentException(
-          s"Invalid $codegenId command line arguments: ${args.mkString(" ")}")
+          s"Invalid ${codegenId: String} command line arguments: ${args.mkString(" "): String}")
     }
 
   private def setGlobalLogLevel(verbosity: Level): Unit = {
     LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) match {
       case a: ch.qos.logback.classic.Logger =>
         a.setLevel(verbosity)
-        logger.info(s"$codegenId verbosity: $verbosity")
+        logger.info(s"${codegenId: String} verbosity: $verbosity")
       case _ =>
-        logger.warn(s"$codegenId cannot set requested verbosity: $verbosity")
+        logger.warn(s"${codegenId: String} cannot set requested verbosity: $verbosity")
     }
   }
 
   private def logUnsupportedEventDecoderOverride(mapping: Option[(String, String)]): Unit =
-    mapping.foreach { decoderMapping =>
-      logger.warn(s"$codegenId does not allow overriding Event Decoder, requested: $decoderMapping")
+    mapping.foreach {
+      case (a, b) =>
+        logger.warn(
+          s"${codegenId: String} does not allow overriding Event Decoder, requested: ${a: String} -> ${b: String}")
     }
 
   private def darsAndOnePackageName(darMap: Map[Path, Option[String]]): (List[File], String) = {
-    val empty: (List[File], Set[String]) = (List.empty, Set.empty)
-    val (dars, packageNames) =
-      darMap.foldRight(empty)((a, as) => (a._1.toFile :: as._1, as._2 + a._2.getOrElse("daml")))
-    packageNames.toSeq match {
+    val dars: List[File] = darMap.keys.map(_.toFile)(breakOut)
+    val uniquePackageNames: Set[String] = darMap.values.collect { case Some(x) => x }(breakOut)
+    uniquePackageNames.toSeq match {
       case Seq(packageName) =>
         (dars, packageName)
       case _ =>
         throw new IllegalStateException(
-          s"$codegenId expects all dars mapped to the same package name, requested: $darMap")
+          s"${codegenId: String} expects all dars mapped to the same package name, " +
+            s"requested: ${format(darMap): String}")
     }
+  }
+
+  private def format(map: Map[Path, Option[String]]): String = {
+    val cord = map.foldLeft(Cord("{")) { (str, kv) =>
+      str ++ kv._1.toFile.getAbsolutePath ++ "->" ++ kv._2.toString ++ ","
+    }
+    (cord ++ "}").toString
   }
 }
