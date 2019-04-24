8dae03ad1f02b54734e683d9692a23b7d8e67794
Gabor Aranyossy <gabor.aranyossy@digitalasset.com>
2019-04-24 16:20:21 +0200

    adding missing copyright headers

--- ledger/sandbox/src/test/lib/scala/com/digitalasset/platform/sandbox/cli/CliSpec.scala
+++ ledger/sandbox/src/test/lib/scala/com/digitalasset/platform/sandbox/cli/CliSpec.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.digitalasset.platform.sandbox.cli
 
 import java.io.File
