7ac438e87b729994770a4f9bb597ef637d204fcf
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2019-04-25 14:30:25 +0200

    Remove //language-support/js (#691)
    The JavaScript ecosystem support is being spun off in its own
    repository. The new home is https://github.com/digital-asset/daml-js
    
    For further details, here is the counterpart issue in the new repo:
    
    https://github.com/digital-asset/daml-js/issues/1

--- language-support/js/daml-ledger/src/call/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-export { ClientCancellableCall } from './client_cancellable_call';
-export { ClientReadableObjectStream } from './client_readable_object_stream';
--- language-support/js/daml-ledger/src/client/testing/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * @namespace ledger.testing
- */
-
-export { TimeClient } from './time_client';
-export { ResetClient } from './reset_client';
