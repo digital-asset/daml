5f7b93e7a474bea71d53af005fc3f761c89bd758
Robert Autenrieth <31539813+rautenrieth-da@users.noreply.github.com>
2019-04-26 08:45:26 +0200

    Fix Navigator console issues (#710)
    * Navigator: Fix pretty-printing empty objects
    
    * Navigator: fix display of tables on dumb terminals
    
    For dumb terminals, reader.getTerminal.getWidth returns 0.

--- navigator/backend/src/main/scala/com/digitalasset/navigator/console/Pretty.scala
+++ navigator/backend/src/main/scala/com/digitalasset/navigator/console/Pretty.scala
@@ -254,6 +254,8 @@ object Pretty {
         values
           .map(v => arrayIndent + go(v, i + 1))
           .mkString("\n" + indent * i)
+      case PrettyObject(fields) if fields.isEmpty =>
+        "{}"
       case PrettyObject(fields) =>
         val maxFieldLength = fields.map(_.name.length).max + 1
         fields
@@ -280,8 +282,9 @@ object Pretty {
       state: State,
       header: List[String],
       data: TraversableOnce[TraversableOnce[String]]): String = {
+    val width = state.reader.getTerminal.getWidth
     AsciiTable()
-      .width(state.reader.getTerminal.getWidth)
+      .width(if (width > 4) width else 80)
       .multiline(false)
       .columnMinWidth(4)
       .sampleAtMostRows(200)
