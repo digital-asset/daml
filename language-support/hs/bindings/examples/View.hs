7d1db8045e0b7241a3e130bf03f9013a8d6e2662
Nick Chapman <nick.chapman@digitalasset.com>
2019-04-29 13:07:28 +0100

    fix haskell indentation

--- language-support/hs/bindings/examples/View.hs
+++ language-support/hs/bindings/examples/View.hs
@@ -1,16 +1,15 @@
 -- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-
 module Main(main) where
 
-import System.Environment
-import LedgerIdentity(Port(..),ledgerId)
+import           LedgerIdentity     (Port (..), ledgerId)
+import           System.Environment
 
 main :: IO ()
 main = do
-  putStrLn "Ledger App (View)..."
-  [port] <- getArgs
-  id <- LedgerIdentity.ledgerId (Port (read port))
-  print (port,id)
+    putStrLn "Ledger App (View)..."
+    [port] <- getArgs
+    id <- LedgerIdentity.ledgerId (Port (read port))
+    print (port, id)
 
--- language-support/hs/bindings/src/LedgerIdentity.hs
+++ language-support/hs/bindings/src/LedgerIdentity.hs
@@ -6,38 +6,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module LedgerIdentity(
-  LedgerId,
-  ledgerId,
-  Port(..),
-  )
-where
-
+    LedgerId,
+    ledgerId,
+    Port(..),
+    ) where
+    
 import qualified Data.Map           as Map
 import           Data.Text.Lazy     (Text)
 
 import Network.GRPC.HighLevel.Generated (
-  withGRPCClient,
-  ClientRequest(
-      --ClientReaderRequest
-      ClientNormalRequest
-      ),
-  ClientResult(
-        --ClientReaderResponse,
-      ClientNormalResponse,
-      ClientErrorResponse
-      ),
-  MetadataMap(..),
-  GRPCMethodType(Normal
-                 --ServerStreaming,
-                 ),
-  ClientConfig(..),
-  Port(..),
-  Host(..),
-  )
+    withGRPCClient,
+    ClientRequest(ClientNormalRequest),
+    ClientResult(ClientNormalResponse, ClientErrorResponse),
+    MetadataMap(..),
+    GRPCMethodType(Normal),
+    ClientConfig(..),
+    Port(..),
+    Host(..),
+    )
 
-import Network.GRPC.HighLevel.Client(
-  TimeoutSeconds,
-  )
+import Network.GRPC.HighLevel.Client(TimeoutSeconds)
 
 import Com.Digitalasset.Ledger.Api.V1.TraceContext(TraceContext)
 import Com.Digitalasset.Ledger.Api.V1.LedgerIdentityService as LIS
@@ -46,16 +34,16 @@ data LedgerId = LedgerId Text deriving Show
 
 ledgerId :: Port -> IO LedgerId
 ledgerId port = do
-  withGRPCClient (config port) $ \client -> do
-    rpcs <- LIS.ledgerIdentityServiceClient client
-    callLedgerIdService rpcs
+    withGRPCClient (config port) $ \client -> do
+        rpcs <- LIS.ledgerIdentityServiceClient client
+        callLedgerIdService rpcs
 
 callLedgerIdService :: LIS.LedgerIdentityService ClientRequest ClientResult -> IO LedgerId
 callLedgerIdService (LIS.LedgerIdentityService rpc) = do
-  response <- rpc (wrap (LIS.GetLedgerIdentityRequest noTrace))
-  LIS.GetLedgerIdentityResponse text <- unwrap response
-  let id = LedgerId text
-  return id
+    response <- rpc (wrap (LIS.GetLedgerIdentityRequest noTrace))
+    LIS.GetLedgerIdentityResponse text <- unwrap response
+    let id = LedgerId text
+    return id
 
 noTrace :: Maybe TraceContext
 noTrace = Nothing
@@ -65,17 +53,16 @@ wrap r = ClientNormalRequest r timeout mdm
 
 unwrap :: ClientResult 'Normal a -> IO a
 unwrap = \case
-  ClientNormalResponse x _m1 _m2 _status _details -> return x
-  ClientErrorResponse e -> fail (show e)
-
+    ClientNormalResponse x _m1 _m2 _status _details -> return x
+    ClientErrorResponse e -> fail (show e)
 
 config :: Port -> ClientConfig
 config port =
-  ClientConfig { clientServerHost = Host "localhost"
-               , clientServerPort = port
-               , clientArgs = []
-               , clientSSLConfig = Nothing
-               }
+    ClientConfig { clientServerHost = Host "localhost"
+                 , clientServerPort = port
+                 , clientArgs = []
+                 , clientSSLConfig = Nothing
+                 }
 
 timeout :: TimeoutSeconds
 timeout = 1
