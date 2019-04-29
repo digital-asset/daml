54ed98944fd3e15021cbc1feb7d6cec5b74fd7f2
Jussi Maki <jussi.maki@digitalasset.com>
2019-04-29 10:59:28 +0200

    copyright headers & reformat

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/Conversions.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/Conversions.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.participant.state.kvutils
 
 import java.time.{Duration, Instant}
