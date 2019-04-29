1cb8bd80c533b88da154d10d94bcae3f8acc4056
Jussi Maki <jussi.maki@digitalasset.com>
2019-04-29 14:49:16 +0200

    Update kvutils package documentation

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/package.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/package.scala
@@ -9,8 +9,8 @@ package com.daml.ledger.participant.state
   * [[com.daml.ledger.participant.state.v1.WriteService]] on top of ledger's that provide a key-value state storage.
   *
   * The key-value utilities are based around the concept of modelling the ledger around
-  * an abstract state that can be described (abstractly) as the tuple (logEntryIds, logEntryMap, kvState),
-  * of type (List[DamlLogEntryId], Map[DamlLogEntryId, DamlLogEntry], Map[DamlStateKey, DamlStateValue]).
+  * an abstract state that can be described as the tuple `(logEntryIds, logEntryMap, kvState)`,
+  * of type `(List[DamlLogEntryId], Map[DamlLogEntryId, DamlLogEntry], Map[DamlStateKey, DamlStateValue])`.
   *
   * `logEntryIds` describes the ordering of log entries. The `logEntryMap` contains the data for the log entries.
   * This map is expected to be append-only and existing entries are never modified or removed.
@@ -19,35 +19,42 @@ package com.daml.ledger.participant.state
   * describe their activeness).
   *
   * While these can be represented in a key-value store directly, some implementations may
-  * provide the ordering of log entries from outside the state (e.g. via the transaction chain).
+  * provide the ordering of log entries from outside the state (e.g. via a transaction chain).
   * The distinction between DAML log entries and DAML state values is that log entries are immutable,
   * and that their keys are not necessarily known beforehand, which is why the implementation deals
-  * with them separately.
+  * with them separately, even though both log entries and DAML state values may live in the same storage.
   *
-  * As an example, lets take an implementation in which the ledger deals with an ordered chain of transactions
-  * that, when applied, mutate a key-value store. In this setup we can choose to allocate entry identifiers
-  * using a random number generator and validating uniqueness at commit time. We furthermore assume that
-  * the log entry identifier can be derived from the transaction entry of the underlying ledger.
+  * As an example, lets look at how a DAML transaction is committed in the [[InMemoryKVParticipantState]].
+  * The example implementation maintains a bytes to bytes key-value store and a linked-list of committed
+  * blocks. The log entries and the DAML state is stored in the same key-value store and the linked-list of
+  * commits provides the ordering to the log entries. Log entry identifiers are randomly generated.
   *
-  * With these assumptions an example submission looks as follows:
+  * The transaction submission with the in-memory example would look as follows:
   *
-  * 1. The [[v1.WriteService.submitTransaction]] is invoked in the DAML Ledger API server.
+  * 1. The [[InMemoryKVParticipantState.submitTransaction]] is invoked in the DAML Ledger API server.
   *  The implementation assigns a unique log entry identifier "D8AF41AB" for the submission and using [[KeyValueSubmission]]
-  *  produces a [[DamlKvutils.DamlSubmission]] which is then communicated to the underlying ledger for committing.
+  *  produces a [[DamlKvutils.DamlSubmission]] which is then asynchronously submitted for processing.
   *
-  * 2. The underlying ledger's transaction processor receives the submission, verifies that the identifier for the log entry
+  * 2. The commit actor receives the submission, verifies that the identifier for the log entry
   *  is unique, fetches the inputs to the submission and calls [[KeyValueCommitting.processSubmission]] to
   *  validate and produce the log entry and state updates. The log entry is stored on the ledger in the
-  *  location assigned at submission, and the state updates are applied.
+  *  location assigned at submission, and the state updates are applied. The submission itself and the
+  *  log entry identifier is appended to the linked-list of commits.
   *
-  *  State after applying changes:
-  *    damlState/DamlStateKey(command_dedup = <...>) = ()
-  *    damlState/DamlStateKey(contract_id = (D8AF41AB, 1) = DamlContractState(active_at = <...>)
-  *    damlLogEntries/D8AF41AB = DamlLogEntry(transaction_entry = <...>)
+  *  The applied state updates:
   *
-  * 3. The implementation of [[v1.ReadService.stateUpdates]] receives event on newly committed ledger entry.
-  *  From the ledger's transaction it deduces the unique log entry identifier "D8AF41AB" and queries the key-value
-  *  store to retrieve key `damlLogEntries/D8AF41AB`, and using [[KeyValueConsumption.logEntryToUpdate]] produces
-  *  an [[v1.Update]], with an offset corresponding to the block height of the ledger (for example).
+  *    * `DS/DamlStateKey(command_dedup = { command_id = ... }) = {}`
+  *    * `DS/DamlStateKey(contract_id = (D8AF41AB, 0)) = DamlContractState(active_at = 2019-04-29...)`
+  *    * `L/DamlLogEntryId(D8AF41AB) = DamlLogEntry(transaction_entry = ...)`
+  *
+  * 3. The [[InMemoryKVParticipant.stateUpdates]] emits a new update event from the new state of the ledger
+  *  by retrieving the [[DamlLogEntry]] pointed to by the latest commit and transforms it into [[v1.Update]]
+  *  using [[KeyValueConsumption.logEntryToUpdate]], and pairs it with an offset corresponding to the
+  *  position of the commit (the "block height" if you will).
+  *
+  * The above example implementation is suitable for situations where the inputs to the transaction have
+  * to be pre-computed. For ledger's that do not require this a more natural implementation could assign
+  * the log entry identifiers at commit time and store the log entries with a monotonically increasing
+  * integer as the key (which would allow for efficient range queries).
   */
 package object kvutils {}
