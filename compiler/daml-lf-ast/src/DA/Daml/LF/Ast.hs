20c10426ca4feb343dac1c68103cfc5dc28c89b2
Neil Mitchell <neil.mitchell@digitalasset.com>
2019-05-01 08:29:26 +0100

    Whitespace only

--- compiler/daml-lf-ast/src/DA/Daml/LF/Ast.hs
+++ compiler/daml-lf-ast/src/DA/Daml/LF/Ast.hs
@@ -6,8 +6,8 @@ module DA.Daml.LF.Ast
   ( module LF
   ) where
 
-import DA.Daml.LF.Ast.Base   as LF
-import DA.Daml.LF.Ast.Util   as LF
+import DA.Daml.LF.Ast.Base as LF
+import DA.Daml.LF.Ast.Util as LF
 import DA.Daml.LF.Ast.Version as LF
-import DA.Daml.LF.Ast.World  as LF
+import DA.Daml.LF.Ast.World as LF
 import DA.Daml.LF.Ast.Pretty ()
