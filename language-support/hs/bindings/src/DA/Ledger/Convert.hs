549cf086e1f507812fd948694a62ff3b5f1e9bc7
nickchapman-da <49153372+nickchapman-da@users.noreply.github.com>
2019-05-03 11:37:08 +0100

    Haskell ledger bindings (#879)
    * HL types for Ledger API. Command Submission & Completion services.
    
    * whitespace
    
    * remove renamed file that came back after bad merge
    
    * delint
    
    * adapt to change in transaction_service.proto

---
+++ language-support/hs/bindings/src/DA/Ledger/Convert.hs
@@ -0,0 +1,101 @@
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+{-# LANGUAGE OverloadedStrings #-}
+
+-- Convert between HL Ledger.Types and the LL types generated from .proto files
+module DA.Ledger.Convert (lowerCommands) where
+
+import           Data.Maybe         (fromMaybe)
+import           Data.Vector        as Vector (fromList)
+
+import qualified DA.Ledger.LowLevel as LL
+import           DA.Ledger.Types
+
+xxx :: a -- TODO: not finished while any instances of "xxx" of "_" remain in this file
+xxx = undefined
+
+lowerCommands :: Commands -> LL.Commands
+lowerCommands = \case
+    Commands{lid,wid,aid,cid,party,leTime,mrTime,coms} ->
+        LL.Commands {
+        commandsLedgerId = unLedgerId lid,
+        commandsWorkflowId = unWorkflowId (fromMaybe (WorkflowId "") wid),
+        commandsApplicationId = unApplicationId aid,
+        commandsCommandId = unCommandId cid,
+        commandsParty = unParty party,
+        commandsLedgerEffectiveTime = Just (lowerTimestamp leTime),
+        commandsMaximumRecordTime = Just (lowerTimestamp mrTime),
+        commandsCommands = Vector.fromList $ map lowerCommand coms }
+
+lowerCommand :: Command -> LL.Command
+lowerCommand = \case
+    CreateCommand{tid,args} ->
+        LL.Command $ Just $ LL.CommandCommandCreate $ LL.CreateCommand {
+        createCommandTemplateId = Just (lowerTemplateId tid),
+        createCommandCreateArguments = Just (lowerRecord args)}
+        
+    ExerciseCommand{tid,cid,choice,arg} ->
+        LL.Command $ Just $ LL.CommandCommandExercise $ LL.ExerciseCommand {
+        exerciseCommandTemplateId = Just (lowerTemplateId tid),
+        exerciseCommandContractId = unContractId cid,
+        exerciseCommandChoice = unChoice choice,
+        exerciseCommandChoiceArgument = Just (lowerValue arg) }
+
+    CreateAndExerciseCommand{tid,createArgs,choice,choiceArg} ->
+        LL.Command $ Just $ LL.CommandCommandCreateAndExercise $ LL.CreateAndExerciseCommand {
+        createAndExerciseCommandTemplateId = Just (lowerTemplateId tid),
+        createAndExerciseCommandCreateArguments = Just (lowerRecord createArgs),
+        createAndExerciseCommandChoice = unChoice choice,
+        createAndExerciseCommandChoiceArgument = Just (lowerValue choiceArg) }
+
+lowerTemplateId :: TemplateId -> LL.Identifier
+lowerTemplateId (TemplateId x) = lowerIdentifier x
+
+lowerIdentifier :: Identifier -> LL.Identifier
+lowerIdentifier = \case
+    Identifier{pid,mod,ent} ->
+        LL.Identifier {
+        identifierPackageId = unPackageId pid,
+        identifierName = "", -- marked as deprecated in .proto
+        identifierModuleName = unModuleName mod,
+        identifierEntityName = unEntityName ent }
+
+lowerTimestamp :: Timestamp -> LL.Timestamp
+lowerTimestamp = \case
+    Timestamp{seconds,nanos} ->
+        LL.Timestamp {
+        timestampSeconds = fromIntegral seconds,
+        timestampNanos = fromIntegral nanos
+        }
+
+lowerValue :: Value -> LL.Value
+lowerValue = \case -- TODO: more cases here
+    VRecord r -> LL.Value $ Just $ LL.ValueSumRecord $ lowerRecord r
+    VVariant _ -> xxx
+    VContract _ -> xxx
+    VList vs -> LL.Value $ Just $ LL.ValueSumList $ LL.List $ Vector.fromList $ map lowerValue vs
+    VInt i -> LL.Value $ Just $ LL.ValueSumInt64 $ fromIntegral i
+    VDecimal s -> LL.Value $ Just $ LL.ValueSumDecimal s
+    VString s -> LL.Value $ Just $ LL.ValueSumText s
+    VTimestamp _ -> xxx
+    VParty p -> LL.Value $ Just $ LL.ValueSumParty $ unParty p
+    VBool _ -> xxx
+    VUnit -> xxx
+    VDate _ -> xxx
+    VOpt _ -> xxx
+    VMap _ -> xxx
+
+lowerRecord :: Record -> LL.Record
+lowerRecord = \case
+    Record{rid,fields} ->
+        LL.Record {
+        recordRecordId = fmap lowerIdentifier rid,
+        recordFields = Vector.fromList $ map lowerRecordField fields }
+
+lowerRecordField :: RecordField -> LL.RecordField
+lowerRecordField = \case
+    RecordField{label,value} ->
+        LL.RecordField {
+        recordFieldLabel = label,
+        recordFieldValue = Just (lowerValue value) }
--- language-support/hs/bindings/src/LedgerId.hs
+++
@@ -1,8 +0,0 @@
--- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
--- SPDX-License-Identifier: Apache-2.0
-
-module LedgerId(LedgerId(..)) where
-
-import           Data.Text.Lazy (Text)
-
-data LedgerId = LedgerId Text deriving Show
