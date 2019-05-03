9c4806e583a3ab621fbe4a2badef4f12cf148fe3
David Millar-Durrant <dmillardurrant@gmail.com>
2019-05-03 18:02:24 +0200

    Fix tests

--- daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Damlc/Test.hs
+++ daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Damlc/Test.hs
@@ -42,7 +42,7 @@ execTest :: [FilePath] -> UseColor -> Maybe FilePath -> Compiler.Options -> IO (
 execTest inFiles color mbJUnitOutput cliOptions = do
     loggerH <- getLogger cliOptions "test"
     opts <- Compiler.mkOptions cliOptions
-    let eventLogger (EventFileDiagnostics (fp, diags)) = printDiagnostics $ map ((fp,)) diags
+    let eventLogger (EventFileDiagnostics (fp, diags)) = printDiagnostics $ map (fp,) diags
         eventLogger _ = return ()
     Managed.with (Compiler.newIdeState opts (Just eventLogger) loggerH) $ \h -> do
         let lfVersion = Compiler.optDamlLfVersion cliOptions
