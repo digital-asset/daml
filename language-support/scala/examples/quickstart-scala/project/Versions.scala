0ca414f09c72a81707f98f081fad5572f6562ce5
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-05-08 11:18:22 -0400

    quickstart-scala daml assistant template cleanup (#614) (#1007)

--- language-support/scala/examples/quickstart-scala/project/Versions.scala
+++ language-support/scala/examples/quickstart-scala/project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
 
   private val daSdkVersionKey = "da.sdk.version"
 
-  val daSdkVersion: String = sys.props.get(daSdkVersionKey).getOrElse(getSdkVersionFromFile())
+  val daSdkVersion: String = sys.props.get(daSdkVersionKey).getOrElse(sdkVersionFromFile())
   println(s"$daSdkVersionKey = ${daSdkVersion: String}")
 
   private val darFileKey = "dar.file"
@@ -14,7 +14,7 @@ object Versions {
     .get(darFileKey)
     .map(s => new sbt.File(s))
     .filter(_.exists)
-    .getOrElse(new sbt.File(s"dist/${getProjectName(): String}.dar"))
+    .getOrElse(new sbt.File(s"./dist/${projectNameFromConfig(): String}.dar"))
   println(s"$darFileKey = ${darFile.getAbsolutePath: String}")
 
   lazy val detectedOs: String = sys.props("os.name") match {
@@ -22,13 +22,13 @@ object Versions {
     case _ => "linux"
   }
 
-  private def getSdkVersionFromFile(): String =
+  private def sdkVersionFromFile(): String =
     "10" + sbt.IO.read(new sbt.File("./SDK_VERSION").getAbsoluteFile).trim
 
-  private def getProjectName(): String =
+  private def projectNameFromConfig(): String =
     sbt.IO
       .readLines(new sbt.File("./daml.yaml").getAbsoluteFile)
       .find(_.startsWith("name:"))
       .map(_.replaceFirst("name:", "").trim)
-      .getOrElse("iou")
+      .getOrElse(throw new IllegalStateException(s"Cannot read project name from a config file"))
 }
