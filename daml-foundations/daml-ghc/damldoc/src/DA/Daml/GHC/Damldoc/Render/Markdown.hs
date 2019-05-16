a02b82a348f904797b9d7e6c08125082113aa655
Beth Aitman <beth.aitman@digital-asset.com>
2019-05-16 10:38:59 +0200

    Address the rest of review comments

--- daml-foundations/daml-ghc/damldoc/src/DA/Daml/GHC/Damldoc/Render/Markdown.hs
+++ daml-foundations/daml-ghc/damldoc/src/DA/Daml/GHC/Damldoc/Render/Markdown.hs
@@ -34,7 +34,7 @@ renderSimpleMD ModuleDoc{..} = T.unlines $
     then []
     else [ "## Typeclasses"
          , ""
-         , "Typeclasses in DAML are an interface that defines some behavior."
+         , "Typeclasses in DAML are interfaces that define some behavior."
          , ""
          , T.unlines $ map cls2md md_classes
          , ""
