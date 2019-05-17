71fee23774f7e54bd77f8c99bdc6906b9df7aa61
Michal Majcherski <michal.majcherski@gft.com>
2019-05-17 13:17:57 +0200

    windows: daml-ghc-deterministic test working

--- daml-foundations/daml-ghc/tests/daml-ghc-deterministic.sh
+++ daml-foundations/daml-ghc/tests/daml-ghc-deterministic.sh
@@ -2,6 +2,32 @@
 # All rights reserved.
 set -euo pipefail
 
+# --- begin runfiles.bash initialization ---
+# Copy-pasted from Bazel's Bash runfiles library (tools/bash/runfiles/runfiles.bash).
+if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  if [[ -f "$0.runfiles_manifest" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles_manifest"
+  elif [[ -f "$0.runfiles/MANIFEST" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles/MANIFEST"
+  elif [[ -f "$0.runfiles/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+    export RUNFILES_DIR="$0.runfiles"
+  fi
+fi
+if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+  source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
+elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
+            "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
+else
+  echo >&2 "ERROR: cannot find @bazel_tools//tools/bash/runfiles:runfiles.bash"
+  exit 1
+fi
+# --- end runfiles.bash initialization ---
+
+TESTS_DIR=$(dirname $(rlocation "$TEST_WORKSPACE/daml-foundations/daml-ghc/tests/Examples.daml"))
+damlc=$(rlocation "$TEST_WORKSPACE/$1")
+protoc=$(rlocation "$TEST_WORKSPACE/$2")
+
 # Check that DAML compilation is deterministic.
 TMP_SRC1=$(mktemp -d)
 TMP_SRC2=$(mktemp -d)
@@ -12,11 +38,8 @@ cleanup () {
 }
 trap cleanup EXIT
 
-cp -r daml-foundations/daml-ghc/tests/* "$TMP_SRC1"
-cp -r daml-foundations/daml-ghc/tests/* "$TMP_SRC2"
-
-damlc="$PWD/$1"
-protoc="$PWD/$2"
+cp -r $TESTS_DIR/* "$TMP_SRC1"
+cp -r $TESTS_DIR/* "$TMP_SRC2"
 
 (cd "$TMP_SRC1" && $damlc compile "Examples.daml" -o "$TMP_OUT/out_1")
 (cd "$TMP_SRC2" && $damlc compile "Examples.daml" -o "$TMP_OUT/out_2")
