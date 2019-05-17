56bc4d0c11029a4b4542239f65e5b5fd164fdf80
Remy Haemmerle <Remy.Haemmerle@digitalasset.com>
2019-05-17 16:39:47 +0200

    sandbox-sql: migrate db in small batches

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/sql/migration/scripts/V3__Recompute_Key_Hash.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/sql/migration/scripts/V3__Recompute_Key_Hash.scala
@@ -9,7 +9,7 @@ import java.sql.{Connection, ResultSet}
 import anorm.{BatchSql, NamedParameter}
 import com.digitalasset.daml.lf.data._
 import com.digitalasset.daml.lf.transaction.Node.GlobalKey
-import com.digitalasset.daml.lf.value.Value.{AbsoluteContractId, VersionedValue}
+import com.digitalasset.daml.lf.value.Value.AbsoluteContractId
 import com.digitalasset.platform.sandbox.stores.ledger.sql.serialisation.{
   KeyHasher,
   ValueSerializer
@@ -18,13 +18,20 @@ import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
 
 class V3__Recompute_Key_Hash extends BaseJavaMigration {
 
+  // the number of contracts proceeded in a batch.
+  private val batchSize = 100 * 1000
+
   def migrate(context: Context): Unit = {
     implicit val conn: Connection = context.getConnection
     updateKeyHashed(loadContractKeys)
   }
 
-  private val SQL_SELECT_CONTRACT_KEYS =
-    """
+  private def loadContractKeys(
+      implicit connection: Connection
+  ): Iterator[(AbsoluteContractId, GlobalKey)] = {
+
+    val SQL_SELECT_CONTRACT_KEYS =
+      """
       |SELECT
       |  contracts.id as contract_id,
       |  contracts.package_id as package_id,
@@ -34,62 +41,55 @@ class V3__Recompute_Key_Hash extends BaseJavaMigration {
       |  contracts
       |WHERE
       |  contracts.key is not null
-     """.stripMargin
-
-  private val SQL_UPDATE_CONTRACT_KEYS_HASH =
-    """
-      |UPDATE
-      |  contract_keys
-      |SET
-      |  value_hash = {valueHash}
-      |WHERE
-      |  contract_id = {contractId}
-      |""".stripMargin
-
-  def loadContractKeys(
-      implicit connection: Connection
-  ): ImmArray[(AbsoluteContractId, GlobalKey)] = {
-
-    var stack = FrontStack.empty[(AbsoluteContractId, GlobalKey)]
+    """.stripMargin
 
     val rows: ResultSet = connection.createStatement().executeQuery(SQL_SELECT_CONTRACT_KEYS)
 
-    while (rows.next()) {
-      val contractId = AbsoluteContractId(rows.getString("contract_id"))
-      val packageId = Ref.PackageId.assertFromString(rows.getString("package_id"))
-      val templateName = rows.getString("template_name")
-      val templateId = Ref.Identifier(packageId, Ref.QualifiedName.assertFromString(templateName))
-      val keyStream = rows.getBytes("contract_key")
-      val key: VersionedValue[AbsoluteContractId] = ValueSerializer
-        .deserialiseValue(keyStream)
-        .fold(err => throw new IllegalArgumentException(err.errorMessage), identity)
+    new Iterator[(AbsoluteContractId, GlobalKey)] {
 
-      stack = (contractId -> GlobalKey(templateId, key)) +: stack
+      var hasNext: Boolean = rows.next()
 
+      def next(): (AbsoluteContractId, GlobalKey) = {
+        val contractId = AbsoluteContractId(rows.getString("contract_id"))
+        val packageId = Ref.PackageId.assertFromString(rows.getString("package_id"))
+        val templateName = rows.getString("template_name")
+        val templateId = Ref.Identifier(packageId, Ref.QualifiedName.assertFromString(templateName))
+        val key = ValueSerializer
+          .deserialiseValue(rows.getBytes("contract_key"))
+          .fold(err => throw new IllegalArgumentException(err.errorMessage), identity)
+
+        hasNext = rows.next()
+
+        contractId -> GlobalKey(templateId, key)
+      }
     }
-    stack.toImmArray
+
   }
 
-  def updateKeyHashed(contractKeys: ImmArray[(AbsoluteContractId, GlobalKey)])(
+  private def updateKeyHashed(contractKeys: Iterator[(AbsoluteContractId, GlobalKey)])(
       implicit conn: Connection): Unit = {
+
+    val SQL_UPDATE_CONTRACT_KEYS_HASH =
+      """
+        |UPDATE
+        |  contract_keys
+        |SET
+        |  value_hash = {valueHash}
+        |WHERE
+        |  contract_id = {contractId}
+      """.stripMargin
+
     val statements = contractKeys.map {
       case (cid, key) =>
-        Seq[NamedParameter](
-          "contractId" -> cid.coid,
-          "valueHash" -> KeyHasher.hashKeyString(key)
-        )
+        Seq[NamedParameter]("contractId" -> cid.coid, "valueHash" -> KeyHasher.hashKeyString(key))
     }
 
-    statements match {
-      case ImmArrayCons(firstStatement, otherStatements) =>
-        BatchSql(
-          SQL_UPDATE_CONTRACT_KEYS_HASH,
-          firstStatement,
-          otherStatements.toSeq: _*
-        ).execute
-        ()
-      case _ =>
-        ()
+    statements.toStream.grouped(batchSize).foreach { batch =>
+      BatchSql(
+        SQL_UPDATE_CONTRACT_KEYS_HASH,
+        batch.head,
+        batch.tail: _*
+      ).execute()
     }
   }
 
