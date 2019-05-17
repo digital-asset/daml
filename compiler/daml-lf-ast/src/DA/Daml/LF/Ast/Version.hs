24e305c1af01dee26c58cb588141e1510d94cd9d
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-05-17 22:06:23 +0200

    Relax syntactic restriction on contract keys in DAML-LF 1.dev (#1219)
    * Relax syntactic restriction on contract keys in DAML-LF 1.dev
    
    We lift the syntactic restriction that contact keys must be built using
    only record constructions and projections entirely when compiling to
    DAML-LF 1.dev. To make this more useful, we also search all sub-expressions
    of `maintainer` in `key` during our rewriting of `maintainer` for using
    `this` to using `key`.
    
    As one of our next steps we should bring `key` into scope in `maintainer`
    and perhaps deprecate the use of `this` at some point in the future.
    
    * Fix versioning
    
    * Adapt package validation to complext contract keys

--- compiler/daml-lf-ast/src/DA/Daml/LF/Ast/Version.hs
+++ compiler/daml-lf-ast/src/DA/Daml/LF/Ast/Version.hs
@@ -32,6 +32,11 @@ version1_2 = V1 $ PointStable 2
 version1_3 :: Version
 version1_3 = V1 $ PointStable 3
 
+-- TODO(MH): Change this when freezing 1.dev into 1.4.
+version1_4 :: Version
+version1_4 = versionDev
+-- version1_4 = V1 $ PointStable 4
+
 -- | The DAML-LF version used by default.
 versionDefault :: Version
 versionDefault = version1_3
@@ -66,6 +71,9 @@ featureContractKeys = Feature "Contract keys" version1_3
 
 featurePartyFromText :: Feature
 featurePartyFromText = Feature "partyFromText function" version1_2
+
+featureComplexContractKeys :: Feature
+featureComplexContractKeys = Feature "Complex contract keys" version1_4
 
 supports :: Version -> Feature -> Bool
 supports version feature = version >= featureMinVersion feature
