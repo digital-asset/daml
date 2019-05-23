fe8b752f5b7a38914db06ba2135ea777739ebaf2
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-05-23 11:43:49 +0200

    Test damlc against all supported DAML-LF version
    I disabled this in the past because the tests were to slow. They have gotten
    quite a bit faster since then and I'm slowly getting scared by the fact we
    don't do this right now. Also, it incentivices us to drop support for old
    DAML-LF versions more frequently, which will most of the time also remove
    complexity from the compiler.

--- daml-foundations/daml-ghc/test-src/DA/Test/GHC.hs
+++ daml-foundations/daml-ghc/test-src/DA/Test/GHC.hs
@@ -8,8 +8,7 @@
 -- typecheck with LF, test it.  Test annotations are documented as 'Ann'.
 module DA.Test.GHC
   ( main
-  , mainVersionDefault
-  , mainVersionDev
+  , mainAll
   ) where
 
 import DA.Daml.GHC.Compiler.Options
@@ -72,22 +71,19 @@ import Test.Tasty.Runners
 newtype TODO = TODO String
 
 main :: IO ()
-main = mainVersionDev
+main = mainWithVersions [versionDev]
 
-mainVersionDefault :: IO ()
-mainVersionDefault = mainWithVersion versionDefault
+mainAll :: IO ()
+mainAll = mainWithVersions (delete versionDev supportedInputVersions)
 
-mainVersionDev :: IO ()
-mainVersionDev = mainWithVersion versionDev
-
-mainWithVersion :: Version -> IO ()
-mainWithVersion version =
+mainWithVersions :: [Version] -> IO ()
+mainWithVersions versions =
   with (SS.startScenarioService (\_ -> pure ()) Logger.makeNopHandle) $ \scenarioService -> do
   setEnv "TASTY_NUM_THREADS" "1" True
   todoRef <- newIORef DList.empty
   let registerTODO (TODO s) = modifyIORef todoRef (`DList.snoc` ("TODO: " ++ s))
-  integrationTest <- getIntegrationTests registerTODO scenarioService version
-  let tests = testGroup "All" [uniqueUniques, integrationTest]
+  integrationTests <- mapM (getIntegrationTests registerTODO scenarioService) versions
+  let tests = testGroup "All" $ uniqueUniques : integrationTests
   defaultMainWithIngredients ingredients tests
     `finally` (do
     todos <- readIORef todoRef
