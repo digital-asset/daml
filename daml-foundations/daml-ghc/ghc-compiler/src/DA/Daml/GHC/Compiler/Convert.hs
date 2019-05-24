f831ccfde1f43afbcdd2dc9329ec288f14206ae6
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-05-24 14:52:25 +0200

    Change damlc and daml-stdlib to use exercise without actors
    The main purpose of this is to get rid of the `fetch` preceding each
    `exercise`. This will work for DAML-LF 1.5 when it is frozen.
    
    This finishes #1347.

--- daml-foundations/daml-ghc/ghc-compiler/src/DA/Daml/GHC/Compiler/Convert.hs
+++ daml-foundations/daml-ghc/ghc-compiler/src/DA/Daml/GHC/Compiler/Convert.hs
@@ -299,7 +299,7 @@ isTypeableInfo _ = False
 
 convertTemplate :: Env -> GHC.Expr Var -> ConvertM [Definition]
 convertTemplate env (VarIs "C:Template" `App` Type (TypeCon ty [])
-        `App` ensure `App` signatories `App` observer `App` agreement `App` _create `App` _fetch `App` _archive)
+        `App` ensure `App` signatories `App` observer `App` agreement `App` _create `App` _fetch `App` _archive `App` _archiveWithActors)
     = do
     tplSignatories <- applyTplParam <$> convertExpr env signatories
     tplChoices <- fmap (\cs -> NM.fromList (archiveChoice tplSignatories : cs)) (choices tplSignatories)
@@ -346,7 +346,7 @@ convertChoice env signatories
            _templateDict `App`
              consuming `App`
                Var controller `App`
-                 choice `App` _exercise) = do
+                 choice `App` _exercise `App` _exerciseWithActors) = do
     consumption <- f (10 :: Int) consuming
     let chcConsuming = consumption == PreConsuming -- Runtime should auto-archive?
     argType <- convertType env $ TypeCon chc []
@@ -493,7 +493,7 @@ convertBind env x = convertBind2 env x
 
 convertBind2 :: Env -> CoreBind -> ConvertM [Definition]
 convertBind2 env (NonRec name x)
-    | Just internals <- MS.lookup (envGHCModuleName env) internalFunctions
+    | Just internals <- MS.lookup (envGHCModuleName env) (internalFunctions $ envLfVersion env)
     , is name `elem` internals
     = pure []
     -- NOTE(MH): Our inline return type syntax produces a local letrec for
@@ -532,20 +532,27 @@ convertBind2 env (Rec xs) = concatMapM (\(a, b) -> convertBind env (NonRec a b))
 internalTypes :: [String]
 internalTypes = ["Scenario","Update","ContractId","Time","Date","Party","Pair"]
 
-internalFunctions :: MS.Map GHC.ModuleName [String]
-internalFunctions = MS.fromList $ map (first mkModuleName)
+internalFunctions :: LF.Version -> MS.Map GHC.ModuleName [String]
+internalFunctions version = MS.fromList $ map (first mkModuleName)
     [ ("DA.Internal.Record",
         [ "getFieldPrim"
         , "setFieldPrim"
         ])
     , ("DA.Internal.Template", -- template funcs defined with magic
-        [ "$dminternalExercise"
+        [ "$dminternalExerciseWithActors"
         , "$dminternalFetch"
         , "$dminternalCreate"
-        , "$dminternalArchive"
+        , "$dminternalArchiveWithActors"
         , "$dminternalFetchByKey"
         , "$dminternalLookupByKey"
-        ])
+        ] ++
+        if version `supports` featureExerciseActorsOptional
+          then
+            [ "$dminternalExercise"
+            , "$dminternalArchive"
+            ]
+          else []
+      )
     , ("DA.Internal.LF", "unpackPair" : map ("$W" ++) internalTypes)
     , ("GHC.Base",
         [ "getTag"
@@ -587,7 +594,15 @@ convertExpr env0 e = do
         tmpl' <- convertQualified env tmpl
         withTmArg env (varV1, TContractId t') args $ \x args ->
             pure (EUpdate $ UFetch tmpl' x, args)
-    go env (VarIs "$dminternalExercise") (LType t@(TypeCon tmpl []) : LType c@(TypeCon chc []) : LType _result : _dict : args) = do
+    go env (VarIs "$dminternalExercise") (LType t@(TypeCon tmpl []) : LType c@(TypeCon chc []) : LType _result : _dict : args)
+      | envLfVersion env `supports` featureExerciseActorsOptional = do
+        t' <- convertType env t
+        c' <- convertType env c
+        tmpl' <- convertQualified env tmpl
+        withTmArg env (varV2, TContractId t') args $ \x2 args ->
+            withTmArg env (varV3, c') args $ \x3 args ->
+                pure (EUpdate $ UExercise tmpl' (mkChoiceName $ is chc) x2 Nothing x3, args)
+    go env (VarIs "$dminternalExerciseWithActors") (LType t@(TypeCon tmpl []) : LType c@(TypeCon chc []) : LType _result : _dict : args) = do
         t' <- convertType env t
         c' <- convertType env c
         tmpl' <- convertQualified env tmpl
@@ -595,7 +610,13 @@ convertExpr env0 e = do
             withTmArg env (varV2, TContractId t') args $ \x2 args ->
             withTmArg env (varV3, c') args $ \x3 args ->
                 pure (EUpdate $ UExercise tmpl' (mkChoiceName $ is chc) x2 (Just x1) x3, args)
-    go env (VarIs "$dminternalArchive") (LType t@(TypeCon tmpl []) : _dict : args) = do
+    go env (VarIs "$dminternalArchive") (LType t@(TypeCon tmpl []) : _dict : args)
+      | envLfVersion env `supports` featureExerciseActorsOptional = do
+        t' <- convertType env t
+        tmpl' <- convertQualified env tmpl
+        withTmArg env (varV2, TContractId t') args $ \x2 args ->
+            pure (EUpdate $ UExercise tmpl' (mkChoiceName "Archive") x2 Nothing EUnit, args)
+    go env (VarIs "$dminternalArchiveWithActors") (LType t@(TypeCon tmpl []) : _dict : args) = do
         t' <- convertType env t
         tmpl' <- convertQualified env tmpl
         withTmArg env (varV1, TList TParty) args $ \x1 args ->
@@ -767,7 +788,7 @@ convertExpr env0 e = do
           withTmArg env (varV1, t) args $ \x args ->
             pure (ESome t x, args)
     go env (Var x) args
-        | Just internals <- MS.lookup modName internalFunctions
+        | Just internals <- MS.lookup modName (internalFunctions $ envLfVersion env)
         , is x `elem` internals
         = unsupported "Direct call to internal function" x
         | is x == "()" = fmap (, args) $ pure EUnit
