2ac2fed37cd6a27b12cffe0237c0fc61ca3ba548
Brian Healey <brian.healey@digitalasset.com>
2019-05-31 16:48:08 -0400

    backout trivial commit that failed copyright check

--- extractor/src/test/lib/scala/com/digitalasset/extractor/config/Generators.scala
+++ extractor/src/test/lib/scala/com/digitalasset/extractor/config/Generators.scala
@@ -1,5 +1,5 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0 
+// SPDX-License-Identifier: Apache-2.0
 
 package com.digitalasset.extractor.config
 
