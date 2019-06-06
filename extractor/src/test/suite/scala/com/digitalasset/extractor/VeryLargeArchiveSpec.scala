22f941532f19a2e6a6ab348173e1e071aa697fc0
Stephen Compall <stephen.compall@daml.com>
2019-06-06 15:46:27 -0400

    extractor: comment on the size choices

--- extractor/src/test/suite/scala/com/digitalasset/extractor/VeryLargeArchiveSpec.scala
+++ extractor/src/test/suite/scala/com/digitalasset/extractor/VeryLargeArchiveSpec.scala
@@ -32,6 +32,17 @@ class VeryLargeArchiveSpec
     finally Await.result(extractor.shutdown(), Duration.Inf) // as with ExtractorFixture#kill
   }
 
+  // there are a couple goals with these choices:
+  //  1. ensure that we can actually observe failure when the limit is too low
+  //  2. ensure that no other system we aren't reconfiguring doesn't impose a
+  //     similar limit to the original 50MiB limit
+  //
+  // A smaller test case (with smaller numbers below) would satisfy 1, but not 2.
+  //
+  // That said, the only purpose is to *ensure that failure can be observed*;
+  // future editors of this test should not feel obliged to synthesize a failure
+  // if the system design has really changed so failures of this nature cannot
+  // happen.
   val failMB = 50
   val successMB = 60
 
