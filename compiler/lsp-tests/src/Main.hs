0512aee711e2609a3a0173992469dca98ce89fe5
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-06-06 14:35:26 +0200

    Move diagnostic tests from language-server-tests to new lsp-tests

--- compiler/lsp-tests/src/Main.hs
+++ compiler/lsp-tests/src/Main.hs
@@ -23,8 +23,8 @@ main = do
     damlcPath <- locateRunfiles $
         mainWorkspace </> "daml-foundations" </> "daml-tools" </>
         "da-hs-damlc-app" </> "da-hs-damlc-app"
-    let runNoScenarios s = withTempDir $ \dir -> runSessionWithConfig conf (damlcPath <> " ide --scenarios=no") fullCaps dir s
-        _runWithScenarios s
+    let run s = withTempDir $ \dir -> runSessionWithConfig conf (damlcPath <> " ide --scenarios=no") fullCaps dir s
+        runScenarios s
             -- We are currently seeing issues with GRPC FFI calls which make everything
             -- that uses the scenario service extremely flaky and forces us to disable it on
             -- CI. Once https://github.com/digital-asset/daml/issues/1354 is fixed we can
@@ -32,7 +32,7 @@ main = do
             | isWindows = pure ()
             | otherwise = withTempDir $ \dir -> runSessionWithConfig conf (damlcPath <> " ide --scenarios=yes") fullCaps dir s
     defaultMain $ testGroup "LSP"
-        [ diagnosticTests runNoScenarios
+        [ diagnosticTests run runScenarios
         ]
     where
         conf = defaultConfig
@@ -40,29 +40,24 @@ main = do
             -- which can be quite useful for debugging.
             -- { logMessages = True, logColor = False, logStdErr = True }
 
-diagnosticTests :: (forall a. Session a -> IO a) -> TestTree
-diagnosticTests run = testGroup "diagnostics"
-    [ testCase "diagnostics on parse error" $ run $ do
+diagnosticTests
+    :: (forall a. Session a -> IO a)
+    -> (Session () -> IO ())
+    -> TestTree
+diagnosticTests run runScenarios = testGroup "diagnostics"
+    [ testCase "diagnostics disappear after error is fixed" $ run $ do
           test <- openDoc' "Test.daml" damlId $ T.unlines
               [ "daml 1.2"
               , "module Test where"
               , "f 1"
               ]
-          expectDiagnostics [(DsError, (2, 0), "Parse error")]
-          closeDoc test
-    , testCase "diagnostics disappear after error is fixed" $ run $ do
-          test <- openDoc' "Test.daml" damlId $ T.unlines
-              [ "daml 1.2"
-              , "module Test where"
-              , "f 1"
-              ]
-          expectDiagnostics [(DsError, (2,0), "Parse error")]
+          expectDiagnostics [("Test.daml", [(DsError, (2, 0), "Parse error")])]
           replaceDoc test $ T.unlines
               [ "daml 1.2"
               , "module Test where"
               , "f = ()"
               ]
-          expectDiagnostics []
+          expectDiagnostics [("Test.daml", [])]
           closeDoc test
     , testCase "diagnostics appear after introducing an error" $ run $ do
           test <- openDoc' "Test.daml" damlId $ T.unlines
@@ -75,7 +70,121 @@ diagnosticTests run = testGroup "diagnostics"
               , "module Test where"
               , "f 1"
               ]
-          expectDiagnostics [(DsError, (2, 0), "Parse error")]
+          expectDiagnostics [("Test.daml", [(DsError, (2, 0), "Parse error")])]
           closeDoc test
+    , testCase "failed name resolution" $ run $ do
+          main' <- openDoc' "Main.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Main where"
+              , "add : Int -> Int -> Int"
+              , "add a b = ab + b"
+              , "succ : Int -> Int"
+              , "succ a = abdd 1 a"
+              ]
+          expectDiagnostics
+              [ ( "Main.daml"
+                , [ (DsError, (3, 10), "Variable not in scope: ab")
+                  , (DsError, (5, 9), "Variable not in scope: abdd")
+                  ]
+                )
+              ]
+          closeDoc main'
+    , testCase "import cycle" $ run $ do
+          a <- openDoc' "A.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module A where"
+              , "import B"
+              ]
+          b <- openDoc' "B.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module B where"
+              , "import A"
+              ]
+          expectDiagnostics
+              [ ( "A.daml"
+                , [(DsError, (2, 7), "Cyclic module dependency between A, B")]
+                )
+              , ( "B.daml"
+                , [(DsError, (2, 7), "Cyclic module dependency between A, B")]
+                )
+              ]
+          closeDoc b
+          closeDoc a
+    , testCase "import error" $ run $ do
+          main' <- openDoc' "Main.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Main where"
+              , "import Oops"
+              ]
+          expectDiagnostics
+              [("Main.daml", [(DsError, (2, 7), "Could not find module 'Oops'")])]
+          closeDoc main'
+    , testCase "multi module funny" $ run $ do
+          libsC <- openDoc' "Libs/C.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Libs.C where"
+              ]
+          libsB <- openDoc' "Libs/B.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Libs.B where"
+              , "import Libs.C"
+              ]
+          libsA <- openDoc' "Libs/A.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Libs.A where"
+              , "import C"
+              ]
+          expectDiagnostics
+              [ ( "Libs/A.daml"
+                , [(DsError, (2, 7), "Could not find module 'C'")]
+                )
+              , ( "Libs/B.daml"
+                , [(DsWarning, (2, 0), "import of 'Libs.C' is redundant")]
+                )
+              ]
+          closeDoc libsA
+          closeDoc libsB
+          closeDoc libsC
+    , testCase "parse error" $ run $ do
+          test <- openDoc' "Test.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Test where"
+              , "f 1"
+              ]
+          expectDiagnostics [("Test.daml", [(DsError, (2, 0), "Parse error")])]
+          closeDoc test
+    , testCase "type error" $ run $ do
+          main' <- openDoc' "Main.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Main where"
+              , "oops = 1 + \"foo\""
+              ]
+          expectDiagnostics
+              [ ( "Main.daml"
+                , [ ( DsError
+                    , (2, 11)
+                    , "Couldn't match expected type 'Int' with actual type 'Text'"
+                    )
+                  ]
+                )
+              ]
+          closeDoc main'
+    , testCase "scenario error" $ runScenarios $ do
+          main' <- openDoc' "Main.daml" damlId $ T.unlines
+              [ "daml 1.2"
+              , "module Main where"
+              , "template Agree with p1 : Party; p2 : Party where"
+              , "  signatory [p1, p2]"
+              , "myScenario = scenario do"
+              , "  foo <- getParty \"Foo\""
+              , "  alice <- getParty \"Alice\""
+              , "  submit foo $ create $ Agree with p1 = foo, p2 = alice"
+              ]
+          expectDiagnostics
+              [ ( "Main.daml"
+                , [(DsError, (4, 0), "missing authorization from 'Alice'")]
+                )
+              ]
+          closeDoc main'
     ]
 
