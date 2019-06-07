3e8f6a67987a2e7eaec208748d8af31e07de28af
Anup Kalburgi <anup.kalburgi@digitalasset.com>
2019-06-07 10:04:23 -0400

    Removing unused extenstions

--- daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Visual.hs
+++ daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Visual.hs
@@ -1,8 +1,6 @@
 -- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE TemplateHaskell     #-}
 {-# LANGUAGE ApplicativeDo       #-}
 
 -- | Main entry-point of the DAML compiler
