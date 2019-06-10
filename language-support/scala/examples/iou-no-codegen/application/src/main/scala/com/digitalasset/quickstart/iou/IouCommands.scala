1e39a4f8f3fde3d605cbedf592eb32e22da6b02b
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-06-10 12:23:31 -0400

    Fixing formatting

--- language-support/scala/examples/iou-no-codegen/application/src/main/scala/com/digitalasset/quickstart/iou/IouCommands.scala
+++ language-support/scala/examples/iou-no-codegen/application/src/main/scala/com/digitalasset/quickstart/iou/IouCommands.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.digitalasset.quickstart.iou
 
 import com.digitalasset.ledger.api.v1.commands.Command.Command
