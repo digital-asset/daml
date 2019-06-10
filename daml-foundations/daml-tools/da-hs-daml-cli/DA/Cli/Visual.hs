d9c4dd9c4e0cafc715712a660c545eee7d1bb6c3
Anup Kalburgi <anup.kalburgi@digitalasset.com>
2019-06-10 14:47:53 -0400

    Connected but not really

--- daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Visual.hs
+++ daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Visual.hs
@@ -20,10 +20,10 @@ import qualified DA.Daml.LF.Proto3.Archive as Archive
 import qualified Data.ByteString.Lazy as BSL
 import Text.Dot
 import qualified Data.ByteString as B
-import Control.Monad.Except
+-- import Control.Monad.Except
 import Codec.Archive.Zip
 import System.FilePath
-import qualified Data.List as DL
+-- import qualified Data.List as DL
 
 data Action = ACreate (LF.Qualified LF.TypeConName) 
             | AExercise (LF.Qualified LF.TypeConName) LF.ChoiceName deriving (Eq, Ord, Show )
@@ -104,11 +104,27 @@ prettyAction :: Action -> String
 prettyAction (ACreate  (LF.Qualified _ _ tpl) )  = DAP.renderPretty tpl 
 prettyAction (AExercise  (LF.Qualified _ _ tpl) _ ) = DAP.renderPretty tpl
 
+templateInAction :: Action -> LF.TypeConName
+templateInAction (ACreate  (LF.Qualified _ _ tpl) ) = tpl
+templateInAction (AExercise  (LF.Qualified _ _ tpl) _ ) = tpl
+
 -- prettyTemplateWithAction :: (LF.TypeConName ,DS.Set Action) -> String
 -- prettyTemplateWithAction (tplCon, actions) =  DAP.renderPretty tplCon ++ "->" ++ show(DS.map prettyAction actions)
 
 src :: String -> Dot NodeId
 src label = node [ ("shape","none"),("label",label) ]
+
+src' :: (LF.TypeConName, Set.Set Action) -> [(String, String)]                                              -- (b -> (String, String))
+src' (tc, _) = [ ("shape","none"),("label",DAP.renderPretty tc) ]
+
+
+templatePairs :: (LF.TypeConName, Set.Set Action) -> (LF.TypeConName , (LF.TypeConName , Set.Set Action))   -- [(a, b)]
+templatePairs (tc, actions) = (tc , (tc, actions))
+
+dotGraphTemplateAndActionHelper' :: (LF.TypeConName, Set.Set Action) -> [LF.TypeConName]      -- b -> [a]
+dotGraphTemplateAndActionHelper' (_tplCon, actions) = actionsStrs
+    where 
+        actionsStrs = Set.elems $ Set.map templateInAction actions
 
 dotGraphTemplateAndActionHelper :: (LF.TypeConName, Set.Set Action) -> (String, [String])
 dotGraphTemplateAndActionHelper (tplCon, actions) = (tplStr, actionsStrs)
@@ -119,10 +135,12 @@ dotGraphTemplateAndActionHelper (tplCon, actions) = (tplStr, actionsStrs)
 dotTemplateNodes :: LF.TypeConName -> (String, Dot NodeId)
 dotTemplateNodes tplCon = (DAP.renderPretty tplCon , src $ DAP.renderPretty tplCon)
 
-nodeToDot :: NodeId -> String -> Dot ()
-nodeToDot a b = do
-    n2 <- src b
-    a .->. n2
+
+
+-- nodeToDot :: NodeId -> String -> Dot ()
+-- nodeToDot a b = do
+--     n2 <- src b
+--     a .->. n2
 
 errorOnLeft :: Show a => String -> Either a b -> IO b
 errorOnLeft desc = \case
@@ -138,17 +156,23 @@ execVisual darFilePath dalfFile = do
     let modules = NM.toList $ LF.packageModules lfPkg
         res = concatMap (moduleAndTemplates world) modules
         -- ppString = map prettyTemplateWithAction res
-        tpls =  map (dotTemplateNodes . fst) res
-        dotThing = map dotGraphTemplateAndActionHelper res
-
-    putStrLn $ showDot $ do
-        attribute ("rankdir","LR")
-        forM_ dotThing $ \(tplName, actions) -> do
-            case DL.find (\t ->  fst t == tplName ) tpls of 
-                Just (_, tplNode) -> do 
-                    tNode <- tplNode
-                    mapM (\a -> nodeToDot tNode a ) actions
-                Nothing -> error "Unknow teplate referenced"
+        _tpls =  map (dotTemplateNodes . fst) res
+        dotThing = map dotGraphTemplateAndActionHelper res -- ListOfTemplateAndActions
+        
+        pairs = map templatePairs res
+    putStrLn (show dotThing)  
+    putStrLn $ showDot $ do 
+        netlistGraph (src')
+                     (dotGraphTemplateAndActionHelper' )
+                     (pairs)
+    -- putStrLn $ showDot $ do
+    --     attribute ("rankdir","LR")
+    --     forM_ dotThing $ \(tplName, actions) -> do
+    --         case DL.find (\t ->  fst t == tplName ) tpls of 
+    --             Just (_, tplNode) -> do 
+    --                 tNode <- tplNode
+    --                 mapM (\a -> nodeToDot tNode a ) actions
+    --             Nothing -> error "Unknow teplate referenced"
 
 
 
