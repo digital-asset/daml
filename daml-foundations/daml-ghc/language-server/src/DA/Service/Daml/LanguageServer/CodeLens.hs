c05a0f01b0b7161326827b577449970312b483fa
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-06-14 01:46:05 +0200

    Remove DA.Service.DAML.LanguageServer.Common (#1651)

--- daml-foundations/daml-ghc/language-server/src/DA/Service/Daml/LanguageServer/CodeLens.hs
+++ daml-foundations/daml-ghc/language-server/src/DA/Service/Daml/LanguageServer/CodeLens.hs
@@ -8,16 +8,15 @@ module DA.Service.Daml.LanguageServer.CodeLens
     ( handle
     ) where
 
-import           Development.IDE.LSP.Protocol
 import Language.Haskell.LSP.Types
-
+import qualified Data.Aeson as Aeson
 import Data.Foldable
 import Data.Maybe
 import qualified Data.Text as T
 import qualified DA.Service.Daml.Compiler.Impl.Handle as Compiler
-import           DA.Service.Daml.LanguageServer.Common
 import qualified Development.IDE.Logger as Logger
 import Development.IDE.Types.Diagnostics
+import Development.IDE.Types.LSP
 
 -- | Gather code lenses like scenario execution for a DAML file.
 handle
@@ -34,3 +33,19 @@ handle loggerH compilerH (CodeLensParams (TextDocumentIdentifier uri)) = do
         Nothing       -> pure []
 
     pure $ List $ toList mbResult
+
+-- | Convert a compiler virtual resource into a code lens.
+virtualResourceToCodeLens
+    :: (Range, T.Text, VirtualResource)
+    -> Maybe CodeLens
+virtualResourceToCodeLens (range, title, vr) =
+ Just CodeLens
+    { _range = range
+    , _command = Just $ Command
+        "Scenario results"
+        "daml.showResource"
+        (Just $ List
+              [ Aeson.String title
+              , Aeson.String $ Compiler.virtualResourceToUri vr])
+    , _xdata = Nothing
+    }
