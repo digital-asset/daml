873f6eb45f879181a69596d4b1f370e736b32810
David Millar-Durrant <dmillardurrant@gmail.com>
2019-06-14 16:37:23 +0200

    Added copyright header

--- compiler/haskell-ide-core/extension/src/extension.ts
+++ compiler/haskell-ide-core/extension/src/extension.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import * as path from 'path';
 import { workspace, ExtensionContext, window } from 'vscode';
 
