c3e27626346bc41060641d7f7ef08d7bb8342f5e
Fran <231829+associahedron@users.noreply.github.com>
2019-06-17 16:49:37 +0100

    Delete da-assistant and sdk directories. (#1721)
    * Delete da-assistant and sdk directories.
    
    * Remove references to da-assistant
    
    * Remove reference to da-sdk-head

--- da-assistant/apps/Main.hs
+++
@@ -1,6 +0,0 @@
--- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
--- SPDX-License-Identifier: Apache-2.0
-
-module Main (main) where
-
-import DA.Sdk.Cli (main)
