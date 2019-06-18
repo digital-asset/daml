1963b5b684a403794690c170305538c0cb8d4850
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-06-18 04:14:38 -0400

    Adding dependencies

---
+++ ledger-api/http-json/src/main/scala/com/digitalasset/ledger/json/domain.scala
@@ -0,0 +1,6 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.ledger.json
+
+class domain {}
