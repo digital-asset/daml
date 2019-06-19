06b9c43bf4201021f8f288ae1ef40df0d2499c77
Anup Kalburgi <anup.kalburgi@digitalasset.com>
2019-06-19 11:04:59 -0400

    Adding the real example and removing the unwanted tests

--- daml-foundations/daml-tools/da-hs-daml-cli/tests/VisualTest.hs
+++ daml-foundations/daml-tools/da-hs-daml-cli/tests/VisualTest.hs
@@ -7,7 +7,6 @@ module VisualTest
 
 import Test.Tasty
 import Test.Tasty.HUnit
-import System.Directory
 import System.IO.Extra
 import DA.Cli.Visual
 
@@ -21,8 +20,6 @@ tests = testGroup "Tests" [unitTests]
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [
-    testCase "file exists" (doesFileExist "daml-foundations/daml-tools/da-hs-daml-cli/visual-test-daml.dar" @? "missing dar file to process") ,
-    testCase "file exists" (doesFileExist "daml-foundations/daml-tools/da-hs-daml-cli/tests/res/out.dot" @? "missing the expected dot file to check") ,
     testCase "dot file check" $ do
         withTempFile $ \path -> do
             let darPath = "daml-foundations/daml-tools/da-hs-daml-cli/visual-test-daml.dar"
