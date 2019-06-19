11acde6c9979c3d5935482384c0a56f5013d719f
Stephen Compall <stephen.compall@daml.com>
2019-06-19 17:37:20 +0200

    adding that ghc extension didn't make sense

--- compiler/daml-lf-proto/src/DA/Daml/LF/Proto3/Error.hs
+++ compiler/daml-lf-proto/src/DA/Daml/LF/Proto3/Error.hs
@@ -1,7 +1,6 @@
 -- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-{-# LANGUAGE ConstraintKinds #-}
 module DA.Daml.LF.Proto3.Error
     ( Decode
     , Error(..)
