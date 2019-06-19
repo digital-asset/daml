7574d179921f8de806e3cfed0c2568f454c9eacb
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-06-19 15:06:47 +0200

    Address https://github.com/digital-asset/daml/pull/1762#pullrequestreview-251678791

--- release/src/Util.hs
+++ release/src/Util.hs
@@ -382,9 +382,9 @@ isReleaseCommit = do
     let isRelease = "VERSION" `elem` files
                  && "unreleased.rst" `elem` files
                  && "docs/source/support/release-notes.rst" `elem` files
-                 && length files == 2
+                 && length files == 3
     if "VERSION" `elem` files && not isRelease
-    then throwIO $ CIException "Release commit should only change VERSION and release-notes.rst."
+    then throwIO $ CIException "Release commit should only change VERSION, release-notes.rst and unreleased.rst."
     else return isRelease
 
 runFastLoggingT :: LoggingT IO c -> IO c
