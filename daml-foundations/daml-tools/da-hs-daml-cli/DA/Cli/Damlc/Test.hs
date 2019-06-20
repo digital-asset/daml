efba6b3ba884db4dcbfa5ebac9c1626a24482ea2
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-06-20 10:37:52 +0200

    Run a final sync in damlc test
    We have seen a few segfaults and assertion failures at the end of test
    suites that use damlc test. I haven’t been able to reproduce them
    locally but I’m hoping that this fixes them. It looks like this might
    also been fixed in newer gRPC releases but my attempts to upgrade
    turned into a mess that is at least going to take quite some time to
    fix, see https://github.com/digital-asset/daml/pull/1771.

--- daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Damlc/Test.hs
+++ daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Damlc/Test.hs
@@ -48,6 +48,10 @@ execTest inFiles color mbJUnitOutput cliOptions = do
     Compiler.withIdeState opts loggerH eventLogger $ \h -> do
         let lfVersion = Compiler.optDamlLfVersion cliOptions
         testRun h inFiles lfVersion color mbJUnitOutput
+        -- Run synchronously at the end to make sure that all gRPC requests
+        -- finish properly. We have seen segfaults on CI sometimes
+        -- which are probably caused by not doing this.
+        CompilerService.runActionSync h (pure ())
         diags <- CompilerService.getDiagnostics h
         when (any ((Just DsError ==) . _severity . snd) diags) exitFailure
 
