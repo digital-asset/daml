5991fbcee8363e5f9d0a1f6473c35888e10247ab
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-06-21 09:27:40 +0200

    Use BadDependency consistently (#1787)
    Previously we had a weird mix of using ExceptT to shortcircuit on
    failed dependencies where the list of diagnostics was always empty and
    throwing BadDependency exceptions.
    
    This PR switches everything over to use BadDependency for failed
    dependencies and removes a lot of conversions from one style to the
    other.

--- daml-foundations/daml-ghc/damldoc/src/DA/Daml/GHC/Damldoc/HaddockParse.hs
+++ daml-foundations/daml-ghc/damldoc/src/DA/Daml/GHC/Damldoc/HaddockParse.hs
@@ -20,8 +20,8 @@ import qualified "ghc-lib-parser" DynFlags                        as DF
 import           "ghc-lib-parser" Bag (bagToList)
 
 import           Control.Monad.Except             as Ex
+import Control.Monad.Trans.Maybe
 import           Data.Char (isSpace)
-import Data.Either.Extra
 import           Data.List.Extra
 import           Data.Maybe
 import qualified Data.Map.Strict as MS
@@ -125,13 +125,13 @@ haddockParse opts f = ExceptT $ do
   service <- Service.initialise Service.mainRule (const $ pure ()) Logger.makeNopHandle opts vfs
   Service.setFilesOfInterest service (Set.fromList f)
   parsed  <- Service.runAction service $
-             Ex.runExceptT $
+             runMaybeT $
              -- We only _parse_ the modules, the documentation generator is syntax-directed
              do deps <- Service.usesE Service.GetDependencies f
                 Service.usesE Service.GetParsedModule $ nubOrd $ f ++ concatMap Service.transitiveModuleDeps deps
                 -- The DAML compiler always parses with Opt_Haddock on
   diags <- Service.getDiagnostics service
-  pure (mapLeft (const diags) parsed)
+  pure (maybe (Left diags) Right parsed)
 
 -- | Pair up all doc decl.s from a parsed module with their referred-to
 --   declaration. See Haddock's equivalent Haddock.Interface.Create.collectDocs
