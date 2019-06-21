4db274c396ed8d356ce713127bc7af0aa17b9772
associahedron <231829+associahedron@users.noreply.github.com>
2019-06-21 17:24:39 +0100

    Copyright header

--- daml-assistant/daml-helper/src/DamlHelper/Signals.hs
+++ daml-assistant/daml-helper/src/DamlHelper/Signals.hs
@@ -1,6 +1,9 @@
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
 {-# LANGUAGE CPP #-}
 
--- | Module that turns SIGTERM into a UserInterrput exception in unix.
+-- | Module that turns SIGTERM into a UserInterrupt exception in Unix.
 module DamlHelper.Signals
     ( installSignalHandlers
     ) where
