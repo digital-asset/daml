b9c225bdfd123ae2cf3456980fb760ce074f71b5
Neil Mitchell <neil.mitchell@digitalasset.com>
2019-06-23 13:36:00 +0100

    Move most of the pretty printing inside hie-core

--- compiler/hie-core/src/Development/IDE/Types/Diagnostics.hs
+++ compiler/hie-core/src/Development/IDE/Types/Diagnostics.hs
@@ -39,6 +39,9 @@ import Language.Haskell.LSP.Types as LSP (
   , DiagnosticRelatedInformation(..)
   )
 import Language.Haskell.LSP.Diagnostics
+import Data.Text.Prettyprint.Doc.Render.Text
+import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Terminal
+import Data.Text.Prettyprint.Doc.Render.Terminal (Color(..), color, colorDull)
 
 import Development.IDE.Types.Location
 
@@ -155,3 +158,46 @@ filterDiagnostics ::
     DiagnosticStore
 filterDiagnostics keep =
     Map.filterWithKey (\uri _ -> maybe True (keep . toNormalizedFilePath) $ uriToFilePath' $ fromNormalizedUri uri)
+
+
+
+-- | Label a document.
+slabel_ :: String -> Doc a -> Doc a
+slabel_ t d = nest 2 $ sep [pretty t, d]
+
+-- | The layout options used for the SDK assistant.
+cliLayout ::
+       Int
+    -- ^ Rendering width of the pretty printer.
+    -> LayoutOptions
+cliLayout renderWidth = LayoutOptions
+    { layoutPageWidth = AvailablePerLine renderWidth 0.9
+    }
+
+-- | Render without any syntax annotations
+srenderPlain :: Doc ann -> T.Text
+srenderPlain = renderStrict . layoutSmart (cliLayout defaultTermWidth)
+
+-- | Render a 'Document' as an ANSII colored string.
+srenderColored :: Doc SyntaxClass -> T.Text
+srenderColored =
+    Terminal.renderStrict .
+    layoutSmart defaultLayoutOptions { layoutPageWidth = AvailablePerLine 100 1.0 } .
+    fmap toAnsiStyle
+  where
+    toAnsiStyle ann = case ann of
+        OperatorSC -> colorDull Red
+        KeywordSC -> colorDull Green
+        PredicateSC -> colorDull Magenta
+        ConstructorSC -> color Blue
+        TypeSC -> color Green
+        ErrorSC -> color Red
+        WarningSC -> color Yellow
+        InfoSC -> color Blue
+        HintSC -> color Magenta
+        LinkSC _ _ -> color Green
+        IdSC _ -> mempty
+        OnClickSC _ -> mempty
+
+defaultTermWidth :: Int
+defaultTermWidth = 80
--- libs-haskell/prettyprinter-syntax/src/Data/Text/Prettyprint/Doc/Syntax.hs
+++ libs-haskell/prettyprinter-syntax/src/Data/Text/Prettyprint/Doc/Syntax.hs
@@ -3,18 +3,8 @@
 
 module Data.Text.Prettyprint.Doc.Syntax
     ( SyntaxClass(..)
-    -- prefixing these names with an 's' is not pleasant
-    -- but we have duplicates of the functions with identical signatures
-    -- and different semantics at DA.Pretty, so important to try and disambiguate
-    , slabel_
-    , srenderPlain
-    , srenderColored
     ) where
 
-import Data.Text.Prettyprint.Doc
-import Data.Text.Prettyprint.Doc.Render.Text
-import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Terminal
-import Data.Text.Prettyprint.Doc.Render.Terminal (Color(..), color, colorDull)
 import qualified Data.Text as T
 
 -- | Classes of syntax elements, which are used for highlighting.
@@ -35,44 +25,3 @@ data SyntaxClass
     -- ^ @IdSC id@: Identifier for the node. For linking into.
     | OnClickSC T.Text
     deriving (Eq, Ord, Show)
-
--- | Label a document.
-slabel_ :: String -> Doc a -> Doc a
-slabel_ t d = nest 2 $ sep [pretty t, d]
-
--- | The layout options used for the SDK assistant.
-cliLayout ::
-       Int
-    -- ^ Rendering width of the pretty printer.
-    -> LayoutOptions
-cliLayout renderWidth = LayoutOptions
-    { layoutPageWidth = AvailablePerLine renderWidth 0.9
-    }
-
--- | Render without any syntax annotations
-srenderPlain :: Doc ann -> T.Text
-srenderPlain = renderStrict . layoutSmart (cliLayout defaultTermWidth)
-
--- | Render a 'Document' as an ANSII colored string.
-srenderColored :: Doc SyntaxClass -> T.Text
-srenderColored =
-    Terminal.renderStrict .
-    layoutSmart defaultLayoutOptions { layoutPageWidth = AvailablePerLine 100 1.0 } .
-    fmap toAnsiStyle
-  where
-    toAnsiStyle ann = case ann of
-        OperatorSC -> colorDull Red
-        KeywordSC -> colorDull Green
-        PredicateSC -> colorDull Magenta
-        ConstructorSC -> color Blue
-        TypeSC -> color Green
-        ErrorSC -> color Red
-        WarningSC -> color Yellow
-        InfoSC -> color Blue
-        HintSC -> color Magenta
-        LinkSC _ _ -> color Green
-        IdSC _ -> mempty
-        OnClickSC _ -> mempty
-
-defaultTermWidth :: Int
-defaultTermWidth = 80
