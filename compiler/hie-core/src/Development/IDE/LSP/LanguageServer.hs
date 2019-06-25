8d7ae6c08763077a964ec289ac3ea9383622b58f
Neil Mitchell <neil.mitchell@digitalasset.com>
2019-06-25 14:47:01 +0100

    Avoid the Protocol module

--- compiler/hie-core/src/Development/IDE/LSP/LanguageServer.hs
+++ compiler/hie-core/src/Development/IDE/LSP/LanguageServer.hs
@@ -10,7 +10,7 @@ module Development.IDE.LSP.LanguageServer
     ( runLanguageServer
     ) where
 
-import           Development.IDE.LSP.Protocol
+import           Language.Haskell.LSP.Types
 import           Development.IDE.LSP.Server hiding (runServer)
 import qualified Language.Haskell.LSP.Control as LSP
 import qualified Language.Haskell.LSP.Core as LSP
