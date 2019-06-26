41574d33f76619cf4770d478d3027c7ffc81932b
Remy <remy.haemmerle@daml.com>
2019-06-26 13:50:25 +0200

    java-codegen: Add support for enum types (#1648)
    * java-codegen: add support for Enum
    
    * Address Stefano's comment

---
+++ language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/EnumClass.scala
@@ -0,0 +1,102 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.daml.lf.codegen.backend.java.inner
+
+import com.daml.ledger.javaapi
+import com.digitalasset.daml.lf.data.Ref.Identifier
+import com.digitalasset.daml.lf.iface
+import com.squareup.javapoet._
+import com.typesafe.scalalogging.StrictLogging
+import javax.lang.model.element.Modifier
+
+import scala.collection.JavaConverters._
+
+private[inner] object EnumClass extends StrictLogging {
+
+  def generate(
+      className: ClassName,
+      identifier: Identifier,
+      enum: iface.Enum,
+  ): TypeSpec = {
+    TrackLineage.of("enum", className.simpleName()) {
+      logger.info("Start")
+      val enumType = TypeSpec.enumBuilder(className).addModifiers(Modifier.PUBLIC)
+      enum.constructors.foreach(c => enumType.addEnumConstant(c.toUpperCase()))
+      enumType.addField(generateValuesArray(enum))
+      enumType.addMethod(generateEnumsMapBuilder(enum))
+      enumType.addField(generateEnumsMap(className))
+      enumType.addMethod(generateFromValue(className, enum))
+      enumType.addMethod(generateToValue(className))
+      logger.debug("End")
+      enumType.build()
+    }
+  }
+
+  private def generateValuesArray(enum: iface.Enum): FieldSpec = {
+    val fieldSpec = FieldSpec.builder(ArrayTypeName.of(classOf[javaapi.data.DamlEnum]), "__values$")
+    fieldSpec.addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+    val constructorValues = enum.constructors
+      .map(c => CodeBlock.of("new $T($S)", classOf[javaapi.data.DamlEnum], c))
+      .asJava
+    fieldSpec.initializer(constructorValues.stream().collect(CodeBlock.joining(", ", "{", "}")))
+    fieldSpec.build()
+  }
+
+  private def generateEnumsMap(className: ClassName): FieldSpec =
+    FieldSpec
+      .builder(classOf[java.util.Map[Any, Any]], "__enums$")
+      .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+      .initializer("$T.__buildEnumsMap$$()", className)
+      .build()
+
+  private def generateEnumsMapBuilder(enum: iface.Enum): MethodSpec = {
+    val builder = MethodSpec.methodBuilder("__buildEnumsMap$")
+    builder.addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+    builder.returns(classOf[java.util.Map[Any, Any]])
+    builder.addStatement(
+      "$T m = new $T()",
+      classOf[java.util.Map[Any, Any]],
+      classOf[java.util.HashMap[Any, Any]])
+    enum.constructors.foreach(c => builder.addStatement(s"""m.put("$c", ${c.toUpperCase()})"""))
+    builder.addStatement("return m")
+    builder.build()
+  }
+
+  private def generateFromValue(
+      className: ClassName,
+      enum: iface.Enum
+  ): MethodSpec = {
+    logger.debug(s"Generating fromValue static method for $enum")
+
+    MethodSpec
+      .methodBuilder("fromValue")
+      .addModifiers(Modifier.STATIC, Modifier.PUBLIC, Modifier.FINAL)
+      .returns(className)
+      .addParameter(classOf[javaapi.data.Value], "value$")
+      .addStatement(
+        "$T constructor$$ = value$$.asEnum().orElseThrow(() -> new $T($S)).getConstructor()",
+        classOf[String],
+        classOf[IllegalArgumentException],
+        s"Expected DamlEnum to build an instance of the  ${className.simpleName()}"
+      )
+      .addStatement(
+        "if (!$T.__enums$$.containsKey(constructor$$)) throw new $T($S + constructor$$)",
+        className,
+        classOf[IllegalArgumentException],
+        s"Expected a DamlEnum with ${className.simpleName()} constructor, found "
+      )
+      .addStatement("return ($T) $T.__enums$$.get(constructor$$)", className, className)
+      .build()
+
+  }
+
+  private def generateToValue(className: ClassName): MethodSpec =
+    MethodSpec
+      .methodBuilder("toValue")
+      .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+      .returns(classOf[javaapi.data.DamlEnum])
+      .addStatement("return $T.__values$$[ordinal()]", className)
+      .build()
+
+}
