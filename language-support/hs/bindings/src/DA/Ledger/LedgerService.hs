b03d184c8c4a4f646a948ca7ada7ebf72913757c
Nick Chapman <nick.chapman@digitalasset.com>
2019-06-26 09:42:18 +0100

    Define LedgerService using ReaderT to avoid boilerplate

--- language-support/hs/bindings/src/DA/Ledger/LedgerService.hs
+++ language-support/hs/bindings/src/DA/Ledger/LedgerService.hs
@@ -6,39 +6,21 @@ module DA.Ledger.LedgerService (
     LedgerService, runLedgerService, makeLedgerService, TimeoutSeconds(..),
     ) where
 
-import Prelude hiding (fail)
-import Control.Monad (ap,liftM)
-import Control.Monad.Fail (MonadFail,fail)
-import Control.Monad.IO.Class (MonadIO,liftIO)
+import Control.Monad.Fail (MonadFail)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Reader (ReaderT(..),runReaderT)
 import DA.Ledger.Retry (ledgerRetry)
 import Network.GRPC.HighLevel.Generated
 
 newtype TimeoutSeconds = TimeoutSeconds { unTimeoutSeconds :: Int } deriving Num
 
-newtype LedgerService a =
-    LedgerService { runLedgerService :: TimeoutSeconds -> ClientConfig -> IO a }
-    --deriving (Monad)
+type Context = (TimeoutSeconds,ClientConfig)
 
--- TODO: How do we avoid the boiler plate here?
+newtype LedgerService a = LedgerService (ReaderT Context IO a)
+    deriving (Functor,Applicative,Monad,MonadFail,MonadIO)
 
-instance Functor LedgerService where fmap = liftM
-instance Applicative LedgerService where pure = return; (<*>) = ap
-
-instance Monad LedgerService where
-    return a = LedgerService $ \_ _ -> return a
-    (>>=) = bind
-
-instance MonadIO LedgerService where
-    liftIO io = LedgerService $ \_ _ -> io
-
-instance MonadFail LedgerService where
-    fail s = LedgerService $ \_ _ -> fail s
+runLedgerService :: LedgerService a -> TimeoutSeconds -> ClientConfig -> IO a
+runLedgerService (LedgerService r) ts cc = runReaderT r (ts,cc)
 
 makeLedgerService :: (TimeoutSeconds -> ClientConfig -> IO a) -> LedgerService a
-makeLedgerService f = LedgerService $ \t cc -> do ledgerRetry (f t cc)
-
-bind :: LedgerService a -> (a -> LedgerService b) -> LedgerService b
-bind m f =
-    LedgerService $ \to cc -> do
-        a <- runLedgerService m to cc
-        runLedgerService (f a) to cc
+makeLedgerService f = LedgerService $ ReaderT $ \(ts,cc) -> ledgerRetry $ f ts cc
