e4479a939ea8ef930f7f0a11ad307371ac63e306
Neil Mitchell <ndmitchell@gmail.com>
2019-06-28 22:04:13 +0100

    Make suggestAction do sensible figuring out if you remove the next line too

--- compiler/hie-core/src/Development/IDE/LSP/CodeAction.hs
+++ compiler/hie-core/src/Development/IDE/LSP/CodeAction.hs
@@ -18,7 +18,7 @@ import qualified Language.Haskell.LSP.Core as LSP
 import Language.Haskell.LSP.VFS
 import Language.Haskell.LSP.Messages
 import qualified Data.Rope.UTF16 as Rope
-
+import Data.Char
 import qualified Data.Text as T
 
 -- | Generate code actions.
@@ -40,15 +40,28 @@ codeAction lsp _ CodeActionParams{_textDocument=TextDocumentIdentifier uri,_cont
 
 
 suggestAction :: Maybe T.Text -> Diagnostic -> [(T.Text, [TextEdit])]
-suggestAction _ Diagnostic{..}
+suggestAction contents Diagnostic{_range=_range@Range{..},..}
+
 -- File.hs:16:1: warning:
 --     The import of `Data.List' is redundant
 --       except perhaps to import instances from `Data.List'
 --     To import instances alone, use: import Data.List()
     | "The import of " `T.isInfixOf` _message
     , " is redundant" `T.isInfixOf` _message
-    = [("Remove import", [TextEdit _range ""])]
+    , let newlineAfter = maybe False (T.isPrefixOf "\n" . T.dropWhile (\x -> isSpace x && x /= '\n') . snd . textAtPosition _end) contents
+    , let extend = newlineAfter && _character _start == 0 -- takes up an entire line, so remove the whole line
+    = [("Remove import", [TextEdit (if extend then Range _start (Position (_line _end + 1) 0) else _range) ""])]
+
 suggestAction _ _ = []
+
+
+textAtPosition :: Position -> T.Text -> (T.Text, T.Text)
+textAtPosition (Position row col) x
+    | (preRow, mid:postRow) <- splitAt row $ T.splitOn "\n" x
+    , (preCol, postCol) <- T.splitAt col mid
+        = (T.intercalate "\n" $ preRow ++ [preCol], T.intercalate "\n" $ postCol : postRow)
+    | otherwise = (x, T.empty)
+
 
 setHandlersCodeAction :: PartialHandlers
 setHandlersCodeAction = PartialHandlers $ \WithMessage{..} x -> return x{
