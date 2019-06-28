82add82bfe16fa94dd88a6ca515edd165976412d
Neil Mitchell <ndmitchell@gmail.com>
2019-06-28 21:04:18 +0100

    Push the suggestion work further up

--- compiler/hie-core/src/Development/IDE/LSP/CodeAction.hs
+++ compiler/hie-core/src/Development/IDE/LSP/CodeAction.hs
@@ -15,7 +15,6 @@ import Development.IDE.Core.Rules
 import Development.IDE.LSP.Server
 import qualified Data.HashMap.Strict as Map
 import qualified Language.Haskell.LSP.Core as LSP
-import qualified Language.Haskell.LSP.Types as LSP
 import Language.Haskell.LSP.Messages
 
 import qualified Data.Text as T
@@ -30,19 +29,21 @@ codeAction _ CodeActionParams{_textDocument=TextDocumentIdentifier uri,_context=
     -- logInfo (ideLogger ide) $ T.pack $ "Code action req: " ++ show arg
     pure $ List
         [ CACodeAction $ CodeAction title (Just CodeActionQuickFix) (Just $ List [x]) (Just edit) Nothing
-        | x <- xs, (title, edit) <- suggestAction uri x]
+        | x <- xs, (title, tedit) <- suggestAction x
+        , let edit = WorkspaceEdit (Just $ Map.singleton uri $ List tedit) Nothing
+        ]
 
 
-suggestAction :: Uri -> Diagnostic -> [(T.Text, LSP.WorkspaceEdit)]
-suggestAction uri Diagnostic{..}
+suggestAction :: Diagnostic -> [(T.Text, [TextEdit])]
+suggestAction Diagnostic{..}
 -- File.hs:16:1: warning:
 --     The import of `Data.List' is redundant
 --       except perhaps to import instances from `Data.List'
 --     To import instances alone, use: import Data.List()
     | "The import of " `T.isInfixOf` _message
     , " is redundant" `T.isInfixOf` _message
-    = [("Remove import", WorkspaceEdit (Just $ Map.singleton uri $ List [TextEdit _range ""]) Nothing)]
-suggestAction _ _ = []
+    = [("Remove import", [TextEdit _range ""])]
+suggestAction _ = []
 
 setHandlersCodeAction :: PartialHandlers
 setHandlersCodeAction = PartialHandlers $ \WithMessage{..} x -> return x{
