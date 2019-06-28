7dbae747967a5803021055adfa0a7df26eda0975
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-06-28 14:07:35 +0200

    Remove DA.Service.Daml.Compiler.Impl.Handle module
    Now we have one less thing called Handle :)

--- daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Damlc/Base.hs
+++ daml-foundations/daml-tools/da-hs-daml-cli/DA/Cli/Damlc/Base.hs
@@ -4,25 +4,18 @@
 module DA.Cli.Damlc.Base
     ( module DA.Cli.Options
     , module DA.Cli.Output
-    , CommandM
-    , Command
     , getLogger
     )
 where
 import           DA.Cli.Options
 import           DA.Cli.Output
-import           DA.Service.Daml.Compiler.Impl.Handle as Compiler
+import DA.Daml.GHC.Compiler.Options
 import qualified Data.Text as T
 import qualified DA.Service.Logger                 as Logger
 import qualified DA.Service.Logger.Impl.IO         as Logger.IO
 
-
-type CommandM = IO
-type Command  = IO ()
-
-
-getLogger :: Compiler.Options -> T.Text -> IO (Logger.Handle IO)
-getLogger Compiler.Options {optDebug} name =
+getLogger :: Options -> T.Text -> IO (Logger.Handle IO)
+getLogger Options {optDebug} name =
     if optDebug
         then Logger.IO.newStderrLogger Logger.Debug name
         else Logger.IO.newStderrLogger Logger.Warning name
