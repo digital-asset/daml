622cb6562517aed94c3c76668204e41a8cf80a06
Neil Mitchell <ndmitchell@gmail.com>
2019-06-28 22:32:40 +0100

    Code action to add GHC extensions as required

--- compiler/hie-core/src/Development/IDE/LSP/CodeAction.hs
+++ compiler/hie-core/src/Development/IDE/LSP/CodeAction.hs
@@ -10,10 +10,11 @@ module Development.IDE.LSP.CodeAction
     ) where
 
 import           Language.Haskell.LSP.Types
-
+import GHC.LanguageExtensions.Type
 import Development.IDE.Core.Rules
 import Development.IDE.LSP.Server
 import qualified Data.HashMap.Strict as Map
+import qualified Data.HashSet as Set
 import qualified Language.Haskell.LSP.Core as LSP
 import Language.Haskell.LSP.VFS
 import Language.Haskell.LSP.Messages
@@ -52,7 +53,35 @@ suggestAction contents Diagnostic{_range=_range@Range{..},..}
     , let extend = newlineAfter && _character _start == 0 -- takes up an entire line, so remove the whole line
         = [("Remove import", [TextEdit (if extend then Range _start (Position (_line _end + 1) 0) else _range) ""])]
 
+-- File.hs:22:8: error:
+--     Illegal lambda-case (use -XLambdaCase)
+-- File.hs:22:6: error:
+--     Illegal view pattern:  x -> foo
+--     Use ViewPatterns to enable view patterns
+-- File.hs:26:8: error:
+--     Illegal `..' in record pattern
+--     Use RecordWildCards to permit this
+-- File.hs:53:28: error:
+--     Illegal tuple section: use TupleSections
+-- File.hs:238:29: error:
+--     * Can't make a derived instance of `Data FSATrace':
+--         You need DeriveDataTypeable to derive an instance for this class
+--     * In the data declaration for `FSATrace'
+-- C:\Neil\shake\src\Development\Shake\Command.hs:515:31: error:
+--     * Illegal equational constraint a ~ ()
+--       (Use GADTs or TypeFamilies to permit this)
+--     * In the context: a ~ ()
+--       While checking an instance declaration
+--       In the instance declaration for `Unit (m a)'
+    | exts@(_:_) <- filter (`Set.member` ghcExtensions) $ T.split (not . isAlpha) $ T.replace "-X" "" _message
+        = [("Add " <> x <> " extension", [TextEdit (Range (Position 0 0) (Position 0 0)) $ "{-# LANGUAGE " <> x <> " #-}\n"]) | x <- exts]
+
 suggestAction _ _ = []
+
+
+-- | All the GHC extensions
+ghcExtensions :: Set.HashSet T.Text
+ghcExtensions = Set.fromList $ map (T.pack . show) [Cpp .. StarIsType] -- use enumerate from GHC 8.8 and beyond
 
 
 textAtPosition :: Position -> T.Text -> (T.Text, T.Text)
