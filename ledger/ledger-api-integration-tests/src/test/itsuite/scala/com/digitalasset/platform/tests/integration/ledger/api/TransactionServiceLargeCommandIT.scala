6f5149fcb72a9d0ad492d3ab0800fb0edbbbf849
Gabor Aranyossy <gabor.aranyossy@digitalasset.com>
2019-07-01 15:56:54 +0200

    fxied wrong filter usage in TransactionServiceLargeCommandIT

--- ledger/ledger-api-integration-tests/src/test/itsuite/scala/com/digitalasset/platform/tests/integration/ledger/api/TransactionServiceLargeCommandIT.scala
+++ ledger/ledger-api-integration-tests/src/test/itsuite/scala/com/digitalasset/platform/tests/integration/ledger/api/TransactionServiceLargeCommandIT.scala
@@ -3,7 +3,6 @@
 
 package com.digitalasset.platform.tests.integration.ledger.api
 
-import com.digitalasset.ledger.api.testing.utils.MockMessages._
 import com.digitalasset.ledger.api.testing.utils.{
   AkkaBeforeAndAfterAll,
   SuiteResourceManagementAroundAll
@@ -13,7 +12,11 @@ import com.digitalasset.ledger.api.v1.commands.{Command, CreateCommand}
 import com.digitalasset.ledger.api.v1.value.{Identifier, Value}
 import com.digitalasset.platform.apitesting.LedgerContextExtensions._
 import com.digitalasset.platform.apitesting.TestParties._
-import com.digitalasset.platform.apitesting.{MultiLedgerFixture, TestTemplateIds}
+import com.digitalasset.platform.apitesting.{
+  MultiLedgerFixture,
+  TestTemplateIds,
+  TransactionFilters
+}
 import com.digitalasset.platform.esf.TestExecutionSequencerFactory
 import com.digitalasset.platform.participant.util.ValueConversions._
 import com.digitalasset.platform.services.time.TimeProviderType
@@ -47,8 +50,6 @@ class TransactionServiceLargeCommandIT
 
   override val timeLimit: Span = scaled(300.seconds)
 
-  private val getAllContracts = transactionFilter
-
   "Transaction Service" when {
 
     "submitting and reading transactions" should {
@@ -64,7 +65,9 @@ class TransactionServiceLargeCommandIT
           .update(_.commands.maximumRecordTime := Timestamp(60L, 0))
 
         c.testingHelpers
-          .submitAndListenForSingleResultOfCommand(superSizedCommand, getAllContracts)
+          .submitAndListenForSingleResultOfCommand(
+            superSizedCommand,
+            TransactionFilters.allForParties(Alice))
           .map { tx =>
             tx.events.size shouldEqual targetNumberOfSubCommands
           }
