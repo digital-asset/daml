f08c95ae78cd4206d5bd7ab6d5d8daeeabc1a84d
Remy Haemmerle <Remy.Haemmerle@digitalasset.com>
2019-07-01 17:44:35 +0200

    daml-lf: fix version timeline

--- daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/VersionTimeline.scala
+++ daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/VersionTimeline.scala
@@ -53,8 +53,8 @@ private[lf] object VersionTimeline {
       That(LanguageVersion(LMV.V1, "4")),
       That(LanguageVersion(LMV.V1, "5")),
       This(That(TransactionVersion("8"))),
-      That(LanguageVersion(LMV.V1, Dev)),
       This(This(ValueVersion("5"))),
+      That(LanguageVersion(LMV.V1, Dev)),
       // add new versions above this line
       // do *not* backfill to make more Boths, because such would
       // invalidate the timeline, except to accompany Dev language
