e29ce53e6f59efffb82e0f630db2da4fe305580f
Gabor Aranyossy <gabor.aranyossy@digitalasset.com>
2019-07-02 16:21:26 +0200

    fixing broken tests

--- ledger/ledger-api-integration-tests/src/test/itsuite/scala/com/digitalasset/platform/tests/integration/ledger/api/WitnessesIT.scala
+++ ledger/ledger-api-integration-tests/src/test/itsuite/scala/com/digitalasset/platform/tests/integration/ledger/api/WitnessesIT.scala
@@ -3,23 +3,23 @@
 
 package com.digitalasset.platform.tests.integration.ledger.api
 
-import scala.concurrent.Future
-import org.scalatest.{AsyncFreeSpec, Matchers}
 import com.digitalasset.ledger.api.testing.utils.{
-  SuiteResourceManagementAroundEach,
-  AkkaBeforeAndAfterAll
+  AkkaBeforeAndAfterAll,
+  SuiteResourceManagementAroundEach
 }
-import org.scalatest.concurrent.{AsyncTimeLimitedTests, ScalaFutures}
-import com.digitalasset.platform.apitesting.TestTemplateIds
-import com.digitalasset.platform.apitesting.{MultiLedgerFixture, LedgerContext}
-import com.digitalasset.ledger.client.services.commands.SynchronousCommandClient
-import com.digitalasset.ledger.api.v1.transaction_filter.{Filters, TransactionFilter}
-import com.digitalasset.platform.apitesting.LedgerContextExtensions._
 import com.digitalasset.ledger.api.v1.commands.{CreateCommand, ExerciseCommand}
-import com.digitalasset.ledger.api.v1.value.{Record, RecordField, Value}
-import com.digitalasset.platform.participant.util.ValueConversions._
-import com.digitalasset.ledger.api.v1.event.{ExercisedEvent}
+import com.digitalasset.ledger.api.v1.event.ExercisedEvent
 import com.digitalasset.ledger.api.v1.transaction.TreeEvent
+import com.digitalasset.ledger.api.v1.transaction_filter.{Filters, TransactionFilter}
+import com.digitalasset.ledger.api.v1.value.{Record, RecordField, Value}
+import com.digitalasset.platform.apitesting.LedgerContextExtensions._
+import com.digitalasset.platform.apitesting.TestParties._
+import com.digitalasset.platform.apitesting.{MultiLedgerFixture, TestTemplateIds}
+import com.digitalasset.platform.participant.util.ValueConversions._
+import org.scalatest.concurrent.{AsyncTimeLimitedTests, ScalaFutures}
+import org.scalatest.{AsyncFreeSpec, Matchers}
+
+import scala.concurrent.Future
 
 @SuppressWarnings(Array("org.wartremover.warts.Any"))
 class WitnessesIT
@@ -35,34 +35,31 @@ class WitnessesIT
   protected val testTemplateIds = new TestTemplateIds(config)
   protected val templateIds = testTemplateIds.templateIds
 
-  private def commandClient(ctx: LedgerContext): SynchronousCommandClient =
-    new SynchronousCommandClient(ctx.commandService)
-
   private val filter = TransactionFilter(
     Map(
-      "alice" -> Filters.defaultInstance,
-      "bob" -> Filters.defaultInstance,
-      "charlie" -> Filters.defaultInstance,
+      Alice -> Filters.defaultInstance,
+      Bob -> Filters.defaultInstance,
+      Charlie -> Filters.defaultInstance,
     ))
 
   "disclosure rules are respected" in allFixtures { ctx =>
     val createArg = Record(
       fields = List(
-        RecordField("p_signatory", "alice".asParty),
-        RecordField("p_observer", "bob".asParty),
-        RecordField("p_actor", "charlie".asParty),
+        RecordField("p_signatory", Alice.asParty),
+        RecordField("p_observer", Bob.asParty),
+        RecordField("p_actor", Charlie.asParty)
       ))
     val exerciseArg = Value(Value.Sum.Record(Record()))
+
     def exercise(cid: String, choice: String): Future[ExercisedEvent] =
       ctx.testingHelpers
         .submitAndListenForSingleTreeResultOfCommand(
           ctx.testingHelpers
-            .submitRequestWithId(s"$choice-exercise")
+            .submitRequestWithId(s"$choice-exercise", Charlie)
             .update(
               _.commands.commands :=
                 List(
-                  ExerciseCommand(Some(templateIds.witnesses), cid, choice, Some(exerciseArg)).wrap),
-              _.commands.party := "charlie"
+                  ExerciseCommand(Some(templateIds.witnesses), cid, choice, Some(exerciseArg)).wrap)
             ),
           filter,
           false
@@ -73,15 +70,15 @@ class WitnessesIT
             case _ => fail("unexpected event")
           }
         }
+
     for {
       // Create Witnesses contract
       createTx <- ctx.testingHelpers.submitAndListenForSingleResultOfCommand(
         ctx.testingHelpers
-          .submitRequestWithId("create")
+          .submitRequestWithId("create", Alice)
           .update(
             _.commands.commands :=
-              List(CreateCommand(Some(templateIds.witnesses), Some(createArg)).wrap),
-            _.commands.party := "alice"
+              List(CreateCommand(Some(templateIds.witnesses), Some(createArg)).wrap)
           ),
         filter
       )
@@ -90,15 +87,14 @@ class WitnessesIT
       // see it by default.
       divulgeCreatedEv <- ctx.testingHelpers.simpleCreate(
         "create-divulge",
-        "charlie",
+        Charlie,
         templateIds.divulgeWitnesses,
         Record(
-          fields =
-            List(RecordField(value = "alice".asParty), RecordField(value = "charlie".asParty)))
+          fields = List(RecordField(value = Alice.asParty), RecordField(value = Charlie.asParty)))
       )
       _ <- ctx.testingHelpers.simpleExercise(
         "exercise-divulge",
-        "alice",
+        Alice,
         templateIds.divulgeWitnesses,
         divulgeCreatedEv.contractId,
         "Divulge",
@@ -111,14 +107,9 @@ class WitnessesIT
       // And then the consuming one
       consumingExerciseEv <- exercise(createdEv.contractId, "WitnessesChoice")
     } yield {
-      createdEv.witnessParties should contain theSameElementsAs List("alice", "bob") // stakeholders = signatories \cup observers
-      nonConsumingExerciseEv.witnessParties should contain theSameElementsAs List(
-        "alice",
-        "charlie") // signatories \cup actors
-      consumingExerciseEv.witnessParties should contain theSameElementsAs List(
-        "alice",
-        "bob",
-        "charlie") // stakeholders \cup actors
+      createdEv.witnessParties should contain theSameElementsAs List(Alice, Bob) // stakeholders = signatories \cup observers
+      nonConsumingExerciseEv.witnessParties should contain theSameElementsAs List(Alice, Charlie) // signatories \cup actors
+      consumingExerciseEv.witnessParties should contain theSameElementsAs List(Alice, Bob, Charlie) // stakeholders \cup actors
     }
   }
 }
