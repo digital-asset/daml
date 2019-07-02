395e3574d1fb605b5e3590a35cc352ad7fa19011
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-07-02 08:01:54 -0400

    Improving generator test

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/GeneratorsTest.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/GeneratorsTest.scala
@@ -4,24 +4,25 @@
 package com.digitalasset.http
 
 import com.digitalasset.http.Generators.genDuplicateApiIdentifiers
-import org.scalacheck.Shrink
-import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
 
 class GeneratorsTest extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
-  import Shrink.shrinkAny
-
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
     PropertyCheckConfiguration(minSuccessful = 10000)
+
+  import org.scalacheck.Shrink.shrinkAny
 
   "Generators.genDuplicateApiIdentifiers" should "generate only duplicate API Identifiers" in
     forAll(genDuplicateApiIdentifiers) { ids =>
       ids.size should be >= 2
-      ids.sliding(2).foreach {
-        case List(a1, a2) =>
-          a1.packageId should not be a2.packageId
-          (a1.moduleName, a1.entityName) should be(a2.moduleName -> a2.entityName)
-        case x @ _ => fail(s"Should never happen when sliding(2): $x")
-      }
+      val (packageIds, moduleNames, entityNames) =
+        ids.foldLeft((Set.empty[String], Set.empty[String], Set.empty[String])) { (b, a) =>
+          (b._1 + a.packageId, b._2 + a.moduleName, b._3 + a.entityName)
+        }
+
+      packageIds.size shouldBe ids.size
+      moduleNames.size shouldBe 1
+      entityNames.size shouldBe 1
     }
 }
