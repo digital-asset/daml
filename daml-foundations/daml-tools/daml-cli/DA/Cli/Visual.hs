5a153f9e967df3e0128efdb7f6a87357b4f74bf5
Anup Kalburgi <42743833+akalburgi@users.noreply.github.com>
2019-07-02 18:06:15 -0400

    Visual choice node (#1977)
    now visual graph with template choices

--- daml-foundations/daml-tools/daml-cli/DA/Cli/Visual.hs
+++ daml-foundations/daml-tools/daml-cli/DA/Cli/Visual.hs
@@ -1,8 +1,6 @@
 -- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ApplicativeDo       #-}
-
 -- | Main entry-point of the DAML compiler
 module DA.Cli.Visual
   ( execVisual
@@ -18,10 +16,10 @@ import qualified DA.Pretty as DAP
 import qualified DA.Daml.LF.Proto3.Archive as Archive
 import qualified Codec.Archive.Zip as ZIPArchive
 import qualified Data.ByteString.Lazy as BSL
-import Text.Dot
 import qualified Data.ByteString as B
-import qualified Data.Map as M
 import Data.Generics.Uniplate.Data
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
 
 data Action = ACreate (LF.Qualified LF.TypeConName)
             | AExercise (LF.Qualified LF.TypeConName) LF.ChoiceName deriving (Eq, Ord, Show )
@@ -54,15 +52,21 @@ startFromExpr seen world e = case e of
 startFromChoice :: LF.World -> LF.TemplateChoice -> Set.Set Action
 startFromChoice world chc = startFromExpr Set.empty world (LF.chcUpdate chc)
 
-templatePossibleUpdates :: LF.World -> LF.Template -> Set.Set Action
-templatePossibleUpdates world tpl = Set.unions $ map (startFromChoice world) (NM.toList (LF.tplChoices tpl))
+data ChoiceAndAction = ChoiceAndAction
+    { choiceForTemplate :: LF.Template
+    , choice :: LF.TemplateChoice
+    , actions :: Set.Set Action
+    }
+data TemplateChoiceAction = TemplateChoiceAction
+    { template :: LF.Template
+    , choiceAndAction :: [ChoiceAndAction]
+    }
 
-moduleAndTemplates :: LF.World -> LF.Module -> [(LF.TypeConName, Set.Set Action)]
-moduleAndTemplates world mod = retTypess
-    where
-        templates = NM.toList $ LF.moduleTemplates mod
-        retTypess = map (\t-> (LF.tplTypeCon t, templatePossibleUpdates world t )) templates
+templatePossibleUpdates :: LF.World -> LF.Template -> [ChoiceAndAction]
+templatePossibleUpdates world tpl = map (\c -> ChoiceAndAction tpl c (startFromChoice world c)) (NM.toList (LF.tplChoices tpl))
 
+moduleAndTemplates :: LF.World -> LF.Module -> [TemplateChoiceAction]
+moduleAndTemplates world mod = map (\t -> TemplateChoiceAction t (templatePossibleUpdates world t)) $ NM.toList $ LF.moduleTemplates mod
 
 dalfBytesToPakage :: BSL.ByteString -> (LF.PackageId, LF.Package)
 dalfBytesToPakage bytes = case Archive.decodeArchive $ BSL.toStrict bytes of
@@ -74,47 +78,85 @@ darToWorld manifest pkg = AST.initWorldSelf pkgs pkg
     where
         pkgs = map dalfBytesToPakage (dalfsContent manifest)
 
+tplName :: LF.Template -> T.Text
+tplName LF.Template {..} = head (LF.unTypeConName tplTypeCon)
 
-templateInAction :: Action -> LF.TypeConName
-templateInAction (ACreate  (LF.Qualified _ _ tpl) ) = tpl
-templateInAction (AExercise  (LF.Qualified _ _ tpl) _ ) = tpl
+handleChoiceAndAction :: ChoiceAndAction -> LF.ChoiceName
+handleChoiceAndAction (ChoiceAndAction tpl choice _)
+    | LF.chcName choice == LF.ChoiceName "Create" = LF.ChoiceName $ tplName tpl <> "_Create"
+    | LF.chcName choice == LF.ChoiceName "Archive" = LF.ChoiceName $ tplName tpl <> "_Archive"
+    | otherwise = LF.chcName choice
 
-srcLabel :: (LF.TypeConName, Set.Set Action) -> [(String, String)]
-srcLabel (tc, _) = [ ("shape","none"),("label",DAP.renderPretty tc) ]
+-- Making choiceName is very weird
+handleCreateAndArchive :: TemplateChoiceAction -> [LF.ChoiceName]
+handleCreateAndArchive TemplateChoiceAction {..} = [createChoice, archiveChoice] ++ map handleChoiceAndAction choiceAndAction
+    where archiveChoice = LF.ChoiceName $ tplName template <> "_Archive"
+          createChoice = LF.ChoiceName $ tplName template <> "_Create"
 
-templatePairs :: (LF.TypeConName, Set.Set Action) -> (LF.TypeConName , (LF.TypeConName , Set.Set Action))
-templatePairs (tc, actions) = (tc , (tc, actions))
+-- This is used to generate the node ids and use as look up table
+choiceNameWithId :: [TemplateChoiceAction] -> Map.Map LF.ChoiceName Int
+choiceNameWithId tplChcActions = Map.fromList $ zip choiceActions [0..]
+  where choiceActions = concatMap handleCreateAndArchive tplChcActions
 
-actionsForTemplate :: (LF.TypeConName, Set.Set Action) -> [LF.TypeConName]
-actionsForTemplate (_tplCon, actions) = Set.elems $ Set.map templateInAction actions
+nodeIdForChoice :: Map.Map LF.ChoiceName Int -> LF.ChoiceName -> Int
+nodeIdForChoice nodeLookUp chc = case Map.lookup chc nodeLookUp of
+  Just node -> node
+  Nothing -> error "Template node lookup failed"
 
-errorOnLeft :: Show a => String -> Either a b -> IO b
-errorOnLeft desc = \case
-  Left err -> ioError $ userError $ unlines [ desc, show err ]
-  Right x  -> return x
+-- probably storing the choice is a better Idea, as we can determine what kind of choice it is.
+data SubGraph = SubGraph
+    { nodes :: [(LF.ChoiceName ,Int)]
+    , clusterTemplate :: LF.Template
+    }
 
--- | 'netlistGraph' generates a simple graph from a netlist.
--- The default implementation does the edeges other way round. The change is on # 143
-netlistGraph' :: (Ord a)
-          => (b -> [(String,String)])   -- ^ Attributes for each node
-          -> (b -> [a])                 -- ^ Out edges leaving each node
-          -> [(a,b)]                    -- ^ The netlist
-          -> Dot ()
-netlistGraph' attrFn outFn assocs = do
-    let nodes = Set.fromList [a | (a, _) <- assocs]
-    let outs = Set.fromList [o | (_, b) <- assocs, o <- outFn b]
-    nodeTab <- sequence
-                [do nd <- node (attrFn b)
-                    return (a, nd)
-                | (a, b) <- assocs]
-    otherTab <- sequence
-               [do nd <- node []
-                   return (o, nd)
-                | o <- Set.toList outs, o `Set.notMember` nodes]
-    let fm = M.fromList (nodeTab ++ otherTab)
-    sequence_
-        [(fm M.! dst) .->. (fm M.! src) | (dst, b) <- assocs,
-        src <- outFn b]
+addCreateChoice :: TemplateChoiceAction -> Map.Map LF.ChoiceName Int -> (LF.ChoiceName ,Int)
+addCreateChoice TemplateChoiceAction {..} lookupData = (tplNameCreateChoice, nodeIdForChoice lookupData tplNameCreateChoice)
+    where tplNameCreateChoice = LF.ChoiceName $ T.pack $ DAP.renderPretty (head (LF.unTypeConName (LF.tplTypeCon template))) ++ "_Create"
+
+constructSubgraphsWithLables :: Map.Map LF.ChoiceName Int -> TemplateChoiceAction -> SubGraph
+constructSubgraphsWithLables lookupData tpla@TemplateChoiceAction {..} = SubGraph nodesWithCreate template
+  where choicesInTemplete = map handleChoiceAndAction choiceAndAction
+        nodes = map (\chc -> (chc, nodeIdForChoice lookupData chc)) choicesInTemplete
+        nodesWithCreate = nodes ++ [addCreateChoice tpla lookupData]
+
+actionToChoice :: LF.Template -> Action -> LF.ChoiceName
+actionToChoice tpl (ACreate _) = LF.ChoiceName $ tplName tpl <> "_Create"
+actionToChoice tpl (AExercise _ (LF.ChoiceName "Archive")) = LF.ChoiceName $ tplName tpl <> "_Archive"
+actionToChoice _tpl (AExercise _ chc) = chc
+
+choiceActionToChoicePairs :: ChoiceAndAction -> [(LF.ChoiceName, LF.ChoiceName)]
+choiceActionToChoicePairs cha@ChoiceAndAction {..} = pairs
+    where pairs = map (\ac -> (handleChoiceAndAction cha, actionToChoice choiceForTemplate ac)) (Set.elems actions)
+
+graphEdges :: Map.Map LF.ChoiceName Int -> [TemplateChoiceAction] -> [(Int, Int)]
+graphEdges lookupData tplChcActions = map (\(chn1, chn2) -> (nodeIdForChoice lookupData chn1, nodeIdForChoice lookupData chn2)) choicePairsForTemplates
+  where chcActionsFromAllTemplates = concatMap choiceAndAction tplChcActions
+        choicePairsForTemplates = concatMap choiceActionToChoicePairs chcActionsFromAllTemplates
+
+subGraphHeader :: LF.Template -> String
+subGraphHeader tpl = "subgraph cluster_" ++ (DAP.renderPretty $ head (LF.unTypeConName $ LF.tplTypeCon tpl)) ++ "{\n"
+
+
+subGraphBodyLine :: (LF.ChoiceName ,Int) -> String
+subGraphBodyLine (chc, nodeId) = "n" ++ show nodeId ++ "[label=" ++ DAP.renderPretty chc ++ "];"
+
+subGraphEnd :: LF.Template -> String
+subGraphEnd tpl = "label=" ++ DAP.renderPretty (LF.tplTypeCon tpl) ++ ";color=" ++ "blue" ++ "\n}"
+
+
+subGraphCluster :: SubGraph -> String
+subGraphCluster SubGraph {..} = subGraphHeader clusterTemplate ++ unlines (map subGraphBodyLine nodes) ++ subGraphEnd clusterTemplate
+
+-- TODO Later on should decorate the edge too
+drawEdge :: Int -> Int -> String
+drawEdge n1 n2 = "n" ++ show n1 ++ "->" ++ "n" ++ show n2
+
+
+constructDotGraph :: [SubGraph] -> [(Int, Int)] -> String
+constructDotGraph subgraphs edges = "digraph G {\ncompound=true;\n" ++ "rankdir=LR;\n"++ graphLines ++ "\n}\n"
+  where subgraphsLines = concatMap subGraphCluster subgraphs
+        edgesLines = unlines $ map (uncurry drawEdge) edges
+        graphLines = subgraphsLines ++ edgesLines
 
 execVisual :: FilePath -> Maybe FilePath -> IO ()
 execVisual darFilePath dotFilePath = do
@@ -123,11 +165,17 @@ execVisual darFilePath dotFilePath = do
     (_, lfPkg) <- errorOnLeft "Cannot decode package" $ Archive.decodeArchive (BSL.toStrict (mainDalfContent manifestData) )
     let modules = NM.toList $ LF.packageModules lfPkg
         world = darToWorld manifestData lfPkg
-        res = concatMap (moduleAndTemplates world) modules
-        actionEdges = map templatePairs res
-        dotString = showDot $ netlistGraph' srcLabel actionsForTemplate actionEdges
+        templatesAndModules = concatMap (moduleAndTemplates world) modules
+        nodeWorld = choiceNameWithId templatesAndModules
+        subgraphClusters = map (constructSubgraphsWithLables nodeWorld) templatesAndModules
+        graphConnectedEdges = graphEdges nodeWorld templatesAndModules
+        dotString = constructDotGraph subgraphClusters graphConnectedEdges
     case dotFilePath of
         Just outDotFile -> writeFile outDotFile dotString
         Nothing -> putStrLn dotString
 
+errorOnLeft :: Show a => String -> Either a b -> IO b
+errorOnLeft desc = \case
+  Left err -> ioError $ userError $ unlines [ desc, show err ]
+  Right x  -> return x
 
