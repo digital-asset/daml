045b65cb4e30e2a448a6687962ac4b2802592ad0
Gerolf Seitz <gerolf.seitz@digitalasset.com>
2019-07-02 21:19:35 +0200

    Add NotSupported Submission Result

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/services/ApiSubmissionService.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/services/ApiSubmissionService.scala
@@ -5,7 +5,11 @@ package com.digitalasset.platform.sandbox.services
 import com.digitalasset.ledger.api.v1.command_submission_service.CommandSubmissionServiceLogging
 import akka.stream.ActorMaterializer
 import com.daml.ledger.participant.state.index.v2.ContractStore
-import com.daml.ledger.participant.state.v2.SubmissionResult.{Acknowledged, Overloaded}
+import com.daml.ledger.participant.state.v2.SubmissionResult.{
+  Acknowledged,
+  NotSupported,
+  Overloaded
+}
 import com.daml.ledger.participant.state.v2.{
   SubmissionResult,
   SubmitterInfo,
@@ -105,6 +109,10 @@ class ApiSubmissionService private (
             logger.debug(
               s"Submission of command {} has failed due to back pressure",
               commands.commandId.unwrap)
+            Failure(Status.RESOURCE_EXHAUSTED.asRuntimeException)
+
+          case Success(NotSupported) =>
+            logger.debug(s"Submission of command {} was not supported", commands.commandId.unwrap)
             Failure(Status.RESOURCE_EXHAUSTED.asRuntimeException)
 
           case Failure(error) =>
