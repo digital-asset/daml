03978ce6c6774963af28181652e74831ddd3ffa2
Neil Mitchell <ndmitchell@gmail.com>
2019-07-03 10:16:18 +0100

    Add standalone pieces to make hie-core work with a global Cabal install

---
+++ compiler/hie-core/install.bat
@@ -0,0 +1,3 @@
+@REM Install hie-core where cabal install would put it on Windows
+@REM but avoid checking configure or installing local libraries (faster)
+ghc Main -o dist\obj\hie-core.exe -XBangPatterns -XDeriveGeneric -XGeneralizedNewtypeDeriving -XLambdaCase -XNamedFieldPuns -XRecordWildCards -XScopedTypeVariables -XStandaloneDeriving -XTupleSections -XTypeApplications -XViewPatterns -package=ghc -DGHC_STABLE -isrc -iexe -outputdir dist\obj && copy dist\obj\hie-core.exe %AppData%\cabal\bin\hie-core.exe
