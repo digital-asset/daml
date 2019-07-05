d7575848a90315f7bd52bab8054961628843c6a4
Gerolf Seitz <gerolf.seitz@digitalasset.com>
2019-07-05 16:45:25 +0200

    Move tests that use contract keys into their own integration test file
    This allows us to run the other tests via the ledger api test tool with
    the postgres indexer while InMemoryKVParticipantState doesn't support
    contract keys yet.

--- ledger/ledger-api-integration-tests/src/test/itsuite/scala/com/digitalasset/platform/tests/integration/ledger/api/commands/CommandTransactionChecksNo1866IT.scala
+++ ledger/ledger-api-integration-tests/src/test/itsuite/scala/com/digitalasset/platform/tests/integration/ledger/api/commands/CommandTransactionChecksNo1866IT.scala
@@ -4,21 +4,16 @@
 package com.digitalasset.platform.tests.integration.ledger.api.commands
 
 import java.io.File
+
 import com.digitalasset.daml.bazeltools.BazelRunfiles.rlocation
-import com.digitalasset.daml.lf.language.{
-  LanguageVersion,
-  LanguageMajorVersion => LMajV,
-  LanguageMinorVersion => LMinV
-}
-import com.digitalasset.platform.apitesting.CommandTransactionChecksHighLevel
+import com.digitalasset.daml.lf.language.{LanguageVersion, LanguageMajorVersion => LMajV, LanguageMinorVersion => LMinV}
+import com.digitalasset.platform.tests.integration.ledger.api.ContractKeysIT
 
 /** Runs tests with an older version of the language that does not include changes
   * performed in #1866 -- specifically the fact that the submitter must be in
   * lookup maintainers.
-  *
-  * TODO extract version-dependent parts out of the huge `CommandTransactionChecks`.
   */
-class CommandTransactionChecksNo1866IT extends CommandTransactionChecksHighLevel {
+class CommandTransactionChecksNo1866IT extends ContractKeysIT {
   override protected val languageVersion: LanguageVersion =
     LanguageVersion(LMajV.V1, LMinV.fromProtoIdentifier("5"))
   override protected lazy val config: Config = Config.default.copy(
