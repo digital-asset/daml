93bb95054ebc39230f81cb8bf1df33ca164f13f5
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-07-08 17:11:16 -0400

    Cleanup

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceIntegrationTest.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceIntegrationTest.scala
@@ -9,7 +9,9 @@ import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes, Uri}
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
 import com.digitalasset.grpc.adapter.{AkkaExecutionSequencerPool, ExecutionSequencerFactory}
+import com.digitalasset.http.HttpServiceTestFixture.withHttpService
 import com.digitalasset.http.util.TestUtil.requiredFile
+import com.typesafe.scalalogging.StrictLogging
 import org.scalatest._
 import spray.json._
 
@@ -19,7 +21,8 @@ class HttpServiceIntegrationTest
     extends AsyncFreeSpec
     with Matchers
     with Inside
-    with BeforeAndAfterAll {
+    with BeforeAndAfterAll
+    with StrictLogging {
 
   private val dar = requiredFile("./docs/quickstart-model.dar")
     .fold(e => throw new IllegalStateException(e), identity)
@@ -31,16 +34,11 @@ class HttpServiceIntegrationTest
   implicit val aesf: ExecutionSequencerFactory = new AkkaExecutionSequencerPool(testId)(asys)
   implicit val ec: ExecutionContext = asys.dispatcher
 
-  "contracts/search test" in {
-    HttpServiceTestFixture.withHttpService(dar, testId)(contractsSearchTest)
-  }
-
-  private def contractsSearchTest(uri: Uri): Future[Assertion] =
+  "contracts/search test" in withHttpService(dar, testId) { uri: Uri =>
     Http().singleRequest(HttpRequest(uri = uri.withPath(Uri.Path("/contracts/search")))).flatMap {
       resp =>
         resp.status shouldBe StatusCodes.OK
         val bodyF: Future[ByteString] = getResponseDataBytes(resp)
-        bodyF.foreach(x => println(s"---- body: ${x.utf8String}"))
         bodyF.flatMap { body =>
           val jsonAst: JsValue = body.utf8String.parseJson
           inside(jsonAst) {
@@ -53,8 +51,27 @@ class HttpServiceIntegrationTest
               }
           }
         }
-    }
+    }: Future[Assertion]
+  }
 
-  private def getResponseDataBytes(resp: HttpResponse): Future[ByteString] =
-    resp.entity.dataBytes.runFold(ByteString.empty)((b, a) => b ++ a)
+  "request non-existent endpoint should return 404 with no data" in withHttpService(dar, testId) {
+    uri: Uri =>
+      Http()
+        .singleRequest(HttpRequest(uri = uri.withPath(Uri.Path("/contracts/does-not-exist"))))
+        .flatMap { resp =>
+          resp.status shouldBe StatusCodes.NotFound
+          val bodyF: Future[ByteString] = getResponseDataBytes(resp)
+          bodyF.flatMap { body =>
+            body.utf8String should have length 0
+          }
+        }: Future[Assertion]
+  }
+
+  private def getResponseDataBytes(
+      resp: HttpResponse,
+      debug: Boolean = false): Future[ByteString] = {
+    val fb = resp.entity.dataBytes.runFold(ByteString.empty)((b, a) => b ++ a)
+    if (debug) fb.foreach(x => logger.info(s"---- response data: ${x.utf8String}"))
+    fb
+  }
 }
