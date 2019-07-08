89f91181603f4cdf9ddc73768fd285aeb38a053a
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-07-08 16:56:18 -0400

    Copyright check is naieve

--- compiler/hlint-testing/src/Main.hs
+++ compiler/hlint-testing/src/Main.hs
@@ -1,6 +1,5 @@
--- Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its
--- affiliates. All rights reserved.  SPDX-License-Identifier:
--- (Apache-2.0 OR BSD-3-Clause)
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
 
 -- A little program to help prove the use of hlint with DAML.
 
