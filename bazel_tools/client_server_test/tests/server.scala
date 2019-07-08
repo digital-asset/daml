9a5dc1483482b2286a750f1b87c4e247f836063d
Brian Healey <brian.healey@digitalasset.com>
2019-07-08 18:26:33 -0400

    add package bazel_tools.client_server_test.tests

--- bazel_tools/client_server_test/tests/server.scala
+++ bazel_tools/client_server_test/tests/server.scala
@@ -1,6 +1,8 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+package bazel_tools.client_server_test.tests
+
 import java.net._
 import java.io._
 import scala.io._
