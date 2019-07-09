0715c76b534f1e92a0c0cb17d6be8ff66bfb536e
Brian Healey <brian.healey@digitalasset.com>
2019-07-09 14:22:30 -0400

    Issue #2064 Add package for bazel_tools client_server_testAdd package without underscore or dash to each of the scala-protoc-plugins components

--- scala-protoc-plugins/scalapb/ScalaPbCompilerPlugin.scala
+++ scala-protoc-plugins/scalapb/ScalaPbCompilerPlugin.scala
@@ -1,6 +1,8 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+package protoc.plugins.scalapb
+
 import scala.reflect.io.Streamable
 
 // This file is mostly copied over from ScalaPbCodeGenerator and ProtobufGenerator
