b2e0d75cba12377df701586e18e835b5ab9d75ec
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-07-09 12:44:15 -0400

    CommandService WIP

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/CommandService.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/CommandService.scala
@@ -1,3 +1,10 @@
-package com.digitalasset.http class CommandService {
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.http
+
+class CommandService {
+
+  def create() = ()
 
 }
