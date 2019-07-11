83247244fbf94c201014c957db8810a7604d43f7
Anup Kalburgi <anup.kalburgi@digitalasset.com>
2019-07-11 16:44:05 -0400

    Removing redundant data

--- compiler/damlc/lib/DA/Cli/Visual.hs
+++ compiler/damlc/lib/DA/Cli/Visual.hs
@@ -26,8 +26,8 @@ data Action = ACreate (LF.Qualified LF.TypeConName)
             | AExercise (LF.Qualified LF.TypeConName) LF.ChoiceName deriving (Eq, Ord, Show )
 
 data ChoiceAndAction = ChoiceAndAction
-    { choiceForTemplate :: LF.Template
-    , choice :: LF.TemplateChoice
+    { choiceName :: LF.ChoiceName
+    , consumingChc :: IsConsuming
     , actions :: Set.Set Action
     }
 
@@ -74,8 +74,13 @@ startFromExpr seen world e = case e of
 startFromChoice :: LF.World -> LF.TemplateChoice -> Set.Set Action
 startFromChoice world chc = startFromExpr Set.empty world (LF.chcUpdate chc)
 
+archiveChoiceAndAction :: LF.Template -> ChoiceAndAction
+archiveChoiceAndAction tpl = ChoiceAndAction (LF.ChoiceName $ tplName tpl <> "_Archive") True Set.empty
+
 templatePossibleUpdates :: LF.World -> LF.Template -> [ChoiceAndAction]
-templatePossibleUpdates world tpl = map (\c -> ChoiceAndAction tpl c (startFromChoice world c)) (NM.toList (LF.tplChoices tpl))
+templatePossibleUpdates world tpl = actions ++ [archiveChoiceAndAction tpl]
+    where actions =  map (\c -> ChoiceAndAction (LF.chcName c) (LF.chcConsuming c) (startFromChoice world c)) (NM.toList (LF.tplChoices tpl))
+
 
 moduleAndTemplates :: LF.World -> LF.Module -> [TemplateChoices]
 moduleAndTemplates world mod = map (\t -> TemplateChoices t (templatePossibleUpdates world t)) $ NM.toList $ LF.moduleTemplates mod
@@ -94,13 +99,9 @@ tplName :: LF.Template -> T.Text
 tplName LF.Template {..} = head (LF.unTypeConName tplTypeCon)
 
 extractChoiceData :: ChoiceAndAction -> (LF.ChoiceName, IsConsuming)
-extractChoiceData (ChoiceAndAction tpl choice _) = (newChoiceName, LF.chcConsuming choice)
-    where newChoiceName
-            | LF.chcName choice == LF.ChoiceName "Archive" = LF.ChoiceName $ tplName tpl <> "_Archive"
-            | otherwise = LF.chcName choice
+extractChoiceData (ChoiceAndAction choiceN consuming _) = (choiceN, consuming)
 
 
--- Making choiceName is very weird
 templateWithCreateChoice :: TemplateChoices -> [(LF.ChoiceName, IsConsuming)]
 templateWithCreateChoice TemplateChoices {..} = createChoice : map extractChoiceData choiceAndAction
     where createChoice = (LF.ChoiceName $ tplName template <> "_Create", False)
@@ -125,14 +126,17 @@ constructSubgraphsWithLables lookupData tpla@TemplateChoices {..} = SubGraph nod
         nodes = map (\(chc, _) -> (chc, nodeIdForChoice lookupData chc)) choicesInTemplete
         nodesWithCreate = nodes ++ [addCreateChoice tpla lookupData]
 
-actionToChoice :: LF.Template -> Action -> LF.ChoiceName
-actionToChoice tpl (ACreate _) = LF.ChoiceName $ tplName tpl <> "_Create"
-actionToChoice tpl (AExercise _ (LF.ChoiceName "Archive")) = LF.ChoiceName $ tplName tpl <> "_Archive"
-actionToChoice _tpl (AExercise _ chc) = chc
+tplNamet :: LF.TypeConName -> T.Text
+tplNamet tplConName = head (LF.unTypeConName tplConName)
+
+actionToChoice :: Action -> LF.ChoiceName
+actionToChoice (ACreate LF.Qualified {..}) = LF.ChoiceName $ tplNamet qualObject <> "_Create"
+actionToChoice (AExercise LF.Qualified {..} (LF.ChoiceName "Archive")) = LF.ChoiceName $ tplNamet qualObject <> "_Archive"
+actionToChoice (AExercise _ chc) = chc
 
 choiceActionToChoicePairs :: ChoiceAndAction -> [(LF.ChoiceName, LF.ChoiceName)]
 choiceActionToChoicePairs cha@ChoiceAndAction {..} = pairs
-    where pairs = map (\ac -> (fst $ extractChoiceData cha, actionToChoice choiceForTemplate ac)) (Set.elems actions)
+    where pairs = map (\ac -> (fst $ extractChoiceData cha, actionToChoice ac)) (Set.elems actions)
 
 graphEdges :: Map.Map LF.ChoiceName ChoiceDetails -> [TemplateChoices] -> [(ChoiceDetails, ChoiceDetails)]
 graphEdges lookupData tplChcActions = map (\(chn1, chn2) -> (nodeIdForChoice lookupData chn1, nodeIdForChoice lookupData chn2)) choicePairsForTemplates
@@ -152,14 +156,11 @@ subGraphBodyLine (chc, ChoiceDetails{..}) = "n" ++ show nodeId ++ "[label=" ++ D
 subGraphEnd :: LF.Template -> String
 subGraphEnd tpl = "label=" ++ DAP.renderPretty (LF.tplTypeCon tpl) ++ ";color=" ++ "blue" ++ "\n}"
 
-
 subGraphCluster :: SubGraph -> String
 subGraphCluster SubGraph {..} = subGraphHeader clusterTemplate ++ unlines (map subGraphBodyLine nodes) ++ subGraphEnd clusterTemplate
 
--- TODO Later on should decorate the edge too
 drawEdge :: ChoiceDetails -> ChoiceDetails -> String
 drawEdge n1 n2 = "n" ++ show (nodeId n1) ++ "->" ++ "n" ++ show (nodeId n2)
-
 
 constructDotGraph :: [SubGraph] -> [(ChoiceDetails, ChoiceDetails)] -> String
 constructDotGraph subgraphs edges = "digraph G {\ncompound=true;\n" ++ "rankdir=LR;\n"++ graphLines ++ "\n}\n"
