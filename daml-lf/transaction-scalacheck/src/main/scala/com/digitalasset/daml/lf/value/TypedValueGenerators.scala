a5dadd252106be259a5f6fdb6ab9e965be5d8409
Stephen Compall <stephen.compall@daml.com>
2019-07-15 16:29:28 -0400

    experiment with an inductive case in TypedValueGenerators

--- daml-lf/transaction-scalacheck/src/main/scala/com/digitalasset/daml/lf/value/TypedValueGenerators.scala
+++ daml-lf/transaction-scalacheck/src/main/scala/com/digitalasset/daml/lf/value/TypedValueGenerators.scala
@@ -6,15 +6,27 @@ package value
 
 import scala.language.higherKinds
 
+import data.{Decimal, FrontStack, Ref, Time}
 import data.ImmArray.ImmArraySeq
 import iface.{PrimType => PT, Type, TypePrim}
 
+import org.scalacheck.{Arbitrary, Shrink}
+
+/** [[ValueGenerators]] produce untyped values; for example, if you use the list gen,
+  * you get a heterogeneous list.  The generation target here, on the other hand, is
+  * ''a pair of a type and a generator of values of that type''.  For example, you might
+  * generate the type `[Map Decimal]`, which would be accompanied by a `Gen` that produced
+  * [[Value]]s each guaranteed to be a list of maps, whose values are all guaranteed to
+  * be Decimals.
+  */
 object TypedValueGenerators {
   sealed abstract class ValueAddend {
     type Inj[Cid]
     def t: Type
     def inj[Cid]: Inj[Cid] => Value[Cid]
     def prj[Cid]: Value[Cid] PartialFunction Inj[Cid]
+    implicit def injarb[Cid: Arbitrary]: Arbitrary[Inj[Cid]]
+    implicit def injshrink[Cid: Shrink]: Shrink[Inj[Cid]]
   }
 
   @SuppressWarnings(Array("org.wartremover.warts.Any"))
@@ -26,15 +38,18 @@ object TypedValueGenerators {
       type Inj[_] = Inj0
     }
 
-    def noCid[Inj0](pt: PT, inj0: Inj0 => Value[Nothing])(
-        prj0: Value[Any] PartialFunction Inj0): NoCid[Inj0] = new ValueAddend {
+    def noCid[Inj0](pt: PT, inj0: Inj0 => Value[Nothing])(prj0: Value[Any] PartialFunction Inj0)(
+        implicit arb: Arbitrary[Inj0],
+        shr: Shrink[Inj0]): NoCid[Inj0] = new ValueAddend {
       type Inj[Cid] = Inj0
       override val t = TypePrim(pt, ImmArraySeq.empty)
       override def inj[Cid] = inj0
       override def prj[Cid] = prj0
+      override def injarb[Cid: Arbitrary] = arb
+      override def injshrink[Cid: Shrink] = shr
     }
 
-    import Value._
+    import Value._, ValueGenerators.Implicits._
     val text = noCid(PT.Text, ValueText) { case ValueText(t) => t }
     val int64 = noCid(PT.Int64, ValueInt64) { case ValueInt64(i) => i }
     val decimal = noCid(PT.Decimal, ValueDecimal) { case ValueDecimal(d) => d }
@@ -43,9 +58,27 @@ object TypedValueGenerators {
     val timestamp = noCid(PT.Timestamp, ValueTimestamp) { case ValueTimestamp(t) => t }
     val bool = noCid(PT.Bool, ValueBool) { case ValueBool(b) => b }
     val party = noCid(PT.Party, ValueParty) { case ValueParty(p) => p }
+
+    type Compose[F[_], G[_], A] = F[G[A]]
+    def list(elt: ValueAddend): Aux[Compose[FrontStack, elt.Inj, ?]] = new ValueAddend {
+      type Inj[Cid] = FrontStack[elt.Inj[Cid]]
+      override val t = TypePrim(PT.List, ImmArraySeq(elt.t))
+      override def inj[Cid] = elts => ValueList(elts map elt.inj)
+      override def prj[Cid] = { case ValueList(v) => v map elt.prj }
+      override def injarb[Cid: Arbitrary] = ???
+      override def injshrink[Cid: Shrink] = ???
+    }
   }
 
   trait PrimInstances[F[_]] {
     def text: F[String]
+    def int64: F[Long]
+    def decimal: F[Decimal]
+    def unit: F[Unit]
+    def date: F[Time.Date]
+    def timestamp: F[Time.Timestamp]
+    def bool: F[Boolean]
+    def party: F[Ref.Party]
+    def leafInstances: Seq[F[_]] = Seq(text, int64, decimal, unit, date, timestamp, bool, party)
   }
 }
