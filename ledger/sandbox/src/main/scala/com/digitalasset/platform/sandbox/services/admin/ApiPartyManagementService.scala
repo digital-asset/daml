452d3814598776453ceebe9c9d59504d75b78b2c
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2019-07-17 14:30:20 +0200

    Wait for index before completing package upload
    This makes sure users can immediately query the new
    packages after the upload completed.
    
    Fixes #2066

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/services/admin/ApiPartyManagementService.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/services/admin/ApiPartyManagementService.scala
@@ -4,7 +4,6 @@
 package com.digitalasset.platform.sandbox.services.admin
 
 import akka.actor.Scheduler
-import akka.pattern.after
 import akka.stream.ActorMaterializer
 import com.daml.ledger.participant.state.index.v2.IndexPartyManagementService
 import com.daml.ledger.participant.state.v2.{PartyAllocationResult, WritePartyService}
@@ -52,48 +51,7 @@ class ApiPartyManagementService private (
       .map(ps => ListKnownPartiesResponse(ps.map(mapPartyDetails)))(DE)
 
   /**
-    * Continuously polls the party management service to check if a party has been persisted.
-    *
-    * Despite the `go` inner function not being stack-safe per se, only one stack frame will be on
-    * the stack at any given time since the "recursive" invocation happens on a different thread.
-    *
-    * The backoff waiting time are applied after the first poll returns without a result (i.e. the first call is not delayed).
-    *
-    * @param party The party whose persistence we're waiting for
-    * @param minWait The minimum waiting time - will not be enforced if less than `maxWait`
-    *                Does not make sense to set this lower than the OS scheduler threshold
-    *                Anyway always padded to 50 milliseconds
-    * @param maxWait The maximum waiting time - takes precedence over `minWait` and `backoffProgression`
-    *                Does not make sense to set this lower than the OS scheduler threshold
-    *                Anyway always padded to 50 milliseconds
-    * @param backoffProgression How the following backoff time is computed as a function of the current one - `maxWait` takes precedence though
-    * @return The number of attempts before the party was found wrapped in a [[Future]]
-    */
-  private def pollUntilPersisted(
-      party: String,
-      minWait: FiniteDuration,
-      maxWait: FiniteDuration,
-      backoffProgression: FiniteDuration => FiniteDuration): Future[Int] = {
-    def go(party: String, attempt: Int, waitTime: FiniteDuration): Future[Int] = {
-      logger.debug(s"Polling for party '$party' being persisted (attempt #$attempt)...")
-      partyManagementService
-        .listParties()
-        .flatMap {
-          case persisted if persisted.exists(_.party == party) => Future.successful(attempt)
-          case _ =>
-            logger.debug(s"Party '$party' not yet persisted, backing off for $waitTime...")
-            after(waitTime, scheduler)(
-              go(
-                party,
-                attempt + 1,
-                backoffProgression(waitTime).min(maxWait).max(50.milliseconds)))(DE)
-        }(DE)
-    }
-    go(party, 1, minWait.min(maxWait).max(50.milliseconds))
-  }
-
-  /**
-    * Wraps a call [[pollUntilPersisted]] so that it can be chained on the party allocation with a `flatMap`.
+    * Wraps a call [[PollingUtils.pollUntilPersisted]] so that it can be chained on the party allocation with a `flatMap`.
     *
     * Checks invariants and forwards the original result after the party is found to be persisted.
     *
@@ -106,11 +64,19 @@ class ApiPartyManagementService private (
       maxWait: FiniteDuration,
       iteration: FiniteDuration => FiniteDuration): Future[AllocatePartyResponse] = {
     require(result.partyDetails.isDefined, "Party allocation response must have the party details")
-    pollUntilPersisted(result.partyDetails.get.party, minWait, maxWait, iteration).map {
-      numberOfAttempts =>
-        logger.debug(s"Party available, read after $numberOfAttempts attempt(s)")
+    val newParty = result.partyDetails.get.party
+    PollingUtils
+      .pollUntilPersisted(() => partyManagementService.listParties())(
+        _.exists(_.party == newParty),
+        s"party $newParty",
+        minWait,
+        maxWait,
+        iteration,
+        scheduler)
+      .map { numberOfAttempts =>
+        logger.debug(s"Party $newParty available, read after $numberOfAttempts attempt(s)")
         result
-    }(DE)
+      }(DE)
   }
 
   override def allocateParty(request: AllocatePartyRequest): Future[AllocatePartyResponse] = {
---
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/services/admin/PollingUtils.scala
@@ -0,0 +1,61 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.platform.sandbox.services.admin
+
+import akka.actor.Scheduler
+import akka.pattern.after
+import com.digitalasset.platform.common.util.{DirectExecutionContext => DE}
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.Future
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+case object PollingUtils {
+
+  private val logger: Logger = LoggerFactory.getLogger(PollingUtils.getClass)
+
+  /**
+    * Continuously polls the given service to check if the given item has been persisted.
+    *
+    * Despite the `go` inner function not being stack-safe per se, only one stack frame will be on
+    * the stack at any given time since the "recursive" invocation happens on a different thread.
+    *
+    * The backoff waiting time are applied after the first poll returns without a result (i.e. the first call is not delayed).
+    *
+    * @param poll               The service, returning a collection of items
+    * @param check              Returns true iff the service returned all items that are being waited for
+    * @param description        A human readable description of the item that is being waited for, for logging purposes
+    * @param minWait            The minimum waiting time - will not be enforced if less than `maxWait`
+    *                           Does not make sense to set this lower than the OS scheduler threshold
+    *                           Anyway always padded to 50 milliseconds
+    * @param maxWait            The maximum waiting time - takes precedence over `minWait` and `backoffProgression`
+    *                           Does not make sense to set this lower than the OS scheduler threshold
+    *                           Anyway always padded to 50 milliseconds
+    * @param backoffProgression How the following backoff time is computed as a function of the current one - `maxWait` takes precedence though
+    * @return The number of attempts before the item was found wrapped in a [[Future]]
+    */
+  def pollUntilPersisted[T](poll: () => Future[T])(
+      check: T => Boolean,
+      description: String,
+      minWait: FiniteDuration,
+      maxWait: FiniteDuration,
+      backoffProgression: FiniteDuration => FiniteDuration,
+      scheduler: Scheduler): Future[Int] = {
+    def go(attempt: Int, waitTime: FiniteDuration): Future[Int] = {
+      logger.debug(s"Polling for '$description' being persisted (attempt #$attempt)...")
+      poll()
+        .flatMap {
+          case persisted if check(persisted) => Future.successful(attempt)
+          case _ =>
+            logger.debug(
+              s"'${description.capitalize}' not yet persisted, backing off for $waitTime...")
+            after(waitTime, scheduler)(
+              go(attempt + 1, backoffProgression(waitTime).min(maxWait).max(50.milliseconds)))(DE)
+        }(DE)
+    }
+
+    go(1, minWait.min(maxWait).max(50.milliseconds))
+  }
+
+}
