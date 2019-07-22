eb543e57de43e061b94173af941ec0aea196e514
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-07-22 10:42:24 +0200

    Cache nix store

--- ci/dev-env-install.sh
+++ ci/dev-env-install.sh
@@ -17,16 +17,6 @@ step() {
 
 cd "$(dirname "$0")/.."
 
-if [[ ! -e /nix ]]; then
-  step "Installing Nix"
-
-  sudo mkdir -m 0755 /nix
-  sudo chown "$(id -u):$(id -g)" /nix
-
-  # 2.2.2 seems to segfault on MacOS in CI so for now we use 2.2.1.
-  curl -sfL https://nixos.org/releases/nix/nix-2.2.1/install | bash
-fi
-
 # shellcheck source=../dev-env/lib/ensure-nix
 source dev-env/lib/ensure-nix
 
