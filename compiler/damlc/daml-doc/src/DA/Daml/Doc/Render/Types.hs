cb39474bed99bb14662c71dc6e6067a94df9fb98
associahedron <231829+associahedron@users.noreply.github.com>
2019-07-23 10:18:17 +0100

    Remove unused lang extensions

--- compiler/damlc/daml-doc/src/DA/Daml/Doc/Render/Types.hs
+++ compiler/damlc/daml-doc/src/DA/Daml/Doc/Render/Types.hs
@@ -1,8 +1,6 @@
 -- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-{-# LANGUAGE OverloadedStrings, DerivingStrategies #-}
-
 -- | Types common to DA.Daml.Doc.Render
 module DA.Daml.Doc.Render.Types
     ( module DA.Daml.Doc.Render.Types
