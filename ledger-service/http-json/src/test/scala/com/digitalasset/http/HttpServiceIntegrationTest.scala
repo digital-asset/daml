bc93c8eb9c8ff44c4325a032797268122d021acb
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-07-25 14:44:09 -0400

    exercise command json encoding/decoding works

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceIntegrationTest.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceIntegrationTest.scala
@@ -12,15 +12,15 @@ import com.digitalasset.grpc.adapter.{AkkaExecutionSequencerPool, ExecutionSeque
 import com.digitalasset.http.HttpServiceTestFixture.{jsonCodecs, withHttpService, withLedger}
 import com.digitalasset.http.domain.TemplateId.OptionalPkg
 import com.digitalasset.http.json._
-import com.digitalasset.http.util.FutureUtil.toFuture
 import com.digitalasset.http.util.TestUtil.requiredFile
+import com.digitalasset.ledger.api.refinements.{ApiTypes => lar}
 import com.digitalasset.ledger.api.v1.value.Record
 import com.digitalasset.ledger.api.v1.{value => v}
 import com.typesafe.scalalogging.StrictLogging
 import org.scalatest._
 import scalaz.syntax.functor._
-import spray.json._
 import scalaz.syntax.show._
+import spray.json._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -61,43 +61,6 @@ class HttpServiceIntegrationTest
     }: Future[Assertion]
   }
 
-  "should be able to serialize and deserialize domain.CreateCommand" in withLedger(dar, testId) {
-    client =>
-      {
-        import json.JsonProtocol._
-
-        val command0: domain.CreateCommand[v.Record] = iouCreateCommand
-
-        for {
-          (encoder, decoder) <- jsonCodecs(client)
-
-          command1 <- toFuture(encoder.encodeUnderlyingRecord(command0)): Future[
-            domain.CreateCommand[JsObject]]
-
-          jsValue = command1.toJson: JsValue
-
-          command2 <- toFuture(SprayJson.decode[domain.CreateCommand[JsObject]](jsValue)): Future[
-            domain.CreateCommand[JsObject]]
-
-          _ <- Future(command1 shouldBe command2)
-
-          command3 <- toFuture(decoder.decodeUnderlyingRecords(command2)): Future[
-            domain.CreateCommand[Record]]
-
-          _ <- Future(command3.map(removeRecordId) shouldBe command0)
-
-          jsObject <- toFuture(encoder.encodeR(command0))
-
-          command4 <- toFuture(decoder.decodeR[domain.CreateCommand](jsObject)): Future[
-            domain.CreateCommand[Record]]
-
-          x <- Future(command4 shouldBe command3)
-
-        } yield x
-
-      }: Future[Assertion]
-  }
-
   "command/create IOU" in withHttpService(dar, testId) { (uri, encoder, decoder) =>
     import json.JsonProtocol._
 
@@ -113,12 +76,12 @@ class HttpServiceIntegrationTest
           entity = HttpEntity(ContentTypes.`application/json`, input)))
       .flatMap { resp =>
         resp.status shouldBe StatusCodes.OK
-        val bodyF: Future[String] = getResponseDataBytes(resp, true)
+        val bodyF: Future[String] = getResponseDataBytes(resp, debug = true)
         bodyF.flatMap { body =>
           val jsonAst: JsValue = body.parseJson
           println(s"----- jsonAst: $jsonAst")
           inside(jsonAst) {
-            case JsObject(fields) => {
+            case JsObject(fields) =>
               inside(fields.get("status")) {
                 case Some(JsNumber(status)) => status shouldBe BigDecimal("200")
               }
@@ -126,7 +89,6 @@ class HttpServiceIntegrationTest
                 case Some(activeContract: JsObject) =>
                   assertActiveContract(decoder, activeContract, command)
               }
-            }
           }
         }
       }: Future[Assertion]
@@ -144,6 +106,45 @@ class HttpServiceIntegrationTest
     inside(jsObject.fields.get("argument")) {
       case Some(JsObject(fields)) => fields.size shouldBe 5
     }
+  }
+
+  "should be able to serialize and deserialize domain commands" in withLedger(dar, testId) {
+    client =>
+      jsonCodecs(client).map {
+        case (encoder, decoder) =>
+          testCreateCommandEncodingDecoding(encoder, decoder)
+          testExerciseCommandEncodingDecoding(encoder, decoder)
+      }: Future[Assertion]
+  }
+
+  private def testCreateCommandEncodingDecoding(
+      encoder: DomainJsonEncoder,
+      decoder: DomainJsonDecoder): Assertion = {
+    import json.JsonProtocol._
+
+    val command0: domain.CreateCommand[v.Record] = iouCreateCommand
+
+    val x = for {
+      jsonObj <- encoder.encodeR(command0)
+      command1 <- decoder.decodeR[domain.CreateCommand](jsonObj)
+    } yield command1.map(removeRecordId) should ===(command0)
+
+    x.fold(e => fail(e.shows), identity)
+  }
+
+  private def testExerciseCommandEncodingDecoding(
+      encoder: DomainJsonEncoder,
+      decoder: DomainJsonDecoder): Assertion = {
+    import json.JsonProtocol._
+
+    val command0: domain.ExerciseCommand[v.Record] = iouExerciseCommand
+
+    val x = for {
+      jsonObj <- encoder.encodeR(command0)
+      command1 <- decoder.decodeR[domain.ExerciseCommand](jsonObj)
+    } yield command1.map(removeRecordId) should ===(command0)
+
+    x.fold(e => fail(e.shows), identity)
   }
 
   "request non-existent endpoint should return 404 with no data" in withHttpService(dar, testId) {
@@ -179,5 +180,16 @@ class HttpServiceIntegrationTest
       ))
 
     domain.CreateCommand(templateId, arg, None)
+  }
+
+  private def iouExerciseCommand: domain.ExerciseCommand[v.Record] = {
+    val templateId: OptionalPkg = domain.TemplateId(None, "Iou", "Iou")
+    val contractId = lar.ContractId("a-contract-ID")
+    val arg: Record = v.Record(
+      fields = List(v.RecordField("newOwner", Some(v.Value(v.Value.Sum.Party("Alice")))))
+    )
+    val choice = lar.Choice("Iou_Transfer")
+
+    domain.ExerciseCommand(templateId, contractId, choice, arg, None)
   }
 }
