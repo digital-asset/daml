83d4087c3d0384579dd39383e08b2e59b9839d17
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-07-25 14:25:20 +0200

    ./fmt.sh

--- compiler/hie-core/test/cabal/Development/IDE/Test/Runfiles.hs
+++ compiler/hie-core/test/cabal/Development/IDE/Test/Runfiles.hs
@@ -1,7 +1,9 @@
 -- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-module Development.IDE.Test.Runfiles where
+module Development.IDE.Test.Runfiles
+  ( locateHieCoreExecutable
+  ) where
 
 import System.FilePath (FilePath)
 
--- compiler/hie-core/test/exe/Main.hs
+++ compiler/hie-core/test/exe/Main.hs
@@ -2,7 +2,6 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
