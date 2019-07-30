b16f86483389fd3eece2a614a55412107de65207
Anup Kalburgi <anup.kalburgi@digitalasset.com>
2019-07-30 12:28:14 -0400

    moved all shaketest and visual tests become dar-reader test

---
+++ compiler/damlc/tests/src/DarReaderTest.hs
@@ -0,0 +1,25 @@
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+module DarReaderTest
+   ( main
+   ) where
+
+import Test.Tasty
+import DA.Daml.LF.Reader
+import Test.Tasty.HUnit
+
+main :: IO ()
+main = defaultMain unitTests
+
+unitTests :: TestTree
+unitTests = testGroup "testing dar reader for longer manifest lines"
+    [
+    testCase "multiline manifest file test" $
+        assertEqual "content over multiple lines"
+            ["Dalfs: stdlib.dalf, prim.dalf", "Main-Dalf: testing.dalf"]
+            (multiLineContent ["Dalfs: stdlib.da", " lf, prim.dalf" , "Main-Dalf: testing.dalf"])
+    , testCase "multiline manifest file test" $
+        assertEqual "all content in the same line"
+            ["Dalfs: stdlib.dalf", "Main-Dalf:solution.dalf"]
+            (multiLineContent ["Dalfs: stdlib.dalf" , "Main-Dalf:solution.dalf"])
+    ]
