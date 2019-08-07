c7b5482e43a0810bde96a7ecb6779d5ad973a75d
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-08-07 12:26:26 -0400

    JWT HMAC256 Authorization (#2389)
    * Starting //ledger-service/jwt module
    
    adding //3rdparty/jvm/com/auth0:java_jwt dependency, there are Scala JWT
    libraries, however they are either immature or unsupported
    
    * Keys and JWT generator utility/wrapper, WIP
    
    * Keys and JWT generator utility/wrapper, WIP
    
    * RSA Keys generator
    
    * HMAC token validation added to command/create endpoint
    
    HMAC is actually not good, need RSA, this is a shortcut for DABL team.
    
    * Renaming
    
    * command/create test cases fixed to pass OAth2 Bearer token
    
    * removing unused method
    
    * JWT authorization for /command/exercise
    
    * the rest of the endpoints uses JWT authorization
    
    removed hardcoded JwtPayload
    
    * cleanup
    
    * cleanup
    
    * http status code in the header and in the json body should match now
    
    * NotFound would also return a json body now
    
    * Removing hardcoded JWT in the integration test
    
    it is generated using the JwtSigner.HMAC256
    
    * cleanup
    
    * fixing error message
    
    * instructions for making a JWT HMAC token for dev

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/util/FutureUtil.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/util/FutureUtil.scala
@@ -5,7 +5,7 @@ package com.digitalasset.http.util
 
 import scalaz.EitherT.rightT
 import scalaz.syntax.show._
-import scalaz.{-\/, EitherT, Functor, Show, \/, \/-}
+import scalaz.{-\/, Applicative, EitherT, Functor, Show, \/, \/-}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.higherKinds
@@ -26,6 +26,12 @@ object FutureUtil {
   final class LiftET[E](private val ignore: Int) extends AnyVal {
     def apply[F[_]: Functor, A](fa: F[A]): EitherT[F, E, A] = rightT(fa)
   }
+
+  def eitherT[A, B](fa: Future[A \/ B])(implicit ev: Applicative[Future]): EitherT[Future, A, B] =
+    EitherT.eitherT[Future, A, B](fa)
+
+  def either[A, B](d: A \/ B)(implicit ev: Applicative[Future]): EitherT[Future, A, B] =
+    EitherT.either[Future, A, B](d)
 
   def stripLeft[A: Show, B](fa: Future[A \/ B])(implicit ec: ExecutionContext): Future[B] =
     fa.flatMap {
