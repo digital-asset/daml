532376aadb02cdbfc54cd30eafd0da57161d30c1
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-08-08 15:56:48 +0200

    Add missing copyright headers

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/templates/ShowDelegated.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/templates/ShowDelegated.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.api.testtool.templates
 
 import com.daml.ledger.api.testtool.infrastructure.LedgerTestContext
