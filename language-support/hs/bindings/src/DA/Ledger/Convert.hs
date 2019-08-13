99ea93168d84d456060bbdad4d7922e19eacf4f8
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2019-08-13 17:23:03 +0100

    update copyright notices (#2499)

--- language-support/hs/bindings/src/DA/Ledger/Convert.hs
+++ language-support/hs/bindings/src/DA/Ledger/Convert.hs
@@ -1,4 +1,4 @@
--- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- Copyright (c) 2019 The DAML Authors. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
 -- Convert between HL Ledger.Types and the LL types generated from .proto files
