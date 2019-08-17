a42b3b9dc933e2061495026d5b5642076e1d9f92
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2019-08-18 00:24:03 +0200

    Recover indexer from errors
    Fixes #2065

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/index/JdbcIndexer.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/index/JdbcIndexer.scala
@@ -44,8 +44,6 @@ import scalaz.syntax.tag._
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.util.control.NonFatal
-import scala.util.{Failure, Success}
 
 object JdbcIndexer {
   private val logger = LoggerFactory.getLogger(classOf[JdbcIndexer])
@@ -140,24 +138,15 @@ class JdbcIndexer private (
     * Subscribes to an instance of ReadService.
     *
     * @param readService the ReadService to subscribe to
-    * @param onError     callback to signal error during feed processing
-    * @param onComplete  callback fired only once at normal feed termination.
     * @return a handle of IndexFeedHandle or a failed Future
     */
-  override def subscribe(
-      readService: ReadService,
-      onError: Throwable => Unit,
-      onComplete: () => Unit): Future[IndexFeedHandle] = {
+  override def subscribe(readService: ReadService): Future[IndexFeedHandle] = {
     val (killSwitch, completionFuture) = readService
       .stateUpdates(beginAfterExternalOffset.map(Offset.assertFromString))
       .viaMat(KillSwitches.single)(Keep.right[NotUsed, UniqueKillSwitch])
       .mapAsync(1)((handleStateUpdate _).tupled)
       .toMat(Sink.ignore)(Keep.both)
       .run()
-    completionFuture.onComplete {
-      case Failure(NonFatal(t)) => onError(t)
-      case Success(_) => onComplete()
-    }(DEC)
 
     Future.successful(indexHandleFromKillSwitch(killSwitch, completionFuture))
   }
@@ -278,6 +267,10 @@ class JdbcIndexer private (
       completionFuture: Future[Done]): IndexFeedHandle = new IndexFeedHandle {
     override def stop(): Future[akka.Done] = {
       ks.shutdown()
+      completionFuture
+    }
+
+    override def completed(): Future[akka.Done] = {
       completionFuture
     }
   }
--- ledger/sandbox/src/main/scala/com/digitalasset/platform/index/StandaloneIndexerServer.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/index/StandaloneIndexerServer.scala
@@ -3,13 +3,14 @@
 
 package com.digitalasset.platform.index
 
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 
 import com.daml.ledger.participant.state.v1.ReadService
 import com.digitalasset.platform.common.util.{DirectExecutionContext => DEC}
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.control.NonFatal
 
 // Main entry point to start an indexer server.
 // See v2.ReferenceServer for the usage
@@ -17,22 +18,38 @@ object StandaloneIndexerServer {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   def apply(readService: ReadService, jdbcUrl: String): AutoCloseable = {
-    val server = JdbcIndexer.create(readService, jdbcUrl)
-    val indexHandleF = server.flatMap(
-      _.subscribe(
-        readService,
-        t => logger.error("error while processing state updates", t),
-        () => logger.info("successfully finished processing state updates")))(DEC)
-
-    val indexFeedHandle = Await.result(indexHandleF, JdbcIndexer.asyncTolerance)
-    logger.info("Started Indexer Server")
 
     val closed = new AtomicBoolean(false)
+    val lastHandle = new AtomicReference[Option[IndexFeedHandle]](None)
+
+    def restart(): Future[akka.Done] = {
+      logger.info("Starting Indexer Server")
+      implicit val ec: ExecutionContext = DEC
+      val completedF = for {
+        server <- JdbcIndexer.create(readService, jdbcUrl)
+        handle <- server.subscribe(readService)
+        _ = {
+          logger.info("Started Indexer Server")
+          lastHandle.set(Some(handle))
+        }
+        completed <- handle.completed()
+        _ = logger.info("Successfully finished processing state updates")
+      } yield completed
+
+      completedF.recoverWith {
+        case NonFatal(t) =>
+          logger.error("Error while processing state updates", t)
+          restart()
+      }(DEC)
+    }
+
+    restart()
 
     new AutoCloseable {
       override def close(): Unit = {
         if (closed.compareAndSet(false, true)) {
-          val _ = Await.result(indexFeedHandle.stop(), JdbcIndexer.asyncTolerance)
+          val _ =
+            lastHandle.get.foreach(h => Await.result(h.stop(), JdbcIndexer.asyncTolerance))
         }
       }
     }
