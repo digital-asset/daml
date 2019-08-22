7103266c3942f479ba4739f42007eef9dd83d308
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-08-22 11:20:18 +0200

    Add stripInfix to daml stdlib
    fixes #2617

--- compiler/damlc/lib/DA/Cli/Damlc.hs
+++ compiler/damlc/lib/DA/Cli/Damlc.hs
@@ -840,7 +840,7 @@ execDocTest opts files = do
         -- This is horrible but we do not have a way to change the import paths in a running
         -- IdeState at the moment.
         pure $ nubOrd $ mapMaybe moduleImportPaths pmS
-    opts <- mkOptions opts { optImportPath = importPaths <> optImportPath opts}
+    opts <- mkOptions opts { optImportPath = importPaths <> optImportPath opts, optHaddock = Haddock True }
     withDamlIdeState opts logger diagnosticsLogger $ \ideState ->
         docTest ideState files'
 
