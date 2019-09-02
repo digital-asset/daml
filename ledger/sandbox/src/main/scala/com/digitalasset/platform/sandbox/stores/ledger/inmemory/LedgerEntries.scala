07fef0d7842d2c00d3cb2cf752681f4eb91f8cfd
Jussi Mäki <jussi.maki@digitalasset.com>
2019-09-02 11:05:08 +0200

    Dispatcher: Provide sub-source in startingAt (#2715)
    This allows using the dispatcher in situations where the index is common,
    but different data streams are materialized from it.
    
    The practical reason for this is to allow using a single dispatcher to read
    from both the ledger_entries and configuration_entries tables, which share
    the same ledger offset construction.

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/inmemory/LedgerEntries.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/inmemory/LedgerEntries.scala
@@ -43,17 +43,16 @@ private[ledger] class LedgerEntries[T](identify: T => String) {
     newOffset
   }
 
-  private val dispatcher = Dispatcher[Long, T](
-    RangeSource(
-      (inclusiveStart, exclusiveEnd) =>
-        Source[(Long, T)](state.get().items.range(inclusiveStart, exclusiveEnd)),
-    ),
-    ledgerBeginning,
-    ledgerEnd
-  )
+  private val dispatcher = Dispatcher[Long](ledgerBeginning, ledgerEnd)
 
   def getSource(offset: Option[Long]): Source[(Long, T), NotUsed] =
-    dispatcher.startingAt(offset.getOrElse(ledgerBeginning))
+    dispatcher.startingAt(
+      offset.getOrElse(ledgerBeginning),
+      RangeSource(
+        (inclusiveStart, exclusiveEnd) =>
+          Source[(Long, T)](state.get().items.range(inclusiveStart, exclusiveEnd)),
+      )
+    )
 
   def publish(item: T): Long = {
     val newHead = store(item)
