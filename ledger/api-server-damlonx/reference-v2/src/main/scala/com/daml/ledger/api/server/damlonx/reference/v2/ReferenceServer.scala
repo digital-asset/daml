916d3db0a6d4f5ba925803f6b34640e143c48060
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-09-04 11:32:01 +0200

    Allow reference server to specify extra participants

--- ledger/api-server-damlonx/reference-v2/src/main/scala/com/daml/ledger/api/server/damlonx/reference/v2/ReferenceServer.scala
+++ ledger/api-server-damlonx/reference-v2/src/main/scala/com/daml/ledger/api/server/damlonx/reference/v2/ReferenceServer.scala
@@ -27,7 +27,8 @@ object ReferenceServer extends App {
       .parse(
         args,
         "damlonx-reference-server",
-        "A fully compliant DAML Ledger API server backed by an in-memory store.")
+        "A fully compliant DAML Ledger API server backed by an in-memory store.",
+        allowExtraParticipants = true)
       .getOrElse(sys.exit(1))
 
   // Name of this participant
@@ -56,23 +57,33 @@ object ReferenceServer extends App {
   val indexerServer = StandaloneIndexerServer(readService, config.jdbcUrl)
   val indexServer = StandaloneIndexServer(config, readService, writeService).start()
 
-  val indexerServer2 =
-    StandaloneIndexerServer(readService, "jdbc:h2:mem:daml_on_x2;db_close_on_exit=false")
-  val indexServer2 =
-    StandaloneIndexServer(
-      config.copy(port = 6866, jdbcUrl = "jdbc:h2:mem:daml_on_x2;db_close_on_exit=false"),
-      readService,
-      writeService
-    ).start()
+  val extraParticipants =
+    for {
+      (participantId, port, jdbcUrl) <- config.extraPartipants
+    } yield {
+      val extraIndexer = StandaloneIndexerServer(readService, jdbcUrl)
+      val extraLedgerApiServer = StandaloneIndexServer(
+        config.copy(
+          port = port,
+          participantId = participantId,
+          jdbcUrl = jdbcUrl
+        ),
+        readService,
+        writeService
+      )
+      (extraIndexer, extraLedgerApiServer.start())
+    }
 
   val closed = new AtomicBoolean(false)
 
   def closeServer(): Unit = {
     if (closed.compareAndSet(false, true)) {
       indexServer.close()
-      indexServer2.close()
       indexerServer.close()
-      indexerServer2.close()
+      for ((extraIndexer, extraLedgerApi) <- extraParticipants) {
+        extraIndexer.close()
+        extraLedgerApi.close()
+      }
       ledger.close()
       materializer.shutdown()
       val _ = system.terminate()
