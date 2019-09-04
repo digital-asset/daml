73603107edefc45276d748dc9bb19118ddfecffd
Jussi Maki <jussi.maki@digitalasset.com>
2019-09-04 16:04:20 +0200

    Remove divulgedTo from DivulgedContract
    We decided not to prematurely add this feature.

--- ledger/participant-state/src/main/scala/com/daml/ledger/participant/state/v1/DivulgedContract.scala
+++ ledger/participant-state/src/main/scala/com/daml/ledger/participant/state/v1/DivulgedContract.scala
@@ -13,13 +13,8 @@ import com.digitalasset.daml.lf.value.Value
   *
   * @param contractId: The absolute contract identifier.
   * @param contractInst: The contract instance.
-  * @param divulgedTo:
-  *    The optional set of parties to which the contract has been divulged to. If not set, then
-  *    the set of parties this contract is revealed to is derived from the associated transaction.
-  *    This primary purpose of this field is to allow synthetic divulgence after a ledger pruning event.
   */
 final case class DivulgedContract(
     contractId: Value.AbsoluteContractId,
-    contractInst: AbsoluteContractInst,
-    divulgedTo: Option[Set[Party]]
+    contractInst: AbsoluteContractInst
 )
