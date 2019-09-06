02012cfc71f9af8f353d79f1ac0bc151999483ea
Nick Chapman <nick.chapman@digitalasset.com>
2019-09-06 15:54:27 +0100

    export-package, run here

--- language-support/hs/bindings/export-package.sh
+++ language-support/hs/bindings/export-package.sh
@@ -10,6 +10,8 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+cd "$(dirname ${BASH_SOURCE[0]})"
+
 TARGET_DIR=$1
 
 bazel build //ledger-api/grpc-definitions:google-protobuf-haskellpb-sources //ledger-api/grpc-definitions:google-rpc-haskellpb-sources //ledger-api/grpc-definitions:ledger-api-haskellpb-sources //ledger-api/grpc-definitions:ledger-api-haskellpb-sources-admin //ledger-api/grpc-definitions:ledger-api-haskellpb-sources-testing
@@ -20,7 +22,7 @@ rm -rf gen
 mkdir gen
 mkdir gen/src
 
-cp -r $BAZEL_BIN/ledger-api/grpc-definitions/Google gen/src/Google
-cp -r $BAZEL_BIN/ledger-api/grpc-definitions/Com gen/src/Com
+cp -rp $BAZEL_BIN/ledger-api/grpc-definitions/Google gen/src/Google
+cp -rp $BAZEL_BIN/ledger-api/grpc-definitions/Com gen/src/Com
 
 stack sdist --tar-dir $TARGET_DIR
