0912b749cfbdb9b015396f7fec6aee85772fe78e
Stephen Compall <stephen.compall@daml.com>
2019-09-09 18:21:42 -0400

    expand more cases for the predicate reader

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/query/ValuePredicate.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/query/ValuePredicate.scala
@@ -20,27 +20,42 @@ object ValuePredicate {
   final case class RecordSubset[+Ty, +LfV](fields: Map[String, ValuePredicate[Ty, LfV]])
       extends ValuePredicate[Ty, LfV]
   // boolean is whether inclusive (lte vs lt)
-  final case class Range[+Ty, +LfV](ltgt: (Boolean, LfV) \&/ (Boolean, LfV), typ: Ty) extends ValuePredicate[Ty, LfV]
+  final case class Range[+Ty, +LfV](ltgt: (Boolean, LfV) \&/ (Boolean, LfV), typ: Ty)
+      extends ValuePredicate[Ty, LfV]
 
-  def fromJsObject(it: Map[String, JsValue], typ: iface.Type, defs: TypeLookup): ValuePredicate[iface.Type, JsValue] = {
-    RecordSubset(Map())
-  }
-
-  private[this] def fromPrim(it: JsValue, typ: iface.TypePrim): ValuePredicate[iface.Type, JsValue] = {
-    import iface.PrimType._
-    def lit = Literal(it, typ)
-    (typ.typ, it).match2 {
-      case Bool => {case JsBoolean(_) => lit}
-      case Int64 => {case JsNumber(_) | JsString(_) => lit}
-      case Text => {case JsString(_) => lit}
-      case Date => {case JsString(_) => lit}
-      case Timestamp => {case JsString(_) => lit}
-      case Party => {case JsString(_) => lit}
-      case ContractId => {case JsString(_) => lit}
-      case List => {case JsArray(_) => lit}
-      case Unit => {case JsObject(_) => lit}
-      case Optional => {case todo => lit}
-      case Map => {case JsObject(_) => lit}
-    }(fallback = sys.error("TODO fallback"))
+  def fromJsObject(
+      it: Map[String, JsValue],
+      typ: iface.Type,
+      defs: TypeLookup): ValuePredicate[iface.Type, JsValue] = {
+    type Result = ValuePredicate[iface.Type, JsValue]
+    def fromValue(it: JsValue, typ: iface.Type): Result =
+      (typ, it).match2 {
+        case p @ iface.TypePrim(_, _) => { case _ => fromPrim(it, p) }
+        case iface.TypeCon(_, _) => ???
+        case iface.TypeNumeric(_) => ???
+        case iface.TypeVar(_) => sys.error("no vars allowed!")
+      }(fallback = ???)
+    def fromDDT(it: Map[String, JsValue], typ: iface.DefDataType.FWT): Result =
+      typ match {
+        case _ => ???
+      }
+    def fromPrim(it: JsValue, typ: iface.TypePrim): Result = {
+      import iface.PrimType._
+      def lit = Literal(it, typ)
+      (typ.typ, it).match2 {
+        case Bool => { case JsBoolean(_) => lit }
+        case Int64 => { case JsNumber(_) | JsString(_) => lit }
+        case Text => { case JsString(_) => lit }
+        case Date => { case JsString(_) => lit }
+        case Timestamp => { case JsString(_) => lit }
+        case Party => { case JsString(_) => lit }
+        case ContractId => { case JsString(_) => lit }
+        case List => { case JsArray(_) => lit }
+        case Unit => { case JsObject(_) => lit }
+        case Optional => { case todo => lit }
+        case Map => { case JsObject(_) => lit }
+      }(fallback = sys.error("TODO fallback"))
+    }
+    RecordSubset(Map()) // TODO
   }
 }
