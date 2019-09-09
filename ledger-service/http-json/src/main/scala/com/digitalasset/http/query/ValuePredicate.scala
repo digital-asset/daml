11b94b3b028a756f8c8d60e78d13f246143d6d95
Stephen Compall <stephen.compall@daml.com>
2019-09-09 16:43:39 -0400

    expand predicates into primitive cases

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/query/ValuePredicate.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/query/ValuePredicate.scala
@@ -4,18 +4,43 @@
 package com.digitalasset.http
 package query
 
-import scalaz.\&/
-import spray.json.{JsObject, JsValue}
+import com.digitalasset.daml.lf.data.Ref
+import com.digitalasset.daml.lf.data.ScalazEqual._
+import com.digitalasset.daml.lf.iface
 
-sealed abstract class ValuePredicate[+LfV] extends Product with Serializable
+import scalaz.\&/
+import spray.json._
+
+sealed abstract class ValuePredicate[+Ty, +LfV] extends Product with Serializable
 
 object ValuePredicate {
-  final case class Literal[+LfV](literal: LfV) extends ValuePredicate[LfV]
-  final case class RecordSubset[+LfV](fields: Map[String, ValuePredicate[LfV]])
-      extends ValuePredicate[LfV]
-  // boolean is whether inclusive (lte vs lt)
-  final case class Range[+LfV](ltgt: (Boolean, LfV) \&/ (Boolean, LfV)) extends ValuePredicate[LfV]
+  type TypeLookup = Ref.Identifier => Option[iface.DefDataType.FWT]
 
-  def fromJsObject(it: Map[String, JsValue]): ValuePredicate[JsValue] =
-    Literal(JsObject())
+  final case class Literal[+Ty, +LfV](literal: LfV, typ: Ty) extends ValuePredicate[Ty, LfV]
+  final case class RecordSubset[+Ty, +LfV](fields: Map[String, ValuePredicate[Ty, LfV]])
+      extends ValuePredicate[Ty, LfV]
+  // boolean is whether inclusive (lte vs lt)
+  final case class Range[+Ty, +LfV](ltgt: (Boolean, LfV) \&/ (Boolean, LfV), typ: Ty) extends ValuePredicate[Ty, LfV]
+
+  def fromJsObject(it: Map[String, JsValue], typ: iface.Type, defs: TypeLookup): ValuePredicate[iface.Type, JsValue] = {
+    RecordSubset(Map())
+  }
+
+  private[this] def fromPrim(it: JsValue, typ: iface.TypePrim): ValuePredicate[iface.Type, JsValue] = {
+    import iface.PrimType._
+    def lit = Literal(it, typ)
+    (typ.typ, it).match2 {
+      case Bool => {case JsBoolean(_) => lit}
+      case Int64 => {case JsNumber(_) | JsString(_) => lit}
+      case Text => {case JsString(_) => lit}
+      case Date => {case JsString(_) => lit}
+      case Timestamp => {case JsString(_) => lit}
+      case Party => {case JsString(_) => lit}
+      case ContractId => {case JsString(_) => lit}
+      case List => {case JsArray(_) => lit}
+      case Unit => {case JsObject(_) => lit}
+      case Optional => {case todo => lit}
+      case Map => {case JsObject(_) => lit}
+    }(fallback = sys.error("TODO fallback"))
+  }
 }
