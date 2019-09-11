1b2047f7345a64de8e680943020f4d51002aecb1
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-09-11 10:32:44 +0200

    Use cc_wrapper in ghcide test
    The cc_wrapper is needed to find library dependencies.

--- bazel_tools/ghci-template.sh
+++ bazel_tools/ghci-template.sh
@@ -5,5 +5,10 @@
 ENV_FILE=$1
 ARGS_FILE=$2
 RULES_HASKELL_EXEC_ROOT=$(dirname $(readlink ${BUILD_WORKSPACE_DIRECTORY}/bazel-out))/
+# Setting -pgm* flags explicitly has the unfortunate side effect
+# of resetting any program flags in the GHC settings file. So we
+# restore them here. See
+# https://ghc.haskell.org/trac/ghc/ticket/7929.
+PGM_ARGS="-pgma {CC} -pgmc {CC} -pgml {CC} -pgmP {CC} -optc-fno-stack-protector -optP-E -optP-undef -optP-traditional"
 echo "{ENV}" > "$ENV_FILE"
-echo "{ARGS}" > "$ARGS_FILE"
+echo $PGM_ARGS "{ARGS}" > "$ARGS_FILE"
