703c95b423462e445ff63ff71d6df4d1b276f9a3
Stephen Compall <stephen.compall@daml.com>
2019-09-18 16:48:30 -0400

    make error throwing in ValuePredicate a little nicer

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/query/ValuePredicate.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/query/ValuePredicate.scala
@@ -67,7 +67,7 @@ sealed abstract class ValuePredicate extends Product with Serializable {
         oq map go cata (csq => { case V.ValueOptional(Some(v)) => csq(v); case _ => false },
         { case V.ValueOptional(None) => true; case _ => false })
 
-      case Range(_, _) => sys.error("range not supported yet")
+      case Range(_, _) => predicateParseError("range not supported yet")
     }
     go(this)
   }
@@ -101,19 +101,19 @@ object ValuePredicate {
         case p @ iface.TypePrim(_, _) => { case _ => fromPrim(it, p) }
         case tc @ iface.TypeCon(iface.TypeConName(id), _) => {
           case _ =>
-            val ddt = defs(id).getOrElse(sys.error(s"Type $id not found"))
+            val ddt = defs(id).getOrElse(predicateParseError(s"Type $id not found"))
             fromCon(it, id, tc instantiate ddt)
         }
         case iface.TypeNumeric(scale) => {
           case JsString(q) =>
-            val nq = Numeric fromString q fold (sys.error(_), identity)
+            val nq = Numeric fromString q fold (predicateParseError(_), identity)
             Literal { case V.ValueNumeric(v) if nq == v => }
           case JsNumber(q) =>
-            val nq = Numeric fromBigDecimal (scale, q) fold (sys.error(_), identity)
+            val nq = Numeric fromBigDecimal (scale, q) fold (predicateParseError(_), identity)
             Literal { case V.ValueNumeric(v) if nq == v => }
         }
-        case iface.TypeVar(_) => sys.error("no vars allowed!")
-      }(fallback = ???)
+        case iface.TypeVar(_) => predicateParseError("no vars allowed!")
+      }(fallback = illTypedQuery(it, typ))
 
     def fromCon(it: JsValue, id: Ref.Identifier, typ: iface.DataType.FWT): Result =
       (typ, it).match2 {
@@ -126,7 +126,7 @@ object ValuePredicate {
             fromVariant(fields, id, fieldTyps)
         }
         case e @ iface.Enum(_) => {
-          case JsString(s) => fromEnum(s, e)
+          case JsString(s) => fromEnum(s, id, e)
         }
       }(fallback = ???)
 
@@ -137,7 +137,7 @@ object ValuePredicate {
       val iface.Record(fieldTyps) = typ
       val invalidKeys = fields.keySet diff fieldTyps.iterator.map(_._1).toSet
       if (invalidKeys.nonEmpty)
-        sys.error(s"$id does not have fields $invalidKeys")
+        predicateParseError(s"$id does not have fields $invalidKeys")
       RecordSubset(fieldTyps map {
         case (fName, fTy) =>
           fields get fName map (fSpec => (fName, fromValue(fSpec, fTy)))
@@ -155,16 +155,16 @@ object ValuePredicate {
           case (n, t) => (n, fromValue(v, t))
         }
         fieldP.fold(
-          sys.error(s"Cannot locate Variant's (datacon, type) field, id: $id, name: $name")
+          predicateParseError(s"Cannot locate Variant's (datacon, type) field, id: $id, name: $name")
         )(VariantMatch)
 
-      case _ => sys.error(s"Variant must have exactly 1 field, got: $fields, id: $id")
+      case _ => predicateParseError(s"Variant must have exactly 1 field, got: $fields, id: $id")
     }
 
-    def fromEnum(it: String, typ: iface.Enum): Result =
+    def fromEnum(it: String, id: Ref.Identifier, typ: iface.Enum): Result =
       if (typ.constructors contains it)
         Literal { case V.ValueEnum(_, v) if it == (v: String) => } else
-        sys.error("not a member of the enum")
+        predicateParseError(s"$it not a member of the enum $id")
 
     def fromOptional(it: JsValue, typ: iface.Type): Result =
       (typ, it).match2 {
@@ -177,13 +177,13 @@ object ValuePredicate {
           case JsNull => OptionalMatch(None)
           case other => OptionalMatch(Some(fromValue(other, typ)))
         }
-      }(fallback = ???)
+      }(fallback = illTypedQuery(it, typ))
 
     def fromPrim(it: JsValue, typ: iface.TypePrim): Result = {
       import iface.PrimType._
       def soleTypeArg(of: String) = typ.typArgs match {
         case Seq(hd) => hd
-        case _ => sys.error(s"missing type arg to $of")
+        case _ => predicateParseError(s"missing type arg to $of")
       }
       (typ.typ, it).match2 {
         case Bool => { case JsBoolean(q) => Literal { case V.ValueBool(v) if q == v => } }
@@ -198,12 +198,12 @@ object ValuePredicate {
         case Text => { case JsString(q) => Literal { case V.ValueText(v) if q == v => } }
         case Date => {
           case JsString(q) =>
-            val dq = Time.Date fromString q fold (sys.error(_), identity)
+            val dq = Time.Date fromString q fold (predicateParseError(_), identity)
             Literal { case V.ValueDate(v) if dq == v => }
         }
         case Timestamp => {
           case JsString(q) =>
-            val tq = Time.Timestamp fromString q fold (sys.error(_), identity)
+            val tq = Time.Timestamp fromString q fold (predicateParseError(_), identity)
             Literal { case V.ValueTimestamp(v) if tq == v => }
         }
         case Party => {
@@ -228,7 +228,7 @@ object ValuePredicate {
             val elemTy = soleTypeArg("Map")
             MapMatch(SortedLookupList(q) mapValue (fromValue(_, elemTy)))
         }
-      }(fallback = sys.error("TODO fallback"))
+      }(fallback = illTypedQuery(it, typ))
     }
 
     (typ match {
@@ -238,6 +238,12 @@ object ValuePredicate {
           recTy <- tc instantiate dt match { case r @ iface.Record(_) => Some(r); case _ => None }
         } yield fromRecord(it, id, recTy)
       case _ => None
-    }) getOrElse sys.error(s"No record type found for $typ")
+    }) getOrElse predicateParseError(s"No record type found for $typ")
   }
+
+  private[this] def illTypedQuery(it: JsValue, typ: Any): Nothing =
+    predicateParseError(s"$it is not a query that can match type $typ")
+
+  private def predicateParseError(s: String): Nothing =
+    sys.error(s)
 }
