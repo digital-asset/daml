617b0cf965ef341f8a871b019ae50c3017a0e61e
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2019-10-03 16:11:10 +0200

    Add authorization wrappers for services

---
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/authorization/services/CommandSubmissionServiceAuthorization.scala
@@ -0,0 +1,38 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.platform.server.api.authorization.services
+
+import com.daml.ledger.participant.state.v1.AuthService
+import com.digitalasset.grpc.adapter.utils.DirectExecutionContext
+import com.digitalasset.ledger.api.v1.command_submission_service.CommandSubmissionServiceGrpc.CommandSubmissionService
+import com.digitalasset.ledger.api.v1.command_submission_service._
+import com.digitalasset.platform.api.grpc.GrpcApiService
+import com.digitalasset.platform.server.api.ProxyCloseable
+import com.digitalasset.platform.server.api.authorization.ApiServiceAuthorization
+import com.google.protobuf.empty.Empty
+import io.grpc.ServerServiceDefinition
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.Future
+
+class CommandSubmissionServiceAuthorization(
+    protected val service: CommandSubmissionService with AutoCloseable,
+    protected val authService: AuthService)
+    extends CommandSubmissionService
+    with ProxyCloseable
+    with GrpcApiService {
+
+  protected val logger: Logger = LoggerFactory.getLogger(CommandSubmissionService.getClass)
+
+  override def submit(request: SubmitRequest): Future[Empty] =
+    ApiServiceAuthorization
+      .requireClaimsForParty(request.commands.map(_.party))
+      .fold(Future.failed(_), _ => service.submit(request))
+
+  override def bindService(): ServerServiceDefinition =
+    CommandSubmissionServiceGrpc.bindService(this, DirectExecutionContext)
+
+  override def close(): Unit = service.close()
+
+}
---
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/authorization/services/LedgerConfigurationServiceAuthorization.scala
@@ -0,0 +1,39 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.platform.server.api.authorization.services
+
+import com.daml.ledger.participant.state.v1.AuthService
+import com.digitalasset.grpc.adapter.utils.DirectExecutionContext
+import com.digitalasset.ledger.api.v1.ledger_configuration_service.LedgerConfigurationServiceGrpc.LedgerConfigurationService
+import com.digitalasset.ledger.api.v1.ledger_configuration_service._
+import com.digitalasset.platform.api.grpc.GrpcApiService
+import com.digitalasset.platform.server.api.ProxyCloseable
+import com.digitalasset.platform.server.api.authorization.ApiServiceAuthorization
+import io.grpc.ServerServiceDefinition
+import io.grpc.stub.StreamObserver
+import org.slf4j.{Logger, LoggerFactory}
+
+class LedgerConfigurationServiceAuthorization(
+    protected val service: LedgerConfigurationService with AutoCloseable,
+    protected val authService: AuthService)
+    extends LedgerConfigurationService
+    with ProxyCloseable
+    with GrpcApiService {
+
+  protected val logger: Logger = LoggerFactory.getLogger(LedgerConfigurationService.getClass)
+
+  override def getLedgerConfiguration(
+      request: GetLedgerConfigurationRequest,
+      responseObserver: StreamObserver[GetLedgerConfigurationResponse]): Unit =
+    ApiServiceAuthorization
+      .requirePublicClaims()
+      .fold(
+        responseObserver.onError,
+        _ => service.getLedgerConfiguration(request, responseObserver))
+
+  override def bindService(): ServerServiceDefinition =
+    LedgerConfigurationServiceGrpc.bindService(this, DirectExecutionContext)
+
+  override def close(): Unit = service.close()
+}
