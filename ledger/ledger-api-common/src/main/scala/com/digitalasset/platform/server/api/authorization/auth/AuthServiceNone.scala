fd46d1dadfbdcd57005f8d18404f81677c526313
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2019-10-03 16:10:48 +0200

    Add two dummy AuthService implementations

---
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/authorization/auth/AuthServiceNone.scala
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.platform.server.api.authorization.auth
+
+import java.util.concurrent.{CompletableFuture, CompletionStage}
+
+import com.daml.ledger.participant.state.v1.{AuthService, Claims}
+import io.grpc.Metadata
+
+/** An AuthService that rejects all calls by always returning an empty set of [[Claims]] */
+case class AuthServiceNone() extends AuthService {
+  override def decodeMetadata(headers: Metadata): CompletionStage[Claims] = {
+    CompletableFuture.completedFuture(Claims.empty)
+  }
+}
---
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/authorization/auth/AuthServiceWildcard.scala
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.platform.server.api.authorization.auth
+
+import java.util.concurrent.{CompletableFuture, CompletionStage}
+
+import com.daml.ledger.participant.state.v1.{AuthService, Claims}
+import io.grpc.Metadata
+
+/** An AuthService that authorizes all calls by always returning a wildcard [[Claims]] */
+case class AuthServiceWildcard() extends AuthService {
+  override def decodeMetadata(headers: Metadata): CompletionStage[Claims] = {
+    CompletableFuture.completedFuture(Claims.wildcard)
+  }
+}
