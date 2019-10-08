9a1b10ef82f99f51048b552470c371b66f696208
Robert Autenrieth <31539813+rautenrieth-da@users.noreply.github.com>
2019-10-08 12:46:43 +0200

    Add a mock AuthService (#3125)
    A simple AuthService implementation useful for tests

---
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/authorization/auth/AuthServiceStatic.scala
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.platform.server.api.authorization.auth
+
+import java.util.concurrent.{CompletableFuture, CompletionStage}
+
+import com.daml.ledger.participant.state.v1.{AuthService, Claims}
+import io.grpc.Metadata
+
+/** An AuthService that matches the value of the `Authorization` HTTP header against
+  * a static map of header values to [[Claims]].
+  *
+  * Note: This AuthService is meant to be used for testing purposes only.
+  */
+class AuthServiceStatic(claims: PartialFunction[String, Claims]) extends AuthService {
+  override def decodeMetadata(headers: Metadata): CompletionStage[Claims] = {
+    if (headers.containsKey(AuthServiceStatic.AUTHORIZATION_KEY)) {
+      val authorizationValue = headers.get(AuthServiceStatic.AUTHORIZATION_KEY)
+      CompletableFuture.completedFuture(claims.lift(authorizationValue).getOrElse(Claims.empty))
+    } else {
+      CompletableFuture.completedFuture(Claims.empty)
+    }
+  }
+}
+
+object AuthServiceStatic {
+  private val AUTHORIZATION_KEY: Metadata.Key[String] =
+    Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER)
+
+  def apply(claims: PartialFunction[String, Claims]) = new AuthServiceStatic(claims)
+}
--- ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/authorization/auth/AuthServiceWildcard.scala
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/authorization/auth/AuthServiceWildcard.scala
@@ -9,7 +9,7 @@ import com.daml.ledger.participant.state.v1.{AuthService, Claims}
 import io.grpc.Metadata
 
 /** An AuthService that authorizes all calls by always returning a wildcard [[Claims]] */
-case object AuthServiceWildcard extends AuthService {
+object AuthServiceWildcard extends AuthService {
   override def decodeMetadata(headers: Metadata): CompletionStage[Claims] = {
     CompletableFuture.completedFuture(Claims.wildcard)
   }
