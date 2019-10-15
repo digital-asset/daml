962b9f0f0d4c2feda9c76300943452f462fd86f1
Stephen Compall <stephen.compall@daml.com>
2019-10-15 16:32:11 -0400

    borrow contract insertion, removing some data to be unused

--- ledger-service/db-backend/src/main/scala/com/digitalasset/http/dbbackend/Queries.scala
+++ ledger-service/db-backend/src/main/scala/com/digitalasset/http/dbbackend/Queries.scala
@@ -5,8 +5,11 @@ package com.digitalaset.http.dbbackend
 
 import doobie._
 import doobie.implicits._
+import spray.json._
 
 object Queries {
+  import Implicits._
+
   val createContractsTable: Fragment = sql"""
       CREATE TABLE
         contract
@@ -22,4 +25,30 @@ object Queries {
         ,witness_parties JSONB NOT NULL
         )
     """
+
+  def insertContract[CA: JsonWriter, WP: JsonWriter](
+      contractId: String,
+      packageId: String,
+      moduleName: String,
+      entityName: String,
+      createArguments: CA,
+      witnessParties: WP): Fragment =
+    sql"""
+        INSERT INTO contract
+        VALUES (
+          DEFAULT, -- archived_by_event_id
+          $contractId,
+          DEFAULT, -- archived_by_transaction_id
+          $packageId,
+          $moduleName,
+          $entityName,
+          ${createArguments.toJson}::jsonb,
+          ${witnessParties.toJson}::jsonb
+        )
+      """
+
+  object Implicits {
+    implicit val `JsValue put`: Put[JsValue] =
+      Put[String].tcontramap(_.compactPrint)
+  }
 }
