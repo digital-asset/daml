f482c9e1bd031556cb113e5cc1cbb8032cf1ef09
Stephen Compall <stephen.compall@daml.com>
2019-10-15 17:34:02 -0400

    offset table declarations

--- ledger-service/db-backend/src/main/scala/com/digitalasset/http/dbbackend/Queries.scala
+++ ledger-service/db-backend/src/main/scala/com/digitalasset/http/dbbackend/Queries.scala
@@ -10,6 +10,8 @@ import spray.json._
 object Queries {
   import Implicits._
 
+  def dropTableIfExists(table: String): Fragment = Fragment.const(s"DROP TABLE IF EXISTS ${table}")
+
   // NB: #, order of arguments must match createContractsTable
   final case class DBContract[CA, WP](
       contractId: String,
@@ -28,6 +30,28 @@ object Queries {
         ,template_entity_name TEXT NOT NULL
         ,create_arguments JSONB NOT NULL
         ,witness_parties JSONB NOT NULL
+        )
+    """
+
+  val indexContractsTable: Fragment = sql"""
+      CREATE INDEX ON contract (package_id, template_module_name, template_entity_name)
+    """
+
+  final case class DBOffset(
+      party: String,
+      packageId: String,
+      templateModuleName: String,
+      templateEntityName: String,
+      lastOffset: String)
+
+  val createOffsetTable: Fragment = sql"""
+      CREATE TABLE
+        ledger_offsets
+        (party TEXT NOT NULL
+        ,package_id TEXT NOT NULL
+        ,template_module_name TEXT NOT NULL
+        ,template_entity_name TEXT NOT NULL
+        ,last_offset TEXT NOT NULL
         )
     """
 
