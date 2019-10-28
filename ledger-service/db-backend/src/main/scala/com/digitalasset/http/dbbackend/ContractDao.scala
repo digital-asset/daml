194e30d57df733eb31420bec624773469aba5d06
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-10-28 10:01:54 -0400

    fixing merge conflicts, updating the way 3rd party deps are specified

--- ledger-service/db-backend/src/main/scala/com/digitalasset/http/dbbackend/ContractDao.scala
+++ ledger-service/db-backend/src/main/scala/com/digitalasset/http/dbbackend/ContractDao.scala
@@ -5,39 +5,33 @@ package com.digitalasset.http.dbbackend
 
 import cats.effect._
 import cats.syntax.apply._
-import com.digitalasset.http.dbbackend.Queries.SurrogateTpId
-import com.digitalasset.http.domain
-import com.typesafe.scalalogging.StrictLogging
-import doobie.free.connection.ConnectionIO
 import doobie.implicits._
-import scalaz.syntax.tag._
 
 import scala.concurrent.ExecutionContext
 
-class ContractDao(xa: DbConnection.T) extends StrictLogging {
+class ContractDao(xa: DbConnection.T) {
 
   private implicit val lh = doobie.util.log.LogHandler.jdkLogHandler
 
   @SuppressWarnings(Array("org.wartremover.warts.Any"))
   def initialize: IO[Unit] = {
-    logger.info(s"Initialzing DB: $xa")
     (Queries.dropAllTablesIfExist *>
       Queries.initDatabase).transact(xa)
   }
 
-  def lastOffset(
-      party: domain.Party,
-      templateId: domain.TemplateId.RequiredPkg): IO[Option[String]] = {
-    val compositeQuery: ConnectionIO[Option[String]] =
-      Queries.surrogateTemplateId(
-        templateId.packageId,
-        templateId.moduleName,
-        templateId.entityName) flatMap { surrogateTpId: SurrogateTpId =>
-        Queries.lastOffset(party.unwrap, surrogateTpId)
-      }
-
-    compositeQuery.transact(xa)
-  }
+//  def lastOffset(
+//      party: domain.Party,
+//      templateId: domain.TemplateId.RequiredPkg): IO[Option[String]] = {
+//    val compositeQuery: ConnectionIO[Option[String]] =
+//      Queries.surrogateTemplateId(
+//        templateId.packageId,
+//        templateId.moduleName,
+//        templateId.entityName) flatMap { surrogateTpId: SurrogateTpId =>
+//        Queries.lastOffset(party.unwrap, surrogateTpId)
+//      }
+//
+//    compositeQuery.transact(xa)
+//  }
 }
 
 object ContractDao {
