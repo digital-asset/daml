6dfd7754511f67869aa2df9b6438c1d299b814b3
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-11-03 21:39:47 +0100

    Restore tests deleted by mistake

---
+++ ledger-api/rs-grpc-akka/src/test/scala/com/digitalasset/grpc/adapter/operation/AkkaClientWithReferenceServiceInProcessSpec.scala
@@ -0,0 +1,7 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.grpc.adapter.operation
+
+class AkkaClientWithReferenceServiceInProcessSpec
+    extends AkkaClientWithReferenceServiceSpecBase(None)
