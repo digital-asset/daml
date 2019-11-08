0fc5f81c61aa57a873695819d095fb3d3ea9c2d7
Gerolf Seitz <gerolf.seitz@digitalasset.com>
2019-11-08 12:54:17 +0100

    simplify test certificate handling
    Adresses https://github.com/digital-asset/daml/pull/3394#pullrequestreview-314076057

--- extractor/src/test/suite/scala/com/digitalasset/extractor/TlsSpec.scala
+++ extractor/src/test/suite/scala/com/digitalasset/extractor/TlsSpec.scala
@@ -4,7 +4,7 @@
 package com.digitalasset.extractor
 
 import java.io.File
-import java.nio.file.{Files, Path}
+import java.nio.file.Files
 
 import com.digitalasset.daml.bazeltools.BazelRunfiles._
 import com.digitalasset.extractor.config.SnapshotEndSetting
@@ -13,7 +13,6 @@ import com.digitalasset.extractor.targets.TextPrintTarget
 import com.digitalasset.ledger.api.testing.utils.SuiteResourceManagementAroundAll
 import com.digitalasset.ledger.api.tls.TlsConfiguration
 import com.digitalasset.platform.sandbox.config.SandboxConfig
-import org.apache.commons.io.FileUtils
 import org.scalatest._
 
 import scala.concurrent.Await
@@ -29,44 +28,25 @@ class TlsSpec
 
   override protected def darFile = new File(rlocation("extractor/VeryLargeArchive.dar"))
 
-  private def extractCerts: Path = {
-    val dir = Files.createTempDirectory("TlsIT").toFile
-    dir.deleteOnExit()
-    List("server.crt", "server.pem", "ca.crt", "client.crt", "client.pem").foreach { src =>
-      val target = new File(dir, src)
-      target.deleteOnExit()
+  val List(serverCrt, serverPem, caCrt, clientCrt, clientPem) = {
+    val dir = Files.createTempDirectory("TlsIT")
+    dir.toFile.deleteOnExit()
+    List("server.crt", "server.pem", "ca.crt", "client.crt", "client.pem").map { src =>
+      val target = dir.resolve(src)
+      target.toFile.deleteOnExit()
       val stream = getClass.getClassLoader.getResourceAsStream("certificates/" + src)
-      FileUtils.copyInputStreamToFile(stream, target)
+      Files.copy(stream, target)
+      Some(target.toFile)
     }
-    dir.toPath
   }
 
-  private lazy val certificatesPath = extractCerts
-  private lazy val certificatesDirPrefix: String = certificatesPath.toString + File.separator
-
-  private lazy val serverCertChainFilePath = Some(new File(certificatesDirPrefix, "server.crt"))
-  private lazy val serverPrivateKeyFilePath = Some(new File(certificatesDirPrefix, "server.pem"))
-  private lazy val trustCertCollectionFilePath = Some(new File(certificatesDirPrefix, "ca.crt"))
-  private lazy val clientCertChainFilePath = Some(new File(certificatesDirPrefix, "client.crt"))
-  private lazy val clientPrivateKeyFilePath = Some(new File(certificatesDirPrefix, "client.pem"))
-
   override protected def config: SandboxConfig =
-    super.config.copy(
-      tlsConfig = Some(
-        TlsConfiguration(
-          true,
-          clientCertChainFilePath,
-          clientPrivateKeyFilePath,
-          trustCertCollectionFilePath)))
+    super.config.copy(tlsConfig = Some(TlsConfiguration(true, serverCrt, serverPem, caCrt)))
 
   "Extractor" should "be able to connect with TLS parameters" in {
     val config = baseConfig.copy(
       ledgerPort = getSandboxPort,
-      tlsConfig = TlsConfiguration(
-        true,
-        serverCertChainFilePath,
-        serverPrivateKeyFilePath,
-        trustCertCollectionFilePath),
+      tlsConfig = TlsConfiguration(true, clientCrt, clientPem, caCrt),
       to = SnapshotEndSetting.Head
     )
     val extractor =
