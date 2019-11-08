0066dfb93fe1056c506d57a1f933a2027c5f89ae
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-11-08 15:36:08 +0100

    Update nixpkgs
    The versions of `pipenv` and `pip` contained in `nixpkgs` where
    previously incompatible. They are compatible in the new `nixpkgs`
    revision.
    
    Just overriding the pip version caused too many uncached rebuilds and
    timed out on CI.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -32,21 +32,6 @@ let
         haskell-src-meta = super.callPackage ./overrides/haskell-src-meta-0.8.2.nix {};
       };
     };
-    python37 = let
-      packageOverrides = _: pythonPackages: {
-        # Update pip for compatibility with pipenv.
-        # This can be removed once nixpkgs is updated to
-        # 12c9003b0208866c7e95b7d3a92bcae559c6424a or later.
-        pip = pythonPackages.pip.overrideAttrs (attrs: rec {
-          pname = "pip";
-          version = "19.3.1";
-          src = pkgs.python37Packages.fetchPypi {
-            inherit pname version;
-            sha256 = "19vyfpkdj2m4svgza3kilk3h25gvm536p2l9d1v527h3q5v7s811";
-          };
-        });
-      };
-    in pkgs.python37.override {inherit packageOverrides;};
   };
 
   nixpkgs = import src {
