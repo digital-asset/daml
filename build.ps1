89d5653800b4027fd6d69af23909a1755c519693
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-11-13 12:47:07 +0100

    Prefetch nodejs_dev_env on Windows (#3447)
    Maybe this helps ¯\_(ツ)_/¯

--- build.ps1
+++ build.ps1
@@ -30,6 +30,10 @@ function bazel() {
 # which is a workaround for this problem.
 bazel shutdown
 
+# Prefetch nodejs_dev_env to avoid permission denied errors on external/nodejs_dev_env/nodejs_dev_env/node.exe
+# It isn’t clear where exactly those errors are coming from.
+bazel fetch @nodejs_dev_env//...
+
 bazel build `-`-experimental_execution_log_file ${ARTIFACT_DIRS}/build_execution_windows.log //...
 
 bazel shutdown
