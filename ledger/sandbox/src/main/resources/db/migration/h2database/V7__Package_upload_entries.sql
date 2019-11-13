11e68dc23302a042875abd059123ff4fc756193f
Brian Healey <brian.healey@digitalasset.com>
2019-11-13 12:28:53 -0500

    create separate package_upload_entries table and include only minimal required data

--- ledger/sandbox/src/main/resources/db/migration/h2database/V7__Package_upload_entries.sql
+++ ledger/sandbox/src/main/resources/db/migration/h2database/V7__Package_upload_entries.sql
@@ -14,22 +14,8 @@ CREATE TABLE package_upload_entries
     submission_id       varchar primary key     not null,
     -- The unique identifier of the package (the hash of its content)
     package_id          varchar                 not null,
-    -- Packages are uploaded as DAR files (i.e., in groups)
-    -- This field can be used to find out which packages were uploaded together
-    upload_id           varchar                 not null,
-    -- A human readable description of the package source
-    source_description  varchar,
     -- participant id that initiated the package upload
     participant_id      varchar                 not null,
-    -- The size of the archive payload (i.e., the serialized DAML-LF package), in bytes
-    size                bigint                   not null,
-    -- The time when the package was added
-    known_since         timestamp                not null,
-    -- The ledger end at the time when the package was added
-    ledger_offset       bigint                   not null,
-    -- The DAML-LF archive, serialized using the protobuf message `daml_lf.Archive`.
-    --  See also `daml-lf/archive/da/daml_lf.proto`.
-    package             bytea                    not null,
     -- If the type is 'rejection', then the rejection reason is set.
     -- Rejection reason is a human-readable description why the change was rejected.
     rejection_reason    varchar
