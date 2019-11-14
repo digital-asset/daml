4eeffaaaab48216b1c6bcec413506a991330369a
Remy Haemmerle <Remy.Haemmerle@digitalasset.com>
2019-11-14 10:55:48 +0100

    cleanup InsertOrdMap

--- daml-lf/data/src/test/scala/com/digitalasset/daml/lf/data/InsertOrdMapTest.scala
+++ daml-lf/data/src/test/scala/com/digitalasset/daml/lf/data/InsertOrdMapTest.scala
@@ -9,15 +9,14 @@ class InsertOrdMapTest extends WordSpec with Matchers {
   "toSeq" should {
     "preserve order" in {
       (InsertOrdMap.empty + (1 -> "a") + (2 -> "b")).toSeq shouldEqual Seq(1 -> "a", 2 -> "b")
-
       (InsertOrdMap.empty + (2 -> "b") + (1 -> "a")).toSeq shouldEqual Seq(2 -> "b", 1 -> "a")
     }
   }
 
-  "fromSeq" should {
+  "apply" should {
     "preserve order" in {
-      InsertOrdMap.fromSeq(Seq(1 -> "a", 2 -> "b")).toSeq shouldEqual Seq(1 -> "a", 2 -> "b")
-      InsertOrdMap.fromSeq(Seq(2 -> "b", 1 -> "a")).toSeq shouldEqual Seq(2 -> "b", 1 -> "a")
+      InsertOrdMap(1 -> "a", 2 -> "b").toSeq shouldEqual Seq(1 -> "a", 2 -> "b")
+      InsertOrdMap(2 -> "b", 1 -> "a").toSeq shouldEqual Seq(2 -> "b", 1 -> "a")
     }
   }
 }
