fd6658688881fd15888b814e59938283c30f4a73
Brian Healey <brian.healey@digitalasset.com>
2019-11-18 11:59:10 -0500

    Add copyright header

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/PackageUploadEntry.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/PackageUploadEntry.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.digitalasset.platform.sandbox.stores.ledger
 
 import com.daml.ledger.participant.state.v1.ParticipantId
@@ -15,3 +18,4 @@ object PackageUploadEntry {
       reason: String
   ) extends PackageUploadEntry
 }
+
