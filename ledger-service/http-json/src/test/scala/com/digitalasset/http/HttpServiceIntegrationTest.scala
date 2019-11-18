86dae7d2d5f2a413dcd4af67fae2a6e8ae9d6685
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-11-18 09:15:08 -0500

    creating a static content dir from the test

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceIntegrationTest.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceIntegrationTest.scala
@@ -7,36 +7,33 @@ import java.io.File
 
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, StatusCodes, Uri}
-import com.digitalasset.daml.bazeltools.BazelRunfiles.rlocation
 import com.digitalasset.http.Statement.discard
-import com.digitalasset.http.util.TestUtil.requiredFile
+import com.digitalasset.http.util.TestUtil.{createDirectory, writeToFile}
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.Assertion
 
 import scala.concurrent.Future
-import scala.io.Source
 
 class HttpServiceIntegrationTest extends AbstractHttpServiceIntegrationTest {
 
-  private val staticContentDir =
-    requiredFile(rlocation("ledger-service/http-json/integration-test-static-content"))
-      .fold(e => throw new IllegalStateException(e), identity)
+  private val staticContentDir = createDirectory("integration-test-static-content")
+    .fold(e => throw new IllegalStateException(e), identity)
 
   override def jdbcConfig: Option[JdbcConfig] = None
 
   override def staticContentConfig: Option[StaticContentConfig] =
     Some(StaticContentConfig(prefix = "static", directory = staticContentDir))
 
-  private val dummyFile = new File(staticContentDir, "dummy.txt")
-  require(dummyFile.exists)
+  private val expectedDummyContent: String = Gen
+    .listOfN(100, Arbitrary.arbitrary[String])
+    .map(_.mkString(" "))
+    .sample
+    .getOrElse(throw new IllegalStateException(s"Cannot create dummy text content"))
 
-  private def expectedDummyContent: String = {
-    val buffer = Source.fromFile(dummyFile)
-    try {
-      buffer.mkString
-    } finally {
-      buffer.close
-    }
-  }
+  private val dummyFile: File =
+    writeToFile(new File(staticContentDir, "dummy.txt"), expectedDummyContent)
+      .fold(e => throw new IllegalStateException(e), identity)
+  require(dummyFile.exists)
 
   "should serve static content from configured directory" in withHttpService { (uri: Uri, _, _) =>
     Http()
--- ledger-service/http-json/src/test/scala/com/digitalasset/http/util/TestUtil.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/util/TestUtil.scala
@@ -3,8 +3,9 @@
 
 package com.digitalasset.http.util
 
-import java.io.File
+import java.io.{BufferedWriter, File, FileWriter}
 import java.net.ServerSocket
+import com.digitalasset.daml.lf.data.TryOps.Bracket.bracket
 
 import scala.util.{Failure, Success, Try}
 
@@ -22,4 +23,19 @@ object TestUtil {
     else
       Failure(new IllegalStateException(s"File doest not exist: $fileName"))
   }
+
+  def createDirectory(path: String): Try[File] = {
+    val file = new File(path)
+    val created = file.mkdirs
+    if (created && file.isDirectory) Success(file)
+    else Failure(new IllegalStateException(s"Cannot create directory: $path"))
+  }
+
+  def writeToFile(file: File, text: String): Try[File] =
+    bracket(Try(new BufferedWriter(new FileWriter(file))))(x => Try(x.close())).flatMap { bw =>
+      Try {
+        bw.write(text)
+        file
+      }
+    }
 }
