105f5dae995fc0ffb1b7fc14f88d8bddec975ea1
Samir Talwar <samir.talwar@digitalasset.com>
2019-11-19 15:36:58 +0100

    sandbox: Implement a dummy grpc.health.v1.Health.Watch endpoint.

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/health/HealthService.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/health/HealthService.scala
@@ -3,19 +3,42 @@
 
 package com.digitalasset.platform.sandbox.health
 
+import akka.Done
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Sink, Source}
 import io.grpc.health.v1.{HealthCheckRequest, HealthCheckResponse, HealthGrpc}
 import io.grpc.stub.StreamObserver
 
-class HealthService extends HealthGrpc.HealthImplBase {
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+import scala.util.{Failure, Success}
+
+class HealthService(implicit materializer: Materializer, executionContext: ExecutionContext)
+    extends HealthGrpc.HealthImplBase {
+  private val servingResponse = HealthCheckResponse
+    .newBuilder()
+    .setStatus(HealthCheckResponse.ServingStatus.SERVING)
+    .build()
+
   override def check(
       request: HealthCheckRequest,
       responseObserver: StreamObserver[HealthCheckResponse],
   ): Unit = {
-    responseObserver.onNext(
-      HealthCheckResponse
-        .newBuilder()
-        .setStatus(HealthCheckResponse.ServingStatus.SERVING)
-        .build())
+    responseObserver.onNext(servingResponse)
     responseObserver.onCompleted()
+  }
+
+  override def watch(
+      request: HealthCheckRequest,
+      responseObserver: StreamObserver[HealthCheckResponse],
+  ): Unit = {
+    Source
+      .repeat(servingResponse)
+      .throttle(1, per = 1.second)
+      .runWith(Sink.foreach(responseObserver.onNext))
+      .onComplete {
+        case Success(Done) => responseObserver.onCompleted()
+        case Failure(error) => responseObserver.onError(error)
+      }
   }
 }
