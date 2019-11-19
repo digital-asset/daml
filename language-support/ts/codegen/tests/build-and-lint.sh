eff993c856bef39dda178c0b0ca7b3cc76f89f26
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-11-19 14:04:57 +0100

    Make test work on Windows

--- language-support/ts/codegen/tests/build-and-lint.sh
+++ language-support/ts/codegen/tests/build-and-lint.sh
@@ -3,10 +3,33 @@
 
 set -euo pipefail
 
-YARN=$PWD/$1
-DAML2TS=$PWD/$2
-DAR=$PWD/$3
-TS_DIR=$PWD/$4
+# --- begin runfiles.bash initialization ---
+# Copy-pasted from Bazel's Bash runfiles library (tools/bash/runfiles/runfiles.bash).
+if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  if [[ -f "$0.runfiles_manifest" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles_manifest"
+  elif [[ -f "$0.runfiles/MANIFEST" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles/MANIFEST"
+  elif [[ -f "$0.runfiles/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+    export RUNFILES_DIR="$0.runfiles"
+  fi
+fi
+if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+  source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
+elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
+            "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
+else
+  echo >&2 "ERROR: cannot find @bazel_tools//tools/bash/runfiles:runfiles.bash"
+  exit 1
+fi
+# --- end runfiles.bash initialization ---
+
+YARN=$(rlocation "$TEST_WORKSPACE/$1")
+DAML2TS=$(rlocation "$TEST_WORKSPACE/$2")
+DAR=$(rlocation "$TEST_WORKSPACE/$3")
+PACKAGE_JSON=$(rlocation "$TEST_WORKSPACE/$4")
+TS_DIR=$(dirname $PACKAGE_JSON)
 
 TMP_DIR=$(mktemp -d)
 cleanup() {
@@ -18,6 +41,6 @@ cp -r $TS_DIR/* $TMP_DIR
 cd $TMP_DIR
 
 $DAML2TS -o generated/src $DAR
-$YARN install
+$YARN install --frozen-lockfile
 $YARN workspaces run build
 $YARN workspaces run lint
