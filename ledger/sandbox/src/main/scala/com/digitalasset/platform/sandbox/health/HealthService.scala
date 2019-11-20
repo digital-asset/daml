3cc71be4db26685c7c58d52406b9335deeef4fad
Samir Talwar <samir.talwar@digitalasset.com>
2019-11-20 14:00:16 +0100

    sandbox: Stop streaming the server health too.

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/health/HealthService.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/health/HealthService.scala
@@ -6,15 +6,21 @@ package com.digitalasset.platform.sandbox.health
 import akka.Done
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Sink, Source}
+import com.digitalasset.grpc.{GrpcException, GrpcStatus}
+import com.digitalasset.platform.sandbox.health.HealthService._
 import io.grpc.health.v1.{HealthCheckRequest, HealthCheckResponse, HealthGrpc}
 import io.grpc.stub.StreamObserver
+import io.grpc.{Context, Status}
 
 import scala.concurrent.ExecutionContext
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.util.control.NonFatal
 import scala.util.{Failure, Success}
 
-class HealthService(implicit materializer: Materializer, executionContext: ExecutionContext)
-    extends HealthGrpc.HealthImplBase {
+class HealthService(watchThrottleFrequency: FiniteDuration = 1.second)(
+    implicit materializer: Materializer,
+    executionContext: ExecutionContext
+) extends HealthGrpc.HealthImplBase {
   private val servingResponse = HealthCheckResponse
     .newBuilder()
     .setStatus(HealthCheckResponse.ServingStatus.SERVING)
@@ -32,14 +38,40 @@ class HealthService(implicit materializer: Materializer, executionContext: Execu
       request: HealthCheckRequest,
       responseObserver: StreamObserver[HealthCheckResponse],
   ): Unit = {
+    val context = Context.current()
+    watch(request, responseObserver, () => context.isCancelled)
+  }
+
+  private[health] def watch(
+      request: HealthCheckRequest,
+      responseObserver: StreamObserver[HealthCheckResponse],
+      isCancelled: () => Boolean,
+  ): Unit = {
     Source
       .repeat(servingResponse)
-      .throttle(1, per = 1.second)
+      .takeWhile(_ => !isCancelled())
+      .throttle(1, per = watchThrottleFrequency)
       .via(dropRepeated)
       .runWith(Sink.foreach(responseObserver.onNext))
       .onComplete {
-        case Success(Done) => responseObserver.onCompleted()
-        case Failure(error) => responseObserver.onError(error)
+        case Success(Done) =>
+          try {
+            responseObserver.onCompleted()
+          } catch {
+            // The stream was cancelled; don't worry about it.
+            case GrpcException(GrpcStatus(Status.Code.CANCELLED, _), _) =>
+          }
+        case Failure(error) =>
+          suppress(responseObserver.onError(error))
       }
   }
+}
+
+object HealthService {
+  private def suppress(f: => Unit): Unit =
+    try {
+      f
+    } catch {
+      case NonFatal(_) => // ignore
+    }
 }
--- ledger/sandbox/src/test/suite/scala/com/digitalasset/platform/sandbox/health/HealthServiceSpec.scala
+++ ledger/sandbox/src/test/suite/scala/com/digitalasset/platform/sandbox/health/HealthServiceSpec.scala
@@ -9,12 +9,15 @@ import akka.testkit.TestKit
 import com.digitalasset.platform.sandbox.health.HealthServiceSpec._
 import io.grpc.health.v1.{HealthCheckRequest, HealthCheckResponse}
 import io.grpc.stub.StreamObserver
+import io.grpc.{Status, StatusRuntimeException}
 import org.mockito.Mockito
+import org.mockito.Mockito._
 import org.scalatest.concurrent.Eventually
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
 
 object HealthServiceSpec {
   private val request = HealthCheckRequest.getDefaultInstance
@@ -43,8 +46,9 @@ class HealthServiceSpec
   "HealthService" should {
     "check the current health" in {
       val responseObserver = mock[StreamObserver[HealthCheckResponse]]
+      val service = new HealthService()
 
-      new HealthService().check(request, responseObserver)
+      service.check(request, responseObserver)
 
       val inOrder = Mockito.inOrder(responseObserver)
       inOrder.verify(responseObserver).onNext(servingResponse)
@@ -54,12 +58,37 @@ class HealthServiceSpec
 
     "observe changes in health" in {
       val responseObserver = mock[StreamObserver[HealthCheckResponse]]
+      val service = new HealthService()
 
-      new HealthService().watch(request, responseObserver)
+      service.watch(request, responseObserver)
 
       eventually {
         val inOrder = Mockito.inOrder(responseObserver)
         inOrder.verify(responseObserver).onNext(servingResponse)
+        inOrder.verifyNoMoreInteractions()
+      }
+    }
+
+    "stop sending health statuses when the channel is closed" in {
+      val responseObserver = mock[StreamObserver[HealthCheckResponse]]
+      val service = new HealthService(watchThrottleFrequency = 1.millisecond)
+
+      var isCancelled = false
+      service.watch(request, responseObserver, () => isCancelled)
+
+      eventually {
+        val inOrder = Mockito.inOrder(responseObserver)
+        inOrder.verify(responseObserver).onNext(servingResponse)
+        inOrder.verifyNoMoreInteractions()
+      }
+
+      isCancelled = true
+      when(responseObserver.onCompleted()).thenThrow(new StatusRuntimeException(Status.CANCELLED))
+
+      eventually {
+        val inOrder = Mockito.inOrder(responseObserver)
+        inOrder.verify(responseObserver).onNext(servingResponse)
+        inOrder.verify(responseObserver).onCompleted()
         inOrder.verifyNoMoreInteractions()
       }
     }
