4c59cfa02a3227d2d7fa4c32490a3ee1dd612b90
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-11-20 14:14:13 +0100

    Add copyright header to unreleased.sh

--- unreleased.sh
+++ unreleased.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# Copyright (c) 2019 The DAML Authors. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 git log $1 | awk '/^$/{next} /CHANGELOG_END/{flag=0; next} /CHANGELOG_BEGIN/{flag=1; next} flag'
