539c9311eebf75115dcf023dc700738e52b313cc
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-11-21 10:46:39 +0100

    Fix unreleased.sh
    The usage of `git log` to avoid printing the diff of each commit turned
    out to cause the commit to be interpreted as a revision range reachable
    from said commit, which breaks unreleased.sh. This commit reverts to
    using `git show` but with `--quiet` to avoid printing the diff, as
    originally planned.

--- unreleased.sh
+++ unreleased.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2019 The DAML Authors. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-for SHA in $(git log --format=%H "$1"); do git log --format=%b "$SHA" | awk '/^$/{next} toupper($0) ~ /CHANGELOG_END/{flag=0; next} toupper($0) ~ /CHANGELOG_BEGIN/{flag=1; next} flag'; done
+for SHA in $(git log --format=%H "$1"); do git show --quiet --format=%b "$SHA" | awk '/^$/{next} toupper($0) ~ /CHANGELOG_END/{flag=0; next} toupper($0) ~ /CHANGELOG_BEGIN/{flag=1; next} flag'; done
