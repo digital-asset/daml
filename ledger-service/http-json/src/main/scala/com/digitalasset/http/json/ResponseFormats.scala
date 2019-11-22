2f053633bed88d91f20111ae672852d2f4528394
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-11-22 16:32:16 -0500

    function to convert Source[JsVal] into result Source[ByteString]

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/json/ResponseFormats.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/json/ResponseFormats.scala
@@ -3,7 +3,10 @@
 
 package com.digitalasset.http.json
 
+import akka.NotUsed
 import akka.http.scaladsl.model._
+import akka.stream.scaladsl.{Concat, Source}
+import akka.util.ByteString
 import spray.json._
 import spray.json.DefaultJsonProtocol._
 
@@ -19,6 +22,20 @@ private[http] object ResponseFormats {
 
   def resultJsObject(a: JsValue): JsObject = {
     JsObject(statusField(StatusCodes.OK), ("result", a))
+  }
+
+  private val start: Source[ByteString, NotUsed] = Source.single(ByteString("{result=["))
+
+  private val end: Source[ByteString, NotUsed] = Source.single(ByteString("]}"))
+
+  def resultJsObject(jsVals: Source[JsValue, NotUsed]): Source[ByteString, NotUsed] = {
+    val csv: Source[ByteString, NotUsed] = jsVals.zipWithIndex.map {
+      case (a, i) =>
+        if (i == 0L) ByteString(a.compactPrint)
+        else ByteString("," + a.compactPrint)
+    }
+
+    Source.combine(start, csv, end)(Concat.apply)
   }
 
   def statusField(status: StatusCode): (String, JsNumber) =
