a97f2969fc0c94d46a70b310775a5012d2644939
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-11-26 17:58:59 +0100

    remove hack

--- bazel_tools/packaging/package-app.sh
+++ bazel_tools/packaging/package-app.sh
@@ -130,7 +130,7 @@ elif [[ "$(uname -s)" == "Darwin" ]]; then
           /usr/bin/install_name_tool -change "$lib" "/usr/lib/system/$libName" "$from_copied"
       elif [[ "$lib" == @rpath/* ]]; then
           libName="${lib#@rpath/}"
-          local to="$WORKDIR/$NAME/lib/$(basename $libName)"
+          local to="$WORKDIR/$NAME/lib/$libName"
           if [[ ! -f "$to" ]]; then
               libOK=0
               for rpath in $rpaths; do
