061c8f60efc5c0908de90c52c8c45f570dcb14c9
Samir Talwar <samir.talwar@digitalasset.com>
2019-11-26 20:12:24 +0100

    ledger-api-health: Use a Map to represent components for health checks.

--- ledger/ledger-api-health/src/main/scala/com/digitalasset/ledger/api/health/HealthChecks.scala
+++ ledger/ledger-api-health/src/main/scala/com/digitalasset/ledger/api/health/HealthChecks.scala
@@ -5,24 +5,30 @@ package com.digitalasset.ledger.api.health
 
 import com.digitalasset.ledger.api.health.HealthChecks._
 
-class HealthChecks(components: Component*) {
-  def hasComponent(componentName: String): Boolean =
+class HealthChecks(private val components: Components) {
+  def this(components: Component*) = this(components.toMap)
+
+  def hasComponent(componentName: ComponentName): Boolean =
     components.exists(_._1 == componentName)
 
-  def ++(other: HealthChecks): HealthChecks = this
+  def ++(other: HealthChecks): HealthChecks = new HealthChecks(this.components ++ other.components)
 
-  def isHealthy(componentName: Option[String]): Boolean =
+  def isHealthy(componentName: Option[ComponentName]): Boolean =
     componentsMatching(componentName).forall(_._2.currentHealth == Healthy)
 
-  private def componentsMatching(componentName: Option[String]): Seq[Component] =
+  private def componentsMatching(componentName: Option[ComponentName]): Components =
     componentName match {
       case None => components
-      case Some(name) => components.filter(_._1 == name)
+      case Some(name) => components.filterKeys(_ == name)
     }
 }
 
 object HealthChecks {
-  type Component = (String, ReportsHealth)
+  type ComponentName = String
+
+  type Component = (ComponentName, ReportsHealth)
+
+  type Components = Map[ComponentName, ReportsHealth]
 
   val empty: HealthChecks = new HealthChecks()
 }
