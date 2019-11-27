a53caafc980db70594d50580c0996825c39cb503
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2019-11-27 17:13:50 +0100

    Address review comment (#3649)
    Addresses https://github.com/digital-asset/daml/pull/3639/files#r350923206

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceTestFixture.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/HttpServiceTestFixture.scala
@@ -165,7 +165,7 @@ object HttpServiceTestFixture {
       implicit ec: ExecutionContext): Future[(DomainJsonEncoder, DomainJsonDecoder)] = {
     val ledgerId = apiLedgerId(client.ledgerId)
     val packageService = new PackageService(
-      HttpService.loadPackageStoreUpdates(client.packageClient, tokenHolder = None))
+      HttpService.loadPackageStoreUpdates(client.packageClient, holderM = None))
     packageService
       .reload(ec)
       .flatMap(x => FutureUtil.toFuture(x))
