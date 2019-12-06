9e8e8f96dd0a67ae59cfb3d4e612f4d2d2c15b1d
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2019-12-06 17:22:31 +0100

    try to use our custom macOS pool
    Also disable Bazel cache write to avoid polluting it.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ci/configure-bazel.sh
+++ ci/configure-bazel.sh
@@ -44,13 +44,3 @@ if is_windows; then
   echo "build --config windows" > .bazelrc.local
   echo "build --config windows-ci" >> .bazelrc.local
 fi
-
-# sets up write access to the shared remote cache if the branch is not a fork
-if [[ "${IS_FORK}" = False ]]; then
-  step "configuring write access to the remote cache"
-  GOOGLE_APPLICATION_CREDENTIALS=$(mktemp .tmp.XXXXXXXXXX)
-  echo "$GOOGLE_APPLICATION_CREDENTIALS_CONTENT" > "$GOOGLE_APPLICATION_CREDENTIALS"
-  unset GOOGLE_APPLICATION_CREDENTIALS_CONTENT
-  export GOOGLE_APPLICATION_CREDENTIALS
-  echo "build --remote_http_cache=https://storage.googleapis.com/daml-bazel-cache --remote_upload_local_results=true --google_credentials=${GOOGLE_APPLICATION_CREDENTIALS}" >> .bazelrc.local
-fi
