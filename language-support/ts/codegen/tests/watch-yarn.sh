e8d339287f9a8926951736e8b54f9c9afa17d7b1
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-12-10 10:55:04 +0100

    daml2ts: Add instructions on how to develop

---
+++ language-support/ts/codegen/tests/watch-yarn.sh
@@ -0,0 +1,12 @@
+# Copyright (c) 2019 The DAML Authors. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/ts
+pwd
+yarn workspace "@digitalasset/daml-json-types" run build
+yarn workspace "@digitalasset/daml-ledger-fetch" run build
+yarn workspace generated run build:watch
