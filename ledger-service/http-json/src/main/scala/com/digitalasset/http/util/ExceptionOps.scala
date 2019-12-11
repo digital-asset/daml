781a8b7b7719a6a9585402f80d913529f1300f0e
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2019-12-11 10:58:39 -0500

    Remove show instance

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/util/ExceptionOps.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/util/ExceptionOps.scala
@@ -3,15 +3,11 @@
 
 package com.digitalasset.http.util
 
-import scalaz.Show
-
 object ExceptionOps {
 
   implicit class ExceptionOps(val underlying: Throwable) extends AnyVal {
     def description: String = getDescription(underlying)
   }
-
-  implicit val showInstance: Show[Throwable] = Show.shows(e => getDescription(e))
 
   def getDescription(e: Throwable): String = {
     val name: String = getClass.getName
