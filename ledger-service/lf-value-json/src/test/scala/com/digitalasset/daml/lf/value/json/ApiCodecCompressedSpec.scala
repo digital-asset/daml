add47a35a99f3cd3ecae666c656f723c86421e47
Stephen Compall <stephen.compall@daml.com>
2019-12-16 19:09:48 -0500

    add sample tests for the JSON format of records and variants

--- ledger-service/lf-value-json/src/test/scala/com/digitalasset/daml/lf/value/json/ApiCodecCompressedSpec.scala
+++ ledger-service/lf-value-json/src/test/scala/com/digitalasset/daml/lf/value/json/ApiCodecCompressedSpec.scala
@@ -257,6 +257,9 @@ class ApiCodecCompressedSpec
       c("[]", VAs.oooi)(Some(None), "[null]"),
       c("[[]]", VAs.oooi)(Some(Some(None)), "[[null]]"),
       cn("""[["42"]]""", "[[42]]", VAs.oooi)(Some(Some(Some(42)))),
+      cn("""{"fA": "foo", "fB": "100"}""", """{"fA": "foo", "fB": 100}""", C.simpleRecordT)(
+        C.simpleRecordV),
+      c("""{"fA": "foo"}""", C.simpleVariantT)(C.simpleVariantV),
     )
 
     val failures = Table(
