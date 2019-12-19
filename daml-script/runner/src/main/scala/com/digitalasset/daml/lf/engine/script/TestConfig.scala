267354cf2d372d744a34e84b16baee8272bb2eae
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-12-19 14:31:31 +0100

    Start on daml test-scripts

---
+++ daml-script/runner/src/main/scala/com/digitalasset/daml/lf/engine/script/TestConfig.scala
@@ -0,0 +1,81 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.daml.lf.engine.script
+
+import java.io.File
+import java.time.Duration
+
+import com.digitalasset.platform.services.time.TimeProviderType
+
+case class TestConfig(
+    darPath: File,
+    ledgerHost: Option[String],
+    ledgerPort: Option[Int],
+    participantConfig: Option[File],
+    timeProviderType: TimeProviderType,
+    commandTtl: Duration,
+)
+
+object TestConfig {
+  private val parser = new scopt.OptionParser[TestConfig]("test-script") {
+    head("test-script")
+
+    opt[File]("dar")
+      .required()
+      .action((f, c) => c.copy(darPath = f))
+      .text("Path to the dar file containing the script")
+
+    opt[String]("ledger-host")
+      .optional()
+      .action((t, c) => c.copy(ledgerHost = Some(t)))
+      .text("Ledger hostname")
+
+    opt[Int]("ledger-port")
+      .optional()
+      .action((t, c) => c.copy(ledgerPort = Some(t)))
+      .text("Ledger port")
+
+    opt[File]("participant-config")
+      .optional()
+      .action((t, c) => c.copy(participantConfig = Some(t)))
+      .text("File containing the participant configuration in JSON format")
+
+    opt[Unit]('w', "wall-clock-time")
+      .action { (t, c) =>
+        c.copy(timeProviderType = TimeProviderType.WallClock)
+      }
+      .text("Use wall clock time (UTC). When not provided, static time is used.")
+
+    opt[Long]("ttl")
+      .action { (t, c) =>
+        c.copy(commandTtl = Duration.ofSeconds(t))
+      }
+      .text("TTL in seconds used for commands emitted by the trigger. Defaults to 30s.")
+
+    checkConfig(c => {
+      // TODO: Start in-memory ledger automatically. See #3687.
+      if (c.ledgerHost.isDefined != c.ledgerPort.isDefined) {
+        failure("Must specify both --ledger-host and --ledger-port")
+      } else if (c.ledgerHost.isDefined && c.participantConfig.isDefined) {
+        failure("Cannot specify both --ledger-host and --participant-config")
+      } else if (c.ledgerHost.isEmpty && c.participantConfig.isEmpty) {
+        failure("Must specify either --ledger-host or --participant-config")
+      } else {
+        success
+      }
+    })
+  }
+  def parse(args: Array[String]): Option[TestConfig] =
+    parser.parse(
+      args,
+      TestConfig(
+        darPath = null,
+        ledgerHost = None,
+        ledgerPort = None,
+        participantConfig = None,
+        timeProviderType = TimeProviderType.Static,
+        commandTtl = Duration.ofSeconds(30L),
+      )
+    )
+}
---
+++ daml-script/runner/src/main/scala/com/digitalasset/daml/lf/engine/script/TestMain.scala
@@ -0,0 +1,100 @@
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.digitalasset.daml.lf.engine.script
+
+import akka.actor.ActorSystem
+import akka.stream._
+import java.time.Instant
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+import scala.io.Source
+import scalaz.syntax.traverse._
+import spray.json._
+
+import com.digitalasset.api.util.TimeProvider
+import com.digitalasset.daml.lf.archive.{Dar, DarReader}
+import com.digitalasset.daml.lf.archive.Decode
+import com.digitalasset.daml.lf.data.Ref.{Identifier, PackageId, QualifiedName}
+import com.digitalasset.daml.lf.language.Ast.Package
+import com.digitalasset.daml_lf_dev.DamlLf
+import com.digitalasset.grpc.adapter.{AkkaExecutionSequencerPool, ExecutionSequencerFactory}
+import com.digitalasset.ledger.api.refinements.ApiTypes.ApplicationId
+import com.digitalasset.ledger.client.configuration.{
+  CommandClientConfiguration,
+  LedgerClientConfiguration,
+  LedgerIdRequirement
+}
+import com.digitalasset.ledger.client.services.commands.CommandUpdater
+import com.digitalasset.platform.services.time.TimeProviderType
+
+object TestMain {
+
+  def main(args: Array[String]): Unit = {
+
+    TestConfig.parse(args) match {
+      case None => sys.exit(1)
+      case Some(config) => {
+        val encodedDar: Dar[(PackageId, DamlLf.ArchivePayload)] =
+          DarReader().readArchiveFromFile(config.darPath).get
+        val dar: Dar[(PackageId, Package)] = encodedDar.map {
+          case (pkgId, pkgArchive) => Decode.readArchivePayload(pkgId, pkgArchive)
+        }
+
+        val applicationId = ApplicationId("Script Test")
+        val clientConfig = LedgerClientConfiguration(
+          applicationId = ApplicationId.unwrap(applicationId),
+          ledgerIdRequirement = LedgerIdRequirement("", enabled = false),
+          commandClient = CommandClientConfiguration.default,
+          sslContext = None
+        )
+        val timeProvider: TimeProvider =
+          config.timeProviderType match {
+            case TimeProviderType.Static => TimeProvider.Constant(Instant.EPOCH)
+            case TimeProviderType.WallClock => TimeProvider.UTC
+            case _ =>
+              throw new RuntimeException(s"Unexpected TimeProviderType: $config.timeProviderType")
+          }
+        val commandUpdater = new CommandUpdater(
+          timeProviderO = Some(timeProvider),
+          ttl = config.commandTtl,
+          overrideTtl = true)
+
+        val system: ActorSystem = ActorSystem("ScriptTest")
+        implicit val sequencer: ExecutionSequencerFactory =
+          new AkkaExecutionSequencerPool("ScriptTestPool")(system)
+        implicit val ec: ExecutionContext = system.dispatcher
+        implicit val materializer: ActorMaterializer = ActorMaterializer()(system)
+
+        val runner = new Runner(dar, applicationId, commandUpdater)
+        val participantParams = config.participantConfig match {
+          case Some(file) => {
+            val source = Source.fromFile(file)
+            val fileContent = try {
+              source.mkString
+            } finally {
+              source.close
+            }
+            val jsVal = fileContent.parseJson
+            import ParticipantsJsonProtocol._
+            jsVal.convertTo[Participants[ApiParameters]]
+          }
+          case None =>
+            Participants(
+              default_participant =
+                Some(ApiParameters(config.ledgerHost.get, config.ledgerPort.get)),
+              participants = Map.empty,
+              party_participants = Map.empty)
+        }
+        // XXX: Iterate over test scripts.
+        val flow: Future[Unit] = for {
+          clients <- Runner.connect(participantParams, clientConfig)
+          _ <- runner.run(clients, scriptId, None)
+        } yield ()
+
+        flow.onComplete(_ => system.terminate())
+        Await.result(flow, Duration.Inf)
+      }
+    }
+  }
+}
