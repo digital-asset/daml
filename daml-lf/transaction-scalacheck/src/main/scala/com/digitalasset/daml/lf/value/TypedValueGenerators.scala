7c1e40f391d0da26266b881db381e96f721a7738
Stephen Compall <stephen.compall@daml.com>
2019-12-19 09:48:27 -0500

    friendly and gen testing for LF records, variants (#3838)
    * drop Nones from records when JSON-encoding
    
    * elide some needless conversions in ApiCodecCompressed
    
    * record with visible Inj
    
    - seems to be running up against scala/scala#5075
    
    * use new 3rdparty lib reference
    
    * construct record types field-by-field
    
    - works around scala/scala#5075
    
    * switch to record style for records' Inj
    
    - suggested by @leo-da
    
    * include some samples to experiment with record
    
    * record parsing
    
    * RecordVa doesn't benefit from kind-projector
    
    * scalafmt
    
    * make RecordVa details private; only intended API is public
    
    * Arbitrary and Shrink record support
    
    * rename RecordVa#Inj to HRec, to accommodate variants in same cons structure
    
    * use record testing in lf-value-json
    
    * add complex record type
    
    * add complex record value and test it
    
    * more documentation for RecordVa
    
    * Table-ify prior record tests
    
    * start adding variant support
    
    * scalacheck support for variants
    
    * more scalacheck support for variants
    
    * variant examples
    
    * example of using align for records
    
    * test variant roundtrip
    
    * test variant in record
    
    * add sample tests for the JSON format of records and variants
    
    * rename record-specific functions in RecordVa
    
    * rename RecordVa to RecVarSpec

--- daml-lf/transaction-scalacheck/src/main/scala/com/digitalasset/daml/lf/value/TypedValueGenerators.scala
+++ daml-lf/transaction-scalacheck/src/main/scala/com/digitalasset/daml/lf/value/TypedValueGenerators.scala
@@ -5,14 +5,27 @@ package com.digitalasset.daml.lf
 package value
 
 import scala.language.higherKinds
-import data.{FrontStack, Numeric, Ref, SortedLookupList, Time}
-import data.ImmArray.ImmArraySeq
+import data.{FrontStack, ImmArray, ImmArrayCons, Numeric, Ref, SortedLookupList, Time}
+import ImmArray.ImmArraySeq
 import data.DataArbitrary._
-import iface.{Type, TypeNumeric, TypePrim, PrimType => PT}
+import iface.{
+  DefDataType,
+  Record,
+  Type,
+  TypeCon,
+  TypeConName,
+  TypeNumeric,
+  TypePrim,
+  PrimType => PT,
+  Variant,
+}
+
+import scalaz.~>
 import scalaz.Id.Id
 import scalaz.syntax.traverse._
 import scalaz.std.option._
 import org.scalacheck.{Arbitrary, Gen, Shrink}
+import Arbitrary.arbitrary
 
 /** [[ValueGenerators]] produce untyped values; for example, if you use the list gen,
   * you get a heterogeneous list.  The generation target here, on the other hand, is
@@ -152,6 +165,205 @@ object TypedValueGenerators {
       }
       override def injshrink[Cid: Shrink] = Shrink.shrinkAny // XXX descend
     }
+
+    /** See [[RecVarSpec]] companion for usage examples. */
+    def record(name: Ref.Identifier, spec: RecVarSpec): (DefDataType.FWT, Aux[spec.HRec]) =
+      (DefDataType(ImmArraySeq.empty, Record(spec.t.to[ImmArraySeq])), new ValueAddend {
+        private[this] val lfvFieldNames = spec.t map { case (n, _) => Some(n) }
+        type Inj[Cid] = spec.HRec[Cid]
+        override val t = TypeCon(TypeConName(name), ImmArraySeq.empty)
+        override def inj[Cid] =
+          hl => ValueRecord(Some(name), (lfvFieldNames zip spec.injRec(hl)).to[ImmArray])
+        override def prj[Cid] = {
+          case ValueRecord(_, fields) if fields.length == spec.t.length =>
+            spec.prjRec(fields)
+          case _ => None
+        }
+        override def injarb[Cid: Arbitrary] = spec.recarb[Cid]
+        override def injshrink[Cid: Shrink] = spec.recshrink
+      })
+
+    /** See [[RecVarSpec]] companion for usage examples. */
+    def variant(name: Ref.Identifier, spec: RecVarSpec): (DefDataType.FWT, Aux[spec.HVar]) =
+      (DefDataType(ImmArraySeq.empty, Variant(spec.t.to[ImmArraySeq])), new ValueAddend {
+        type Inj[Cid] = spec.HVar[Cid]
+        override val t = TypeCon(TypeConName(name), ImmArraySeq.empty)
+        override def inj[Cid] = { cp =>
+          val (ctor, v) = spec.injVar(cp)
+          ValueVariant(Some(name), ctor, v)
+        }
+        override def prj[Cid] = {
+          case ValueVariant(_, name, vv) =>
+            spec.prjVar get name flatMap (_(vv))
+          case _ => None
+        }
+        override def injarb[Cid: Arbitrary] =
+          Arbitrary(Gen.oneOf(spec.vararb[Cid].toSeq).flatMap(_._2))
+        override def injshrink[Cid: Shrink] = spec.varshrink
+      })
+  }
+
+  sealed abstract class RecVarSpec { self =>
+    import shapeless.{::, :+:, Coproduct, HList, Inl, Inr, Witness}
+    import shapeless.labelled.{field, FieldType => :->>:}
+
+    type HRec[Cid] <: HList
+    type HVar[Cid] <: Coproduct
+    def ::[K <: Symbol](h: K :->>: ValueAddend)(implicit ev: Witness.Aux[K]): RecVarSpec {
+      type HRec[Cid] = (K :->>: h.Inj[Cid]) :: self.HRec[Cid]
+      type HVar[Cid] = (K :->>: h.Inj[Cid]) :+: self.HVar[Cid]
+    } =
+      new RecVarSpec {
+        private[this] val fname = Ref.Name assertFromString ev.value.name
+        type HRec[Cid] = (K :->>: h.Inj[Cid]) :: self.HRec[Cid]
+        type HVar[Cid] = (K :->>: h.Inj[Cid]) :+: self.HVar[Cid]
+        override val t = (fname, h.t) :: self.t
+        override def injRec[Cid](v: HRec[Cid]) =
+          h.inj(v.head) :: self.injRec(v.tail)
+        override def prjRec[Cid](v: ImmArray[(_, Value[Cid])]) = v match {
+          case ImmArrayCons(vh, vt) =>
+            for {
+              pvh <- h.prj(vh._2)
+              pvt <- self.prjRec(vt)
+            } yield field[K](pvh) :: pvt
+          case _ => None
+        }
+        override def recarb[Cid: Arbitrary] = {
+          import self.{recarb => tailarb}, h.{injarb => headarb}
+          Arbitrary(arbitrary[(h.Inj[Cid], self.HRec[Cid])] map {
+            case (vh, vt) =>
+              field[K](vh) :: vt
+          })
+        }
+
+        override def recshrink[Cid: Shrink]: Shrink[HRec[Cid]] = {
+          import h.{injshrink => hshrink}, self.{recshrink => tshrink}
+          Shrink {
+            case vh :: vt =>
+              (Shrink.shrink(vh: h.Inj[Cid]) zip Shrink.shrink(vt)) map {
+                case (nh, nt) => field[K](nh) :: nt
+              }
+          }
+        }
+
+        override def injVar[Cid](v: HVar[Cid]) = v match {
+          case Inl(hv) => (fname, h.inj(hv))
+          case Inr(tl) => self.injVar(tl)
+        }
+
+        @SuppressWarnings(Array("org.wartremover.warts.Any"))
+        override val prjVar = self.prjVar transform { (_, tf) =>
+          Lambda[Value ~> PrjResult](tv => tf(tv) map (Inr(_)))
+        } updated (fname, Lambda[Value ~> PrjResult](hv => h.prj(hv) map (pv => Inl(field[K](pv)))))
+
+        override def vararb[Cid: Arbitrary] =
+          self.vararb[Cid] transform { (_, ta) =>
+            ta map (Inr(_))
+          } updated (fname, {
+            import h.{injarb => harb}
+            arbitrary[h.Inj[Cid]] map (hv => Inl(field[K](hv)))
+          })
+
+        override def varshrink[Cid: Shrink] = {
+          val lshr: Shrink[h.Inj[Cid]] = h.injshrink
+          val rshr: Shrink[self.HVar[Cid]] = self.varshrink
+          Shrink {
+            case Inl(hv) => lshr shrink hv map (shv => Inl(field[K](shv)))
+            case Inr(tl) => rshr shrink tl map (Inr(_))
+          }
+        }
+      }
+
+    private[TypedValueGenerators] val t: List[(Ref.Name, Type)]
+    private[TypedValueGenerators] def injRec[Cid](v: HRec[Cid]): List[Value[Cid]]
+    private[TypedValueGenerators] def prjRec[Cid](v: ImmArray[(_, Value[Cid])]): Option[HRec[Cid]]
+    private[TypedValueGenerators] implicit def recarb[Cid: Arbitrary]: Arbitrary[HRec[Cid]]
+    private[TypedValueGenerators] implicit def recshrink[Cid: Shrink]: Shrink[HRec[Cid]]
+
+    private[TypedValueGenerators] def injVar[Cid](v: HVar[Cid]): (Ref.Name, Value[Cid])
+    private[TypedValueGenerators] type PrjResult[Cid] = Option[HVar[Cid]]
+    // could be made more efficient by replacing ~> with a Nat GADT,
+    // but the :+: case is tricky enough as it is
+    private[TypedValueGenerators] val prjVar: Map[Ref.Name, Value ~> PrjResult]
+    private[TypedValueGenerators] implicit def vararb[Cid: Arbitrary]: Map[Ref.Name, Gen[HVar[Cid]]]
+    private[TypedValueGenerators] implicit def varshrink[Cid: Shrink]: Shrink[HVar[Cid]]
+  }
+
+  case object RNil extends RecVarSpec {
+    import shapeless.{HNil, CNil}
+    type HRec[Cid] = HNil
+    type HVar[Cid] = CNil
+    private[TypedValueGenerators] override val t = List.empty
+    private[TypedValueGenerators] override def injRec[Cid](v: HNil) = List.empty
+    private[TypedValueGenerators] override def prjRec[Cid](v: ImmArray[(_, Value[Cid])]) =
+      Some(HNil)
+    private[TypedValueGenerators] override def recarb[Cid: Arbitrary] =
+      Arbitrary(Gen const HNil)
+    private[TypedValueGenerators] override def recshrink[Cid: Shrink] =
+      Shrink.shrinkAny
+
+    private[TypedValueGenerators] override def injVar[Cid](v: CNil) = v.impossible
+    private[TypedValueGenerators] override val prjVar = Map.empty
+    private[TypedValueGenerators] override def vararb[Cid: Arbitrary] = Map.empty
+    private[TypedValueGenerators] override def varshrink[Cid: Shrink] = Shrink.shrinkAny
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.Any"))
+  private[value] object RecVarSpec {
+    // specifying records and variants works the same way: a
+    // record written with ->> and ::, terminated with RNil (*not* HNil)
+    val sample = {
+      import shapeless.syntax.singleton._
+      'foo ->> ValueAddend.int64 :: 'bar ->> ValueAddend.text :: RNil
+    }
+
+    // a RecVarSpec can be turned into a ValueAddend for records
+    val (sampleRecordDDT, sampleAsRecord) =
+      ValueAddend.record(
+        Ref.Identifier(
+          Ref.PackageId assertFromString "hash",
+          Ref.QualifiedName assertFromString "Foo.SomeRecord"),
+        sample)
+    import shapeless.record.Record
+    // supposing Cid = String, you can ascribe a matching value
+    // using either the spec,
+    val sampleData: sample.HRec[String] =
+      Record(foo = 42L, bar = "hi")
+    // or the record VA
+    val sampleDataAgain: sampleAsRecord.Inj[String] = sampleData
+    // ascription is not necessary; a correct `Record` expression already
+    // has the correct type, as implicit conversion is not used at all
+
+    // unlike most `name = value` calls in Scala, `Record` is order-sensitive,
+    // just as DAML-LF record values are order-sensitive. Most tests should preserve
+    // this behavior, but if you want to automatically reorder the keys (for a runtime cost!)
+    // you can do so with `align`. The resulting error messages are far worse, so
+    // I recommend just writing them in the correct order
+    shapeless.test
+      .illTyped("""Record(bar = "bye", foo = 84L): sample.HRec[String]""", "type mismatch.*")
+    val backwardsSampleData: sample.HRec[String] =
+      Record(bar = "bye", foo = 84L).align[sample.HRec[String]]
+
+    // a RecVarSpec can be turned into a ValueAddend for variants
+    val (sampleVariantDDT, sampleAsVariant) =
+      ValueAddend.variant(
+        Ref.Identifier(
+          Ref.PackageId assertFromString "hash",
+          Ref.QualifiedName assertFromString "Foo.SomeVariant"),
+        sample)
+    // supposing Cid = String, you can create a matching value with Coproduct
+    import shapeless.Coproduct, shapeless.syntax.singleton._
+    val sampleVt =
+      Coproduct[sample.HVar[String]]('foo ->> 42L)
+    val anotherSampleVt =
+      Coproduct[sample.HVar[String]]('bar ->> "hi")
+    // and the `variant` function produces Inj as a synonym for HVar
+    // just as `record` makes it a synonym for HRec
+    val samples: List[sampleAsVariant.Inj[String]] = List(sampleVt, anotherSampleVt)
+    // Coproduct can be factored out, but the implicit resolution means you cannot
+    // turn this into the obvious `map` call
+    val sampleCp = Coproduct[sample.HVar[String]]
+    val moreSamples = List(sampleCp('foo ->> 84L), sampleCp('bar ->> "bye"))
   }
 
   trait PrimInstances[F[_]] {
