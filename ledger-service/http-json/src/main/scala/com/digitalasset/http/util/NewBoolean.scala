f2c026b25c6b80f9364c6cd86205d1720396db23
Stephen Compall <stephen.compall@daml.com>
2019-12-24 18:43:51 -0500

    boolean newtype utility

---
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/util/NewBoolean.scala
@@ -0,0 +1,40 @@
+package com.digitalasset.http.util
+
+import language.higherKinds
+
+/** {{{
+  *  object IsTyped extends NewBoolean.Named {
+  *    val SinglyTyped = False
+  *    val WellTyped = True
+  *  }
+  *
+  *  List[IsTyped.T](IsTyped.SinglyTyped, IsTyped.WellTyped)
+  * }}}
+  */
+sealed abstract class NewBoolean {
+  type T <: Boolean
+  val False: T
+  val True: T
+  def subst[F[_]](fa: F[Boolean]): F[T]
+  def apply(b: Boolean): T
+}
+
+object NewBoolean {
+  def Instance: NewBoolean = new NewBoolean {
+    type T = Boolean
+    val False = false
+    val True = true
+    override def subst[F[_]](fa: F[T]) = fa
+    override def apply(b: T) = b
+  }
+
+  // technically could `extends NewBoolean` but we preserve monomorphic calls this way
+  abstract class Named {
+    val NT: NewBoolean = Instance
+    type T = NT.T
+    val False: T = NT.False
+    val True: T = NT.True
+    def subst[F[_]](fa: F[Boolean]): F[T] = NT subst fa
+    def apply(b: Boolean): T = NT(b)
+  }
+}
