3743112eed0c5f283149888f606aac2b956f7a49
associahedron <231829+associahedron@users.noreply.github.com>
2020-01-03 18:21:30 +0000

    Better hook logic and refactoring

--- daml-assistant/src/DA/Daml/Assistant/Install/BashCompletion.hs
+++ daml-assistant/src/DA/Daml/Assistant/Install/BashCompletion.hs
@@ -1,11 +1,9 @@
 -- Copyright (c) 2020 The DAML Authors. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
--- | Installation of bash completions. These are installed in
--- /usr/local/etc/bash_completion.d (on Mac) or
--- /etc/bash_completion.d (elsewhere). These are not installed on
--- Windows by default, and won't be reinstalled unless the user
--- asks by passing @--bash-completions=yes@ explicitly.
+-- | Installation of bash completions. The completion script is
+-- installed in @~/.daml/bash_completion.sh@ and a hook is added
+-- in @~/.bash_completion@ to invoke it if available.
 module DA.Daml.Assistant.Install.BashCompletion
     ( installBashCompletions
     ) where
@@ -13,50 +11,90 @@ module DA.Daml.Assistant.Install.BashCompletion
 import DA.Daml.Assistant.Types
 
 import qualified Data.ByteString.Lazy as BSL
-import Control.Monad.Extra (andM, whenM)
-import System.Directory (doesDirectoryExist, doesFileExist)
+import Control.Exception.Safe (catchIO)
+import Control.Monad.Extra (unless, andM, whenM)
+import System.Directory (getHomeDirectory, doesFileExist)
 import System.FilePath ((</>))
-import System.Info.Extra (isMac, isWindows)
+import System.Info.Extra (isWindows)
 import System.Process.Typed (proc, readProcessStdout_)
+import System.IO.Extra (readFileUTF8, writeFileUTF8)
 
+-- | Install bash completion script if we should.
 installBashCompletions :: InstallOptions -> DamlPath -> (String -> IO ()) -> IO ()
 installBashCompletions options damlPath output =
-    whenM (shouldInstallBashCompletions options) $
+    whenM (shouldInstallBashCompletions options damlPath) $
         doInstallBashCompletions damlPath output
 
-shouldInstallBashCompletions :: InstallOptions -> IO Bool
-shouldInstallBashCompletions options =
+-- | Should we install bash completions? By default, yes, but only if the
+-- we're not on Windows and the completion script hasn't yet been generated.
+shouldInstallBashCompletions :: InstallOptions -> DamlPath -> IO Bool
+shouldInstallBashCompletions options damlPath =
     case iBashCompletions options of
         BashCompletions Yes -> pure True
         BashCompletions No -> pure False
         BashCompletions Auto -> andM
             [ pure (not isWindows)
-            , doesDirectoryExist bashCompletionDirPath
-            , not <$> doesFileExist bashCompletionScriptPath
+            , not <$> doesFileExist (completionScriptPath damlPath)
             ]
 
+-- | Generate the bash completion script, and add a hook.
 doInstallBashCompletions :: DamlPath -> (String -> IO ()) -> IO ()
 doInstallBashCompletions damlPath output = do
-    dirExists <- doesDirectoryExist bashCompletionDirPath
-    if dirExists
-        then do
-            completionScript <- getCompletionScript damlPath
-            BSL.writeFile bashCompletionScriptPath completionScript
-            output "Bash completions installed for DAML assistant."
-        else output ("Bash completions not installed for DAML assistant: " <> bashCompletionDirPath <> " does not exist")
+    let scriptPath = completionScriptPath damlPath
+    script <- getCompletionScript damlPath
+    BSL.writeFile scriptPath script
+    addCompletionHook scriptPath
+    output "Bash completions installed for DAML assistant."
 
+-- | Read the bash completion script from optparse-applicative's
+-- built-in @--bash-completion-script@ routine. Please read
+-- https://github.com/pcapriotti/optparse-applicative/wiki/Bash-Completion
+-- for more details. Note that the bash completion script doesn't
+-- in general contain any daml-assistant specific information, it's only
+-- specific to the path, so we don't need to regenerate it every version.
 getCompletionScript :: DamlPath -> IO BSL.ByteString
 getCompletionScript damlPath = do
     let assistant = assistantPath damlPath
     readProcessStdout_ (proc assistant ["--bash-completion-script", assistant])
 
+-- | Add a completion hook in ~/.bash_completion
+-- Does nothing if the hook is already there
+addCompletionHook :: FilePath -> IO ()
+addCompletionHook scriptPath = do
+    let newHook = makeHook scriptPath
+    hookPath <- getHookPath
+    hooks <- readHooks hookPath
+    unless (newHook `elem` hooks) $ do
+        writeHooks hookPath (hooks ++ [newHook])
+
+newtype HookPath = HookPath FilePath
+newtype Hook = Hook { unHook :: String } deriving Eq
+
+makeHook :: FilePath -> Hook
+makeHook scriptPath = Hook $ concat
+    [ "[ -f "
+    , show scriptPath
+    , " ] && source "
+    , show scriptPath
+    ]
+
+getHookPath :: IO HookPath
+getHookPath = do
+    home <- getHomeDirectory
+    pure (HookPath $ home </> ".bash_completion")
+
+readHooks :: HookPath -> IO [Hook]
+readHooks (HookPath p) = catchIO
+    (map Hook . lines <$> readFileUTF8 p)
+    (const $ pure [])
+
+writeHooks :: HookPath -> [Hook] -> IO ()
+writeHooks (HookPath p) = writeFileUTF8 p . unlines . map unHook
+
+----
+
 assistantPath :: DamlPath -> FilePath
 assistantPath (DamlPath p) = p </> "bin" </> "daml"
 
-bashCompletionDirPath :: FilePath
-bashCompletionDirPath
-    | isMac = "/usr/local/etc/bash_completion.d"
-    | otherwise = "/etc/bash_completion.d"
-
-bashCompletionScriptPath :: FilePath
-bashCompletionScriptPath = bashCompletionDirPath </> "daml"
+completionScriptPath :: DamlPath -> FilePath
+completionScriptPath (DamlPath p) = p </> "bash_completions.sh"
