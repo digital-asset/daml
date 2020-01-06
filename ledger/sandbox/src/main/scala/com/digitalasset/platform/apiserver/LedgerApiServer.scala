3c789f759f1f964dbc8726eaa922f5a5c87abef7
Ratko Veprek <ravep@digitalasset.com>
2020-01-06 14:23:52 +0100

    Rebased to master

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/apiserver/LedgerApiServer.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/apiserver/LedgerApiServer.scala
@@ -136,11 +136,12 @@ class LedgerApiServer(
       workerEventLoopGroup: EventLoopGroup,
       apiServices: ApiServices,
   ) extends ResourceOwner[Server] {
-    override def acquire()(implicit executionContext: ExecutionContext): Resource[Server] =
+    override def acquire()(implicit executionContext: ExecutionContext): Resource[Server] = {
+      val host = address.map(InetAddress.getByName).getOrElse(InetAddress.getLoopbackAddress)
       Resource(
         Future {
-          val builder = address.fold(NettyServerBuilder.forPort(desiredPort))(address =>
-            NettyServerBuilder.forAddress(new InetSocketAddress(address, desiredPort)))
+          val builder =
+            NettyServerBuilder.forAddress(new InetSocketAddress(host, desiredPort))
           builder.sslContext(sslContext.orNull)
           builder.channelType(classOf[NioServerSocketChannel])
           builder.permitKeepAliveTime(10, SECONDS)
@@ -163,6 +164,7 @@ class LedgerApiServer(
         },
         server => Future(server.shutdown().awaitTermination())
       )
+    }
   }
 
   // This is necessary because we need to signal to the ResetService that we have shut down the
