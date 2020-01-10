9d26746ff2436765fa89bfab5f931c4f18615863
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-01-10 16:52:21 +0100

    Moved LedgerRecord into separate file.

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/LedgerReader.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/LedgerReader.scala
@@ -5,16 +5,8 @@ package com.daml.ledger.participant.state.kvutils.api
 
 import akka.NotUsed
 import akka.stream.scaladsl.Source
-import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlLogEntryId
 import com.daml.ledger.participant.state.v1.{Configuration, LedgerId, Offset, TimeModel}
 import com.digitalasset.ledger.api.health.ReportsHealth
-
-class LedgerRecord(val offset: Offset, val entryId: DamlLogEntryId, val envelope: Array[Byte]) {}
-
-object LedgerRecord {
-  def apply(offset: Offset, entryId: DamlLogEntryId, envelope: Array[Byte]): LedgerRecord =
-    new LedgerRecord(offset, entryId, envelope)
-}
 
 trait LedgerReader extends ReportsHealth with AutoCloseable {
   def events(offset: Option[Offset]): Source[LedgerRecord, NotUsed]
