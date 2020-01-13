86f227514f53f332427b5f2958da98b5b2736276
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-01-13 11:25:43 +0100

    Deleted unnecessary file.

--- ledger/participant-state-kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/package.scala
+++
@@ -1,7 +0,0 @@
-package ledger.participant-state-kvutils.src.main.scala.com.daml.ledger.participant.state.kvutils
-
-- state - kvutils.src.main.scala.com.daml.ledger.participant.state.kvutils
-
-package object api {
-
-}
