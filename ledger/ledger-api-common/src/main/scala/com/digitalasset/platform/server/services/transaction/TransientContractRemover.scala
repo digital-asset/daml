4cc14275d50dc6101363cd02283dcc0234b9c155
Remy <remy.haemmerle@daml.com>
2020-01-21 15:39:35 +0100

    engine: node are ordered in transactions (#4129)
    * engine: node in children are ordered
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * address Gerolf's comment

--- ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/services/transaction/TransientContractRemover.scala
+++ ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/services/transaction/TransientContractRemover.scala
@@ -5,8 +5,6 @@ package com.digitalasset.platform.server.services.transaction
 
 import com.digitalasset.ledger.api.domain.ContractId
 import com.digitalasset.ledger.api.domain.Event.{ArchivedEvent, CreateOrArchiveEvent, CreatedEvent}
-import com.digitalasset.platform.api.v1.event.EventOps._
-import scalaz.syntax.tag._
 
 import scala.collection.{breakOut, mutable}
 
@@ -15,6 +13,7 @@ object TransientContractRemover {
   /**
     * Cancels out witnesses on creates and archives that are about the same contract.
     * If no witnesses remain on either, the node is removed.
+    *
     * @param nodes Must be sorted by event index.
     * @throws IllegalArgumentException if the argument is not sorted properly.
     */
@@ -23,30 +22,13 @@ object TransientContractRemover {
     val resultBuilder = new Array[Option[CreateOrArchiveEvent]](nodes.size)
     val creationByContractId = new mutable.HashMap[ContractId, (Int, CreatedEvent)]()
 
-    nodes.iterator.zipWithIndex
-      .foldLeft(-1) {
-        case (prevEventIndex, (event, indexInList)) =>
-          // Each call adds a new (possibly null) element to resultBuilder, and may update items previously added
-          updateResultBuilder(resultBuilder, creationByContractId, event, indexInList)
-
-          // This defensive code has substantial overhead, because we extract the integer event index of every element
-          // from the eventId String. If we received the nodes with integer IDs, it would make this faster.
-          val processedEventIndex = getEventIndex(event.eventId.unwrap)
-          if (processedEventIndex > prevEventIndex) processedEventIndex
-          else failOnUnsortedInput(nodes, prevEventIndex, processedEventIndex)
-      }
+    nodes.zipWithIndex.foreach {
+      case (event, indexInList) =>
+        // Each call adds a new (possibly null) element to resultBuilder, and may update items previously added
+        updateResultBuilder(resultBuilder, creationByContractId, event, indexInList)
+    }
 
     resultBuilder.collect { case Some(v) if v.witnessParties.nonEmpty => v }(breakOut)
-  }
-
-  @SuppressWarnings(Array("org.wartremover.warts.Any"))
-  private def failOnUnsortedInput(
-      nodes: List[CreateOrArchiveEvent],
-      prevEventIndex: Int,
-      processedEventIndex: Int): Nothing = {
-    throw new IllegalArgumentException(
-      s"This method requires all eventIds in its input to be in order. " +
-        s"This was not true for received events ${nodes.map(_.eventId)}: $prevEventIndex appeared before $processedEventIndex")
   }
 
   /**
@@ -57,14 +39,14 @@ object TransientContractRemover {
       resultBuilder: Array[Option[CreateOrArchiveEvent]],
       creationByContractId: mutable.HashMap[ContractId, (Int, CreatedEvent)],
       event: CreateOrArchiveEvent,
-      indexInList: Int): Unit = {
+      indexInList: Int
+  ): Unit =
     event match {
       case createdEvent @ CreatedEvent(_, contractId, _, _, witnessParties, _, _, _, _) =>
         if (witnessParties.nonEmpty) {
           resultBuilder.update(indexInList, Some(event))
           val _ = creationByContractId.put(contractId, indexInList -> createdEvent)
         }
-
       case archivedEvent @ ArchivedEvent(_, contractId, _, witnessParties) =>
         if (witnessParties.nonEmpty) {
           creationByContractId
@@ -84,5 +66,5 @@ object TransientContractRemover {
             }
         }
     }
-  }
+
 }
