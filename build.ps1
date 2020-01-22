2653c6f1fe96088507ecc3ce63f6d309ef20148b
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-01-22 14:43:15 +0100

    Disable eslint_test on Windows (#4161)
    * Disable eslint_test on Windows
    
    This is currently breaking CI completely so for now let’s disable it.
    
    changelog_begin
    changelog_end
    
    * clean expunge

--- build.ps1
+++ build.ps1
@@ -46,6 +46,8 @@ function bazel() {
 # which is a workaround for this problem.
 bazel shutdown
 
+bazel clean --expunge
+
 # Prefetch nodejs_dev_env to avoid permission denied errors on external/nodejs_dev_env/nodejs_dev_env/node.exe
 # It isn’t clear where exactly those errors are coming from.
 bazel fetch @nodejs_dev_env//...
