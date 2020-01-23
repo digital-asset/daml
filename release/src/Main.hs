d18925b8437d6273a8257c3cab8bd079c2964bcf
Robin Krom <robin@digitalasset.com>
2020-01-23 16:55:53 +0100

    generate the .npmrc file

--- release/src/Main.hs
+++ release/src/Main.hs
@@ -7,11 +7,12 @@ module Main (main) where
 import Control.Monad.Extra
 import Control.Monad.IO.Class
 import Control.Monad.Logger
+import Control.Exception
 import Data.Yaml
 import qualified Data.Set as Set
 import qualified Data.List as List
 import Path
-import Path.IO
+import Path.IO hiding (removeFile)
 
 import qualified Data.Text as T
 import qualified Data.Maybe as Maybe
@@ -106,7 +107,15 @@ main = do
                   else
                     $logInfo "No artifacts to upload to Maven Central"
 
-              forM_ npmPackages $ \rule -> liftIO $ callCommand $ "bazel run " <> rule <> ":npm_package.publish"
+              let npmrcPath = ".npmrc"
+              -- We can't put an .npmrc file in the root of the directory because other bazel npm
+              -- code picks it up and looks for the token which is not yet set before the release
+              -- phase.
+              liftIO $ bracket
+                (writeFile npmrcPath "//registry.npmjs.org/:_authToken=${NPM_TOKEN}")
+                (\() -> Dir.removeFile npmrcPath)
+                (\() -> forM_ npmPackages
+                  $ \rule -> liftIO $ callCommand $ "bazel run " <> rule <> ":npm_package.publish")
 
               -- set variables for next steps in Azure pipelines
               liftIO . putStrLn $ "##vso[task.setvariable variable=has_released;isOutput=true]true"
