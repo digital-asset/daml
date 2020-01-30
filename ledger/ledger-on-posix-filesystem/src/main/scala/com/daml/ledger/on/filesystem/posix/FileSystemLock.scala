55bc901b7f1f36ee01676d3d27db388257e5db3e
Samir Talwar <samir.talwar@digitalasset.com>
2020-01-30 11:25:12 +0100

    ledger-on-posix-filesystem: Use `Resource#use` in `FileSystemLock`.

--- ledger/ledger-on-posix-filesystem/src/main/scala/com/daml/ledger/on/filesystem/posix/FileSystemLock.scala
+++ ledger/ledger-on-posix-filesystem/src/main/scala/com/daml/ledger/on/filesystem/posix/FileSystemLock.scala
@@ -8,29 +8,18 @@ import java.nio.channels.OverlappingFileLockException
 import java.nio.file.Path
 
 import com.daml.ledger.on.filesystem.posix.FileSystemLock._
+import com.digitalasset.resources.ResourceOwner
 import com.digitalasset.timer.RetryStrategy
 
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
 
 class FileSystemLock(location: Path) {
   def apply[T](body: => T)(implicit executionContext: ExecutionContext): Future[T] = {
     val file = new RandomAccessFile(location.toFile, "rw")
     val channel = file.getChannel
-    retry { (_, _) =>
-      Future(channel.lock())
-        .flatMap { lock =>
-          Future(body).transformWith {
-            case Success(value) =>
-              lock.close()
-              Future.successful(value)
-            case Failure(exception) =>
-              lock.close()
-              Future.failed(exception)
-          }
-        }
-    }
+    val lock = ResourceOwner.forCloseable(() => channel.lock())
+    retry((_, _) => lock.acquire().use(_ => Future(body)))
   }
 }
 
