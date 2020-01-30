515180f0b0ab8ad6cb5a60f985ded2a246406e7d
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-01-30 14:59:06 -0500

    testing the laws

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/util/LedgerOffsetUtilTest.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/util/LedgerOffsetUtilTest.scala
@@ -3,6 +3,35 @@
 
 package com.digitalasset.http.util
 
+import com.digitalasset.daml.lf.data.FlatSpecCheckLaws
+import com.digitalasset.http.Generators
+import com.digitalasset.ledger.api.v1.ledger_offset.LedgerOffset
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
+import scalaz.scalacheck.ScalazProperties
 
-class LedgerOffsetUtilTest extends FlatSpec with Matchers {}
+class LedgerOffsetUtilTest
+    extends FlatSpec
+    with Matchers
+    with FlatSpecCheckLaws
+    with GeneratorDrivenPropertyChecks {
+
+  implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
+    PropertyCheckConfiguration(minSuccessful = 100)
+
+  import LedgerOffsetUtilTest._
+
+  behavior of LedgerOffsetUtil.AbsoluteOffsetOrdering.getClass.getSimpleName
+
+  checkLaws(ScalazProperties.order.laws[LedgerOffset.Value.Absolute])
+}
+
+object LedgerOffsetUtilTest {
+  import org.scalacheck.Arbitrary
+
+  implicit val arbAbsoluteOffset: Arbitrary[LedgerOffset.Value.Absolute] = Arbitrary(
+    Generators.absoluteLedgerOffsetVal)
+
+  implicit val scalazOrder: scalaz.Order[LedgerOffset.Value.Absolute] =
+    scalaz.Order.fromScalaOrdering(LedgerOffsetUtil.AbsoluteOffsetOrdering)
+}
