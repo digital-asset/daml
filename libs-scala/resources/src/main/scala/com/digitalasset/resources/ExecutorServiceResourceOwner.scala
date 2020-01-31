03654c52a00ffd74928dad38440d43d20032346d
Samir Talwar <samir.talwar@digitalasset.com>
2020-01-31 13:12:28 +0100

    resources: Explain a little more how the ExecutionContext check works.

--- libs-scala/resources/src/main/scala/com/digitalasset/resources/ExecutorServiceResourceOwner.scala
+++ libs-scala/resources/src/main/scala/com/digitalasset/resources/ExecutorServiceResourceOwner.scala
@@ -15,12 +15,21 @@ class ExecutorServiceResourceOwner[T <: ExecutorService](acquireExecutorService:
     Resource(
       Future {
         val executorService = acquireExecutorService()
+        // If we try and release an executor service which is itself being used to power the
+        // releasing, we end up in a deadlock—the executor can't shut down, and therefore
+        // `awaitTermination `never completes. We mitigate this by attempting to catch the problem
+        // early and fail with a meaningful exception.
         executionContext match {
           case context: ExecutionContextExecutorService =>
             if (executorService == context) {
               throw new CannotAcquireExecutionContext()
             }
             // Ugly, but important so that we make sure we're not going to end up in deadlock.
+            // This calls a method on the private `ExecutionContextExecutorServiceImpl` class to get
+            // the underlying executor, then compares it. If you're using a custom
+            // `ExecutionContextExecutorService`, it probably won't follow the same API, so this
+            // will silently ignore it. This means it'll acquire happily but deadlock on release.
+            // We can't cover everything in these checks, unfortunately.
             try {
               val contextExecutor =
                 context.getClass.getMethod("executor").invoke(context).asInstanceOf[ExecutorService]
