3a30c0ca2aa78d1aaac2e2cf6f5b96c6df206038
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-02-03 16:35:52 +0100

    @daml/react: Handle contract keys in TemplateStore.addEvents
    CHANGELOG_BEGIN
    CHANGELOG_END

--- language-support/ts/daml-react/templateStore.test.ts
+++ language-support/ts/daml-react/templateStore.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 The DAML Authors. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Party, Int, Template, ContractId, registerTemplate } from '@daml/types';
+import { Party, Int, Template, ContractId, Date, registerTemplate } from '@daml/types';
 import { CreateEvent, Query, ArchiveEvent } from '@daml/ledger';
 import * as jtv from '@mojotech/json-type-validation';
 import * as Store from './templateStore';
@@ -10,10 +10,18 @@ import * as Store from './templateStore';
 type Foo = {
   owner: Party;
   count: Int;
+  date: Date;
 }
-const Foo: Template<Foo, undefined, 'Mock:Foo'> = {
+// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace Foo {
+  export type Key = {
+    _1: Party;
+    _2: Int;
+  }
+}
+const Foo: Template<Foo, Foo.Key, 'Mock:Foo'> = {
   templateId: 'Mock:Foo',
-  keyDecoder: undefined as unknown as () => jtv.Decoder<undefined>,
+  keyDecoder: undefined as unknown as () => jtv.Decoder<Foo.Key>,
   decoder: undefined as unknown as () => jtv.Decoder<Foo>,
   Archive: {
     template: () => Foo,
@@ -31,37 +39,44 @@ function makeContractId(): ContractId<Foo> {
   return contractId;
 }
 
-function makeCreateEvent(foo: Foo): CreateEvent<Foo, undefined> {
+function makeCreateEvent(foo: Foo): CreateEvent<Foo, Foo.Key> {
   return {
     templateId: Foo.templateId,
     contractId: makeContractId(),
     signatories: [foo.owner],
     observers: [],
     agreementText: '',
-    key: undefined,
+    key: {_1: foo.owner, _2: foo.count},
     payload: foo,
 
   };
 }
 
-function makeArchiveEvent(contract: CreateEvent<Foo, undefined>): {archived: ArchiveEvent<Foo>} {
+function makeArchiveEvent(contract: CreateEvent<Foo, Foo.Key>): {archived: ArchiveEvent<Foo>} {
   return {archived: {templateId: contract.templateId, contractId: contract.contractId}};
 }
 
-function emptyFooStore(): Store.Store<Foo, unknown> {
+function emptyFooStore(): Store.Store<Foo, Foo.Key> {
   return Store.empty();
 }
 
-const alice1 = makeCreateEvent({owner: 'Alice', count: '1'});
-const alice2 = makeCreateEvent({owner: 'Alice', count: '2'});
-const bob1 = makeCreateEvent({owner: 'Bob', count: '1'});
-const bob2 = makeCreateEvent({owner: 'Bob', count: '2'});
+const alice1 = makeCreateEvent({owner: 'Alice', count: '1', date: '2020-02-02'});
+const alice2 = makeCreateEvent({owner: 'Alice', count: '2', date: '2020-02-02'});
+const bob1 = makeCreateEvent({owner: 'Bob', count: '1', date: '2020-02-02'});
+const bob2 = makeCreateEvent({owner: 'Bob', count: '2', date: '2020-02-02'});
+const alice1Next = makeCreateEvent({owner: 'Alice', count: '1', date: '2020-02-03'});
 
 const emptyQuery: Query<Foo> = {};
 const aliceQuery: Query<Foo> = {owner: 'Alice'};
 const bobQuery: Query<Foo> = {owner: 'Bob'};
 const oneQuery: Query<Foo> = {count: '1'};
 const twoQuery: Query<Foo> = {count: '2'};
+
+const alice1Key: Foo.Key = {_1: 'Alice', _2: '1'};
+const bob1Key: Foo.Key = {_1: 'Bob', _2: '1'};
+// NOTE(MH): We need a version of `alice1Key` which has the same value but a
+// different reference.
+const alice1KeyAlt: Foo.Key = {_1: 'Alice', _2: '1'};
 
 describe('setQueryResults', () => {
   it('set empty result', () => {
@@ -106,7 +121,7 @@ describe('setQueryResults', () => {
   });
 });
 
-describe('addEvents', () => {
+describe('addEvents/queryResults', () => {
   it('no events and empty store', () => {
     let store = emptyFooStore();
     store = Store.setQueryResult(store, aliceQuery, []);
@@ -162,6 +177,7 @@ describe('addEvents', () => {
     expect(store.queryResults.size).toBe(1);
     expect(store.queryResults.get(aliceQuery)?.contracts).toEqual([alice1, alice2]);
   });
+
   it('create event matching multiple queries', () => {
     let store = emptyFooStore();
     store = Store.setQueryResult(store, emptyQuery, []);
@@ -278,12 +294,12 @@ describe('addEvents', () => {
     expect(store.queryResults.get(aliceQuery)?.contracts).toEqual([]);
   });
 
+  // NOTE(MH): The JSON API does not do this to us.
   it('create and archive event for same in bad order and empty store', () => {
     let store = emptyFooStore();
     store = Store.setQueryResult(store, aliceQuery, []);
-    store = Store.addEvents(store, [makeArchiveEvent(alice1), {created: alice1}]);
-    expect(store.queryResults.size).toBe(1);
-    expect(store.queryResults.get(aliceQuery)?.contracts).toEqual([]);
+    expect(() => Store.addEvents(store, [makeArchiveEvent(alice1), {created: alice1}]))
+      .toThrow('bad order of create and archive events');
   });
 
   it('create and archive event matching multiple queries', () => {
@@ -300,5 +316,280 @@ describe('addEvents', () => {
     expect(store.queryResults.get(bobQuery)?.contracts).toEqual([bob1]);
     expect(store.queryResults.get(oneQuery)?.contracts).toEqual([alice1, bob1]);
     expect(store.queryResults.get(twoQuery)?.contracts).toEqual([alice2]);
+  });
+});
+
+describe('setFetchByKeyResults', () => {
+  it('set null', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+    expect(store.fetchByKeyResults.get(alice1KeyAlt)).toBeUndefined();
+  });
+
+  it('set contract', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+    expect(store.fetchByKeyResults.get(alice1KeyAlt)).toBeUndefined();
+  });
+
+  it('reset contract to null', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('reset null to contract', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+  });
+
+  it('reset contract to contract', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1Next);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1Next);
+  });
+
+  it('set multiple with different value', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.setFetchByKeyResult(store, bob1Key, bob1);
+    expect(store.fetchByKeyResults.size).toBe(2);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+    expect(store.fetchByKeyResults.get(bob1Key)?.contract).toEqual(bob1);
+  });
+
+  it('set multiple with same value and different reference', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.setFetchByKeyResult(store, alice1KeyAlt, alice1Next);
+    expect(store.fetchByKeyResults.size).toBe(2);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+    expect(store.fetchByKeyResults.get(alice1KeyAlt)?.contract).toEqual(alice1Next);
+  });
+});
+
+describe('addEvents/fetchByKeyResults', () => {
+  it('no events and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, []);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('no events and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, []);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+  });
+
+  it('single relevant create event and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, [{created: alice1}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+  });
+
+  it('single relevant create event and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [{created: alice1Next}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1Next);
+  });
+
+  it('irrelevant create events and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, [{created: alice2}, {created: bob1}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('irrelevant create events and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [{created: alice1}, {created: bob1}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+  });
+
+  it('relevant and irrelevant create events and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, [{created: alice1}, {created: alice2}, {created: bob1}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+  });
+
+  it('relevant and irrelevant create events and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [{created: alice1Next}, {created: alice2}, {created: bob1}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1Next);
+  });
+
+  it('single create event matching multiple keys', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.setFetchByKeyResult(store, alice1KeyAlt, null);
+    store = Store.addEvents(store, [{created: alice1}]);
+    expect(store.fetchByKeyResults.size).toBe(2);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+    expect(store.fetchByKeyResults.get(alice1KeyAlt)?.contract).toEqual(alice1);
+  });
+
+  it('create events matching multiple keys', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.setFetchByKeyResult(store, bob1Key, null);
+    store = Store.addEvents(store, [{created: alice1}, {created: bob1}]);
+    expect(store.fetchByKeyResults.size).toBe(2);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+    expect(store.fetchByKeyResults.get(bob1Key)?.contract).toEqual(bob1);
+  });
+
+  it('archive event and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, [makeArchiveEvent(alice1)]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('relevant archive event and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [makeArchiveEvent(alice1)]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('irrelevant archive event and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [makeArchiveEvent(alice2)]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+  });
+
+  it('relevant and irrelevant archive event and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [makeArchiveEvent(alice1), makeArchiveEvent(alice2)]);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('archive event matching multiple keys', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.setFetchByKeyResult(store, alice1KeyAlt, alice1);
+    store = Store.addEvents(store, [makeArchiveEvent(alice1)]);
+    expect(store.fetchByKeyResults.size).toBe(2);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+    expect(store.fetchByKeyResults.get(alice1KeyAlt)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1KeyAlt)?.contract).toBeNull();
+  });
+
+  it('create and archive event for same contract and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, [{created: alice1}, makeArchiveEvent(alice1)]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('create and archive event for different keys and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.setFetchByKeyResult(store, bob1Key, bob1);
+    store = Store.addEvents(store, [{created: alice1}, makeArchiveEvent(bob1)]);
+    expect(store.fetchByKeyResults.size).toBe(2);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+    expect(store.fetchByKeyResults.get(bob1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(bob1Key)?.contract).toBeNull();
+  });
+
+  it('archive and create event for same key and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [makeArchiveEvent(alice1), {created: alice1Next}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1Next);
+  });
+
+  it('archive and create event for same contract in bad order and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    expect(() => Store.addEvents(store, [makeArchiveEvent(alice1), {created: alice1}]))
+      .toThrow('bad order of create and archive events');
+  });
+
+  it('archive and create event for different keys and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.setFetchByKeyResult(store, bob1Key, bob1);
+    store = Store.addEvents(store, [makeArchiveEvent(bob1), {created: alice1}]);
+    expect(store.fetchByKeyResults.size).toBe(2);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1);
+    expect(store.fetchByKeyResults.get(bob1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(bob1Key)?.contract).toBeNull();
+  });
+
+  it('create and archive event for same key in bad order and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [{created: alice1Next}, makeArchiveEvent(alice1)]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1Next);
+  });
+
+  it('create and archive and create event for same key and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, [{created: alice1}, makeArchiveEvent(alice1), {created: alice1Next}]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1Next);
+  });
+
+  it('archive and create and archive event for same key and populated store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, alice1);
+    store = Store.addEvents(store, [makeArchiveEvent(alice1), {created: alice1Next}, makeArchiveEvent(alice1Next)]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)).toBeDefined();
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toBeNull();
+  });
+
+  it('create and create and archive event for same key in bad order and empty store', () => {
+    let store = emptyFooStore();
+    store = Store.setFetchByKeyResult(store, alice1Key, null);
+    store = Store.addEvents(store, [{created: alice1}, {created: alice1Next}, makeArchiveEvent(alice1)]);
+    expect(store.fetchByKeyResults.size).toBe(1);
+    expect(store.fetchByKeyResults.get(alice1Key)?.contract).toEqual(alice1Next);
   });
 });
