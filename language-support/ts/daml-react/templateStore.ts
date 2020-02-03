0847e549c2105788b93ce20ed5fe8303e55c98ef
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-02-03 16:35:52 +0100

    @daml/react: Handle contract keys in TemplateStore.addEvents
    CHANGELOG_BEGIN
    CHANGELOG_END

--- language-support/ts/daml-react/templateStore.ts
+++ language-support/ts/daml-react/templateStore.ts
@@ -3,7 +3,8 @@
 
 import * as immutable from 'immutable';
 import { Event, Query, CreateEvent} from '@daml/ledger';
-import { ContractId } from '@daml/types'
+import { ContractId } from '@daml/types';
+import deepEqual from 'fast-deep-equal';
 
 export type QueryResult<T extends object, K> = {
   contracts: CreateEvent<T, K>[];
@@ -66,14 +67,23 @@ export const payloadMatchesQuery = <T>(payload: T, query: Query<T>): boolean => 
 }
 
 
-// TODO(MH): We need to update the key lookups as well.
 export const addEvents = <T extends object, K>(store: Store<T, K>, events: Event<T, K>[]): Store<T, K> => {
   const archived: Set<ContractId<T>> = new Set();
   const created: CreateEvent<T, K>[] = [];
   for (const event of events) {
     if ('created' in event) {
-      created.push(event.created);
-    } else {
+      const createEvent = event.created;
+      if (archived.has(createEvent.contractId)) {
+        // NOTE(MH): The handling of contact keys below would do the wrong
+        // thing when the archive event for a contract comes before its create
+        // event. Since the JSON API is not supposed to produce event series
+        // with this property, we fail loudly should it ever do so.
+        throw Error(`TemplateStore.addEvent: bad order of create and archive events for \
+          contract ID ${createEvent.contractId} for template ${createEvent.templateId}.`);
+      } else {
+        created.push(createEvent);
+      }
+    } else { // i.e. 'archived' in event
       archived.add(event.archived.contractId);
     }
   }
@@ -83,7 +93,35 @@ export const addEvents = <T extends object, K>(store: Store<T, K>, events: Event
       .filter((contract) => !archived.has(contract.contractId));
     return {...queryResult, contracts};
   });
-  return {...store, queryResults};
+
+  // NOTE(MH): Since `immutable.Map` uses reference equality for keys when they
+  // are regular JavaScript objects, we cannot simply walk over all create
+  // events and use their key as an index into `store.fetchByKeyResults` but
+  // rather have to walk over the whole cross product of
+  // `store.fetchByKeyResults` and `events`.
+  const fetchByKeyResults = store.fetchByKeyResults.map((result, key) => {
+    let contract = result.contract;
+    for (const event of events) {
+      if ('created' in event) {
+        const createEvent = event.created;
+        if (deepEqual(key, createEvent.key)) {
+          // TODO(MH, #4360): If we were sure we never miss any events and
+          // always get them in causality order, `contract` would always be
+          // `null` when we get here and we should throw an exception
+          // otherwise. However, the current impementation does not even
+          // attempt to get all events...
+          contract = createEvent;
+        }
+      } else { // i.e. 'archived' in event
+        if (contract !== null && contract.contractId === event.archived.contractId) {
+          contract = null;
+        }
+      }
+    }
+    return {...result, contract};
+  });
+
+  return {queryResults, fetchByKeyResults};
 }
 
 export const setFetchByKeyLoading = <T extends object, K>(store: Store<T, K>, key: K): Store<T, K> => ({
