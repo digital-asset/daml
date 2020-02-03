8c26cbc067be18c8591fc290d731bb776773d4c2
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-02-03 13:51:09 +0100

    Update nixpkgs: To update to Bazel 2.0.0
    CHANGELOG_BEGIN
    CHANGELOG_END

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -17,6 +17,7 @@ let
         sha256 = "1jcyd9jy7kz5zfch25s4inwlivb1y1w52fzfjy5ra5vcnp3hmqyr";
         fetchSubmodules = true;
       };
+      patches = [];
     });
     ephemeralpg = pkgs.ephemeralpg.overrideAttrs(oldAttrs: {
       installPhase = ''
