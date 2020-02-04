c175d62c30abc0c58788182e4468e99310035b34
Samir Talwar <samir.talwar@digitalasset.com>
2020-02-04 15:35:23 +0100

    ledger-on-sql: Add a prefix of "ledger_" to the Flyway migrations table.
    And while we're at it, make this a constant in the code and use Flyway
    placeholders to substitute it in.
    
    We don't have any users right now, so changing the migrations should be
    fine.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/ledger-on-sql/src/main/resources/com/daml/ledger/on/sql/migrations/postgresql/V1__create_tables.sql
+++ ledger/ledger-on-sql/src/main/resources/com/daml/ledger/on/sql/migrations/postgresql/V1__create_tables.sql
@@ -1,14 +1,14 @@
 -- Copyright (c) 2020 The DAML Authors. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-CREATE TABLE ledger_log
+CREATE TABLE ${table.prefix}log
 (
     sequence_no SERIAL PRIMARY KEY,
     entry_id    BYTEA NOT NULL,
     envelope    BYTEA NOT NULL
 );
 
-CREATE TABLE ledger_state
+CREATE TABLE ${table.prefix}state
 (
     key   BYTEA PRIMARY KEY NOT NULL,
     value BYTEA             NOT NULL
