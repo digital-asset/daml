b7c117d8e6a4f433d6158a3f71bb4e06cc0c8c00
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-02-10 12:54:36 +0100

    Added some Scaladoc.

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/LedgerStateAccess.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/LedgerStateAccess.scala
@@ -6,8 +6,17 @@ package com.daml.ledger.validator
 import scala.concurrent.{ExecutionContext, Future}
 
 trait LedgerStateAccess {
+
+  /**
+    * Performs read and write operations on the backing store in a single atomic transaction.
+    * @param body  operations to perform
+    * @tparam T type of result returned after execution
+    */
   def inTransaction[T](body: LedgerStateOperations => Future[T]): Future[T]
 
+  /**
+    * @return  participant's ID from which the backing store is being accessed from
+    */
   def participantId: String
 }
 
@@ -15,15 +24,40 @@ trait LedgerStateOperations {
   type Key = Array[Byte]
   type Value = Array[Byte]
 
+  /**
+    * Reads value of a single key from the backing store.
+    * @param key  key to look up data for
+    * @return value corresponding to requested key or None in case it does not exist
+    */
   def readState(key: Key): Future[Option[Value]]
+
+  /**
+    * Reads values of a set of keys from the backing store.
+    * @param keys  list of keys to look up data for
+    * @return  values corresponding to the requested keys, in the same order as requested
+    */
   def readState(keys: Seq[Key]): Future[Seq[Option[Value]]]
 
+  /**
+    * Writes a single key-value pair to the backing store.  In case the key already exists its value is overwritten.
+    */
   def writeState(key: Key, value: Value): Future[Unit]
+
+  /**
+    * Writes a list of key-value pairs to the backing store.  In case a key already exists its value is overwritten.
+    */
   def writeState(keyValuePairs: Seq[(Key, Value)]): Future[Unit]
 
+  /**
+    * Writes a single log entry to the backing store.  The implementation may return Future.failed in case the key
+    * (i.e., the log entry ID) already exists.
+    */
   def appendToLog(key: Key, value: Value): Future[Unit]
 }
 
+/**
+  * Implements non-batching read and write operations on the backing store based on batched implementations.
+  */
 abstract class BatchingLedgerStateOperations(implicit executionContext: ExecutionContext)
     extends LedgerStateOperations {
   override def readState(key: Key): Future[Option[Value]] =
@@ -33,6 +67,9 @@ abstract class BatchingLedgerStateOperations(implicit executionContext: Executio
     writeState(Seq((key, value)))
 }
 
+/**
+  * Implements batching read and write operations on the backing store based on non-batched implementations.
+  */
 abstract class NonBatchingLedgerStateOperations(implicit executionContext: ExecutionContext)
     extends LedgerStateOperations {
   override def readState(keys: Seq[Key]): Future[Seq[Option[Value]]] =
