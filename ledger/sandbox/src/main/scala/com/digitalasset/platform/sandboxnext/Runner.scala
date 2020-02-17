5bf392b83fd8c8c4323b865d7b1d5f12ed67eb62
Fabio Tudone <fabio.tudone@digitalasset.com>
2020-02-17 15:20:35 +0100

    Restore implicit parties allocation for `sandbox-next`

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandboxnext/Runner.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandboxnext/Runner.scala
@@ -32,6 +32,7 @@ import com.digitalasset.platform.indexer.{
 import com.digitalasset.platform.sandbox.banner.Banner
 import com.digitalasset.platform.sandbox.config.SandboxConfig
 import com.digitalasset.platform.sandboxnext.Runner._
+import com.digitalasset.platform.store.ActiveLedgerStateManager.IndexingOptions
 import com.digitalasset.resources.ResourceOwner
 import com.digitalasset.resources.akka.AkkaResourceOwner
 import scalaz.syntax.tag._
@@ -53,6 +54,7 @@ class Runner {
     implicit val system: ActorSystem = ActorSystem("sandbox")
     implicit val materializer: Materializer = Materializer(system)
     implicit val executionContext: ExecutionContext = system.dispatcher
+    implicit val indexingOptions: IndexingOptions = IndexingOptions(implicitPartyAllocation = true)
 
     val ledgerId: v1.LedgerId = Ref.LedgerString.assertFromString(config.ledgerIdMode match {
       case LedgerIdMode.Static(ledgerId) => ledgerId.unwrap
