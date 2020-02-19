57317870429d1bddbc6479276f447493ce93e1ea
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-02-19 09:08:52 +0100

    Cleanup DarReaderTest (#4590)
    Manually matching on every package isn’t actually testing for anything
    meaningful since the number of packages here really isn’t something
    that we should test since we change it quite regularly in the
    compiler.
    
    changelog_begin
    changelog_end

--- daml-lf/archive/src/test/scala/com/digitalasset/daml/lf/archive/DarReaderTest.scala
+++ daml-lf/archive/src/test/scala/com/digitalasset/daml/lf/archive/DarReaderTest.scala
@@ -9,13 +9,13 @@ import com.digitalasset.daml.bazeltools.BazelRunfiles
 import com.digitalasset.daml.lf.data.Ref
 import com.digitalasset.daml.lf.language.LanguageMajorVersion
 import com.digitalasset.daml_lf_dev.{DamlLf, DamlLf1}
-import org.scalatest.{Inside, Matchers, WordSpec}
+import org.scalatest.{Inside, Inspectors, Matchers, WordSpec}
 
 import scala.collection.JavaConverters._
 import scala.util.{Success, Try}
 
 @SuppressWarnings(Array("org.wartremover.warts.Any"))
-class DarReaderTest extends WordSpec with Matchers with Inside with BazelRunfiles {
+class DarReaderTest extends WordSpec with Matchers with Inside with BazelRunfiles with Inspectors {
 
   private val darFile = resource(rlocation("daml-lf/archive/DarReaderTest.dar"))
 
@@ -30,103 +30,55 @@ class DarReaderTest extends WordSpec with Matchers with Inside with BazelRunfile
       DarReaderWithVersion.readArchiveFromFile(darFile)
 
     inside(archives) {
-      case Success(
-          Dar(
-            ((packageId1, archive1), LanguageMajorVersion.V1),
-            ((packageId2, archive2), LanguageMajorVersion.V1) ::
-              ((packageId3, archive3), LanguageMajorVersion.V1) ::
-              ((packageId4, archive4), LanguageMajorVersion.V1) ::
-              ((packageId5, archive5), LanguageMajorVersion.V1) ::
-              ((packageId6, archive6), LanguageMajorVersion.V1) ::
-              ((packageId7, archive7), LanguageMajorVersion.V1) ::
-              ((packageId8, archive8), LanguageMajorVersion.V1) ::
-              ((packageId9, archive9), LanguageMajorVersion.V1) ::
-              ((packageId10, archive10), LanguageMajorVersion.V1) ::
-              ((packageId11, archive11), LanguageMajorVersion.V1) ::
-              ((packageId12, archive12), LanguageMajorVersion.V1) ::
-              ((packageId13, archive13), LanguageMajorVersion.V1) ::
-              ((packageId14, archive14), LanguageMajorVersion.V1) ::
-              ((packageId15, archive15), LanguageMajorVersion.V1) ::
-              ((packageId16, archive16), LanguageMajorVersion.V1) ::
-              ((packageId17, archive17), LanguageMajorVersion.V1) ::
-              ((packageId18, archive18), LanguageMajorVersion.V1) ::
-              Nil)) =>
-        packageId1 shouldNot be('empty)
-        packageId2 shouldNot be('empty)
-        packageId3 shouldNot be('empty)
-        packageId4 shouldNot be('empty)
-        packageId5 shouldNot be('empty)
-        packageId6 shouldNot be('empty)
-        packageId7 shouldNot be('empty)
-        packageId8 shouldNot be('empty)
-        packageId9 shouldNot be('empty)
-        packageId10 shouldNot be('empty)
-        packageId11 shouldNot be('empty)
-        packageId12 shouldNot be('empty)
-        packageId13 shouldNot be('empty)
-        packageId14 shouldNot be('empty)
-        packageId15 shouldNot be('empty)
-        packageId16 shouldNot be('empty)
-        packageId17 shouldNot be('empty)
-        packageId18 shouldNot be('empty)
-        archive1.getDamlLf1.getModulesCount should be > 0
-        archive2.getDamlLf1.getModulesCount should be > 0
-        archive3.getDamlLf1.getModulesCount should be > 0
-        archive4.getDamlLf1.getModulesCount should be > 0
-        archive5.getDamlLf1.getModulesCount should be > 0
-        archive6.getDamlLf1.getModulesCount should be > 0
-        archive7.getDamlLf1.getModulesCount should be > 0
-        archive8.getDamlLf1.getModulesCount should be > 0
-        archive9.getDamlLf1.getModulesCount should be > 0
-        archive10.getDamlLf1.getModulesCount should be > 0
-        archive11.getDamlLf1.getModulesCount should be > 0
-        archive12.getDamlLf1.getModulesCount should be > 0
-        archive13.getDamlLf1.getModulesCount should be > 0
-        archive14.getDamlLf1.getModulesCount should be > 0
-        archive15.getDamlLf1.getModulesCount should be > 0
-        archive16.getDamlLf1.getModulesCount should be > 0
-        archive17.getDamlLf1.getModulesCount should be > 0
-        archive18.getDamlLf1.getModulesCount should be > 0
+      case Success(dar @ Dar(((_, mainArchive), _), dalfDeps)) =>
+        forAll(dar.all) {
+          case ((packageId, archive), ver) =>
+            packageId shouldNot be('empty)
+            archive.getDamlLf1.getModulesCount should be > 0
+            ver should be(LanguageMajorVersion.V1)
+        }
 
-        val archive1Modules = archive1.getDamlLf1.getModulesList.asScala
-        val archive1InternedDotted = archive1.getDamlLf1.getInternedDottedNamesList.asScala
-        val archive1InternedStrings = archive1.getDamlLf1.getInternedStringsList.asScala
+        val mainArchiveModules = mainArchive.getDamlLf1.getModulesList.asScala
+        val mainArchiveInternedDotted = mainArchive.getDamlLf1.getInternedDottedNamesList.asScala
+        val mainArchiveInternedStrings = mainArchive.getDamlLf1.getInternedStringsList.asScala
         inside(
-          archive1Modules
+          mainArchiveModules
             .find(
               m =>
                 internedName(
-                  archive1InternedDotted,
-                  archive1InternedStrings,
+                  mainArchiveInternedDotted,
+                  mainArchiveInternedStrings,
                   m.getNameInternedDname) == "DarReaderTest")) {
           case Some(module) =>
             val actualTypes: Set[String] =
               module.getDataTypesList.asScala.toSet.map(
                 (t: DamlLf1.DefDataType) =>
                   internedName(
-                    archive1InternedDotted,
-                    archive1InternedStrings,
+                    mainArchiveInternedDotted,
+                    mainArchiveInternedStrings,
                     t.getNameInternedDname))
             actualTypes should contain allOf ("Transfer", "Call2", "CallablePayout", "PayOut")
         }
 
-        val archive2Modules = archive2.getDamlLf1.getModulesList.asScala
-        val archive2InternedDotted = archive2.getDamlLf1.getInternedDottedNamesList.asScala
-        val archive2InternedStrings = archive2.getDamlLf1.getInternedStringsList.asScala
-        val archive2ModuleNames: Set[String] = archive2Modules
-          .map(m =>
-            internedName(archive2InternedDotted, archive2InternedStrings, m.getNameInternedDname))
-          .toSet
-        archive2ModuleNames shouldBe Set(
-          "GHC.Enum",
-          "GHC.Show",
-          "GHC.Num",
-          "GHC.Classes",
-          "Control.Exception.Base",
-          "GHC.Err",
-          "GHC.Base",
-          "LibraryModules"
-        )
+        forExactly(1, dalfDeps) {
+          case ((_, archive), _) =>
+            val archiveModules = archive.getDamlLf1.getModulesList.asScala
+            val archiveInternedDotted = archive.getDamlLf1.getInternedDottedNamesList.asScala
+            val archiveInternedStrings = archive.getDamlLf1.getInternedStringsList.asScala
+            val archiveModuleNames = archiveModules
+              .map(m =>
+                internedName(archiveInternedDotted, archiveInternedStrings, m.getNameInternedDname))
+              .toSet
+            archiveModuleNames shouldBe Set(
+              "GHC.Enum",
+              "GHC.Show",
+              "GHC.Num",
+              "GHC.Classes",
+              "Control.Exception.Base",
+              "GHC.Err",
+              "GHC.Base",
+              "LibraryModules")
+        }
     }
   }
 
