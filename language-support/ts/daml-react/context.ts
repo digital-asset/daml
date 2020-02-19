9eafe3c89d5679f67fef131bb919e0ba076d1b03
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-02-19 11:28:42 +0100

    @daml/react: Port version from DAVL
    By using the streaming endpoint of the JSON API we can get rid of  a
    lot of internal state management.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- language-support/ts/daml-react/context.ts
+++ language-support/ts/daml-react/context.ts
@@ -1,15 +1,13 @@
 // Copyright (c) 2020 The DAML Authors. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import React from 'react';
 import Ledger from '@daml/ledger';
-import * as LedgerStore from './ledgerStore';
-import React from "react";
-import { Action } from "./reducer";
 import { Party } from '@daml/types';
 
 export type DamlLedgerState = {
-  store: LedgerStore.Store;
-  dispatch: React.Dispatch<Action>;
+  reloadToken: unknown;
+  triggerReload: () => void;
   party: Party;
   ledger: Ledger;
 }
