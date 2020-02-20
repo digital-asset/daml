85c2d267ac579751b502e825e784022a51c253b2
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-02-20 12:13:08 +0100

    Update MainView in App Arch section

--- docs/source/getting-started/code/ui-before/MainView.tsx
+++ docs/source/getting-started/code/ui-before/MainView.tsx
@@ -1,26 +1,28 @@
-// Copyright (c) 2020 The DAML Authors. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Container, Grid, Header, Icon, Segment, Divider } from 'semantic-ui-react';
 import { Party } from '@daml/types';
+import { User } from '@daml2ts/create-daml-app/lib/create-daml-app-0.1.0/User';
 import { useParty, useReload, useExerciseByKey, useFetchByKey, useQuery } from '@daml/react';
 import UserList from './UserList';
 import PartyListEdit from './PartyListEdit';
-import { User } from '@daml2ts/create-daml-app/lib/create-daml-app-0.1.0/User';
 
 const MainView: React.FC = () => {
-// -- HOOKS_BEGIN
+// HOOKS_BEGIN
   const username = useParty();
-  const myUserResult = useFetchByKey<User, Party>(User, () => username, [username]);
+  const myUserResult = useFetchByKey(User, () => username, [username]);
   const myUser = myUserResult.contract?.payload;
-  const allUsersResult = useQuery<User, Party>(User);
-  const allUsers = allUsersResult.contracts.map((user) => user.payload);
-// -- HOOKS_END
-  const reload = useReload();
+  const allUsers = useQuery(User).contracts;
+// HOOKS_END
+
+  // Sorted list of friends of the current user
+  const friends = useMemo(() =>
+    allUsers
+    .map(user => user.payload)
+    .filter(user => user.username !== username)
+    .sort((x, y) => x.username.localeCompare(y.username)),
+    [allUsers, username]);
 
   const [exerciseAddFriend] = useExerciseByKey(User.AddFriend);
-  const [exerciseRemoveFriend] = useExerciseByKey(User.RemoveFriend);
 
   const addFriend = async (friend: Party): Promise<boolean> => {
     try {
@@ -32,14 +34,10 @@ const MainView: React.FC = () => {
     }
   }
 
-  const removeFriend = async (friend: Party): Promise<void> => {
-    try {
-      await exerciseRemoveFriend(username, {friend});
-    } catch (error) {
-      alert("Unknown error:\n" + JSON.stringify(error));
-    }
-  }
+  const messageFriend = (friend: Party) =>
+    alert('Messaging parties is not yet implemented.');
 
+  const reload = useReload();
   React.useEffect(() => {
     const interval = setInterval(reload, 5000);
     return () => clearInterval(interval);
@@ -66,7 +64,7 @@ const MainView: React.FC = () => {
               <PartyListEdit
                 parties={myUser?.friends ?? []}
                 onAddParty={addFriend}
-                onRemoveParty={removeFriend}
+                onMessageParty={messageFriend}
               />
             </Segment>
             <Segment>
@@ -86,7 +84,7 @@ const MainView: React.FC = () => {
               </Header>
               <Divider />
               <UserList
-                users={allUsers.sort((user1, user2) => user1.username.localeCompare(user2.username))}
+                users={friends}
                 onAddFriend={addFriend}
               />
             </Segment>
