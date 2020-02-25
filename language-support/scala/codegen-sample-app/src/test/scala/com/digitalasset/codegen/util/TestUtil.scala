9bdfbb0534a575cab6f2bea21dcac18d021fb0d5
Samir Talwar <samir.talwar@digitalasset.com>
2020-02-25 14:09:28 +0100

    Avoid opening a server to the world when finding a free port. (#4683)
    * Avoid opening a server to the world when finding a free port.
    
    This is very annoying on macOS because we get a focus-stealing popup for
    a split second, asking for permission to allow the server through the
    firewall. The popup pretty much always disappears before it can even be
    read, when the server is closed.
    
    This is almost certainly not an attack vector, because:
      - we only do this in tests,
      - the server is open for only a few milliseconds,
      - nothing is served,
      - and finding the port is tricky, because it's effectively random.
    
    Nevertheless, it's very annoying.
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Extract a Bazel package for finding free ports.
    
    We seem to do it in 4 different places, which I think is enough to
    remove the duplication.

--- language-support/scala/codegen-sample-app/src/test/scala/com/digitalasset/codegen/util/TestUtil.scala
+++ language-support/scala/codegen-sample-app/src/test/scala/com/digitalasset/codegen/util/TestUtil.scala
@@ -3,26 +3,15 @@
 
 package com.digitalasset.codegen.util
 
-import com.digitalasset.daml.bazeltools.BazelRunfiles._
-
 import java.io.File
-import java.net.ServerSocket
 
+import com.digitalasset.daml.bazeltools.BazelRunfiles._
 import scalaz.{@@, Tag}
-
-import scala.util.Try
 
 object TestUtil {
   sealed trait TestContextTag
   type TestContext = String @@ TestContextTag
-  val TestContext = Tag.of[TestContextTag]
-
-  def findOpenPort(): Try[Int] = Try {
-    val socket = new ServerSocket(0)
-    val result = socket.getLocalPort
-    socket.close()
-    result
-  }
+  val TestContext: Tag.TagOf[TestContextTag] = Tag.of[TestContextTag]
 
   def requiredResource(path: String): File = {
     val f = new File(rlocation(path)).getAbsoluteFile
