e3c1186abf8325887930951e1b30a3fbccd5749c
Samir Talwar <samir.talwar@digitalasset.com>
2020-02-25 11:02:11 +0100

    Extract a Bazel package for finding free ports.
    We seem to do it in 4 different places, which I think is enough to
    remove the duplication.

--- language-support/scala/codegen-sample-app/src/test/scala/com/digitalasset/codegen/util/TestUtil.scala
+++ language-support/scala/codegen-sample-app/src/test/scala/com/digitalasset/codegen/util/TestUtil.scala
@@ -4,24 +4,14 @@
 package com.digitalasset.codegen.util
 
 import java.io.File
-import java.net.{InetAddress, ServerSocket}
 
 import com.digitalasset.daml.bazeltools.BazelRunfiles._
 import scalaz.{@@, Tag}
 
-import scala.util.Try
-
 object TestUtil {
   sealed trait TestContextTag
   type TestContext = String @@ TestContextTag
-  val TestContext = Tag.of[TestContextTag]
-
-  def findOpenPort(): Try[Int] = Try {
-    val socket = new ServerSocket(0, 0, InetAddress.getLoopbackAddress)
-    val result = socket.getLocalPort
-    socket.close()
-    result
-  }
+  val TestContext: Tag.TagOf[TestContextTag] = Tag.of[TestContextTag]
 
   def requiredResource(path: String): File = {
     val f = new File(rlocation(path)).getAbsoluteFile
