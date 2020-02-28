82ab736916e843fa80faee96f0c50d788ec1a96d
Samir Talwar <samir.talwar@digitalasset.com>
2020-02-28 13:51:34 +0100

    ledger-on-sql: Add a script to hash all migrations.

---
+++ ledger/ledger-on-sql/hash-migrations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2020 The DAML Authors. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -u
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+for file in "$DIR"/src/main/resources/com/daml/ledger/on/sql/migrations/**/*.sql; do
+  shasum -a 256 "$file" | awk '{ print $1 }' > "$file.sha256"
+done
