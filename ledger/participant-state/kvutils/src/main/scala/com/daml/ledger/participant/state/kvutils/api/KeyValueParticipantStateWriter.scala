87b0693d4960e4391ee8a4576b5fab6d7fa76ec5
Fabio Tudone <fabio.tudone@digitalasset.com>
2020-03-03 23:45:30 +0100

    Remove unneeded `val` from `KeyvalueParticipantState[Reader|Writer]` constructor params

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/KeyValueParticipantStateWriter.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/KeyValueParticipantStateWriter.scala
@@ -16,7 +16,7 @@ import com.digitalasset.ledger.api.health.HealthStatus
 import scala.compat.java8.FutureConverters
 import scala.concurrent.ExecutionContext
 
-class KeyValueParticipantStateWriter(val writer: LedgerWriter)(
+class KeyValueParticipantStateWriter(writer: LedgerWriter)(
     implicit executionContext: ExecutionContext)
     extends WriteService {
 
