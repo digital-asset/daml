fc42e80cedced680969f430628865c50cd5ca7e3
Samir Talwar <samir.talwar@digitalasset.com>
2020-03-05 14:33:00 +0100

    sandbox-next: Disable ResetService tests.
    They're too flaky. We'll bring them back once we work out the
    performance issues.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/sandbox/src/test/suite/scala/com/digitalasset/platform/sandboxnext/services/reset/ResetServiceInMemoryIT.scala
+++ ledger/sandbox/src/test/suite/scala/com/digitalasset/platform/sandboxnext/services/reset/ResetServiceInMemoryIT.scala
@@ -5,7 +5,9 @@ package com.digitalasset.platform.sandboxnext.services.reset
 
 import com.digitalasset.platform.sandbox.services.reset.ResetServiceITBase
 import com.digitalasset.platform.sandboxnext.SandboxNextFixture
+import org.scalatest.Ignore
 
+@Ignore
 final class ResetServiceInMemoryIT extends ResetServiceITBase with SandboxNextFixture {
   override def spanScaleFactor: Double = super.spanScaleFactor * 2
 }
