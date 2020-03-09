5e110aecc4e2083ff9c7d5e50944d36ae8822e8d
Samir Talwar <samir.talwar@digitalasset.com>
2020-03-09 15:57:31 +0100

    sandbox: Document why `implicitPartyAllocation` is dangerous.

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/configuration/PartyConfiguration.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/configuration/PartyConfiguration.scala
@@ -3,7 +3,22 @@
 
 package com.digitalasset.platform.configuration
 
-case class PartyConfiguration(implicitPartyAllocation: Boolean)
+/**
+  * Configuration surrounding parties and party allocation.
+  */
+case class PartyConfiguration(
+    /**
+      * Informs the Command Submission Service that any parties mentioned in a submission that do
+      * not already exist should be created on the fly. This behavior will not make sense for most
+      * ledgers, because:
+      *   - allocating parties can be racy if multiple transactions are coming through at once,
+      *   - if allocation fails, the errors are a little confusing, and
+      *   - parties can be allocated without administrator privileges.
+      *
+      * Enable at your own risk.
+      */
+    implicitPartyAllocation: Boolean,
+)
 
 object PartyConfiguration {
   val default: PartyConfiguration = PartyConfiguration(
