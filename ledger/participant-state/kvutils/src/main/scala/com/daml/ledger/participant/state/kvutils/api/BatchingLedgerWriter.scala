a03dfdc51a1b41f2412477d9f4f2e7a0dae7d8ee
Jussi Maki <jussi.maki@digitalasset.com>
2020-03-12 17:49:45 +0100

    Add missing copyright header

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/BatchingLedgerWriter.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/BatchingLedgerWriter.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2020 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.participant.state.kvutils.api
 
 import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
