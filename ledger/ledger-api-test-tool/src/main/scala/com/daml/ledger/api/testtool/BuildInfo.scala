2436b97cf7189e23dc1fbfca3ea6d87257333727
Fabio Tudone <fabio.tudone@digitalasset.com>
2020-03-13 21:19:59 +0100

    Add copyright header

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/BuildInfo.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/BuildInfo.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2020 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.api.testtool
 
 import java.io.{BufferedReader, InputStreamReader}
@@ -16,3 +19,4 @@ object BuildInfo {
       }
     }
 }
+
