26ef467b146a93870df16a6958fc2c1a40332f5f
Samir Talwar <samir.talwar@digitalasset.com>
2020-03-17 15:43:23 +0100

    daml-assistant: Add `daml sandbox-next`.
    CHANGELOG_BEGIN
    - [DAML Assistant] You can now run a pre-release version of Sandbox with
      ``daml sandbox-next`` so you can test it out and verify everything is
      working as expected. Running this will launch Sandbox rebuilt on a
      more modern architecture. An upcoming release of DAML will switch over
      to the new implementation by default.
    CHANGELOG_END

--- daml-assistant/daml-sdk/src/main/scala/com/digitalasset/daml/sdk/SdkMain.scala
+++ daml-assistant/daml-sdk/src/main/scala/com/digitalasset/daml/sdk/SdkMain.scala
@@ -2,14 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package com.digitalasset.daml.sdk
-import com.digitalasset.daml.lf.engine.trigger.{RunnerMain => Trigger}
-import com.digitalasset.daml.lf.engine.script.{RunnerMain => Script}
-import com.digitalasset.daml.lf.engine.script.{TestMain => TestScript}
+
 import com.digitalasset.codegen.{CodegenMain => Codegen}
+import com.digitalasset.daml.lf.engine.script.{RunnerMain => Script, TestMain => TestScript}
+import com.digitalasset.daml.lf.engine.trigger.{RunnerMain => Trigger}
 import com.digitalasset.extractor.{Main => Extractor}
 import com.digitalasset.http.{Main => JsonApi}
 import com.digitalasset.navigator.{NavigatorBackend => Navigator}
 import com.digitalasset.platform.sandbox.{SandboxMain => Sandbox}
+import com.digitalasset.platform.sandboxnext.{Main => SandboxNext}
 
 object SdkMain {
   def main(args: Array[String]): Unit = {
@@ -24,6 +25,7 @@ object SdkMain {
       case "json-api" => JsonApi.main(rest)
       case "navigator" => Navigator.main(rest)
       case "sandbox" => Sandbox.main(rest)
+      case "sandbox-next" => SandboxNext.main(rest)
       case _ => sys.exit(1)
     }
   }
