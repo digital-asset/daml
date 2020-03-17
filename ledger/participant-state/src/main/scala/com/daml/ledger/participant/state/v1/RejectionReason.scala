89832a788482cb4316af912ba73c5abcbae963aa
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2020-03-17 13:05:24 +0100

    Improve wording

--- ledger/participant-state/src/main/scala/com/daml/ledger/participant/state/v1/RejectionReason.scala
+++ ledger/participant-state/src/main/scala/com/daml/ledger/participant/state/v1/RejectionReason.scala
@@ -76,11 +76,11 @@ object RejectionReason {
 
   /** The ledger time of the transaction submission violated one of the
     *  following constraints on ledger time:
-    *  - The difference between ledger time and the record time
-    *    in the ledger state at which the transaction was sequenced needs
-    *    to stay within bounds defined by the ledger.
-    *  - The ledger time has to be greater than or equal to the ledger time
-    *    of any contract referenced by the transaction.
+    *  - The difference between the ledger time and the record time
+    *    in the ledger state at which the transaction was sequenced must
+    *    stay within bounds defined by the ledger.
+    *  - The ledger time of the transaction must be greater than or equal
+    *    to the ledger time of any contract used by the transaction.
     */
   final case class InvalidLedgerTime(reason: String) extends RejectionReason {
     override def description: String = "Invalid ledger time: " + reason
