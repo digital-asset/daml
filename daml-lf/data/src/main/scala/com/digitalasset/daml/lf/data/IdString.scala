5d30263ca4c8a77cb239f485d446a13119984e4a
Stephen Compall <stephen.compall@daml.com>
2020-03-24 14:06:17 -0400

    remove stray import

--- daml-lf/data/src/main/scala/com/digitalasset/daml/lf/data/IdString.scala
+++ daml-lf/data/src/main/scala/com/digitalasset/daml/lf/data/IdString.scala
@@ -108,7 +108,7 @@ sealed abstract class IdString {
 }
 
 object IdString {
-  import Ref.{Name, Party}
+  import Ref.Name
   @annotation.compileTimeOnly("SC test TODO")
   implicit def `Name equal instance`: Equal[Name] = Name.equalInstance
 }
