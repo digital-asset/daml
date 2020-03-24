778eb01a2df242fafe67be074a97d7789aa7287c
Stephen Compall <stephen.compall@daml.com>
2020-03-24 17:05:06 -0400

    scalafmt

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/WebsocketEndpoints.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/WebsocketEndpoints.scala
@@ -84,7 +84,8 @@ class WebsocketEndpoints(
           payload <- preconnect(decodeJwt, upgradeReq, wsProtocol)
           (jwt, jwtPayload) = payload
         } yield
-          handleWebsocketRequest[NonEmptyList[domain.ContractKeyStreamRequest[None.type, domain.LfValue]]](
+          handleWebsocketRequest[
+            NonEmptyList[domain.ContractKeyStreamRequest[None.type, domain.LfValue]]](
             jwt,
             jwtPayload,
             upgradeReq,
