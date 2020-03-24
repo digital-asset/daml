00ad01cc40a0bfaf528fae5391280acb224f6176
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2020-03-24 01:19:30 +0100

    Improve test

--- ledger/sandbox/src/test/lib/scala/com/digitalasset/platform/store/dao/JdbcLedgerDaoCompletionsSpec.scala
+++ ledger/sandbox/src/test/lib/scala/com/digitalasset/platform/store/dao/JdbcLedgerDaoCompletionsSpec.scala
@@ -104,6 +104,7 @@ private[dao] trait JdbcLedgerDaoCompletionsSpec extends OptionValues with LoneEl
       RejectionReason.PartyNotKnownOnLedger(""),
       RejectionReason.SubmitterCannotActViaParticipant(""),
       RejectionReason.TimedOut(""),
+      RejectionReason.InvalidLedgerTime(""),
     )
 
     for {
