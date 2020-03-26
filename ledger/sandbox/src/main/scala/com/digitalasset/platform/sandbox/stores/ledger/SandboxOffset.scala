efb32fe6cea2bb533570567fbdb8402112f138dc
Gerolf Seitz <gerolf.seitz@digitalasset.com>
2020-03-26 10:57:42 +0100

    Add comments to ApiOffset and SandboxOffset
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/SandboxOffset.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/stores/ledger/SandboxOffset.scala
@@ -5,6 +5,10 @@ package com.digitalasset.platform.sandbox.stores.ledger
 
 import com.daml.ledger.participant.state.v1.Offset
 
+// Sandbox classic simply uses integral numbers starting from 1 for offsets.
+// This utility object makes sure that the offset representation is always
+// padded with zeros up to 8 bytes.
+// This should only be used in SqlLedger and InMemoryLedger.
 object SandboxOffset {
 
   def numBytes = 8
