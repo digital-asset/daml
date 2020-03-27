4ccf08ab1b371a3f0ef6c3fa251d83ea2b0c6348
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-03-27 15:40:50 -0400

    Remove scripts

--- templates/create-daml-app/scripts/daml-start-backend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-daml sandbox -w --ledgerid create-daml-app-sandbox &
-sleep 5
-daml json-api --ledger-host localhost --ledger-port 6865 --http-port 7575 --application-id create-daml-app-sandbox
--- templates/create-daml-app/scripts/restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while [ 1 ] ; do
-  $1 &
-  PID=$!
-  fswatch --one-event --event Updated $2
-  kill $PID
-  sleep 1 # give it a window to receive SIGINT
-done
