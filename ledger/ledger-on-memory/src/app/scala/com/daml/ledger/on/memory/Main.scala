ce62bfb596cd3f2bea9686df45dda80dfa1aed84
mziolekda <marcin.ziolek@digitalasset.com>
2020-03-27 14:00:24 +0100

    Support for large grpc messages in ledger-on-memory (#5230)
    * Support for large grpc messages in ledger-on-memory
    
    * adress review comments
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/ledger-on-memory/src/app/scala/com/daml/ledger/on/memory/Main.scala
+++ ledger/ledger-on-memory/src/app/scala/com/daml/ledger/on/memory/Main.scala
@@ -5,11 +5,18 @@ package com.daml.ledger.on.memory
 
 import akka.stream.Materializer
 import com.daml.ledger.on.memory.InMemoryLedgerReaderWriter.Index
-import com.daml.ledger.participant.state.kvutils.app.LedgerFactory.KeyValueLedgerFactory
-import com.daml.ledger.participant.state.kvutils.app.{Config, ParticipantConfig, Runner}
+import com.daml.ledger.participant.state.kvutils.api.KeyValueParticipantState
+import com.daml.ledger.participant.state.kvutils.app.{
+  Config,
+  LedgerFactory,
+  ParticipantConfig,
+  Runner
+}
 import com.digitalasset.logging.LoggingContext
 import com.digitalasset.platform.akkastreams.dispatcher.Dispatcher
+import com.digitalasset.platform.apiserver.ApiServerConfig
 import com.digitalasset.resources.{ProgramResource, ResourceOwner}
+import scopt.OptionParser
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -24,9 +31,24 @@ object Main {
   }
 
   class InMemoryLedgerFactory(dispatcher: Dispatcher[Index], state: InMemoryState)
-      extends KeyValueLedgerFactory[InMemoryLedgerReaderWriter] {
+      extends LedgerFactory[KeyValueParticipantState, ExtraConfig] {
 
-    override def owner(config: Config[Unit], participantConfig: ParticipantConfig)(
+    override final def readWriteServiceOwner(
+        config: Config[ExtraConfig],
+        participantConfig: ParticipantConfig,
+    )(
+        implicit materializer: Materializer,
+        logCtx: LoggingContext,
+    ): ResourceOwner[KeyValueParticipantState] =
+      for {
+        readerWriter <- owner(config, participantConfig)
+      } yield
+        new KeyValueParticipantState(
+          readerWriter,
+          readerWriter,
+          metricRegistry(participantConfig, config))
+
+    def owner(config: Config[ExtraConfig], participantConfig: ParticipantConfig)(
         implicit materializer: Materializer,
         logCtx: LoggingContext,
     ): ResourceOwner[InMemoryLedgerReaderWriter] =
@@ -37,5 +59,26 @@ object Main {
         dispatcher = dispatcher,
         state = state,
       )
+
+    override val defaultExtraConfig: ExtraConfig = ExtraConfig.default
+
+    override final def extraConfigParser(parser: OptionParser[Config[ExtraConfig]]): Unit = {
+      parser
+        .opt[Int]("maxInboundMessageSize")
+        .text(
+          s"Max inbound message size in bytes. Defaults to ${ExtraConfig.defaultMaxInboundMessageSize}.")
+        .action((maxInboundMessageSize, config) => {
+          val extra = config.extra
+          config.copy(extra = extra.copy(maxInboundMessageSize = maxInboundMessageSize))
+        })
+      ()
+    }
+
+    override def apiServerConfig(
+        participantConfig: ParticipantConfig,
+        config: Config[ExtraConfig]): ApiServerConfig =
+      super
+        .apiServerConfig(participantConfig, config)
+        .copy(maxInboundMessageSize = config.extra.maxInboundMessageSize)
   }
 }
