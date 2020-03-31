aeefaa1d0c73076f13d5c5a8dac6242ccef819b8
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2020-03-31 14:13:14 +0200

    Add queries to get flat transactions straight from the index (#5285)
    * Add queries to get flat transactions straight from the index
    
    changelog_begin
    changelog_end
    
    * Fix compilation errors
    
    * Remove unused method, loosen transaction check
    
    * Fix grammar in test case description
    
    Thanks @SamirTalwar-DA
    
    Co-Authored-By: Samir Talwar <samir.talwar@digitalasset.com>
    
    Co-authored-by: Samir Talwar <samir.talwar@digitalasset.com>

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/TransactionsReader.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/TransactionsReader.scala
@@ -3,23 +3,58 @@
 
 package com.digitalasset.platform.store.dao.events
 
-import com.daml.ledger.participant.state.v1.TransactionId
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+import com.daml.ledger.participant.state.v1.{Offset, TransactionId}
+import com.digitalasset.daml.lf.data.Ref.Identifier
 import com.digitalasset.ledger.api.v1.transaction_service.{
   GetFlatTransactionResponse,
   GetTransactionResponse,
+  GetTransactionsResponse
 }
-import com.digitalasset.platform.store.DbType
-import com.digitalasset.platform.store.dao.DbDispatcher
+import com.digitalasset.platform.ApiOffset
+import com.digitalasset.platform.store.dao.{DbDispatcher, PaginatingAsyncStream}
+import com.digitalasset.platform.store.SimpleSqlAsVectorOf.SimpleSqlAsVectorOf
 
 import scala.concurrent.{ExecutionContext, Future}
 
 private[dao] object TransactionsReader {
 
-  def apply(
-      dispatcher: DbDispatcher,
-      dbType: DbType,
-      executionContext: ExecutionContext): TransactionsReader =
+  private def offsetFor(response: GetTransactionsResponse): Offset =
+    ApiOffset.assertFromString(response.transactions.head.offset)
+
+  def apply(dispatcher: DbDispatcher, executionContext: ExecutionContext): TransactionsReader =
     new TransactionsReader {
+
+      override def getFlatTransactions(
+          startExclusive: Offset,
+          endInclusive: Offset,
+          filter: FilterRelation,
+          pageSize: Int,
+          verbose: Boolean,
+      ): Source[(Offset, GetTransactionsResponse), NotUsed] = {
+        val events =
+          PaginatingAsyncStream(pageSize) { offset =>
+            val query =
+              EventsTable.preparePagedGetFlatTransactions(
+                startExclusive = startExclusive,
+                endInclusive = endInclusive,
+                filter = filter,
+                pageSize = pageSize,
+                rowOffset = offset,
+              )
+            dispatcher.executeSql("get_flat_transactions") { implicit connection =>
+              query.asVectorOf(EventsTable.flatEventParser(verbose = verbose))
+            }
+          }
+
+        groupContiguous(events)(by = _.transactionId)
+          .flatMapConcat { events =>
+            val response = EventsTable.Entry.toGetTransactionsResponse(events)
+            Source(response.map(r => offsetFor(r) -> r))
+          }
+      }
+
       override def lookupFlatTransactionById(
           transactionId: TransactionId,
           requestingParties: Set[Party],
@@ -54,6 +89,14 @@ private[dao] object TransactionsReader {
 }
 
 private[dao] trait TransactionsReader {
+
+  def getFlatTransactions(
+      startExclusive: Offset,
+      endInclusive: Offset,
+      filter: Map[Party, Set[Identifier]],
+      pageSize: Int,
+      verbose: Boolean,
+  ): Source[(Offset, GetTransactionsResponse), NotUsed]
 
   def lookupFlatTransactionById(
       transactionId: TransactionId,
