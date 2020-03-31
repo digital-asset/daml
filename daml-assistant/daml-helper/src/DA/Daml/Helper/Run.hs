92209ce46a7fe6c32b2d8f61c19072c9e407ea20
associahedron <231829+associahedron@users.noreply.github.com>
2020-03-31 11:00:16 +0100

    Make new sandbox the default.
    changelog_begin
    
    - [DAML SDK] The new sandbox is now the default that runs with ``daml sandbox`` and ``daml start``. The command ``daml sandbox-next`` has been removed. The old sandbox can be invoked via ``daml sandbox-classic`` and ``daml start --sandbox-classic=yes``.
    
    changelog_end

--- daml-assistant/daml-helper/src/DA/Daml/Helper/Run.hs
+++ daml-assistant/daml-helper/src/DA/Daml/Helper/Run.hs
@@ -44,6 +44,7 @@ module DA.Daml.Helper.Run
     , NavigatorOptions(..)
     , JsonApiOptions(..)
     , ScriptOptions(..)
+    , SandboxClassic(..)
     ) where
 
 import Control.Concurrent
@@ -741,10 +742,11 @@ navigatorPortNavigatorArgs (NavigatorPort p) = ["--port", show p]
 navigatorURL :: NavigatorPort -> String
 navigatorURL (NavigatorPort p) = "http://localhost:" <> show p
 
-withSandbox :: SandboxPort -> [String] -> (Process () () () -> IO a) -> IO a
-withSandbox (SandboxPort port) args a = withTempFile $ \portFile -> do
+withSandbox :: SandboxClassic -> SandboxPort -> [String] -> (Process () () () -> IO a) -> IO a
+withSandbox (SandboxClassic classic) (SandboxPort port) args a = withTempFile $ \portFile -> do
     logbackArg <- getLogbackArg (damlSdkJarFolder </> "sandbox-logback.xml")
-    withJar damlSdkJar [logbackArg] (["sandbox", "--port", show port, "--port-file", portFile] ++ args) $ \ph -> do
+    let sandbox = if classic then "sandbox-classic" else "sandbox"
+    withJar damlSdkJar [logbackArg] ([sandbox, "--port", show port, "--port-file", portFile] ++ args) $ \ph -> do
         putStrLn "Waiting for sandbox to start: "
         _port <- readPortFile maxRetries portFile
         a ph
@@ -807,6 +809,7 @@ newtype SandboxOptions = SandboxOptions [String]
 newtype NavigatorOptions = NavigatorOptions [String]
 newtype JsonApiOptions = JsonApiOptions [String]
 newtype ScriptOptions = ScriptOptions [String]
+newtype SandboxClassic = SandboxClassic { unSandboxClassic :: Bool }
 
 withOptsFromProjectConfig :: T.Text -> [String] -> ProjectConfig -> IO [String]
 withOptsFromProjectConfig fieldName cliOpts projectConfig = do
@@ -828,6 +831,7 @@ runStart
     -> NavigatorOptions
     -> JsonApiOptions
     -> ScriptOptions
+    -> SandboxClassic
     -> IO ()
 runStart
   sandboxPortM
@@ -840,6 +844,7 @@ runStart
   (NavigatorOptions navigatorOpts)
   (JsonApiOptions jsonApiOpts)
   (ScriptOptions scriptOpts)
+  sandboxClassic
   = withProjectRoot Nothing (ProjectCheck "daml start" True) $ \_ _ -> do
     let sandboxPort = fromMaybe defaultSandboxPort sandboxPortM
     projectConfig <- getProjectConfig
@@ -863,7 +868,7 @@ runStart
     scriptOpts <- withOptsFromProjectConfig "script-options" scriptOpts projectConfig
     doBuild
     let scenarioArgs = maybe [] (\scenario -> ["--scenario", scenario]) mbScenario
-    withSandbox sandboxPort (darPath : scenarioArgs ++ sandboxOpts) $ \sandboxPh -> do
+    withSandbox sandboxClassic sandboxPort (darPath : scenarioArgs ++ sandboxOpts) $ \sandboxPh -> do
         withNavigator' shouldStartNavigator sandboxPh sandboxPort navigatorPort navigatorOpts $ \navigatorPh -> do
             whenJust mbInitScript $ \initScript -> do
                 procScript <- toAssistantCommand $
