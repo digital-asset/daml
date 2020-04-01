f60c530a63ea05a20e881d381f29ed5ad72da7b9
Stefano Baghino <stefano.baghino@digitalasset.com>
2020-04-01 18:24:49 +0200

    Revert "Reduce memory necessary to run the zipbomb detection test (#5345)"
    This reverts commit bc8c8bfd5d4b1362f6d9fecb6ebbb1b4bf87c2b6.

--- daml-lf/archive/src/test/scala/com/digitalasset/daml/lf/archive/ZipBombDetectionSpec.scala
+++ daml-lf/archive/src/test/scala/com/digitalasset/daml/lf/archive/ZipBombDetectionSpec.scala
@@ -12,19 +12,14 @@ import org.scalatest.{FlatSpec, Matchers, TryValues}
 final class ZipBombDetectionSpec extends FlatSpec with Matchers with TryValues {
 
   private def bomb: ZipInputStream =
-    new ZipInputStream(
-      new FileInputStream(BazelRunfiles.rlocation("daml-lf/archive/DarReaderTest.dar"))
-    )
+    new ZipInputStream(new FileInputStream(BazelRunfiles.rlocation("libs_python/zblg.zip")))
 
   "DarReader" should "reject a zip bomb with the proper error" in {
-    DarReader()
-      .readArchive("t", bomb, entrySizeThreshold = 1024)
-      .failure
-      .exception shouldBe a[Errors.ZipBomb]
+    DarReader().readArchive("t", bomb).failure.exception shouldBe a[Errors.ZipBomb]
   }
 
   "UniversalArchiveReader" should "reject a zip bomb with the proper error" in {
-    UniversalArchiveReader(entrySizeThreshold = 1024)
+    UniversalArchiveReader()
       .readDarStream("t", bomb)
       .failure
       .exception shouldBe a[Errors.ZipBomb]
