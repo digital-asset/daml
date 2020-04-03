ed87100f7199e36c42eb70797c4e2ca68561f4ba
associahedron <231829+associahedron@users.noreply.github.com>
2020-04-03 10:50:13 +0100

    Fix comment

--- daml-assistant/src/DA/Daml/Assistant/Version.hs
+++ daml-assistant/src/DA/Daml/Assistant/Version.hs
@@ -132,7 +132,7 @@ getAvailableSdkVersions = wrapErr "Fetching list of available SDK versions" $ do
 
     pure . sort $ mapMaybe (eitherToMaybe . parseVersion) (M.keys versionsMap)
 
--- | Get the list of available versions afresh. This will fetch.
+-- | Get the list of available snapshot versions. This will fetch.
 -- https://docs.daml.com/snapshots.json and parse the obtained list
 -- of versions.
 getAvailableSdkSnapshotVersions :: IO [SdkVersion]
