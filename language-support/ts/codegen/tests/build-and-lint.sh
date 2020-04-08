a469d9471b16bed81df3ff51709efcb1154cf7ea
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-04-08 13:44:05 +0200

    daml2js: Write all dependencies into package.json (#5490)
    * daml2js: Write all dependencies into package.json
    
    The dependencies of the generated packages on `@daml/types` and
    `@mojotech/json-type-validation` somehow got lost either during a
    refactoring or when changing how we depend on other generated packages.
    
    The tests simply running `daml2ts` did not catch this because we write
    a temporary `package.json` one level higher up in the directory tree
    in order to `yarn install` the dependencies only once.
    
    The tests using the generated packages also did not catch this since
    they depend on `@daml/types`, which in turn depends on
    `@mojotech/json-type-validation`, themselves and hence had both packages
    in scope.
    
    In other words, this is very tricky to test. We try to alleviate this
    problem by sharing the code for generating the dependencies between the
    function writing the actual `package.json` files and the one writing
    the temporary `package.json`. This makes sure that these dependencies
    do not get out of sync again.
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Adjust tests
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- language-support/ts/codegen/tests/build-and-lint.sh
+++ language-support/ts/codegen/tests/build-and-lint.sh
@@ -60,16 +60,10 @@ cd $TMP_DIR
 
 # Call daml2js.
 PATH=`dirname $YARN`:$PATH $DAML2TS -o daml2js $DAR
-# We only needed this package.json in order to provide resolution of
-# daml-types to daml2js and that's done with now.
-rm package.json
-# Since we eschew using workspaces and resolutions in this test, patch
-# up the daml-ledger dependency on daml-types.
-sed -i"" "s|\"@daml/types\": \"$SDK_VERSION\"|\"@daml/types\": \"file:../daml-types\"|" daml-ledger/package.json
 
 # Build, lint, test.
 cd build-and-lint-test
-$YARN install --pure-lockfile > /dev/null 2>&1
+$YARN install --pure-lockfile > /dev/null
 $YARN run build && $YARN run lint
 # Invoke 'yarn test'. Control is thereby passed to
 # 'language-support/ts/codegen/tests/ts/build-and-lint-test/src/__tests__/test.ts'.
