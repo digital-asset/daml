f6ff133c66fbf504a6efe69368d3d5f83231359d
Stephen Compall <stephen.compall@daml.com>
2020-04-09 15:05:29 -0400

    factor genAddend and genAddendNoListMap

--- daml-lf/transaction-scalacheck/src/main/scala/com/digitalasset/daml/lf/value/TypedValueGenerators.scala
+++ daml-lf/transaction-scalacheck/src/main/scala/com/digitalasset/daml/lf/value/TypedValueGenerators.scala
@@ -484,6 +484,29 @@ object TypedValueGenerators {
     def leafInstances: Seq[F[_]] = Seq(text, int64, unit, date, timestamp, bool, party)
   }
 
+  /** Given a function that produces one-level-nested cases, produce a
+    * ValueAddend gen.
+    */
+  @SuppressWarnings(Array("org.wartremover.warts.Any"))
+  def indGenAddend(f: Gen[ValueAddend] => Seq[Gen[ValueAddend]]): Gen[ValueAddend] = {
+    object Knot {
+      val tie: Gen[ValueAddend] = Gen.sized { sz =>
+        val self = Gen.resize(sz / 2, Gen.lzy(tie))
+        val nestSize = sz / 3
+        Gen.frequency(
+          Seq(
+            ((sz max 1) * ValueAddend.leafInstances.length, Gen.oneOf(ValueAddend.leafInstances)),
+            (sz max 1, Gen.const(ValueAddend.contractId)),
+            (sz max 1, Gen.oneOf(Numeric.Scale.values).map(ValueAddend.numeric)),
+            (nestSize, self.map(ValueAddend.optional(_)))
+          ) ++
+            f(self).map((nestSize, _)): _*
+        )
+      }
+    }
+    Knot.tie
+  }
+
   /** This is the key member of interest, supporting many patterns:
     *
     *  1. generating a type and value
@@ -494,31 +517,16 @@ object TypedValueGenerators {
     * prism into [[Value]], a [[Type]] describing that type, and
     * Scalacheck support surrounding that type.''
     */
-  @SuppressWarnings(Array("org.wartremover.warts.Any"))
-  val genAddend: Gen[ValueAddend] = Gen.sized { sz =>
-    val self = Gen.resize(sz / 2, Gen.lzy(genAddend))
-    val nestSize = sz / 3
-    Gen.frequency(
-      ((sz max 1) * ValueAddend.leafInstances.length, Gen.oneOf(ValueAddend.leafInstances)),
-      (sz max 1, Gen.const(ValueAddend.contractId)),
-      (sz max 1, Gen.oneOf(Numeric.Scale.values).map(ValueAddend.numeric)),
-      (nestSize, self.map(ValueAddend.list(_))),
-      (nestSize, self.map(ValueAddend.optional(_))),
-      (nestSize, self.map(ValueAddend.map(_))),
-    )
-  }
+  val genAddend: Gen[ValueAddend] =
+    indGenAddend(
+      self =>
+        Seq(
+          self.map(ValueAddend.list(_)),
+          self.map(ValueAddend.map(_)),
+          Gen.zip(self, self).map { case (k, v) => ValueAddend.genMap(k, v) },
+      ))
 
-  @SuppressWarnings(Array("org.wartremover.warts.Any"))
-  val genAddendNoListMap: Gen[ValueAddend] = Gen.sized { sz =>
-    val self = Gen.resize(sz / 2, Gen.lzy(genAddendNoListMap))
-    val nestSize = sz / 3
-    Gen.frequency(
-      ((sz max 1) * ValueAddend.leafInstances.length, Gen.oneOf(ValueAddend.leafInstances)),
-      (sz max 1, Gen.const(ValueAddend.contractId)),
-      (sz max 1, Gen.oneOf(Numeric.Scale.values).map(ValueAddend.numeric)),
-      (nestSize, self.map(ValueAddend.optional(_))),
-    )
-  }
+  val genAddendNoListMap: Gen[ValueAddend] = indGenAddend(_ => Seq.empty)
 
   /** Generate a type and value guaranteed to conform to that type. */
   def genTypeAndValue[Cid: Order](cid: Gen[Cid]): Gen[(Type, Value[Cid])] =
