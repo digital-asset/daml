14308cd431bbcdf1d44d7863aefac1bc633e3ca9
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-04-10 09:01:50 -0400

    Add patch to dev_env and reference it from integration tests

--- daml-assistant/integration-tests/src/DA/Daml/Assistant/IntegrationTests.hs
+++ daml-assistant/integration-tests/src/DA/Daml/Assistant/IntegrationTests.hs
@@ -556,7 +556,7 @@ createDamlAppTests = testGroup "create-daml-app" [gettingStartedGuideTest | not 
           callCommandSilent "daml build"
           setupYarnEnv tmpDir (Workspaces ["create-daml-app/daml.js"]) [DamlTypes]
           callCommandSilent "daml codegen js -o daml.js .daml/dist/create-daml-app-0.1.0.dar"
-        assertFileDoesNotExist (cdaDir </> "ui" </> "build" </> "index.html")
+        -- assertFileDoesNotExist (cdaDir </> "ui" </> "build" </> "index.html")
         withCurrentDirectory (cdaDir </> "ui") $ do
           -- NOTE(MH): We set up the yarn env again to avoid having all the
           -- dependencies of the UI already in scope when `daml2js` runs
@@ -571,8 +571,9 @@ createDamlAppTests = testGroup "create-daml-app" [gettingStartedGuideTest | not 
         -- Now test that the messaging feature works by applying the necessary
         -- changes and testing in the same way as above.
         messagingPatch <- locateRunfiles (mainWorkspace </> "templates" </> "messaging.patch")
+        patchTool <- locateRunfiles "patch_dev_env/bin/patch"
         withCurrentDirectory cdaDir $ do
-          callCommandSilent $ "patch -s -p2 < " ++ messagingPatch
+          callCommandSilent $ unwords [patchTool, "-s", "-p2", "<", messagingPatch]
           forM_ ["MessageEdit", "MessageList"] $ \messageComponent ->
             assertFileExists ("ui" </> "src" </> "components" </> messageComponent <.> "tsx")
           callCommandSilent "daml build"
