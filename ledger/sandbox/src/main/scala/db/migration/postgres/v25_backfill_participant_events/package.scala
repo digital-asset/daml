000bb046db1721de768910fb8cd35638854a61ab
Gerolf Seitz <gerolf.seitz@daml.com>
2020-04-11 11:07:48 +0200

    Sadbonx: Add migration that backfills the participant_events table (#5528)
    We started serving the LedgerAPI from the new schema in #5104, #5305, #5346,
    and #5397.
    
    We must therefore also add a migration that moves the data from
    ledger_entries to the new tables.
    
    To keep the migration relatively stable with respect to refactoring the
    code, I copied a bunch of files to
    `db.migration.postgres.v25_backfill_participant_events`.
    
    As a bonus, I added a line to logback.xml so that we get the usual
    sandbox output on startup.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

---
+++ ledger/sandbox/src/main/scala/db/migration/postgres/v25_backfill_participant_events/package.scala
@@ -0,0 +1,29 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package db.migration.postgres
+
+// Copied here to make it safe against future refactoring
+// in production code
+/**
+  * Type aliases used throughout the package
+  */
+package object v25_backfill_participant_events {
+
+  import com.daml.lf.value.{Value => lfval}
+  type ContractId = lfval.AbsoluteContractId
+
+  import com.daml.lf.{transaction => lftx}
+  type NodeId = lftx.Transaction.NodeId
+  type Transaction = lftx.GenTransaction.WithTxValue[NodeId, ContractId]
+  type Create = lftx.Node.NodeCreate.WithTxValue[ContractId]
+  type Exercise = lftx.Node.NodeExercises.WithTxValue[NodeId, ContractId]
+
+  import com.daml.lf.{data => lfdata}
+  type Party = lfdata.Ref.Party
+  type Identifier = lfdata.Ref.Identifier
+  type LedgerString = lfdata.Ref.LedgerString
+  type WitnessRelation[A] = lfdata.Relation.Relation[A, Party]
+  type DisclosureRelation = WitnessRelation[NodeId]
+  val Relation = lfdata.Relation.Relation
+}
