e673bf4dd6969d0814cf5256f2201310da4e1c91
Samir Talwar <samir.talwar@digitalasset.com>
2020-04-14 14:31:55 +0200

    kvutils: Write a test that times envelope compression and decompression.

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/EnvelopeSpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/EnvelopeSpec.scala
@@ -3,7 +3,11 @@
 
 package com.daml.ledger.participant.state.kvutils
 
+import java.security.MessageDigest
+
+import com.daml.daml_lf_dev.DamlLf
 import com.daml.ledger.participant.state.kvutils.{DamlKvutils => Proto}
+import com.daml.lf.data.Ref.PackageId
 import com.google.protobuf.ByteString
 import org.scalatest.{Matchers, WordSpec}
 
@@ -35,5 +39,36 @@ class EnvelopeSpec extends WordSpec with Matchers {
       Envelope.open(Envelope.enclose(submissionBatch)) shouldEqual
         Right(Envelope.SubmissionBatchMessage(submissionBatch))
     }
+
+    "compresses and decompresses quickly" in {
+      val payload = ByteString.copyFrom(Array.fill[Byte](100 * 1024 * 1024)(0))
+      val hash = PackageId.assertFromString(
+        MessageDigest
+          .getInstance("SHA-256")
+          .digest(payload.toByteArray)
+          .map("%02x".format(_))
+          .mkString)
+      val stateValue = Proto.DamlStateValue
+        .newBuilder()
+        .setArchive(
+          DamlLf.Archive
+            .newBuilder()
+            .setHashFunction(DamlLf.HashFunction.SHA256)
+            .setPayload(payload)
+            .setHash(hash))
+        .build()
+
+      val envelope = timeAndLog("enclose", Envelope.enclose(stateValue, compression = true))
+      val opened = timeAndLog("open", Envelope.open(envelope))
+      opened shouldEqual Right(Envelope.StateValueMessage(stateValue))
+    }
+  }
+
+  def timeAndLog[T](name: String, f: => T): T = {
+    val start = System.nanoTime()
+    val value = f
+    val end = System.nanoTime()
+    println(s"$name: ${(end - start) / 1000000}ms")
+    value
   }
 }
