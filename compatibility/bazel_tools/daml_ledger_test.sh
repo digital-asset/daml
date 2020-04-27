8d46f3e4873930f88c9df138876278735ba3c9a4
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-04-27 17:50:32 +0200

    ./fmt.sh

--- compatibility/bazel_tools/daml_ledger_test.sh
+++ compatibility/bazel_tools/daml_ledger_test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 # Copy-pasted from the Bazel Bash runfiles library v2.
 set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
 source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
