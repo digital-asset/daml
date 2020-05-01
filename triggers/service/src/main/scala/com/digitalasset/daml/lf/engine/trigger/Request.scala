e28aa70b06d433f265c12fa31aa1e0da1094cdd4
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-05-01 11:47:10 -0400

    Trigger service: Use tagged Party type instead of String
    changelog_begin
    changelog_end

--- triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/Request.scala
+++ triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/Request.scala
@@ -6,6 +6,8 @@ package com.daml.lf.engine.trigger
 import com.daml.lf.data.Ref.{DottedName, Identifier, PackageId, QualifiedName}
 import spray.json.DefaultJsonProtocol._
 import spray.json.{JsString, JsValue, JsonFormat, deserializationError}
+import com.daml.ledger.api.refinements.ApiTypes.{Party}
+import scalaz.syntax.tag._
 
 object Request {
   implicit object IdentifierFormat extends JsonFormat[Identifier] {
@@ -28,12 +30,21 @@ object Request {
       }
       case _ => deserializationError("Expected trigger identifier of the form pkgid:mod:name")
     }
-    def write(id: Identifier) = JsString(s"${id.packageId}:${id.qualifiedName}")
+    def write(id: Identifier) = JsString(id.toString)
   }
 
-  case class StartParams(identifier: Identifier, party: String)
+  implicit object PartyFormat extends JsonFormat[Party] {
+    def read(value: JsValue) = value match {
+      case JsString(s) => Party(s)
+      case _ => deserializationError("Expected Party string")
+    }
+
+    def write(party: Party) = JsString(party.unwrap)
+  }
+
+  case class StartParams(identifier: Identifier, party: Party)
   implicit val startParamsFormat = jsonFormat2(StartParams)
 
-  case class ListParams(party: String)
+  case class ListParams(party: Party)
   implicit val listParamsFormat = jsonFormat1(ListParams)
 }
