2391e54a4ec5552f413e5ab6cbfa3f147c77260d
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-05-07 16:16:12 +0200

    try to build bazel in the step
    changelog_begin
    changelog_end

---
+++ build-bazel.ps1
@@ -0,0 +1,9 @@
+cd ..
+.\daml\dev-env\windows\bin\dadew.ps1 install
+Set-StrictMode -Version latest
+.\daml\dev-env\windows\bin\dadew.ps1 sync
+.\daml\dev-env\windows\bin\dadew.ps1 enable
+curl -L https://github.com/bazelbuild/bazel/releases/download/2.0.1/bazel-2.0.1-windows-x86_64.exe -o bazel-bootstrap.exe
+git clone https://github.com/cocreature/bazel.git -b more-debug-output
+cd bazel
+../\azel-bootstrap.exe build //scripts/packages:zip-bazel-exe -c opt --stamp --embed_label 2.0.
