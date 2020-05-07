28a7068b2b008e4c41c67da45016273b979452b6
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-05-07 16:31:43 +0200

    .
    changelog_begin
    changelog_end

--- build-bazel.ps1
+++ build-bazel.ps1
@@ -1,10 +1,9 @@
-pwd
 cd ..
 ls
-.\daml\dev-env\windows\bin\dadew.ps1 install
+.\a\dev-env\windows\bin\dadew.ps1 install
 Set-StrictMode -Version latest
-.\daml\dev-env\windows\bin\dadew.ps1 sync
-.\daml\dev-env\windows\bin\dadew.ps1 enable
+.\a\dev-env\windows\bin\dadew.ps1 sync
+.\a\dev-env\windows\bin\dadew.ps1 enable
 curl -L https://github.com/bazelbuild/bazel/releases/download/2.0.1/bazel-2.0.1-windows-x86_64.exe -o bazel-bootstrap.exe
 git clone https://github.com/cocreature/bazel.git -b more-debug-output
 cd bazel
