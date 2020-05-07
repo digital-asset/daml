935c9c470ab3122a2694605e60d17fc87fa4ffd6
Stephen Compall <stephen.compall@daml.com>
2020-05-07 13:59:48 -0400

    add error locations and new `drain` primitive

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/WebsocketServiceIntegrationTest.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/WebsocketServiceIntegrationTest.scala
@@ -12,6 +12,7 @@ import com.daml.http.json.{DomainJsonEncoder, SprayJson}
 import com.daml.http.util.TestUtil
 import HttpServiceTestFixture.UseTls
 import com.typesafe.scalalogging.StrictLogging
+import org.scalactic.source
 import org.scalatest._
 import scalaz.{-\/, Functor, \/, \/-, ~>}
 import scalaz.std.option._
@@ -739,8 +740,11 @@ object WebsocketServiceIntegrationTest {
   object Consume {
     import scalaz.Free
     import scalaz.std.scalaFuture._
+
     type FCC[T, V] = Free[Consume[T, ?], V]
-    final case class Listen[-T, +V](f: T => V) extends Consume[T, V]
+    type Description = source.Position
+    final case class Listen[-T, +V](f: T => V, desc: Description) extends Consume[T, V]
+    final case class Drain[S, -T, +V](init: S, next: (S, T) => S, out: S => V) extends Consume[T, V]
     final case class Emit[+V](run: Future[V]) extends Consume[Any, V]
 
     def interpret[T, V](steps: FCC[T, V])(implicit ec: ExecutionContext): Sink[T, Future[V]] =
@@ -751,38 +755,48 @@ object WebsocketServiceIntegrationTest {
           // either the end or the next listen
           def go(steps: FCC[T, V], listened: Boolean): Future[FCC[T, V]] =
             steps.resume fold ({
-              case listen @ Listen(f) =>
+              case listen @ Listen(f, _) =>
                 if (listened) Future successful (Free roll listen) else go(f(t), true)
+              case drain: Drain[s, T, FCC[T, V]] =>
+                Future {
+                  Free.roll(if (listened) drain
+                  else drain.copy(init = drain.next(drain.init, t)))
+                }
               case Emit(run) => run flatMap (go(_, listened))
             }, v =>
               if (listened) Future successful (Free point v)
               else
-                Future.failed(
-                  new IllegalStateException(s"unexpected element $t, script terminated with $v")))
+                Future.failed(new IllegalStateException(
+                  s"unexpected element $t, script already terminated with $v")))
           go(steps, false)
         }
         .mapMaterializedValue(_.flatMap(_.foldMap(Lambda[Consume[T, ?] ~> Future] {
-          case Listen(f) =>
-            Future.failed(
-              new IllegalStateException(s"script terminated early, expected another value"))
+          case Listen(_, desc) =>
+            Future.failed(new IllegalStateException(
+              s"${describe(desc)}: script terminated early, expected another value"))
+          case Drain(init, _, out) => Future(out(init))
           case Emit(run) => run
         })))
 
     implicit def `consume functor`[T](implicit ec: ExecutionContext): Functor[Consume[T, ?]] =
       new Functor[Consume[T, ?]] {
         override def map[A, B](fa: Consume[T, A])(f: A => B): Consume[T, B] = fa match {
-          case Listen(g) => Listen(g andThen f)
+          case Listen(g, desc) => Listen(g andThen f, desc)
+          case Drain(init, next, out) => Drain(init, next, out andThen f)
           case Emit(run) => Emit(run map f)
         }
       }
 
+    private def describe(d: Description) = s"${d.fileName}:${d.lineNumber}"
+
     implicit final class `Consume Ops`[T, V](private val steps: FCC[T, V]) extends AnyVal {
-      def withFilter(p: V => Boolean): Free[Consume[T, ?], V] =
+      def withFilter(p: V => Boolean)(implicit pos: source.Position): Free[Consume[T, ?], V] =
         steps flatMap { v =>
           if (p(v)) Free point v
           else
             Free liftF Emit(
-              Future failed new IllegalStateException(s"script cancelled by match error on $v"))
+              Future failed new IllegalStateException(
+                s"${describe(pos)}: script cancelled by match error on $v"))
         }
     }
 
@@ -796,7 +810,9 @@ object WebsocketServiceIntegrationTest {
     def syntax[T]: Syntax[T] = new Syntax
 
     final class Syntax[T] {
-      def readOne: FCC[T, T] = Free liftF Listen(identity)
+      def readOne(implicit pos: source.Position): FCC[T, T] = Free liftF Listen(identity, pos)
+      def drain: FCC[T, Seq[T]] =
+        Free liftF Drain(Nil, (acc: List[T], t) => t :: acc, (_: List[T]).reverse)
       def liftF[V](run: Future[V]): FCC[T, V] = Free liftF Emit(run)
       def point[V](v: V): FCC[T, V] = Free point v
     }
