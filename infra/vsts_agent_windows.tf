b283f6b2df19ac4561ef7202d7b4538e3d3be3dc
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2020-05-08 18:02:36 +0200

    reinstate changes for Linux and Windows
    After further thought I believe it is better to do macOS separately, and
    will open another PR for that.m

--- infra/vsts_agent_windows.tf
+++ infra/vsts_agent_windows.tf
@@ -128,6 +128,9 @@ net start winrm
 
 echo "== Installing the VSTS agent"
 
+New-Item -ItemType Directory -Path 'C:\agent'
+Set-Content -Path 'C:\agent\.capabilities' -Value 'assignment=default'
+
 $MachineName = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object CSName | ForEach{ $_.CSName }
 choco install azure-pipelines-agent --no-progress --yes --params "'/Token:${local.vsts_token} /Pool:${local.vsts_pool} /Url:https://dev.azure.com/${local.vsts_account}/ /LogonAccount:$Account /LogonPassword:$Password /Work:D:\a /AgentName:$MachineName /Replace'"
 echo OK
