21e9ce530941ba165bf2b6dac73f7abcdb949c29
Remy Haemmerle <Remy.Haemmerle@daml.com>
2020-05-08 19:06:00 +0200

    Address Stephen and Gerolf reviews

--- daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/value/Value.scala
+++ daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/value/Value.scala
@@ -4,6 +4,7 @@
 package com.daml.lf
 package value
 
+import com.daml.lf.crypto.Hash
 import com.daml.lf.data.Ref.{Identifier, Name}
 import com.daml.lf.data._
 import data.ScalazEqual._
@@ -17,8 +18,6 @@ import scalaz.std.tuple._
 import scalaz.syntax.order._
 import scalaz.syntax.semigroup._
 import scalaz.syntax.std.option._
-
-import scala.util.hashing.MurmurHash3
 
 /** Values   */
 sealed abstract class Value[+Cid] extends CidContainer[Value[Cid]] with Product with Serializable {
@@ -362,28 +361,16 @@ object Value extends CidContainer1WithDefaultCidResolver[Value] {
     */
   sealed abstract class ContractId extends Equals
 
-  sealed abstract class AbsoluteContractId extends ContractId with Equals {
+  sealed abstract class AbsoluteContractId extends ContractId with Product with Serializable {
     def coid: String
   }
 
   object AbsoluteContractId {
-    final class V0(val coid: Ref.ContractIdString) extends AbsoluteContractId {
+    final case class V0(coid: Ref.ContractIdString) extends AbsoluteContractId {
       override def toString: String = s"AbsoluteContractId($coid)"
-
-      override def hashCode(): Int = coid.hashCode()
-
-      override def equals(obj: Any): Boolean = obj match {
-        case that: V0 => this.coid == that.coid
-        case _ => false
-      }
-
     }
 
     object V0 {
-      def apply(coid: Ref.ContractIdString): V0 = new V0(coid)
-
-      def unapply(arg: V0): Some[Ref.ContractIdString] = Some(arg.coid)
-
       def fromString(s: String): Either[String, V0] =
         Ref.ContractIdString.fromString(s).map(V0(_))
 
@@ -392,37 +379,16 @@ object Value extends CidContainer1WithDefaultCidResolver[Value] {
       implicit val `V0 Order`: Order[V0] = Order.fromScalaOrdering[String] contramap (_.coid)
     }
 
-    final class V1 private(val discriminator: crypto.Hash, val suffix: Bytes) extends AbsoluteContractId {
+    final case class V1 private (discriminator: crypto.Hash, suffix: Bytes)
+        extends AbsoluteContractId
+        with data.NoCopy {
       lazy val toBytes: Bytes = V1.prefix ++ discriminator.bytes ++ suffix
       lazy val coid: Ref.HexString = toBytes.toHexString
       override def toString: String = s"AbsoluteContractId($coid)"
-
-      // We copy how hashcode evaluation is cached in String
-      // http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/tip/src/share/classes/java/lang/String.java
-      private[this] var _hashCode = 0
-
-      override def hashCode(): Int =
-        if (_hashCode != 0)
-          _hashCode
-        else {
-          val hash = MurmurHash3.mixLast(discriminator.hashCode(), suffix.hashCode())
-          _hashCode = if (hash == 0) 1 else hash
-          _hashCode
-        }
-
-      override def equals(obj: Any): Boolean = obj match {
-        case that: V1 =>
-          this.discriminator == that.discriminator && this.suffix == that.suffix
-        case _ => false
-      }
-
     }
 
     object V1 {
-      def apply(discriminator: crypto.Hash): V1 = new V1(discriminator, Bytes.Empty)
-
-      def unapply(arg: V1): Some[(crypto.Hash, Bytes)] =
-        Some((arg.discriminator, arg.suffix))
+      def apply(discriminator: Hash): V1 = new V1(discriminator, Bytes.Empty)
 
       def build(discriminator: crypto.Hash, suffix: Bytes): Either[String, V1] =
         Either.cond(
