66a2ab879064bc73aad827a58d4dd5ff9b875a49
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-05-18 19:07:37 +0200

    Actually enable batching.

--- ledger/ledger-on-memory/src/test/suite/scala/com/daml/ledger/on/memory/InMemoryBatchedLedgerReaderWriterIntegrationSpec.scala
+++ ledger/ledger-on-memory/src/test/suite/scala/com/daml/ledger/on/memory/InMemoryBatchedLedgerReaderWriterIntegrationSpec.scala
@@ -16,9 +16,25 @@ import com.daml.logging.LoggingContext
 import com.daml.metrics.Metrics
 import com.daml.resources.ResourceOwner
 
+import scala.concurrent.duration._
+
 class InMemoryBatchedLedgerReaderWriterIntegrationSpec
     extends ParticipantStateIntegrationSpecBase(
       "In-memory ledger/participant with parallel validation") {
+  private val enableBatching = true
+  private val batchingLedgerWriterConfig =
+    BatchingLedgerWriterConfig(
+      enableBatching = enableBatching,
+      // In case of serial validation, we need a queue length of 1000 because the
+      // "process many party allocations" test case will be sending in that many requests at once
+      // (otherwise some of those would be rejected).
+      // See [[ParticipantStateIntegrationSpecBase]].
+      maxBatchQueueSize = if (enableBatching) 100 else 1000,
+      maxBatchSizeBytes = 4 * 1024 * 1024 /* 4MB */,
+      maxBatchWaitDuration = 100.millis,
+      // In-memory ledger doesn't support concurrent commits.
+      maxBatchConcurrentCommits = 1
+    )
 
   override val isPersistent: Boolean = false
 
@@ -35,7 +51,7 @@ class InMemoryBatchedLedgerReaderWriterIntegrationSpec
       engine = Engine(),
     ).map { readerWriter =>
       val batchingLedgerWriter = new BatchingLedgerWriter(
-        BatchingQueueFactory.batchingQueueFrom(BatchingLedgerWriterConfig.reasonableDefault),
+        BatchingQueueFactory.batchingQueueFrom(batchingLedgerWriterConfig),
         readerWriter)
       new KeyValueParticipantState(readerWriter, batchingLedgerWriter, metrics)
     }
