124a614014d18cdbbe0fba80cb6414a69f0de170
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-05-19 15:00:42 +0200

    Chain futures for persisting state.

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/LogAppendingCommitStrategy.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/LogAppendingCommitStrategy.scala
@@ -20,22 +20,24 @@ class LogAppendingCommitStrategy[Index](
     keySerializationStrategy: StateKeySerializationStrategy)(
     implicit executionContext: ExecutionContext)
     extends CommitStrategy[Index] {
+  @SuppressWarnings(Array("org.wartremover.warts.Any")) // so we can use `breakOut`
   override def commit(
       participantId: ParticipantId,
       correlationId: String,
       entryId: DamlLogEntryId,
       entry: DamlLogEntry,
       inputState: Map[DamlStateKey, Option[DamlStateValue]],
-      outputState: Map[DamlStateKey, DamlStateValue]): Future[Index] = {
-    ledgerStateOperations.writeState(outputState.map {
-      case (key, value) =>
-        (keySerializationStrategy.serializeStateKey(key), Envelope.enclose(value))
-    }(breakOut))
-
-    ledgerStateOperations
-      .appendToLog(
-        entryId.toByteString,
-        Envelope.enclose(entry)
-      )
-  }
+      outputState: Map[DamlStateKey, DamlStateValue]): Future[Index] =
+    for {
+      serializedKeyValuePairs <- Future.successful(outputState.map {
+        case (key, value) =>
+          (keySerializationStrategy.serializeStateKey(key), Envelope.enclose(value))
+      }(breakOut))
+      _ <- ledgerStateOperations.writeState(serializedKeyValuePairs)
+      index <- ledgerStateOperations
+        .appendToLog(
+          entryId.toByteString,
+          Envelope.enclose(entry)
+        )
+    } yield index
 }
