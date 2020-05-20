83fe341297f5f0a34baa29e609d27218c7e30be6
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-05-20 10:19:20 +0200

    Missing headers.

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/LogAppendingCommitStrategySpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/LogAppendingCommitStrategySpec.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.validator
 
 import org.scalatest.mockito.MockitoSugar
@@ -76,3 +79,4 @@ class LogAppendingCommitStrategySpec extends AsyncWordSpec with Matchers with Mo
 
   private val aStateValue: DamlStateValue = DamlStateValue.getDefaultInstance
 }
+
