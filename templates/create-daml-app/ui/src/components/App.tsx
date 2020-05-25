3bcece95b666b66778d3155e16bb8f7e2f438b75
Phoebe <phoebe.nichols@digitalasset.com>
2020-05-25 14:03:14 +0200

    change create daml app to run against several participants
    CHANGELOG_BEGIN
    CHANGELOG_END

--- templates/create-daml-app/ui/src/components/App.tsx
+++ templates/create-daml-app/ui/src/components/App.tsx
@@ -6,7 +6,7 @@ import LoginScreen from './LoginScreen';
 import MainScreen from './MainScreen';
 import DamlLedger from '@daml/react';
 import Credentials from '../Credentials';
-import { httpBaseUrl, wsBaseUrl } from '../config';
+import { httpBaseUrl } from '../config';
 
 /**
  * React component for the entry point into the application.
@@ -20,7 +20,6 @@ const App: React.FC = () => {
         token={credentials.token}
         party={credentials.party}
         httpBaseUrl={httpBaseUrl}
-        wsBaseUrl={wsBaseUrl}
       >
         <MainScreen onLogout={() => setCredentials(undefined)}/>
       </DamlLedger>
---
+++ templates/create-daml-app/ui/src/setupProxy.js
@@ -0,0 +1,28 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const httpJsonDevUrl =
+  "http://localhost:" + process.env.REACT_APP_HTTP_JSON_PORT;
+
+/**
+ * @return {Boolean}
+ */
+const filter = function (pathname, req) {
+  const proxied = pathname.match("^/v1") && process.env.NODE_ENV === "development";
+
+  if (proxied) {
+    console.log(
+      "Request with path " + pathname + " proxied from host " + req.headers.host + " to host " + httpJsonDevUrl
+    );
+  }
+
+  return proxied;
+};
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware(filter, {
+      target: httpJsonDevUrl,
+      ws: true, //Proxy websockets
+    })
+  );
+};
