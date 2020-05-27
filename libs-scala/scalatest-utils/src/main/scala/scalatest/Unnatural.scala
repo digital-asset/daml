8edcd02684a1c41db233884a6b2ab0a674633ce9
Stephen Compall <stephen.compall@daml.com>
2020-05-27 14:00:20 -0400

    move *SpecCheckLaws, Unnatural to scalatest-utils

--- libs-scala/scalatest-utils/src/main/scala/scalatest/Unnatural.scala
+++ libs-scala/scalatest-utils/src/main/scala/scalatest/Unnatural.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.lf.data
+package com.daml.scalatest
 
 import org.scalacheck.Arbitrary
 import scalaz.Equal
@@ -10,9 +10,9 @@ import scalaz.Equal
   * that short-circuit when the tparam's equalIsNatural, so you're testing the
   * handwritten paths rather than the compiler-generated ones.
   */
-private[lf] final case class Unnatural[+A](a: A)
+private[daml] final case class Unnatural[+A](a: A)
 
-private[lf] object Unnatural {
+private[daml] object Unnatural {
   implicit def arbUA[A: Arbitrary]: Arbitrary[Unnatural[A]] =
     Arbitrary(Arbitrary.arbitrary[A] map (Unnatural(_)))
   implicit def eqUA[A: Equal]: Equal[Unnatural[A]] = Equal.equalBy(_.a)
