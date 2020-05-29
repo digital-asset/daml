e097cf410bfaeeda873c424b4c57808e7c4f885b
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-05-29 15:57:04 -0400

    Making `PaginatingAsyncStream.streamFrom` more generic (#6170)
    * Making `PaginatingAsyncStream.streamFrom` more generic
    
    so it does not specify what exactly `Offset` is.
    
    changelog_begin
    changelog_end
    
    * Addressing code review comments + cleanup

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/PaginatingAsyncStream.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/PaginatingAsyncStream.scala
@@ -6,7 +6,6 @@ package com.daml.platform.store.dao
 import akka.NotUsed
 import akka.stream.scaladsl.Source
 import com.daml.dec.DirectExecutionContext
-import com.daml.ledger.participant.state.v1.Offset
 
 import scala.concurrent.Future
 
@@ -48,10 +47,10 @@ object PaginatingAsyncStream {
 
   /**
     * Concatenates the results of multiple asynchronous calls into
-    * a single [[Source]], passing the last seen event's ledger [[Offset]] and node index to the
+    * a single [[Source]], passing the last seen event's offset to the
     * next iteration query, so it can continue reading events from this point.
     *
-    * This is to implement pagination based on ledger offset and event node index.
+    * This is to implement pagination based on generic offset.
     * The main purpose of the pagination is to break down large queries
     * into smaller batches. The reason for this is that we are currently using
     * simple blocking JDBC APIs and a long-running stream would end up
@@ -59,25 +58,23 @@ object PaginatingAsyncStream {
     * of keeping multiple, concurrent, long-running streams while serving
     * lookup calls.
     *
-    * @param initialOffset initial ledger [[Offset]]
-    * @param extractOffsetAndNodeIndex function that extracts [[Offset]] and node index from the result entry of type [[T]]
-    * @param query a function that takes [[Offset]] and optional node index to start pagination from
+    * @param startFromOffset initial offset
+    * @param getOffset function that returns a position/offset from the element of type [[T]]
+    * @param query a function that fetches results starting from provided offset
+    * @tparam Off the type of the offset
     * @tparam T the type of the items returned in each call
     */
-  def streamFrom[T](initialOffset: Offset, extractOffsetAndNodeIndex: T => (Offset, Int))(
-      query: (Offset, Option[Int]) => Future[Vector[T]]
+  def streamFrom[Off, T](startFromOffset: Off, getOffset: T => Off)(
+      query: Off => Future[Vector[T]]
   ): Source[T, NotUsed] = {
     Source
-      .unfoldAsync(Option((initialOffset, Option.empty[Int]))) {
+      .unfoldAsync(Option(startFromOffset)) {
         case None =>
           Future.successful(None) // finished reading the whole thing
-        case Some((prevOffset, prevNodeIndex)) =>
-          query(prevOffset, prevNodeIndex).map { result =>
-            val newState = result.lastOption.map { t =>
-              val event: (Offset, Int) = extractOffsetAndNodeIndex(t)
-              (event._1, Some(event._2))
-            }
-            Some((newState, result))
+        case Some(offset) =>
+          query(offset).map { result =>
+            val nextPageOffset: Option[Off] = result.lastOption.map(getOffset)
+            Some((nextPageOffset, result))
           }(DirectExecutionContext) // run in the same thread as the query, avoid context switch for a cheap operation
       }
       .flatMapConcat(Source(_))
