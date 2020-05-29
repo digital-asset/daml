c814742cbc33f694ffcbed7cd4e9f2fd2ec88cee
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-05-29 16:29:49 -0400

    events denormalization, WIP

---
+++ ledger/sandbox/src/main/resources/db/migration/postgres/V33_Add_witnesses_to_participant_events.sql
@@ -0,0 +1,11 @@
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+
+-- TODO
+
+
+
+
+drop table participant_event_flat_transaction_witnesses;
+drop table participant_event_transaction_tree_witnesses;
