63e4be16b1387d64db02281231af8aad5ffb21b1
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-06-02 19:36:27 -0400

    Removing truncates for deleted tables

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/JdbcLedgerDao.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/JdbcLedgerDao.scala
@@ -1052,8 +1052,6 @@ object JdbcLedgerDao {
         |truncate table participant_command_completions cascade;
         |truncate table participant_command_submissions cascade;
         |truncate table participant_events cascade;
-        |truncate table participant_event_flat_transaction_witnesses cascade;
-        |truncate table participant_event_transaction_tree_witnesses cascade;
         |truncate table participant_contracts cascade;
         |truncate table participant_contract_witnesses cascade;
         |truncate table parties cascade;
