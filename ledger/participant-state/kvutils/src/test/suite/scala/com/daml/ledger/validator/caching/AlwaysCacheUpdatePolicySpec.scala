41c5843a44798677800dbfd73417a434d86350da
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-06-02 17:59:38 +0200

    Made it explicit what policy we are testing against.

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/caching/AlwaysCacheUpdatePolicySpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/caching/AlwaysCacheUpdatePolicySpec.scala
@@ -4,20 +4,21 @@
 package com.daml.ledger.validator.caching
 
 import com.daml.ledger.validator.TestHelper
-import com.daml.ledger.validator.caching.AlwaysCacheUpdatePolicy._
 import org.scalatest.{Matchers, WordSpec}
 
 class AlwaysCacheUpdatePolicySpec extends WordSpec with Matchers {
+  private val policy = AlwaysCacheUpdatePolicy
+
   "cache update policy" should {
     "allow caching of all types of keys" in {
       for (stateKey <- TestHelper.allDamlStateKeyTypes) {
-        shouldCacheOnWrite(stateKey) shouldBe true
+        policy.shouldCacheOnWrite(stateKey) shouldBe true
       }
     }
 
     "allow caching of all read values" in {
       for (stateKey <- TestHelper.allDamlStateKeyTypes) {
-        shouldCacheOnRead(stateKey) shouldBe true
+        policy.shouldCacheOnRead(stateKey) shouldBe true
       }
     }
   }
--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/caching/ImmutablesOnlyCacheUpdatePolicySpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/caching/ImmutablesOnlyCacheUpdatePolicySpec.scala
@@ -5,29 +5,30 @@ package com.daml.ledger.validator.caching
 
 import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlStateKey
 import com.daml.ledger.validator.TestHelper
-import com.daml.ledger.validator.caching.ImmutablesOnlyCacheUpdatePolicy._
 import org.scalatest.{Matchers, WordSpec}
 
 class ImmutablesOnlyCacheUpdatePolicySpec extends WordSpec with Matchers {
+  private val policy = ImmutablesOnlyCacheUpdatePolicy
+
   "cache update policy" should {
     "allow write-through caching of packages" in {
-      shouldCacheOnWrite(aPackageKey) shouldBe true
+      policy.shouldCacheOnWrite(aPackageKey) shouldBe true
     }
 
     "not allow write-through caching of non-packages" in {
       for (stateKey <- nonPackageKeyTypes) {
-        shouldCacheOnWrite(stateKey) shouldBe false
+        policy.shouldCacheOnWrite(stateKey) shouldBe false
       }
     }
 
     "allow caching of read packages or parties" in {
-      shouldCacheOnRead(aPackageKey) shouldBe true
-      shouldCacheOnRead(aPartyKey) shouldBe true
+      policy.shouldCacheOnRead(aPackageKey) shouldBe true
+      policy.shouldCacheOnRead(aPartyKey) shouldBe true
     }
 
     "not allow caching of read mutable values" in {
       for (stateKey <- nonPackageKeyTypes if stateKey.getParty.isEmpty) {
-        shouldCacheOnRead(stateKey) shouldBe false
+        policy.shouldCacheOnRead(stateKey) shouldBe false
       }
     }
   }
