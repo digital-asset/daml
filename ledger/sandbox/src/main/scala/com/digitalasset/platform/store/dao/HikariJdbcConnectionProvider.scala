2ac19285f90af7cf9721ec9dea0517bc54472427
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2020-06-04 12:18:51 -0400

    Code review comments.

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/HikariJdbcConnectionProvider.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/HikariJdbcConnectionProvider.scala
@@ -9,6 +9,7 @@ import java.util.{Timer, TimerTask}
 
 import com.codahale.metrics.MetricRegistry
 import com.daml.ledger.api.health.{HealthStatus, Healthy, Unhealthy}
+import com.daml.logging.{ContextualizedLogger, LoggingContext}
 import com.daml.platform.configuration.ServerRole
 import com.daml.platform.store.DbType
 import com.daml.platform.store.dao.HikariJdbcConnectionProvider._
@@ -20,7 +21,54 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.util.control.NonFatal
 
+final class HikariConnection(
+    serverRole: ServerRole,
+    jdbcUrl: String,
+    minimumIdle: Int,
+    maxPoolSize: Int,
+    connectionTimeout: FiniteDuration,
+    metrics: Option[MetricRegistry],
+    connectionPoolPrefix: String,
+    maxInitialConnectRetryAttempts: Int,
+)(implicit logCtx: LoggingContext) extends ResourceOwner[HikariDataSource] {
+
+  private val logger = ContextualizedLogger.get(this.getClass)
+
+  override def acquire()(
+      implicit executionContext: ExecutionContext
+  ): Resource[HikariDataSource] = {
+    val config = new HikariConfig
+    config.setJdbcUrl(jdbcUrl)
+    config.setDriverClassName(DbType.jdbcType(jdbcUrl).driver)
+    config.addDataSourceProperty("cachePrepStmts", "true")
+    config.addDataSourceProperty("prepStmtCacheSize", "128")
+    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
+    config.setAutoCommit(false)
+    config.setMaximumPoolSize(maxPoolSize)
+    config.setMinimumIdle(minimumIdle)
+    config.setConnectionTimeout(connectionTimeout.toMillis)
+    config.setPoolName(s"$connectionPoolPrefix.${serverRole.threadPoolSuffix}")
+    metrics.foreach(config.setMetricRegistry)
+
+    // Hikari dies if a database connection could not be opened almost immediately
+    // regardless of any connection timeout settings. We retry connections so that
+    // Postgres and Sandbox can be started in any order.
+    Resource(
+      RetryStrategy.constant(
+        attempts = maxInitialConnectRetryAttempts,
+        waitTime = 1.second
+      ) { (i, _) =>
+        Future {
+          logger.info(s"Attempting to connect to Postgres (attempt ${i + 1}/${maxInitialConnectRetryAttempts})")
+          new HikariDataSource(config)
+        }
+      }
+    )(conn => Future { conn.close() })
+  }
+}
+
 object HikariConnection {
+  private val MaxInitialConnectRetryAttempts = 600
   private val ConnectionPoolPrefix: String = "daml.index.db.connection"
 
   def owner(
@@ -30,54 +78,24 @@ object HikariConnection {
       maxPoolSize: Int,
       connectionTimeout: FiniteDuration,
       metrics: Option[MetricRegistry],
-  ): ResourceOwner[HikariDataSource] =
-    new Owner(
+  )(
+      implicit logCtx: LoggingContext
+  ): HikariConnection =
+    new HikariConnection(
       serverRole,
       jdbcUrl,
       minimumIdle,
       maxPoolSize,
       connectionTimeout,
-      metrics
+      metrics,
+      ConnectionPoolPrefix,
+      MaxInitialConnectRetryAttempts,
     )
-
-  final class Owner(
-      serverRole: ServerRole,
-      jdbcUrl: String,
-      minimumIdle: Int,
-      maxPoolSize: Int,
-      connectionTimeout: FiniteDuration,
-      metrics: Option[MetricRegistry],
-  ) extends ResourceOwner[HikariDataSource] {
-    override def acquire()(
-        implicit executionContext: ExecutionContext
-    ): Resource[HikariDataSource] = {
-      val config = new HikariConfig
-      config.setJdbcUrl(jdbcUrl)
-      config.setDriverClassName(DbType.jdbcType(jdbcUrl).driver)
-      config.addDataSourceProperty("cachePrepStmts", "true")
-      config.addDataSourceProperty("prepStmtCacheSize", "128")
-      config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
-      config.setAutoCommit(false)
-      config.setMaximumPoolSize(maxPoolSize)
-      config.setMinimumIdle(minimumIdle)
-      config.setConnectionTimeout(connectionTimeout.toMillis)
-      config.setPoolName(s"$ConnectionPoolPrefix.${serverRole.threadPoolSuffix}")
-      metrics.foreach(config.setMetricRegistry)
-
-      // Hikari dies if a database connection could not be opened almost immediately
-      // regardless of any connection timeout settings. We retry connections so that
-      // Postgres and Sandbox can be started in any order.
-      Resource(
-        RetryStrategy.constant(attempts = 600, waitTime = 1.second) { (_, _) =>
-          Future { new HikariDataSource(config) }
-        }
-      )(conn => Future { conn.close() })
-    }
-  }
 }
 
-class HikariJdbcConnectionProvider(dataSource: HikariDataSource, healthPoller: Timer)
-    extends JdbcConnectionProvider {
+class HikariJdbcConnectionProvider(dataSource: HikariDataSource, healthPoller: Timer)(
+    implicit logCtx: LoggingContext
+) extends JdbcConnectionProvider {
   private val transientFailureCount = new AtomicInteger(0)
 
   private val checkHealth = new TimerTask {
@@ -130,7 +148,7 @@ object HikariJdbcConnectionProvider {
       jdbcUrl: String,
       maxConnections: Int,
       metrics: MetricRegistry,
-  ): ResourceOwner[HikariJdbcConnectionProvider] =
+  )(implicit logCtx: LoggingContext): ResourceOwner[HikariJdbcConnectionProvider] =
     for {
       // these connections should never time out as we have the same number of threads as connections
       dataSource <- HikariConnection.owner(
