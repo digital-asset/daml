6476dc3c9e2005fa684f4cc65fdb4a9335164ebf
Stephen Compall <stephen.compall@daml.com>
2020-06-10 15:41:08 -0400

    abstraction for doing multiple anorm queries, ending in a parsed result

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/SqlSequence.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/SqlSequence.scala
@@ -1,5 +1,41 @@
 package com.daml.platform.store.dao.events
 
+import com.daml.platform.store.SimpleSqlAsVectorOf._
+import anorm.{ResultSetParser, Row, RowParser, SimpleSql}
+import scalaz.{-\/, Free, Functor, \/-}
+
+import java.sql.Connection
+
 object SqlSequence {
 
+  /** A sequence of `SimpleSql`s, terminating in A. */
+  type T[A] = Free[Elt, A]
+
+  final case class Elt[+A](run: Connection => A)
+
+  object Elt {
+    implicit final class syntax[A](private val self: T[A]) extends AnyVal {
+      def executeSql(implicit conn: Connection): A = {
+        @annotation.tailrec
+        def go(self: T[A]): A =
+          self.resume match {
+            case -\/(elt) => go(elt.run(conn))
+            case \/-(a) => a
+          }
+        go(self)
+      }
+    }
+
+    implicit val covariant: Functor[Elt] = new Functor[Elt] {
+      override def map[A, B](fa: Elt[A])(f: A => B) = fa copy (run = fa.run andThen f)
+    }
+  }
+
+  def apply[A](s: SimpleSql[_], p: ResultSetParser[A]): T[A] =
+    Free liftF Elt(implicit conn => s as p)
+
+  def vector[A](s: SimpleSql[Row], p: RowParser[A]): T[Vector[A]] =
+    Free liftF Elt(implicit conn => s asVectorOf p)
+
+  def point[A](a: A): T[A] = Free point a
 }
