fb6141f7b306fd85da8e2081d3662f0afb0c900d
Leonid Rozenberg <leonid.rozenberg@digitalasset.com>
2020-06-23 09:43:00 -0400

    Add copyright notice

--- language-support/ts/daml-react/defaultLedgerContext.ts
+++ language-support/ts/daml-react/defaultLedgerContext.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 import createLedgerContext, { FetchResult, QueryResult, LedgerProps } from "./createLedgerContext";
 import { Party, Template } from '@daml/types';
