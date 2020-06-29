1fc5c5d318aa696d44ee129367eaec785a3994ac
Sofia Faro <sofia.faro@digitalasset.com>
2020-06-29 13:34:31 +0100

    Explicit export list in tests module

--- compiler/daml-lf-tools/tests/DA/Daml/LF/Simplifier/Tests.hs
+++ compiler/daml-lf-tools/tests/DA/Daml/LF/Simplifier/Tests.hs
@@ -1,7 +1,9 @@
 -- Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-module DA.Daml.LF.Simplifier.Tests where
+module DA.Daml.LF.Simplifier.Tests
+    ( main
+    ) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
