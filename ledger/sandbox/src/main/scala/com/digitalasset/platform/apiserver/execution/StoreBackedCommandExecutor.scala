442e34249c0a8773ebe5578e045b0d40386ad31e
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-06-29 12:26:32 +0200

    Code tidying.

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/apiserver/execution/StoreBackedCommandExecutor.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/apiserver/execution/StoreBackedCommandExecutor.scala
@@ -48,7 +48,7 @@ final class StoreBackedCommandExecutor(
   ): Future[Either[ErrorCause, CommandExecutionResult]] = {
     val start = System.nanoTime()
     val submissionResult = engine.submit(commands.commands, participant, submissionSeed)
-    val interpretationTime = System.nanoTime() - start
+    val interpretationTimeNanos = System.nanoTime() - start
     consume(commands.submitter, submissionResult)
       .map { submission =>
         (for {
@@ -75,7 +75,7 @@ final class StoreBackedCommandExecutor(
             ),
             transaction = updateTx,
             dependsOnLedgerTime = meta.dependsOnTime,
-            interpretationTimeNanos = interpretationTime
+            interpretationTimeNanos = interpretationTimeNanos
           )).left.map(ErrorCause.DamlLf)
       }
   }
