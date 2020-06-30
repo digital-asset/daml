2cb65f58fe2d2ffa55b3d7817d8149493c34bc9c
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2020-06-30 08:17:45 +0200

    Accommodate implicitly created, local parties

--- ledger/sandbox/src/main/resources/db/migration/postgres/V34__Parties_is_local.sql
+++ ledger/sandbox/src/main/resources/db/migration/postgres/V34__Parties_is_local.sql
@@ -15,4 +15,7 @@ UPDATE parties SET is_local = (
 SELECT party_entries.is_local
 FROM party_entries where party_entries.party = parties.party);
 
+-- implicit party allocation produces local parties not present in party_entries
+UPDATE parties SET is_local = true WHERE is_local IS NULL;
+
 ALTER TABLE parties ALTER COLUMN is_local SET NOT NULL;
