01e40b04c0cda13748f58ac12ade3dcba79cb60e
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-06-30 11:54:23 -0400

    forcing postgres to use index when looking up lower and upper bound row ids

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/EventsRange.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/EventsRange.scala
@@ -3,7 +3,7 @@
 package com.daml.platform.store.dao.events
 
 import anorm.SqlParser.get
-import anorm.{RowParser, SqlStringInterpolation}
+import anorm.SqlStringInterpolation
 import com.daml.ledger.participant.state.v1.Offset
 
 final case class EventsRange[A](startExclusive: A, endInclusive: A)
@@ -11,16 +11,7 @@ final case class EventsRange[A](startExclusive: A, endInclusive: A)
 object EventsRange {
   private val EmptyRowIdRange = EventsRange(0L, 0L)
 
-  private val rangeParser: RowParser[EventsRange[Long]] =
-    (get[Option[Long]]("start") ~ get[Option[Long]]("end")).map { row =>
-      EventsRange(
-        startExclusive = row._1.getOrElse(EmptyRowIdRange.startExclusive),
-        endInclusive = row._2.getOrElse(EmptyRowIdRange.endInclusive))
-    }
-
-  private val endParser: RowParser[EventsRange[Long]] =
-    get[Option[Long]]("end")
-      .map(_.fold(EmptyRowIdRange)(x => EmptyRowIdRange.copy(endInclusive = x)))
+  private val oneRow = Some(1)
 
   /**
     * Converts Offset range to Row ID range.
@@ -30,18 +21,11 @@ object EventsRange {
     * @return Row ID range
     */
   def readRowIdRange(range: EventsRange[Offset])(
-      connection: java.sql.Connection): EventsRange[Long] = {
-    import com.daml.platform.store.Conversions.OffsetToStatement
-
-    if (range.startExclusive == Offset.beforeBegin) {
-      readRowIdRange(range.endInclusive)(connection)
-    } else {
-      // start is exclusive, that is why -1
-      val query =
-        SQL"select min(row_id) - 1 as start, max(row_id) as end from participant_events where event_offset > ${range.startExclusive} and event_offset <= ${range.endInclusive}"
-      query.as(rangeParser.single)(connection)
-    }
-  }
+      connection: java.sql.Connection): EventsRange[Long] =
+    EventsRange(
+      startExclusive = readLowerBound(range.startExclusive)(connection),
+      endInclusive = readUpperBound(range.endInclusive)(connection)
+    )
 
   /**
     * Converts ledger end offset into a Row ID range.
@@ -50,15 +34,34 @@ object EventsRange {
     * @param connection SQL connection
     * @return Row ID range
     */
-  def readRowIdRange(endInclusive: Offset)(connection: java.sql.Connection): EventsRange[Long] = {
-    import com.daml.platform.store.Conversions.OffsetToStatement
+  def readRowIdRange(endInclusive: Offset)(connection: java.sql.Connection): EventsRange[Long] =
+    EmptyRowIdRange.copy(endInclusive = readUpperBound(endInclusive)(connection))
 
-    if (endInclusive == Offset.beforeBegin) {
-      EmptyRowIdRange
+  private def readLowerBound(startExclusive: Offset)(connection: java.sql.Connection): Long =
+    if (startExclusive == Offset.beforeBegin) {
+      EmptyRowIdRange.startExclusive
     } else {
-      val query =
-        SQL"select max(row_id) as end from participant_events where event_offset <= ${endInclusive}"
-      query.as(endParser.single)(connection)
+      import com.daml.platform.store.Conversions.OffsetToStatement
+      // This query could be: "select min(row_id) - 1 as start from participant_events where event_offset > ${range.startExclusive}"
+      // however there are cases when postgres decides not to use the index. We are forcing the index usage specifying `order by event_offset`
+      SQL"select min(row_id) from participant_events where event_offset > ${startExclusive} group by event_offset order by event_offset asc limit 1"
+        .withFetchSize(oneRow)
+        .as(get[Option[Long]](1).single)(connection)
+        .map(_ - 1L)
+        .getOrElse(EmptyRowIdRange.startExclusive)
     }
-  }
+
+  private def readUpperBound(endInclusive: Offset)(connection: java.sql.Connection): Long =
+    if (endInclusive == Offset.beforeBegin) {
+      EmptyRowIdRange.endInclusive
+    } else {
+      import com.daml.platform.store.Conversions.OffsetToStatement
+      // This query could be: "select max(row_id) as end from participant_events where event_offset <= ${range.endInclusive}"
+      // however there are cases (query takes 24 min on DB with 4million participant_events rows)
+      // when postgres decides not to use the index. We are forcing the index usage specifying `order by event_offset`
+      SQL"select max(row_id) from participant_events where event_offset <= ${endInclusive} group by event_offset order by event_offset desc limit 1"
+        .withFetchSize(oneRow)
+        .as(get[Option[Long]](1).single)(connection)
+        .getOrElse(EmptyRowIdRange.endInclusive)
+    }
 }
