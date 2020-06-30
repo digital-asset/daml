99a9662781f305e5d7356230857d4e73016ba5f5
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-06-30 12:00:45 -0400

    Trigger service: change convention for updating running trigger store (#6532)
    * Don't modify running trigger store via the trigger runner actor
    
    changelog_begin
    changelog_end
    
    * Remove runner actor from RunningTrigger type
    
    And parent actor from TriggerRunnerImpl
    
    * Add to running trigger store on start request
    
    * Update tests for new trigger store convention
    
    * Fix test with network outage

--- triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/TriggerRunner.scala
+++ triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/TriggerRunner.scala
@@ -7,7 +7,6 @@ import akka.actor.typed.{Behavior, PostStop}
 import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.SupervisorStrategy._
 import akka.actor.typed.Signal
-import akka.actor.typed.PostStop
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.ActorContext
 import akka.stream.Materializer
@@ -46,7 +45,7 @@ class TriggerRunner(
       Behaviors
         .supervise(
           Behaviors
-            .supervise(TriggerRunnerImpl(ctx.self, config))
+            .supervise(TriggerRunnerImpl(config))
             .onFailure[InitializationHalted](stop)
         )
         .onFailure(
