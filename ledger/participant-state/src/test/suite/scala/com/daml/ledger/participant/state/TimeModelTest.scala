474e92c13c6989fb2ed2b59af58007880818b465
fabiotudone-da <fabio.tudone@digitalasset.com>
2020-07-08 09:53:44 +0200

    Track min and max record time in `CommitContext` when pre-executing (#6591)

--- ledger/participant-state/src/test/suite/scala/com/daml/ledger/participant/state/TimeModelTest.scala
+++ ledger/participant-state/src/test/suite/scala/com/daml/ledger/participant/state/TimeModelTest.scala
@@ -13,10 +13,12 @@ class TimeModelTest extends WordSpec with Matchers {
   private val epsilon = Duration.ofMillis(10L)
   private val timeModel =
     TimeModel(
-      avgTransactionLatency = Duration.ofSeconds(0L),
+      avgTransactionLatency = Duration.ZERO,
       minSkew = Duration.ofSeconds(30L),
       maxSkew = Duration.ofSeconds(30L),
     ).get
+  private val smallSkew = Duration.ofSeconds(1L)
+  private val largeSkew = Duration.ofHours(1L)
 
   "Ledger time model" when {
     "checking ledger time" should {
@@ -55,6 +57,45 @@ class TimeModelTest extends WordSpec with Matchers {
           .checkTime(referenceTime.minus(timeModel.minSkew).minus(epsilon), referenceTime)
           .isRight shouldEqual false
       }
+
+      "succeed if the ledger time is equal to the high boundary (asymmetric case)" in {
+        val instance = createAsymmetricTimeModel(largeSkew, smallSkew)
+
+        instance
+          .checkTime(referenceTime.plus(instance.maxSkew), referenceTime)
+          .isRight shouldEqual true
+      }
+
+      "succeed if the ledger time is equal to the low boundary (asymmetric case)" in {
+        val instance = createAsymmetricTimeModel(smallSkew, largeSkew)
+
+        instance
+          .checkTime(referenceTime.minus(instance.minSkew), referenceTime)
+          .isRight shouldEqual true
+      }
+
+      "fail if the ledger time is higher than the high boundary (asymmetric case)" in {
+        val instance = createAsymmetricTimeModel(largeSkew, smallSkew)
+
+        instance
+          .checkTime(referenceTime.plus(instance.maxSkew).plus(epsilon), referenceTime)
+          .isLeft shouldEqual true
+      }
+
+      "fail if the ledger time is lower than the low boundary (asymmetric case)" in {
+        val instance = createAsymmetricTimeModel(smallSkew, largeSkew)
+
+        instance
+          .checkTime(referenceTime.minus(instance.minSkew).minus(epsilon), referenceTime)
+          .isLeft shouldEqual true
+      }
     }
   }
+
+  private def createAsymmetricTimeModel(minSkew: Duration, maxSkew: Duration): TimeModel =
+    TimeModel(
+      avgTransactionLatency = Duration.ZERO,
+      minSkew = minSkew,
+      maxSkew = maxSkew,
+    ).get
 }
