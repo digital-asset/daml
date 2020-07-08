7534dfa6269c3ef121f67f0f2d1259ca7c6cb9ca
Samir Talwar <samir.talwar@digitalasset.com>
2020-07-08 11:41:40 +0200

    sandbox: In EmptyLedgerIT, use the submission service.

--- ledger/sandbox/src/test/suite/scala/com/digitalasset/platform/sandbox/services/completion/EmptyLedgerIT.scala
+++ ledger/sandbox/src/test/suite/scala/com/digitalasset/platform/sandbox/services/completion/EmptyLedgerIT.scala
@@ -8,7 +8,8 @@ import java.nio.file.Files
 import java.time.Duration
 
 import com.daml.api.util.DurationConversion
-import com.daml.ledger.api.testing.utils.SuiteResourceManagementAroundEach
+import com.daml.ledger.api.domain
+import com.daml.ledger.api.testing.utils.{MockMessages, SuiteResourceManagementAroundEach}
 import com.daml.ledger.api.v1.admin.config_management_service.{
   ConfigManagementServiceGrpc,
   GetTimeModelRequest,
@@ -31,13 +32,10 @@ import com.daml.ledger.api.v1.command_completion_service.{
   CompletionStreamRequest,
   CompletionStreamResponse
 }
-import com.daml.ledger.api.v1.command_service.{CommandServiceGrpc, SubmitAndWaitRequest}
-import com.daml.ledger.api.v1.commands.{Command, Commands, CreateCommand}
+import com.daml.ledger.api.v1.command_submission_service.CommandSubmissionServiceGrpc
 import com.daml.ledger.api.v1.ledger_offset.LedgerOffset
-import com.daml.ledger.api.v1.value.{Record, RecordField, Value}
 import com.daml.ledger.participant.state.v1.TimeModel
 import com.daml.platform.ApiOffset
-import com.daml.platform.participant.util.ValueConversions.CreateCommands
 import com.daml.platform.sandbox.SandboxBackend
 import com.daml.platform.sandbox.config.SandboxConfig
 import com.daml.platform.sandbox.services.TestCommands
@@ -50,7 +48,7 @@ import io.grpc.Channel
 import org.scalatest.{AsyncWordSpec, Inspectors, Matchers}
 import scalaz.syntax.tag._
 
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.concurrent.{ExecutionContext, Future}
 
 final class EmptyLedgerIT
@@ -74,12 +72,18 @@ final class EmptyLedgerIT
     )
 
   "CommandCompletionService gives sensible ledger end on an empty ledger" in {
-    val lid = ledgerId().unwrap
+    val lid = ledgerId()
     val party = "partyA"
     val completionService = CommandCompletionServiceGrpc.stub(channel)
     for {
-      end <- completionService.completionEnd(CompletionEndRequest(lid))
-      completions <- completionsFromOffset(completionService, lid, List(party), end.getOffset)
+      end <- completionService.completionEnd(CompletionEndRequest(lid.unwrap))
+      completions <- completionsFromOffset(
+        completionService = completionService,
+        ledgerId = lid,
+        parties = List(party),
+        offset = end.getOffset,
+        completionTimeout = 2.seconds,
+      )
     } yield {
       end.getOffset.value.absolute.get shouldBe ApiOffset.begin.toHexString
       completions shouldBe empty
@@ -98,26 +102,26 @@ final class EmptyLedgerIT
   }
 
   "CommandCompletionService can stream completions from the beginning" in {
-    val lid = ledgerId().unwrap
+    val lid = ledgerId()
     val party = "partyA"
     val commandId = "commandId"
-    val command: Command = CreateCommand(
-      Some(templateIds.dummy),
-      Some(
-        Record(
-          Some(templateIds.dummy),
-          Seq(RecordField("operator", Option(Value(Value.Sum.Party(party)))))))).wrap
-    val commands = Commands(lid, workflowId, applicationId, commandId, party, Seq(command))
 
-    val commandService = CommandServiceGrpc.stub(channel)
+    val submissionService = CommandSubmissionServiceGrpc.stub(channel)
     val completionService = CommandCompletionServiceGrpc.stub(channel)
     for {
-      end <- completionService.completionEnd(CompletionEndRequest(lid))
+      end <- completionService.completionEnd(CompletionEndRequest(lid.unwrap))
       _ <- publishATimeModel(channel)
       _ <- uploadDarFile(darFile, channel)
       _ <- allocateParty(party, channel)
-      _ <- commandService.submitAndWait(SubmitAndWaitRequest(Some(commands), None))
-      completions <- completionsFromOffset(completionService, lid, List(party), end.getOffset)
+      completionsF = completionsFromOffset(
+        completionService = completionService,
+        ledgerId = lid,
+        parties = List(party),
+        offset = end.getOffset,
+        completionTimeout = 10.seconds,
+      )
+      _ <- submissionService.submit(dummyCommands(lid, commandId, party))
+      completions <- completionsF
     } yield {
       completions shouldBe Vector(commandId)
     }
@@ -125,15 +129,6 @@ final class EmptyLedgerIT
 }
 
 object EmptyLedgerIT {
-  private val applicationId = classOf[EmptyLedgerIT].getSimpleName
-
-  private val workflowId = "workflowId"
-
-  // How long it takes to download the entire completion stream.
-  // Because the stream does not terminate, we use a timeout to determine when the stream
-  // is done emitting elements.
-  private val completionTimeout = 2.seconds
-
   private def publishATimeModel(channel: Channel)(implicit ec: ExecutionContext): Future[Unit] = {
     val configService = ConfigManagementServiceGrpc.stub(channel)
     for {
@@ -175,13 +170,16 @@ object EmptyLedgerIT {
 
   private def completionsFromOffset(
       completionService: CommandCompletionServiceGrpc.CommandCompletionServiceStub,
-      ledgerId: String,
+      ledgerId: domain.LedgerId,
       parties: Seq[String],
       offset: LedgerOffset,
+      // Because the stream does not terminate, we use a timeout to cut it off when it *should* be
+      // done. Obviously, this is prone to error, so the timeouts are fairly generous.
+      completionTimeout: FiniteDuration,
   )(implicit ec: ExecutionContext): Future[Vector[String]] = {
     new StreamConsumer[CompletionStreamResponse](
       completionService.completionStream(
-        CompletionStreamRequest(ledgerId, applicationId, parties, Some(offset)),
+        CompletionStreamRequest(ledgerId.unwrap, MockMessages.applicationId, parties, Some(offset)),
         _
       )
     ).within(completionTimeout)
