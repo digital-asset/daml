digital-asset/daml

commit 31995ee00073b446f7361016fc22d0fb0f320ce0

Ledger API Test Tool: deprecate unneeded tests and options  (#6651)
* Ledger API Test Tool: deprecate unneeded tests and options

Furthermore, now all active tests are now run be default without needing
to specify --all-tests. The scheduler makes sure to run non-isolated
tests last (and sequentially).

Fixes #3747
Fixes #6518

A bit of historical context: tests in the Ledger API Test Tool used to
be the sandbox integration tests. As part of the Ledger API Test Tool
project, those tests have been ported one-to-one, with little review of
those same tests.

Examining the tests that cause #3747, it became evident that those tests
were originally put in place to verify that an isolated sandbox spun up
for a single test could withstand a number of commands slightly lower
than what at the time was the hard-coded back-pressure threshold. Hence,
implementing back-pressure on the Ledger API Test Tool would have
basically negated the usefulness of those tests. Furthermore, those
tests can be easily passed by simply raising the back-pressure
threshold. As such, they do not convey any meaningful information for
the DAML ledger implementor. This means we are retiring those tests. The
tests will be nominally kept in for a deprecation period to not break
existing build scripts, but running them explicitly will simply show
them as skipped. The documentation has been updated with the information
necessary to users to deal with retired tests.

Since all tests are now run by default --all-tests is being deprecated.
--load-scale-factor is also deprecated as it was used only by
TransactionScaleIT.

changelog_begin
[Ledger API Test Tool] The LotsOfPartiesIT and TransactionScaleIT test
suite have been deemed not providing relevant signal to DAML ledger
implementers and have been retired. The tests will be nominally kept in
but will be skipped while they are in a deprecation period. You are
advised to remove explicit references to those tests before they are
fully removed.

ledger/ledger-api-test-tool/BUILD.bazel
@@ -159,3 +159,37 @@ conformance_test(
         "--pem $(rlocation $TEST_WORKSPACE/$(rootpath //ledger/test-common/test-certificates:client.pem))",
     ],
 )
+
+# Explicitly include retired tests here to make sure existing CI pipelines are not broken
+# Retired tests will be eventually removed
+conformance_test(
+    name = "retired-tests",
+    server = "//ledger/ledger-on-memory:app",
+    server_args = [
+        "--contract-id-seeding=testing-weak",
+        "--participant participant-id=ssl-test,port=6865",
+    ],
+    test_tool_args = [
+        "--include=LotsOfPartiesIT",
+        "--include=TransactionScaleIT",
+    ],
+)
+
+# Makes sure that deprecated CLI options can still be used to make sure existing CI pipelines are not broken
+# This test should fail if any deprecated CLI option has any effect whatsoever -- they are preserved
+# exclusively for backwards-compatibility
+# Deprecated CLI options will be eventually removed
+conformance_test(
+    name = "deprecated-cli-options",
+    server = "//ledger/ledger-on-memory:app",
+    server_args = [
+        "--contract-id-seeding=testing-weak",
+        "--participant participant-id=ssl-test,port=6865",
+    ],
+    test_tool_args = [
+        "--include=IdentityIT",
+        "--all-tests",
+        "--load-scale-factor=THIS_OPTION_IS_DEPRECATED_AND_HAS_NO_EFFECT",
+        "--target-port=THIS_OPTION_IS_DEPRECATED_AND_HAS_NO_EFFECT",
+    ],
+)

ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/LedgerApiTestTool.scala
@@ -11,6 +11,7 @@ import com.daml.ledger.api.testtool.infrastructure.{
   LedgerSessionConfiguration,
   LedgerTestCase,
   LedgerTestCasesRunner,
+  LedgerTestSuite,
   LedgerTestSummary
 }
 import org.slf4j.LoggerFactory
@@ -38,29 +39,25 @@ object LedgerApiTestTool {
   private def exitCode(summaries: Vector[LedgerTestSummary], expectFailure: Boolean): Int =
     if (summaries.exists(_.result.isLeft) == expectFailure) 0 else 1
 
-  private def printListOfTests[A](defaults: Seq[A], optionals: Seq[A])(
-      getName: A => String): Unit = {
-    println("Tests marked with * are run by default.")
-    println(
-      "You can include extra tests with `--include=TEST-NAME`, or run all tests with `--all-tests`.\n")
-    defaults.map(getName(_) + " * ").sorted.foreach(println(_))
-    optionals.map(getName).sorted.foreach(println(_))
+  private def printListOfTests[A](tests: Seq[A])(getName: A => String): Unit = {
+    println("All tests are run by default.")
+    println()
+    tests.map(getName).sorted.foreach(println(_))
 
-    println("\nAlternatively, you can run performance tests.")
-    println(
-      "Performance tests are not run by default, but can be run with `--perf-tests=TEST-NAME`.\n")
+    println()
+    println("Alternatively, you can run performance tests.")
+    println("They are not run by default, but can be run with `--perf-tests=TEST-NAME`.")
+    println()
     Tests.PerformanceTestsKeys.sorted.foreach(println(_))
   }
   private def printAvailableTestSuites(config: Config): Unit = {
     println("Listing test suites. Run with --list-all to see individual tests.")
-    printListOfTests(Tests.default.values.toSeq, Tests.optional(config).values.toSeq)(_.name)
+    printListOfTests(Tests.all)(_.name)
   }
 
   private def printAvailableTests(config: Config): Unit = {
     println("Listing all tests. Run with --list to only see test suites.")
-    printListOfTests(
-      Tests.default.values.flatMap(_.tests).toSeq,
-      Tests.optional(config).values.flatMap(_.tests).toSeq)(_.name)
+    printListOfTests(Tests.all.flatMap(_.tests).toSeq)(_.name)
   }
 
   private def extractResources(resources: String*): Unit = {
@@ -74,6 +71,12 @@ object LedgerApiTestTool {
       println(s"Extracted $resource to $targetFile")
     }
   }
+
+  private def cases(m: Map[String, LedgerTestSuite]): Iterable[LedgerTestCase] =
+    m.values.flatMap(_.tests)
+
+  private def matches(prefixes: Iterable[String])(test: LedgerTestCase): Boolean =
+    prefixes.exists(test.name.startsWith)
 
   def main(args: Array[String]): Unit = {
 
@@ -104,11 +107,7 @@ object LedgerApiTestTool {
       sys.exit(0)
     }
 
-    val cases: Tests.Tests => Iterable[LedgerTestCase] = m => m.values.flatMap(_.tests)
-    val matches: Iterable[String] => LedgerTestCase => Boolean = prefixes =>
-      test => prefixes.exists(prefix => test.name.startsWith(prefix))
-
-    val allTestCaseNames: Set[String] = cases(Tests.all(config)).map(_.name).toSet
+    val allTestCaseNames: Set[String] = (Tests.all ++ Tests.retired).map(_.name).toSet
     val missingTests = (config.included ++ config.excluded).filterNot(prefix =>
       allTestCaseNames.exists(_.startsWith(prefix)))
     if (missingTests.nonEmpty) {
@@ -122,7 +121,7 @@ object LedgerApiTestTool {
     val performanceTestsToRun =
       Tests.performanceTests(config.performanceTestsReport).filterKeys(config.performanceTests)
 
-    if ((config.allTests || config.included.nonEmpty) && performanceTestsToRun.nonEmpty) {
+    if (config.included.nonEmpty && performanceTestsToRun.nonEmpty) {
       println("Either regular or performance tests can be run, but not both.")
       sys.exit(64)
     }
@@ -134,11 +133,15 @@ object LedgerApiTestTool {
         sys.exit(1)
       })
 
+    val allCases = Tests.all.flatMap(_.tests)
+    val retiredCases = Tests.retired.flatMap(_.tests)
+
+    val includedTests =
+      if (config.included.isEmpty) allCases
+      else (allCases ++ retiredCases).filter(matches(config.included))
+
     val testsToRun: Iterable[LedgerTestCase] =
-      (if (config.allTests) cases(Tests.all(config))
-       else if (config.included.isEmpty) cases(Tests.default)
-       else cases(Tests.all(config)).filter(matches(config.included)))
-        .filterNot(matches(config.excluded))
+      includedTests.filterNot(matches(config.excluded))
 
     val runner =
       if (performanceTestsToRun.nonEmpty)
@@ -155,7 +158,10 @@ object LedgerApiTestTool {
 
     runner.verifyRequirementsAndRun {
       case Success(summaries) =>
-        new ColorizedPrintStreamReporter(System.out, config.verbose).report(summaries)
+        new ColorizedPrintStreamReporter(
+          System.out,
+          config.verbose,
+        ).report(summaries)
         sys.exit(exitCode(summaries, config.mustFail))
       case Failure(e) =>
         logger.error(e.getMessage, e)
@@ -172,7 +178,6 @@ object LedgerApiTestTool {
         config.participants,
         config.shuffleParticipants,
         config.tlsConfig,
-        config.loadScaleFactor,
         config.partyAllocation,
       ),
       cases.toVector,

ledger/ledger-on-memory/BUILD.bazel
@@ -123,8 +123,6 @@ conformance_test(
     ],
     test_tool_args = [
         "--verbose",
-        "--all-tests",
-        "--exclude=ConfigManagementServiceIT",
     ],
 )
 
@@ -143,22 +141,7 @@ conformance_test(
     ],
     test_tool_args = [
         "--verbose",
-        "--all-tests",
         "--exclude=ConfigManagementServiceIT",
-    ],
-)
-
-conformance_test(
-    name = "conformance-test-config-management",
-    ports = [6865],
-    server = ":app",
-    server_args = [
-        "--contract-id-seeding=testing-weak",
-        "--participant participant-id=example,port=6865",
-    ],
-    test_tool_args = [
-        "--verbose",
-        "--include=ConfigManagementServiceIT",
     ],
 )
 

ledger/ledger-on-sql/BUILD.bazel
@@ -241,22 +241,6 @@ da_scala_test_suite(
             tags = db.get("conformance_test_tags", []),
             test_tool_args = db.get("conformance_test_tool_args", []) + [
                 "--verbose",
-                "--all-tests",
-                "--exclude=ConfigManagementServiceIT",
-            ],
-        ),
-        conformance_test(
-            name = "conformance-test-config-management-{}".format(db["name"]),
-            ports = [6865],
-            server = ":conformance-test-{}-bin".format(db["name"]),
-            server_args = [
-                "--contract-id-seeding=testing-weak",
-                "--participant participant-id=conformance-test,port=6865",
-            ] + db.get("conformance_test_server_args", []),
-            tags = db.get("conformance_test_tags", []),
-            test_tool_args = db.get("conformance_test_tool_args", []) + [
-                "--verbose",
-                "--include=ConfigManagementServiceIT",
             ],
         ),
         conformance_test(

ledger/sandbox/BUILD.bazel
@@ -397,8 +397,6 @@ server_conformance_test(
     servers = SERVERS,
     test_tool_args = [
         "--open-world",
-        "--all-tests",
-        "--exclude=ConfigManagementServiceIT",
         "--exclude=ClosedWorldIT",
     ],
 )
@@ -411,33 +409,7 @@ server_conformance_test(
     servers = SERVERS,
     test_tool_args = [
         "--open-world",
-        "--all-tests",
-        "--exclude=ConfigManagementServiceIT",
         "--exclude=ClosedWorldIT",
-    ],
-)
-
-server_conformance_test(
-    name = "conformance-test-config-management",
-    server_args = [
-        "--wall-clock-time",
-    ],
-    servers = SERVERS,
-    test_tool_args = [
-        "--open-world",
-        "--include=ConfigManagementServiceIT",
-    ],
-)
-
-server_conformance_test(
-    name = "conformance-test-command-deduplication",
-    server_args = [
-        "--wall-clock-time",
-    ],
-    servers = SERVERS,
-    test_tool_args = [
-        "--open-world",
-        "--include=CommandDeduplicationIT",
     ],
 )
 
@@ -450,8 +422,6 @@ server_conformance_test(
     servers = SERVERS,
     test_tool_args = [
         "--open-world",
-        "--all-tests",
-        "--exclude=ConfigManagementServiceIT",
         "--exclude=ClosedWorldIT",
     ],
 )
@@ -483,9 +453,7 @@ server_conformance_test(
     servers = NEXT_SERVERS,
     test_tool_args = [
         "--open-world",
-        "--all-tests",
         "--exclude=ClosedWorldIT",
-        "--exclude=ConfigManagementServiceIT",
     ],
 )
 
@@ -497,21 +465,7 @@ server_conformance_test(
     servers = NEXT_SERVERS,
     test_tool_args = [
         "--open-world",
-        "--all-tests",
         "--exclude=ClosedWorldIT",
-        "--exclude=ConfigManagementServiceIT",
-    ],
-)
-
-server_conformance_test(
-    name = "next-conformance-test-config-management",
-    server_args = [
-        "--wall-clock-time",
-    ],
-    servers = NEXT_SERVERS,
-    test_tool_args = [
-        "--open-world",
-        "--include=ConfigManagementServiceIT",
     ],
 )
 

ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/Cli.scala
@@ -14,6 +14,14 @@ import scopt.Read.{intRead, stringRead}
 
 object Cli {
 
+  private def reportUsageOfDeprecatedOption[A, B](
+      option: String,
+  )(ignoredValue: A, ignoredConfig: B): B = {
+    System.err.println(
+      s"WARNING: $option has been deprecated and will be removed in a future version")
+    ignoredConfig
+  }
+
   private def endpointRead: Read[(String, Int)] = new Read[(String, Int)] {
     val arity = 2
     val reads: String => (String, Int) = { s: String =>
@@ -67,6 +75,8 @@ object Cli {
     opt[String]("target-port")
       .optional()
       .text("DEPRECATED: this option is no longer used and has no effect")
+      .action(reportUsageOfDeprecatedOption("--target-port"))
+      .hidden()
 
     opt[String]("pem")
       .optional()
@@ -93,24 +103,21 @@ object Cli {
               |Defaults to 1.0. Use numbers higher than 1.0 to make test timeouts more lax,
               |use numbers lower than 1.0 to make test timeouts more strict.""".stripMargin)
 
-    opt[Double](name = "load-scale-factor")
+    opt[String](name = "load-scale-factor")
       .optional()
-      .action((v, c) => c.copy(loadScaleFactor = v))
-      .text("""Scale factor for the load used in scale test suites. Useful to adapt the load
-              |depending on the environment and the Ledger implementation under test.
-              |Defaults to 1.0. Use numbers higher than 1.0 to increase the load,
-              |use numbers lower than 1.0 to decrease the load.""".stripMargin)
+      .text("DEPRECATED: this option is no longer used and has no effect")
+      .action(reportUsageOfDeprecatedOption("--load-scale-factor"))
+      .hidden()
 
     opt[Int](name = "concurrent-test-runs")
       .optional()
       .action((v, c) => c.copy(concurrentTestRuns = v))
-      .text("""Number of tests to run concurrently. Defaults to the number of available
-              |processors.""".stripMargin)
+      .text("Number of tests to run concurrently. Defaults to the number of available processors")
 
     opt[Unit]("verbose")
       .abbr("v")
       .action((_, c) => c.copy(verbose = true))
-      .text("Prints full stacktraces on failures.")
+      .text("Prints full stack traces on failures.")
 
     opt[Unit]("must-fail")
       .action((_, c) => c.copy(mustFail = true))
@@ -146,11 +153,13 @@ object Cli {
     opt[Path]("perf-tests-report")
       .action((inc, c) => c.copy(performanceTestsReport = Some(inc)))
       .optional()
-      .text("""The path of the the benchmark report file produced by performance tests (default: stdout).""")
+      .text(
+        "The path of the the benchmark report file produced by performance tests (default: stdout).")
 
     opt[Unit]("all-tests")
-      .action((_, c) => c.copy(allTests = true))
-      .text("""Run all default and optional tests. Respects the --exclude flag.""")
+      .text("DEPRECATED: All tests are always run by default.")
+      .action(reportUsageOfDeprecatedOption("--all-tests"))
+      .hidden()
 
     opt[Unit]("shuffle-participants")
       .action((_, c) => c.copy(shuffleParticipants = true))

ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/Config.scala
@@ -15,7 +15,6 @@ final case class Config(
     mustFail: Boolean,
     verbose: Boolean,
     timeoutScaleFactor: Double,
-    loadScaleFactor: Double,
     concurrentTestRuns: Int,
     extract: Boolean,
     tlsConfig: Option[TlsConfiguration],
@@ -25,7 +24,6 @@ final case class Config(
     performanceTestsReport: Option[Path],
     listTests: Boolean,
     listTestSuites: Boolean,
-    allTests: Boolean,
     shuffleParticipants: Boolean,
     partyAllocation: PartyAllocationConfiguration,
 )
@@ -37,7 +35,6 @@ object Config {
     mustFail = false,
     verbose = false,
     timeoutScaleFactor = 1.0,
-    loadScaleFactor = 1.0,
     concurrentTestRuns = Runtime.getRuntime.availableProcessors(),
     extract = false,
     tlsConfig = None,
@@ -47,7 +44,6 @@ object Config {
     performanceTestsReport = None,
     listTests = false,
     listTestSuites = false,
-    allTests = false,
     shuffleParticipants = false,
     partyAllocation = PartyAllocationConfiguration.ClosedWorldWaitingForAllParticipants,
   )

ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/Tests.scala
@@ -11,54 +11,41 @@ import com.daml.ledger.api.testtool.tests._
 import org.slf4j.LoggerFactory
 
 object Tests {
-  type Tests = Map[String, LedgerTestSuite]
 
-  /**
-    * These tests are safe to be run concurrently and are
-    * always run by default, unless otherwise specified.
-    */
-  val default: Tests = Seq(
-    new ActiveContractsServiceIT,
-    new CommandServiceIT,
-    new CommandSubmissionCompletionIT,
-    new CommandDeduplicationIT,
-    new ContractKeysIT,
-    new DivulgenceIT,
-    new HealthServiceIT,
-    new IdentityIT,
-    new LedgerConfigurationServiceIT,
-    new PackageManagementServiceIT,
-    new PackageServiceIT,
-    new PartyManagementServiceIT,
-    new SemanticTests,
-    new TransactionServiceIT,
-    new WitnessesIT,
-    new WronglyTypedContractIdIT,
-    new ClosedWorldIT,
-  ).map(suite => (suite.name, suite)).toMap
-
-  /**
-    * These tests can:
-    * - change the global state of the ledger, or
-    * - be especially resource intensive (by design)
-    *
-    * These are consequently not run unless otherwise specified.
-    */
-  def optional(config: Config): Tests =
-    Seq(
+  val all: Vector[LedgerTestSuite] =
+    Vector(
+      new ActiveContractsServiceIT,
+      new ClosedWorldIT,
+      new CommandServiceIT,
+      new CommandSubmissionCompletionIT,
+      new CommandDeduplicationIT,
       new ConfigManagementServiceIT,
-      new LotsOfPartiesIT,
-      new TransactionScaleIT(config.loadScaleFactor),
-    ).map(suite => (suite.name, suite)).toMap
+      new ContractKeysIT,
+      new DivulgenceIT,
+      new HealthServiceIT,
+      new IdentityIT,
+      new LedgerConfigurationServiceIT,
+      new PackageManagementServiceIT,
+      new PackageServiceIT,
+      new PartyManagementServiceIT,
+      new SemanticTests,
+      new TransactionServiceIT,
+      new WitnessesIT,
+      new WronglyTypedContractIdIT,
+    )
 
-  def all(config: Config): Tests = default ++ optional(config)
+  val retired: Vector[LedgerTestSuite] =
+    Vector(
+      new LotsOfPartiesIT,
+      new TransactionScaleIT,
+    )
 
   /**
     * These are performance envelope tests that also provide benchmarks and are always run
     * sequentially; they also must be specified explicitly with --perf-tests and will exclude
     * all other tests.
     */
-  def performanceTests(path: Option[Path]): Tests = {
+  def performanceTests(path: Option[Path]): Map[String, LedgerTestSuite] = {
     val reporter =
       (key: String, value: Double) =>
         path

commit b09a5b1e0b61e304983544a7df0733498daeb48b

Fix fine-grained exclusions in ledger-api-test-tool
31995ee00073b446f7361016fc22d0fb0f320ce0 accidentally changed
`allTestCaseNames` to be a list of test suite names instead of a list
of test case names. That breaks fine-grained exclusions that we use in
our compatibility tests. I’ve also added a very basic test that both
the coarse-grained and fine-grained syntax is supported.

changelog_begin
changelog_end

ledger/ledger-api-test-tool/BUILD.bazel
@@ -193,3 +193,18 @@ conformance_test(
         "--target-port=THIS_OPTION_IS_DEPRECATED_AND_HAS_NO_EFFECT",
     ],
 )
+
+# Test that both --include ContractKeysIT as well as --include ContractKeysIT:CKFetchOrLookup
+# are supporte.
+conformance_test(
+    name = "test-name-syntax",
+    server = "//ledger/ledger-on-memory:app",
+    server_args = [
+        "--contract-id-seeding=testing-weak",
+        "--participant participant-id=test-name,port=6865",
+    ],
+    test_tool_args = [
+        "--include=ContractKeysIT",
+        "--include=ContractKeysIT:CKFetchOrLookup",
+    ],
+)

ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/LedgerApiTestTool.scala
@@ -107,7 +107,8 @@ object LedgerApiTestTool {
       sys.exit(0)
     }
 
-    val allTestCaseNames: Set[String] = (Tests.all ++ Tests.retired).map(_.name).toSet
+    val allTestCaseNames: Set[String] =
+      (Tests.all ++ Tests.retired).flatMap(_.tests).map(_.name).toSet
     val missingTests = (config.included ++ config.excluded).filterNot(prefix =>
       allTestCaseNames.exists(_.startsWith(prefix)))
     if (missingTests.nonEmpty) {