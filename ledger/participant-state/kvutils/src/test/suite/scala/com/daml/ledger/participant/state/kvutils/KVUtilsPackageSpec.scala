3f2938ecf6d641ed6bdbf1715cc78dd159ad3ba1
fabiotudone-da <fabio.tudone@digitalasset.com>
2020-07-14 11:29:07 +0200

    Add KVUtils pre-execution round-trip test (#6671)
    * Round-trip pre-execution tests for all committers
    
    * Address review comments
    
    * Address review comments

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/KVUtilsPackageSpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/KVUtilsPackageSpec.scala
@@ -55,11 +55,23 @@ class KVUtilsPackageSpec extends WordSpec with Matchers with BazelRunfiles {
 
     "be able to submit model-test.dar" in KVTest.runTest {
       for {
-        // NOTE(JM): 'runTest' always uploads 'simpleArchive' by default.
         logEntry <- submitArchives("model-test-submission", testStablePackages.all: _*).map(_._2)
       } yield {
         logEntry.getPayloadCase shouldEqual DamlLogEntry.PayloadCase.PACKAGE_UPLOAD_ENTRY
         logEntry.getPackageUploadEntry.getArchivesCount shouldEqual testStablePackages.all.length
+      }
+    }
+
+    "be able to pre-execute model-test.dar" in KVTest.runTest {
+      for {
+        preExecutionResult <- preExecuteArchives(
+          "model-test-submission",
+          testStablePackages.all: _*)
+          .map(_._2)
+        actualLogEntry = preExecutionResult.successfulLogEntry
+      } yield {
+        actualLogEntry.getPayloadCase shouldEqual DamlLogEntry.PayloadCase.PACKAGE_UPLOAD_ENTRY
+        actualLogEntry.getPackageUploadEntry.getArchivesCount shouldEqual testStablePackages.all.length
       }
     }
 
@@ -81,11 +93,10 @@ class KVUtilsPackageSpec extends WordSpec with Matchers with BazelRunfiles {
           DamlLogEntry.PayloadCase.PACKAGE_UPLOAD_REJECTION_ENTRY
         logEntry1.getPackageUploadRejectionEntry.getReasonCase shouldEqual
           DamlPackageUploadRejectionEntry.ReasonCase.DUPLICATE_SUBMISSION
-
       }
     }
 
-    "metrics get updated" in KVTest.runTestWithSimplePackage() {
+    "update metrics" in KVTest.runTestWithSimplePackage() {
       for {
         //Submit archive twice to force one acceptance and one rejection on duplicate
         _ <- submitArchives("simple-archive-submission-1", simpleArchive).map(_._2)
@@ -97,6 +108,5 @@ class KVUtilsPackageSpec extends WordSpec with Matchers with BazelRunfiles {
         metrics.daml.kvutils.committer.runTimer("package_upload").getCount should be >= 1L
       }
     }
-
   }
 }
