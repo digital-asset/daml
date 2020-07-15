a8c62b02698866cb8b3914822a9cee8b519a3b31
Stephen Compall <stephen.compall@daml.com>
2020-07-15 08:53:19 -0400

    rename SqlSequence.Elt to SqlSequence.Element
    - suggested by @stefanobaghino-da; thanks

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/SqlSequence.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/store/dao/events/SqlSequence.scala
@@ -12,15 +12,15 @@ import java.sql.Connection
 object SqlSequence {
 
   /** A sequence of `SimpleSql`s, terminating in A. */
-  type T[A] = Free[Elt, A]
+  type T[A] = Free[Element, A]
 
   // this representation is just trampolined Reader, but exposing that
   // would be unsound because it is _not_ distributive, and anyway
   // we may want to make the representation more explicit for more complex
   // analysis (e.g. applying polymorphic transforms to all contained SimpleSqls...)
-  final class Elt[+A] private[SqlSequence] (private[SqlSequence] val run: Connection => A)
+  final class Element[+A] private[SqlSequence] (private[SqlSequence] val run: Connection => A)
 
-  object Elt {
+  object Element {
     implicit final class syntax[A](private val self: T[A]) extends AnyVal {
       def executeSql(implicit conn: Connection): A = {
         @annotation.tailrec
@@ -33,16 +33,16 @@ object SqlSequence {
       }
     }
 
-    implicit val covariant: Functor[Elt] = new Functor[Elt] {
-      override def map[A, B](fa: Elt[A])(f: A => B) = new Elt(run = fa.run andThen f)
+    implicit val covariant: Functor[Element] = new Functor[Element] {
+      override def map[A, B](fa: Element[A])(f: A => B) = new Element(run = fa.run andThen f)
     }
   }
 
   def apply[A](s: SimpleSql[_], p: ResultSetParser[A]): T[A] =
-    Free liftF new Elt(implicit conn => s as p)
+    Free liftF new Element(implicit conn => s as p)
 
   def vector[A](s: SimpleSql[Row], p: RowParser[A]): T[Vector[A]] =
-    Free liftF new Elt(implicit conn => s asVectorOf p)
+    Free liftF new Element(implicit conn => s asVectorOf p)
 
   def point[A](a: A): T[A] = Free point a
 }
