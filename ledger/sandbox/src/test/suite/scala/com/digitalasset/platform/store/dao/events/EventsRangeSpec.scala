eb7452d9f8c31803a85825cb10f02570d150db5d
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-07-15 17:29:21 -0400

    EventsRange.isEmpty test case

---
+++ ledger/sandbox/src/test/suite/scala/com/digitalasset/platform/store/dao/events/EventsRangeSpec.scala
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+package com.daml.platform.store.dao.events
+
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
+
+class EventsRangeSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
+
+  implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
+    PropertyCheckConfiguration(minSuccessful = 1000)
+
+  behavior of EventsRange.getClass.getSimpleName
+
+  "isEmpty" should "work" in forAll(eventsRangeGen) { range =>
+    val expected = range.startExclusive >= range.endInclusive
+    EventsRange.isEmpty(range) shouldBe expected
+  }
+
+  private def eventsRangeGen: Gen[EventsRange[Int]] =
+    for {
+      a <- Arbitrary.arbitrary[Int]
+      b <- Arbitrary.arbitrary[Int]
+    } yield EventsRange(a, b)
+}
