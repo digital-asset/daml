062d4e6ee010a41c02ac2ce30773e3f81c693d57
Fabio Tudone <fabio.tudone@digitalasset.com>
2020-07-20 12:03:47 +0200

    Document `StateAccessingValidatingCommitter`

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/StateAccessingValidatingCommitter.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/StateAccessingValidatingCommitter.scala
@@ -8,6 +8,12 @@ import com.daml.ledger.participant.state.v1.{ParticipantId, SubmissionResult}
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+  * A committer that orchestrates committing to a ledger after validating submissions and directly accesses the ledger
+  * state as needed.
+  *
+  * @tparam LogResult type of the offset used for a log entry.
+  */
 trait StateAccessingValidatingCommitter[LogResult] {
   def commit(
       correlationId: String,
