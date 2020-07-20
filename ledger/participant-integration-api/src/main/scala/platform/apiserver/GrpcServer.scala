714ef9d6d76e2bd834946d933be620be5a9be8b1
Samir Talwar <samir.talwar@digitalasset.com>
2020-07-20 15:36:29 +0200

    participant-integration-api: `GrpcServerOwner` -> `GrpcServer.Owner`.
    Mostly so I can create a test class named `GrpcServerSpec`.

--- ledger/participant-integration-api/src/main/scala/platform/apiserver/GrpcServer.scala
+++ ledger/participant-integration-api/src/main/scala/platform/apiserver/GrpcServer.scala
@@ -8,55 +8,63 @@ import java.net.{BindException, InetAddress, InetSocketAddress}
 import java.util.concurrent.TimeUnit.SECONDS
 
 import com.daml.metrics.Metrics
-import com.daml.platform.apiserver.GrpcServerOwner._
 import com.daml.ports.Port
 import com.daml.resources.{Resource, ResourceOwner}
 import com.google.protobuf.Message
-import io.grpc.netty.NettyServerBuilder
 import io.grpc._
+import io.grpc.netty.NettyServerBuilder
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.handler.ssl.SslContext
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NoStackTrace
 
-final class GrpcServerOwner(
-    address: Option[String],
-    desiredPort: Port,
-    maxInboundMessageSize: Int,
-    sslContext: Option[SslContext] = None,
-    interceptors: List[ServerInterceptor] = List.empty,
-    metrics: Metrics,
-    eventLoopGroups: ServerEventLoopGroups,
-    services: Iterable[BindableService],
-) extends ResourceOwner[Server] {
-  override def acquire()(implicit executionContext: ExecutionContext): Resource[Server] = {
-    val host = address.map(InetAddress.getByName).getOrElse(InetAddress.getLoopbackAddress)
-    Resource(Future {
-      val builder = NettyServerBuilder.forAddress(new InetSocketAddress(host, desiredPort.value))
-      builder.sslContext(sslContext.orNull)
-      builder.channelType(classOf[NioServerSocketChannel])
-      builder.permitKeepAliveTime(10, SECONDS)
-      builder.permitKeepAliveWithoutCalls(true)
-      builder.directExecutor()
-      builder.maxInboundMessageSize(maxInboundMessageSize)
-      interceptors.foreach(builder.intercept)
-      builder.intercept(new MetricsInterceptor(metrics))
-      eventLoopGroups.populate(builder)
-      services.foreach { service =>
-        builder.addService(service)
-        toLegacyService(service).foreach(builder.addService)
-      }
-      val server = builder.build()
-      try {
-        server.start()
-      } catch {
-        case e: IOException if e.getCause != null && e.getCause.isInstanceOf[BindException] =>
-          throw new UnableToBind(desiredPort, e.getCause)
-      }
-      server
-    })(server => Future(server.shutdown().awaitTermination()))
+object GrpcServer {
+
+  final class Owner(
+      address: Option[String],
+      desiredPort: Port,
+      maxInboundMessageSize: Int,
+      sslContext: Option[SslContext] = None,
+      interceptors: List[ServerInterceptor] = List.empty,
+      metrics: Metrics,
+      eventLoopGroups: ServerEventLoopGroups,
+      services: Iterable[BindableService],
+  ) extends ResourceOwner[Server] {
+    override def acquire()(implicit executionContext: ExecutionContext): Resource[Server] = {
+      val host = address.map(InetAddress.getByName).getOrElse(InetAddress.getLoopbackAddress)
+      Resource(Future {
+        val builder = NettyServerBuilder.forAddress(new InetSocketAddress(host, desiredPort.value))
+        builder.sslContext(sslContext.orNull)
+        builder.channelType(classOf[NioServerSocketChannel])
+        builder.permitKeepAliveTime(10, SECONDS)
+        builder.permitKeepAliveWithoutCalls(true)
+        builder.directExecutor()
+        builder.maxInboundMessageSize(maxInboundMessageSize)
+        interceptors.foreach(builder.intercept)
+        builder.intercept(new MetricsInterceptor(metrics))
+        eventLoopGroups.populate(builder)
+        services.foreach { service =>
+          builder.addService(service)
+          toLegacyService(service).foreach(builder.addService)
+        }
+        val server = builder.build()
+        try {
+          server.start()
+        } catch {
+          case e: IOException if e.getCause != null && e.getCause.isInstanceOf[BindException] =>
+            throw new UnableToBind(desiredPort, e.getCause)
+        }
+        server
+      })(server => Future(server.shutdown().awaitTermination()))
+    }
   }
+
+  final class UnableToBind(port: Port, cause: Throwable)
+      extends RuntimeException(
+        s"The API server was unable to bind to port $port. Terminate the process occupying the port, or choose a different one.",
+        cause)
+      with NoStackTrace
 
   // This exposes the existing services under com.daml also under com.digitalasset.
   // This is necessary to allow applications built with an earlier version of the SDK
@@ -88,14 +96,5 @@ final class GrpcServerOwner(
       Option(digitalassetDef.build())
     } else None
   }
-}
-
-object GrpcServerOwner {
-
-  final class UnableToBind(port: Port, cause: Throwable)
-      extends RuntimeException(
-        s"The API server was unable to bind to port $port. Terminate the process occupying the port, or choose a different one.",
-        cause)
-      with NoStackTrace
 
 }
