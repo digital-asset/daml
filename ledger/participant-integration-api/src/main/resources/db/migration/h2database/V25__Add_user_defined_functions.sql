5675e20f1333f8f29588c4bbcaf78a9498f7a697
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-07-21 12:57:33 -0400

    rebasing

--- ledger/participant-integration-api/src/main/resources/db/migration/h2database/V25__Add_user_defined_functions.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/h2database/V25__Add_user_defined_functions.sql
@@ -2,16 +2,14 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 ---------------------------------------------------------------------------------------------------
--- V24: Add user-defined functions
+-- V25: Add user-defined functions
 --
 -- Add `da_varchar_arrays_intersection` returns intersection of two passed varchar arrays (`varchar[]`).
 -- Add `da_do_varchar_arrays_intersect` returns `boolean` if two passed varchar arrays (`varchar[]`) has intersection.
 ---------------------------------------------------------------------------------------------------
 
-create function da_varchar_arrays_intersection(varchar[], varchar[]) returns varchar[] as $$
-    select array(select unnest($1) intersect select unnest($2))
-$$ language SQL;
+drop alias array_intersection;
 
-create function da_do_varchar_arrays_intersect(varchar[], varchar[]) returns boolean as $$
-    select $1 && $2
-$$ language SQL;
+create alias da_varchar_arrays_intersection for "com.daml.platform.store.dao.events.SqlFunctions.varcharArraysIntersection";
+
+create alias da_do_varchar_arrays_intersect for "com.daml.platform.store.dao.events.SqlFunctions.doVarcharArraysIntersect";
--- ledger/participant-integration-api/src/main/resources/db/migration/postgres/V36__Add_user_defined_functions.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/postgres/V36__Add_user_defined_functions.sql
@@ -2,14 +2,16 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 ---------------------------------------------------------------------------------------------------
--- V34: Add user-defined functions
+-- V36: Add user-defined functions
 --
 -- Add `da_varchar_arrays_intersection` returns intersection of two passed varchar arrays (`varchar[]`).
 -- Add `da_do_varchar_arrays_intersect` returns `boolean` if two passed varchar arrays (`varchar[]`) has intersection.
 ---------------------------------------------------------------------------------------------------
 
-drop alias array_intersection;
+create function da_varchar_arrays_intersection(varchar[], varchar[]) returns varchar[] as $$
+    select array(select unnest($1) intersect select unnest($2))
+$$ language SQL;
 
-create alias da_varchar_arrays_intersection for "com.daml.platform.store.dao.events.SqlFunctions.varcharArraysIntersection";
-
-create alias da_do_varchar_arrays_intersect for "com.daml.platform.store.dao.events.SqlFunctions.doVarcharArraysIntersect";
+create function da_do_varchar_arrays_intersect(varchar[], varchar[]) returns boolean as $$
+    select $1 && $2
+$$ language SQL;
