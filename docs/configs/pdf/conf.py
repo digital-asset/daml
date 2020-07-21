ee74551642313525827738a15878b3338963d9e9
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-07-21 17:14:33 +0200

    Add previous/next buttons to the documentation (#6811)
    fixes #6701
    
    changelog_begin
    changelog_end

--- docs/configs/pdf/conf.py
+++ docs/configs/pdf/conf.py
@@ -107,6 +107,8 @@ html_static_path = ['_static']
 # Don't show "powered by sphinx"
 html_show_sphinx = False
 
+html_show_copyright = False
+
 # Don't display the link to the sources
 html_show_sourcelink = False
 
