2c15ae15e57e21aa2c1c4b17932cd408dad2249f
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-07-21 17:05:09 -0400

    Test getting service account before request

--- triggers/service/src/test/scala/com/digitalasset/daml/lf/engine/trigger/auth/client/AuthServiceClientTest.scala
+++ triggers/service/src/test/scala/com/digitalasset/daml/lf/engine/trigger/auth/client/AuthServiceClientTest.scala
@@ -5,12 +5,12 @@ package com.daml.lf.engine.trigger.auth.client
 
 import akka.actor.ActorSystem
 import com.daml.grpc.adapter.{AkkaExecutionSequencerPool, ExecutionSequencerFactory}
-import org.scalatest.concurrent.Eventually
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
 import org.scalatest.{AsyncFlatSpec, Matchers}
 
 import scala.concurrent.ExecutionContext
 
-class AuthServiceClientTest extends AsyncFlatSpec with Eventually with Matchers {
+class AuthServiceClientTest extends AsyncFlatSpec with Eventually with Matchers with ScalaFutures {
 
   def testId: String = this.getClass.getSimpleName
   implicit val system: ActorSystem = ActorSystem(testId)
@@ -35,6 +35,17 @@ class AuthServiceClientTest extends AsyncFlatSpec with Eventually with Matchers 
         _ <- cred.credId should equal(credId.credId)
         ledgerAccessToken <- authServiceClient.login(cred)
         _ <- ledgerAccessToken.token should not be empty
+      } yield succeed
+    }
+
+  it should "fail to get service account without a request" in
+    AuthServiceFixture.withAuthServiceClient(testId) { authServiceClient =>
+      for {
+        authServiceToken <- authServiceClient.authorize("username", "password")
+        _ <- authServiceToken.token should not be empty
+        getSa = authServiceClient.getServiceAccount(authServiceToken)
+        error <- getSa.failed
+        _ <- assert(error.isInstanceOf[NoSuchElementException])
       } yield succeed
     }
 }
