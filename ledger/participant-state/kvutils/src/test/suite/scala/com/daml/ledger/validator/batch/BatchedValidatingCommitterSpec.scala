263c267f004d0ba09675be628b293ff1294da879
Fabio Tudone <fabio.tudone@digitalasset.com>
2020-07-21 18:07:06 +0200

    Fix `BatchedValidatingCommitterSpec`

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/batch/BatchedValidatingCommitterSpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/batch/BatchedValidatingCommitterSpec.scala
@@ -9,10 +9,16 @@ import akka.stream.Materializer
 import com.daml.ledger.api.testing.utils.AkkaBeforeAndAfterAll
 import com.daml.ledger.participant.state.v1.{ParticipantId, SubmissionResult}
 import com.daml.ledger.validator.TestHelper.aParticipantId
-import com.daml.ledger.validator.{CommitStrategy, DamlLedgerStateReader, LedgerStateAccess}
+import com.daml.ledger.validator.{
+  CommitStrategy,
+  DamlLedgerStateReader,
+  LedgerStateAccess,
+  LedgerStateOperations
+}
 import com.google.protobuf.ByteString
 import org.mockito.ArgumentMatchers.{any, anyString}
 import org.mockito.Mockito.when
+import org.mockito.stubbing.OngoingStubbing
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{AsyncWordSpec, Matchers}
 
@@ -26,20 +32,17 @@ class BatchedValidatingCommitterSpec
   "commit" should {
     "return Acknowledged in case of success" in {
       val mockValidator = mock[BatchedSubmissionValidator[Unit]]
-      when(
-        mockValidator.validateAndCommit(
-          any[ByteString](),
-          anyString(),
-          any[Instant](),
-          any[ParticipantId](),
-          any[DamlLedgerStateReader](),
-          any[CommitStrategy[Unit]]())(any[Materializer](), any[ExecutionContext]()))
+      whenValidateAndCommit(mockValidator)
         .thenReturn(Future.unit)
       val instance =
         BatchedValidatingCommitter[Unit](() => Instant.now(), mockValidator)
 
       instance
-        .commit("", ByteString.EMPTY, aParticipantId, mock[LedgerStateAccess[Unit]])
+        .commit(
+          correlationId = "",
+          submissionEnvelope = ByteString.EMPTY,
+          submittingParticipantId = aParticipantId,
+          ledgerStateAccess = createLedgerStateAccessMock())
         .map { actual =>
           actual shouldBe SubmissionResult.Acknowledged
         }
@@ -47,22 +50,40 @@ class BatchedValidatingCommitterSpec
 
     "return InternalError in case of an exception" in {
       val mockValidator = mock[BatchedSubmissionValidator[Unit]]
-      when(
-        mockValidator.validateAndCommit(
-          any[ByteString](),
-          anyString(),
-          any[Instant](),
-          any[ParticipantId](),
-          any[DamlLedgerStateReader](),
-          any[CommitStrategy[Unit]]())(any[Materializer](), any[ExecutionContext]()))
+      whenValidateAndCommit(mockValidator)
         .thenReturn(Future.failed(new IllegalArgumentException("Validation failure")))
       val instance = BatchedValidatingCommitter[Unit](() => Instant.now(), mockValidator)
 
       instance
-        .commit("", ByteString.EMPTY, aParticipantId, mock[LedgerStateAccess[Unit]])
+        .commit(
+          correlationId = "",
+          submissionEnvelope = ByteString.EMPTY,
+          submittingParticipantId = aParticipantId,
+          ledgerStateAccess = createLedgerStateAccessMock())
         .map { actual =>
           actual shouldBe SubmissionResult.InternalError("Validation failure")
         }
     }
   }
+
+  private def whenValidateAndCommit(
+      mockValidator: BatchedSubmissionValidator[Unit]): OngoingStubbing[Future[Unit]] =
+    when(
+      mockValidator.validateAndCommit(
+        any[ByteString](),
+        anyString(),
+        any[Instant](),
+        any[ParticipantId](),
+        any[DamlLedgerStateReader](),
+        any[CommitStrategy[Unit]]())(any[Materializer](), any[ExecutionContext]()))
+
+  private def createLedgerStateAccessMock(): LedgerStateAccess[Unit] = {
+    val mockLedgerStateAccess = mock[LedgerStateAccess[Unit]]
+    when(mockLedgerStateAccess.inTransaction(any[LedgerStateTransaction]()))
+      .thenAnswer(invocation =>
+        invocation.getArgument[LedgerStateTransaction](0)(mock[LedgerStateOperations[Unit]]))
+    mockLedgerStateAccess
+  }
+
+  private type LedgerStateTransaction = LedgerStateOperations[Unit] => Future[Unit]
 }
