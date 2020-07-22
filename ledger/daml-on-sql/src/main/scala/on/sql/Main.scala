b5d1349afcc41a1c99a8a596c9f623e9109e2ac1
Samir Talwar <samir.talwar@digitalasset.com>
2020-07-22 10:44:55 +0200

    daml-on-sql: Fail if a JDBC URL is not provided or not for PostgreSQL.

--- ledger/daml-on-sql/src/main/scala/on/sql/Main.scala
+++ ledger/daml-on-sql/src/main/scala/on/sql/Main.scala
@@ -11,9 +11,16 @@ import com.daml.resources.ProgramResource
 object Main {
   def main(args: Array[String]): Unit = {
     val config = new Cli(SandboxServer.defaultConfig).parse(args).getOrElse(sys.exit(1))
+    if (config.jdbcUrl.isEmpty) {
+      throw new InvalidConfigException("The JDBC URL is mandatory.")
+    }
+    if (config.jdbcUrl.exists(!_.startsWith("jdbc:postgresql://"))) {
+      throw new InvalidConfigException(
+        s"The JDBC URL, '${config.jdbcUrl.get}', is invalid. DAML-on-SQL only supports PostgreSQL.")
+    }
     if (!config.implicitPartyAllocation) {
       throw new InvalidConfigException(
-        "This version of Sandbox does not support disabling implicit party allocation.")
+        "You cannot disable implicit party allocation in DAML-on-SQL.")
     }
     config.logLevel.foreach(GlobalLogLevel.set)
     new ProgramResource(SandboxServer.owner(config)).run()
