3ed0a6ec35b94e7fc5e6c6c3a07eefcfb9095ebf
Stephen Compall <stephen.compall@daml.com>
2020-07-22 18:05:43 -0400

    FilterRelation is used in a private[dao] context
    - you won't get a warning for this because aliases are expanded before
      this is checked, so the method can still be called, you simply can't
      use the same type name used in the written signature

--- ledger/participant-integration-api/src/main/scala/platform/store/dao/events/package.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/dao/events/package.scala
@@ -39,7 +39,7 @@ package object events {
   private[events] type WitnessRelation[A] = lfdata.Relation.Relation[A, Party]
   private[events] type DisclosureRelation = WitnessRelation[NodeId]
   private[events] type DivulgenceRelation = WitnessRelation[ContractId]
-  private[events] type FilterRelation = lfdata.Relation.Relation[Party, lfdata.Ref.Identifier]
+  private[dao] type FilterRelation = lfdata.Relation.Relation[Party, lfdata.Ref.Identifier]
   private[events] val Relation = lfdata.Relation.Relation
 
   import com.daml.lf.crypto
