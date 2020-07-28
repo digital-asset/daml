11c4fe07276ced64583545e963a7af1a8c2f226d
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-07-28 13:44:29 +0200

    daml2js: Memoize lazily constructed decoders (#6892)
    Currently, if you have a record type `T` with a field of type, say,
    `Optional S` and you're decoding a list of type `[T]`, then the decoder
    for `S` has to be reconstructed for each element of the list. This is
    because the `lazy` combinator from the `json-type-validation` does not
    memoize the decoder it receives as a thunk.
    
    This PR adds a new combinator `lazyMemo` which behaves like `lazy` but
    also memoizes the decoder on its first invocation. All use sites of the
    old `lazy` combinator are then replaced with `lazyMemo`.
    
    We could consider upstreaming `lazyMemo` but I'm not sure how much
    effort this is given that `json-type-validation` seems to be in
    maintenance mode rather than active development.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- language-support/ts/daml-types/index.test.ts
+++ language-support/ts/daml-types/index.test.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { Optional, Text } from './index';
+import { Optional, Text, memo } from './index';
 
 describe('@daml/types', () => {
   it('optional', () => {
@@ -21,4 +21,15 @@ describe('@daml/types', () => {
     expect(dict.decoder().run([[]]).ok).toBe(false);
     expect(dict.decoder().run([null]).ok).toBe(false);
   });
+});
+
+test('memo', () => {
+  let x = 0;
+  const f = memo(() => {
+    x += 1;
+    return x;
+  });
+  expect(f()).toBe(1);
+  expect(f()).toBe(1);
+  expect(x).toBe(1);
 });
