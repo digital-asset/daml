cb216113a19ed19c5b2ed0ce06203252aa26f3f5
Jost Berthold <jost.berthold@digitalasset.com>
2020-07-29 12:51:28 +1000

    HOTFIX use a custom Setup to generate hs from protobuf

--- build_hs_lf_tooling.sh
+++ build_hs_lf_tooling.sh
@@ -40,29 +40,95 @@ DIR=$(mktemp -d)
 mkdir -p "$DIR/protobuf/com/daml"
 cp -RL "daml-lf/archive/src/main/protobuf/com/daml/daml_lf_dev" "$DIR/protobuf/com/daml/"
 # generate code from protobuf using the matching tool, as a configure script
-cat <<EOF >"$DIR/configure"
-mkdir src
-for file in \$(ls protobuf/com/daml/daml_lf_dev/ | grep 'proto\$'); do
-     echo \$file
-     compile-proto-file --includeDir protobuf \
-                        --out src \
-                        --proto com/daml/daml_lf_dev/\$file
-done
+cat <<EOF >"$DIR/Setup.hs"
+-- Copyright (c) 2020 The DAML Authors. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+--
+-- Parts of this code were adapted from
+--     https://hackage.haskell.org/package/proto-lens-setup-0.4.0.4
+-- which is (c) 2016 Google Inc. with a BSD-style license that can be found at
+-- https://developers.google.com/open-source/licenses/bsd
+
+{-# Language RecordWildCards #-}
+module Main where
+
+import Distribution.Simple
+import Distribution.Simple.BuildPaths (autogenPackageModulesDir)
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Utils
+    ( createDirectoryIfMissingVerbose
+    , installOrdinaryFile
+    )
+import Distribution.PackageDescription
+
+import System.FilePath -- filepath package
+
+import System.Directory
+import System.Process
+
+main :: IO ()
+main = defaultMainWithHooks $ generateHooks
+  where
+    generateHooks :: UserHooks
+    generateHooks =
+      simple { buildHook   = \p l h f -> generate l >> buildHook simple p l h f
+             , haddockHook = \p l h f -> generate l >> haddockHook simple p l h f
+             , replHook    = \p l h f args -> generate l >> replHook simple p l h f args
+             }
+
+    simple = simpleUserHooks
+
+    generate :: LocalBuildInfo -> IO ()
+    generate locInfo =
+      generateFromProtos locInfo "protobuf" (autogenPackageModulesDir locInfo)
+
+-- generates Haskell from all proto files from extra-source-files which are
+-- under a given srcDir path (used as include directory)
+generateFromProtos :: LocalBuildInfo -> FilePath -> FilePath -> IO ()
+generateFromProtos locInfo srcDir outDir = do
+  let protos = collectProtos srcDir
+  mapM_ (generateProto srcDir outDir) protos
+    where
+      collectProtos :: FilePath -> [FilePath]
+      collectProtos path =
+        map (makeRelative path)
+          . filter ((== ".proto") . takeExtension)
+          . filter (isInside path) $
+          (extraSrcFiles $ localPkgDescr locInfo)
+
+      isInside :: FilePath -> FilePath -> Bool
+      isInside path file = isRelative file &&
+                           equalFilePath file (path </> makeRelative path file)
+
+-- | runs @compile-proto-file@ for the given path @proto@, assumed relative to
+-- @imports@, output in @output@ path.
+generateProto :: FilePath -> FilePath -> FilePath -> IO ()
+generateProto imports output proto = do
+  putStrLn $ "generate from proto file " ++ proto
+  createDirectoryIfMissing True output
+  callProcess "compile-proto-file"
+    ["--includeDir", imports, "--out", output, "--proto", proto]
 EOF
-chmod +x "$DIR/configure"
 cat <<EOF > "$DIR/daml-lf-proto-types.cabal"
 cabal-version: 2.4
 name: daml-lf-proto-types
-build-type: Configure
 version: 0.1.0
 
 extra-source-files:
-  configure
-  protobuf/com/daml/daml_lf_dev/*.proto
+  protobuf/com/daml/daml_lf_dev/daml_lf.proto
+  protobuf/com/daml/daml_lf_dev/daml_lf_0.proto
+  protobuf/com/daml/daml_lf_dev/daml_lf_1.proto
+build-type: Custom
+custom-setup
+  setup-depends:
+    base,
+    Cabal,
+    directory,
+    filepath,
+    process
 
 library
   default-language: Haskell2010
-  hs-source-dirs: src
   build-depends:
     base,
     bytestring,
