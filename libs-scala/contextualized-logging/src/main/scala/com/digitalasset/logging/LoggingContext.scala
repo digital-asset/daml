b5af25bc64bdaf1b7303accd3d9522a1ec3c9a4b
Stefano Baghino <stefano.baghino@digitalasset.com>
2020-07-30 16:25:27 +0200

    Document good practices regarding the logging context

--- libs-scala/contextualized-logging/src/main/scala/com/digitalasset/logging/LoggingContext.scala
+++ libs-scala/contextualized-logging/src/main/scala/com/digitalasset/logging/LoggingContext.scala
@@ -22,11 +22,57 @@ object LoggingContext {
   ): A =
     newLoggingContext(withEnrichedLoggingContext(kv, kvs: _*)(f)(_))
 
+  /**
+    * ## Principles to follow when enriching the logging context
+    *
+    * ### Don't add values coming from a scope outside of the current method
+    *
+    * If a method receives a value as a parameter, it should trust that,
+    * if it was relevant, the caller already added this value to the context.
+    * Add values to the context as upstream as possible in the call chain.
+    * This ensures to not add duplicates, possibly using slightly different
+    * names to track the same value. The context was implemented to ensure
+    * that values did not have to be passed down the entire call stack to
+    * be logged at relevant points.
+    *
+    * ### Don't dump string representations of complex objects
+    *
+    * The purpose of the context is to be consumed by structured logging
+    * frameworks. Dumping the string representation of an object, like a
+    * Scala case class instance, means embedding some form of string
+    * formatting in another (likely to be JSON). This can be difficult
+    * to manage and parse, so stick to simple values (strings, numbers,
+    * dates, etc.) and be deliberate in adding
+    *
+    */
   def withEnrichedLoggingContext[A](kvs: Map[String, String])(f: LoggingContext => A)(
       implicit loggingContext: LoggingContext,
   ): A =
     f(loggingContext ++ kvs)
 
+  /**
+    * ## Principles to follow when enriching the logging context
+    *
+    * ### Don't add values coming from a scope outside of the current method
+    *
+    * If a method receives a value as a parameter, it should trust that,
+    * if it was relevant, the caller already added this value to the context.
+    * Add values to the context as upstream as possible in the call chain.
+    * This ensures to not add duplicates, possibly using slightly different
+    * names to track the same value. The context was implemented to ensure
+    * that values did not have to be passed down the entire call stack to
+    * be logged at relevant points.
+    *
+    * ### Don't dump string representations of complex objects
+    *
+    * The purpose of the context is to be consumed by structured logging
+    * frameworks. Dumping the string representation of an object, like a
+    * Scala case class instance, means embedding some form of string
+    * formatting in another (likely to be JSON). This can be difficult
+    * to manage and parse, so stick to simple values (strings, numbers,
+    * dates, etc.) and be deliberate in adding
+    *
+    */
   def withEnrichedLoggingContext[A](kv: (String, String), kvs: (String, String)*)(
       f: LoggingContext => A,
   )(implicit loggingContext: LoggingContext): A =
