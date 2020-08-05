b2b3254a84f76e4baf43942e1d7618f0312b87d6
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-08-05 14:09:25 -0400

    Thanks @S11001001

--- ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/scenario/HasRandomAmount.scala
+++ ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/scenario/HasRandomAmount.scala
@@ -7,7 +7,7 @@ private[scenario] trait HasRandomAmount {
   private val rng = new scala.util.Random(123456789)
 
   // can be called from two different scenarios, need to synchronize access
-  def randomAmount(): Int = {
+  final def randomAmount(): Int = {
     val x = this.synchronized { rng.nextInt(10) }
     x + 5 // [5, 15)
   }
