9aede28f779aaa8ed8bb04655d6a0af61c47ba3a
Miklos <57664299+miklos-da@users.noreply.github.com>
2020-08-05 19:08:29 +0200

    Publish artifacts required for integrity checking tools (#7024)
    * Create scala library for integrity checking tools.
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Moved integrity checking drivers into separate package.
    
    * First define the scala library then the rest.
    
    * Added missing header.
    
    * Moved all export related code to under package kvutils.tools.export.
    
    * Added missing header.
    
    * Make all binaries depend on the library and not need sources.

--- ledger/participant-state/kvutils/tools/src/main/scala/com/daml/ledger/participant/state/kvutils/tools/driver/IntegrityCheck.scala
+++ ledger/participant-state/kvutils/tools/src/main/scala/com/daml/ledger/participant/state/kvutils/tools/driver/IntegrityCheck.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.ledger.participant.state.kvutils.tools
+package com.daml.ledger.participant.state.kvutils.tools.driver
 
 import java.io.{DataInputStream, FileInputStream}
 import java.time.Duration
