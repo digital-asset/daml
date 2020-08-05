115d1996302885e14c4313d8606808d802b77a24
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-08-05 17:43:35 +0200

    Added missing header.

--- ledger/participant-state/kvutils/tools/src/main/scala/com/daml/ledger/participant/state/kvutils/tools/export/LogAppendingCommitStrategySupport.scala
+++ ledger/participant-state/kvutils/tools/src/main/scala/com/daml/ledger/participant/state/kvutils/tools/export/LogAppendingCommitStrategySupport.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.participant.state.kvutils.tools.export
 
 import com.daml.ledger.on.memory.{InMemoryLedgerStateOperations, Index}
