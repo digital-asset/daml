559b7e09609525cc9d5d8a43637c012b3eea6ea6
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-08-05 18:12:18 -0400

    AsyncQueryConstantAcs scenario
    changelog_begin
    changelog_end

--- ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/scenario/SimulationConfig.scala
+++ ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/scenario/SimulationConfig.scala
@@ -8,20 +8,19 @@ import io.gatling.http.protocol.HttpProtocolBuilder
 
 private[scenario] trait SimulationConfig {
   lazy val httpProtocol: HttpProtocolBuilder = http
-    .baseUrl(baseUrl)
-    .warmUp(warmupUrl)
+    .baseUrl(s"http://$hostAndPort")
+    .wsBaseUrl(s"ws://$hostAndPort")
+    .warmUp(s"http://$hostAndPort/v1/query")
     .inferHtmlResources()
     .acceptHeader("*/*")
     .acceptEncodingHeader("gzip, deflate")
     .authorizationHeader(s"Bearer $aliceJwt")
     .contentTypeHeader("application/json")
 
-  private val baseUrl: String = "http://localhost:7575"
-
-  private val warmupUrl: String = "http://localhost:7575/v1/query"
+  private val hostAndPort = "localhost:7575"
 
   // {"https://daml.com/ledger-api": {"ledgerId": "MyLedger", "applicationId": "foobar", "actAs": ["Alice"]}}
-  private val aliceJwt: String =
+  val aliceJwt: String =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2RhbWwuY29tL2xlZGdlci1hcGkiOnsibGVkZ2VySWQiOiJNeUxlZGdlciIsImFwcGxpY2F0aW9uSWQiOiJmb29iYXIiLCJhY3RBcyI6WyJBbGljZSJdfX0.VdDI96mw5hrfM5ZNxLyetSVwcD7XtLT4dIdHIOa9lcU"
 
 }
