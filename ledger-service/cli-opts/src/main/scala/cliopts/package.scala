194f931d37b5cc6471249d320c2a4a539cecc13d
Stephen Compall <stephen.compall@daml.com>
2020-08-11 14:39:21 -0400

    document Setter

--- ledger-service/cli-opts/src/main/scala/cliopts/package.scala
+++ ledger-service/cli-opts/src/main/scala/cliopts/package.scala
@@ -2,6 +2,9 @@ package com.daml
 
 package object cliopts {
 
-  /** A lens-style setter. */
-  type Setter[S, A] = (A => A, S) => S
+  /** A lens-style setter.  When you want nested structures to be possible, this
+    * is vastly superior to the more obvious `(B, T) => T`, because unlike that
+    * one, this form permits nesting via trivial composition.
+    */
+  type Setter[T, B] = (B => B, T) => T
 }
