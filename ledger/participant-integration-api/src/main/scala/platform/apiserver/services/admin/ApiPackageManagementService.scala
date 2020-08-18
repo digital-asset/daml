f4d64df3deff1a00b6c911dad5c1b36d60556060
Samir Talwar <samir.talwar@digitalasset.com>
2020-08-18 21:58:10 +0200

    participant-integration-api: Extract out more into SynchronousResponse.

--- ledger/participant-integration-api/src/main/scala/platform/apiserver/services/admin/ApiPackageManagementService.scala
+++ ledger/participant-integration-api/src/main/scala/platform/apiserver/services/admin/ApiPackageManagementService.scala
@@ -8,19 +8,21 @@ import java.util.UUID
 import java.util.zip.ZipInputStream
 
 import akka.stream.Materializer
+import akka.stream.scaladsl.Source
 import com.daml.daml_lf_dev.DamlLf.Archive
 import com.daml.dec.{DirectExecutionContext => DE}
-import com.daml.ledger.api.domain.PackageEntry
+import com.daml.ledger.api.domain.{LedgerOffset, PackageEntry}
 import com.daml.ledger.api.v1.admin.package_management_service.PackageManagementServiceGrpc.PackageManagementService
 import com.daml.ledger.api.v1.admin.package_management_service._
 import com.daml.ledger.participant.state.index.v2.{IndexPackagesService, IndexTransactionsService}
 import com.daml.ledger.participant.state.v1.{SubmissionId, SubmissionResult, WritePackagesService}
-import com.daml.lf.archive.DarReader
+import com.daml.lf.archive.{Dar, DarReader}
 import com.daml.logging.{ContextualizedLogger, LoggingContext}
 import com.daml.platform.api.grpc.GrpcApiService
+import com.daml.platform.apiserver.services.admin.ApiPackageManagementService._
 import com.daml.platform.server.api.validation.ErrorFactories
 import com.google.protobuf.timestamp.Timestamp
-import io.grpc.ServerServiceDefinition
+import io.grpc.{ServerServiceDefinition, StatusRuntimeException}
 
 import scala.compat.java8.FutureConverters._
 import scala.concurrent.duration.DurationInt
@@ -31,7 +33,8 @@ private[apiserver] final class ApiPackageManagementService private (
     packagesIndex: IndexPackagesService,
     transactionsService: IndexTransactionsService,
     packagesWrite: WritePackagesService,
-    materializer: Materializer)(implicit loggingContext: LoggingContext)
+    materializer: Materializer,
+)(implicit loggingContext: LoggingContext)
     extends PackageManagementService
     with GrpcApiService {
 
@@ -75,6 +78,7 @@ private[apiserver] final class ApiPackageManagementService private (
 
     // Execute subsequent transforms in the thread of the previous operation.
     implicit val executionContext: ExecutionContext = DE
+    implicit val mat: Materializer = materializer
 
     val response = for {
       dar <- DarReader { case (_, x) => Try(Archive.parseFrom(x)) }
@@ -85,36 +89,18 @@ private[apiserver] final class ApiPackageManagementService private (
           err => Future.failed(ErrorFactories.invalidArgument(err.getMessage)),
           Future.successful
         )
-      ledgerEndBeforeRequest <- transactionsService.currentLedgerEnd()
-      submissionResult <- packagesWrite.uploadPackages(submissionId, dar.all, None).toScala
-      entry <- submissionResult match {
-        case SubmissionResult.Acknowledged =>
-          SynchronousResponse.pollUntilPersisted(
-            packagesIndex
-              .packageEntries(ledgerEndBeforeRequest)
-              .collect {
-                case entry @ PackageEntry.PackageUploadAccepted(`submissionId`, _) => entry
-                case entry @ PackageEntry.PackageUploadRejected(`submissionId`, _, _) => entry
-              },
-            timeToLive,
-          )(executionContext, materializer)
-        case r @ SubmissionResult.Overloaded =>
-          Future.failed(ErrorFactories.resourceExhausted(r.description))
-        case r @ SubmissionResult.InternalError(_) =>
-          Future.failed(ErrorFactories.internal(r.reason))
-        case r @ SubmissionResult.NotSupported =>
-          Future.failed(ErrorFactories.unimplemented(r.description))
+      synchronousResponse = new SynchronousResponse(
+        transactionsService,
+        timeToLive,
+        new SynchronousResponseStrategy(packagesIndex, packagesWrite, dar),
+      )
+      _ <- synchronousResponse.submitAndWait(submissionId)
+    } yield {
+      for (archive <- dar.all) {
+        logger.info(s"Package ${archive.getHash} successfully uploaded")
       }
-      response <- entry match {
-        case _: PackageEntry.PackageUploadAccepted =>
-          for (archive <- dar.all) {
-            logger.info(s"Package ${archive.getHash} successfully uploaded")
-          }
-          Future.successful(UploadDarFileResponse())
-        case PackageEntry.PackageUploadRejected(_, _, reason) =>
-          Future.failed(ErrorFactories.invalidArgument(reason))
-      }
-    } yield response
+      UploadDarFileResponse()
+    }
 
     response.andThen(logger.logErrorsOnCall[UploadDarFileResponse])
   }
@@ -130,5 +116,33 @@ private[apiserver] object ApiPackageManagementService {
   )(implicit mat: Materializer, loggingContext: LoggingContext)
     : PackageManagementServiceGrpc.PackageManagementService with GrpcApiService =
     new ApiPackageManagementService(readBackend, transactionsService, writeBackend, mat)
+
+  private final class SynchronousResponseStrategy(
+      packagesIndex: IndexPackagesService,
+      packagesWrite: WritePackagesService,
+      dar: Dar[Archive],
+  )(implicit loggingContext: LoggingContext)
+      extends SynchronousResponse.Strategy[PackageEntry, PackageEntry.PackageUploadAccepted] {
+
+    override def submit(submissionId: SubmissionId): Future[SubmissionResult] =
+      packagesWrite.uploadPackages(submissionId, dar.all, None).toScala
+
+    override def entries(offset: LedgerOffset.Absolute): Source[PackageEntry, _] =
+      packagesIndex.packageEntries(offset)
+
+    override def accept(
+        submissionId: SubmissionId,
+    ): PartialFunction[PackageEntry, PackageEntry.PackageUploadAccepted] = {
+      case entry @ PackageEntry.PackageUploadAccepted(`submissionId`, _) => entry
+    }
+
+    override def reject(
+        submissionId: SubmissionId,
+    ): PartialFunction[PackageEntry, StatusRuntimeException] = {
+      case PackageEntry.PackageUploadRejected(`submissionId`, _, reason) =>
+        ErrorFactories.invalidArgument(reason)
+    }
+
+  }
 
 }
--- ledger/participant-integration-api/src/main/scala/platform/apiserver/services/admin/SynchronousResponse.scala
+++ ledger/participant-integration-api/src/main/scala/platform/apiserver/services/admin/SynchronousResponse.scala
@@ -5,10 +5,59 @@ package com.daml.platform.apiserver.services.admin
 
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Sink, Source}
+import com.daml.ledger.api.domain.LedgerOffset
+import com.daml.ledger.participant.state.index.v2.LedgerEndService
+import com.daml.ledger.participant.state.v1.{SubmissionId, SubmissionResult}
+import com.daml.logging.LoggingContext
+import com.daml.platform.apiserver.services.admin.SynchronousResponse.{Accepted, Rejected}
 import com.daml.platform.server.api.validation.ErrorFactories
+import io.grpc.StatusRuntimeException
 
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{ExecutionContext, Future, TimeoutException}
+
+class SynchronousResponse[Entry, AcceptedEntry](
+    ledgerEndService: LedgerEndService,
+    timeToLive: FiniteDuration,
+    strategy: SynchronousResponse.Strategy[Entry, AcceptedEntry],
+) {
+
+  def submitAndWait(submissionId: SubmissionId)(
+      implicit executionContext: ExecutionContext,
+      materializer: Materializer,
+      loggingContext: LoggingContext,
+  ): Future[AcceptedEntry] = {
+    for {
+      ledgerEndBeforeRequest <- ledgerEndService.currentLedgerEnd()
+      submissionResult <- strategy.submit(submissionId)
+      entry <- submissionResult match {
+        case SubmissionResult.Acknowledged =>
+          val isAccepted = new Accepted(strategy.accept(submissionId))
+          val isRejected = new Rejected(strategy.reject(submissionId))
+          strategy
+            .entries(ledgerEndBeforeRequest)
+            .collect {
+              case isAccepted(entry) => Future.successful(entry)
+              case isRejected(exception) => Future.failed(exception)
+            }
+            .completionTimeout(timeToLive)
+            .runWith(Sink.head)
+            .recoverWith {
+              case _: TimeoutException =>
+                Future.failed(ErrorFactories.aborted("Request timed out"))
+            }
+            .flatten
+        case r @ SubmissionResult.Overloaded =>
+          Future.failed(ErrorFactories.resourceExhausted(r.description))
+        case r @ SubmissionResult.InternalError(_) =>
+          Future.failed(ErrorFactories.internal(r.reason))
+        case r @ SubmissionResult.NotSupported =>
+          Future.failed(ErrorFactories.unimplemented(r.description))
+      }
+    } yield entry
+  }
+
+}
 
 object SynchronousResponse {
 
@@ -23,5 +72,33 @@ object SynchronousResponse {
         case _: TimeoutException =>
           Future.failed(ErrorFactories.aborted("Request timed out"))
       }
+
+  trait Strategy[Entry, AcceptedEntry] {
+
+    def submit(submissionId: SubmissionId): Future[SubmissionResult]
+
+    def entries(offset: LedgerOffset.Absolute): Source[Entry, _]
+
+    def accept(submissionId: SubmissionId): PartialFunction[Entry, AcceptedEntry]
+
+    def reject(submissionId: SubmissionId): PartialFunction[Entry, StatusRuntimeException]
+
+  }
+
+  private final class Accepted[Entry, AcceptedEntry](
+      accept: PartialFunction[Entry, AcceptedEntry],
+  ) {
+    private val liftedAccept = accept.lift
+
+    def unapply(entry: Entry): Option[AcceptedEntry] =
+      liftedAccept(entry)
+  }
+
+  private final class Rejected[Entry](reject: PartialFunction[Entry, StatusRuntimeException]) {
+    private val liftedReject = reject.lift
+
+    def unapply(entry: Entry): Option[StatusRuntimeException] =
+      liftedReject(entry)
+  }
 
 }
