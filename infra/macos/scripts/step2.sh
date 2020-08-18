5b2319e13788dc2a7b955eedda2bc468129b6b15
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2020-08-18 16:01:02 +0200

    multistep macos setup (#5768)
    multistep macos setup
    
    This updates the macOS node setup instructions to avoid repeating
    identical work and network traffic across all machines through
    initialization by building a "daily" image with all the tools and code
    we need.
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Fix 3-running-box to remount nix partition
    
    * updated scripts to use multi-step process
    
    * add copyright notices
    
    Co-authored-by: nycnewman <edward@digitalasset.com>

---
+++ infra/macos/scripts/step2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+  
+cd ~/daml/infra/macos/2-common-box
+
+GUEST_NAME=$(HOSTNAME) vagrant up
+
+vagrant package --output ~/images/initialized-$(date +%Y%m%d).box
+
+vagrant destroy -f
+
+cd ~
+./copyfile.sh images/initialized-$(date +%Y%m%d).box
+
