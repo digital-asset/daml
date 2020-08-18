0958648d26117955fb795009cea16612ce12ef3b
Samir Talwar <samir.talwar@digitalasset.com>
2020-08-18 09:18:29 +0200

    daml-on-sql: Generate a Markdown README with a script.
    This is easier to parse by human beings than the reStructuredText.

---
+++ ledger/daml-on-sql/generate-markdown-readme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -u
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+pandoc \
+  --reference-links \
+  --reference-location=section \
+  --to=markdown+backtick_code_blocks-fenced_code_attributes \
+  --output=./README.md \
+  ./README.rst
+
+# Pandoc generates unnecessary escape characters.
+sed -i 's/\\//g' ./README.md
