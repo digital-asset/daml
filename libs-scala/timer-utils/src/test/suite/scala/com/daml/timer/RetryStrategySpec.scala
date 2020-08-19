97ed3cf077e667bccad220c0b3894c4e576fbdee
Samir Talwar <samir.talwar@digitalasset.com>
2020-08-19 10:29:12 +0200

    timer-utils: Remove duplication in RetryStrategy tests.

--- libs-scala/timer-utils/src/test/suite/scala/com/daml/timer/RetryStrategySpec.scala
+++ libs-scala/timer-utils/src/test/suite/scala/com/daml/timer/RetryStrategySpec.scala
@@ -6,56 +6,37 @@ package com.daml.timer
 import java.time
 import java.util.concurrent.atomic.AtomicInteger
 
-import org.scalatest.{AsyncWordSpec, Matchers}
+import com.daml.timer.RetryStrategySpec._
+import org.scalatest.{AsyncWordSpec, Inside, Matchers}
 
-import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
-class RetryStrategySpec extends AsyncWordSpec with Matchers {
+class RetryStrategySpec extends AsyncWordSpec with Matchers with Inside {
 
   "RetryStrategy.constant" should {
     "try a number of times, with a constant delay" in {
       val retry = RetryStrategy.constant(attempts = 10, waitTime = 10.milliseconds)
-      val retryCount = new AtomicInteger()
-      val start = time.Instant.now()
       for {
-        result <- retry { (_, _) =>
-          Future {
-            if (retryCount.incrementAndGet() >= 5) {
-              "Success!"
-            } else {
-              throw new IllegalStateException("Failure!")
-            }
-          }
-        }
-        end = time.Instant.now()
+        (retryCount, result, duration) <- succeedAfter(tries = 5, retry)
       } yield {
-        result should be("Success!")
-        retryCount.get() should be(5)
-        time.Duration.between(start, end).toMillis should (be >= 50L and be <= 250L)
+        result should be(Success("Success!"))
+        retryCount should be(5)
+        duration.toMillis should (be >= 50L and be <= 250L)
       }
     }
 
     "fail if the number of attempts is exceeded" in {
       val retry = RetryStrategy.constant(attempts = 10, waitTime = 10.milliseconds)
-      val retryCount = new AtomicInteger()
-      val start = time.Instant.now()
       for {
-        result <- retry { (_, _) =>
-          Future {
-            if (retryCount.incrementAndGet() > 11) {
-              "Success!"
-            } else {
-              throw new IllegalStateException("Failure!")
-            }
-          }
-        }.failed
-        end = time.Instant.now()
+        (retryCount, result, duration) <- succeedAfter(tries = 12, retry)
       } yield {
-        result should be(an[IllegalStateException])
-        result.getMessage should be("Failure!")
-        retryCount.get() should be(11)
-        time.Duration.between(start, end).toMillis should (be >= 100L and be <= 500L)
+        inside(result) {
+          case Failure(_: FailureDuringRetry) =>
+        }
+        retryCount should be(11)
+        duration.toMillis should (be >= 100L and be <= 500L)
       }
     }
   }
@@ -63,48 +44,51 @@ class RetryStrategySpec extends AsyncWordSpec with Matchers {
   "RetryStrategy.exponentialBackoff" should {
     "try a number of times, with an exponentially-increasing delay" in {
       val retry = RetryStrategy.exponentialBackoff(attempts = 10, firstWaitTime = 10.milliseconds)
-      val retryCount = new AtomicInteger()
-      val start = time.Instant.now()
       for {
-        result <- retry { (_, _) =>
-          Future {
-            if (retryCount.incrementAndGet() >= 5) {
-              "Success!"
-            } else {
-              throw new IllegalStateException("Failure!")
-            }
-          }
-        }
-        end = time.Instant.now()
+        (retryCount, result, duration) <- succeedAfter(tries = 5, retry)
       } yield {
-        result should be("Success!")
-        retryCount.get() should be(5)
-        time.Duration.between(start, end).toMillis should (be >= 150L and be <= 250L)
+        result should be(Success("Success!"))
+        retryCount should be(5)
+        duration.toMillis should (be >= 150L and be <= 250L)
       }
     }
 
     "fail if the number of attempts is exceeded" in {
       val retry = RetryStrategy.exponentialBackoff(attempts = 5, firstWaitTime = 10.milliseconds)
-      val retryCount = new AtomicInteger()
-      val start = time.Instant.now()
       for {
-        result <- retry { (_, _) =>
-          Future {
-            if (retryCount.incrementAndGet() > 6) {
-              "Success!"
-            } else {
-              throw new IllegalStateException("Failure!")
-            }
-          }
-        }.failed
-        end = time.Instant.now()
+        (retryCount, result, duration) <- succeedAfter(tries = 7, retry)
       } yield {
-        result should be(an[IllegalStateException])
-        result.getMessage should be("Failure!")
-        retryCount.get() should be(6)
-        time.Duration.between(start, end).toMillis should (be >= 310L and be <= 500L)
+        inside(result) {
+          case Failure(_: FailureDuringRetry) =>
+        }
+        retryCount should be(6)
+        duration.toMillis should (be >= 310L and be <= 500L)
       }
     }
   }
+
+}
+
+object RetryStrategySpec {
+
+  private def succeedAfter(
+      tries: Int,
+      retry: RetryStrategy,
+  )(implicit executionContext: ExecutionContext): Future[(Int, Try[String], time.Duration)] = {
+    val retryCount = new AtomicInteger()
+    val start = time.Instant.now()
+    retry { (_, _) =>
+      Future {
+        if (retryCount.incrementAndGet() >= tries) {
+          "Success!"
+        } else {
+          throw new FailureDuringRetry
+        }
+      }
+    }.transform(Success(_))
+      .map((retryCount.get(), _, time.Duration.between(start, time.Instant.now())))
+  }
+
+  private final class FailureDuringRetry extends RuntimeException
 
 }
