1748d4a539ac4f0a71b52912f5ef88c75d4894f6
Stephen Compall <stephen.compall@daml.com>
2020-08-20 16:44:42 -0400

    most of JsonFormat[Identifier] is in companion

--- triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/Request.scala
+++ triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/Request.scala
@@ -3,7 +3,7 @@
 
 package com.daml.lf.engine.trigger
 
-import com.daml.lf.data.Ref.{DottedName, Identifier, PackageId, QualifiedName}
+import com.daml.lf.data.Ref.Identifier
 import spray.json.DefaultJsonProtocol._
 import spray.json.{JsString, JsValue, JsonFormat, RootJsonFormat, deserializationError}
 
@@ -11,20 +11,7 @@ object Request {
   implicit object IdentifierFormat extends JsonFormat[Identifier] {
     def read(value: JsValue): Identifier = value match {
       case JsString(s) =>
-        val components = s.split(":")
-        if (components.length == 3) {
-          val parsed = for {
-            pkgId <- PackageId.fromString(components(0))
-            mod <- DottedName.fromString(components(1))
-            entity <- DottedName.fromString(components(2))
-          } yield Identifier(pkgId, QualifiedName(mod, entity))
-          parsed match {
-            case Left(e) => deserializationError(e)
-            case Right(id) => id
-          }
-        } else {
-          deserializationError(s"Expected trigger identifier of the form pkgid:mod:name but got $s")
-        }
+        Identifier fromString s fold (deserializationError(_), identity)
       case _ => deserializationError("Expected trigger identifier of the form pkgid:mod:name")
     }
     def write(id: Identifier): JsValue = JsString(id.toString)
