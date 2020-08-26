509626749fafa186494a8ccc807833fdb6be8d2f
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-08-26 14:56:20 +0200

    Add a dummy OAuth2 authorization server
    This is intended for use in the trigger service integration tests not
    as an artifact that we ship to users.
    
    changelog_begin
    changelog_end

---
+++ triggers/service/auth/src/main/scala/com/daml/oauth/server/Request.scala
@@ -0,0 +1,84 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.oauth.server
+
+import akka.http.scaladsl.model.Uri
+import akka.http.scaladsl.model.Uri.Query
+import spray.json.{
+  DefaultJsonProtocol,
+  JsString,
+  JsValue,
+  JsonFormat,
+  RootJsonFormat,
+  deserializationError
+}
+
+object Request {
+
+  // https://tools.ietf.org/html/rfc6749#section-4.1.1
+  case class Authorize(
+      responseType: String,
+      clientId: String,
+      redirectUri: Uri, // optional in oauth but we require it
+      scope: Option[String],
+      state: Option[String]) {
+    def toQuery: Query = {
+      var params: Seq[(String, String)] =
+        Seq(
+          ("response_type", responseType),
+          ("client_id", clientId),
+          ("redirect_uri", redirectUri.toString))
+      scope.foreach { scope =>
+        params ++= Seq(("scope", scope))
+      }
+      state.foreach { state =>
+        params ++= Seq(("state", state))
+      }
+      Query(params: _*)
+    }
+  }
+
+  // https://tools.ietf.org/html/rfc6749#section-4.1.3
+  case class Token(
+      grantType: String,
+      code: String,
+      redirectUri: Option[Uri],
+      clientId: String,
+      clientSecret: String)
+
+}
+
+object Response {
+
+  // https://tools.ietf.org/html/rfc6749#section-4.1.2
+  case class Authorize(code: String, state: Option[String]) {
+    def toQuery: Query = state match {
+      case None => Query(("code", code))
+      case Some(state) => Query(("code", code), ("state", state))
+    }
+  }
+
+  // https://tools.ietf.org/html/rfc6749#section-4.1.1
+  case class Token(
+      accessToken: String,
+      tokenType: String,
+      expiresIn: Option[String],
+      refreshToken: Option[String],
+      scope: Option[String])
+
+}
+
+object JsonProtocol extends DefaultJsonProtocol {
+  implicit object UriFormat extends JsonFormat[Uri] {
+    def read(value: JsValue) = value match {
+      case JsString(s) => Uri(s)
+      case _ => deserializationError(s"Expected Uri string but got $value")
+    }
+    def write(uri: Uri) = JsString(uri.toString)
+  }
+  implicit val tokenReqFormat: RootJsonFormat[Request.Token] =
+    jsonFormat(Request.Token, "grant_type", "code", "redirect_uri", "client_id", "client_secret")
+  implicit val tokenRespFormat: RootJsonFormat[Response.Token] =
+    jsonFormat(Response.Token, "access_token", "token_type", "expires_in", "refresh_token", "scope")
+}
---
+++ triggers/service/auth/src/main/scala/com/daml/oauth/server/Server.scala
@@ -0,0 +1,112 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.oauth.server
+
+import java.util.UUID
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.{StatusCodes, Uri}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import com.daml.jwt.JwtSigner
+import com.daml.jwt.domain.DecodedJwt
+import com.daml.ledger.api.auth.{AuthServiceJWTCodec, AuthServiceJWTPayload}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
+
+// This is a test authorization server that implements the OAuth2 authorization code flow.
+// This is primarily intended for use in the trigger service tests but could also serve
+// as a useful ground for experimentation.
+// Given scopes of the form `actAs:$party`, the authorization server will issue
+// tokens with the respective claims. All requests will be accepted and request to
+// /authorize are immediately redirected to the redirect_uri.
+object Server {
+  private val jwtHeader = """{"alg": "HS256", "typ": "JWT"}"""
+  def start(config: Config)(implicit system: ActorSystem): Future[ServerBinding] = {
+    // To keep things as simple as possible, we use a UUID as the authorization code
+    // and in the /authorize request we already pre-compute the JWT payload based on the scope.
+    // The token request then only does a lookup and signs the token.
+    var requests = Map.empty[UUID, AuthServiceJWTPayload]
+
+    def toPayload(req: Request.Authorize): AuthServiceJWTPayload = {
+      val parties: List[String] =
+        req.scope.fold(List.empty[String])(s => List[String](s.split(" "): _*)).collect {
+          case s if s.startsWith("actAs:") => s.stripPrefix("actAs:")
+        }
+      AuthServiceJWTPayload(
+        ledgerId = Some(config.ledgerId),
+        applicationId = Some(config.applicationId),
+        // Not required by the default auth service
+        participantId = None,
+        // Only for testing, expire never.
+        exp = None,
+        // no admin claim for now.
+        admin = false,
+        actAs = parties,
+        readAs = List()
+      )
+    }
+
+    import JsonProtocol._
+
+    implicit val unmarshal: Unmarshaller[String, Uri] = Unmarshaller.strict(Uri(_))
+
+    val route = concat(
+      path("authorize") {
+        get {
+          parameters(('response_type, 'client_id, 'redirect_uri.as[Uri], 'scope ?, 'state ?))
+            .as[Request.Authorize](Request.Authorize) {
+              request =>
+                val authorizationCode = UUID.randomUUID()
+                val params =
+                  Response
+                    .Authorize(code = authorizationCode.toString, state = request.state)
+                    .toQuery
+                requests += (authorizationCode -> toPayload(request))
+                // We skip any actual consent screen since this is only intended for testing and
+                // this is outside of the scope of the trigger service anyway.
+                redirect(request.redirectUri.withQuery(params), StatusCodes.Found)
+            }
+        }
+      },
+      path("token") {
+        post {
+          entity(as[Request.Token]) {
+            request =>
+              // No validation to keep things simple
+              requests.get(UUID.fromString(request.code)) match {
+                case None => sys.exit(2)
+                case Some(payload) =>
+                  import JsonProtocol._
+                  complete(
+                    Response.Token(
+                      accessToken = JwtSigner.HMAC256
+                        .sign(
+                          DecodedJwt(jwtHeader, AuthServiceJWTCodec.compactPrint(payload)),
+                          config.jwtSecret)
+                        .getOrElse(
+                          throw new IllegalArgumentException("Failed to sign a token")
+                        )
+                        .value,
+                      refreshToken = None,
+                      expiresIn = None,
+                      scope = None,
+                      tokenType = "bearer"
+                    ))
+              }
+          }
+        }
+      }
+    )
+
+    Http().bindAndHandle(route, "localhost", config.port.value)
+  }
+  def stop(f: Future[ServerBinding])(implicit ec: ExecutionContext): Future[Done] =
+    f.flatMap(_.unbind())
+}
