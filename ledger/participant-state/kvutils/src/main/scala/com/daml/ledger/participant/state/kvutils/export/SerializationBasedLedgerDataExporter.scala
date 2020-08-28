9c27af15228aed1cdae4f5fb5c9245d6a58b75c1
Samir Talwar <samir.talwar@digitalasset.com>
2020-08-28 11:46:37 +0200

    kvutils: Inline (De)Serialization into the exporter.

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/export/SerializationBasedLedgerDataExporter.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/export/SerializationBasedLedgerDataExporter.scala
@@ -36,11 +36,44 @@ final class SerializationBasedLedgerDataExporter(output: DataOutputStream)
     override def write(submissionInfo: SubmissionInfo, writeSet: Seq[(Key, Value)]): Unit = {
       val stamp = outputLock.writeLock()
       try {
-        Serialization.serializeEntry(submissionInfo, writeSet, output)
+        serializeEntry(submissionInfo, writeSet)
         output.flush()
       } finally {
         outputLock.unlock(stamp)
       }
+    }
+
+    private def serializeEntry(
+        submissionInfo: SubmissionInfo,
+        writeSet: Seq[(Key, Value)],
+    ): Unit = {
+      serializeSubmissionInfo(submissionInfo)
+      serializeWriteSet(writeSet)
+    }
+
+    private def serializeSubmissionInfo(submissionInfo: SubmissionInfo): Unit = {
+      output.writeUTF(submissionInfo.correlationId)
+      writeBytes(submissionInfo.submissionEnvelope)
+      writeInstant(submissionInfo.recordTimeInstant)
+      output.writeUTF(submissionInfo.participantId)
+    }
+
+    private def serializeWriteSet(writeSet: WriteSet): Unit = {
+      output.writeInt(writeSet.size)
+      for ((key, value) <- writeSet.sortBy(_._1.asReadOnlyByteBuffer())) {
+        writeBytes(key)
+        writeBytes(value)
+      }
+    }
+
+    private def writeBytes(bytes: Key): Unit = {
+      output.writeInt(bytes.size())
+      bytes.writeTo(output)
+    }
+
+    private def writeInstant(instant: Instant): Unit = {
+      output.writeLong(instant.getEpochSecond)
+      output.writeInt(instant.getNano)
     }
   }
 
--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/export/SerializationBasedLedgerDataImporter.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/export/SerializationBasedLedgerDataImporter.scala
@@ -4,6 +4,10 @@
 package com.daml.ledger.participant.state.kvutils.export
 
 import java.io.DataInputStream
+import java.time.Instant
+
+import com.daml.ledger.participant.state
+import com.google.protobuf.ByteString
 
 /**
   * Enables importing ledger data from an input stream.
@@ -16,6 +20,47 @@ final class SerializationBasedLedgerDataImporter(input: DataInputStream)
     if (input.available() == 0)
       Stream.empty
     else
-      Deserialization.deserializeEntry(input) #:: read()
+      deserializeEntry() #:: read()
+
+  private def deserializeEntry(): (SubmissionInfo, WriteSet) = {
+    val submissionInfo = deserializeSubmissionInfo()
+    val writeSet = deserializeWriteSet()
+    (submissionInfo, writeSet)
+  }
+
+  private def deserializeSubmissionInfo(): SubmissionInfo = {
+    val correlationId = input.readUTF()
+    val submissionEnvelope = readBytes()
+    val recordTimeInstant: Instant = readInstant()
+    val participantId = input.readUTF()
+    SubmissionInfo(
+      state.v1.ParticipantId.assertFromString(participantId),
+      correlationId,
+      submissionEnvelope,
+      recordTimeInstant,
+    )
+  }
+
+  private def deserializeWriteSet(): WriteSet = {
+    val numKeyValuePairs = input.readInt()
+    (1 to numKeyValuePairs).map { _ =>
+      val key = readBytes()
+      val value = readBytes()
+      key -> value
+    }
+  }
+
+  private def readBytes(): ByteString = {
+    val size = input.readInt()
+    val byteArray = new Array[Byte](size)
+    input.readFully(byteArray)
+    ByteString.copyFrom(byteArray)
+  }
+
+  private def readInstant(): Instant = {
+    val epochSecond = input.readLong()
+    val nano = input.readInt()
+    Instant.ofEpochSecond(epochSecond, nano.toLong)
+  }
 
 }
