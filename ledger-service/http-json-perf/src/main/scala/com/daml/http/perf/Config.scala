a858d363288308949cc659bc59590d3b787921d8
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-08-31 18:40:01 -0400

    Starting sandbox and json-api from perf-test main
    changelog_begin
    changelog_end

--- ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/Config.scala
+++ ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/Config.scala
@@ -3,34 +3,34 @@
 
 package com.daml.http.perf
 
+import java.io.File
+
 import com.daml.jwt.JwtDecoder
 import com.daml.jwt.domain.Jwt
-import com.daml.ports.Port
 import scopt.RenderingMode
 
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
 private[perf] final case class Config(
-    jsonApiHost: String,
-    jsonApiPort: Port,
     scenario: String,
+    dars: List[File],
     jwt: Jwt,
     packageId: Option[String],
+    maxDuration: Option[FiniteDuration]
 ) {
   override def toString: String =
-    s"Config(jsonApiHost=${this.jsonApiHost}, " +
-      s"jsonApiPort=${this.jsonApiPort.value: Int}, " +
+    s"Config(" +
       s"scenario=${this.scenario}, " +
+      s"dars=${dars: List[File]}," +
       s"jwt=..., " + // don't print the JWT
-      s"packageId=${this.packageId.toString})"
+      s"packageId=${this.packageId: Option[String]}," +
+      s"maxDuration=${this.maxDuration: Option[FiniteDuration]}" +
+      ")"
 }
 
 private[perf] object Config {
   val Empty =
-    Config(
-      jsonApiHost = "",
-      jsonApiPort = Port.Dynamic,
-      scenario = "",
-      jwt = Jwt(""),
-      packageId = None)
+    Config(scenario = "", dars = List.empty, jwt = Jwt(""), packageId = None, maxDuration = None)
 
   def parseConfig(args: Seq[String]): Option[Config] =
     configParser.parse(args, Config.Empty)
@@ -44,21 +44,15 @@ private[perf] object Config {
 
       help("help").text("Print this usage text")
 
-      opt[String]("json-api-host")
-        .action((x, c) => c.copy(jsonApiHost = x))
-        .required()
-        .text("JSON API host name or IP address")
-
-      opt[Int]("json-api-port")
-        .action((x, c) => c.copy(jsonApiPort = Port(x)))
-        .required()
-        .validate(validatePort)
-        .text("JSON API port number")
-
       opt[String]("scenario")
         .action((x, c) => c.copy(scenario = x))
         .required()
         .text("Performance test scenario to run")
+
+      opt[Seq[File]]("dars")
+        .action((x, c) => c.copy(dars = x.toList))
+        .required()
+        .text("DAR files to pass to Sandbox")
 
       opt[String]("jwt")
         .action((x, c) => c.copy(jwt = Jwt(x)))
@@ -70,10 +64,12 @@ private[perf] object Config {
         .action((x, c) => c.copy(packageId = Some(x)))
         .optional()
         .text("Optional package ID to specify in the commands sent to JSON API")
-    }
 
-  private def validatePort(p: Int): Either[String, Unit] =
-    Port.validate(p).toEither.left.map(x => x.getMessage)
+      opt[Duration]("max-duration")
+        .action((x, c) => c.copy(maxDuration = Some(FiniteDuration(x.length, x.unit))))
+        .optional()
+        .text(s"Optional maximum perf test duration. Default value infinity. Examples: 500ms, 5s, 10min, 1h, 1d.")
+    }
 
   private def validateJwt(s: String): Either[String, Unit] = {
     import scalaz.syntax.show._
--- ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/Main.scala
+++ ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/Main.scala
@@ -4,11 +4,14 @@
 package com.daml.http.perf
 
 import akka.actor.ActorSystem
+import akka.stream.Materializer
+import com.daml.grpc.adapter.{AkkaExecutionSequencerPool, ExecutionSequencerFactory}
+import com.daml.http.HttpServiceTestFixture.withHttpService
 import com.daml.scalautil.Statement.discard
 import com.typesafe.scalalogging.StrictLogging
 
 import scala.concurrent.duration.{Duration, _}
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, Future, TimeoutException}
 import scala.util.{Failure, Success}
 
 object Main extends StrictLogging {
@@ -21,18 +24,35 @@ object Main extends StrictLogging {
     case object InvalidUsage extends ExitCode(100)
     case object StartupError extends ExitCode(101)
     case object InvalidScenario extends ExitCode(102)
+    case object TimedOutScenario extends ExitCode(103)
   }
 
   def main(args: Array[String]): Unit = {
-    implicit val asys: ActorSystem = ActorSystem("http-json-perf")
-    //    implicit val mat: Materializer = Materializer(asys)
+    val name = "http-json-per"
+    val terminationTimeout: FiniteDuration = 30.seconds
+
+    implicit val asys: ActorSystem = ActorSystem(name)
+    implicit val mat: Materializer = Materializer(asys)
+    implicit val aesf: ExecutionSequencerFactory =
+      new AkkaExecutionSequencerPool(poolName = name, terminationTimeout = terminationTimeout)
     implicit val ec: ExecutionContext = asys.dispatcher
 
-    def terminate(): Unit = discard { Await.result(asys.terminate(), 10.seconds) }
+    def terminate(): Unit = discard { Await.result(asys.terminate(), terminationTimeout) }
 
     val exitCode: ExitCode = Config.parseConfig(args) match {
       case Some(config) =>
-        main(config)
+        val exitCodeF: Future[ExitCode] = main(config)
+        exitCodeF.onComplete {
+          case Success(_) => logger.info(s"Scenario: ${config.scenario: String} completed")
+          case Failure(e) => logger.error(s"Scenario: ${config.scenario: String} failed", e)
+        }
+        try {
+          Await.result(exitCodeF, config.maxDuration.getOrElse(Duration.Inf))
+        } catch {
+          case e: TimeoutException =>
+            logger.error(s"Scenario: ${config.scenario: String} failed", e)
+            ExitCode.TimedOutScenario
+        }
       case None =>
         // error is printed out by scopt
         ExitCode.InvalidUsage
@@ -42,25 +62,25 @@ object Main extends StrictLogging {
     sys.exit(exitCode.code)
   }
 
-  private def main(config: Config)(implicit ec: ExecutionContext): ExitCode = {
+  private def main(config: Config)(
+      implicit asys: ActorSystem,
+      mat: Materializer,
+      aesf: ExecutionSequencerFactory,
+      ec: ExecutionContext
+  ): Future[ExitCode] = {
     logger.info(s"$config")
     if (config.scenario == "commands") {
-      runScenarioSync(config, null) // XXX
-      ExitCode.Ok
+      withHttpService(config.scenario, config.dars, None, None) { (_, _, _, _) =>
+        runScenarioSync(config, _ => Future.unit) // TODO(Leo): scenario runner goes here
+      }.map(_ => ExitCode.Ok)
     } else {
       logger.error(s"Unsupported scenario: ${config.scenario}")
-      ExitCode.InvalidScenario
+      Future.successful(ExitCode.InvalidScenario)
     }
   }
 
   private def runScenarioSync(config: Config, scenario: Scenario)(
-      implicit ec: ExecutionContext
-  ): Unit = {
-    val scenarioF: Future[Unit] = scenario(config)
-    scenarioF.onComplete {
-      case Success(_) => logger.info(s"Scenario: ${config.scenario} completed")
-      case Failure(e) => logger.error(s"Scenario: ${config.scenario} failed", e)
-    }
-    Await.result(scenarioF, Duration.Inf)
+      implicit ec: ExecutionContext): Future[Unit] = {
+    scenario(config).map(_ => Thread.sleep(5000)) // XXX
   }
 }
