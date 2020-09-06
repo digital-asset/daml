93f10561a35e81b7aec13050358ad2de14d81a38
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-09-06 20:43:16 +0200

    Added input & output keys to CommitMetadata trait along with a trivial implementation. Exposed submissionOutputs from KeyValueCommitting via the companion object.

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/CommitMetadata.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/api/CommitMetadata.scala
@@ -3,6 +3,13 @@
 
 package com.daml.ledger.participant.state.kvutils.api
 
+import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlSubmission
+import com.daml.ledger.participant.state.kvutils.{Bytes, KeyValueCommitting}
+import com.daml.ledger.validator.DefaultStateKeySerializationStrategy
+import com.google.protobuf.ByteString
+
+import scala.collection.JavaConverters._
+
 /**
   * Exposes metadata about the commit.
   * The methods may lazily evaluate.
@@ -14,12 +21,35 @@ sealed trait CommitMetadata {
     *         submissions
     */
   def estimatedInterpretationCost: Option[Long]
+
+  def inputKeys: Iterable[Bytes]
+
+  def outputKeys: Iterable[Bytes]
 }
 
 object CommitMetadata {
   val Empty: CommitMetadata =
     SimpleCommitMetadata(estimatedInterpretationCost = None)
+
+  def fromSubmission(
+      submission: DamlSubmission,
+      inputEstimatedInterpretationCost: Option[Long]): CommitMetadata = new CommitMetadata {
+    override def estimatedInterpretationCost: Option[Long] = inputEstimatedInterpretationCost
+
+    override def inputKeys: Iterable[Bytes] =
+      submission.getInputDamlStateList.asScala
+        .map(DefaultStateKeySerializationStrategy.serializeStateKey)
+
+    override def outputKeys: Iterable[Bytes] =
+      KeyValueCommitting
+        .submissionOutputs(submission)
+        .map(DefaultStateKeySerializationStrategy.serializeStateKey)
+  }
 }
 
 final case class SimpleCommitMetadata(override val estimatedInterpretationCost: Option[Long])
-    extends CommitMetadata
+    extends CommitMetadata {
+  override def inputKeys: Iterable[Bytes] = throw new NotImplementedError()
+
+  override def outputKeys: Iterable[Bytes] = throw new NotImplementedError()
+}
