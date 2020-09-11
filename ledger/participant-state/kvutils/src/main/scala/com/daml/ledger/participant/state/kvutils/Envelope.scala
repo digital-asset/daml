da7d5a3c5971d6a0b7829a9aeeaafac4f1e629e5
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-09-11 12:01:36 +0200

    Close GzipInputStream after decompressing message. CHANGELOG_BEGIN CHANGELOG_END

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/Envelope.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/Envelope.scala
@@ -132,7 +132,7 @@ object Envelope {
       case msg => Left(s"Expected state value, got ${msg.getClass}")
     }
 
-  private def compress(payload: ByteString): ByteString = {
+  private[kvutils] def compress(payload: ByteString): ByteString = {
     val out = ByteString.newOutput
     val gzipOut = new GZIPOutputStream(out)
     gzipOut.write(payload.toByteArray)
@@ -140,9 +140,11 @@ object Envelope {
     out.toByteString
   }
 
-  private def decompress(payload: ByteString): ByteString = {
+  private[kvutils] def decompress(payload: ByteString): ByteString = {
     val gzipIn = new GZIPInputStream(payload.newInput)
-    ByteString.readFrom(gzipIn)
+    val decompressed = ByteString.readFrom(gzipIn)
+    gzipIn.close()
+    decompressed
   }
 
   private def parseMessageSafe[T](callParser: () => T): Either[String, T] =
