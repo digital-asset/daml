dbc5de960489177037afdcd7a81eed0a67a8f79c
Stephen Compall <stephen.compall@daml.com>
2020-09-11 16:42:43 -0400

    move Future, ExecutionContext companions into normal package

--- libs-scala/scala-utils/src/main/scala/com/daml/scalautil/concurrent/package.scala
+++ libs-scala/scala-utils/src/main/scala/com/daml/scalautil/concurrent/package.scala
@@ -4,6 +4,9 @@
 package com.daml.scalautil
 
 import scala.util.Try
+import scala.{concurrent => sc}
+
+import scalaz.Id.Id
 
 package object concurrent {
 
@@ -13,22 +16,34 @@ package object concurrent {
     */
   type Future[-EC, +A] = FutureOf.Instance.T[EC, A]
   type ExecutionContext[+P] = ExecutionContextOf.Instance.T[P]
+}
+
+// keeping the companions with the same-named type aliases
+package concurrent {
 
   object Future {
+
+    /** {{{
+      *  Future[MyECTag] { expr }
+      * }}}
+      *
+      * returns `Future[MyECTag, E]` where `E` is `expr`'s inferred type and
+      * `ExecutionContext[MyECTag]` is required implicitly.
+      */
     def apply[EC]: apply[EC] =
       new apply(())
 
     final class apply[EC](private val ignore: Unit) extends AnyVal {
       def apply[A](body: => A)(implicit ec: ExecutionContext[EC]): Future[EC, A] =
-        scala.concurrent.Future(body)(ec)
+        sc.Future(body)(ec)
     }
 
     def fromTry[EC, A](result: Try[A]): Future[EC, A] =
-      scala.concurrent.Future.fromTry(result)
+      sc.Future.fromTry(result)
   }
 
   object ExecutionContext {
-    def apply[EC](ec: scala.concurrent.ExecutionContext): ExecutionContext[EC] =
-      ExecutionContextOf.Instance.subst[scalaz.Id.Id, EC](ec)
+    def apply[EC](ec: sc.ExecutionContext): ExecutionContext[EC] =
+      ExecutionContextOf.Instance.subst[Id, EC](ec)
   }
 }
