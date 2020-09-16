48b90f85c751402a91df1b70f7e6d116fb85332b
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2020-09-16 23:19:13 +0200

    reenable Windows cache
    All machines have been reset.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- build.ps1
+++ build.ps1
@@ -48,8 +48,6 @@ function bazel() {
 # which later causes issues on Bazel init (source forest creation) on Windows. A shutdown closes workers,
 # which is a workaround for this problem.
 bazel shutdown
-# Temporary until nodes are reset
-bazel clean --expunge
 
 # Prefetch nodejs_dev_env to avoid permission denied errors on external/nodejs_dev_env/nodejs_dev_env/node.exe
 # It isn’t clear where exactly those errors are coming from.
