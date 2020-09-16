fe63816431003f82fae71ef79171fed8933276ef
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-09-16 09:31:54 -0400

    Write report summary in text format (#7407)
    * write summary.txt
    
    changelog_begin
    changelog_end
    
    * make `Response time distribution` brackets configurable

--- libs-scala/gatling-utils/src/main/scala/com/daml/gatling/stats/SimulationLogSyntax.scala
+++ libs-scala/gatling-utils/src/main/scala/com/daml/gatling/stats/SimulationLogSyntax.scala
@@ -7,6 +7,7 @@ import java.io.File
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 
+import com.daml.gatling.stats.SimulationLog.ScenarioStats
 import com.daml.scalautil.Statement.discard
 
 object SimulationLogSyntax {
@@ -16,12 +17,33 @@ object SimulationLogSyntax {
       * Will write a summary.csv given a Gatling result directory.
       * @param targetDirectory the directory where the summary.csv will be created.
       */
-    def writeSummary(targetDirectory: File): Unit = {
+    def writeSummaryCsv(targetDirectory: File): Unit = {
       discard {
         Files.write(
           new File(targetDirectory, "summary.csv").toPath,
           log.toCsvString.getBytes(StandardCharsets.UTF_8))
       }
+    }
+
+    def writeSummaryText(targetDirectory: File): String = {
+      val summary = formatTextReport(log.scenarios)
+      discard {
+        Files.write(
+          new File(targetDirectory, "summary.txt").toPath,
+          summary.getBytes(StandardCharsets.UTF_8))
+      }
+      summary
+    }
+
+    private def formatTextReport(scenarios: List[ScenarioStats]): String = {
+      val buf = new StringBuffer()
+      scenarios.foreach { x =>
+        x.requestsByType.foreach {
+          case (name, stats) =>
+            buf.append(stats.formatted(name))
+        }
+      }
+      buf.toString
     }
   }
 }
