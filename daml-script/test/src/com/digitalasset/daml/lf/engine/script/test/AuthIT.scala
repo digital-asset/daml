3f52199fb81a0d1bdf7d90c3b744830759cd75f4
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-09-17 08:50:48 +0200

    Move script tests to scalatest
    There isn’t really any good reason to insist on client_server_test
    here so this PR replaces all of that by scalatest and a few test
    fixtures which gives us better assertion failures, allows us to run
    individual tests, ….
    
    fixes #7356
    
    changelog_begin
    changelog_end

---
+++ daml-script/test/src/com/digitalasset/daml/lf/engine/script/test/AuthIT.scala
@@ -0,0 +1,38 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.lf.engine.script.test
+
+import java.io.File
+
+import com.daml.bazeltools.BazelRunfiles._
+import com.daml.ledger.api.testing.utils.SuiteResourceManagementAroundAll
+import com.daml.lf.data.Ref._
+import com.daml.lf.speedy.SValue._
+import org.scalatest._
+import spray.json.{JsObject, JsString}
+
+final class AuthIT
+    extends AsyncWordSpec
+    with SandboxAuthParticipantFixture
+    with Matchers
+    with SuiteResourceManagementAroundAll {
+  override def darFile = new File(rlocation("daml-script/test/script-test.dar"))
+  val (dar, envIface) = readDar(darFile)
+
+  "DAML Script against authorized ledger" can {
+    "auth" should {
+      "create and accept Proposal" in {
+        for {
+          clients <- participantClients(List("Alice", "Bob"), false)
+          r <- run(
+            clients,
+            QualifiedName.assertFromString("ScriptTest:auth"),
+            inputValue = Some(JsObject(("_1", JsString("Alice")), ("_2", JsString("Bob")))),
+            dar = dar)
+        } yield
+          assert(r == SUnit) // Boring assertion, we just want to see that we do not get an authorization error
+      }
+    }
+  }
+}
---
+++ daml-script/test/src/test-utils/com/daml/lf/engine/script/test/LogCollector.scala
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.lf.engine.script.test
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
+
+import scala.collection.mutable.ArrayBuffer
+
+// Custom log collector to test order of trace statements
+object LogCollector {
+  val events = new ArrayBuffer[ILoggingEvent]
+  def clear(): Unit = events.clear
+}
+
+final class LogCollector extends AppenderBase[ILoggingEvent] {
+  override def append(e: ILoggingEvent): Unit = {
+    LogCollector.events += e
+  }
+}
