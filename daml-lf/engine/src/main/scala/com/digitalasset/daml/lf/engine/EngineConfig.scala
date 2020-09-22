63be90c0fd464e3f83e49c3437b0e8bdbc75a8ae
Remy <remy.haemmerle@daml.com>
2020-09-22 09:08:02 +0200

    Engine: documents for Engine configuration (#7420)
    CHANGELOG_BEGIN
    CHANGELOG_END

--- daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/EngineConfig.scala
+++ daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/EngineConfig.scala
@@ -6,25 +6,61 @@ package engine
 
 import java.nio.file.Path
 
-import com.daml.lf.language.{LanguageVersion => LV}
-import com.daml.lf.transaction.{TransactionVersions, TransactionVersion => TV}
+import com.daml.lf.language.LanguageVersion
+import com.daml.lf.transaction.{TransactionVersions, TransactionVersion}
+import com.daml.lf.value.ValueVersion
 
+/**
+  * The Engine configurations describes the versions of language and
+  * transaction the engine is allowed to read and write together with
+  * engine debugging feature.
+  *
+  * <p>
+  *
+  * @param allowedLanguageVersions The range of language versions the
+  *     engine is allowed to load.  The engine will crash if it asked
+  *     to load a language version that is not included in this range.
+  * @param allowedInputTransactionVersions The range of transaction
+  *     version the engine is allowed to load. The engine will crash
+  *     if it is asked to load transaction version that is not
+  *     included in this range
+  * @param allowedOutputTransactionVersions The range of output
+  *     transactions the engine is allowed to produce. The Engine
+  *     will always use the lowest possible version from this range to
+  *     encode the output transaction, and fails if such version does
+  *     not exist.
+  * @param stackTraceMode The flag enables the runtime support for
+  *     stack trace.
+  * @param profileDir The optional specifies the directory where to
+  *     save the output of the DAML scenario profiler. The profiler is
+  *     disabled if the option is empty.
+  */
 final case class EngineConfig(
-    allowedLanguageVersions: VersionRange[LV],
-    allowedInputTransactionVersions: VersionRange[TV],
-    allowedOutputTransactionVersions: VersionRange[TV],
+    allowedLanguageVersions: VersionRange[LanguageVersion],
+    allowedInputTransactionVersions: VersionRange[TransactionVersion],
+    allowedOutputTransactionVersions: VersionRange[TransactionVersion],
     packageValidation: Boolean = true,
     stackTraceMode: Boolean = false,
     profileDir: Option[Path] = None,
 ) {
 
-  private[lf] val allowedInputValueVersions =
+  /**
+    * The range of value versions the engine is allowed to load.  This
+    * is deterministically derived from
+    * [[allowedInputTransactionVersions]].
+    */
+  private[lf] val allowedInputValueVersions: VersionRange[ValueVersion] =
     VersionRange(
       TransactionVersions.assignValueVersion(allowedInputTransactionVersions.min),
       TransactionVersions.assignValueVersion(allowedInputTransactionVersions.max),
     )
 
-  private[lf] val allowedOutputValueVersions =
+  /**
+    * The range of value versions the engine is allowed to produce.
+    * This is deterministically derived from
+    * [[allowedOutputTransactionVersions]].
+    */
+  private[lf] val allowedOutputValueVersions: VersionRange[ValueVersion] =
     VersionRange(
       TransactionVersions.assignValueVersion(allowedOutputTransactionVersions.min),
       TransactionVersions.assignValueVersion(allowedOutputTransactionVersions.max),
@@ -34,28 +70,42 @@ final case class EngineConfig(
 
 object EngineConfig {
 
+  /**
+    * Most lenient production engine configuration. This allows the
+    * engine to load and produce all non-deprecated stable versions of
+    * language and transaction.
+    */
+  val Lenient: EngineConfig = new EngineConfig(
+    allowedLanguageVersions = VersionRange(
+      LanguageVersion(LanguageVersion.Major.V1, LanguageVersion.Minor.Stable("6")),
+      LanguageVersion(LanguageVersion.Major.V1, LanguageVersion.Minor.Stable("8")),
+    ),
+    allowedInputTransactionVersions =
+      VersionRange(TransactionVersion("10"), TransactionVersion("10")),
+    allowedOutputTransactionVersions =
+      VersionRange(TransactionVersion("10"), TransactionVersion("10"))
+  )
+
   private[this] def toDev(config: EngineConfig): EngineConfig =
     config.copy(
-      allowedLanguageVersions =
-        config.allowedLanguageVersions.copy(max = LV(LV.Major.V1, LV.Minor.Dev)),
+      allowedLanguageVersions = config.allowedLanguageVersions.copy(
+        max = LanguageVersion(LanguageVersion.Major.V1, LanguageVersion.Minor.Dev)),
       allowedInputTransactionVersions = config.allowedInputTransactionVersions.copy(
         max = TransactionVersions.acceptedVersions.last),
       allowedOutputTransactionVersions = config.allowedOutputTransactionVersions.copy(
         max = TransactionVersions.acceptedVersions.last),
     )
 
-  // recommended configuration
-  val Stable: EngineConfig = new EngineConfig(
-    allowedLanguageVersions = VersionRange(
-      LV(LV.Major.V1, LV.Minor.Stable("6")),
-      LV(LV.Major.V1, LV.Minor.Stable("8")),
-    ),
-    allowedInputTransactionVersions = VersionRange(TV("10"), TV("10")),
-    allowedOutputTransactionVersions = VersionRange(TV("10"), TV("10"))
-  )
+  /**
+    * Recommanded production configuration.
+    */
+  def Stable: EngineConfig = Lenient
 
-  // development configuration, should not be used in PROD.
-  // accept all language and transaction versions supported by SDK_1_x plus development versions.
-  lazy val Dev: EngineConfig = toDev(Stable)
+  /**
+    * Development configuration, should not be used in PROD.  Allowed
+    * the same input and output versions as [[Lenient]] plus the
+    * development versions.
+    */
+  val Dev: EngineConfig = toDev(Lenient)
 
 }
