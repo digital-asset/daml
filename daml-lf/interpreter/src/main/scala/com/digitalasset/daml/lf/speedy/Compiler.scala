662fcad3d19c9afce694290f1dd5d1e1cf0eecd5
Remy Haemmerle <Remy.Haemmerle@daml.com>
2020-09-29 13:12:58 +0200

    Address Moritz's review

--- daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Compiler.scala
+++ daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Compiler.scala
@@ -145,14 +145,14 @@ private[lf] final class Compiler(
   }
 
   @inline
-  private[this] def nextPosition(name: ExprVarName): Position = {
+  private[this] def nextPositionWithName(name: ExprVarName): Position = {
     val p = nextPosition()
     addExprVar(name, p)
     p
   }
 
-  private[this] def nextPosition(name: Option[ExprVarName]): Position =
-    name.fold(nextPosition())(nextPosition)
+  private[this] def nextPositionWithOptionalName(name: Option[ExprVarName]): Position =
+    name.fold(nextPosition())(nextPositionWithName)
 
   private[this] def svar(p: Position): SEVar = SEVar(env.position - p.idx)
 
@@ -170,15 +170,15 @@ private[lf] final class Compiler(
 
   private[this] def vars: List[VarRef] = env.varIndices.keys.toList
 
-  private[this] def lookUpVar(varRef: VarRef): Option[SEVar] =
+  private[this] def lookupVar(varRef: VarRef): Option[SEVar] =
     env.varIndices.get(varRef).map(svar)
 
-  def lookUpExprVar(name: ExprVarName): SEVar =
-    lookUpVar(EVarRef(name))
+  def lookupExprVar(name: ExprVarName): SEVar =
+    lookupVar(EVarRef(name))
       .getOrElse(throw CompilationError(s"Unknown variable: $name. Known: ${vars.mkString(",")}"))
 
-  def lookUpTypeVar(name: TypeVarName): Option[SEVar] =
-    lookUpVar(TVarRef(name))
+  def lookupTypeVar(name: TypeVarName): Option[SEVar] =
+    lookupVar(TVarRef(name))
 
   private[this] case class Env(
       position: Int = 0,
@@ -332,7 +332,7 @@ private[lf] final class Compiler(
   private[this] def compile(expr0: Expr): SExpr =
     expr0 match {
       case EVar(name) =>
-        lookUpExprVar(name)
+        lookupExprVar(name)
       case EVal(ref) =>
         SEVal(LfDefRef(ref))
       case EBuiltin(bf) =>
@@ -654,7 +654,7 @@ private[lf] final class Compiler(
         bindings.map {
           case Binding(optBinder, _, bound) =>
             val bound2 = withOptLabel(optBinder, compile(bound))
-            nextPosition(optBinder)
+            nextPositionWithOptionalName(optBinder)
             bound2
         }.toArray,
         compile(body),
@@ -695,7 +695,7 @@ private[lf] final class Compiler(
   private[this] def compileAbss(expr0: Expr, arity: Int = 0): SExpr =
     expr0 match {
       case EAbs((binder, typ @ _), body, ref @ _) =>
-        nextPosition(binder)
+        nextPositionWithName(binder)
         compileAbss(body, arity + 1)
       case ETyAbs((binder, KNat), body) =>
         addTypeVar(binder, nextPosition())
@@ -725,7 +725,7 @@ private[lf] final class Compiler(
   private[this] def translateType(typ: Type): Option[SExpr] =
     typ match {
       case TNat(n) => SENat(n)
-      case TVar(name) => lookUpTypeVar(name)
+      case TVar(name) => lookupTypeVar(name)
       case _ => None
     }
 
@@ -856,13 +856,13 @@ private[lf] final class Compiler(
 
           // add the first binding into the environment
           val appBoundHead = SEApp(svar(boundHeadPos), Array(svar(tokenPos)))
-          nextPosition(bindings.head.binder)
+          nextPositionWithOptionalName(bindings.head.binder)
 
           // and then the rest
           val boundTail = bindings.tail.toList.map {
             case Binding(optB, _, bound) =>
               val expr = SEApp(compile(bound), Array(svar(tokenPos)))
-              nextPosition(optB)
+              nextPositionWithOptionalName(optB)
               expr
           }
           val allBounds = appBoundHead +: boundTail
@@ -928,7 +928,7 @@ private[lf] final class Compiler(
       s"exercise @${tmplId.qualifiedName} ${choice.name}") {
       case List(actorsPos, cidPos, choiceArgPos, tokenPos) =>
         val tmplArg = SBUFetch(tmplId)(svar(cidPos), svar(tokenPos))
-        nextPosition(tmpl.param)
+        nextPositionWithName(tmpl.param)
         val beginExercise =
           SBUBeginExercise(tmplId, choice.name, choice.consuming, byKey = false)(
             svar(choiceArgPos),
@@ -985,7 +985,7 @@ private[lf] final class Compiler(
         val cid = SBUFetchKey(tmplId)(svar(keyWithMPos), svar(tokenPos))
         val cidPos = nextPosition()
         val tmplArg = SBUFetch(tmplId)(svar(cidPos), svar(tokenPos))
-        nextPosition(tmpl.param)
+        nextPositionWithName(tmpl.param)
         val beginExercise =
           SBUBeginExercise(tmplId, choice.name, choice.consuming, byKey = true)(
             svar(choiceArgPos),
@@ -1068,7 +1068,7 @@ private[lf] final class Compiler(
 
   private[this] def withBinders[A](binders: ExprVarName*)(f: Unit => A): A =
     withEnv { _ =>
-      binders.foreach(nextPosition)
+      binders.foreach(nextPositionWithName)
       f(())
     }
 
@@ -1383,7 +1383,7 @@ private[lf] final class Compiler(
     topLevelFunction(FetchDefRef(tmplId), 2, s"fetch @${tmplId.qualifiedName}") {
       case List(cidPos, tokenPos) =>
         val fetch = SBUFetch(tmplId)(svar(cidPos), svar(tokenPos))
-        val tmplArgPos = nextPosition(tmpl.param)
+        val tmplArgPos = nextPositionWithName(tmpl.param)
         val insertNode = SBUInsertFetchNode(tmplId, byKey = false)(
           svar(cidPos),
           compile(tmpl.signatories),
@@ -1538,7 +1538,7 @@ private[lf] final class Compiler(
         val fetchKey = SBUFetchKey(tmplId)(svar(keyWithMPos), svar(tokenPos))
         val cidPos = nextPosition()
         val fetch = SBUFetch(tmplId)(svar(cidPos), svar(tokenPos))
-        val contractPos = nextPosition(tmpl.param)
+        val contractPos = nextPositionWithName(tmpl.param)
         val insertNode = SBUInsertFetchNode(tmplId, byKey = true)(
           svar(cidPos),
           compile(tmpl.signatories),
