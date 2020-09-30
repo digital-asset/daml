27fc564f2f30e894b2461fd2502ec338b30573a3
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-09-30 13:12:32 +0200

    Factor out request bodies
    Addressing review comment
    https://github.com/digital-asset/daml/pull/7519#discussion_r497321689

--- triggers/service/auth/src/main/scala/com/daml/oauth/middleware/Server.scala
+++ triggers/service/auth/src/main/scala/com/daml/oauth/middleware/Server.scala
@@ -26,6 +26,7 @@ import spray.json._
 // for OAuth2 as specified in `/triggers/service/authentication.md`
 object Server extends StrictLogging {
   import com.daml.oauth.server.JsonProtocol._
+  implicit private val unmarshal: Unmarshaller[String, Uri] = Unmarshaller.strict(Uri(_))
 
   // TODO[AH] Make the redirect URI configurable, especially the authority. E.g. when running behind nginx.
   private def toRedirectUri(uri: Uri) =
@@ -34,84 +35,20 @@ object Server extends StrictLogging {
       .withAuthority(uri.authority)
       .withPath(Uri.Path./("cb"))
 
-  def start(
-      config: Config)(implicit system: ActorSystem, ec: ExecutionContext): Future[ServerBinding] = {
-    implicit val unmarshal: Unmarshaller[String, Uri] = Unmarshaller.strict(Uri(_))
+  def start(serverConfig: Config)(
+      implicit system: ActorSystem,
+      ec: ExecutionContext): Future[ServerBinding] = {
+    implicit val config: Config = serverConfig
     // TODO[AH] Make sure this is bounded in size - or avoid state altogether.
-    val requests = TrieMap[UUID, Uri]()
+    implicit val requests: TrieMap[UUID, Uri] = TrieMap()
     val route = concat(
       path("auth") {
         get {
           complete((StatusCodes.NotImplemented, "The /auth endpoint is not implemented yet"))
         }
       },
-      path("login") {
-        get {
-          parameters(('redirect_uri.as[Uri], 'claims))
-            .as[Request.Login](Request.Login) {
-              login =>
-                extractRequest {
-                  request =>
-                    val requestId = UUID.randomUUID
-                    requests += (requestId -> login.redirectUri)
-                    val authorize = OAuthRequest.Authorize(
-                      responseType = "code",
-                      clientId = config.clientId,
-                      redirectUri = toRedirectUri(request.uri),
-                      scope = Some(login.claims),
-                      state = Some(requestId.toString))
-                    redirect(
-                      config.oauthUri
-                        .withPath(Uri.Path./("authorize"))
-                        .withQuery(authorize.toQuery),
-                      StatusCodes.Found)
-                }
-            }
-        }
-      },
-      path("cb") {
-        get {
-          parameters(('code, 'state ?))
-            .as[OAuthResponse.Authorize](OAuthResponse.Authorize) {
-              authorize =>
-                extractRequest {
-                  request =>
-                    val redirectUri = for {
-                      state <- authorize.state
-                      requestId <- Try(UUID.fromString(state)).toOption
-                      redirectUri <- requests.remove(requestId)
-                    } yield redirectUri
-                    redirectUri match {
-                      case None =>
-                        complete(StatusCodes.NotFound)
-                      case Some(redirectUri) =>
-                        val body = OAuthRequest.Token(
-                          grantType = "authorization_code",
-                          code = authorize.code,
-                          redirectUri = toRedirectUri(request.uri),
-                          clientId = config.clientId,
-                          clientSecret = config.clientSecret)
-                        val req = HttpRequest(
-                          uri = config.oauthUri.withPath(Uri.Path./("token")),
-                          entity =
-                            HttpEntity(MediaTypes.`application/json`, body.toJson.compactPrint),
-                          method = HttpMethods.POST)
-                        val tokenRequest = for {
-                          resp <- Http().singleRequest(req)
-                          tokenResp <- Unmarshal(resp).to[OAuthResponse.Token]
-                        } yield tokenResp
-                        onSuccess(tokenRequest) { token =>
-                          val encoder = Base64.getUrlEncoder()
-                          val content = encoder.encodeToString(token.toJson.compactPrint.getBytes)
-                          setCookie(HttpCookie("daml-ledger-token", content)) {
-                            redirect(redirectUri, StatusCodes.Found)
-                          }
-                        }
-                    }
-                }
-            }
-        }
-      },
+      path("login") { get { login } },
+      path("cb") { get { loginCallback } },
       path("refresh") {
         get {
           complete((StatusCodes.NotImplemented, "The /refresh endpoint is not implemented yet"))
@@ -121,6 +58,69 @@ object Server extends StrictLogging {
 
     Http().bindAndHandle(route, "localhost", config.port.value)
   }
+
   def stop(f: Future[ServerBinding])(implicit ec: ExecutionContext): Future[Done] =
     f.flatMap(_.unbind())
+
+  private def login(implicit config: Config, requests: TrieMap[UUID, Uri]) =
+    parameters(('redirect_uri.as[Uri], 'claims))
+      .as[Request.Login](Request.Login) { login =>
+        extractRequest { request =>
+          val requestId = UUID.randomUUID
+          requests += (requestId -> login.redirectUri)
+          val authorize = OAuthRequest.Authorize(
+            responseType = "code",
+            clientId = config.clientId,
+            redirectUri = toRedirectUri(request.uri),
+            scope = Some(login.claims),
+            state = Some(requestId.toString))
+          redirect(
+            config.oauthUri
+              .withPath(Uri.Path./("authorize"))
+              .withQuery(authorize.toQuery),
+            StatusCodes.Found)
+        }
+      }
+
+  private def loginCallback(
+      implicit config: Config,
+      requests: TrieMap[UUID, Uri],
+      system: ActorSystem,
+      ec: ExecutionContext) =
+    parameters(('code, 'state ?))
+      .as[OAuthResponse.Authorize](OAuthResponse.Authorize) { authorize =>
+        extractRequest { request =>
+          val redirectUri = for {
+            state <- authorize.state
+            requestId <- Try(UUID.fromString(state)).toOption
+            redirectUri <- requests.remove(requestId)
+          } yield redirectUri
+          redirectUri match {
+            case None =>
+              complete(StatusCodes.NotFound)
+            case Some(redirectUri) =>
+              val body = OAuthRequest.Token(
+                grantType = "authorization_code",
+                code = authorize.code,
+                redirectUri = toRedirectUri(request.uri),
+                clientId = config.clientId,
+                clientSecret = config.clientSecret)
+              val req = HttpRequest(
+                uri = config.oauthUri.withPath(Uri.Path./("token")),
+                entity = HttpEntity(MediaTypes.`application/json`, body.toJson.compactPrint),
+                method = HttpMethods.POST)
+              val tokenRequest = for {
+                resp <- Http().singleRequest(req)
+                tokenResp <- Unmarshal(resp).to[OAuthResponse.Token]
+              } yield tokenResp
+              onSuccess(tokenRequest) { token =>
+                val encoder = Base64.getUrlEncoder()
+                val content = encoder.encodeToString(token.toJson.compactPrint.getBytes)
+                setCookie(HttpCookie("daml-ledger-token", content)) {
+                  redirect(redirectUri, StatusCodes.Found)
+                }
+              }
+          }
+        }
+      }
 }
