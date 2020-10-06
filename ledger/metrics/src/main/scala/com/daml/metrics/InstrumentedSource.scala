3ac3dc0b965cb02fa7322e0b83b733ce30d46116
Jussi Maki <jussi.maki@digitalasset.com>
2020-10-06 18:25:04 +0200

    Add missing copyright header

--- ledger/metrics/src/main/scala/com/daml/metrics/InstrumentedSource.scala
+++ ledger/metrics/src/main/scala/com/daml/metrics/InstrumentedSource.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.metrics
 
 import akka.Done
@@ -81,3 +84,4 @@ object InstrumentedSource {
   }
 
 }
+
