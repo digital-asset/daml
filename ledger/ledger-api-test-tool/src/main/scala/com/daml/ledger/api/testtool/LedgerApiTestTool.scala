b8e6353a94e633a5ccbee4aeec2e63645436d1fb
mzagorski <marcin.zagorski@digitalasset.com>
2020-10-20 11:45:18 +0200

    ledger-api-test-tool cmd line option to skip dar upload
    CHANGELOG_BEGIN
    - [ledger-api-test-tool] Introduced new cmd line argument skip-dal-upload see `docs <https://docs.daml.com/tools/ledger-api-test-tool/index.html>`__.
    CHANGELOG_END

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/LedgerApiTestTool.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/LedgerApiTestTool.scala
@@ -187,5 +187,6 @@ object LedgerApiTestTool {
       identifierSuffix,
       config.timeoutScaleFactor,
       concurrencyOverride.getOrElse(config.concurrentTestRuns),
+      uploadDars = config.uploadDars,
     )
 }
