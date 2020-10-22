65116793154ddbcbfcde651e1dbc538dc87c3084
Samir Talwar <samir.talwar@digitalasset.com>
2020-10-22 17:52:37 +0200

    language-support/hs: Use `nix-shell` for export-package.sh.

--- language-support/hs/bindings/export-package.sh
+++ language-support/hs/bindings/export-package.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash ../../../shell.nix
+
 # Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 set -eou pipefail
 
 if [ "$#" -ne 1 ]; then
-    echo "Expected exactly one argument."
-    echo "Usage: ${BASH_SOURCE[0]} TARGET_DIR"
-    exit 1
+  echo "Expected exactly one argument."
+  echo "Usage: ${BASH_SOURCE[0]} TARGET_DIR"
+  exit 1
 fi
 
 TARGET_DIR=$(realpath $1)
