22f14b72c50919ce0d922c3fb65d39b1640cc854
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-10-26 18:18:46 +0100

    Test for database failures in JSON API (#7808)
    changelog_begin
    changelog_end

--- ledger-service/http-json/src/failure/scala/http/HttpTestFixture.scala
+++ ledger-service/http-json/src/failure/scala/http/HttpTestFixture.scala
@@ -7,13 +7,38 @@ import akka.http.scaladsl.model.Uri
 import com.daml.bazeltools.BazelRunfiles
 import com.daml.http.json.{DomainJsonDecoder, DomainJsonEncoder}
 import com.daml.ledger.client.LedgerClient
+import com.daml.ports.LockedFreePort
+import com.daml.testing.postgresql.PostgresAroundAll
+import java.net.InetAddress
 import org.scalatest.Suite
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait HttpFailureTestFixture extends ToxicSandboxFixture { self: Suite =>
+trait HttpFailureTestFixture extends ToxicSandboxFixture with PostgresAroundAll { self: Suite =>
 
   private implicit val ec: ExecutionContext = system.dispatcher
+
+  lazy val (dbProxy, dbProxyPort) = {
+    val host = InetAddress.getLoopbackAddress
+    val proxyPort = LockedFreePort.find()
+    val proxy = proxyClient.createProxy(
+      "database",
+      s"${host.getHostName}:${proxyPort.port}",
+      s"${host.getHostName}:${postgresDatabase.port}")
+    proxyPort.unlock()
+    (proxy, proxyPort.port)
+  }
+
+  // has to be lazy because postgresFixture is NOT initialized yet
+  private lazy val jdbcConfig_ =
+    JdbcConfig(
+      driver = "org.postgresql.Driver",
+      url =
+        s"jdbc:postgresql://${postgresDatabase.hostName}:$dbProxyPort/${postgresDatabase.databaseName}?user=${postgresDatabase.userName}&password=${postgresDatabase.password}",
+      user = "test",
+      password = "",
+      createSchema = true
+    )
 
   override def packageFiles =
     List(
@@ -26,11 +51,13 @@ trait HttpFailureTestFixture extends ToxicSandboxFixture { self: Suite =>
       .map(p => domain.Party(p.party))
 
   protected def withHttpService[A]
-    : ((Uri, DomainJsonEncoder, DomainJsonDecoder, LedgerClient) => Future[A]) => Future[A] =
+    : ((Uri, DomainJsonEncoder, DomainJsonDecoder, LedgerClient) => Future[A]) => Future[A] = {
+    println(proxy.getUpstream())
     HttpServiceTestFixture.withHttpService(
       this.getClass.getSimpleName,
       proxiedPort,
-      None,
+      Some(jdbcConfig_),
       None,
       wsConfig = Some(Config.DefaultWsConfig))
+  }
 }
