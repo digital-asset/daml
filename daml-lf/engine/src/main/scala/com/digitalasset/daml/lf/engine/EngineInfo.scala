8677bd92f356a725a525652bab113ab2d56b866f
Remy <remy.haemmerle@daml.com>
2020-11-03 10:43:06 +0100

    LF: remove allowed transaction versions from EngineConfig. (#7861)
    This is a followup of #7858.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/EngineInfo.scala
+++ daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/EngineInfo.scala
@@ -9,9 +9,7 @@ class EngineInfo(config: EngineConfig) {
   import language.{LanguageVersion => LV}
 
   override def toString: String = show
-  def show: String = pretty.mkString(System.lineSeparator())
-
-  lazy val pretty: Iterable[String] = {
+  def show: String = {
 
     val allLangVersions =
       for {
@@ -19,67 +17,19 @@ class EngineInfo(config: EngineConfig) {
         minor <- major.supportedMinorVersions
       } yield LV(major, minor)
 
-    val allTransactionVersions =
-      transaction.TransactionVersions.acceptedVersions
-
-    val allValueVersions =
-      value.ValueVersions.acceptedVersions
-
-    val allOutputTransactionVersions =
-      allTransactionVersions.filter(transaction.TransactionVersions.DevOutputVersions.contains)
-
-    val allOutputValueVersions =
-      allOutputTransactionVersions.map(transaction.TransactionVersions.assignValueVersion)
-
     val allowedLangVersions =
       allLangVersions.filter(config.allowedLanguageVersions.contains)
 
-    val allowedInputTransactionVersions =
-      allTransactionVersions.filter(config.allowedInputTransactionVersions.contains)
-
-    val allowedInputValueVersions =
-      allValueVersions.filter(config.allowedInputValueVersions.contains)
-
-    val allowedOutputTransactionVersions =
-      allTransactionVersions.filter(config.allowedOutputTransactionVersions.contains)
-
-    val allowedOutputValueVersions =
-      allValueVersions.filter(config.allowedOutputValueVersions.contains)
-
-    List(
-      List(
-        formatLangVersions(allLangVersions),
-        formatTxVersions("input", allTransactionVersions),
-        formatValVersions("input", allValueVersions),
-        formatTxVersions("output", allOutputTransactionVersions),
-        formatValVersions("output", allOutputValueVersions)
-      ).mkString("DAML LF Engine supports ", "; ", "."),
-      List(
-        formatLangVersions(allowedLangVersions),
-        formatTxVersions("input", allowedInputTransactionVersions),
-        formatValVersions("input", allowedInputValueVersions),
-        formatTxVersions("output", allowedOutputTransactionVersions),
-        formatValVersions("output", allowedOutputValueVersions)
-      ).mkString("DAML LF Engine config allows ", "; ", ".")
-    )
+    s"DAML LF Engine supports LF versions: ${formatLangVersions(allowedLangVersions)}"
   }
 
-  private[this] def formatLangVersions(versions: Iterable[LV]) = {
-    val prettyVersions = versions.map {
-      case LV(major, minor) =>
-        val ap = minor.toProtoIdentifier
-        s"${major.pretty}${if (ap.isEmpty) "" else s".$ap"}"
-    }
-    s"LF versions: ${prettyVersions.mkString(", ")}"
-  }
-
-  private[this] def formatTxVersions(
-      prefix: String,
-      versions: List[transaction.TransactionVersion],
-  ) =
-    s"$prefix transaction versions: ${versions.map(_.protoValue).mkString(", ")}"
-
-  private[this] def formatValVersions(prefix: String, versions: List[value.ValueVersion]) =
-    s"$prefix value versions: ${versions.map(_.protoValue).mkString(", ")}"
+  private[this] def formatLangVersions(versions: Iterable[LV]) =
+    versions
+      .map {
+        case LV(major, minor) =>
+          val ap = minor.toProtoIdentifier
+          s"${major.pretty}${if (ap.isEmpty) "" else s".$ap"}"
+      }
+      .mkString(", ")
 
 }
--- daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/EngineInfoTest.scala
+++ daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/EngineInfoTest.scala
@@ -9,26 +9,16 @@ class EngineInfoTest extends WordSpec with Matchers {
 
   "EngineInfo" should {
 
-    def infos = Seq(EngineConfig.Stable, EngineConfig.Dev).map(new EngineInfo(_))
-
-    val Seq(engineInfoStable, engineInfoDev) = infos
+    val Seq(engineInfoStable, engineInfoDev) =
+      Seq(EngineConfig.Stable, EngineConfig.Dev).map(new EngineInfo(_))
 
     "show supported LF, Transaction and Value versions" in {
 
-      infos.foreach(
-        _.pretty.toSeq(0) shouldBe
-          "DAML LF Engine supports LF versions: 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.dev; input transaction versions: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, dev; input value versions: 1, 2, 3, 4, 5, 6, dev; output transaction versions: 10, dev; output value versions: 6, dev."
-      )
-    }
+      engineInfoStable.show shouldBe
+        "DAML LF Engine supports LF versions: 1.6, 1.7, 1.8"
 
-    "show allowed LF, Transaction and Value versions" in {
-
-      engineInfoStable.pretty.toSeq(1) shouldBe
-        "DAML LF Engine config allows LF versions: 1.6, 1.7, 1.8; input transaction versions: 10; input value versions: 6; output transaction versions: 10; output value versions: 6."
-
-      engineInfoDev.pretty.toSeq(1) shouldBe
-        "DAML LF Engine config allows LF versions: 1.6, 1.7, 1.8, 1.dev; input transaction versions: 10, dev; input value versions: 6, dev; output transaction versions: 10, dev; output value versions: 6, dev."
-
+      engineInfoDev.show shouldBe
+        "DAML LF Engine supports LF versions: 1.6, 1.7, 1.8, 1.dev"
     }
 
     "toString returns the same value as show" in {
