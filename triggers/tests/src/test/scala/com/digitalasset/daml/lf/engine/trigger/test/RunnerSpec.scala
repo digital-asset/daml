e05c1e50cc78239137316813333c989164cbd706
Stephen Compall <stephen.compall@daml.com>
2020-11-04 17:49:48 -0500

    further tests for retrying

--- triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/RunnerSpec.scala
+++ triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/RunnerSpec.scala
@@ -4,7 +4,7 @@
 package com.daml.lf.engine.trigger
 package test
 
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.daml.ledger.api.testing.utils.AkkaBeforeAndAfterAll
 import com.daml.scalatest.AsyncForAll
 import org.scalatest.{AsyncWordSpec, Matchers}
@@ -17,18 +17,26 @@ class RunnerSpec extends AsyncWordSpec with Matchers with AsyncForAll with AkkaB
   import Runner.retrying
 
   "retrying" should {
+    import Future.{successful => okf}
+
+    def runItThrough[A, B](xs: Seq[A])(flow: Flow[A, B, _]): Future[Seq[B]] =
+      Source(xs).via(flow).runWith(Sink.seq)
+
     "terminate immediately on empty input" in {
       Source
         .empty[Unit]
-        .via(retrying(5, _ => Zero, 8, a => Future successful Some(a), a => Future successful a))
+        .via(retrying(5, _ => Zero, 8, a => okf(Some(a)), okf))
         .runWith(Sink.seq)
         .map(_ shouldBe empty)
     }
 
     "ignore retryable function if no retries" in forAllAsync(trials = 5) { xs: Seq[Int] =>
-      Source(xs)
-        .via(retrying(1, _ => Zero, 8, _ => fail("retried"), a => Future successful (a + 42)))
-        .runWith(Sink.seq)
+      runItThrough(xs)(retrying(1, _ => Zero, 8, _ => fail("retried"), a => okf(a + 42)))
+        .map(_ should ===(xs map (_ + 42)))
+    }
+
+    "not retry if retryable succeeded" in forAllAsync(trials = 5) { xs: Seq[Int] =>
+      runItThrough(xs)(retrying(2, _ => Zero, 8, a => okf(Some(a + 42)), a => okf(a - 42)))
         .map(_ should ===(xs map (_ + 42)))
     }
   }
