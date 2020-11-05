2c4000c27f0a64591a471361505c47c0fd278c4b
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-11-05 16:00:07 +0100

    Factor out the hash-migrations script

--- ledger/ledger-on-sql/hash-migrations.sh
+++ ledger/ledger-on-sql/hash-migrations.sh
@@ -5,9 +5,9 @@
 
 set -e
 set -u
+shopt -s globstar
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-for file in "$DIR"/src/main/resources/com/daml/ledger/on/sql/migrations/**/*.sql; do
-  shasum -a 256 "$file" | awk '{ print $1 }' > "$file.sha256"
-done
+"$DIR"/../../libs-scala/flyway-testing/hash-migrations.sh \
+  "$DIR"/src/main/resources/com/daml/ledger/on/sql/migrations/**/*.sql
--- libs-scala/flyway-testing/hash-migrations.sh
+++ libs-scala/flyway-testing/hash-migrations.sh
@@ -1,4 +1,11 @@
+#!/usr/bin/env bash
+
 # Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+set -e
+set -u
 
+for file in "$@"; do
+  shasum -a 256 "$file" | awk '{ print $1 }' > "$file.sha256"
+done
