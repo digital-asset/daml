1579159c89155618e4dc4125b5c7c1502867e276
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-11-05 14:19:36 +0100

    Check the trigger dao migrations digest
    Following the example of the corresponding ledger on SQL tests.
    
    The digests had to be updated as both of them had gone out of sync.
    The init digest presumably due to the change in #7226 and the one for
    adding the access token during review of #7890.
    
    changelog_begin
    changelog_end

---
+++ triggers/service/hash-migrations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -u
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+for file in "$DIR"/src/main/resources/com/daml/lf/engine/trigger/db/migration/**/*.sql; do
+  shasum -a 256 "$file" | awk '{ print $1 }' > "$file.sha256"
+done
