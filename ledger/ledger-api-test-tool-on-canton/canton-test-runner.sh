90053a9712f75e052ee7aabe76f425390df1af3a
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2020-11-10 15:51:50 +0100

    Upgrade ledger-api-test-tool-on-canton to the canton 0.19.0 release
    - Reflecting changes related to the Community edition of canton
    - Testing full ContractKeysIT in a separate canton environment with
      unique contract keys. The ability to run multiple canton environments
      will come in handy when we introduce the participant pruning test suite.
    - Slight canton configuration changes
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/ledger-api-test-tool-on-canton/canton-test-runner.sh
+++ ledger/ledger-api-test-tool-on-canton/canton-test-runner.sh
@@ -57,6 +57,10 @@ if [[ -z "$port_file" ]]; then
   exit 2
 fi
 
+if [[ -f $(rlocation com_github_digital_asset_daml/ledger/ledger-api-test-tool-on-canton/unique-contract-keys.conf) ]]; then
+  command+=("--config=$(rlocation com_github_digital_asset_daml/ledger/ledger-api-test-tool-on-canton/unique-contract-keys.conf)")
+fi
+
 # Change HOME since Canton uses ammonite in the default configuration, which tries to write to
 # ~/.ammonite/cache, which is read-only when sandboxing is enabled.
 HOME="$(mktemp -d)"
