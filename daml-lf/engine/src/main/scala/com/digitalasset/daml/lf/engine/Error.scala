c97c269ed1bde31d540f595bbe0b3a54b04eca76
Remy Haemmerle <Remy.Haemmerle@daml.com>
2020-11-11 13:04:48 +0100

    LF: Make node validation taking into account version
    We introduce in #7835 VersionedNode which associate to each node a
    version. In this PR, we update Transaction validation to take into
    account this version when comparing nodes.
    
    This advances the state of #7788
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/Error.scala
+++ daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/Error.scala
@@ -3,6 +3,7 @@
 
 package com.daml.lf
 package engine
+
 import com.daml.lf.value.Value
 import com.daml.lf.value.Value._
 
@@ -51,10 +52,7 @@ final case class ValidationError(override val msg: String)
 }
 
 final case class ReplayMismatch(
-    mismatch: transaction.ReplayMismatch[
-      transaction.NodeId,
-      ContractId,
-      Value.VersionedValue[ContractId]])
+    mismatch: transaction.ReplayMismatch[transaction.NodeId, ContractId])
     extends RuntimeException(s"ValidationError: ${mismatch.msg}", null, true, false)
     with Error {
   override def msg: String = mismatch.msg
