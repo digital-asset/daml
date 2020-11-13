7da8b5c55915e0e65869fd955079f85bd76ead2a
Kamil Bozek <kamil.bozek@digitalasset.com>
2020-11-13 15:33:40 +0100

    Unit test for TlsConfiguration OCSP handling

---
+++ ledger/ledger-api-common/src/test/suite/scala/com/digitalasset/ledger/api/tls/TlsConfigurationTest.scala
@@ -0,0 +1,38 @@
+package com.daml.ledger.api.tls
+
+import org.scalatest.WordSpec
+import java.security.Security
+
+import org.scalatest.Matchers
+
+class TlsConfigurationTest extends WordSpec with Matchers {
+  "TlsConfiguration" should {
+    "set OCSP JVM properties" in {
+      disableChecks()
+      verifyOCSP(DISABLED)
+
+      TlsConfiguration.Empty.copy(
+        enabled = true,
+        revocationChecks = true
+      ).setJvmTlsProperties()
+
+      verifyOCSP(ENABLED)
+    }
+  }
+
+  private val sslCheckRevocationProperty: String = "com.sun.net.ssl.checkRevocation"
+  private val ocspProperty: String = "ocsp.enable"
+  private val ENABLED = "true"
+  private val DISABLED = "false"
+
+  private def disableChecks(): Unit = {
+    System.setProperty(sslCheckRevocationProperty, DISABLED)
+    Security.setProperty(ocspProperty, DISABLED)
+  }
+
+  private def verifyOCSP(expectedValue: String) = {
+    System.getProperty(sslCheckRevocationProperty) shouldBe expectedValue
+    Security.getProperty(ocspProperty) shouldBe expectedValue
+  }
+
+}
