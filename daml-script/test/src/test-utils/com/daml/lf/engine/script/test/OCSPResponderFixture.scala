e5f604299c7a3389ea308cc7c9d8cec5c841492b
Kamil Bozek <kamil.bozek@digitalasset.com>
2020-11-13 12:02:19 +0100

    Managed resource for the OCSP Responder

--- daml-script/test/src/test-utils/com/daml/lf/engine/script/test/OCSPResponderFixture.scala
+++ daml-script/test/src/test-utils/com/daml/lf/engine/script/test/OCSPResponderFixture.scala
@@ -1,14 +1,25 @@
 package com.daml.lf.engine.script.test
 
 import com.daml.ledger.api.testing.utils.AkkaBeforeAndAfterAll
+import com.daml.ledger.api.testing.utils.OwnedResource
+import com.daml.ledger.api.testing.utils.{Resource => TestResource}
+import com.daml.ledger.resources.ResourceContext
+import com.daml.ledger.resources.ResourceOwner
+import com.daml.ledger.resources.Resource
+import com.daml.timer.RetryStrategy
 import org.scalatest.Suite
-import scala.concurrent.duration._
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 import scala.concurrent.Future
+import scala.sys.process._
 
 trait OCSPResponderFixture extends AkkaBeforeAndAfterAll { this: Suite =>
 
-  private val RESPONDER_START_TIMEOUT: Duration = 5.seconds
+  private val ec: ExecutionContext = system.dispatcher
+
+  private val RESPONDER_HOST: String = "127.0.0.1"
+  private val RESPONDER_PORT: Int = 2560
 
   def ocspCommandPath: String
   def indexPath: String
@@ -17,26 +28,62 @@ trait OCSPResponderFixture extends AkkaBeforeAndAfterAll { this: Suite =>
   def ocspCertPath: String
   def clientCertPath: String
 
-  override def beforeAll(): Unit = {
+  override protected def beforeAll(): Unit = {
     super.beforeAll()
-    Future(scala.sys.process.Process(ocspServerCommand).run())(system.dispatcher)
-    waitForResponderToStart()
-    sendTestRequest()
+    responderResource.setup()
   }
 
-  private def waitForResponderToStart(): Unit =
-    Thread.sleep(RESPONDER_START_TIMEOUT.toMillis)
-
-  private def sendTestRequest(): Unit = {
-    scala.sys.process.Process(testOCSPRequestCommand).!!
-    ()
+  override protected def afterAll(): Unit = {
+    responderResource.close()
+    super.afterAll()
   }
+
+  lazy val responderResource: TestResource[Any] = {
+    implicit val resourceContext: ResourceContext = ResourceContext(ec)
+    new OwnedResource[ResourceContext, Process](
+      owner = responderResourceOwner,
+      acquisitionTimeout = 10.seconds,
+      releaseTimeout = 5.seconds
+    )
+  }
+
+  private def responderResourceOwner: ResourceOwner[Process] =
+    new ResourceOwner[Process] {
+      /**
+       * Acquires the [[Resource]].
+       *
+       * @param context The acquisition context, including the asynchronous task execution engine.
+       * @return The acquired [[Resource]].
+       */
+      override def acquire()(implicit context: ResourceContext): Resource[Process] = {
+        def start(): Future[Process] =
+          for {
+            process <- startResponderProcess()
+            _ <- verifyResponderReady()
+          } yield process
+
+        def stop(responderProcess: Process): Future[Unit] =
+          Future {
+            responderProcess.destroy()
+          }.map(_ => ())
+
+        Resource(start())(stop)
+      }
+    }
+
+  private def startResponderProcess()(implicit ec: ExecutionContext): Future[Process] =
+    Future(Process(ocspServerCommand).run())
+
+  private def verifyResponderReady()(implicit ec: ExecutionContext): Future[String] =
+    RetryStrategy.constant(attempts = 3, waitTime = 5.seconds) { (_, _) =>
+      Future(Process(testOCSPRequestCommand).!!)
+    }
 
   private def ocspServerCommand = List(
     ocspCommandPath,
     "ocsp",
     "-port",
-    "2560",
+    RESPONDER_PORT.toString,
     "-text",
     "-index", indexPath,
     "-CA", caCertPath,
@@ -50,7 +97,7 @@ trait OCSPResponderFixture extends AkkaBeforeAndAfterAll { this: Suite =>
     "-CAfile",
     caCertPath,
     "-url",
-    "http://127.0.0.1:2560",
+    s"http://$RESPONDER_HOST:$RESPONDER_PORT",
     "-resp_text",
     "-issuer",
     caCertPath,
