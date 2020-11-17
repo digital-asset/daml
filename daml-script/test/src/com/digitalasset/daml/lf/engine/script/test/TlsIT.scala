35ea9d0e2546667bf3079dd53ee8940054ca4bce
Kamil Bozek <kamil.bozek@digitalasset.com>
2020-11-17 23:58:34 +0100

    Revert not relevant changes to daml-script

--- daml-script/test/src/com/digitalasset/daml/lf/engine/script/test/TlsIT.scala
+++ daml-script/test/src/com/digitalasset/daml/lf/engine/script/test/TlsIT.scala
@@ -10,31 +10,17 @@ import com.daml.ledger.api.testing.utils.SuiteResourceManagementAroundAll
 import com.daml.ledger.api.tls.TlsConfiguration
 import com.daml.lf.data.Ref._
 import com.daml.lf.engine.script.ScriptTimeMode
-import com.daml.lf.speedy.SValue
 import org.scalatest._
-
-import scala.concurrent.Future
 
 final class TlsIT
     extends AsyncWordSpec
     with SandboxParticipantFixture
     with Matchers
     with SuiteResourceManagementAroundAll {
-
   val (dar, envIface) = readDar(stableDarFile)
 
-  val List(
-    serverCrt,
-    serverPem,
-    caCrt,
-    clientCrt,
-    clientPem) = {
-    List(
-      "server.crt",
-      "server.pem",
-      "ca.crt",
-      "client.crt",
-      "client.pem").map { src =>
+  val List(serverCrt, serverPem, caCrt, clientCrt, clientPem) = {
+    List("server.crt", "server.pem", "ca.crt", "client.crt", "client.pem").map { src =>
       Some(new File(rlocation("ledger/test-common/test-certificates/" + src)))
     }
   }
@@ -43,33 +29,28 @@ final class TlsIT
 
   override protected def config =
     super.config
-      .copy(
-        tlsConfig = Some(
-          TlsConfiguration(enabled = true, serverCrt, serverPem, caCrt)))
+      .copy(tlsConfig = Some(TlsConfiguration(enabled = true, serverCrt, serverPem, caCrt)))
 
-  "DAML Script against ledger with TLS" should {
-    "create and accept Proposal" in {
-      executeSampleRequest(clientCrt, clientPem)
-        .map(_ => succeed) // No assertion, we just want to see that it succeeds
+  "DAML Script against ledger with TLS" can {
+    "test0" should {
+      "create and accept Proposal" in {
+        for {
+          clients <- participantClients(
+            tlsConfiguration = TlsConfiguration(
+              enabled = true,
+              keyCertChainFile = clientCrt,
+              keyFile = clientPem,
+              trustCertCollectionFile = caCrt
+            ))
+          _ <- run(
+            clients,
+            QualifiedName.assertFromString("ScriptTest:test0"),
+            dar = dar,
+          )
+        } yield
+        // No assertion, we just want to see that it succeeds
+        succeed
+      }
     }
   }
-
-  private def executeSampleRequest(
-      keyCertChainFile: Option[File],
-      keyFile: Option[File]): Future[SValue] = {
-    participantClients(
-      tlsConfiguration = TlsConfiguration(
-        enabled = true,
-        keyCertChainFile = keyCertChainFile,
-        keyFile = keyFile,
-        trustCertCollectionFile = caCrt
-      )).flatMap { clients =>
-      run(
-        clients,
-        QualifiedName.assertFromString("ScriptTest:test0"),
-        dar = dar,
-      )
-    }
-  }
-
 }
