3a35535ad4d15ea30eae03d891eabb224b257e07
Samir Talwar <samir.talwar@digitalasset.com>
2020-11-18 11:50:33 +0100

    extractor: Make VeryLargeArchiveSpec an async spec.

--- extractor/src/test/suite/scala/com/digitalasset/extractor/VeryLargeArchiveSpec.scala
+++ extractor/src/test/suite/scala/com/digitalasset/extractor/VeryLargeArchiveSpec.scala
@@ -10,14 +10,13 @@ import com.daml.extractor.services.ExtractorFixture
 import com.daml.grpc.{GrpcException, GrpcStatus}
 import com.daml.ledger.api.testing.utils.SuiteResourceManagementAroundAll
 import com.daml.testing.postgresql.PostgresAroundEach
-import io.grpc.{Status, StatusRuntimeException}
+import io.grpc.Status
 import org.scalatest._
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.Future
 
 class VeryLargeArchiveSpec
-    extends FlatSpec
+    extends AsyncFlatSpec
     with Suite
     with PostgresAroundEach
     with SuiteResourceManagementAroundAll
@@ -26,14 +25,15 @@ class VeryLargeArchiveSpec
     with Inside {
   override protected def darFile = new File(rlocation("extractor/VeryLargeArchive.dar"))
 
-  private def runWithInboundLimit[Z](bytes: Int)(f: => Z): Z = {
+  private def runWithInboundLimit[Z](bytes: Int)(f: => Z): Future[Z] = {
     val config = baseConfig.copy(ledgerPort = serverPort, ledgerInboundMessageSizeMax = bytes)
     val extractor =
       new Extractor(config, target)()
 
-    Await.result(extractor.run(), Duration.Inf) // as with ExtractorFixture#run
-    try f
-    finally Await.result(extractor.shutdown(), Duration.Inf) // as with ExtractorFixture#kill
+    extractor
+      .run() // as with ExtractorFixture#run
+      .map(_ => f)
+      .andThen { case _ => extractor.shutdown() } // as with ExtractorFixture#kill
   }
 
   // there are a couple goals with these choices:
@@ -51,10 +51,9 @@ class VeryLargeArchiveSpec
   val successMB = 10
 
   s"${failMB}MiB" should "fail" in {
-    val e = the[StatusRuntimeException] thrownBy runWithInboundLimit(failMB * 1024 * 1024) {
+    runWithInboundLimit(failMB * 1024 * 1024) {
       fail("shouldn't successfully run")
-    }
-    inside(e) {
+    }.recover {
       case GrpcException((GrpcStatus(Status.Code.`RESOURCE_EXHAUSTED`, Some(description)), _)) =>
         description should startWith("gRPC message exceeds maximum size")
     }
@@ -62,7 +61,7 @@ class VeryLargeArchiveSpec
 
   s"${successMB}MiB" should "succeed" in {
     runWithInboundLimit(successMB * 1024 * 1024) {
-      ()
+      succeed
     }
   }
 }
