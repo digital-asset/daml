2ff4d923a98ed42dd3c4fd701f24d1619f145341
fabiotudone-da <fabio.tudone@digitalasset.com>
2020-11-19 15:09:23 +0100

    [KV] proto: add optional DamlTransactionBlindingInfo to DamlTransactionEntry [KVL-736] (#8005)
    * KV proto: add optional DamlTransactionBlindingInfo to DamlTransactionEntry
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Wording
    
    * Wording
    
    * Rename party -> <action>_to for consistency
    
    * Treat contract IDs as opaque strings (and for consistency)
    
    * Address review comments

--- daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/BlindingInfo.scala
+++ daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/BlindingInfo.scala
@@ -7,19 +7,20 @@ import com.daml.lf.data.Ref.Party
 import com.daml.lf.data.Relation.Relation
 import com.daml.lf.value.Value.ContractId
 
-/** This gives disclosure and divulgence info.
+/** A transaction's blinding information, consisting of disclosure and
+  * divulgence info.
   *
-  * "Disclosure" tells us which nodes to communicate to which parties.
-  * See also https://docs.daml.com/concepts/ledger-model/ledger-privacy.html
+  * "Disclosure" tells us which transaction nodes to communicate to which
+  * parties.
+  * See https://docs.daml.com/concepts/ledger-model/ledger-privacy.html
   *
-  * "Divulgence" tells us what to communicate to
-  * each participant node so that they can perform post-commit
-  * validation. Note that divulgence can also divulge
-  * contract ids -- e.g. contract ids that were created
-  * _outside_ this transaction.
+  * "Divulgence" tells us which contracts to communicate to which parties
+  * so that their participant nodes can perform post-commit validation.
+  * Note that divulgence can also divulge e.g. contract IDs that were
+  * created _outside_ this transaction.
   * See also https://docs.daml.com/concepts/ledger-model/ledger-privacy.html#divulgence-when-non-stakeholders-see-contracts
   *
-  * @param disclosure Disclosure, specified in terms of local node IDs
+  * @param disclosure Disclosure, specified in terms of local transaction node IDs
   * @param divulgence
   *     Divulgence, specified in terms of contract IDs.
   *     Note that if this info was produced by blinding a transaction
