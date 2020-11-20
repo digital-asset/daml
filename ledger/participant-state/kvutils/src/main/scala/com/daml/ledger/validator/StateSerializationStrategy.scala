9d10b8ad87e4646ad0ed83b7969a5a72eaef1a08
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2020-11-20 17:04:08 +0100

    Renamed serializeOneState => serializeState. CHANGELOG_BEGIN CHANGELOG_END

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/StateSerializationStrategy.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/StateSerializationStrategy.scala
@@ -10,11 +10,11 @@ import com.daml.ledger.validator.LedgerStateOperations.{Key, Value}
 import scala.collection.{SortedMap, breakOut}
 
 final class StateSerializationStrategy(keyStrategy: StateKeySerializationStrategy) {
-  def serializeOneState(key: DamlStateKey, value: DamlStateValue): (Key, Value) =
+  def serializeState(key: DamlStateKey, value: DamlStateValue): (Key, Value) =
     (keyStrategy.serializeStateKey(key), Envelope.enclose(value))
 
   def serializeStateUpdates(state: Map[DamlStateKey, DamlStateValue]): SortedMap[Key, Value] =
     state.map {
-      case (key, value) => serializeOneState(key, value)
+      case (key, value) => serializeState(key, value)
     }(breakOut)
 }
