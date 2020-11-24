1bd98c068d8124e37d46c8da16b3690297198464
fabiotudone-da <fabio.tudone@digitalasset.com>
2020-11-24 09:17:01 +0100

    Address review comments in #8009 and #8012 [KVL-736] (#8024)
    * Address review comments in #8009 and #8012
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Tidy up
    
    * Make test more meaningful and precise

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/ConversionsSpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/ConversionsSpec.scala
@@ -20,72 +20,66 @@ import com.daml.lf.transaction.{BlindingInfo, NodeId}
 import com.daml.lf.value.Value.ContractId
 import org.scalatest.{Matchers, WordSpec}
 
-import scala.collection.immutable.ListSet
-
 import scala.collection.JavaConverters._
+import scala.collection.immutable.{ListMap, ListSet}
 
 class ConversionsSpec extends WordSpec with Matchers {
   "Conversions" should {
     "correctly and deterministically encode Blindinginfo" in {
-      encodeBlindingInfo(blindingInfoNotInOrder) shouldBe orderedEncodedBlindingInfo
+      encodeBlindingInfo(wronglySortedBlindingInfo) shouldBe correctlySortedEncodedBlindingInfo
     }
 
     "correctly decode BlindingInfo" in {
-      decodeBlindingInfo(orderedEncodedBlindingInfo) shouldBe orderedBlindingInfo
+      val decodedBlindingInfo = decodeBlindingInfo(correctlySortedEncodedBlindingInfo)
+      decodedBlindingInfo.disclosure.toSet should contain theSameElementsAs wronglySortedBlindingInfo.disclosure.toSet
+      decodedBlindingInfo.divulgence.toSet should contain theSameElementsAs wronglySortedBlindingInfo.divulgence.toSet
     }
   }
 
+  private def newDisclosureEntry(node: NodeId, parties: List[String]) =
+    DisclosureEntry.newBuilder
+      .setNodeId(node.index.toString)
+      .addAllDisclosedToLocalParties(parties.asJava)
+      .build
+
+  private def newDivulgenceEntry(contractId: String, parties: List[String]) =
+    DivulgenceEntry.newBuilder
+      .setContractId(contractId)
+      .addAllDivulgedToLocalParties(parties.asJava)
+      .build
+
   private lazy val party0: Party = Party.assertFromString("party0")
   private lazy val party1: Party = Party.assertFromString("party1")
-  private lazy val partySetNotInOrder = ListSet(party1, party0)
-  private lazy val hashesNotInOrder: List[Hash] =
-    List(crypto.Hash.hashPrivateKey("hash0"), crypto.Hash.hashPrivateKey("hash1")).sorted.reverse
-  private lazy val contractId0 = ContractId.V1(hashesNotInOrder.tail.head)
-  private lazy val contractId1 = ContractId.V1(hashesNotInOrder.head)
+  private lazy val contractId0: ContractId = ContractId.V1(wronglySortedHashes.tail.head)
+  private lazy val contractId1: ContractId = ContractId.V1(wronglySortedHashes.head)
   private lazy val node0: NodeId = NodeId(0)
   private lazy val node1: NodeId = NodeId(1)
-  private lazy val disclosureNotInOrder: Relation[NodeId, Party] =
-    Map(node1 -> partySetNotInOrder, node0 -> partySetNotInOrder)
-  private lazy val divulgenceNotInOrder: Relation[ContractId, Party] =
-    Map(contractId1 -> partySetNotInOrder, contractId0 -> partySetNotInOrder)
-  private lazy val blindingInfoNotInOrder = BlindingInfo(
-    disclosure = disclosureNotInOrder,
-    divulgence = divulgenceNotInOrder,
+  private lazy val wronglySortedPartySet = ListSet(party1, party0)
+  private lazy val wronglySortedHashes: List[Hash] =
+    List(crypto.Hash.hashPrivateKey("hash0"), crypto.Hash.hashPrivateKey("hash1")).sorted.reverse
+  private lazy val wronglySortedDisclosure: Relation[NodeId, Party] =
+    ListMap(node1 -> wronglySortedPartySet, node0 -> wronglySortedPartySet)
+  private lazy val wronglySortedDivulgence: Relation[ContractId, Party] =
+    ListMap(contractId1 -> wronglySortedPartySet, contractId0 -> wronglySortedPartySet)
+  private lazy val wronglySortedBlindingInfo = BlindingInfo(
+    disclosure = wronglySortedDisclosure,
+    divulgence = wronglySortedDivulgence,
   )
-  private lazy val orderedPartySet = Set(party0, party1)
-  private lazy val orderedDisclosure: Relation[NodeId, Party] =
-    Map(node0 -> orderedPartySet, node1 -> orderedPartySet)
-  private lazy val orderedDivulgence: Relation[ContractId, Party] =
-    Map(contractId0 -> orderedPartySet, contractId1 -> orderedPartySet)
-  private lazy val orderedBlindingInfo = BlindingInfo(
-    disclosure = orderedDisclosure,
-    divulgence = orderedDivulgence,
-  )
-  private lazy val partiesInOrder = List(party0, party1)
-  private lazy val orderedEncodedBlindingInfo =
+  private lazy val correctlySortedParties = List(party0, party1)
+  private lazy val correctlySortedPartiesAsStrings =
+    correctlySortedParties.asInstanceOf[List[String]]
+  private lazy val correctlySortedEncodedBlindingInfo =
     DamlTransactionBlindingInfo.newBuilder
       .addAllDisclosures(
         List(
-          DisclosureEntry.newBuilder
-            .setNodeId(node0.index.toString)
-            .addAllDisclosedToLocalParties(partiesInOrder.asInstanceOf[List[String]].asJava)
-            .build,
-          DisclosureEntry.newBuilder
-            .setNodeId(node1.index.toString)
-            .addAllDisclosedToLocalParties(partiesInOrder.asInstanceOf[List[String]].asJava)
-            .build,
+          newDisclosureEntry(node0, correctlySortedPartiesAsStrings),
+          newDisclosureEntry(node1, correctlySortedPartiesAsStrings),
         ).asJava
       )
       .addAllDivulgences(
         List(
-          DivulgenceEntry.newBuilder
-            .setContractId(contractId0.coid)
-            .addAllDivulgedToLocalParties(partiesInOrder.asInstanceOf[List[String]].asJava)
-            .build,
-          DivulgenceEntry.newBuilder
-            .setContractId(contractId1.coid)
-            .addAllDivulgedToLocalParties(partiesInOrder.asInstanceOf[List[String]].asJava)
-            .build,
+          newDivulgenceEntry(contractId0.coid, correctlySortedPartiesAsStrings),
+          newDivulgenceEntry(contractId1.coid, correctlySortedPartiesAsStrings),
         ).asJava
       )
       .build
