98f930f86af760986c73f995dfd80211b0c82c69
Stefano Baghino <stefano.baghino@digitalasset.com>
2020-11-25 10:17:14 +0100

    Add comment to detail what SET STORAGE EXTERNAL does

--- ledger/participant-integration-api/src/main/resources/db/migration/postgres/V40__explicit_compression.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/postgres/V40__explicit_compression.sql
@@ -1,6 +1,14 @@
 -- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
+-- SET STORAGE EXTERNAL is for external (from the standpoint of TOAST), uncompressed data.
+--
+-- Note that SET STORAGE doesn't itself change anything in the table,
+-- it just sets the strategy to be pursued during future table updates.
+--
+-- `SET STORAGE EXTERNAL` https://www.postgresql.org/docs/13/sql-altertable.html
+-- TOAST https://www.postgresql.org/docs/13/storage-toast.html
+
 ALTER TABLE participant_events
     ADD COLUMN   create_argument_compression SMALLINT,
     ALTER COLUMN create_argument SET STORAGE EXTERNAL,
