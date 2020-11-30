a1d3456c9e0fe787b5bb8f6106ec77c70d5298c8
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-11-30 15:06:16 +0100

    debug borked lsof
    changelog_begin
    changelog_end

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ fi
 # a cheaper solution than having to reset the node.
 # Note that lsof returns a non-zero exit code if there is no match.
 SANDBOX_PID="$(lsof -ti tcp:6865 || true)"
+echo "'$SANDBOX_PID'"
 if [ -n "$SANDBOX_PID" ]; then
     kill "$SANDBOX_PID"
 fi
