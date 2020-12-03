38de5b1eb35f3ec8c5102211d981abd6574db2d3
Samir Talwar <samir.talwar@digitalasset.com>
2020-12-03 18:07:42 +0100

    daml-lf: Remove aliases that have been deprecated since v1.4. (#8155)
    CHANGELOG_BEGIN
    - [DAML-LF] Some types that were deprecated in DAML 1.4 have been
      removed. Please fix any deprecation warnings by using the new types
      before upgrading.
    CHANGELOG_END

--- ledger/participant-integration-api/src/test/suite/scala/platform/apiserver/execution/StoreBackedCommandExecutorSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/platform/apiserver/execution/StoreBackedCommandExecutorSpec.scala
@@ -10,8 +10,8 @@ import com.daml.lf.crypto.Hash
 import com.daml.lf.data.Ref.ParticipantId
 import com.daml.lf.data.{ImmArray, Ref, Time}
 import com.daml.lf.engine.{Engine, ResultDone}
-import com.daml.lf.transaction.Transaction
 import com.daml.lf.transaction.test.TransactionBuilder
+import com.daml.lf.transaction.{SubmittedTransaction, Transaction}
 import com.daml.logging.LoggingContext
 import com.daml.metrics.Metrics
 import org.mockito.ArgumentMatchers._
@@ -34,7 +34,7 @@ class StoreBackedCommandExecutorSpec extends AsyncWordSpec with MockitoSugar wit
       val mockEngine = mock[Engine]
       when(mockEngine.submit(any[com.daml.lf.command.Commands], any[ParticipantId], any[Hash]))
         .thenReturn(
-          ResultDone[(Transaction.SubmittedTransaction, Transaction.Metadata)](
+          ResultDone[(SubmittedTransaction, Transaction.Metadata)](
             (TransactionBuilder.EmptySubmitted, emptyTransactionMetadata)
           )
         )
