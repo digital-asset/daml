56c876ba4657c7371b7dea55f202c79566cc1856
Miklos <57664299+miklos-da@users.noreply.github.com>
2020-12-03 15:21:11 +0100

    kvutils: Simplified CommitContext [KVL-750] (#8146)
    * Converted CommitContext into a class.
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * newCommitContext => createCommitContext
    
    * Made CommitContext a case class.
    
    * Apply suggestions from code review
    
    Co-authored-by: Samir Talwar <samir.talwar@digitalasset.com>
    
    * Made CommitContext a case class.
    
    Co-authored-by: Samir Talwar <samir.talwar@digitalasset.com>

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/committer/CommitContext.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/committer/CommitContext.scala
@@ -21,10 +21,12 @@ import scala.collection.mutable
 /** Commit context provides access to state inputs, commit parameters (e.g. record time) and
   * allows committer to set state outputs.
   */
-private[kvutils] trait CommitContext {
+private[kvutils] case class CommitContext(
+    private val inputs: DamlStateMap,
+    recordTime: Option[Timestamp],
+    participantId: ParticipantId,
+) {
   private[this] val logger = LoggerFactory.getLogger(this.getClass)
-
-  protected def inputs: DamlStateMap
 
   // NOTE(JM): The outputs must be iterable in deterministic order, hence we
   // keep track of insertion order.
@@ -42,11 +44,7 @@ private[kvutils] trait CommitContext {
   // pre-execution.
   var outOfTimeBoundsLogEntry: Option[DamlLogEntry] = None
 
-  def getRecordTime: Option[Timestamp]
-
-  def getParticipantId: ParticipantId
-
-  def preExecute: Boolean = getRecordTime.isEmpty
+  def preExecute: Boolean = recordTime.isEmpty
 
   /** Retrieve value from output state, or if not found, from input state. */
   def get(key: DamlStateKey): Option[DamlStateValue] =
