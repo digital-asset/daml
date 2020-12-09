a705bbc510865aa106326cb1864e5bcd21097b52
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2020-12-09 18:48:04 +0100

    Ledger api server index h2 database migration fix
    Dropping reference to non-existing `INDEX_2` index
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/participant-integration-api/src/main/resources/db/migration/h2database/V28__multiple_submitters.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/h2database/V28__multiple_submitters.sql
@@ -8,5 +8,4 @@ alter table participant_events drop column submitter;
 alter table participant_command_completions add column submitters array;
 update participant_command_completions set submitters = array[submitting_party];
 create index participant_command_completions_idx on participant_command_completions(completion_offset, application_id, submitters);
-drop index PUBLIC.INDEX_2;
 alter table participant_command_completions drop column submitting_party;
