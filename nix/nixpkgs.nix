ac8d5967ed3520f766be84c207402f122ce32a29
Brian Healey <brian.healey@digitalasset.com>
2020-12-11 10:41:15 -0500

    duplicate nodejs and grpc

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -19,17 +19,6 @@ let
         sha256 = "1jcyd9jy7kz5zfch25s4inwlivb1y1w52fzfjy5ra5vcnp3hmqyr";
         fetchSubmodules = true;
       };
-    });
-    nodejs = pkgs.nodejs-12_x;
-    grpc = pkgs.grpc.overrideAttrs (oldAttrs: {
-      version = "1.23.1";
-      src = pkgs.fetchFromGitHub {
-        owner = "grpc";
-        repo = "grpc";
-        rev = "v1.23.1";
-        sha256 = "1jcyd9jy7kz5zfch25s4inwlivb1y1w52fzfjy5ra5vcnp3hmqyr";
-        fetchSubmodules = true;
-      };
       # Upstream nixpkgs applies patches that are incompatbile with our version
       # of grpc. So, we disable them.
       patches = [
