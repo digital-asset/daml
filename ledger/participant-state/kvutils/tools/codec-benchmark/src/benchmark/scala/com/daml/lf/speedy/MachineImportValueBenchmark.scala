c50e19072da9ab18b4fc9089192347af30c5f80b
Stefano Baghino <stefano.baghino@digitalasset.com>
2020-12-14 09:47:28 +0100

    Address https://github.com/digital-asset/daml/pull/8272#discussion_r542194308

--- ledger/participant-state/kvutils/tools/codec-benchmark/src/benchmark/scala/com/daml/lf/speedy/MachineImportValueBenchmark.scala
+++ ledger/participant-state/kvutils/tools/codec-benchmark/src/benchmark/scala/com/daml/lf/speedy/MachineImportValueBenchmark.scala
@@ -3,7 +3,10 @@
 
 package com.daml.lf.speedy
 
+import com.daml.lf.CompiledPackages
 import com.daml.lf.benchmark.{BenchmarkWithLedgerExport, DecodedValue, assertDecode}
+import com.daml.lf.data.Time
+import com.daml.lf.speedy.Speedy.Machine
 import org.openjdk.jmh.annotations.{Benchmark, Setup}
 
 class MachineImportValueBenchmark extends BenchmarkWithLedgerExport {
@@ -11,11 +14,23 @@ class MachineImportValueBenchmark extends BenchmarkWithLedgerExport {
   private var machine: Speedy.Machine = _
   private var decodedValues: Vector[DecodedValue] = _
 
+  // Construct a machine for testing.
+  private def testingMachine(
+      compiledPackages: CompiledPackages,
+  ): Machine = Machine(
+    compiledPackages = compiledPackages,
+    submissionTime = Time.Timestamp.MinValue,
+    initialSeeding = InitialSeeding.NoSeed,
+    expr = null,
+    globalCids = Set.empty,
+    committers = Set.empty,
+  )
+
   @Setup
   override def setup(): Unit = {
     super.setup()
     decodedValues = submissions.values.map(_.value).map(assertDecode).toVector
-    machine = Speedy.Machine.dummy(submissions.compiledPackages)
+    machine = testingMachine(submissions.compiledPackages)
   }
 
   @Benchmark
