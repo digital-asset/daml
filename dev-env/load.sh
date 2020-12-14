f3cdbd2d9f97504253c54142e5bea532f247236b
Samir Talwar <samir.talwar@digitalasset.com>
2020-12-14 18:20:08 +0100

    dev-env: Run `ensure-nix` as part of `load.sh`.

--- dev-env/load.sh
+++ dev-env/load.sh
@@ -4,5 +4,8 @@
 
 # Source this file to add all tools to your PATH.
 
+# shellcheck source=./lib/ensure-nix
+source "$(dirname "${BASH_SOURCE[0]}")/lib/ensure-nix"
+# shellcheck disable=SC2016
 PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && nix-shell --pure --run 'echo $PATH'):${PATH}"
 export PATH
