eb8859824080ccd8fe3c21416a9f450b9d01fe3f
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2020-12-15 18:26:17 +0100

    .

--- nix/tools/hello/src/hello.c
+++ nix/tools/hello/src/hello.c
@@ -1,3 +1,6 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 #include <stdio.h>
 
 // hopefully this is unique enough that we build it :shrug:
@@ -7,3 +10,4 @@ int main(int argc, char const *argv[])
         printf("Hello, Nix world!\n");
             return 0;
 }
+
