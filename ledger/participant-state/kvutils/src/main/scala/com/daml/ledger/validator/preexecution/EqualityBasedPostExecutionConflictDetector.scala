c7ebb4e11eff90380874a751f0546f0c10d36b61
Samir Talwar <samir.talwar@digitalasset.com>
2020-12-17 10:13:25 +0100

    kvutils: Make equality-based post execution even more generic.

--- ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/preexecution/EqualityBasedPostExecutionConflictDetector.scala
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/validator/preexecution/EqualityBasedPostExecutionConflictDetector.scala
@@ -3,21 +3,15 @@
 
 package com.daml.ledger.validator.preexecution
 
-import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlStateKey
 import com.daml.ledger.validator.reading.StateReader
 
 import scala.concurrent.{ExecutionContext, Future}
 
-final class EqualityBasedPostExecutionConflictDetector[StateValue]
-    extends PostExecutionConflictDetector[
-      DamlStateKey,
-      StateValue,
-      Map[DamlStateKey, StateValue],
-      Any,
-    ] {
+final class EqualityBasedPostExecutionConflictDetector[StateKey, StateValue]
+    extends PostExecutionConflictDetector[StateKey, StateValue, Map[StateKey, StateValue], Any] {
   override def detectConflicts(
-      preExecutionOutput: PreExecutionOutput[Map[DamlStateKey, StateValue], Any],
-      reader: StateReader[DamlStateKey, StateValue],
+      preExecutionOutput: PreExecutionOutput[Map[StateKey, StateValue], Any],
+      reader: StateReader[StateKey, StateValue],
   )(implicit executionContext: ExecutionContext): Future[Unit] = {
     val (keys, preExecutionValues) = preExecutionOutput.readSet.unzip
     reader.read(keys).map { currentValues =>
