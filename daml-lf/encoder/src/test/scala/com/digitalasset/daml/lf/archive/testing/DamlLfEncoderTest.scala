57aa4993e9ca33af3db94d9414f932465e148272
Remy <remy.haemmerle@daml.com>
2021-01-04 16:15:06 +0100

    LF: test LF 1.11  in DamlLfEncoderTest (#8365)
    This is part of #7139.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- daml-lf/encoder/src/test/scala/com/digitalasset/daml/lf/archive/testing/DamlLfEncoderTest.scala
+++ daml-lf/encoder/src/test/scala/com/digitalasset/daml/lf/archive/testing/DamlLfEncoderTest.scala
@@ -47,13 +47,15 @@ class DamlLfEncoderTest
 
       val modules_1_7 = modules_1_6 + "NumericMod" + "AnyMod"
       val modules_1_8 = modules_1_7 + "SynonymMod"
-      val modules_1_dev = modules_1_8 + "GenMapMod"
+      val modules_1_11 = modules_1_8 + "GenMapMod"
+      val modules_1_dev = modules_1_11
 
       val versions = Table(
         "versions" -> "modules",
         "1.6" -> modules_1_6,
         "1.7" -> modules_1_7,
         "1.8" -> modules_1_8,
+        "1.11" -> modules_1_11,
         "1.dev" -> modules_1_dev
       )
 
