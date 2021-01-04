digital-asset/daml

commit 2083f74cf29b35fc4f2478c989e48ff110ebeed6

add docs.daml.com/latest (#8372)
This commit changes the docs cron to create a new file
`docs.daml.com/latest`, a simple text file containing the version number
of the latest released version. This is done in response to #8354, to
avoid having to replicate the logic for which version is the latest
across this, the assistant and the get-daml.sh script.

This commit also cleans up a small transitional FIXME in the docs cron
regarding the transition from `snapshots.json` to `hidden.json`.

For the solution to #8354 to be complete, we'll also need to update
get-daml.sh and the assistant to use the new latest file. Note that this
file would only be published on the next stable release, so this commit
also includes a temporary hack to re-generate it (and `versions.json`
and `hidden.json`) unconditionally on every run; this can be removed as
soon as this has run once.

CHANGELOG_BEGIN
CHANGELOG_END

ci/cron/src/Main.hs
@@ -128,14 +128,16 @@ fetch_if_missing opts temp v = do
 
 update_s3 :: DocOptions -> FilePath -> Versions -> IO ()
 update_s3 opts temp vs = do
-    putStrLn "Updating versions.json & hidden.json..."
+    putStrLn "Updating versions.json, hidden.json, latest..."
     create_versions_json (dropdown vs) (temp </> "versions.json")
     let hidden = Data.List.sortOn Data.Ord.Down $ Set.toList $ all_versions vs `Set.difference` (Set.fromList $ dropdown vs)
     create_versions_json hidden (temp </> "hidden.json")
-    shell_ $ "aws s3 cp " <> temp </> "versions.json " <> s3Path opts "versions.json" <> " --acl public-read"
-    shell_ $ "aws s3 cp " <> temp </> "hidden.json " <> s3Path opts "hidden.json" <> " --acl public-read"
-    -- FIXME: remove after running once (and updating the reading bit in this file)
-    shell_ $ "aws s3 cp " <> temp </> "hidden.json " <> s3Path opts "snapshots.json" <> " --acl public-read"
+    let push f = shell_ $ "aws s3 cp " <> temp </> f <> s3Path opts f <> " --acl public-read"
+    push "versions.json"
+    push "hidden.json"
+    Control.Monad.Extra.whenJust (top vs) $ \latest -> do
+        writeFile (temp </> "latest") (show latest)
+        push "latest"
     putStrLn "Done."
     where
         create_versions_json versions file = do
@@ -238,8 +240,7 @@ fetch_s3_versions opts = do
     -- We could technically remove the catch later.
     dropdown <- fetch "versions.json" False `catchIO`
       (\_ -> pure [])
-    -- TODO: read hidden.json after this has run once
-    hidden <- fetch "snapshots.json" True `catchIO`
+    hidden <- fetch "hidden.json" True `catchIO`
       (\_ -> pure [])
     return $ versions $ dropdown <> hidden
     where fetch file prerelease = do
@@ -454,4 +455,9 @@ main = do
       Docs -> do
           docs sdkDocOpts
           docs damlOnSqlDocOpts
+          -- FIXME: this is meant to run once to create the `latest` file
+          -- without waiting for the next stable release.
+          gh_versions <- fetch_gh_versions (includedVersion sdkDocOpts)
+          IO.withTempDir $ \temp_dir -> do
+              update_s3 sdkDocOpts temp_dir gh_versions
       Check { bash_lib, gcp_credentials, max_releases } -> check_releases gcp_credentials bash_lib max_releases

commit 13dfbe4f4da4a78511139643b9d8624ee0bdd557

fix typo in docs cron
Introduced in #8372.

CHANGELOG_BEGIN
CHANGELOG_END

ci/cron/src/Main.hs
@@ -132,7 +132,7 @@ update_s3 opts temp vs = do
     create_versions_json (dropdown vs) (temp </> "versions.json")
     let hidden = Data.List.sortOn Data.Ord.Down $ Set.toList $ all_versions vs `Set.difference` (Set.fromList $ dropdown vs)
     create_versions_json hidden (temp </> "hidden.json")
-    let push f = shell_ $ "aws s3 cp " <> temp </> f <> s3Path opts f <> " --acl public-read"
+    let push f = shell_ $ "aws s3 cp " <> temp </> f <> " " <> s3Path opts f <> " --acl public-read"
     push "versions.json"
     push "hidden.json"
     Control.Monad.Extra.whenJust (top vs) $ \latest -> do