74ed9e14c052946b6b84dd22498e236c19e9981a
Fabio Tudone <fabio.tudone@digitalasset.com>
2021-01-05 10:45:08 +0100

    Remove test about double load not happening with DAML contract key inputs

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/preexecution/PreExecutingSubmissionValidatorSpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/validator/preexecution/PreExecutingSubmissionValidatorSpec.scala
@@ -34,11 +34,7 @@ import org.scalatest.wordspec.AsyncWordSpec
 import scala.collection.JavaConverters._
 import scala.concurrent.{ExecutionContext, Future}
 
-class PreExecutingSubmissionValidatorSpec
-    extends AsyncWordSpec
-    with Matchers
-    with MockitoSugar
-    with ArgumentMatchersSugar {
+class PreExecutingSubmissionValidatorSpec extends AsyncWordSpec with Matchers with MockitoSugar {
   private implicit val loggingContext: LoggingContext = LoggingContext.ForTesting
 
   "validate" should {
@@ -71,35 +67,6 @@ class PreExecutingSubmissionValidatorSpec
           actual.outOfTimeBoundsWriteSet shouldBe expectedOutOfTimeBoundsWriteSet
           actual.readSet shouldBe TestReadSet(expectedReadSet)
           actual.involvedParticipants shouldBe expectedInvolvedParticipants
-        }
-    }
-
-    "read from ledger state only once when a DAML contract key is in the read-set" in {
-      // Given a KV state key input of a DAML contract key, de-referencing it at validation time
-      // to force conflict detection also on the KV state key of the corresponding DAML contract ID
-      // harms performance (because it requires an extra read) and it is useless, because,
-      // if the KV state value of such a DAML contract key (i.e., the referenced contract ID)
-      // has changed between inputs fetching and conflict-detection, then the associated fingerprint
-      // will have changed as well and conflict detection will raise a red flag anyway.
-
-      val expectedReadSet = Set(
-        makeContractKeyStateKey("a template"),
-      )
-      val instance = createInstance(expectedReadSet = expectedReadSet)
-
-      val ledgerStateReaderMock = mock[StateReader[DamlStateKey, TestValue]]
-      val ledgerStateReaderMockResult = Seq(
-        TestValue(Some(makeContractKeyStateValue("contract ID")))
-      )
-      when(ledgerStateReaderMock.read(any[Seq[DamlStateKey]])(any[ExecutionContext]))
-        .thenReturn(Future.successful(ledgerStateReaderMockResult))
-
-      instance
-        .validate(anEnvelope(expectedReadSet), aParticipantId, ledgerStateReaderMock)
-        .map { _ =>
-          verify(ledgerStateReaderMock, times(1))
-            .read(any[Seq[DamlStateKey]])(any[ExecutionContext])
-          succeed
         }
     }
 
@@ -207,9 +174,11 @@ object PreExecutingSubmissionValidatorSpec {
   private final case class TestValue(value: Option[DamlStateValue])
 
   private object TestValue {
+
     implicit object `TestValue has DamlStateValue` extends HasDamlStateValue[TestValue] {
       override def damlStateValue(value: TestValue): Option[DamlStateValue] = value.value
     }
+
   }
 
   private final case class TestReadSet(keys: Set[DamlStateKey])
@@ -226,13 +195,13 @@ object PreExecutingSubmissionValidatorSpec {
   }
 
   private def createInstance(
-      expectedReadSet: Set[DamlStateKey] = Set.empty,
-      expectedMinRecordTime: Option[Instant] = None,
-      expectedMaxRecordTime: Option[Instant] = None,
-      expectedSuccessWriteSet: TestWriteSet = TestWriteSet(""),
-      expectedOutOfTimeBoundsWriteSet: TestWriteSet = TestWriteSet(""),
-      expectedInvolvedParticipants: Set[ParticipantId] = Set.empty,
-  ): PreExecutingSubmissionValidator[TestValue, TestReadSet, TestWriteSet] = {
+                              expectedReadSet: Set[DamlStateKey] = Set.empty,
+                              expectedMinRecordTime: Option[Instant] = None,
+                              expectedMaxRecordTime: Option[Instant] = None,
+                              expectedSuccessWriteSet: TestWriteSet = TestWriteSet(""),
+                              expectedOutOfTimeBoundsWriteSet: TestWriteSet = TestWriteSet(""),
+                              expectedInvolvedParticipants: Set[ParticipantId] = Set.empty,
+                            ): PreExecutingSubmissionValidator[TestValue, TestReadSet, TestWriteSet] = {
     val mockCommitter = mock[KeyValueCommitting]
     val result = PreExecutionResult(
       expectedReadSet,
@@ -281,21 +250,21 @@ object PreExecutingSubmissionValidatorSpec {
   }
 
   private def createLedgerStateReader(
-      inputState: Map[DamlStateKey, Option[DamlStateValue]]
-  ): StateReader[DamlStateKey, TestValue] = {
+                                       inputState: Map[DamlStateKey, Option[DamlStateValue]]
+                                     ): StateReader[DamlStateKey, TestValue] = {
     val wrappedInputState = inputState.mapValues(TestValue(_))
     new StateReader[DamlStateKey, TestValue] {
       override def read(
-          keys: Iterable[DamlStateKey]
-      )(implicit executionContext: ExecutionContext): Future[Seq[TestValue]] =
+                         keys: Iterable[DamlStateKey]
+                       )(implicit executionContext: ExecutionContext): Future[Seq[TestValue]] =
         Future.successful(keys.view.map(wrappedInputState).toVector)
     }
   }
 
   private def verifyReadSet(
-      output: PreExecutionOutput[TestReadSet, Any],
-      expectedReadSet: Set[DamlStateKey],
-  ): Assertion = {
+                             output: PreExecutionOutput[TestReadSet, Any],
+                             expectedReadSet: Set[DamlStateKey],
+                           ): Assertion = {
     import org.scalatest.matchers.should.Matchers._
     output.readSet.keys should be(expectedReadSet)
   }
