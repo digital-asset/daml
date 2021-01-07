61beb175f382cf0646a7205cbd7bd4750d072e35
Bernhard Elsner <40762178+bame-da@users.noreply.github.com>
2021-01-07 18:22:12 +0100

    Improve the docs copyright notice (#8205)
    * Improve the docs copyright notice
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Update docs/configs/html/conf.py
    
    Co-authored-by: Gary Verhaegen <gary.verhaegen@digitalasset.com>
    
    Co-authored-by: Gary Verhaegen <gary.verhaegen@digitalasset.com>

--- docs/configs/html/conf.py
+++ docs/configs/html/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DAML SDK'
-copyright = u'© Copyright 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved. Any unauthorized use, duplication or distribution is strictly prohibited.'
+copyright = u'© Copyright 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved. Any unauthorized use, duplication or distribution is strictly prohibited. "Digital Asset" and "DAML" are Registered in the U.S. Patent and Trademark Office.'
 author = u'Digital Asset'
 
 # The version info for the project you're documenting, acts as replacement for
