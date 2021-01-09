80918afea53fbe7875d93eb65fa94acd23842fb9
tudor-da <tudor.voicu@digitalasset.com>
2021-01-09 12:18:31 +0200

    Pass MetricsRegistry to commitStrategySupport from IntegrityChecker
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/participant-state/kvutils/tools/src/main/scala/ledger/participant/state/kvutils/tools/integritycheck/IntegrityChecker.scala
+++ ledger/participant-state/kvutils/tools/src/main/scala/ledger/participant/state/kvutils/tools/integritycheck/IntegrityChecker.scala
@@ -38,7 +38,13 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
 import scala.util.{Failure, Success}
 
-class IntegrityChecker[LogResult](commitStrategySupport: CommitStrategySupport[LogResult]) {
+class IntegrityChecker[LogResult](
+    commitStrategySupportBuilder: Metrics => CommitStrategySupport[LogResult]
+) {
+
+  private val metricRegistry = new MetricRegistry
+  private val metrics = new Metrics(metricRegistry)
+  private val commitStrategySupport = commitStrategySupportBuilder(metrics)
 
   import IntegrityChecker._
 
@@ -357,7 +363,7 @@ object IntegrityChecker {
 
   def run[LogResult](
       args: Array[String],
-      commitStrategySupportFactory: ExecutionContext => CommitStrategySupport[LogResult],
+      commitStrategySupportFactory: (Metrics, ExecutionContext) => CommitStrategySupport[LogResult],
   ): Unit = {
     val config = Config.parse(args).getOrElse {
       sys.exit(1)
@@ -367,7 +373,7 @@ object IntegrityChecker {
 
   def run[LogResult](
       config: Config,
-      commitStrategySupportFactory: ExecutionContext => CommitStrategySupport[LogResult],
+      commitStrategySupportFactory: (Metrics, ExecutionContext) => CommitStrategySupport[LogResult],
   ): Unit = {
     runAsync(config, commitStrategySupportFactory).failed
       .foreach {
@@ -395,7 +401,7 @@ object IntegrityChecker {
 
   private def runAsync[LogResult](
       config: Config,
-      commitStrategySupportFactory: ExecutionContext => CommitStrategySupport[LogResult],
+      commitStrategySupportFactory: (Metrics, ExecutionContext) => CommitStrategySupport[LogResult],
   ): Future[Unit] = {
     println(s"Verifying integrity of ${config.exportFilePath}...")
 
@@ -405,7 +411,7 @@ object IntegrityChecker {
     implicit val materializer: Materializer = Materializer(actorSystem)
 
     val importer = ProtobufBasedLedgerDataImporter(config.exportFilePath)
-    new IntegrityChecker(commitStrategySupportFactory(executionContext))
+    new IntegrityChecker(commitStrategySupportFactory(_, executionContext))
       .run(importer, config)
       .andThen { case _ =>
         sys.exit(0)
