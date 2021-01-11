14933e5484a4342be3ea196b9baeb612dc5896e0
mzagorski <marcin.zagorski@digitalasset.com>
2021-01-11 12:54:35 +0100

    DPP-131 using anorm parsed stmts

--- ledger/participant-integration-api/src/main/scala/platform/store/dao/events/EventsTablePostgresql.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/dao/events/EventsTablePostgresql.scala
@@ -6,7 +6,7 @@ package com.daml.platform.store.dao.events
 import java.sql.{Connection, PreparedStatement}
 import java.time.Instant
 
-import anorm.{BatchSql, NamedParameter}
+import anorm.{BatchSql, NamedParameter, Row, SimpleSql, ToStatement}
 import com.daml.ledger.participant.state.v1.Offset
 import com.daml.lf.ledger.EventId
 import com.daml.platform.store.Conversions._
@@ -14,19 +14,19 @@ import com.daml.platform.store.Conversions._
 object EventsTablePostgresql extends EventsTable {
 
   /**
-   * Insertions are represented by a single statement made of nested arrays, one per column, instead of JDBC batches.
-   * This leverages a PostgreSQL-specific feature known as "array unnesting", which has shown to be considerable
-   * faster than using JDBC batches.
-   */
+    * Insertions are represented by a single statement made of nested arrays, one per column, instead of JDBC batches.
+    * This leverages a PostgreSQL-specific feature known as "array unnesting", which has shown to be considerable
+    * faster than using JDBC batches.
+    */
   final class Batches(
-                       insertEvents: Connection => PreparedStatement,
-                       updateArchives: Option[BatchSql],
-                     ) extends EventsTable.Batches {
+      insertEvents: Option[SimpleSql[Row]],
+      updateArchives: Option[BatchSql],
+  ) extends EventsTable.Batches {
     override def execute()(implicit connection: Connection): Unit = {
-      insertEvents(connection).executeUpdate()
+      insertEvents.foreach(_.execute())
       updateArchives.foreach(_.execute())
     }
- }
+  }
 
   private val updateArchived =
     """update participant_events set create_consumed_at={consumed_at} where contract_id={contract_id} and create_argument is not null"""
@@ -38,10 +38,10 @@ object EventsTablePostgresql extends EventsTable {
     )
 
   override def toExecutables(
-                              tx: TransactionIndexing.TransactionInfo,
-                              info: TransactionIndexing.EventsInfo,
-                              serialized: TransactionIndexing.Serialized,
-                            ): EventsTable.Batches = {
+      tx: TransactionIndexing.TransactionInfo,
+      info: TransactionIndexing.EventsInfo,
+      serialized: TransactionIndexing.Serialized,
+  ): EventsTable.Batches = {
     val batchSize = info.events.size
     val eventIds = Array.ofDim[String](batchSize)
     val eventOffsets = Array.fill(batchSize)(tx.offset.toByteArray)
@@ -111,7 +111,7 @@ object EventsTablePostgresql extends EventsTable {
       }
     }
 
-    val inserts = (conn: Connection) => insertEventsBatched(
+    val inserts = insertEvents(
       eventIds,
       eventOffsets,
       contractIds,
@@ -137,77 +137,33 @@ object EventsTablePostgresql extends EventsTable {
       exerciseResults,
       exerciseActors,
       exerciseChildEventIds
-    )(conn)
+    )
 
     val archivals =
       info.archives.iterator.map(archive(tx.offset)).toList
 
     new Batches(
-      insertEvents = (conn: Connection) => inserts(conn),
+      insertEvents = Some(inserts),
       updateArchives = batch(updateArchived, archivals),
     )
   }
 
-  private def insertEventsBatched(
-                                   eventIds: Array[String],
-                                   eventOffsets: Array[Array[Byte]],
-                                   contractIds: Array[String],
-                                   transactionIds: Array[String],
-                                   workflowIds: Array[String],
-                                   ledgerEffectiveTimes: Array[Instant],
-                                   templateIds: Array[String],
-                                   nodeIndexes: Array[java.lang.Integer],
-                                   commandIds: Array[String],
-                                   applicationIds: Array[String],
-                                   submitters: Array[String],
-                                   flatEventWitnesses: Array[String],
-                                   treeEventWitnesses: Array[String],
-                                   createArguments: Array[Array[Byte]],
-                                   createSignatories: Array[String],
-                                   createObservers: Array[String],
-                                   createAgreementTexts: Array[String],
-                                   createConsumedAt: Array[Array[Byte]],
-                                   createKeyValues: Array[Array[Byte]],
-                                   exerciseConsuming: Array[java.lang.Boolean],
-                                   exerciseChoices: Array[String],
-                                   exerciseArguments: Array[Array[Byte]],
-                                   exerciseResults: Array[Array[Byte]],
-                                   exerciseActors: Array[String],
-                                   exerciseChildEventIds: Array[String],
-                                 )(conn: Connection) = {
-    val preparedStatement = conn.prepareStatement(batchInsertSqlString)
-    preparedStatement.setObject(1, eventIds)
-    preparedStatement.setObject(2, eventOffsets)
-    preparedStatement.setObject(3, contractIds)
-    preparedStatement.setObject(4, transactionIds)
-    preparedStatement.setObject(5, workflowIds)
-    preparedStatement.setArray(6, conn.createArrayOf("TIMESTAMP", ledgerEffectiveTimes.map(java.sql.Timestamp.from))) // Handle instant
-    preparedStatement.setObject(7, templateIds)
-    preparedStatement.setObject(8, nodeIndexes)
-    preparedStatement.setObject(9, commandIds)
-    preparedStatement.setObject(10, applicationIds)
-    preparedStatement.setObject(11, submitters)
-    preparedStatement.setObject(12, flatEventWitnesses)
-    preparedStatement.setObject(13, treeEventWitnesses)
-    preparedStatement.setObject(14, createArguments)
-    preparedStatement.setObject(15, createSignatories)
-    preparedStatement.setObject(16, createObservers)
-    preparedStatement.setObject(17, createAgreementTexts)
-    preparedStatement.setObject(18, createConsumedAt)
-    preparedStatement.setObject(19, createKeyValues)
-    preparedStatement.setObject(20, exerciseConsuming)
-    preparedStatement.setObject(21, exerciseChoices)
-    preparedStatement.setObject(22, exerciseArguments)
-    preparedStatement.setObject(23, exerciseResults)
-    preparedStatement.setObject(24, exerciseActors)
-    preparedStatement.setObject(25, exerciseChildEventIds)
-    preparedStatement
+  // Specific for PostgreSQL parallel unnesting insertions
+
+  private implicit object ByteArrayArrayToStatement extends ToStatement[Array[Array[Byte]]] {
+    override def set(s: PreparedStatement, index: Int, v: Array[Array[Byte]]): Unit =
+      s.setObject(index, v)
   }
 
-  private val batchInsertSqlString =
-    """
-       SET LOCAL synchronous_commit = 'off';
-       insert into participant_events(
+  private implicit object InstantArrayToStatement extends ToStatement[Array[Instant]] {
+    override def set(s: PreparedStatement, index: Int, v: Array[Instant]): Unit = {
+      val conn = s.getConnection
+      val ts = conn.createArrayOf("TIMESTAMP", v.map(java.sql.Timestamp.from))
+      s.setArray(index, ts)
+    }
+  }
+
+  private val insertStmt = anorm.SQL("""insert into participant_events(
            event_id, event_offset, contract_id, transaction_id, workflow_id, ledger_effective_time, template_id, node_index, command_id, application_id, submitters, flat_event_witnesses, tree_event_witnesses,
            create_argument, create_signatories, create_observers, create_agreement_text, create_consumed_at, create_key_value,
            exercise_consuming, exercise_choice, exercise_argument, exercise_result, exercise_actors, exercise_child_event_ids
@@ -217,12 +173,71 @@ object EventsTablePostgresql extends EventsTable {
            create_argument, string_to_array(create_signatories,'|'), string_to_array(create_observers,'|'), create_agreement_text, create_consumed_at, create_key_value,
            exercise_consuming, exercise_choice, exercise_argument, exercise_result, string_to_array(exercise_actors,'|'), string_to_array(exercise_child_event_ids,'|')
          from
-           unnest(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+           unnest(
+             {eventIds}, {eventOffsets}, {contractIds}, {transactionIds}, {workflowIds}, {ledgerEffectiveTimes}, {templateIds}, {nodeIndexes}, {commandIds}, {applicationIds}, {submitters}, {flatEventWitnesses}, {treeEventWitnesses},
+             {createArguments}, {createSignatories}, {createObservers}, {createAgreementTexts}, {createConsumedAt}, {createKeyValues},
+             {exerciseConsuming}, {exerciseChoices}, {exerciseArguments}, {exerciseResults}, {exerciseActors}, {exerciseChildEventIds}
+           )
            as
                t(
                  event_id, event_offset, contract_id, transaction_id, workflow_id, ledger_effective_time, template_id, node_index, command_id, application_id, submitters, flat_event_witnesses, tree_event_witnesses,
                  create_argument, create_signatories, create_observers, create_agreement_text, create_consumed_at, create_key_value,
                  exercise_consuming, exercise_choice, exercise_argument, exercise_result, exercise_actors, exercise_child_event_ids
-               );
-       """
+               )
+       """)
+
+  private def insertEvents(
+      eventIds: Array[String],
+      eventOffsets: Array[Array[Byte]],
+      contractIds: Array[String],
+      transactionIds: Array[String],
+      workflowIds: Array[String],
+      ledgerEffectiveTimes: Array[Instant],
+      templateIds: Array[String],
+      nodeIndexes: Array[java.lang.Integer],
+      commandIds: Array[String],
+      applicationIds: Array[String],
+      submitters: Array[String],
+      flatEventWitnesses: Array[String],
+      treeEventWitnesses: Array[String],
+      createArguments: Array[Array[Byte]],
+      createSignatories: Array[String],
+      createObservers: Array[String],
+      createAgreementTexts: Array[String],
+      createConsumedAt: Array[Array[Byte]],
+      createKeyValues: Array[Array[Byte]],
+      exerciseConsuming: Array[java.lang.Boolean],
+      exerciseChoices: Array[String],
+      exerciseArguments: Array[Array[Byte]],
+      exerciseResults: Array[Array[Byte]],
+      exerciseActors: Array[String],
+      exerciseChildEventIds: Array[String],
+  ) =
+    insertStmt.on(
+      "eventIds" -> eventIds,
+      "eventOffsets" -> eventOffsets,
+      "contractIds" -> contractIds,
+      "transactionIds" -> transactionIds,
+      "workflowIds" -> workflowIds,
+      "ledgerEffectiveTimes" -> ledgerEffectiveTimes,
+      "templateIds" -> templateIds,
+      "nodeIndexes" -> nodeIndexes,
+      "commandIds" -> commandIds,
+      "applicationIds" -> applicationIds,
+      "submitters" -> submitters,
+      "flatEventWitnesses" -> flatEventWitnesses,
+      "treeEventWitnesses" -> treeEventWitnesses,
+      "createArguments" -> createArguments,
+      "createSignatories" -> createSignatories,
+      "createObservers" -> createObservers,
+      "createAgreementTexts" -> createAgreementTexts,
+      "createConsumedAt" -> createConsumedAt,
+      "createKeyValues" -> createKeyValues,
+      "exerciseConsuming" -> exerciseConsuming,
+      "exerciseChoices" -> exerciseChoices,
+      "exerciseArguments" -> exerciseArguments,
+      "exerciseResults" -> exerciseResults,
+      "exerciseActors" -> exerciseActors,
+      "exerciseChildEventIds" -> exerciseChildEventIds
+    )
 }
--- ledger/participant-integration-api/src/main/scala/platform/store/dao/events/InsertsPhase2.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/dao/events/InsertsPhase2.scala
@@ -1,7 +1,8 @@
 package com.daml.platform.store.dao.events
 
-import java.sql.{Connection, PreparedStatement}
+import java.sql.{Connection, PreparedStatement, Timestamp}
 
+import anorm.{Row, SimpleSql, ToStatement}
 import com.daml.ledger.participant.state.v1.DivulgedContract
 
 object InsertsPhase2 {
@@ -9,24 +10,54 @@ object InsertsPhase2 {
   protected val IdColumn = "contract_id"
   protected val WitnessColumn = "contract_witness"
 
-  def toExecutable(tx: TransactionIndexing.TransactionInfo, contractsInfo: TransactionIndexing.ContractsInfo, serialized: TransactionIndexing.Serialized, witnesses: TransactionIndexing.ContractWitnessesInfo): InsertsPhase2 = {
-    val inserts = (conn: Connection) => {
+  private implicit object ByteArrayArrayToStatement extends ToStatement[Array[Array[Byte]]] {
+    override def set(s: PreparedStatement, index: Int, v: Array[Array[Byte]]): Unit =
+      s.setObject(index, v)
+  }
+
+  private val insertContractQuery =
+    anorm.SQL("""insert into participant_contracts(
+       contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, create_stakeholders
+     )
+     select
+       contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, string_to_array(create_stakeholders,'|')
+     from
+       unnest({contractIds}, {templateIds}, {createArgs}, {timestamps}, {hashes}, {stakeholders})
+       as t(contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, create_stakeholders)
+            on conflict do nothing;
+     insert into participant_contract_witnesses(contract_id, contract_witness)
+            select contract_id, contract_witness
+            from unnest({witnessesContractIds}, {parties}) as t(contract_id, contract_witness)
+            on conflict do nothing;
+     """)
+
+  def toExecutable(
+      tx: TransactionIndexing.TransactionInfo,
+      contractsInfo: TransactionIndexing.ContractsInfo,
+      serialized: TransactionIndexing.Serialized,
+      witnesses: TransactionIndexing.ContractWitnessesInfo): InsertsPhase2 = {
+    val inserts = (_: Connection) => { // TODO conn not needed
       val netCreatesSize = contractsInfo.netCreates.size
       val divulgedSize = contractsInfo.divulgedContracts.size
       val batchSize = netCreatesSize + divulgedSize
 
       val timestamp = java.sql.Timestamp.from(tx.ledgerEffectiveTime)
-      val timestamps = Array.fill[AnyRef](netCreatesSize)(timestamp) ++ Array.fill[AnyRef](divulgedSize)(null)
+      val timestamps = Array.fill[Timestamp](netCreatesSize)(timestamp) ++ Array.fill[Timestamp](
+        divulgedSize)(null)
 
       val contractIds, templateIds, stakeholders = Array.ofDim[String](batchSize)
       val createArgs, hashes = Array.ofDim[Array[Byte]](batchSize)
 
-      contractsInfo.netCreates.iterator.zipWithIndex.foreach { case (create, idx) =>
-        contractIds(idx) = create.coid.coid
-        templateIds(idx) = create.templateId.toString
-        stakeholders(idx) = create.stakeholders.mkString("|")
-        createArgs(idx) = serialized.createArgumentsByContract(create.coid)
-        hashes(idx) = create.key.map(convertLfValueKey(create.templateId, _)).map(_.hash.bytes.toByteArray).orNull
+      contractsInfo.netCreates.iterator.zipWithIndex.foreach {
+        case (create, idx) =>
+          contractIds(idx) = create.coid.coid
+          templateIds(idx) = create.templateId.toString
+          stakeholders(idx) = create.stakeholders.mkString("|")
+          createArgs(idx) = serialized.createArgumentsByContract(create.coid)
+          hashes(idx) = create.key
+            .map(convertLfValueKey(create.templateId, _))
+            .map(_.hash.bytes.toByteArray)
+            .orNull
       }
 
       contractsInfo.divulgedContracts.iterator.zipWithIndex.foreach {
@@ -38,39 +69,45 @@ object InsertsPhase2 {
           hashes(idx + netCreatesSize) = null
       }
       val flattened: Iterator[(ContractId, String)] = Relation.flatten(witnesses.netVisibility)
-      val (witnessesContractIds, parties) = flattened.map {
-        case (id, party) => id.coid -> party
-      }.toArray.unzip
+      val (witnessesContractIds, parties) = flattened
+        .map {
+          case (id, party) => id.coid -> party
+        }
+        .toArray
+        .unzip
+//      val timestampsArr = conn.createArrayOf("TIMESTAMP", timestamps) // TODO MAYBE TIMESTAMPS SHOULD BE CONVERTED INTO STRINGS ???
+//      timestampsArr
 
-      val preparedStatement = conn.prepareStatement(insertContractQuery)
-      preparedStatement.setObject(1, contractIds)
-      preparedStatement.setObject(2, templateIds)
-      preparedStatement.setObject(3, createArgs)
-      preparedStatement.setArray(4, conn.createArrayOf("TIMESTAMP", timestamps))
-      preparedStatement.setObject(5, hashes)
-      preparedStatement.setObject(6, stakeholders)
-      preparedStatement.setObject(7, witnessesContractIds)
-      preparedStatement.setObject(8, parties)
-      preparedStatement
+      insertContractQuery.on(
+        "contractIds" -> contractIds,
+        "templateIds" -> templateIds,
+        "createArgs" -> createArgs,
+        "timestamps" -> timestamps,
+        "hashes" -> hashes,
+        "stakeholders" -> stakeholders,
+        "witnessesContractIds" -> witnessesContractIds,
+        "parties" -> parties
+      )
     }
 
     InsertsPhase2(inserts)
   }
 
-  val insertContractQuery =
-    s"""insert into participant_contracts(
-       contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, create_stakeholders
-     )
-     select
-       contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, string_to_array(create_stakeholders,'|')
-     from
-       unnest(?, ?, ?, ?, ?, ?)
-       as t(contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, create_stakeholders)
-     on conflict do nothing;
-     insert into $TableName($IdColumn, $WitnessColumn)
-            select $IdColumn, $WitnessColumn
-            from unnest(?, ?) as t($IdColumn, $WitnessColumn)
-            on conflict do nothing;"""
+//
+//  val insertContractQuery =
+//    SQL"""insert into participant_contracts(
+//       contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, create_stakeholders
+//     )
+//     select
+//       contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, string_to_array(create_stakeholders,'|')
+//     from
+//       unnest(contractIds, templateIds, createArgs, timestampsArr, hashes, stakeholders)
+//       as t(contract_id, template_id, create_argument, create_ledger_effective_time, create_key_hash, create_stakeholders)
+//     on conflict do nothing;
+//     insert into participant_contract_witnesses(contract_id, contract_witness)
+//            select contract_id, contract_witness
+//            from unnest(witnessesContractIds, parties) as t(contract_id, contract_witness)
+//            on conflict do nothing;"""
 }
 
-case class InsertsPhase2(insert: Connection => PreparedStatement)
+case class InsertsPhase2(insert: Connection => SimpleSql[Row])
