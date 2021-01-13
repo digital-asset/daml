a33ebf455c78bb350594433fc466cde27b8056fd
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-01-13 08:05:23 +0100

    avoid symbol literal in CommandClientIT
    changelog_begin
    changelog_end

--- ledger/ledger-api-client/src/it/scala/com/digitalasset/ledger/client/CommandClientIT.scala
+++ ledger/ledger-api-client/src/it/scala/com/digitalasset/ledger/client/CommandClientIT.scala
@@ -153,7 +153,7 @@ final class CommandClientIT
       expectedMessageSubString: String,
   ): Future[Assertion] =
     submitCommand(submitRequest).map { completion =>
-      completion.getStatus should have(Symbol("code")(expectedErrorCode.value))
+      completion.getStatus.code should be(expectedErrorCode.value)
       completion.getStatus.message should include(expectedMessageSubString)
     }(DirectExecutionContext)
 
