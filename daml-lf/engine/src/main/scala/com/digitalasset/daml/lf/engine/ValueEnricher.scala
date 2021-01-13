414e8043f013f0ea1a8787453a3d39f1a4dd105e
Remy Haemmerle <Remy.Haemmerle@daml.com>
2021-01-13 15:57:34 +0100

    Address Moritz's review

--- daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/ValueEnricher.scala
+++ daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/ValueEnricher.scala
@@ -12,6 +12,9 @@ import com.daml.lf.transaction.{CommittedTransaction, Node, NodeId, VersionedTra
 import com.daml.lf.value.Value
 import com.daml.lf.value.Value.ContractId
 
+// Provide methods to add information in values (and value containers) if missing:
+// - type constructor in records, variants, and enums
+// - Records' field names
 final class ValueEnricher(engine: Engine) {
 
   private[this] def getPackage(pkgId: PackageId): Result[PackageSignature] = {
