6d1ba0fd368fc72b8918834d1edd0ab07851e87e
Stephen Compall <stephen.compall@daml.com>
2021-01-15 10:19:08 -0500

    explaining the typeclass

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/dbbackend/SupportedJdbcDriver.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/dbbackend/SupportedJdbcDriver.scala
@@ -5,7 +5,15 @@ package com.daml.http.dbbackend
 
 import doobie.{Get, Put}
 
-class SupportedJdbcDriver(val retrySqlStates: Set[String])(implicit
+/** A nullary typeclass of incompatible JDBC operations and settings, selected by
+  * the options passed to json-api at startup.
+  *
+  * We currently use the fact that it is nonsensical to run json-api against more
+  * than one JDBC driver ''in the image'' as justification to make this typeclass
+  * nullary.  If that changes in the future, a phantom type parameter should be
+  * introduced so as to distinguish instances by type.
+  */
+final class SupportedJdbcDriver(val retrySqlStates: Set[String])(implicit
     val gvs: Get[Vector[String]],
     val pvs: Put[Vector[String]],
     val pls: Put[List[String]],
