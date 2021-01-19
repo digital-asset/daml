6a60b4d8b8b5cecbb688aa33c42b13b5639eb6c5
Remy Haemmerle <Remy.Haemmerle@daml.com>
2021-01-19 15:43:19 +0100

    Sandbox-classic: revert back the change of default contract ID seeding
    We revert the change of default contract ID seeding that was
    introduced in #7945.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- language-support/java/codegen/src/ledger-tests/scala/com/digitalasset/TestUtil.scala
+++ language-support/java/codegen/src/ledger-tests/scala/com/digitalasset/TestUtil.scala
@@ -48,7 +48,6 @@ object TestUtil {
   )(implicit resourceContext: ResourceContext): Future[Assertion] = {
     val config = sandbox.DefaultConfig.copy(
       port = Port.Dynamic,
-      seeding = None,
       damlPackages = List(testDalf),
       ledgerIdMode = LedgerIdMode.Static(LedgerId(LedgerID)),
       timeProviderType = Some(TimeProviderType.WallClock),
