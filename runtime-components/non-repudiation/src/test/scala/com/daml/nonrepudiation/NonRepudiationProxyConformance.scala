95874462b4cf86239e6425c803b1d069d9dce144
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2021-01-26 16:08:41 +0100

    Add the first non-repudiation middleware prototype (#8608)
    * Add the first non-repudiation middleware prototype, passing all conformance tests
    
    Closes #8598
    
    changelog_begin
    changelog_end
    
    * Address https://github.com/digital-asset/daml/pull/8608#discussion_r563780495
    
    * Address https://github.com/digital-asset/daml/pull/8608#discussion_r563781026
    
    * Address https://github.com/digital-asset/daml/pull/8608#discussion_r563795509
    
    * Address https://github.com/digital-asset/daml/pull/8608#discussion_r563789111
    
    * Address https://github.com/digital-asset/daml/pull/8608#discussion_r563792789
    
    * Address https://github.com/digital-asset/daml/pull/8608#discussion_r563788373
    
    * Address https://github.com/digital-asset/daml/pull/8608#discussion_r563789111

---
+++ runtime-components/non-repudiation/src/test/scala/com/daml/nonrepudiation/NonRepudiationProxyConformance.scala
@@ -0,0 +1,95 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.nonrepudiation
+
+import java.security.KeyPairGenerator
+
+import com.daml.ledger.api.testtool.infrastructure.{
+  LedgerTestCasesRunner,
+  LedgerTestSummary,
+  Result,
+}
+import com.daml.ledger.api.testtool.suites.ClosedWorldIT
+import com.daml.ledger.api.testtool.tests._
+import com.daml.ledger.api.v1.command_service.CommandServiceGrpc.CommandService
+import com.daml.ledger.api.v1.command_submission_service.CommandSubmissionServiceGrpc.CommandSubmissionService
+import com.daml.ledger.resources.{ResourceContext, ResourceOwner}
+import com.daml.nonrepudiation.client.SigningInterceptor
+import com.daml.platform.sandbox.config.SandboxConfig
+import com.daml.platform.sandboxnext.{Runner => Sandbox}
+import com.daml.ports.Port
+import io.grpc.netty.NettyChannelBuilder
+import org.scalatest.EitherValues
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration.DurationInt
+
+final class NonRepudiationProxyConformance extends AsyncFlatSpec with Matchers with EitherValues {
+
+  import NonRepudiationProxySpec._
+  import NonRepudiationProxyConformance.ConformanceTestCases
+
+  behavior of "NonRepudiationProxy"
+
+  it should "pass all conformance tests" in {
+    implicit val context: ResourceContext = ResourceContext(executionContext)
+    val config = SandboxConfig.defaultConfig.copy(port = Port.Dynamic)
+    val Setup(keys, signatures, proxyBuilder, proxyChannel) = Setup.newInstance
+    val keyPair = KeyPairGenerator.getInstance("RSA").generateKeyPair()
+    keys.put(keyPair.getPublic)
+
+    val proxy =
+      for {
+        sandboxPort <- new Sandbox(config)
+        sandboxChannelBuilder = NettyChannelBuilder
+          .forAddress("localhost", sandboxPort.value)
+          .usePlaintext()
+        sandboxChannel <- ResourceOwner.forChannel(
+          sandboxChannelBuilder,
+          shutdownTimeout = 5.seconds,
+        )
+        proxy <- NonRepudiationProxy.owner[ResourceContext](
+          sandboxChannel,
+          proxyBuilder,
+          keys,
+          signatures,
+          CommandService.scalaDescriptor.fullName,
+          CommandSubmissionService.scalaDescriptor.fullName,
+        )
+      } yield proxy
+
+    proxy.use { _ =>
+      val runner = new LedgerTestCasesRunner(
+        testCases = ConformanceTestCases,
+        participants = Vector(proxyChannel),
+        commandInterceptors = Seq(
+          new SigningInterceptor(keyPair, SigningAlgorithm)
+        ),
+      )
+
+      runner.runTests.map { summaries =>
+        summaries.foldLeft(succeed) { case (_, LedgerTestSummary(_, name, description, result)) =>
+          withClue(s"$name: $description") {
+            result.right.value shouldBe a[Result.Succeeded]
+          }
+        }
+      }
+    }
+  }
+
+}
+
+object NonRepudiationProxyConformance {
+
+  private val ConformanceTestCases =
+    Tests
+      .default(ledgerClockGranularity = 1.second)
+      .filter {
+        case _: ClosedWorldIT => false
+        case _ => true
+      }
+      .flatMap(_.tests)
+
+}
