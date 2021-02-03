4fd25d3e2811bde58fdec6209241c40d4c71e5d8
Stefano Baghino <stefano.baghino@digitalasset.com>
2021-02-03 18:00:30 +0100

    Switch from public keys to certificates for non-repudiation
    changelog_begin
    changelog_end
    
    Un-butchering the original design document

--- runtime-components/non-repudiation-core/src/main/java/com/daml/nonrepudiation/Fingerprints.java
+++ runtime-components/non-repudiation-core/src/main/java/com/daml/nonrepudiation/Fingerprints.java
@@ -5,7 +5,8 @@ package com.daml.nonrepudiation;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
 
 public final class Fingerprints {
 
@@ -13,14 +14,16 @@ public final class Fingerprints {
 
   private static final String ALGORITHM = "SHA-256";
 
-  public static byte[] compute(PublicKey key) {
+  public static byte[] compute(X509Certificate certificate) {
     try {
       MessageDigest md = MessageDigest.getInstance(ALGORITHM);
-      byte[] encodedKey = key.getEncoded();
-      return md.digest(encodedKey);
+      byte[] encodedCertificate = certificate.getEncoded();
+      return md.digest(encodedCertificate);
     } catch (NoSuchAlgorithmException e) {
       throw new IllegalArgumentException(
           String.format("Provider for algorithm '%s' not found", ALGORITHM), e);
+    } catch (CertificateEncodingException e) {
+      throw new IllegalArgumentException("Unable to encode the certificate", e);
     }
   }
 }
