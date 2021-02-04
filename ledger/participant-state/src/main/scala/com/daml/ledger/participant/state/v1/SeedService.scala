e122ddd149b800be020c7502300e083af2ceb019
Remy Haemmerle <Remy.Haemmerle@daml.com>
2021-02-04 17:01:10 +0100

    Participant: Log when the system block on gathering entropy
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/participant-state/src/main/scala/com/daml/ledger/participant/state/v1/SeedService.scala
+++ ledger/participant-state/src/main/scala/com/daml/ledger/participant/state/v1/SeedService.scala
@@ -6,9 +6,10 @@ package com.daml.ledger.participant.state.v1
 import java.security.SecureRandom
 
 import com.daml.lf.crypto
+import org.slf4j.LoggerFactory
 
-trait SeedService {
-  def nextSeed: () => crypto.Hash
+final class SeedService(seed: crypto.Hash) {
+  def nextSeed: () => crypto.Hash = crypto.Hash.secureRandom(seed)
 }
 
 object SeedService {
@@ -33,38 +34,60 @@ object SeedService {
     seeding match {
       case Seeding.Strong => StrongRandom
       case Seeding.Weak => WeakRandom
-      case Seeding.Static => new StaticRandom("static random seed service")
+      case Seeding.Static => StaticRandom
     }
 
-  // Pseudo random generator seeded with high entropy seed.
-  // May block while gathering entropy from the underlying operating system
-  // Thread safe.
-  object StrongRandom extends SeedService {
-    override val nextSeed: () => crypto.Hash =
-      crypto.Hash.secureRandom(
-        crypto.Hash.assertFromByteArray(
-          SecureRandom.getInstanceStrong.generateSeed(crypto.Hash.underlyingHashLength)
-        )
-      )
+  /** Pseudo random generator seeded with high entropy seed.
+    * May block while gathering entropy from the underlying operating system
+    * Thread safe.
+    */
+  // lazy to avoid gathering unecessary entropy.
+  lazy val StrongRandom = {
+    val logger = LoggerFactory.getLogger(this.getClass)
+    var done = false
+
+    new Thread {
+      override def run(): Unit = {
+        Thread.sleep(5 * 1000)
+        if (!done) {
+          logger.info(
+            s"""Trying to gather entropy from the underlying operating system to initialized the contract ID seeding, but the entropy pool seems empty.
+               |On testing environment consider using the "${Seeding.Weak.name}" mode, that does not block on startup but produces insecure contract IDs.""".stripMargin
+          )
+        }
+        while (!done) {
+          Thread.sleep(10 * 1000)
+          logger.info("Still Trying to gather entropy to initialized the contract ID seeding...  ")
+        }
+      }
+    }.start()
+
+    val seed = SecureRandom.getInstanceStrong.generateSeed(crypto.Hash.underlyingHashLength)
+
+    done = true
+
+    new SeedService(crypto.Hash.assertFromByteArray(seed))
   }
 
-  // Pseudo random generator seeded with low entropy seed.
-  // Do not block. Thread safe.
-  // Do not use in production mode.
-  object WeakRandom extends SeedService {
-    override val nextSeed: () => crypto.Hash = {
-      val a = Array.ofDim[Byte](crypto.Hash.underlyingHashLength)
-      scala.util.Random.nextBytes(a)
-      crypto.Hash.secureRandom(crypto.Hash.assertFromByteArray(a))
-    }
+  /** Pseudo random generator seeded with low entropy seed.
+    * Do not block. Thread safe.
+    * Do not use in production mode.
+    */
+  val WeakRandom: SeedService = {
+    val seed = new SecureRandom().generateSeed(crypto.Hash.underlyingHashLength)
+    new SeedService(crypto.Hash.assertFromByteArray(seed))
   }
 
-  // Pseudo random generator seeded with a given seed.
-  // Do not block. Thread safe.
-  // Can be use to get reproducible run. Do not use in production mode.
-  final class StaticRandom(seed: String) extends SeedService {
-    override val nextSeed: () => crypto.Hash =
-      crypto.Hash.secureRandom(crypto.Hash.hashPrivateKey(seed))
-  }
+  /** Pseudo random generator seeded with a static seed.
+    * Do not block. Thread safe.
+    * Can be use to get reproducible run. Do not use in production mode.
+    */
+  val StaticRandom = staticRandom("static random seed service")
+
+  /** Pseudo random generator seeded with a given seed.
+    * Do not block. Thread safe.
+    * Can be use to get reproducible run. Do not use in production mode.
+    */
+  def staticRandom(seed: String) = new SeedService(crypto.Hash.hashPrivateKey(seed))
 
 }
