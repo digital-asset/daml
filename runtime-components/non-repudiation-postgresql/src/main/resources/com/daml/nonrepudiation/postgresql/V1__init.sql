19c5a1d5fe2780ac71e283b9bed6648948823eb9
Stefano Baghino <stefano.baghino@digitalasset.com>
2021-02-04 12:04:23 +0100

    Address comments from https://github.com/digital-asset/daml/pull/8723#pullrequestreview-582106893
    - Address https://github.com/digital-asset/daml/pull/8723#discussion_r569216711
    - Address https://github.com/digital-asset/daml/pull/8723#discussion_r569217401

--- runtime-components/non-repudiation-postgresql/src/main/resources/com/daml/nonrepudiation/postgresql/V1__init.sql
+++ runtime-components/non-repudiation-postgresql/src/main/resources/com/daml/nonrepudiation/postgresql/V1__init.sql
@@ -2,19 +2,18 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 create table ${tables.prefix}_signed_payloads(
-    command_id text,
-    algorithm text not null,
+    command_id varchar(255),
+    algorithm varchar(64) not null,
     fingerprint bytea not null,
     payload bytea not null,
     signature bytea not null,
     "timestamp" timestamptz not null
 );
 
+-- Supports fast retrieval of signed payloads by command identifier
 create index ${tables.prefix}_signed_payloads_command_id_index on ${tables.prefix}_signed_payloads using hash(command_id);
 
 create table ${tables.prefix}_certificates(
-    fingerprint bytea not null,
+    fingerprint bytea primary key not null,
     certificate bytea not null
 );
-
-create index ${tables.prefix}_certificates_fingerprint_index on ${tables.prefix}_certificates using hash(fingerprint);
