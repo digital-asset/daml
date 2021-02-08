b4517caa79bd6b51942dac8df3f9858969785c78
Stephen Compall <stephen.compall@daml.com>
2021-02-08 18:34:30 -0500

    ensure 2.12 aliases are inferrable

--- libs-scala/scala-utils/src/main/2.12/scalautil/nonempty/NonEmptyCollCompat.scala
+++ libs-scala/scala-utils/src/main/2.12/scalautil/nonempty/NonEmptyCollCompat.scala
@@ -6,6 +6,6 @@ package com.daml.scalautil.nonempty
 import scala.collection.{IterableLike, SeqLike}
 
 private[nonempty] object NonEmptyCollCompat {
-  type IterableOps[A, CC[_], C] = IterableLike[A, C]
-  type SeqOps[A, CC[_], C] = SeqLike[A, C]
+  type IterableOps[A, CC[_], C] = IterableLike[A, CC[A] with C]
+  type SeqOps[A, CC[_], C] = SeqLike[A, CC[A] with C]
 }
