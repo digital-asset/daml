47b125a7af0f6640ba0947e56520545fcf5d91cd
Stephen Compall <stephen.compall@daml.com>
2021-02-17 09:08:15 -0500

    an idea for handling the recursive reference case elegantly

--- language-support/scala/codegen/src/main/scala/com/digitalasset/codegen/lf/UsedTypeParams.scala
+++ language-support/scala/codegen/src/main/scala/com/digitalasset/codegen/lf/UsedTypeParams.scala
@@ -36,33 +36,57 @@ object UsedTypeParams {
     case iface.TypeNumeric(_) => Set.empty
   }
 
+  final case class LookupType[+RF, +VF](
+      run: iface.TypeConName => Option[(ScopedDataType.DT[RF, VF], LookupType[RF, VF])]
+  )
+
   /** Variance of `sdt.typeVars` in order. */
   def covariantVars[RF <: iface.Type, VF <: iface.Type](
-      sdt: ScopedDataType.DT[RF, VF]
+      sdt: ScopedDataType.DT[RF, VF],
+      lookupType: LookupType[RF, VF],
   ): ImmArraySeq[Variance] = {
     import iface._, Variance._
     def goType(typ: iface.Type): Map[Ref.Name, Variance] = typ match {
       case TypeVar(name) => Map(name -> Covariant)
+
       case TypePrim(pt, typArgs) =>
         import PrimType._
         pt match {
           case GenMap =>
             val Seq(kt, vt) = typArgs
+            // we don't need to inspect `kt` any further than enumerating it;
+            // every occurrence therein is invariant
             goType(vt) |+| collectTypeParams(kt).view.map((_, Invariant)).toMap
           case Bool | Int64 | Text | Date | Timestamp | Party | ContractId | List | Unit |
               Optional | TextMap =>
             typArgs foldMap goType
         }
-      case TypeCon(tcName, typArgs) => sys.error(s"TODO $tcName $typArgs")
+
+      case TypeCon(tcName, typArgs) =>
+        val (refSdt, refLookupType @ _) =
+          lookupType.run(tcName) getOrElse sys.error(s"$tcName not found")
+        // TODO must use refLookupType in goSdt's dynamic scope
+        goSdt(refSdt).zip(typArgs).foldMap { case (pVariance, aContents) =>
+          val aPositions = goType(aContents)
+          pVariance match {
+            case Covariant => aPositions
+            case Invariant => aPositions.transform((_, _) => Invariant)
+          }
+        }
+
       case TypeNumeric(_) => Map.empty
     }
 
-    val vLookup: Map[Ref.Name, Variance] = sdt.dataType match {
-      case Record(fields) => fields foldMap { case (_, typ) => goType(typ) }
-      case Variant(fields) => fields foldMap { case (_, typ) => goType(typ) }
-      case Enum(_) => Map.empty
+    def goSdt(sdt: ScopedDataType.DT[RF, VF]): ImmArraySeq[Variance] = {
+      val vLookup: Map[Ref.Name, Variance] = sdt.dataType match {
+        case Record(fields) => fields foldMap { case (_, typ) => goType(typ) }
+        case Variant(fields) => fields foldMap { case (_, typ) => goType(typ) }
+        case Enum(_) => Map.empty
+      }
+      sdt.typeVars map (vLookup.getOrElse(_, Covariant))
     }
-    sdt.typeVars map (vLookup.getOrElse(_, Covariant))
+
+    goSdt(sdt)
   }
 
   sealed abstract class Variance extends Product with Serializable
