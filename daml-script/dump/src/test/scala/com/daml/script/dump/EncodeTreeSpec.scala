ddf8e1370566157a005dc2bf826574037402c6ac
Andreas Herrmann <42969706+aherrmann-da@users.noreply.github.com>
2021-02-26 17:41:36 +0100

    Helpers to create test ACS and TransactionTree (#8961)
    * Helpers to create test ACS and TransactionTree
    
    changelog_begin
    changelog_end
    
    * use mutable count
    
    https://github.com/digital-asset/daml/pull/8961/files/c62fff79216fc8189d839b800bea536b9e65e3c4#r583743837
    
    Co-authored-by: Andreas Herrmann <andreas.herrmann@tweag.io>

--- daml-script/dump/src/test/scala/com/daml/script/dump/EncodeTreeSpec.scala
+++ daml-script/dump/src/test/scala/com/daml/script/dump/EncodeTreeSpec.scala
@@ -4,77 +4,24 @@
 package com.daml.script.dump
 
 import com.daml.ledger.api.refinements.ApiTypes.{ContractId, Party}
-import com.daml.ledger.api.v1.event.{CreatedEvent, ExercisedEvent}
-import com.daml.ledger.api.v1.transaction.{TransactionTree, TreeEvent}
-import com.daml.ledger.api.v1.value.{Identifier, Record, Value, Variant}
-import com.google.protobuf
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 class EncodeTreeSpec extends AnyFreeSpec with Matchers {
   import Encode._
-  private def mkCreated(i: Int) = {
-    s"create$i" -> TreeEvent(
-      TreeEvent.Kind.Created(
-        CreatedEvent(
-          eventId = s"create$i",
-          templateId = Some(Identifier("package", "Module", "Template")),
-          contractId = s"cid$i",
-          signatories = Seq("Alice"),
-          createArguments = Some(
-            Record(
-              recordId = Some(Identifier("package", "Module", "Template")),
-              fields = Seq.empty,
-            )
-          ),
-        )
-      )
-    )
-  }
-  private def mkExercised(i: Int, cid: ContractId, result: Value, childEventIds: Seq[String]) = {
-    s"exercise$i" -> TreeEvent(
-      TreeEvent.Kind.Exercised(
-        ExercisedEvent(
-          eventId = s"exercise$i",
-          templateId = Some(Identifier("package", "Module", "Template")),
-          contractId = ContractId.unwrap(cid),
-          actingParties = Seq("Alice"),
-          choice = "Choice",
-          choiceArgument = Some(
-            Value()
-              .withVariant(
-                Variant(
-                  Some(Identifier("package", "Module", "Choice")),
-                  "Choice",
-                  Some(Value().withUnit(protobuf.empty.Empty())),
-                )
-              )
-          ),
-          childEventIds = childEventIds,
-          exerciseResult = Some(result),
-        )
-      )
-    )
-  }
-
   "encodeTree" - {
     "contract id bindings" - {
       "unreferenced create" in {
         val parties = Map(Party("Alice") -> "alice_0")
         val cidMap = Map(ContractId("cid1") -> "contract_0_0")
         val cidRefs = Set.empty[ContractId]
-        val tree = TransactionTree(
-          transactionId = "txid",
-          commandId = "cmdid",
-          workflowId = "flowid",
-          effectiveAt = None,
-          offset = "",
-          eventsById = Map(
-            mkCreated(1)
-          ),
-          rootEventIds = Seq("create1"),
-          traceContext = None,
-        )
+        val tree = TestData
+          .Tree(
+            Seq(
+              TestData.Created(ContractId("cid1"))
+            )
+          )
+          .toTransactionTree
         encodeTree(parties, cidMap, cidRefs, tree).render(80) shouldBe
           """_ <- submitMulti [alice_0] [] do
             |  createCmd Module.Template""".stripMargin.replace("\r\n", "\n")
@@ -86,19 +33,14 @@ class EncodeTreeSpec extends AnyFreeSpec with Matchers {
           ContractId("cid2") -> "contract_1_1",
         )
         val cidRefs = Set.empty[ContractId]
-        val tree = TransactionTree(
-          transactionId = "txid",
-          commandId = "cmdid",
-          workflowId = "flowid",
-          effectiveAt = None,
-          offset = "",
-          eventsById = Map(
-            mkCreated(1),
-            mkCreated(2),
-          ),
-          rootEventIds = Seq("create1", "create2"),
-          traceContext = None,
-        )
+        val tree = TestData
+          .Tree(
+            Seq(
+              TestData.Created(ContractId("cid1")),
+              TestData.Created(ContractId("cid2")),
+            )
+          )
+          .toTransactionTree
         encodeTree(parties, cidMap, cidRefs, tree).render(80) shouldBe
           """submitMulti [alice_0] [] do
             |  _ <- createCmd Module.Template
@@ -113,19 +55,18 @@ class EncodeTreeSpec extends AnyFreeSpec with Matchers {
           ContractId("cid1") -> "contract_1_1",
         )
         val cidRefs = Set.empty[ContractId]
-        val tree = TransactionTree(
-          transactionId = "txid",
-          commandId = "cmdid",
-          workflowId = "flowid",
-          effectiveAt = None,
-          offset = "",
-          eventsById = Map(
-            mkCreated(1),
-            mkExercised(2, ContractId("cid0"), Value().withContractId("cid2"), Seq("create1")),
-          ),
-          rootEventIds = Seq("exercise2"),
-          traceContext = None,
-        )
+        val tree = TestData
+          .Tree(
+            Seq(
+              TestData.Exercised(
+                ContractId("cid0"),
+                Seq(
+                  TestData.Created(ContractId("cid1"))
+                ),
+              )
+            )
+          )
+          .toTransactionTree
         encodeTree(parties, cidMap, cidRefs, tree).render(80) shouldBe
           """tree <- submitTreeMulti [alice_0] [] do
             |  exerciseCmd contract_0_0 (Module.Choice ())""".stripMargin.replace("\r\n", "\n")
@@ -134,18 +75,13 @@ class EncodeTreeSpec extends AnyFreeSpec with Matchers {
         val parties = Map(Party("Alice") -> "alice_0")
         val cidMap = Map(ContractId("cid1") -> "contract_0_0")
         val cidRefs = Set(ContractId("cid1"))
-        val tree = TransactionTree(
-          transactionId = "txid",
-          commandId = "cmdid",
-          workflowId = "flowid",
-          effectiveAt = None,
-          offset = "",
-          eventsById = Map(
-            mkCreated(1)
-          ),
-          rootEventIds = Seq("create1"),
-          traceContext = None,
-        )
+        val tree = TestData
+          .Tree(
+            Seq(
+              TestData.Created(ContractId("cid1"))
+            )
+          )
+          .toTransactionTree
         encodeTree(parties, cidMap, cidRefs, tree).render(80) shouldBe
           """contract_0_0 <- submitMulti [alice_0] [] do
             |  createCmd Module.Template""".stripMargin.replace(
@@ -160,19 +96,14 @@ class EncodeTreeSpec extends AnyFreeSpec with Matchers {
           ContractId("cid2") -> "contract_1_1",
         )
         val cidRefs = Set(ContractId("cid1"), ContractId("cid2"))
-        val tree = TransactionTree(
-          transactionId = "txid",
-          commandId = "cmdid",
-          workflowId = "flowid",
-          effectiveAt = None,
-          offset = "",
-          eventsById = Map(
-            mkCreated(1),
-            mkCreated(2),
-          ),
-          rootEventIds = Seq("create1", "create2"),
-          traceContext = None,
-        )
+        val tree = TestData
+          .Tree(
+            Seq(
+              TestData.Created(ContractId("cid1")),
+              TestData.Created(ContractId("cid2")),
+            )
+          )
+          .toTransactionTree
         encodeTree(parties, cidMap, cidRefs, tree).render(80) shouldBe
           """(contract_1_0, contract_1_1) <- submitMulti [alice_0] [] do
             |  contract_1_0 <- createCmd Module.Template
@@ -190,25 +121,19 @@ class EncodeTreeSpec extends AnyFreeSpec with Matchers {
           ContractId("cid2") -> "contract_1_1",
         )
         val cidRefs = Set(ContractId("cid1"), ContractId("cid2"))
-        val tree = TransactionTree(
-          transactionId = "txid",
-          commandId = "cmdid",
-          workflowId = "flowid",
-          effectiveAt = None,
-          offset = "",
-          eventsById = Map(
-            mkCreated(1),
-            mkCreated(2),
-            mkExercised(
-              3,
-              ContractId("cid0"),
-              Value().withContractId("cid2"),
-              Seq("create1", "create2"),
-            ),
-          ),
-          rootEventIds = Seq("exercise3"),
-          traceContext = None,
-        )
+        val tree = TestData
+          .Tree(
+            Seq(
+              TestData.Exercised(
+                ContractId("cid0"),
+                Seq(
+                  TestData.Created(ContractId("cid1")),
+                  TestData.Created(ContractId("cid2")),
+                ),
+              )
+            )
+          )
+          .toTransactionTree
         encodeTree(parties, cidMap, cidRefs, tree).render(80) shouldBe
           """tree <- submitTreeMulti [alice_0] [] do
             |  exerciseCmd contract_0_0 (Module.Choice ())
