e3f5f36ba3d43b4233317152b729b657b7f22a7c
Nick Chapman <nick.chapman@digitalasset.com>
2021-03-04 11:36:52 +0000

    test value of created contracts, not just the count

--- daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/RollbackTest.scala
+++ daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/RollbackTest.scala
@@ -5,6 +5,7 @@ package com.daml.lf
 package speedy
 
 import com.daml.lf.PureCompiledPackages
+import com.daml.lf.data.ImmArray
 import com.daml.lf.data.Ref.Party
 import com.daml.lf.language.Ast._
 import com.daml.lf.speedy.Compiler.FullStackTrace
@@ -14,6 +15,7 @@ import com.daml.lf.transaction.Node
 import com.daml.lf.transaction.SubmittedTransaction
 import com.daml.lf.validation.Validation
 import com.daml.lf.value.Value
+import com.daml.lf.value.Value._
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks
@@ -27,13 +29,6 @@ class ExceptionTest extends AnyWordSpec with Matchers with TableDrivenPropertyCh
     data.assertRight(
       PureCompiledPackages(rawPkgs, Compiler.Config.Default.copy(stacktracing = FullStackTrace))
     )
-  }
-
-  private def contractIdsInOrder(tx: SubmittedTransaction): Seq[Value.ContractId] = {
-    tx.fold(Vector.empty[Value.ContractId]) {
-      case (acc, (_, create: Node.NodeCreate[Value.ContractId])) => acc :+ create.coid
-      case (acc, _) => acc
-    }
   }
 
   private def runUpdateExpr_GetTx(pkgs1: PureCompiledPackages)(e: Expr): SubmittedTransaction = {
@@ -150,27 +145,41 @@ class ExceptionTest extends AnyWordSpec with Matchers with TableDrivenPropertyCh
        }
       """)
 
-  val testCases = Table[String, Int](
+  val testCases = Table[String, List[Long]](
     ("expression", "expected-number-of-contracts"),
-    ("create0", 0),
-    ("create1", 1),
-    ("create2", 2),
-    ("create3", 3),
-    ("create3nested", 3),
-    ("create3catchNoThrow", 3),
-    ("create3throwAndCatch", 1),
-    ("create3throwAndOuterCatch", 1),
+    ("create0", Nil),
+    ("create1", List(100)),
+    ("create2", List(100, 200)),
+    ("create3", List(100, 200, 300)),
+    ("create3nested", List(100, 200, 300)),
+    ("create3catchNoThrow", List(100, 200, 300)),
+    ("create3throwAndCatch", List(300)),
+    ("create3throwAndOuterCatch", List(300)),
   )
 
-  forEvery(testCases) { (exp: String, expected: Int) =>
-    s"""$exp, contracts expected: #$expected """ in {
+  forEvery(testCases) { (exp: String, expected: List[Long]) =>
+    s"""$exp, contracts expected: $expected """ in {
       val party = Party.assertFromString("Alice")
       val lit: PrimLit = PLParty(party)
       val arg: Expr = EPrimLit(lit)
       val example: Expr = EApp(e"M:$exp", arg)
       val tx: SubmittedTransaction = runUpdateExpr_GetTx(pkgs)(example)
-      val ids: Seq[Value.ContractId] = contractIdsInOrder(tx)
-      ids.size shouldBe expected
+      val ids: Seq[Long] = contractValuesInOrder(tx)
+      ids shouldBe expected
     }
   }
+
+  private def contractValuesInOrder(tx: SubmittedTransaction): Seq[Long] = {
+    tx.fold(Vector.empty[Long]) {
+      case (acc, (_, create: Node.NodeCreate[Value.ContractId])) =>
+        create.arg match {
+          case ValueRecord(_, ImmArray(_, (Some("info"), ValueInt64(n)))) =>
+            acc :+ n
+          case _ =>
+            sys.error(s"unexpected create.arg: ${create.arg}")
+        }
+      case (acc, _) => acc
+    }
+  }
+
 }
