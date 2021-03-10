608f7d6deeb51babaa7e9433cfeb2769ba0b805c
Stephen Compall <stephen.compall@daml.com>
2021-03-10 16:20:06 -0500

    reference tests

--- language-support/scala/codegen/src/test/scala/com/digitalasset/codegen/lf/UsedTypeParamsSpec.scala
+++ language-support/scala/codegen/src/test/scala/com/digitalasset/codegen/lf/UsedTypeParamsSpec.scala
@@ -7,17 +7,19 @@ package lf
 import com.daml.lf.data.Ref
 import com.daml.lf.iface
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.wordspec.AnyWordSpec
 
-class UsedTypeParamsSpec extends AnyWordSpec with Matchers {
+class UsedTypeParamsSpec extends AnyWordSpec with Matchers with TableDrivenPropertyChecks {
   import UsedTypeParams.{ResolvedVariance, Variance}, Variance.{Covariant, Invariant}
 
   private[this] def ref(n: String) = Ref.Identifier.assertFromString(s"abc:Mod:$n")
 
-  val sampleEi: iface.EnvironmentInterface = iface.EnvironmentInterface {
-    import iface.{DefDataType => DT, Record /*, Variant*/}, com.daml.lf.data.ImmArray.{
+  private val sampleEi: iface.EnvironmentInterface = iface.EnvironmentInterface {
+    import iface.{DefDataType => DT, Record, Variant, TypeVar => TVar}, com.daml.lf.data.ImmArray.{
       ImmArraySeq => IASeq
     }, Ref.Name.{assertFromString => rn}
+    val a = rn("a")
     val k = rn("k")
     val v = rn("v")
     Map(
@@ -25,18 +27,38 @@ class UsedTypeParamsSpec extends AnyWordSpec with Matchers {
         IASeq(k, v),
         Record(
           IASeq(
-            (rn("unwrap"), iface.TypePrim(iface.PrimType.GenMap, IASeq(k, v) map iface.TypeVar))
+            (rn("unwrap"), iface.TypePrim(iface.PrimType.GenMap, IASeq(k, v) map TVar))
           )
         ),
-      )
+      ),
+      "FlippedMap" -> DT(
+        IASeq(v, k),
+        Record(
+          IASeq(
+            (rn("unwrap"), iface.TypeCon(iface.TypeConName(ref("JustMap")), IASeq(k, v) map TVar))
+          )
+        ),
+      ),
+      "MyList" -> DT(
+        IASeq(a),
+        Variant(
+          IASeq(
+            (rn("Cons"), iface.TypeCon(iface.TypeConName(ref("MyList_Cons")), IASeq(a) map TVar)),
+            (rn("Nil"), iface.TypeCon(iface.TypeConName(ref("MyList_Nil")), IASeq(a) map TVar)),
+          )
+        ),
+      ),
     ).map { case (k, v) => (ref(k), iface.InterfaceType.Normal(v)) }
   }
 
+  private val exVariances =
+    Seq("JustMap" -> Seq(Invariant, Covariant), "FlippedMap" -> Seq(Covariant, Invariant))
+
+  private val exVarianceTable = Table(("type ctor", "positional variances"), exVariances: _*)
+
   "allCovariant" should {
-    "see the simplest implication of JustMap" in {
-      ResolvedVariance.Empty.allCovariantVars(ref("JustMap"), sampleEi)._2 should ===(
-        Seq(Invariant, Covariant)
-      )
+    "find each variance set independently" in forEvery(exVarianceTable) { (ctor, variance) =>
+      ResolvedVariance.Empty.allCovariantVars(ref(ctor), sampleEi)._2 should ===(variance)
     }
   }
 }
