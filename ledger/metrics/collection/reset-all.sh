11afca7b2649712d81c6ae8de8c6c4f5d192104e
Matthias Schmalz <matthias.schmalz@digitalasset.com>
2021-03-10 11:06:19 +0100

    Copyright...

--- ledger/metrics/collection/reset-all.sh
+++ ledger/metrics/collection/reset-all.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 
 docker-compose down -v 
 
