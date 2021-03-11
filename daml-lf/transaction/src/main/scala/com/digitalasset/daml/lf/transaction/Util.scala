b919e3bda38aeed584b2f2bb059e572765af9002
Remy Haemmerle <Remy.Haemmerle@daml.com>
2021-03-11 09:32:00 +0100

    formatting

--- daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/Util.scala
+++ daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/Util.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.lf
 package transaction
 
@@ -96,3 +99,4 @@ object Util {
     }
 
 }
+
