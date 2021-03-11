63623dc9a4e9ca090ebb1422cde698b40114ce56
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2021-03-11 14:27:30 +0100

    Refactor TableSpec (#9088)
    changelog_begin
    changelog_end

--- runtime-components/non-repudiation-postgresql/src/test/scala/com/daml/nonrepudiation/postgresql/TablesSpec.scala
+++ runtime-components/non-repudiation-postgresql/src/test/scala/com/daml/nonrepudiation/postgresql/TablesSpec.scala
@@ -17,11 +17,11 @@ import com.daml.nonrepudiation.{
 import com.daml.resources.{HasExecutionContext, ResourceOwnerFactories}
 import com.daml.testing.postgresql.PostgresAroundEach
 import doobie.util.log.LogHandler
-import org.scalatest.OptionValues
+import org.scalatest.{Assertion, OptionValues}
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 final class TablesSpec
     extends AsyncFlatSpec
@@ -33,12 +33,69 @@ final class TablesSpec
 
   implicit val logHandler: LogHandler = Slf4jLogHandler(getClass)
 
+  it should "correctly read and write certificates" in withDatabase { db =>
+    val (_, expectedCertificate) = generateKeyAndCertificate()
+    val fingerprint = db.certificates.put(expectedCertificate)
+    val certificate = db.certificates.get(fingerprint)
+    certificate.value.getEncoded shouldEqual expectedCertificate.getEncoded
+  }
+
+  it should "guarantee that adding a certificate is idempotent" in withDatabase { db =>
+    val (_, expectedCertificate) = generateKeyAndCertificate()
+    val fingerprint1 = db.certificates.put(expectedCertificate)
+    val fingerprint2 = db.certificates.put(expectedCertificate)
+    fingerprint1 shouldEqual fingerprint2
+    val certificate = db.certificates.get(fingerprint1)
+    certificate.value.getEncoded shouldEqual expectedCertificate.getEncoded
+  }
+
+  it should "correctly read and write signed payloads" in withDatabase { db =>
+    val (privateKey, expectedCertificate) = generateKeyAndCertificate()
+
+    val expectedTimestamp = Instant.ofEpochMilli(42)
+
+    val expectedAlgorithm =
+      AlgorithmString.SHA256withRSA
+
+    val expectedPayload =
+      PayloadBytes.wrap(generateCommand().toByteArray)
+
+    val expectedKey =
+      db.signedPayloads.keyEncoder.encode(expectedPayload)
+
+    val expectedFingerprint =
+      FingerprintBytes.compute(expectedCertificate)
+
+    val expectedSignature =
+      SignatureBytes.sign(
+        expectedAlgorithm,
+        privateKey,
+        expectedPayload,
+      )
+
+    val expectedSignedPayload =
+      SignedPayload(
+        expectedAlgorithm,
+        expectedFingerprint,
+        expectedPayload,
+        expectedSignature,
+        expectedTimestamp,
+      )
+
+    db.signedPayloads.put(expectedSignedPayload)
+
+    val result = db.signedPayloads.get(expectedKey)
+
+    result should contain only expectedSignedPayload
+
+  }
+
   private val resourceFactory = new ResourceOwnerFactories[ExecutionContext] {
     override protected implicit val hasExecutionContext: HasExecutionContext[ExecutionContext] =
       HasExecutionContext.`ExecutionContext has itself`
   }
 
-  it should "correctly read and write certificates" in {
+  private def withDatabase(test: Tables => Future[Assertion]): Future[Assertion] =
     createTransactor(
       postgresDatabase.url,
       postgresDatabase.userName,
@@ -47,80 +104,7 @@ final class TablesSpec
       resourceFactory,
     ).use { transactor =>
       val db = Tables.initialize(transactor)
-      val (_, expectedCertificate) = generateKeyAndCertificate()
-      val fingerprint = db.certificates.put(expectedCertificate)
-      val certificate = db.certificates.get(fingerprint)
-      certificate.value.getEncoded shouldEqual expectedCertificate.getEncoded
+      test(db)
     }
-  }
-
-  it should "guarantee that adding a certificate is idempotent" in {
-    createTransactor(
-      postgresDatabase.url,
-      postgresDatabase.userName,
-      postgresDatabase.password,
-      maxPoolSize = 10,
-      resourceFactory,
-    ).use { transactor =>
-      val db = Tables.initialize(transactor)
-      val (_, expectedCertificate) = generateKeyAndCertificate()
-      val fingerprint1 = db.certificates.put(expectedCertificate)
-      val fingerprint2 = db.certificates.put(expectedCertificate)
-      fingerprint1 shouldEqual fingerprint2
-      val certificate = db.certificates.get(fingerprint1)
-      certificate.value.getEncoded shouldEqual expectedCertificate.getEncoded
-    }
-  }
-
-  it should "correctly read and write signed payloads" in {
-    createTransactor(
-      postgresDatabase.url,
-      postgresDatabase.userName,
-      postgresDatabase.password,
-      maxPoolSize = 10,
-      resourceFactory,
-    ).use { transactor =>
-      val db = Tables.initialize(transactor)
-      val (privateKey, expectedCertificate) = generateKeyAndCertificate()
-
-      val expectedTimestamp = Instant.ofEpochMilli(42)
-
-      val expectedAlgorithm =
-        AlgorithmString.SHA256withRSA
-
-      val expectedPayload =
-        PayloadBytes.wrap(generateCommand().toByteArray)
-
-      val expectedKey =
-        db.signedPayloads.keyEncoder.encode(expectedPayload)
-
-      val expectedFingerprint =
-        FingerprintBytes.compute(expectedCertificate)
-
-      val expectedSignature =
-        SignatureBytes.sign(
-          expectedAlgorithm,
-          privateKey,
-          expectedPayload,
-        )
-
-      val expectedSignedPayload =
-        SignedPayload(
-          expectedAlgorithm,
-          expectedFingerprint,
-          expectedPayload,
-          expectedSignature,
-          expectedTimestamp,
-        )
-
-      db.signedPayloads.put(expectedSignedPayload)
-
-      val result = db.signedPayloads.get(expectedKey)
-
-      result should contain only expectedSignedPayload
-
-    }
-
-  }
 
 }
