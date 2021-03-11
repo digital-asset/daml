2396ccb759cb927bf97b815287520f10446a06b3
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-03-11 11:15:42 +0100

    Update ci/copy-unix-release-artifacts.sh
    Co-authored-by: Gary Verhaegen <gary.verhaegen@digitalasset.com>

--- ci/copy-unix-release-artifacts.sh
+++ ci/copy-unix-release-artifacts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-
+set -euo pipefail
 
 RELEASE_TAG=$1
 NAME=$2
