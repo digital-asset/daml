d2dd876a1924e296d3bbf2e0fc60271d210f5152
Remy Haemmerle <Remy.Haemmerle@daml.com>
2021-03-11 18:37:14 +0100

    formatting

--- daml-lf/kvutils/src/main/scala/com/daml/lf/kvutils/Normalizer.scala
+++ daml-lf/kvutils/src/main/scala/com/daml/lf/kvutils/Normalizer.scala
@@ -11,8 +11,8 @@ object Normalizer {
   // KV specific normalization.
   // drops Fetch and Lookup nodes from a transaction.
   def normalizeTransaction(
-                           tx: CommittedTransaction
-                         ): CommittedTransaction = {
+      tx: CommittedTransaction
+  ): CommittedTransaction = {
     val nodes = tx.nodes.filter {
       case (_, _: Node.NodeFetch[_] | _: Node.NodeLookupByKey[_]) => false
       case (_, _: Node.NodeExercises[_, _] | _: Node.NodeCreate[_]) => true
@@ -20,13 +20,13 @@ object Normalizer {
     val filteredNodes = nodes.transform {
       case (_, node: Node.NodeExercises[NodeId, ContractId]) =>
         val filteredNode = node.copy(children = node.children.filter(nodes.contains))
-         filteredNode
+        filteredNode
       case (_, keep) =>
         keep
     }
     val filteredRoots = tx.roots.filter(filteredNodes.contains)
     CommittedTransaction(
-      VersionedTransaction(tx.version,   filteredNodes, filteredRoots)
+      VersionedTransaction(tx.version, filteredNodes, filteredRoots)
     )
   }
 
