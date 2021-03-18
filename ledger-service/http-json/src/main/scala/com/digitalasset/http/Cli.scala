f0c8b1240ded118fbc1ad73351305fed46cc4a8b
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2021-03-18 16:15:52 +0100

    Integrate non-repudiation into HTTP JSON API (#9180)
    * Integrate non-repudiation into HTTP JSON API
    
    changelog_begin
    changelog_end
    
    Closes #9094
    
    * Address https://github.com/digital-asset/daml/pull/9180#discussion_r596400828
    
    * Address https://github.com/digital-asset/daml/pull/9180#discussion_r596408663
    
    * Address https://github.com/digital-asset/daml/pull/9180#discussion_r596402533
    
    * Address https://github.com/digital-asset/daml/pull/9180#discussion_r596624708
    
    * Address https://github.com/digital-asset/daml/pull/9180#discussion_r596624174
    
    * Solve compilation error in daml-script
    
    * Address https://github.com/digital-asset/daml/pull/9180#issuecomment-801875959
    
    changelog_begin
    - [JSON API] The JAR for the HTTP JSON API is no longer published on
    Maven, use the fat JAR as indicated in the documentation.
    changelog_end

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/Cli.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/Cli.scala
@@ -166,5 +166,39 @@ object Cli extends StrictLogging {
         .optional()
         .valueName(WebsocketConfig.usage)
         .text(s"Optional websocket configuration string. ${WebsocketConfig.help}")
+
+      opt[String]("non-repudiation-certificate-path")
+        .action((path, config) =>
+          config.copy(nonRepudiation =
+            config.nonRepudiation.copy(certificateFile = Some(Paths.get(path)))
+          )
+        )
+        .text(nonRepudiationText)
+        .hidden()
+
+      opt[String]("non-repudiation-private-key-path")
+        .action((path, config) =>
+          config.copy(nonRepudiation =
+            config.nonRepudiation.copy(privateKeyFile = Some(Paths.get(path)))
+          )
+        )
+        .text(nonRepudiationText)
+        .hidden()
+
+      opt[String]("non-repudiation-private-key-algorithm")
+        .action((algorithm, config) =>
+          config.copy(nonRepudiation =
+            config.nonRepudiation.copy(privateKeyAlgorithm = Some(algorithm))
+          )
+        )
+        .text(nonRepudiationText)
+        .hidden()
     }
+
+  private val nonRepudiationText: String =
+    """EARLY ACCESS FEATURE
+      |--non-repudiation-certificate-path, --non-repudiation-private-key-path and --non-repudiation-certificate-path must be passed together.
+      |All commands issued by the HTTP JSON API will be signed with the private key and the X.509 certificate at the provided paths.
+      |This is relevant exclusively if you are using the non-repudiation middleware.""".stripMargin
+
 }
