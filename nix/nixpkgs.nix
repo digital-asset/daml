077d8da58fe5a0facc4121df4350ac9dd947eb2c
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-03-26 21:26:40 +0100

    Upgrade nixpkgs (#9248)
    * Upgrade nixpkgs
    
    changelog_begin
    changelog_end
    
    * ghc bump
    
    changelog_begin
    changelog_end
    
    * more native buildinputs
    
    changelog_begin
    changelog_end
    
    * Switch assignment
    
    changelog_begin
    changelog_end
    
    * update lockfile
    
    changelog_begin
    changelog_end
    
    * Revert "Switch assignment"
    
    This reverts commit af6d25120bc5be76ea8991af400b053d01c0e37a.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -49,8 +49,8 @@ let
    haskell = pkgs.haskell // {
      packages = pkgs.haskell.packages // {
        integer-simple = pkgs.haskell.packages.integer-simple // {
-        ghc8103 = pkgs.haskell.packages.integer-simple.ghc8103.override {
-          ghc = pkgs.haskell.compiler.integer-simple.ghc8103.overrideAttrs (old: {
+        ghc8104 = pkgs.haskell.packages.integer-simple.ghc8104.override {
+          ghc = pkgs.haskell.compiler.integer-simple.ghc8104.overrideAttrs (old: {
             # We need to include darwin.cctools in PATH to make sure GHC finds
             # otool.
             postInstall = ''
