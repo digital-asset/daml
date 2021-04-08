8a4cc9cbfdcaf21710cbf83c7fb1a5628d7fd28f
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2021-04-08 16:26:24 +0200

    step 5: clean up

--- infra/hoogle_server.tf
+++ infra/hoogle_server.tf
@@ -42,34 +42,11 @@ locals {
       suffix         = "-blue",
       ubuntu_version = "2004",
       size           = 3,
-      install        = <<EOF
-mkdir /nix
-chown hoogle:hoogle /nix
-runuser -l hoogle <<'HOOGLE_SETUP'
-curl -sSfL https://nixos.org/nix/install | sh
-. /home/hoogle/.nix-profile/etc/profile.d/nix.sh
-# Feel free to bump the commit, this was the latest
-# # at the time of creation.
-NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/c50e680b03adecae01fdd1ea4e44c82e641de0cf.tar.gz
-HOOGLE_PATH=$(nix-build --no-out-link -E '(import <nixpkgs> {}).haskellPackages.hoogle')
-mkdir -p /home/hoogle/.local/bin
-ln -s $HOOGLE_PATH/bin/hoogle /home/hoogle/.local/bin/hoogle
-EOF
     },
     {
       suffix         = "-green",
       ubuntu_version = "2004",
       size           = 0,
-      install        = <<EOF
-curl -sSL https://get.haskellstack.org/ | sh
-runuser -u hoogle bash <<HOOGLE_SETUP
-git clone https://github.com/ndmitchell/hoogle.git
-cd hoogle
-git checkout 73fa6b5c156e0015e135a564e2821719611abe03
-stack init --resolver=lts-14.7
-stack build
-stack install
-EOF
     }
   ]
 }
@@ -127,7 +104,17 @@ useradd hoogle
 mkdir /home/hoogle
 chown hoogle:hoogle /home/hoogle
 cd /home/hoogle
-${trimspace(local.h_clusters[count.index].install)}
+mkdir /nix
+chown hoogle:hoogle /nix
+runuser -l hoogle <<'HOOGLE_SETUP'
+curl -sSfL https://nixos.org/nix/install | sh
+. /home/hoogle/.nix-profile/etc/profile.d/nix.sh
+# Feel free to bump the commit, this was the latest
+# # at the time of creation.
+NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/c50e680b03adecae01fdd1ea4e44c82e641de0cf.tar.gz
+HOOGLE_PATH=$(nix-build --no-out-link -E '(import <nixpkgs> {}).haskellPackages.hoogle')
+mkdir -p /home/hoogle/.local/bin
+ln -s $HOOGLE_PATH/bin/hoogle /home/hoogle/.local/bin/hoogle
 export PATH=/home/hoogle/.local/bin:$PATH
 mkdir daml
 curl https://docs.daml.com/hoogle_db/base.txt --output daml/base.txt
