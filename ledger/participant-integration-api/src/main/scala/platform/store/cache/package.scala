8f885f457a746a2298b0d9845e46f7ebc8d47359
tudor-da <tudor.voicu@digitalasset.com>
2021-04-13 07:19:30 -0700

    MutableCacheBackedContractStore implementation (#9378)
    * MutableCacheBackedContractStore implementation
    * Implemented ContractStore methods
    * Implemented event stream consumption via consumeFrom
    * Tests for asserting correctness
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Addressed review comments

---
+++ ledger/participant-integration-api/src/main/scala/platform/store/cache/package.scala
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.platform.store
+
+package object cache {
+  import com.daml.lf.value.{Value => lfval}
+  private[cache] type ContractId = lfval.ContractId
+  private[cache] val ContractId = com.daml.lf.value.Value.ContractId
+  private[cache] type Value = lfval.VersionedValue[ContractId]
+  private[cache] type Contract = lfval.ContractInst[Value]
+
+  import com.daml.lf.{transaction => lftx}
+  private[cache] type Key = lftx.GlobalKey
+
+  import com.daml.lf.{data => lfdata}
+  private[cache] type Party = lfdata.Ref.Party
+  private[cache] val Party = lfdata.Ref.Party
+  private[cache] type Identifier = lfdata.Ref.Identifier
+  private[cache] val Identifier = lfdata.Ref.Identifier
+}
