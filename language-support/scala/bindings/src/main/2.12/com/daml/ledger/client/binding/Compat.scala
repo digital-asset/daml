d1ee3bed081601e953448a9da75fc18182309189
Stephen Compall <stephen.compall@daml.com>
2021-04-15 14:24:05 -0400

    add the MapOps argument to Compat.MapLike

--- language-support/scala/bindings/src/main/2.12/com/daml/ledger/client/binding/Compat.scala
+++ language-support/scala/bindings/src/main/2.12/com/daml/ledger/client/binding/Compat.scala
@@ -6,7 +6,12 @@ package com.daml.ledger.client.binding
 import scala.collection.immutable
 
 object Compat {
-  private[binding] type MapLike[K, +V, +C <: immutable.MapLike[K, V, C] with immutable.Map[K, V]] =
+  private[binding] type MapLike[
+      K,
+      +V,
+      +CC[X, +Y],
+      +C <: immutable.MapLike[K, V, C] with immutable.Map[K, V],
+  ] =
     immutable.MapLike[K, V, C]
 
   type DummyImplicit = scala.Predef.DummyImplicit
