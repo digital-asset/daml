d3eb4fdf82b15164521ee49cc506d67afde14b46
Stefano Baghino <stefano.baghino@digitalasset.com>
2021-04-20 10:54:58 +0200

    Re-structure the test following @S11001001's input in https://github.com/digital-asset/daml/pull/9443#discussion_r616083932 -- thanks

--- ledger-service/http-json/src/itlib/scala/http/AbstractWebsocketServiceIntegrationTest.scala
+++ ledger-service/http-json/src/itlib/scala/http/AbstractWebsocketServiceIntegrationTest.scala
@@ -860,39 +860,25 @@ abstract class AbstractWebsocketServiceIntegrationTest
   "no duplicates should be returned when retrieving contracts for multiple parties" in withHttpService {
     (uri, encoder, _) =>
       val aliceAndBob = List("Alice", "Bob")
-      val sharedAccountNumber = "4444"
       val jwtForAliceAndBob = jwtForParties(actAs = aliceAndBob, readAs = Nil, ledgerId = testId)
       import spray.json._
 
-      val (killSwitch, source) = singleClientQueryStream(
-        jwt = jwtForAliceAndBob,
-        serviceUri = uri,
-        query = """{"templateIds": ["Account:SharedAccount"]}""",
-      )
-        .viaMat(KillSwitches.single)(Keep.right)
-        .preMaterialize()
-
-      def test: Sink[JsValue, Future[ShouldHaveEnded]] = {
+      def test(
+          expectedContractId: String,
+          killSwitch: UniqueKillSwitch,
+      ): Sink[JsValue, Future[ShouldHaveEnded]] = {
         val dslSyntax = Consume.syntax[JsValue]
         import dslSyntax._
         Consume.interpret(
           for {
-            ContractDelta(Vector(), _, Some(liveStartOffset)) <- readOne
-            (status, _) <- liftF(
-              postCreateCommand(
-                sharedAccountCreateCommand(aliceAndBob, sharedAccountNumber),
-                encoder,
-                uri,
-                headers = headersWithPartyAuth(aliceAndBob),
-              )
-            )
-            _ = status shouldBe a[StatusCodes.Success]
-            ContractDelta(Vector((_, sharedAccountObject)), _, Some(lastSeenOffset)) <- readOne
-            _ = inside(sharedAccountObject) { case JsObject(obj) =>
+            Vector((sharedAccountId, sharedAccount)) <- readAcsN(1)
+            _ = sharedAccountId shouldBe expectedContractId
+            ContractDelta(Vector(), _, Some(offset)) <- readOne
+            _ = inside(sharedAccount) { case JsObject(obj) =>
               inside((obj get "owners", obj get "number")) {
                 case (Some(JsArray(owners)), Some(JsString(number))) =>
                   owners should contain theSameElementsAs Vector(JsString("Alice"), JsString("Bob"))
-                  number shouldBe sharedAccountNumber
+                  number shouldBe "4444"
               }
             }
             ContractDelta(Vector(), _, Some(_)) <- readOne
@@ -900,19 +886,35 @@ abstract class AbstractWebsocketServiceIntegrationTest
             heartbeats <- drain
             hbCount = (heartbeats.iterator.map {
               case ContractDelta(Vector(), Vector(), Some(currentOffset)) => currentOffset
-            }.toSet + lastSeenOffset).size - 1
+            }.toSet + offset).size - 1
           } yield
           // don't count empty events block if lastSeenOffset does not change
           ShouldHaveEnded(
-            liveStartOffset = liveStartOffset,
+            liveStartOffset = offset,
             msgCount = 2 + hbCount,
-            lastSeenOffset = lastSeenOffset,
+            lastSeenOffset = offset,
           )
         )
       }
 
       for {
-        result <- source via parseResp runWith test
+        (status, value) <-
+          postCreateCommand(
+            cmd = sharedAccountCreateCommand(owners = aliceAndBob, "4444"),
+            encoder = encoder,
+            uri = uri,
+            headers = headersWithPartyAuth(aliceAndBob),
+          )
+        _ = status shouldBe a[StatusCodes.Success]
+        expectedContractId = getContractId(getResult(value))
+        (killSwitch, source) = singleClientQueryStream(
+          jwt = jwtForAliceAndBob,
+          serviceUri = uri,
+          query = """{"templateIds": ["Account:SharedAccount"]}""",
+        )
+          .viaMat(KillSwitches.single)(Keep.right)
+          .preMaterialize()
+        result <- source via parseResp runWith test(expectedContractId.unwrap, killSwitch)
       } yield inside(result) { case ShouldHaveEnded(_, 2, _) =>
         succeed
       }
