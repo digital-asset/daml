3bf79126ba564ac9848e85425a106cd7bc224026
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-04-20 10:44:59 +0200

    Additional unit test for hashing deduplication keys

--- ledger/participant-integration-api/src/test/suite/scala/platform/store/dao/DeduplicationKeyMakerSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/platform/store/dao/DeduplicationKeyMakerSpec.scala
@@ -7,12 +7,13 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import com.daml.ledger.api.domain.CommandId
 import com.daml.lf.data.Ref
+import org.scalatest.prop.TableDrivenPropertyChecks
 
 import java.util.UUID
 import scala.util.Random
 import scalaz.syntax.tag._
 
-class DeduplicationKeyMakerSpec extends AnyWordSpec with Matchers {
+class DeduplicationKeyMakerSpec extends AnyWordSpec with Matchers with TableDrivenPropertyChecks {
 
   val commandId: CommandId = CommandId(Ref.LedgerString.assertFromString(UUID.randomUUID.toString))
 
@@ -21,8 +22,30 @@ class DeduplicationKeyMakerSpec extends AnyWordSpec with Matchers {
       DeduplicationKeyMaker.make(commandId, List(aParty())) should startWith(commandId.unwrap)
     }
 
+    "make different keys for different sets of submitters" in {
+      val aCommonParty = aParty()
+      val cases = Table(
+        ("Submitters for key1", "Submitters for key2"),
+        (List(aParty()), List(aParty())),
+        (List(aCommonParty, aParty()), List(aCommonParty, aParty())),
+        (List(aParty(), aParty()), List(aParty(), aParty())),
+      )
+
+      forAll(cases) { case (key1Submitters, key2Submitters) =>
+        val key1 = DeduplicationKeyMaker.make(commandId, key1Submitters)
+        val key2 = DeduplicationKeyMaker.make(commandId, key2Submitters)
+
+        key1 shouldNot equal(key2)
+      }
+    }
+
     "make a deduplication key with a limited length for a large number of submitters" in {
       val submitters = (1 to 50).map(_ => aParty()).toList
+
+      /** The motivation for the MaxKeyLength is to avoid problems with putting deduplication key in a database
+        * index (e.g. for Postgres the limit for the index row size is 2712).
+        * The value 200 is set arbitrarily to provide some space for other data.
+        */
       val MaxKeyLength = 200
       DeduplicationKeyMaker.make(commandId, submitters).length should be < MaxKeyLength
     }
