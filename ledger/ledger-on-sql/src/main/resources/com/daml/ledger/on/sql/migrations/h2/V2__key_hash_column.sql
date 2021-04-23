3ab77516c695b99c6e6729cb9349d2b5b14d19f9
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-04-23 16:45:01 +0200

    Removed unnecessary NOT NULL constraint

--- ledger/ledger-on-sql/src/main/resources/com/daml/ledger/on/sql/migrations/h2/V2__key_hash_column.sql
+++ ledger/ledger-on-sql/src/main/resources/com/daml/ledger/on/sql/migrations/h2/V2__key_hash_column.sql
@@ -1,4 +1,4 @@
 -- Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-ALTER TABLE ${table.prefix}state ADD COLUMN key_hash VARBINARY(2048) NOT NULL;
+ALTER TABLE ${table.prefix}state ADD COLUMN key_hash VARBINARY(2048);
