45447b9f7f8925476edaf95d01b05ab0e12daeec
Brian Healey <brian.healey@digitalasset.com>
2021-04-23 15:08:01 -0400

    code review: revert files that should not be touched

--- ledger/participant-integration-api/src/main/resources/db/migration/postgres/V2_0__Contract_divulgence.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/postgres/V2_0__Contract_divulgence.sql
@@ -8,17 +8,15 @@
 -- This is required for making sure contracts can only be fetched by parties that see the contract.
 ---------------------------------------------------------------------------------------------------
 
+CREATE TABLE contract_divulgences
+(
+    contract_id    varchar references contracts (id)                  not null,
+    -- The party to which the given contract was divulged
+    party          varchar                                            not null,
+    -- The offset at which the contract was divulged to the given party
+    ledger_offset  bigint references ledger_entries (ledger_offset)   not null,
+    -- The transaction ID at which the contract was divulged to the given party
+    transaction_id varchar references ledger_entries (transaction_id) not null,
 
-
-CREATE TABLE contract_divulgences (
-  contract_id   varchar references contracts (id)                not null,
-  -- The party to which the given contract was divulged
-  party         varchar                                          not null,
-  -- The offset at which the contract was divulged to the given party
-  ledger_offset bigint references ledger_entries (ledger_offset) not null,
-  -- The transaction ID at which the contract was divulged to the given party
-  transaction_id varchar references ledger_entries (transaction_id) not null,
-
-  CONSTRAINT contract_divulgences_idx UNIQUE(contract_id, party)
+    CONSTRAINT contract_divulgences_idx UNIQUE (contract_id, party)
 );
-
