a060258ce3cd3fe7cb04937c0e2fe9cc7607ae38
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-04-23 21:39:21 +0200

    format

--- runtime-components/jdbc-drivers/src/main/ee/scala/com/daml/runtime/JdbcDrivers.scala
+++ runtime-components/jdbc-drivers/src/main/ee/scala/com/daml/runtime/JdbcDrivers.scala
@@ -1,5 +1,9 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.runtime
 
 object JdbcDrivers {
-  val supportedJdbcDriverNames: Set[String] = Set("org.postgresql.Driver", "oracle.jdbc.OracleDriver")
+  val supportedJdbcDriverNames: Set[String] =
+    Set("org.postgresql.Driver", "oracle.jdbc.OracleDriver")
 }
