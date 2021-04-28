9134a58ca6d970b38dd5be9fafad80ef33fb2915
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2021-04-28 18:32:22 +0200

    Determine observers and signatories by submitting a 'fetch' command.

--- ledger/participant-state/kvutils/tools/src/main/scala/ledger/participant/state/kvutils/tools/reindexfromstate/StateToUpdateMapping.scala
+++ ledger/participant-state/kvutils/tools/src/main/scala/ledger/participant/state/kvutils/tools/reindexfromstate/StateToUpdateMapping.scala
@@ -8,12 +8,13 @@ import java.util.UUID
 
 import com.daml.ledger.on.memory.InMemoryState.MutableState
 import com.daml.ledger.participant.state.kvutils
-import com.daml.ledger.participant.state.kvutils.Conversions
+import com.daml.ledger.participant.state.kvutils.Conversions.{contractIdToStateKey, packageStateKey}
 import com.daml.ledger.participant.state.kvutils.DamlKvutils.{
   DamlContractState,
   DamlStateKey,
   DamlStateValue,
 }
+import com.daml.ledger.participant.state.kvutils.{Conversions, Err, Raw}
 import com.daml.ledger.participant.state.v1.Update.{
   ConfigurationChanged,
   PartyAddedToParticipant,
@@ -21,12 +22,18 @@ import com.daml.ledger.participant.state.v1.Update.{
   TransactionAccepted,
 }
 import com.daml.ledger.participant.state.v1._
+import com.daml.lf.archive.Decode
+import com.daml.lf.archive.Reader.ParseError
 import com.daml.lf.crypto
 import com.daml.lf.data.Ref
+import com.daml.lf.data.Ref.{Identifier, PackageId}
 import com.daml.lf.data.Time.Timestamp
+import com.daml.lf.engine.{Engine, VisibleByKey}
+import com.daml.lf.language.Ast
 import com.daml.lf.transaction.test.TransactionBuilder
-import com.daml.lf.transaction.{BlindingInfo, CommittedTransaction, NodeId}
+import com.daml.lf.transaction.{BlindingInfo, CommittedTransaction, Node, NodeId}
 import com.daml.lf.value.Value
+import com.daml.lf.value.Value.ContractId
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
@@ -127,8 +134,12 @@ class StateToUpdateMapping(state: MutableState) {
               optNodeSeeds = None,
               optByKeyNodes = None,
             ),
-            transaction =
-              transactionFromContractState(damlStateKey.getContractId, contract, contractInstance),
+            transaction = transactionFromContractState(
+              damlStateKey.getContractId,
+              contract,
+              contractInstance,
+              submissionSeed,
+            ),
             transactionId = TransactionId.assertFromString(
               UUID.randomUUID().toString
             ), // TODO: Can be determined from state?
@@ -147,25 +158,107 @@ class StateToUpdateMapping(state: MutableState) {
     }
   }
 
+  private val engine = new Engine()
+
   private def transactionFromContractState(
       contractId: String,
       contract: DamlContractState,
       contractInstance: Value.ContractInst[Value.VersionedValue[Value.ContractId]],
+      submissionSeed: crypto.Hash,
   ): CommittedTransaction = {
+    // Submitters must be stakeholders.
+    val locallyDisclosedTo = contract.getLocallyDisclosedToList.asScala
+    val templateId = contractInstance.template.toString
+    val fetchedContract =
+      fetchContractViaEngine(locallyDisclosedTo, contractId, templateId, submissionSeed).get
+    val signatories = fetchedContract.signatories
+    val observers = fetchedContract.stakeholders
+
     val transactionBuilder = TransactionBuilder()
-    // FIXME: Map from state.
-    val observers = contract.getLocallyDisclosedToList.asScala ++ contract.getDivulgedToList.asScala
     val createNode = transactionBuilder.create(
       id = contractId,
-      template = contractInstance.template.toString,
+      template = templateId,
       argument = contractInstance.arg.value,
-      signatories = Seq.empty,
-      observers = observers.sorted,
+      signatories = signatories.toSeq,
+      observers = observers.toSeq,
       key = None,
     )
     transactionBuilder.add(createNode)
     transactionBuilder.buildCommitted()
   }
+
+  private def fetchContractViaEngine(
+      submitters: Iterable[String],
+      contractId: String,
+      templateId: String,
+      submissionSeed: crypto.Hash,
+  ): Option[Node.NodeFetch[ContractId]] = {
+    val fetchResult = engine.submitFetch(
+      submitters.map(Ref.Party.assertFromString).toSet,
+      Identifier.assertFromString(templateId),
+      ContractId.assertFromString(contractId),
+      Timestamp.Epoch,
+      ParticipantId.assertFromString("a participant"),
+      submissionSeed,
+    )
+    fetchResult
+      .consume(
+        contractId => lookupContract(contractId),
+        packageId => lookupPackage(packageId),
+        // No key lookups.
+        _ => None,
+        // No visibility checks.
+        _ => VisibleByKey.Visible,
+      )
+      .toOption
+  }
+
+  private def lookupPackage(pkgId: PackageId): Option[Ast.Package] = {
+    val stateKey = packageStateKey(pkgId)
+    for {
+      rawValue <- state
+        .get(Raw.StateKey(stateKey.toByteString))
+        .orElse {
+          throw Err.MissingInputState(stateKey)
+        }
+      value <- kvutils.Envelope.openStateValue(rawValue).toOption
+      pkg <- value.getValueCase match {
+        case DamlStateValue.ValueCase.ARCHIVE =>
+          try {
+            Some(Decode.decodeArchive(value.getArchive)._2)
+          } catch {
+            case ParseError(err) =>
+              throw Err.DecodeError("Archive", err)
+          }
+
+        case _ =>
+          val msg = "value is not a DAML-LF archive"
+          throw Err.DecodeError("Archive", msg)
+      }
+    } yield pkg
+  }
+
+  private def lookupContract(
+      coid: Value.ContractId
+  ): Option[Value.ContractInst[Value.VersionedValue[Value.ContractId]]] = {
+    val stateKey = contractIdToStateKey(coid)
+    for {
+      contractState <- state
+        .get(Raw.StateKey(stateKey.toByteString))
+        .flatMap(envelope => kvutils.Envelope.openStateValue(envelope).toOption)
+        .map(_.getContractState)
+      contract = Conversions.decodeContractInstance(contractState.getContractInstance)
+    } yield contract
+  }
+
+//  private def lookupKey(key: GlobalKeyWithMaintainers): Option[Value.ContractId] = {
+//    val stateKey = Conversions.globalKeyToStateKey(key.globalKey)
+//    for {
+//      rawStateValue <- state.get(Raw.StateKey(stateKey.toByteString))
+//      stateValue <- kvutils.Envelope.openStateValue(rawStateValue).toOption
+//      if stateValue.getContractKeyState.getContractId.nonEmpty
+//    } yield decodeContractId(stateValue.getContractKeyState.getContractId)
+//  }
 
   private def blindingInfoFromContractState(
       contractState: DamlContractState,
