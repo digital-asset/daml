a7ee7046f79f1a6fd49d24468b3c6c7aeb6dddab
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2021-04-29 14:51:24 +0200

    Review feedback - remove FIXME

--- ledger/participant-integration-api/src/main/scala/db/migration/oracle/package.scala
+++ ledger/participant-integration-api/src/main/scala/db/migration/oracle/package.scala
@@ -3,6 +3,4 @@
 
 package com.daml.platform.db.migration
 
-// FIXME: Remove empty oracle package once we add the first Oracle-scala migration
-//  This avoids the need to special case Oracle in FlywayMigrations.configurationBase
 package object oracle {}
