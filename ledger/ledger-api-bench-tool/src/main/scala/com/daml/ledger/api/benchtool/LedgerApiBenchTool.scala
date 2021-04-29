1e8fe4029291a6359841b3680a92d1823e8beb20
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-04-29 19:00:41 +0200

    Simple scaffolding for the ledger-api-bench-tool

---
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/LedgerApiBenchTool.scala
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.api.benchtool
+
+object LedgerApiBenchTool {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+}
