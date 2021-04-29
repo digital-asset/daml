1393e3863090b5ee345565b845ca99f4662ee476
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2021-04-29 10:11:15 +0200

    Precreate Oracle indexer scala migration package
    This avoids needing to special case FlywayMigrations to not include
    scala migrations which will be more robust when adding the first oracle-based
    scala migration.
    
    changelog_begin
    changelog_end

---
+++ ledger/participant-integration-api/src/main/scala/db/migration/oracle/package.scala
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.platform.db.migration
+
+// FIXME: Remove empty oracle package once we add the first Oracle-scala migration
+//  This avoids the need to special case Oracle in FlywayMigrations.configurationBase
+package object oracle {}
