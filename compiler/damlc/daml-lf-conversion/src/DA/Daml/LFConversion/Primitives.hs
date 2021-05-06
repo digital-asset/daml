700240ff5024520ddde71ec6c68ffe7a125e6c72
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-05-06 14:19:24 +0200

    Drop todo for conversion of exception primitives
    While there might be nicer ways to do this, the current way is
    consistent what we do for example with `create` which seems pretty
    comparable and the check that `ty1` is an exception type happens in
    the LF typechecker (again consistent with how we check for create that
    the type is a template type) so this seems fine.
    
    changelog_begin
    changelog_end

--- compiler/damlc/daml-lf-conversion/src/DA/Daml/LFConversion/Primitives.hs
+++ compiler/damlc/daml-lf-conversion/src/DA/Daml/LFConversion/Primitives.hs
@@ -415,8 +415,6 @@ convertPrim _ "BEArithmeticErrorMessage" (TBuiltin BTArithmeticError :-> TText) 
 convertPrim _ "BEContractErrorMessage" (TBuiltin BTContractError :-> TText) =
     EBuiltin BEContractErrorMessage
 
--- TODO #8020 https://github.com/digital-asset/daml/issues/8020
--- Handle these three in LFConversion.hs and check that ty1 is an exception type.
 convertPrim _ "EThrow" (ty1 :-> ty2) =
     ETmLam (mkVar "x", ty1) (EThrow ty2 ty1 (EVar (mkVar "x")))
 convertPrim _ "EToAnyException" (ty :-> TBuiltin BTAnyException) =
