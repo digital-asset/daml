5e9624e28d6b5eb8763b1f3d923187b48b6e8383
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-05-07 15:02:26 +0200

    Unified endpoint argument with the ledger-api-test-tool + other minor fixes

--- ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/Cli.scala
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/Cli.scala
@@ -16,22 +16,20 @@ object Cli {
     OParser.sequence(
       programName("ledger-api-bench-tool"),
       head("A tool for measuring transaction streaming performance of a ledger."),
-      opt[String]("hostname")
-        .abbr("h")
-        .text("Ledger API hostname")
+      opt[(String, Int)]("endpoint")(endpointRead)
+        .abbr("e")
+        .text("Ledger API endpoint")
+        .valueName("<hostname>:<port>")
         .optional()
-        .action((host, config) => config.copy(ledger = config.ledger.copy(hostname = host))),
-      opt[Int]("port")
-        .abbr("p")
-        .text("Ledger API port")
-        .optional()
-        .action((port, config) => config.copy(ledger = config.ledger.copy(port = port))),
+        .action { case ((hostname, port), config) =>
+          config.copy(ledger = config.ledger.copy(hostname = hostname, port = port))
+        },
       opt[Config.StreamConfig]("consume-stream")
         .abbr("s")
         .text(
           s"Stream configuration."
         )
-        .valueName("streamType=<transactions|transactionTrees>,name=<streamName>,party=<party>")
+        .valueName("streamType=<transactions|transaction-trees>,name=<streamName>,party=<party>")
         .action { case (streamConfig, config) => config.copy(streamConfig = Some(streamConfig)) },
       help("help").text("Prints this information"),
     )
@@ -54,7 +52,7 @@ object Cli {
 
   private object Reads {
     implicit val streamConfigRead: Read[Config.StreamConfig] =
-      implicitly[Read[Map[String, String]]].map { m =>
+      Read.mapRead[String, String].map { m =>
         def stringField(fieldName: String): Either[String, String] =
           m.get(fieldName) match {
             case Some(value) => Right(value)
@@ -66,7 +64,7 @@ object Cli {
           party <- stringField("party")
           streamType <- stringField("streamType").flatMap[String, Config.StreamConfig.StreamType] {
             case "transactions" => Right(Config.StreamConfig.StreamType.Transactions)
-            case "transactionTrees" => Right(Config.StreamConfig.StreamType.TransactionTrees)
+            case "transaction-trees" => Right(Config.StreamConfig.StreamType.TransactionTrees)
             case invalid => Left(s"Invalid stream type: $invalid")
           }
         } yield Config.StreamConfig(
@@ -76,6 +74,22 @@ object Cli {
         )
 
         config.fold(error => throw new IllegalArgumentException(error), identity)
+      }
+
+    def endpointRead: Read[(String, Int)] = new Read[(String, Int)] {
+      val arity = 1
+      val reads: String => (String, Int) = { s: String =>
+        splitAddress(s) match {
+          case (k, v) => Read.stringRead.reads(k) -> Read.intRead.reads(v)
+        }
+      }
+    }
+
+    private def splitAddress(s: String): (String, String) =
+      s.indexOf(':') match {
+        case -1 =>
+          throw new IllegalArgumentException("Addresses should be specified as `<host>:<port>`")
+        case n: Int => (s.slice(0, n), s.slice(n + 1, s.length))
       }
   }
 
