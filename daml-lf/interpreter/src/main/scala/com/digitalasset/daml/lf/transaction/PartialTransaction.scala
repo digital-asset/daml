490378fc6d104dc2cf918442342a93176067a247
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-05-07 09:34:52 +0200

    review feedback
    changelog_begin
    changelog_end

--- daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/transaction/PartialTransaction.scala
+++ daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/transaction/PartialTransaction.scala
@@ -486,12 +486,10 @@ private[lf] case class PartialTransaction(
             keys = mbKey match {
               case Some(kWithM) if consuming =>
                 val gkey = GlobalKey(templateId, kWithM.key)
-                (keys.get(gkey), globalKeyInputs.get(gkey)) match {
+                keys.get(gkey).orElse(globalKeyInputs.get(gkey)) match {
                   // An archive can only mark a key as inactive
                   // if it was brought into scope before.
-                  case (Some(KeyActive(cid)), _) if cid == targetId =>
-                    keys.updated(gkey, KeyInactive)
-                  case (None, Some(KeyActive(cid))) if cid == targetId =>
+                  case Some(KeyActive(cid)) if cid == targetId =>
                     keys.updated(gkey, KeyInactive)
                   // If the key was not in scope or mapped to a different cid, we don’t change keys. Instead we will do
                   // an activeness check when looking it up later.
