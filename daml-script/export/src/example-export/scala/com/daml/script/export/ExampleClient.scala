c4a0dbe3aacfb516125cc8247983add603c72bdb
Andreas Herrmann <andreas.herrmann@tweag.io>
2021-05-11 15:57:25 +0200

    Build Daml ledger export
    changelog_begin
    changelog_end

---
+++ daml-script/export/src/example-export/scala/com/daml/script/export/ExampleClient.scala
@@ -0,0 +1,112 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.script.export
+
+import java.io.File
+import java.nio.file.{Path, Paths}
+import java.time.Duration
+
+import com.daml.ledger.api.tls.TlsConfiguration
+import com.daml.lf.engine.script.{RunnerConfig, RunnerMain}
+
+case class ExampleClientConfig(
+    darPath: File,
+    targetPort: Int,
+    outputPath: Path,
+)
+
+object ExampleClientConfig {
+  def parse(args: Array[String]): Option[ExampleClientConfig] =
+    parser.parse(
+      args,
+      ExampleClientConfig(
+        darPath = null,
+        targetPort = -1,
+        outputPath = null,
+      ),
+    )
+
+  private def parseExportOut(
+      envVar: String
+  ): Either[String, ExampleClientConfig => ExampleClientConfig] = {
+    if (envVar.isEmpty) Left("Environment variable EXPORT_OUT must not be empty")
+    else
+      envVar.split(" ") match {
+        case Array(export_daml) =>
+          val export_daml_path = Paths.get(export_daml)
+          if (export_daml_path.getParent == null) {
+            Left("First component in environment variable EXPORT_OUT has no parent")
+          } else {
+            Right(c => c.copy(outputPath = export_daml_path.getParent))
+          }
+        case _ => Left("Environment variable EXPORT_OUT must contain one path")
+      }
+  }
+
+  private val parser = new scopt.OptionParser[ExampleClientConfig]("script-export") {
+    help("help")
+      .text("Show this help message.")
+    opt[Int]("target-port")
+      .required()
+      .action((x, c) => c.copy(targetPort = x))
+      .text("Daml ledger port to connect to.")
+    opt[String]("output")
+      .hidden()
+      .withFallback(() => sys.env.getOrElse("EXPORT_OUT", ""))
+      .validate(x => parseExportOut(x).map(_ => ()))
+      .action { (x, c) =>
+        parseExportOut(x) match {
+          case Left(msg) =>
+            throw new RuntimeException(s"Failed to validate EXPORT_OUT environment variable: $msg")
+          case Right(f) => f(c)
+        }
+      }
+    arg[File]("dar")
+      .required()
+      .action((f, c) => c.copy(darPath = f))
+      .text("Path to the dar file containing the initialization script")
+  }
+}
+
+object ExampleClient {
+  def main(args: Array[String]): Unit = {
+    ExampleClientConfig.parse(args) match {
+      case Some(clientConfig) => main(clientConfig)
+      case None => sys.exit(1)
+    }
+  }
+  def main(clientConfig: ExampleClientConfig): Unit = {
+    RunnerMain.main(
+      RunnerConfig(
+        darPath = clientConfig.darPath,
+        scriptIdentifier = "ScriptExample:initializeFixed",
+        ledgerHost = Some("localhost"),
+        ledgerPort = Some(clientConfig.targetPort),
+        participantConfig = None,
+        timeMode = Some(RunnerConfig.DefaultTimeMode),
+        commandTtl = Duration.ofSeconds(30L),
+        inputFile = None,
+        outputFile = None,
+        accessTokenFile = None,
+        tlsConfig = TlsConfiguration(false, None, None, None),
+        jsonApi = false,
+        maxInboundMessageSize = RunnerConfig.DefaultMaxInboundMessageSize,
+        applicationId = None,
+      )
+    )
+    Main.main(
+      Config.Empty.copy(
+        ledgerHost = "localhost",
+        ledgerPort = clientConfig.targetPort,
+        parties = Seq("Alice", "Bob"),
+        exportType = Some(
+          Config.EmptyExportScript.copy(
+            sdkVersion = "0.0.0",
+            outputPath = clientConfig.outputPath,
+          )
+        ),
+      )
+    )
+  }
+}
