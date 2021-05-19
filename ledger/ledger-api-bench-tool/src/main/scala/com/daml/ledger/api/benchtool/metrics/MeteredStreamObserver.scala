ae523c7e2f286aec8d9e36a85da050b360fc7a8a
Kamil Bożek <72492440+kamil-da@users.noreply.github.com>
2021-05-19 10:07:34 +0200

    `ledger-api-bench-tool` - support for multiple streams [DPP-372] (#9691)
    * Added resource management and the LedgerIdentityService
    
    * Added the TransactionService
    
    * Added stream configuration options
    
    * Options for ledger configuration
    
    * Minor improvements
    
    * Refactored packages
    
    * CHANGELOG_BEGIN
    - [Integration Kit] - Created the ledger-api-bench-tool prototype for benchmarking ledger transaction streaming capabilities
    CHANGELOG_END
    
    * Handler for stream completion
    
    * Transaction stream observer with count metrics
    
    * Generic CountingStreamObserver
    
    * Size metrics
    
    * Rounding results
    
    * Simplified observers hierarchy
    
    * Reporting period program argument
    
    * Metric[T] trait
    
    * Named streams
    
    * Renamed reporting-period parameter to log-interval
    
    * Template filters
    
    * Configurable ledger offset parameter
    
    * Delay metric
    
    * Improved metrics naming
    
    * Consumption speed metric
    
    CHANGELOG_BEGIN
    - [Integration Kit] - added metrics to the ledger-api-bench-tool
    CHANGELOG_END
    
    * Minor fix
    
    * Fixed stream type parameter name
    
    * Multi-stream support
    
    * Pretty print config
    
    * Fixed rounding of metric values
    
    * Pretty summary
    
    * Minor change
    
    * CLI parameters for concurrency settings
    
    * Minor change
    
    * Actor-based metrics
    
    * Resource management for the typed ActorSystem
    
    * Abstract MetricsManager
    
    * Immutable metrics
    
    * TransactionMetrics
    
    * Cleaner TransactionService
    
    * Factoring out stream observer from the TransactionService
    
    * Fixed transaction trees filters
    
    * CHANGELOG_BEGIN
    - [Integration Kit] - support for multiple streams in the ledger-api-bench-tool
    CHANGELOG_END
    
    * Removed sleeping in the stream observer
    
    * Improved README.md
    
    * Fixed build errors in Scala 2.13
    
    * Fix counting metric total rate
    
    * Improved concurrency CLI parameters
    
    * Changed the log level of stream completed notification
    
    * Renamed stream observer

---
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/metrics/MeteredStreamObserver.scala
@@ -0,0 +1,27 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.api.benchtool.metrics
+
+import akka.actor.typed.ActorRef
+import org.slf4j.Logger
+
+class MeteredStreamObserver[T](
+    val streamName: String,
+    logger: Logger,
+    metricsManager: ActorRef[MetricsManager.Message[T]],
+) extends ObserverWithResult[T](logger) {
+  import MetricsManager.Message._
+
+  override def onNext(value: T): Unit = {
+    metricsManager ! NewValue(value)
+    super.onNext(value)
+  }
+
+  override def onCompleted(): Unit = {
+    logger.debug(withStreamName(s"Sending $StreamCompleted notification."))
+    metricsManager ! StreamCompleted()
+    super.onCompleted()
+  }
+
+}
