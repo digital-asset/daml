f09a9a9e2bbfeb1413b087ac7d967f892def74c8
victor.mueller@digitalasset.com <mueller.vpr@gmail.com>
2021-05-20 19:07:22 +0200

    Add a simple nameof macro lib which has one function to return the fqn of the current method

---
+++ libs-scala/nameof/src/main/scala/com/digitalasset/nameof/NameOf.scala
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.nameof
+
+trait NameOf {
+  import scala.language.experimental.macros
+
+  /** Obtain the full qualified identifier name of a method as a constant string.
+    *
+    * Example usage:
+    * {{{
+    *   package foo.bar.spam
+    *   case class Ham() {
+    *      def ham() = qualifiedNameOfCurrentFunc()
+    *    }
+    *
+    *   Ham().ham() == "foo.bar.spam.Ham.ham"
+    * }}}
+    */
+  def qualifiedNameOfCurrentFunc: String = macro NameOfImpl.nameOf
+}
+object NameOf extends NameOf
