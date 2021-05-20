859a6e2b0106e061e35d70e95ca02c1ebab8922f
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-05-20 17:33:58 +0200

    Added copyright to a new file

--- ledger/ledger-api-bench-tool/src/test/suite/scala/com/daml/ledger/api/benchtool/metrics/ServiceLevelObjectiveSpec.scala
+++ ledger/ledger-api-bench-tool/src/test/suite/scala/com/daml/ledger/api/benchtool/metrics/ServiceLevelObjectiveSpec.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.api.benchtool
 
 import com.daml.ledger.api.benchtool.metrics.Metric.DelayMetric
