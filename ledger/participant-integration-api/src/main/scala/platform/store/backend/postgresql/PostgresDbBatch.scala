6c722f70201bb6e9ad93c209dc03141346789042
Marton Nagy <marton.nagy@digitalasset.com>
2021-06-01 19:45:01 +0200

    Cleanup/refactoring postgres-batching
    changelog_begin
    changelog_end

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/postgresql/PostgresDbBatch.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/postgresql/PostgresDbBatch.scala
@@ -6,6 +6,8 @@ package com.daml.platform.store.backend.postgresql
 import com.daml.platform.store.backend.DBDTOV1
 import com.daml.scalautil.NeverEqualsOverride
 
+import scala.reflect.ClassTag
+
 case class PostgresDbBatch(
     eventsBatchDivulgence: Array[Array[_]],
     eventsBatchCreate: Array[Array[_]],
@@ -21,41 +23,27 @@ case class PostgresDbBatch(
 ) extends NeverEqualsOverride
 
 object PostgresDbBatch {
-  def apply(dbDtos: Vector[DBDTOV1]): PostgresDbBatch =
+  def apply(dbDtos: Vector[DBDTOV1]): PostgresDbBatch = {
+    def collectWithFilter[T <: DBDTOV1: ClassTag](filter: T => Boolean): Vector[T] =
+      dbDtos.collect { case dbDto: T if filter(dbDto) => dbDto }
+    def collect[T <: DBDTOV1: ClassTag]: Vector[T] = collectWithFilter((_: T) => false)
+    import DBDTOV1._
+    import PGSchema._
     PostgresDbBatch(
-      eventsBatchDivulgence = PGSchema.eventsDivulgence.prepareData(dbDtos.collect {
-        case dbDto: DBDTOV1.EventDivulgence => dbDto
-      }),
-      eventsBatchCreate = PGSchema.eventsCreate.prepareData(dbDtos.collect {
-        case dbDto: DBDTOV1.EventCreate => dbDto
-      }),
-      eventsBatchConsumingExercise = PGSchema.eventsConsumingExercise.prepareData(dbDtos.collect {
-        case dbDto: DBDTOV1.EventExercise if dbDto.consuming => dbDto
-      }),
+      eventsBatchDivulgence = eventsDivulgence.prepareData(collect[EventDivulgence]),
+      eventsBatchCreate = eventsCreate.prepareData(collect[EventCreate]),
+      eventsBatchConsumingExercise =
+        eventsConsumingExercise.prepareData(collectWithFilter[EventExercise](_.consuming)),
       eventsBatchNonConsumingExercise =
-        PGSchema.eventsNonConsumingExercise.prepareData(dbDtos.collect {
-          case dbDto: DBDTOV1.EventExercise if !dbDto.consuming => dbDto
-        }),
-      configurationEntriesBatch = PGSchema.configurationEntries.prepareData(dbDtos.collect {
-        case dbDto: DBDTOV1.ConfigurationEntry => dbDto
-      }),
-      packageEntriesBatch = PGSchema.packageEntries.prepareData(dbDtos.collect {
-        case dbDto: DBDTOV1.PackageEntry => dbDto
-      }),
-      packagesBatch = PGSchema.packages.prepareData(dbDtos.collect { case dbDto: DBDTOV1.Package =>
-        dbDto
-      }),
-      partiesBatch = PGSchema.parties.prepareData(dbDtos.collect { case dbDto: DBDTOV1.Party =>
-        dbDto
-      }),
-      partyEntriesBatch = PGSchema.partyEntries.prepareData(dbDtos.collect {
-        case dbDto: DBDTOV1.PartyEntry => dbDto
-      }),
-      commandCompletionsBatch = PGSchema.commandCompletions.prepareData(dbDtos.collect {
-        case dbDto: DBDTOV1.CommandCompletion => dbDto
-      }),
-      commandDeduplicationBatch = dbDtos.collect { case dbDto: DBDTOV1.CommandDeduplication =>
-        dbDto.deduplication_key
-      }.toArray,
+        eventsConsumingExercise.prepareData(collectWithFilter[EventExercise](!_.consuming)),
+      configurationEntriesBatch = configurationEntries.prepareData(collect[ConfigurationEntry]),
+      packageEntriesBatch = packageEntries.prepareData(collect[PackageEntry]),
+      packagesBatch = packages.prepareData(collect[Package]),
+      partiesBatch = parties.prepareData(collect[Party]),
+      partyEntriesBatch = partyEntries.prepareData(collect[PartyEntry]),
+      commandCompletionsBatch = commandCompletions.prepareData(collect[CommandCompletion]),
+      commandDeduplicationBatch = collect[CommandDeduplication].map(_.deduplication_key).toArray,
     )
+  }
+
 }
