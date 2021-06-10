baf7c997ab5fed75f770d3ac993b4e28594b5a51
Marcin Ziolek <marcin.ziolek@digitalasset.com>
2021-06-10 09:38:33 +0200

    add test for disclosed contract clashing on contract keys

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/ContractKeysIT.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/ContractKeysIT.scala
@@ -332,4 +332,31 @@ final class ContractKeysIT extends LedgerTestSuite {
       )
     }
   })
+
+  test(
+    "CKDivulgedContractKeyReusability",
+    "Subsequent divulged contracts can use the same contract key",
+    allocate(SingleParty, SingleParty),
+  )(implicit ec => {
+    case Participants(Participant(ledger1, party1), Participant(ledger2, party2)) =>
+      for {
+        creator1 <- ledger1.create(party1, WithKeyCreator(party1, party2))
+        withKey1 <- ledger1.exerciseAndGetContract[WithKey](
+          party1,
+          creator1.exerciseWithKeyCreator_DiscloseCreate(_, party1),
+        )
+
+        _ <- ledger1.exercise(party1, withKey1.exerciseWithKey_Archive(_))
+
+        // Repeat the same steps for the second time
+        creator2 <- ledger1.create(party1, WithKeyCreator(party1, party2))
+        _ <- ledger1.exerciseAndGetContract[WithKey](
+          party1,
+          creator2.exerciseWithKeyCreator_DiscloseCreate(_, party1),
+        )
+
+        // Create a dummy contract to verify that the second participant is working
+        _ <- ledger2.create(party2, WithKeyCreator(party2, party1))
+      } yield ()
+  })
 }
