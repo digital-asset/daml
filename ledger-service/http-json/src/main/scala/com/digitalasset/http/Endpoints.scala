56b0d27a57270ac706ccbbf65cd0242df0c4473d
victor.mueller@digitalasset.com <mueller.vpr@gmail.com>
2021-06-15 11:04:22 +0200

    Improve comment

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/Endpoints.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/Endpoints.scala
@@ -130,7 +130,7 @@ class Endpoints(
         decoder.decodeCreateCommand(reqBody).liftErr(InvalidUserInput)
       ): ET[domain.CreateCommand[ApiRecord, TemplateId.RequiredPkg]]
 
-      // If this fails the error is already logged in the CommandService
+      // If the future fails the error is already logged in the CommandService
       ac <- eitherT(
         handleFutureEitherFailure(commandService.create(jwt, jwtPayload, cmd))
       ): ET[domain.ActiveContract[ApiValue]]
@@ -159,7 +159,7 @@ class Endpoints(
 
       resolvedCmd = cmd.copy(argument = apiArg, reference = resolvedRef)
 
-      // If this fails the error is already logged in the CommandService
+      // If the future fails the error is already logged in the CommandService
       resp <- eitherT(
         handleFutureEitherFailure(
           commandService.exercise(jwt, jwtPayload, resolvedCmd)
@@ -182,7 +182,7 @@ class Endpoints(
         decoder.decodeCreateAndExerciseCommand(reqBody).liftErr(InvalidUserInput)
       ): ET[domain.CreateAndExerciseCommand[ApiRecord, ApiValue, TemplateId.RequiredPkg]]
 
-      // If this fails the error is already logged in the CommandService
+      // If the future fails the error is already logged in the CommandService
       resp <- eitherT(
         handleFutureEitherFailure(
           commandService.createAndExercise(jwt, jwtPayload, cmd)
