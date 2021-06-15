758abad2d599d3c5c4baa43e7302610ced83317f
Marcin Ziolek <marcin.ziolek@digitalasset.com>
2021-06-15 17:54:27 +0200

    Log context of all updates written to the database CHANGELOG_BEGIN For every update in the index db log the full context at the INFO level. CHANGELOG_END

--- ledger/participant-integration-api/src/main/scala/platform/indexer/ExecuteUpdate.scala
+++ ledger/participant-integration-api/src/main/scala/platform/indexer/ExecuteUpdate.scala
@@ -129,7 +129,7 @@ trait ExecuteUpdate {
 
   private[indexer] def updateMetadata(
       metadataUpdateStep: OffsetUpdate
-  ): Future[PersistenceResponse] = {
+  )(implicit loggingContext: LoggingContext): Future[PersistenceResponse] = {
     val OffsetUpdate(offsetStep, update) = metadataUpdateStep
     update match {
       case PartyAddedToParticipant(
