67489f563e3f18404684961391d609329e63d2bc
Brian Healey <brian.healey@digitalasset.com>
2021-06-17 16:42:56 -0400

    cleanup

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/oracle/OracleField.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/oracle/OracleField.scala
@@ -9,36 +9,16 @@ import java.time.Instant
 import com.daml.platform.store.backend.common.{Field, TrivialField, TrivialOptionalField}
 
 private[oracle] case class OracleStringField[FROM](extract: FROM => String)
-  extends TrivialField[FROM, String] {
-  override def prepareDataTemplate(preparedStatement: PreparedStatement, index: Int, value: String): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
+  extends TrivialField[FROM, String]
 
 private[oracle] case class OracleStringOptional[FROM](extract: FROM => Option[String])
-  extends TrivialOptionalField[FROM, String] {
-  override def prepareDataTemplate(preparedStatement: PreparedStatement, index: Int, value: String): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
+  extends TrivialOptionalField[FROM, String]
 
 private[oracle] case class OracleBytea[FROM](extract: FROM => Array[Byte])
-  extends TrivialField[FROM, Array[Byte]] {
-  override def prepareDataTemplate(preparedStatement: PreparedStatement, index: Int, value: Array[Byte]): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
+  extends TrivialField[FROM, Array[Byte]]
 
 private[oracle] case class OracleByteaOptional[FROM](extract: FROM => Option[Array[Byte]])
-  extends TrivialOptionalField[FROM, Array[Byte]] {
-  override def prepareDataTemplate(preparedStatement: PreparedStatement, index: Int, value: Array[Byte]): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
+  extends TrivialOptionalField[FROM, Array[Byte]]
 
 private[oracle] case class OracleIntOptional[FROM](extract: FROM => Option[Int])
   extends Field[FROM, Option[Int], java.lang.Integer] {
@@ -84,12 +64,10 @@ private[oracle] case class OracleBooleanOptional[FROM](extract: FROM => Option[B
 
 private[oracle] case class OracleTimestamp[FROM](extract: FROM => Instant)
   extends Field[FROM, Instant, Timestamp] {
+  override def convert: Instant => java.sql.Timestamp = Timestamp.from
   override def prepareDataTemplate(preparedStatement: PreparedStatement, index: Int, value: Timestamp): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
     preparedStatement.setTimestamp(index, value)
   }
-
-  override def convert: Instant => java.sql.Timestamp = Timestamp.from
 }
 
 private[oracle] case class OracleTimestampOptional[FROM](extract: FROM => Option[Instant])
