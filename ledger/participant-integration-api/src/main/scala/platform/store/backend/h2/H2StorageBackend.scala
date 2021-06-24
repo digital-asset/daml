2c6ce631d2c9da242d919c7499a048aa9ded9673
Brian Healey <brian.healey@digitalasset.com>
2021-06-24 10:10:41 -0400

    Use cross-db fetch next $n rows only syntax instead of limit syntax that works only for postgres/h2

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/h2/H2StorageBackend.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/h2/H2StorageBackend.scala
@@ -470,7 +470,7 @@ private[backend] object H2StorageBackend
 
   private def format(parties: Set[Party]): String = parties.view.map(p => s"'$p'").mkString(",")
 
-  private def limitClause(to: Option[Int]): String = to.map(to => s"limit $to").getOrElse("")
+  private def limitClause(to: Option[Int]): String = to.map(to => s"fetch next $to rows only").getOrElse("")
 
   private def arrayIntersectionWhereClause(arrayColumn: String, parties: Set[Ref.Party]): String =
     if (parties.isEmpty)
