b31f3f06551c824a988ddf46918591751395d6ff
Brian Healey <brian.healey@digitalasset.com>
2021-06-25 20:15:06 -0400

    review feedback: OracleField and OracleSchema cleanup

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/oracle/OracleField.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/oracle/OracleField.scala
@@ -3,78 +3,11 @@
 
 package com.daml.platform.store.backend.oracle
 
-import java.lang
 import java.sql.{PreparedStatement, Timestamp}
 import java.time.Instant
-import com.daml.platform.store.backend.common.{Field, TrivialField, TrivialOptionalField}
+import com.daml.platform.store.backend.common.Field
 import spray.json._
 import spray.json.DefaultJsonProtocol._
-
-private[oracle] case class OracleStringField[FROM](extract: FROM => String)
-    extends TrivialField[FROM, String]
-
-private[oracle] case class OracleStringOptional[FROM](extract: FROM => Option[String])
-    extends TrivialOptionalField[FROM, String]
-
-private[oracle] case class OracleBytea[FROM](extract: FROM => Array[Byte])
-    extends TrivialField[FROM, Array[Byte]]
-
-private[oracle] case class OracleByteaOptional[FROM](extract: FROM => Option[Array[Byte]])
-    extends TrivialOptionalField[FROM, Array[Byte]]
-
-private[oracle] case class OracleIntOptional[FROM](extract: FROM => Option[Int])
-    extends Field[FROM, Option[Int], java.lang.Integer] {
-  override def convert: Option[Int] => Integer = _.map(Int.box).orNull
-  override def prepareDataTemplate(
-      preparedStatement: PreparedStatement,
-      index: Int,
-      value: java.lang.Integer,
-  ): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
-
-private[oracle] case class OracleBigint[FROM](extract: FROM => Long)
-    extends TrivialField[FROM, Long] {
-  override def prepareDataTemplate(
-      preparedStatement: PreparedStatement,
-      index: Int,
-      value: Long,
-  ): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
-
-private[oracle] case class OracleSmallintOptional[FROM](extract: FROM => Option[Int])
-    extends Field[FROM, Option[Int], java.lang.Integer] {
-  override def convert: Option[Int] => Integer = _.map(Int.box).orNull
-  override def prepareDataTemplate(
-      preparedStatement: PreparedStatement,
-      index: Int,
-      value: java.lang.Integer,
-  ): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
-
-private[oracle] case class OracleBooleanField[FROM](extract: FROM => Boolean)
-    extends TrivialField[FROM, Boolean]
-
-private[oracle] case class OracleBooleanOptional[FROM](extract: FROM => Option[Boolean])
-    extends Field[FROM, Option[Boolean], java.lang.Boolean] {
-  override def convert: Option[Boolean] => lang.Boolean = _.map(Boolean.box).orNull
-  override def prepareDataTemplate(
-      preparedStatement: PreparedStatement,
-      index: Int,
-      value: java.lang.Boolean,
-  ): Unit = {
-    // TODO FIXME either cleanup and use Field from common, or change the line below
-    preparedStatement.setObject(index, value)
-  }
-}
 
 private[oracle] case class OracleTimestamp[FROM](extract: FROM => Instant)
     extends Field[FROM, Instant, Timestamp] {
--- ledger/participant-integration-api/src/main/scala/platform/store/backend/oracle/OracleSchema.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/oracle/OracleSchema.scala
@@ -11,14 +11,6 @@ import com.daml.platform.store.backend.common.{AppendOnlySchema, Field, Schema, 
 
 private[oracle] object OracleSchema {
   private val OracleFieldStrategy = new FieldStrategy {
-    override def string[FROM, _](extractor: FROM => String): Field[FROM, String, _] =
-      OracleStringField(extractor)
-
-    override def stringOptional[FROM, _](
-        extractor: FROM => Option[String]
-    ): Field[FROM, Option[String], _] =
-      OracleStringOptional(extractor)
-
     override def stringArray[FROM, _](
         extractor: FROM => Iterable[String]
     ): Field[FROM, Iterable[String], _] =
@@ -29,22 +21,6 @@ private[oracle] object OracleSchema {
     ): Field[FROM, Option[Iterable[String]], _] =
       OracleStringArrayOptional(extractor)
 
-    override def bytea[FROM, _](extractor: FROM => Array[Byte]): Field[FROM, Array[Byte], _] =
-      OracleBytea(extractor)
-
-    override def byteaOptional[FROM, _](
-        extractor: FROM => Option[Array[Byte]]
-    ): Field[FROM, Option[Array[Byte]], _] =
-      OracleByteaOptional(extractor)
-
-    override def bigint[FROM, _](extractor: FROM => Long): Field[FROM, Long, _] =
-      OracleBigint(extractor)
-
-    override def smallintOptional[FROM, _](
-        extractor: FROM => Option[Int]
-    ): Field[FROM, Option[Int], _] =
-      OracleSmallintOptional(extractor)
-
     override def timestamp[FROM, _](extractor: FROM => Instant): Field[FROM, Instant, _] =
       OracleTimestamp(extractor)
 
@@ -52,17 +28,6 @@ private[oracle] object OracleSchema {
         extractor: FROM => Option[Instant]
     ): Field[FROM, Option[Instant], _] =
       OracleTimestampOptional(extractor)
-
-    override def intOptional[FROM, _](extractor: FROM => Option[Int]): Field[FROM, Option[Int], _] =
-      OracleIntOptional(extractor)
-
-    override def boolean[FROM, _](extractor: FROM => Boolean): Field[FROM, Boolean, _] =
-      OracleBooleanField(extractor)
-
-    override def booleanOptional[FROM, _](
-        extractor: FROM => Option[Boolean]
-    ): Field[FROM, Option[Boolean], _] =
-      OracleBooleanOptional(extractor)
 
     override def insert[FROM](tableName: String)(
         fields: (String, Field[FROM, _, _])*
