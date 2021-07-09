bb30de7969a000b1517b38a65706744e07ba200c
Miklos Erdelyi <miklos.erdelyi@digitalasset.com>
2021-07-09 16:42:53 +0200

    Added missing header.

--- ledger/participant-state/src/test/suite/scala/com/daml/ledger/participant/state/v2/GrpcStatusesSpec.scala
+++ ledger/participant-state/src/test/suite/scala/com/daml/ledger/participant/state/v2/GrpcStatusesSpec.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.participant.state.v2
 
 import com.daml.ledger.participant.state.v2.GrpcStatuses.{CompletionOffsetKey, DefiniteAnswerKey}
