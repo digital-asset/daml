d68550225e30e93344f67171054a5eeffecebd9b
Samir Talwar <samir.talwar@digitalasset.com>
2021-07-13 17:42:02 +0200

    ledger-on-sql: Add some documentation about the application code.
    Just to make it clear it's not used in production.

--- ledger/ledger-on-sql/src/app/scala/com/daml/ledger/on/sql/RandomLogEntryIdAllocator.scala
+++ ledger/ledger-on-sql/src/app/scala/com/daml/ledger/on/sql/RandomLogEntryIdAllocator.scala
@@ -9,6 +9,7 @@ import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlLogEntryId
 import com.daml.ledger.validator.LogEntryIdAllocator
 import com.google.protobuf.ByteString
 
+// This is intended to be used only in testing, and is therefore a trivial implementation.
 object RandomLogEntryIdAllocator extends LogEntryIdAllocator {
   override def allocate(): DamlLogEntryId =
     DamlLogEntryId.newBuilder
