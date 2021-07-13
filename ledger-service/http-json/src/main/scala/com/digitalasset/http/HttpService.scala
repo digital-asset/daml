b59f36591d34c701674f0f66b9cdb3998274a0d1
Victor Peter Rouven Müller <mueller.vpr@gmail.com>
2021-07-13 10:33:40 +0200

    [JSON-API] Correctly extract the request source URL/IP (#10244)
    * [JSON-API] Correctly extract the request source URL/IP
    
    changelog_begin
    - [JSON-API] If the service is put behind a proxy filling either of these headers X-Forwarded-For & X-Real-Ip then these will now be respected for logging the request source ip/url
    changelog_end
    
    * Return to the simple http server start code
    
    * Remove unused import
    
    * Update ledger-service/http-json/src/main/scala/com/digitalasset/http/Endpoints.scala
    
    Co-authored-by: Stephen Compall <stephen.compall@daml.com>
    
    Co-authored-by: Stephen Compall <stephen.compall@daml.com>

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/HttpService.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/HttpService.scala
@@ -5,9 +5,7 @@ package com.daml.http
 
 import akka.actor.{ActorSystem, Cancellable}
 import akka.http.scaladsl.Http
-import akka.stream.scaladsl.Sink
 import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.settings.ServerSettings
 import akka.stream.Materializer
 import com.daml.auth.TokenHolder
@@ -195,16 +193,14 @@ object HttpService {
         websocketService,
       )
 
-      ignoreConParam = (res: Future[HttpResponse]) => (_: Http.IncomingConnection) => res
-
       defaultEndpoints =
         jsonEndpoints.all orElse
-          (websocketEndpoints.transactionWebSocket andThen ignoreConParam) orElse
-          (EndpointsCompanion.notFound andThen ignoreConParam)
+          websocketEndpoints.transactionWebSocket orElse
+          EndpointsCompanion.notFound
 
       allEndpoints = staticContentConfig.cata(
         c =>
-          (StaticContentEndpoints.all(c) andThen ignoreConParam) orElse
+          StaticContentEndpoints.all(c) orElse
             defaultEndpoints,
         defaultEndpoints,
       )
@@ -213,13 +209,7 @@ object HttpService {
         Http()
           .newServerAt(address, httpPort)
           .withSettings(settings)
-          .connectionSource()
-          .to {
-            Sink.foreach { connection =>
-              connection.handleWithAsyncHandler(allEndpoints(_)(connection))
-            }
-          }
-          .run()
+          .bind(allEndpoints)
       )
 
       _ <- either(portFile.cata(f => createPortFile(f, binding), \/-(()))): ET[Unit]
