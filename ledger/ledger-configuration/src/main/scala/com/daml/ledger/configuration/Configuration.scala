68827c5a15553c8ea9b6ddd033891e07e0cb8a1d
Samir Talwar <samir.talwar@digitalasset.com>
2021-07-15 12:06:29 +0200

    ledger-configuration: Remove duplication in Configuration.

--- ledger/ledger-configuration/src/main/scala/com/daml/ledger/configuration/Configuration.scala
+++ ledger/ledger-configuration/src/main/scala/com/daml/ledger/configuration/Configuration.scala
@@ -21,6 +21,7 @@ final case class Configuration(
 )
 
 object Configuration {
+
   import com.daml.ledger.participant.state.protobuf
 
   /** Version history:
@@ -28,81 +29,6 @@ object Configuration {
     * V2: added maxDeduplicationTime
     */
   val protobufVersion: Long = 2L
-
-  def decode(bytes: Array[Byte]): Either[String, Configuration] =
-    Try(protobuf.LedgerConfiguration.parseFrom(bytes)).toEither.left
-      .map(_.getMessage)
-      .flatMap(decode)
-
-  def decode(config: protobuf.LedgerConfiguration): Either[String, Configuration] =
-    config.getVersion match {
-      case 1 => DecodeV1.decode(config)
-      case 2 => DecodeV2.decode(config)
-      case v => Left(s"Unknown version: $v")
-    }
-
-  private object DecodeV1 {
-
-    def decode(config: protobuf.LedgerConfiguration): Either[String, Configuration] =
-      for {
-        tm <-
-          if (config.hasTimeModel) {
-            decodeTimeModel(config.getTimeModel)
-          } else {
-            Left("Missing time model")
-          }
-      } yield {
-        Configuration(
-          generation = config.getGeneration,
-          timeModel = tm,
-          maxDeduplicationTime = Duration.ofDays(1),
-        )
-      }
-
-    def decodeTimeModel(tm: protobuf.LedgerTimeModel): Either[String, LedgerTimeModel] =
-      LedgerTimeModel(
-        avgTransactionLatency = parseDuration(tm.getAvgTransactionLatency),
-        minSkew = parseDuration(tm.getMinSkew),
-        maxSkew = parseDuration(tm.getMaxSkew),
-      ).toEither.left.map(e => s"decodeTimeModel: ${e.getMessage}")
-  }
-
-  private object DecodeV2 {
-
-    def decode(config: protobuf.LedgerConfiguration): Either[String, Configuration] =
-      for {
-        tm <-
-          if (config.hasTimeModel) {
-            decodeTimeModel(config.getTimeModel)
-          } else {
-            Left("Missing time model")
-          }
-        maxDeduplicationTime <-
-          if (config.hasMaxDeduplicationTime) {
-            val duration = parseDuration(config.getMaxDeduplicationTime)
-            if (duration.isNegative) {
-              Left("requirement failed: Negative maximum command time to live")
-            } else {
-              Right(duration)
-            }
-          } else {
-            Left("Missing maximum command time to live")
-          }
-      } yield {
-        Configuration(
-          generation = config.getGeneration,
-          timeModel = tm,
-          maxDeduplicationTime = maxDeduplicationTime,
-        )
-      }
-
-    def decodeTimeModel(tm: protobuf.LedgerTimeModel): Either[String, LedgerTimeModel] =
-      LedgerTimeModel(
-        avgTransactionLatency = parseDuration(tm.getAvgTransactionLatency),
-        minSkew = parseDuration(tm.getMinSkew),
-        maxSkew = parseDuration(tm.getMaxSkew),
-      ).toEither.left.map(e => s"decodeTimeModel: ${e.getMessage}")
-  }
 
   def encode(config: Configuration): protobuf.LedgerConfiguration = {
     val tm = config.timeModel
@@ -119,6 +45,68 @@ object Configuration {
       .build
   }
 
+  def decode(bytes: Array[Byte]): Either[String, Configuration] =
+    Try(protobuf.LedgerConfiguration.parseFrom(bytes)).toEither.left
+      .map(_.getMessage)
+      .flatMap(decode)
+
+  def decode(config: protobuf.LedgerConfiguration): Either[String, Configuration] =
+    config.getVersion match {
+      case 1 => decodeV1(config)
+      case 2 => decodeV2(config)
+      case v => Left(s"Unknown version: $v")
+    }
+
+  def decodeV1(config: protobuf.LedgerConfiguration): Either[String, Configuration] =
+    for {
+      tm <-
+        if (config.hasTimeModel) {
+          decodeTimeModel(config.getTimeModel)
+        } else {
+          Left("Missing time model")
+        }
+    } yield {
+      Configuration(
+        generation = config.getGeneration,
+        timeModel = tm,
+        maxDeduplicationTime = Duration.ofDays(1),
+      )
+    }
+
+  def decodeV2(config: protobuf.LedgerConfiguration): Either[String, Configuration] =
+    for {
+      tm <-
+        if (config.hasTimeModel) {
+          decodeTimeModel(config.getTimeModel)
+        } else {
+          Left("Missing time model")
+        }
+      maxDeduplicationTime <-
+        if (config.hasMaxDeduplicationTime) {
+          val duration = parseDuration(config.getMaxDeduplicationTime)
+          if (duration.isNegative) {
+            Left("requirement failed: Negative maximum command time to live")
+          } else {
+            Right(duration)
+          }
+        } else {
+          Left("Missing maximum command time to live")
+        }
+    } yield {
+      Configuration(
+        generation = config.getGeneration,
+        timeModel = tm,
+        maxDeduplicationTime = maxDeduplicationTime,
+      )
+    }
+
+  private def decodeTimeModel(tm: protobuf.LedgerTimeModel): Either[String, LedgerTimeModel] =
+    LedgerTimeModel(
+      avgTransactionLatency = parseDuration(tm.getAvgTransactionLatency),
+      minSkew = parseDuration(tm.getMinSkew),
+      maxSkew = parseDuration(tm.getMaxSkew),
+    ).toEither.left.map(e => s"decodeTimeModel: ${e.getMessage}")
+
   private def parseDuration(dur: com.google.protobuf.Duration): Duration = {
     Duration.ofSeconds(dur.getSeconds, dur.getNanos.toLong)
   }
@@ -129,5 +117,4 @@ object Configuration {
       .setNanos(dur.getNano)
       .build
   }
-
 }
