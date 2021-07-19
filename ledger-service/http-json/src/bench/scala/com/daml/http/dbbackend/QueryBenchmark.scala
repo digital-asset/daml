474e2fc64fa9fe5e360afd9cd9942cbb2c315f62
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-07-19 09:02:38 +0200

    Extend JSON API ContractDao query bench’s with different tpids
    This PR extends the test to test a full matrix of different party &
    template id numbers. Summarizing the results, as expected we index by
    party but not by template id:
    
    Benchmark           (batchSize)  (extraParties)  (extraTemplates)  Mode  Cnt  Score   Error  Units
    QueryBenchmark.run        10000               1                 0  avgt    5  0.255 ± 0.064   s/op
    QueryBenchmark.run        10000              10                 0  avgt    5  0.304 ± 0.245   s/op
    QueryBenchmark.run        10000             100                 0 avgt    5  0.296 ± 0.064   s/op
    
    Benchmark           (batchSize)  (extraParties)  (extraTemplates)  Mode  Cnt  Score   Error  Units
    QueryBenchmark.run        10000               0                 1  avgt    5  0.277 ± 0.037   s/op
    QueryBenchmark.run        10000               0                10  avgt    5  0.479 ± 0.301   s/op
    QueryBenchmark.run        10000               0               100  avgt    5  2.131 ± 0.497   s/op
    
    We know how to fix that so I’ll get on that.
    
    changelog_begin
    changelog_end

--- ledger-service/http-json/src/bench/scala/com/daml/http/dbbackend/QueryBenchmark.scala
+++ ledger-service/http-json/src/bench/scala/com/daml/http/dbbackend/QueryBenchmark.scala
@@ -3,30 +3,48 @@
 
 package com.daml.http.dbbackend
 
-import scalaz.OneAnd
+import com.daml.http.dbbackend.Queries.SurrogateTpId
+import com.daml.http.domain.{Party, TemplateId}
 import doobie.implicits._
-import com.daml.http.domain.{Party}
 import org.openjdk.jmh.annotations._
+import scalaz.OneAnd
 
 class QueryBenchmark extends ContractDaoBenchmark {
-  @Param(Array("10"))
-  var batches: Int = _
+  @Param(Array("1", "5", "9"))
+  var extraParties: Int = _
+
+  @Param(Array("1", "5", "9"))
+  var extraTemplates: Int = _
+
+  private val tpid = TemplateId("-pkg-", "M", "T")
+  private var surrogateTpid: SurrogateTpId = _
+  val party = "Alice"
 
   @Setup(Level.Trial)
   override def setup(): Unit = {
     super.setup()
-    (0 until batches - 1).foreach { batch =>
-      insertBatch("Alice", batch * batchSize)
+    surrogateTpid = insertTemplate(tpid)
+
+    val surrogateTpids = surrogateTpid :: (0 until extraTemplates)
+      .map(i => insertTemplate(TemplateId("-pkg-", "M", s"T$i")))
+      .toList
+
+    val parties: List[String] = party :: (0 until extraParties).map(i => s"p$i").toList
+
+    var offset = 0
+    parties.foreach { p =>
+      surrogateTpids.foreach { t =>
+        insertBatch(p, t, offset)
+        offset += batchSize
+      }
     }
-    insertBatch("Bob", (batches - 1) * batchSize)
-    ()
   }
 
   @Benchmark @BenchmarkMode(Array(Mode.AverageTime))
   def run(): Unit = {
     implicit val driver: SupportedJdbcDriver = dao.jdbcDriver
     val result = dao
-      .transact(ContractDao.selectContracts(OneAnd(Party("Bob"), Set.empty), tpid, fr"1 = 1"))
+      .transact(ContractDao.selectContracts(OneAnd(Party(party), Set.empty), tpid, fr"1 = 1"))
       .unsafeRunSync()
     assert(result.size == batchSize)
   }
