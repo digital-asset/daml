7647fa54d89e25909becc83b17196d1c8857477a
Stephen Compall <stephen.compall@daml.com>
2021-07-20 19:12:44 -0400

    test simplest case of projectedIndex; remove uniqueSets tests

--- ledger-service/db-backend/src/test/scala/com/digitalasset/http/dbbackend/QueriesSpec.scala
+++ ledger-service/db-backend/src/test/scala/com/digitalasset/http/dbbackend/QueriesSpec.scala
@@ -3,12 +3,32 @@
 
 package com.daml.http.dbbackend
 
+import doobie.implicits._
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.wordspec.AnyWordSpec
-import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import scala.collection.immutable.Seq
+import scalaz.\/
 
-class QueriesSpec extends AnyWordSpec with Matchers with ScalaCheckDrivenPropertyChecks {
+class QueriesSpec extends AnyWordSpec with Matchers with TableDrivenPropertyChecks {
+  import QueriesSpec._
+
+  "projectedIndex" should {
+    import Queries.{projectedIndex, SurrogateTpId}
+
+    val cases = Table(
+      ("map", "projection"),
+      (Seq((SurrogateTpId(55L), fr"foo") -> 0),
+        sql"CASE WHEN ( tpid = ${55L}) THEN ${0}||''  ELSE NULL END "),
+    )
+
+    "yield expected expressions for sample inputs" in forEvery(cases) {(map, projection) =>
+      val frag = projectedIndex(map, sql"tpid")
+      frag.toString should ===(projection.toString)
+      fragmentElems(frag) should ===(fragmentElems(projection))
+    }
+  }
+  /*
   "uniqueSets" should {
     import Queries.uniqueSets
     "return only a map if given and non-empty" in forAll { (m: Map[Int, Int], c: (Int, Int)) =>
@@ -30,6 +50,20 @@ class QueriesSpec extends AnyWordSpec with Matchers with ScalaCheckDrivenPropert
 
     "never produce an empty map" in forAll { xs: Seq[(Int, Int)] =>
       uniqueSets(xs) shouldNot contain(Map.empty)
+    }
+  }
+   */
+}
+
+object QueriesSpec {
+  // XXX dedup with ValuePredicateTest
+  import cats.data.Chain, doobie.util.fragment.{Elem, Fragment}
+
+  private def fragmentElems(frag: Fragment): Chain[Any \/ Option[Any]] = {
+    import language.reflectiveCalls, Elem.{Arg, Opt}
+    frag.asInstanceOf[{ def elems: Chain[Elem] }].elems.map {
+      case Arg(a, _) => \/.left(a)
+      case Opt(o, _) => \/.right(o)
     }
   }
 }
