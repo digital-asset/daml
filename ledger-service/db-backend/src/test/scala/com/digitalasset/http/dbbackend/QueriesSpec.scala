7a2306754c3d2fb7012c8f398b17315a4fbfcf26
Stephen Compall <stephen.compall@daml.com>
2021-07-20 19:32:30 -0400

    remove uniqueSets tests that were commented for reference

--- ledger-service/db-backend/src/test/scala/com/digitalasset/http/dbbackend/QueriesSpec.scala
+++ ledger-service/db-backend/src/test/scala/com/digitalasset/http/dbbackend/QueriesSpec.scala
@@ -43,31 +43,6 @@ class QueriesSpec extends AnyWordSpec with Matchers with TableDrivenPropertyChec
       fragmentElems(frag) should ===(fragmentElems(projection))
     }
   }
-  /*
-  "uniqueSets" should {
-    import Queries.uniqueSets
-    "return only a map if given and non-empty" in forAll { (m: Map[Int, Int], c: (Int, Int)) =>
-      val nem = m + c
-      uniqueSets(nem) should ===(Seq(nem))
-    }
-
-    "return empty for empty" in {
-      uniqueSets(Seq.empty[(Int, Int)]) should ===(Seq.empty)
-    }
-
-    "return n elements in order if all given the same key" in forAll { xs: Seq[Int] =>
-      uniqueSets(xs map ((42, _))) should ===(xs map (x => Map(42 -> x)))
-    }
-
-    "always return all elements, albeit in different order" in forAll { xs: Seq[(Byte, Int)] =>
-      uniqueSets(xs).flatten should contain theSameElementsAs xs
-    }
-
-    "never produce an empty map" in forAll { xs: Seq[(Int, Int)] =>
-      uniqueSets(xs) shouldNot contain(Map.empty)
-    }
-  }
-   */
 }
 
 object QueriesSpec {
