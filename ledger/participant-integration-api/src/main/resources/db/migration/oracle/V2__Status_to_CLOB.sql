48193374f00bcffcdbae4518fa5501bedf4e9158
danielporter <daniel.porter@digitalasset.com>
2021-07-26 09:38:59 -0400

    simplify oracle migration scripts

--- ledger/participant-integration-api/src/main/resources/db/migration/oracle/V2__Status_to_CLOB.sql
+++
@@ -1,4 +0,0 @@
-alter table participant_command_completions add ( temp clob );
-update participant_command_completions set temp=status_message;
-alter table participant_command_completions drop column status_message;
-alter table participant_command_completions rename column temp to status_message;
