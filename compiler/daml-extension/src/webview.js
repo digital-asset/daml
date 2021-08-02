63d7d1d80153552ec110df5bd4f223ad1befabea
Robin Krom <robin@digitalasset.com>
2021-08-02 16:34:00 +0200

    feature: remember checkboxes in script view
    Fixes #9177. This makes the script view remember the choices for
    'show_archive', 'show detailed disclosure' and 'table/transaction' view
    accross changes to the script.
    
    CHANGELOG_BEGIN
    [daml studio] The script view now remembers checkboxes for detailed
    disclosure view and archived contracts across script changes.
    CHANGELOG_END

--- compiler/daml-extension/src/webview.js
+++ compiler/daml-extension/src/webview.js
@@ -3,17 +3,27 @@
 
 const vscode = acquireVsCodeApi();
 function show_archived_changed() {
-  document.body.classList.toggle('hide_archived', !document.getElementById('show_archived').checked);
+  const isChecked = document.getElementById('show_archived').checked;
+  document.body.classList.toggle('hide_archived', !isChecked);
+  vscode.postMessage({
+    'command': 'set_show_archived',
+    'value': isChecked
+  });
 }
 function toggle_detailed_disclosure() {
-  document.body.classList.toggle('hidden_disclosure', !document.getElementById('show_detailed_disclosure').checked);
+  const isChecked = document.getElementById('show_detailed_disclosure').checked;
+  document.body.classList.toggle('hidden_disclosure', !isChecked);
+  vscode.postMessage({
+    'command': 'set_show_detailed_disclosure',
+    'value': isChecked
+  });
 }
 function toggle_view() {
   document.body.classList.toggle('hide_transaction');
   document.body.classList.toggle('hide_table');
   vscode.postMessage({
-    'command': 'selected_view',
-    'value': document.body.classList.contains('hide_transaction') ? 'table' : 'transaction'
+    'command': 'set_selected_view',
+    'value': document.body.classList.contains('hide_transaction') ? 'table' : 'transaction',
   });
 }
 window.addEventListener('message', event => {
@@ -23,8 +33,8 @@ window.addEventListener('message', event => {
         document.body.classList.remove('hide_note');
         document.getElementById('note').innerHTML = message.value;
         break;
-    case 'select_view':
-      switch (message.value) {
+    case 'set_view':
+      switch (message.value.selected) {
         case 'transaction':
           document.body.classList.remove('hide_transaction');
           document.body.classList.add('hide_table');
@@ -34,9 +44,28 @@ window.addEventListener('message', event => {
           document.body.classList.remove('hide_table');
           break;
         default:
-          console.log('Unexpected value for select_view: ' + message.value);
+          console.log('Unexpected value for select_view: ' + message.value.selected);
           break;
       }
+      if (message.value.showArchived) {
+        document.body.classList.remove('hide_archived');
+        document.body.classList.add('show_archived');
+        document.getElementById('show_archived').checked = true;
+      } else {
+        document.body.classList.remove('show_archived');
+        document.body.classList.add('hide_archived');
+        document.getElementById('show_archived').checked = false;
+      }
+      if (message.value.showDetailedDisclosure) {
+        document.body.classList.remove('hidden_disclosure');
+        document.body.classList.add('show_disclosure');
+        document.getElementById('show_detailed_disclosure').checked = true;
+      } else {
+        document.body.classList.remove('show_disclosure');
+        document.body.classList.add('hidden_disclosure');
+        document.getElementById('show_detailed_disclosure').checked = false;
+      }
+
       break;
   }
 });
