efe7e5079fc5585801c45e78b03798f20b25b0cb
Samir Talwar <samir.talwar@digitalasset.com>
2021-08-03 15:25:51 +0200

    Avoid mocks in the configuration management service test.

--- ledger/participant-integration-api/src/test/suite/scala/db/migration/translation/apiserver/services/admin/ApiConfigManagementServiceSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/db/migration/translation/apiserver/services/admin/ApiConfigManagementServiceSpec.scala
@@ -4,12 +4,14 @@
 package com.daml.platform.apiserver.services.admin
 
 import java.time.{Duration => jDuration}
-import java.util.concurrent.{CompletableFuture, CompletionStage}
+import java.util.concurrent.CompletableFuture.completedFuture
+import java.util.concurrent.CompletionStage
 
+import akka.NotUsed
 import akka.stream.scaladsl.Source
 import com.daml.api.util.TimeProvider
 import com.daml.ledger.api.domain
-import com.daml.ledger.api.domain.LedgerOffset.Absolute
+import com.daml.ledger.api.domain.LedgerOffset
 import com.daml.ledger.api.testing.utils.AkkaBeforeAndAfterAll
 import com.daml.ledger.api.v1.admin.config_management_service.{SetTimeModelRequest, TimeModel}
 import com.daml.ledger.configuration.{Configuration, LedgerTimeModel}
@@ -17,35 +19,30 @@ import com.daml.ledger.participant.state.index.v2.IndexConfigManagementService
 import com.daml.ledger.participant.state.{v2 => state}
 import com.daml.lf.data.{Ref, Time}
 import com.daml.logging.LoggingContext
+import com.daml.platform.apiserver.services.admin.ApiConfigManagementServiceSpec._
 import com.daml.platform.configuration.LedgerConfiguration
 import com.daml.telemetry.TelemetrySpecBase._
 import com.daml.telemetry.{TelemetryContext, TelemetrySpecBase}
 import com.google.protobuf.duration.Duration
 import com.google.protobuf.timestamp.Timestamp
-import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
 
+import scala.collection.immutable
 import scala.concurrent.Future
 
 class ApiConfigManagementServiceSpec
     extends AsyncWordSpec
-    with TelemetrySpecBase
-    with MockitoSugar
     with Matchers
-    with ArgumentMatchersSugar
+    with TelemetrySpecBase
     with AkkaBeforeAndAfterAll {
-
-  import ApiConfigManagementServiceSpec._
 
   private implicit val loggingContext: LoggingContext = LoggingContext.ForTesting
 
   "ApiConfigManagementService" should {
     "propagate trace context" in {
-      val mockReadBackend = mockIndexConfigManagementService()
-
       val apiConfigManagementService = ApiConfigManagementService.createApiService(
-        mockReadBackend,
+        new FakeStreamingIndexConfigManagementService(someConfigurationEntries),
         TestWriteConfigService,
         TimeProvider.UTC,
         LedgerConfiguration.defaultLocalLedger,
@@ -65,18 +62,6 @@ class ApiConfigManagementServiceSpec
         }
     }
   }
-
-  private def mockIndexConfigManagementService() = {
-    val mockIndexConfigManagementService = mock[IndexConfigManagementService]
-    when(mockIndexConfigManagementService.lookupConfiguration()(any[LoggingContext]))
-      .thenReturn(Future.successful(None))
-    when(
-      mockIndexConfigManagementService.configurationEntries(any[Option[Absolute]])(
-        any[LoggingContext]
-      )
-    ).thenReturn(configurationEntries)
-    mockIndexConfigManagementService
-  }
 }
 
 object ApiConfigManagementServiceSpec {
@@ -84,15 +69,16 @@ object ApiConfigManagementServiceSpec {
 
   private val aConfigurationGeneration = 0L
 
-  private val configurationEntries = Source.single(
-    Absolute(Ref.LedgerString.assertFromString("0")) -> domain.ConfigurationEntry.Accepted(
-      aSubmissionId,
-      Configuration(
-        aConfigurationGeneration,
-        LedgerTimeModel.reasonableDefault,
-        jDuration.ZERO,
-      ),
-    )
+  private val someConfigurationEntries = List(
+    LedgerOffset.Absolute(Ref.LedgerString.assertFromString("0")) ->
+      domain.ConfigurationEntry.Accepted(
+        aSubmissionId,
+        Configuration(
+          aConfigurationGeneration,
+          LedgerTimeModel.reasonableDefault,
+          jDuration.ZERO,
+        ),
+      )
   )
 
   private val aSetTimeModelRequest = SetTimeModelRequest(
@@ -108,6 +94,20 @@ object ApiConfigManagementServiceSpec {
     ),
   )
 
+  private final class FakeStreamingIndexConfigManagementService(
+      entries: immutable.Iterable[(LedgerOffset.Absolute, domain.ConfigurationEntry)]
+  ) extends IndexConfigManagementService {
+    override def lookupConfiguration()(implicit
+        loggingContext: LoggingContext
+    ): Future[Option[(LedgerOffset.Absolute, Configuration)]] =
+      Future.successful(None)
+
+    override def configurationEntries(startExclusive: Option[LedgerOffset.Absolute])(implicit
+        loggingContext: LoggingContext
+    ): Source[(LedgerOffset.Absolute, domain.ConfigurationEntry), NotUsed] =
+      Source(entries)
+  }
+
   private object TestWriteConfigService extends state.WriteConfigService {
     override def submitConfiguration(
         maxRecordTime: Time.Timestamp,
@@ -118,7 +118,7 @@ object ApiConfigManagementServiceSpec {
         anApplicationIdSpanAttribute._1,
         anApplicationIdSpanAttribute._2,
       )
-      CompletableFuture.completedFuture(state.SubmissionResult.Acknowledged)
+      completedFuture(state.SubmissionResult.Acknowledged)
     }
   }
 }
