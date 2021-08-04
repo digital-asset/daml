a3aac22ba7ac165e1947d836c6bee63f3691b473
Samir Talwar <samir.talwar@digitalasset.com>
2021-08-04 10:17:46 +0200

    Add tests for `ConfigManagementService.SetTimeModel`.

--- ledger/participant-integration-api/src/test/suite/scala/db/migration/translation/apiserver/services/admin/ApiConfigManagementServiceSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/db/migration/translation/apiserver/services/admin/ApiConfigManagementServiceSpec.scala
@@ -6,12 +6,13 @@ package com.daml.platform.apiserver.services.admin
 import java.time.Duration
 import java.util.concurrent.CompletableFuture.completedFuture
 import java.util.concurrent.CompletionStage
+import java.util.concurrent.atomic.{AtomicLong, AtomicReference}
 
 import akka.NotUsed
+import akka.stream.Materializer
 import akka.stream.scaladsl.Source
 import com.daml.api.util.TimeProvider
-import com.daml.ledger.api.domain
-import com.daml.ledger.api.domain.LedgerOffset
+import com.daml.ledger.api.domain.{ConfigurationEntry, LedgerOffset}
 import com.daml.ledger.api.testing.utils.AkkaBeforeAndAfterAll
 import com.daml.ledger.api.v1.admin.config_management_service.{
   GetTimeModelRequest,
@@ -20,7 +21,9 @@ import com.daml.ledger.api.v1.admin.config_management_service.{
 }
 import com.daml.ledger.configuration.{Configuration, LedgerTimeModel}
 import com.daml.ledger.participant.state.index.v2.IndexConfigManagementService
+import com.daml.ledger.participant.state.v2.{SubmissionResult, WriteConfigService}
 import com.daml.ledger.participant.state.{v2 => state}
+import com.daml.lf.data.Ref.SubmissionId
 import com.daml.lf.data.{Ref, Time}
 import com.daml.logging.LoggingContext
 import com.daml.platform.apiserver.services.admin.ApiConfigManagementServiceSpec._
@@ -29,6 +32,7 @@ import com.daml.telemetry.TelemetrySpecBase._
 import com.daml.telemetry.{TelemetryContext, TelemetrySpecBase}
 import com.google.protobuf.duration.{Duration => DurationProto}
 import com.google.protobuf.timestamp.Timestamp
+import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
 
@@ -38,6 +42,8 @@ import scala.concurrent.Future
 class ApiConfigManagementServiceSpec
     extends AsyncWordSpec
     with Matchers
+    with MockitoSugar
+    with ArgumentMatchersSugar
     with TelemetrySpecBase
     with AkkaBeforeAndAfterAll {
 
@@ -56,12 +62,13 @@ class ApiConfigManagementServiceSpec
         maxSkew = Some(DurationProto.of(2 * 60, 0)),
       )
 
+      val writeService = mock[state.WriteConfigService]
       val apiConfigManagementService = ApiConfigManagementService.createApiService(
         new FakeCurrentIndexConfigManagementService(
           LedgerOffset.Absolute(Ref.LedgerString.assertFromString("0")),
           Configuration(aConfigurationGeneration, indexedTimeModel, Duration.ZERO),
         ),
-        TestWriteConfigService,
+        writeService,
         TimeProvider.UTC,
         LedgerConfiguration.defaultLocalLedger,
         _ => Ref.SubmissionId.assertFromString("aSubmission"),
@@ -69,6 +76,8 @@ class ApiConfigManagementServiceSpec
 
       apiConfigManagementService.getTimeModel(GetTimeModelRequest.defaultInstance).map { response =>
         response.timeModel should be(Some(expectedTimeModel))
+        verifyZeroInteractions(writeService)
+        succeed
       }
     }
 
@@ -84,9 +93,10 @@ class ApiConfigManagementServiceSpec
         maxSkew = Some(DurationProto.of(2 * 60, 0)),
       )
 
+      val writeService = mock[state.WriteConfigService]
       val apiConfigManagementService = ApiConfigManagementService.createApiService(
         EmptyIndexConfigManagementService,
-        TestWriteConfigService,
+        writeService,
         TimeProvider.UTC,
         LedgerConfiguration(
           initialConfiguration =
@@ -99,7 +109,115 @@ class ApiConfigManagementServiceSpec
 
       apiConfigManagementService.getTimeModel(GetTimeModelRequest.defaultInstance).map { response =>
         response.timeModel should be(Some(expectedTimeModel))
+        verifyZeroInteractions(writeService)
+        succeed
       }
+    }
+
+    "set a new time model" in {
+      val maximumDeduplicationTime = Duration.ofHours(6)
+      val initialGeneration = 2L
+      val initialConfiguration = Configuration(
+        generation = initialGeneration,
+        timeModel = LedgerTimeModel(
+          avgTransactionLatency = Duration.ofMinutes(1),
+          minSkew = Duration.ofMinutes(2),
+          maxSkew = Duration.ofMinutes(3),
+        ).get,
+        maxDeduplicationTime = maximumDeduplicationTime,
+      )
+      val expectedGeneration = 3L
+      val expectedConfiguration = Configuration(
+        generation = expectedGeneration,
+        timeModel = LedgerTimeModel(
+          avgTransactionLatency = Duration.ofMinutes(2),
+          minSkew = Duration.ofMinutes(1),
+          maxSkew = Duration.ofSeconds(30),
+        ).get,
+        maxDeduplicationTime = maximumDeduplicationTime,
+      )
+
+      val timeProvider = TimeProvider.UTC
+      val maximumRecordTime = timeProvider.getCurrentTime.plusSeconds(60)
+
+      val (indexService, writeService, currentConfiguration) = fakeServices(
+        startingOffset = 7,
+        submissions = Seq(Ref.SubmissionId.assertFromString("one") -> initialConfiguration),
+      )
+      val apiConfigManagementService = ApiConfigManagementService.createApiService(
+        indexService,
+        writeService,
+        timeProvider,
+        LedgerConfiguration.defaultLocalLedger,
+      )
+
+      apiConfigManagementService
+        .setTimeModel(
+          SetTimeModelRequest.of(
+            "some submission ID",
+            maximumRecordTime = Some(Timestamp.of(maximumRecordTime.getEpochSecond, 0)),
+            configurationGeneration = initialGeneration,
+            newTimeModel = Some(
+              TimeModel(
+                avgTransactionLatency = Some(DurationProto.of(2 * 60, 0)),
+                minSkew = Some(DurationProto.of(60, 0)),
+                maxSkew = Some(DurationProto.of(30, 0)),
+              )
+            ),
+          )
+        )
+        .map { response =>
+          response.configurationGeneration should be(expectedGeneration)
+          currentConfiguration() should be(Some(expectedConfiguration))
+        }
+    }
+
+    "set a new time model using the initial configuration if none is indexed yet" in {
+      val initialGeneration = LedgerConfiguration.NoGeneration
+      val expectedGeneration = LedgerConfiguration.StartingGeneration
+      val expectedConfiguration = Configuration(
+        generation = expectedGeneration,
+        timeModel = LedgerTimeModel(
+          avgTransactionLatency = Duration.ofSeconds(10),
+          minSkew = Duration.ofSeconds(20),
+          maxSkew = Duration.ofSeconds(40),
+        ).get,
+        maxDeduplicationTime = Duration.ofDays(1),
+      )
+
+      val timeProvider = TimeProvider.UTC
+      val maximumRecordTime = timeProvider.getCurrentTime.plusSeconds(60)
+
+      val (indexService, writeService, currentConfiguration) = fakeServices(
+        startingOffset = 1,
+        submissions = Seq.empty,
+      )
+      val apiConfigManagementService = ApiConfigManagementService.createApiService(
+        indexService,
+        writeService,
+        timeProvider,
+        LedgerConfiguration.defaultLocalLedger,
+      )
+
+      apiConfigManagementService
+        .setTimeModel(
+          SetTimeModelRequest.of(
+            "a submission ID",
+            maximumRecordTime = Some(Timestamp.of(maximumRecordTime.getEpochSecond, 0)),
+            configurationGeneration = initialGeneration,
+            newTimeModel = Some(
+              TimeModel(
+                avgTransactionLatency = Some(DurationProto.of(10, 0)),
+                minSkew = Some(DurationProto.of(20, 0)),
+                maxSkew = Some(DurationProto.of(40, 0)),
+              )
+            ),
+          )
+        )
+        .map { response =>
+          response.configurationGeneration should be(expectedGeneration)
+          currentConfiguration() should be(Some(expectedConfiguration))
+        }
     }
 
     "propagate trace context" in {
@@ -133,7 +251,7 @@ object ApiConfigManagementServiceSpec {
 
   private val someConfigurationEntries = List(
     LedgerOffset.Absolute(Ref.LedgerString.assertFromString("0")) ->
-      domain.ConfigurationEntry.Accepted(
+      ConfigurationEntry.Accepted(
         aSubmissionId,
         Configuration(
           aConfigurationGeneration,
@@ -164,7 +282,7 @@ object ApiConfigManagementServiceSpec {
 
     override def configurationEntries(startExclusive: Option[LedgerOffset.Absolute])(implicit
         loggingContext: LoggingContext
-    ): Source[(LedgerOffset.Absolute, domain.ConfigurationEntry), NotUsed] =
+    ): Source[(LedgerOffset.Absolute, ConfigurationEntry), NotUsed] =
       Source.never
   }
 
@@ -179,12 +297,12 @@ object ApiConfigManagementServiceSpec {
 
     override def configurationEntries(startExclusive: Option[LedgerOffset.Absolute])(implicit
         loggingContext: LoggingContext
-    ): Source[(LedgerOffset.Absolute, domain.ConfigurationEntry), NotUsed] =
+    ): Source[(LedgerOffset.Absolute, ConfigurationEntry), NotUsed] =
       Source.never
   }
 
   private final class FakeStreamingIndexConfigManagementService(
-      entries: immutable.Iterable[(LedgerOffset.Absolute, domain.ConfigurationEntry)]
+      entries: immutable.Iterable[(LedgerOffset.Absolute, ConfigurationEntry)]
   ) extends IndexConfigManagementService {
     override def lookupConfiguration()(implicit
         loggingContext: LoggingContext
@@ -193,7 +311,7 @@ object ApiConfigManagementServiceSpec {
 
     override def configurationEntries(startExclusive: Option[LedgerOffset.Absolute])(implicit
         loggingContext: LoggingContext
-    ): Source[(LedgerOffset.Absolute, domain.ConfigurationEntry), NotUsed] =
+    ): Source[(LedgerOffset.Absolute, ConfigurationEntry), NotUsed] =
       Source(entries)
   }
 
@@ -209,5 +327,54 @@ object ApiConfigManagementServiceSpec {
       )
       completedFuture(state.SubmissionResult.Acknowledged)
     }
+  }
+
+  private def fakeServices(
+      startingOffset: Long,
+      submissions: Iterable[(Ref.SubmissionId, Configuration)],
+  )(implicit
+      materializer: Materializer
+  ): (IndexConfigManagementService, WriteConfigService, () => Option[Configuration]) = {
+    val currentOffset = new AtomicLong(startingOffset)
+    val (configurationQueue, configurationSource) =
+      Source.queue[(Long, SubmissionId, Configuration)](1).preMaterialize()
+    submissions.foreach { case (submissionId, configuration) =>
+      configurationQueue.offer((currentOffset.getAndIncrement(), submissionId, configuration))
+    }
+    val currentConfiguration =
+      new AtomicReference[Option[(LedgerOffset.Absolute, Configuration)]](None)
+
+    val indexService = new IndexConfigManagementService {
+      private val source = configurationSource
+        .map { case (offset, submissionId, configuration) =>
+          val ledgerOffset =
+            LedgerOffset.Absolute(Ref.LedgerString.assertFromString(offset.toString))
+          currentConfiguration.set(Some(ledgerOffset -> configuration))
+          val entry = ConfigurationEntry.Accepted(submissionId, configuration)
+          ledgerOffset -> entry
+        }
+        .preMaterialize()
+
+      override def lookupConfiguration()(implicit
+          loggingContext: LoggingContext
+      ): Future[Option[(LedgerOffset.Absolute, Configuration)]] =
+        Future.successful(currentConfiguration.get())
+
+      override def configurationEntries(startExclusive: Option[LedgerOffset.Absolute])(implicit
+          loggingContext: LoggingContext
+      ): Source[(LedgerOffset.Absolute, ConfigurationEntry), NotUsed] =
+        source._2
+    }
+    val writeService = new WriteConfigService {
+      override def submitConfiguration(
+          maxRecordTime: Time.Timestamp,
+          submissionId: SubmissionId,
+          configuration: Configuration,
+      )(implicit telemetryContext: TelemetryContext): CompletionStage[SubmissionResult] = {
+        configurationQueue.offer((currentOffset.getAndIncrement(), submissionId, configuration))
+        completedFuture(state.SubmissionResult.Acknowledged)
+      }
+    }
+    (indexService, writeService, () => currentConfiguration.get.map(_._2))
   }
 }
