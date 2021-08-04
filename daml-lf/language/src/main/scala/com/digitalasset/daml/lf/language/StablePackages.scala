7c6d3c5bc7c006924b0d23637e61ca8a772e8621
Andreas Herrmann <42969706+aherrmann-da@users.noreply.github.com>
2021-08-04 18:11:17 +0200

    Handle package dependencies without metadata in Daml export (#10481)
    * Expose set of stable-packages package-ids
    
    Needed in Daml ledger export to decide whether to depend on a package
    without metadata or not.
    
    changelog_begin
    changelog_end
    
    * Handle package dependencies without metadata in Daml export
    
    This addresses the immediate issue reported in
    https://github.com/digital-asset/daml/issues/10435.
    
    DALF before LF version 1.8 don't contain metadata such as the package
    name and version. However, Daml ledger export assumed that metadata was
    available to generate `--package` flags for data-dependencies.
    
    This change generates `--package=<hash>` flags for such dependencies on
    packages that don't have metadata available.
    
    This change requires additional care when checking if a package is a
    stable-package. As the package name is not available without metadata we
    cannot just check against names such as `daml-stdlib`. Instead this
    change uses the list of stable-package package-ids exposed by
    `com.daml.lf.language.StablePackages.Ids`, which was introduced in
    https://github.com/digital-asset/daml/commit/0da814d2501f87d45c525cb34454bf9fc4832f11#diff-208c6948c7c7f2b582faa8e4792bfe78b231188881947ad0cdd5046e0b9a40e7
    
    Note that this does not fully resolve #10435. Building a generated Daml
    ledger export script will fail on missing instances of standard classes
    such as `HasTemplate` because packages prior to LF version 1.8 don't
    contain type-class instances. This will be addressed in a separate PR.
    
    changelog_begin
    changelog_end
    
    * Fix Scala 2.12
    
    Co-authored-by: Andreas Herrmann <andreas.herrmann@tweag.io>

--- daml-lf/language/src/main/scala/com/digitalasset/daml/lf/language/StablePackages.scala
+++ daml-lf/language/src/main/scala/com/digitalasset/daml/lf/language/StablePackages.scala
@@ -116,5 +116,5 @@ object StablePackages {
       "99a2705ed38c1c26cbb8fe7acf36bbf626668e167a33335de932599219e0a235"
     ),
   )
-  private[lf] val Ids = nameToIdMap.values.toSet
+  val Ids = nameToIdMap.values.toSet
 }
