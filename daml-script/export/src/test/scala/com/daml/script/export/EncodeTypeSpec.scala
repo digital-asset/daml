8256fc6696f7389f7af257e2e201cc04b7008cde
Andreas Herrmann <andreas.herrmann@tweag.io>
2021-08-05 15:59:19 +0200

    test more types

--- daml-script/export/src/test/scala/com/daml/script/export/EncodeTypeSpec.scala
+++ daml-script/export/src/test/scala/com/daml/script/export/EncodeTypeSpec.scala
@@ -3,6 +3,7 @@
 
 package com.daml.script.export
 
+import com.daml.lf.data.{ImmArray, Ref}
 import com.daml.lf.language.Ast
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
@@ -41,5 +42,44 @@ class EncodeTypeSpec extends AnyFreeSpec with Matchers {
         }
       }
     }
+    "simple types" - {
+      "type variable" in {
+        encodeType(Ast.TVar(Ref.Name.assertFromString("foo"))).render(80) shouldBe "foo"
+      }
+      "natural number" in {
+        encodeType(Ast.TNat.values(2)).render(80) shouldBe "2"
+      }
+      "type constructor" in {
+        encodeType(
+          Ast.TTyCon(
+            Ref.Identifier.assertFromString(
+              "e7b2c7155f6dd6fc569c2325be821f1269186a540d0408b9a0c9e30406f6b64b:Module:Foo"
+            )
+          )
+        ).render(80) shouldBe "Module.Foo"
+      }
+    }
+    "composed types" - {
+      "type synonym application" in {
+        val ty = Ast.TSynApp(
+          Ref.Identifier.assertFromString(
+            "e7b2c7155f6dd6fc569c2325be821f1269186a540d0408b9a0c9e30406f6b64b:Module:Foo"
+          ),
+          ImmArray(
+            Ast.TBuiltin(Ast.BTInt64),
+            Ast.TBuiltin(Ast.BTText),
+          ),
+        )
+        encodeType(ty).render(80) shouldBe "Module.Foo Int Text"
+      }
+    }
+    //"" in {
+    //  def foo(x: Ast.Type): Unit = x match {
+    //    case Ast.TSynApp(tysyn, args) =>
+    //    case Ast.TApp(tyfun, arg) =>
+    //    case Ast.TForall(binder, body) =>
+    //    case Ast.TStruct(fields) =>
+    //  }
+    //}
   }
 }
