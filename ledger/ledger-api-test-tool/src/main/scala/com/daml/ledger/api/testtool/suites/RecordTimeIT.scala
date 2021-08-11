5b135d3e0d0db0a2a3a08f6817323a7bdbea9742
Fabio Tudone <fabio.tudone@digitalasset.com>
2021-08-11 16:47:53 +0200

    Explicitly sort checkpoints by offset

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/RecordTimeIT.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/RecordTimeIT.scala
@@ -6,6 +6,7 @@ package com.daml.ledger.api.testtool.suites
 import com.daml.ledger.api.testtool.infrastructure.Allocation._
 import com.daml.ledger.api.testtool.infrastructure.Assertions._
 import com.daml.ledger.api.testtool.infrastructure.LedgerTestSuite
+import com.daml.ledger.api.v1.command_completion_service.Checkpoint
 import com.daml.ledger.test.model.Test.Dummy
 import scalapb.TimestampConverters
 
@@ -17,24 +18,35 @@ final class RecordTimeIT extends LedgerTestSuite {
     "Record Time increases monotonically",
     allocate(SingleParty),
   )(implicit ec => { case Participants(Participant(ledger, party)) =>
-    val submissions = 50
+    val submissions = 100
+
     for {
       _ <- Future.traverse(1 to submissions) { _ =>
         ledger.create(party, Dummy(party))
       }
       checkpoints <- ledger.checkpoints(submissions, ledger.begin)(party)
     } yield {
+
       val recordTimes = checkpoints
-        .flatMap(_.recordTime.toList)
-        .map(TimestampConverters.asJavaInstant)
+        .collect { case Checkpoint(Some(recordTime), Some(offset)) => recordTime -> offset }
+        .sortBy(_._2.getAbsolute)
+        .map { case (recordTime, _) => TimestampConverters.asJavaInstant(recordTime) }
       assertLength("As many record times as submissions", submissions, recordTimes)
-      val unsorted = recordTimes
+
+      val wronglySortedRecordTimes = recordTimes
         .zip(recordTimes.sorted)
         .filter { case (produced, sorted) => produced != sorted }
         .map(_._1)
+      val prettyPrintedWronglySRecordTimes =
+        wronglySortedRecordTimes
+          .mkString(
+            start = "[",
+            sep = ", ",
+            end = "]",
+          ) // Java instants' "toString" prints them in ISO format
       assert(
-        unsorted.isEmpty,
-        s"some record times are not monotonically increasing: $unsorted", // Instants will be printed in ISO format
+        wronglySortedRecordTimes.isEmpty,
+        s"some record times are not monotonically increasing: $prettyPrintedWronglySRecordTimes",
       )
     }
   })
