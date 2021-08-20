2ab7a6365b6c2808a03c820dd891ec6cf8542c47
Samir Talwar <samir.talwar@digitalasset.com>
2021-08-20 12:25:20 +0200

    participant-integration-api: Clean up `TrackerMap` a little more.

--- ledger/participant-integration-api/src/main/scala/platform/apiserver/services/tracking/TrackerMap.scala
+++ ledger/participant-integration-api/src/main/scala/platform/apiserver/services/tracking/TrackerMap.scala
@@ -37,31 +37,28 @@ private[services] final class TrackerMap(
   @volatile private var trackerBySubmitter =
     HashMap.empty[TrackerMap.Key, TrackerMap.AsyncResource[Tracker.WithLastSubmission]]
 
-  val cleanup: Runnable = {
-    require(
-      retentionPeriod < Long.MaxValue.nanoseconds,
-      s"Retention period$retentionPeriod is too long. Must be below ${Long.MaxValue} nanoseconds.",
-    )
+  require(
+    retentionPeriod < Long.MaxValue.nanoseconds,
+    s"Retention period$retentionPeriod is too long. Must be below ${Long.MaxValue} nanoseconds.",
+  )
 
-    val retentionNanos = retentionPeriod.toNanos
+  private val retentionNanos = retentionPeriod.toNanos
 
-    { () =>
-      lock.synchronized {
-        val nanoTime = System.nanoTime()
-        trackerBySubmitter foreach { case (submitter, trackerResource) =>
-          trackerResource.ifPresent(tracker =>
-            if (nanoTime - tracker.getLastSubmission > retentionNanos) {
-              logger.info(
-                s"Shutting down tracker for $submitter after inactivity of $retentionPeriod"
-              )(trackerResource.loggingContext)
-              remove(submitter)
-              tracker.close()
-            }
-          )
+  val cleanup: Runnable = () =>
+    lock.synchronized {
+      val nanoTime = System.nanoTime()
+      trackerBySubmitter foreach { case (submitter, trackerResource) =>
+        trackerResource.ifPresent { tracker =>
+          if (nanoTime - tracker.getLastSubmission > retentionNanos) {
+            logger.info(
+              s"Shutting down tracker for $submitter after inactivity of $retentionPeriod"
+            )(trackerResource.loggingContext)
+            trackerBySubmitter -= submitter
+            tracker.close()
+          }
         }
       }
     }
-  }
 
   def track(
       request: SubmitAndWaitRequest
@@ -83,9 +80,7 @@ private[services] final class TrackerMap(
                 logger.info(s"Registered tracker for submitter $submitter")
                 Tracker.WithLastSubmission(t)
               })
-
               trackerBySubmitter += submitter -> r
-
               r
             },
           )
@@ -94,16 +89,10 @@ private[services] final class TrackerMap(
       .flatMap(_.track(request))
   }
 
-  private def remove(submitter: TrackerMap.Key): Unit = lock.synchronized {
-    trackerBySubmitter -= submitter
-  }
-
-  def close(): Unit = {
-    lock.synchronized {
-      logger.info(s"Shutting down ${trackerBySubmitter.size} trackers")
-      trackerBySubmitter.values.foreach(_.close())
-      trackerBySubmitter = HashMap.empty
-    }
+  def close(): Unit = lock.synchronized {
+    logger.info(s"Shutting down ${trackerBySubmitter.size} trackers")
+    trackerBySubmitter.values.foreach(_.close())
+    trackerBySubmitter = HashMap.empty
   }
 }
 
@@ -143,16 +132,12 @@ private[services] object TrackerMap {
         state.set(Closed)
     }(DirectExecutionContext)
 
-    def flatMap[U](f: T => Future[U])(implicit ex: ExecutionContext): Future[U] = {
+    def flatMap[U](f: T => Future[U])(implicit ex: ExecutionContext): Future[U] =
       state.get() match {
         case Waiting => future.flatMap(f)
         case Closed => throw new IllegalStateException()
         case Ready(t) => f(t)
       }
-    }
-
-    def map[U](f: T => U)(implicit ex: ExecutionContext): Future[U] =
-      flatMap(t => Future.successful(f(t)))
 
     def ifPresent[U](f: T => U): Option[U] = state.get() match {
       case Ready(t) => Some(f(t))
