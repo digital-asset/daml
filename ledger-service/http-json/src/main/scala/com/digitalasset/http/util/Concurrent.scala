061acc548fafe21b770aa2d1dd8991ef08bd9880
Victor Peter Rouven Müller <mueller.vpr@gmail.com>
2021-08-20 13:20:52 +0200

    Remove unused imports in utils.Concurrent & remove packages from the tests

--- ledger-service/http-json/src/main/scala/com/digitalasset/http/util/Concurrent.scala
+++ ledger-service/http-json/src/main/scala/com/digitalasset/http/util/Concurrent.scala
@@ -3,18 +3,8 @@
 
 package com.daml.http.util
 
-import com.daml.http.util.Logging.InstanceUUID
-import com.daml.logging.{ContextualizedLogger, LoggingContextOf}
-import scalaz.MonadError
-import scalaz.Scalaz._
-
-import java.util.concurrent.TimeUnit
-
 object Concurrent {
-
-  private[this] val logger = ContextualizedLogger.get(getClass)
 
   /** Equal to a Semaphore with max count equal to one. */
   final case class Mutex() extends java.util.concurrent.Semaphore(1, true)
-
 }
