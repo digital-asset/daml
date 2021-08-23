6e26754d51b2152ea85b0ee352d3f7f62b2a56b7
Samir Talwar <samir.talwar@digitalasset.com>
2021-08-23 16:19:41 +0200

    participant-integration-api: Clean up QueueBackedTrackerSpec.

--- ledger/participant-integration-api/src/test/suite/scala/platform/apiserver/services/tracking/QueueBackedTrackerSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/platform/apiserver/services/tracking/QueueBackedTrackerSpec.scala
@@ -3,61 +3,42 @@
 
 package com.daml.platform.apiserver.services.tracking
 
-import akka.stream.OverflowStrategy
 import akka.stream.scaladsl.{Keep, Source, SourceQueueWithComplete}
 import akka.stream.testkit.TestSubscriber
 import akka.stream.testkit.scaladsl.TestSink
+import akka.stream.{Materializer, OverflowStrategy}
 import akka.{Done, NotUsed}
-import com.daml.dec.DirectExecutionContext
+import com.daml.grpc.GrpcStatus
 import com.daml.ledger.api.testing.utils.{AkkaBeforeAndAfterAll, TestingException}
 import com.daml.ledger.api.v1.command_service.SubmitAndWaitRequest
 import com.daml.ledger.api.v1.commands.Commands
 import com.daml.ledger.client.services.commands.tracker.CompletionResponse
-import com.daml.ledger.client.services.commands.tracker.CompletionResponse.QueueSubmitFailure
 import com.daml.logging.LoggingContext
+import com.daml.platform.apiserver.services.tracking.QueueBackedTracker.QueueInput
+import com.daml.platform.apiserver.services.tracking.QueueBackedTrackerSpec._
 import com.google.rpc.status.{Status => StatusProto}
-import io.grpc.Status.Code
-import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.{BeforeAndAfterEach, Inside, Succeeded}
+import org.scalatest.wordspec.AsyncWordSpec
+import org.scalatest.{BeforeAndAfterEach, Inside}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
 
 class QueueBackedTrackerSpec
-    extends AnyWordSpec
+    extends AsyncWordSpec
     with Matchers
     with BeforeAndAfterEach
-    with ScalaFutures
     with AkkaBeforeAndAfterAll
     with Inside {
 
-  override implicit def patienceConfig: PatienceConfig = PatienceConfig(5.seconds, 1.second)
-
-  private var sut: Tracker = _
-  private var consumer: TestSubscriber.Probe[NotUsed] = _
-  private var queue: SourceQueueWithComplete[QueueBackedTracker.QueueInput] = _
+  private implicit val ec: ExecutionContext = ExecutionContext.global
   private implicit val loggingContext: LoggingContext = LoggingContext.ForTesting
 
-  private def input(cid: Int) = SubmitAndWaitRequest(Some(Commands(commandId = cid.toString)))
+  private var consumer: TestSubscriber.Probe[NotUsed] = _
+  private var queue: SourceQueueWithComplete[QueueBackedTracker.QueueInput] = _
 
   override protected def beforeEach(): Unit = {
-    val (q, sink) = Source
-      .queue[QueueBackedTracker.QueueInput](1, OverflowStrategy.dropNew)
-      .map { in =>
-        in.context.success(
-          Right(
-            CompletionResponse.CompletionSuccess(in.value.getCommands.commandId, "", StatusProto())
-          )
-        )
-        NotUsed
-      }
-      .toMat(TestSink.probe[NotUsed])(Keep.both)
-      .run()
+    val (q, sink) = alwaysSuccessfulQueue(bufferSize = 1)
     queue = q
-    sut = new QueueBackedTracker(q, Future.successful(Done))
     consumer = sink
   }
 
@@ -67,56 +48,77 @@ class QueueBackedTrackerSpec
   }
 
   "Tracker Implementation" when {
-
     "input is submitted, and the queue is available" should {
-
       "work successfully" in {
-
-        val resultF1 = sut.track(input(1))
+        val tracker = new QueueBackedTracker(queue, Future.successful(Done))
+        val completion1F = tracker.track(input(1))
         consumer.requestNext()
-        val resultF = resultF1.flatMap(_ => sut.track(input(2)))(DirectExecutionContext)
+        val completion2F = tracker.track(input(2))
         consumer.requestNext()
-        whenReady(resultF)(_ => Succeeded)
+        for {
+          _ <- completion1F
+          _ <- completion2F
+        } yield succeed
       }
     }
 
     "input is submitted, and the queue is backpressuring" should {
-
       "return a RESOURCE_EXHAUSTED error" in {
-
-        sut.track(input(1))
-        whenReady(sut.track(input(2)))(failure => {
-          inside(failure) { case Left(QueueSubmitFailure(statusCode)) =>
-            statusCode.getCode should be(Code.RESOURCE_EXHAUSTED)
+        val tracker = new QueueBackedTracker(queue, Future.successful(Done))
+        tracker.track(input(1))
+        tracker.track(input(2)).map { completion =>
+          completion should matchPattern {
+            case Left(CompletionResponse.QueueSubmitFailure(GrpcStatus.RESOURCE_EXHAUSTED())) =>
           }
-        })
+        }
       }
     }
 
     "input is submitted, and the queue has been completed" should {
-
       "return an ABORTED error" in {
-
+        val tracker = new QueueBackedTracker(queue, Future.successful(Done))
         queue.complete()
-        whenReady(sut.track(input(2)))(failure => {
-          inside(failure) { case Left(QueueSubmitFailure(statusCode)) =>
-            statusCode.getCode should be(Code.ABORTED)
+        tracker.track(input(2)).map { completion =>
+          completion should matchPattern {
+            case Left(CompletionResponse.QueueSubmitFailure(GrpcStatus.ABORTED())) =>
           }
-        })
+        }
       }
     }
 
     "input is submitted, and the queue has failed" should {
-
       "return an ABORTED error" in {
-
+        val tracker = new QueueBackedTracker(queue, Future.successful(Done))
         queue.fail(TestingException("The queue fails with this error."))
-        whenReady(sut.track(input(2)))(failure => {
-          inside(failure) { case Left(QueueSubmitFailure(statusCode)) =>
-            statusCode.getCode should be(Code.ABORTED)
+        tracker.track(input(2)).map { completion =>
+          completion should matchPattern {
+            case Left(CompletionResponse.QueueSubmitFailure(GrpcStatus.ABORTED())) =>
           }
-        })
+        }
       }
     }
   }
+}
+
+object QueueBackedTrackerSpec {
+  private def input(commandId: Int) = SubmitAndWaitRequest.of(
+    commands = Some(Commands(commandId = commandId.toString))
+  )
+
+  private def alwaysSuccessfulQueue(bufferSize: Int)(implicit
+      materializer: Materializer
+  ): (SourceQueueWithComplete[QueueInput], TestSubscriber.Probe[NotUsed]) =
+    Source
+      .queue[QueueInput](bufferSize, OverflowStrategy.dropNew)
+      .map { in =>
+        val completion = CompletionResponse.CompletionSuccess(
+          commandId = in.value.getCommands.commandId,
+          transactionId = "",
+          originalStatus = StatusProto.defaultInstance,
+        )
+        in.context.success(Right(completion))
+        NotUsed
+      }
+      .toMat(TestSink.probe[NotUsed](materializer.system))(Keep.both)
+      .run()
 }
