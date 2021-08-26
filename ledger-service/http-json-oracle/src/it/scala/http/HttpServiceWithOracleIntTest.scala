936b6688841942e76e826dbbc30e3ebbc9344249
Stephen Compall <stephen.compall@daml.com>
2021-08-26 18:27:51 -0400

    disable search index for some tests, enable for others

--- ledger-service/http-json-oracle/src/it/scala/http/HttpServiceWithOracleIntTest.scala
+++ ledger-service/http-json-oracle/src/it/scala/http/HttpServiceWithOracleIntTest.scala
@@ -7,6 +7,12 @@ class HttpServiceWithOracleIntTest
     extends AbstractHttpServiceIntegrationTest
     with HttpServiceOracleInt {
 
+  // XXX SC in reality, the only tests that need this to be true are "1kb of
+  // data" &c.  That would entail splitting up
+  // AbstractHttpServiceIntegrationTest a little; it's also possible we want to
+  // run _all tests in both modes_.
+  override def disableContractPayloadIndexing = true
+
   override def staticContentConfig: Option[StaticContentConfig] = None
 
   override def wsConfig: Option[WebsocketConfig] = None
