687537fede066a411f1a5d94cb5284c5786869d9
Samir Talwar <samir.talwar@digitalasset.com>
2021-08-30 15:54:30 +0200

    sandbox-classic: Add ErrorInfo metadata for rejections.
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/sandbox-classic/src/main/scala/platform/sandbox/stores/ledger/Rejection.scala
+++ ledger/sandbox-classic/src/main/scala/platform/sandbox/stores/ledger/Rejection.scala
@@ -6,8 +6,11 @@ package com.daml.platform.sandbox.stores.ledger
 import com.daml.ledger.api.domain
 import com.daml.ledger.configuration.LedgerTimeModel
 import com.daml.ledger.participant.state.{v1, v2}
-import io.grpc.Status
+import com.daml.platform.sandbox.stores.ledger.Rejection._
+import com.google.protobuf.any.{Any => AnyProto}
+import com.google.rpc.error_details.ErrorInfo
 import com.google.rpc.status.{Status => RpcStatus}
+import io.grpc.Status
 
 sealed trait Rejection {
   val reason: String
@@ -16,17 +19,27 @@ sealed trait Rejection {
 
   def description: String
 
+  def metadata: Map[String, String]
+
   def toDomainRejectionReason: domain.RejectionReason
 
   def toStateV1RejectionReason: v1.RejectionReason
 
   def toStateV2RejectionReason: v2.Update.CommandRejected.RejectionReasonTemplate =
     v2.Update.CommandRejected.FinalReason(
-      RpcStatus.of(code.value(), description, Seq.empty)
+      RpcStatus.of(
+        code.value(),
+        description,
+        Seq(
+          AnyProto.pack(ErrorInfo.of(reason = reason, domain = errorDomain, metadata = metadata))
+        ),
+      )
     )
 }
 
 object Rejection {
+  val errorDomain = "com.daml.on.sql"
+
   object NoLedgerConfiguration extends Rejection {
     override val reason: String = "NO_LEDGER_CONFIGURATION"
 
@@ -34,6 +47,8 @@ object Rejection {
       "No ledger configuration available, cannot validate ledger time"
 
     override val code: Status.Code = Status.Code.ABORTED
+
+    override lazy val metadata: Map[String, String] = Map.empty
 
     override lazy val toDomainRejectionReason: domain.RejectionReason =
       domain.RejectionReason.InvalidLedgerTime(description)
@@ -48,6 +63,12 @@ object Rejection {
     override val code: Status.Code = Status.Code.ABORTED
 
     override lazy val description: String = outOfRange.message
+
+    override lazy val metadata: Map[String, String] = Map(
+      "ledgerTime" -> outOfRange.ledgerTime.toString,
+      "lowerBound" -> outOfRange.lowerBound.toString,
+      "upperBound" -> outOfRange.upperBound.toString,
+    )
 
     override lazy val toDomainRejectionReason: domain.RejectionReason =
       domain.RejectionReason.InvalidLedgerTime(description)
