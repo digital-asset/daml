6f08ab73946f58e6117c1a835342838658aa87c3
Stefano Baghino <stefano.baghino@digitalasset.com>
2021-08-31 19:23:11 +0200

    Add test to verify that https://github.com/digital-asset/daml/pull/10713 works
    changelog_begin
    changelog_end

--- language-support/java/bindings-rxjava/src/test/scala/com/daml/ledger/rxjava/grpc/CommandSubmissionClientImplTest.scala
+++ language-support/java/bindings-rxjava/src/test/scala/com/daml/ledger/rxjava/grpc/CommandSubmissionClientImplTest.scala
@@ -26,6 +26,8 @@ class CommandSubmissionClientImplTest
     with OptionValues
     with DataLayerHelpers {
 
+  import CommandSubmissionClientImplTest._
+
   val ledgerServices = new LedgerServices("command-submission-service-ledger")
 
   implicit class JavaOptionalAsScalaOption[A](opt: Optional[A]) {
@@ -34,13 +36,30 @@ class CommandSubmissionClientImplTest
 
   behavior of "[3.1] CommandSubmissionClientImpl.submit"
 
-  it should "timeout after deadline exceeded" in {
+  it should "timeout should work as expected across calls" in {
     ledgerServices.withCommandSubmissionClient(
-      Future.never,
+      sequence(stuck, success, stuck),
       timeout = Optional.of(Duration.of(1, ChronoUnit.SECONDS)),
     ) { (client, serviceImpl) =>
       val commands = genCommands(List.empty)
-      expectDeadlineExceeded(
+
+      withClue("The first command should be stuck") {
+        expectDeadlineExceeded(
+          client
+            .submit(
+              commands.getWorkflowId,
+              commands.getApplicationId,
+              commands.getCommandId,
+              commands.getParty,
+              commands.getCommands,
+            )
+            .timeout(TestConfiguration.timeoutInSeconds, TimeUnit.SECONDS)
+            .blockingGet()
+        )
+        serviceImpl.getSubmittedRequest.value.getCommands.ledgerId shouldBe ledgerServices.ledgerId
+      }
+
+      withClue("The second command should go through") {
         client
           .submit(
             commands.getWorkflowId,
@@ -51,58 +70,72 @@ class CommandSubmissionClientImplTest
           )
           .timeout(TestConfiguration.timeoutInSeconds, TimeUnit.SECONDS)
           .blockingGet()
-      )
-      val receivedCommands = serviceImpl.getSubmittedRequest.value.getCommands
-      receivedCommands.ledgerId shouldBe ledgerServices.ledgerId
+        serviceImpl.getSubmittedRequest.value.getCommands.ledgerId shouldBe ledgerServices.ledgerId
+      }
+
+      withClue("The third command should be stuck") {
+        expectDeadlineExceeded(
+          client
+            .submit(
+              commands.getWorkflowId,
+              commands.getApplicationId,
+              commands.getCommandId,
+              commands.getParty,
+              commands.getCommands,
+            )
+            .timeout(TestConfiguration.timeoutInSeconds, TimeUnit.SECONDS)
+            .blockingGet()
+        )
+        serviceImpl.getSubmittedRequest.value.getCommands.ledgerId shouldBe ledgerServices.ledgerId
+      }
     }
   }
 
   it should "send a commands to the ledger" in {
-    ledgerServices.withCommandSubmissionClient(Future.successful(Empty.defaultInstance)) {
-      (client, serviceImpl) =>
-        val commands = genCommands(List.empty)
-        client
-          .submit(
-            commands.getWorkflowId,
-            commands.getApplicationId,
-            commands.getCommandId,
-            commands.getParty,
-            commands.getMinLedgerTimeAbsolute,
-            commands.getMinLedgerTimeRelative,
-            commands.getDeduplicationTime,
-            commands.getCommands,
-          )
-          .timeout(TestConfiguration.timeoutInSeconds, TimeUnit.SECONDS)
-          .blockingGet()
-        val receivedCommands = serviceImpl.getSubmittedRequest.value.getCommands
-        receivedCommands.ledgerId shouldBe ledgerServices.ledgerId
-        receivedCommands.applicationId shouldBe commands.getApplicationId
-        receivedCommands.workflowId shouldBe commands.getWorkflowId
-        receivedCommands.commandId shouldBe commands.getCommandId
-        receivedCommands.minLedgerTimeAbs.map(
-          _.seconds
-        ) shouldBe commands.getMinLedgerTimeAbsolute.asScala
-          .map(_.getEpochSecond)
-        receivedCommands.minLedgerTimeAbs.map(
-          _.nanos
-        ) shouldBe commands.getMinLedgerTimeAbsolute.asScala
-          .map(_.getNano)
-        receivedCommands.minLedgerTimeRel.map(
-          _.seconds
-        ) shouldBe commands.getMinLedgerTimeRelative.asScala
-          .map(_.getSeconds)
-        receivedCommands.minLedgerTimeRel.map(
-          _.nanos
-        ) shouldBe commands.getMinLedgerTimeRelative.asScala
-          .map(_.getNano)
-        receivedCommands.party shouldBe commands.getParty
-        receivedCommands.commands.size shouldBe commands.getCommands.size()
+    ledgerServices.withCommandSubmissionClient(alwaysSucceed) { (client, serviceImpl) =>
+      val commands = genCommands(List.empty)
+      client
+        .submit(
+          commands.getWorkflowId,
+          commands.getApplicationId,
+          commands.getCommandId,
+          commands.getParty,
+          commands.getMinLedgerTimeAbsolute,
+          commands.getMinLedgerTimeRelative,
+          commands.getDeduplicationTime,
+          commands.getCommands,
+        )
+        .timeout(TestConfiguration.timeoutInSeconds, TimeUnit.SECONDS)
+        .blockingGet()
+      val receivedCommands = serviceImpl.getSubmittedRequest.value.getCommands
+      receivedCommands.ledgerId shouldBe ledgerServices.ledgerId
+      receivedCommands.applicationId shouldBe commands.getApplicationId
+      receivedCommands.workflowId shouldBe commands.getWorkflowId
+      receivedCommands.commandId shouldBe commands.getCommandId
+      receivedCommands.minLedgerTimeAbs.map(
+        _.seconds
+      ) shouldBe commands.getMinLedgerTimeAbsolute.asScala
+        .map(_.getEpochSecond)
+      receivedCommands.minLedgerTimeAbs.map(
+        _.nanos
+      ) shouldBe commands.getMinLedgerTimeAbsolute.asScala
+        .map(_.getNano)
+      receivedCommands.minLedgerTimeRel.map(
+        _.seconds
+      ) shouldBe commands.getMinLedgerTimeRelative.asScala
+        .map(_.getSeconds)
+      receivedCommands.minLedgerTimeRel.map(
+        _.nanos
+      ) shouldBe commands.getMinLedgerTimeRelative.asScala
+        .map(_.getNano)
+      receivedCommands.party shouldBe commands.getParty
+      receivedCommands.commands.size shouldBe commands.getCommands.size()
     }
   }
 
   def toAuthenticatedServer(fn: CommandSubmissionClient => Any): Any =
     ledgerServices.withCommandSubmissionClient(
-      Future.successful(Empty.defaultInstance),
+      alwaysSucceed,
       mockedAuthService,
     ) { (client, _) =>
       fn(client)
@@ -174,6 +207,27 @@ class CommandSubmissionClientImplTest
     toAuthenticatedServer { client =>
       submitDummyCommand(client, Option(somePartyReadWriteToken))
     }
+  }
+
+}
+
+object CommandSubmissionClientImplTest {
+
+  private val stuck = Future.never
+
+  private val success = Future.successful(Empty.defaultInstance)
+
+  private val alwaysSucceed: () => Future[Empty] = () => success
+
+  private def sequence(first: Future[Empty], following: Future[Empty]*): () => Future[Empty] = {
+    val it = Iterator.single(first) ++ Iterator(following: _*)
+    () =>
+      try {
+        it.next()
+      } catch {
+        case e: NoSuchElementException =>
+          throw new RuntimeException("CommandSubmissionClientImplTest.sequence exhausted", e)
+      }
   }
 
 }
