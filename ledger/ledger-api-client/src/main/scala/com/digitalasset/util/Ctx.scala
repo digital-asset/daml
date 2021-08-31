b5edb5a82c1d47e4d4631d3c4907c1b121d626fc
Pawel Batko <pawel.batko@digitalasset.com>
2021-08-31 10:57:04 +0200

    1

--- ledger/ledger-api-client/src/main/scala/com/digitalasset/util/Ctx.scala
+++ ledger/ledger-api-client/src/main/scala/com/digitalasset/util/Ctx.scala
@@ -6,19 +6,19 @@ package com.daml.util
 import com.daml.telemetry.{NoOpTelemetryContext, TelemetryContext}
 
 /** Ctx wraps a value with some contextual information.
- */
+  */
 final case class Ctx[+Context, +Value](
-                                        context: Context,
-                                        value: Value,
-                                        telemetryContext: TelemetryContext = NoOpTelemetryContext,
-                                      ) {
+    context: Context,
+    value: Value,
+    telemetryContext: TelemetryContext = NoOpTelemetryContext,
+) {
 
   def map[T](transform: Value => T): Ctx[Context, T] =
     Ctx(context, transform(value), telemetryContext)
 
   def enrich[NewContext](
-                          enrichingFunction: (Context, Value) => NewContext
-                        ): Ctx[NewContext, Value] =
+      enrichingFunction: (Context, Value) => NewContext
+  ): Ctx[NewContext, Value] =
     Ctx(enrichingFunction(context, value), value, telemetryContext)
 
 }
