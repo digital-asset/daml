cc948e9590b9ea42d2b8109a269c8d6504c27735
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2021-09-06 14:30:57 +0200

    Attempt at making scala 2.12 test-compile

--- ledger/participant-integration-api/src/test/suite/scala/platform/store/backend/h2/H2StorageBackendSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/platform/store/backend/h2/H2StorageBackendSpec.scala
@@ -14,37 +14,37 @@ class H2StorageBackendSpec extends AnyWordSpec with Matchers {
       "strip user from url with user" in {
         H2StorageBackend.extractUserPasswordAndRemoveFromUrl(
           "url;user=harry"
-        ) shouldBe ("url", Some("harry"), None)
+        ) shouldBe (("url", Some("harry"), None))
       }
 
       "strip user from url with user in the middle" in {
         H2StorageBackend.extractUserPasswordAndRemoveFromUrl(
           "url;user=harry;password=weak"
-        ) shouldBe ("url", Some("harry"), Some("weak"))
+        ) shouldBe (("url", Some("harry"), Some("weak")))
       }
 
       "only strip password if user absent" in {
         H2StorageBackend.extractUserPasswordAndRemoveFromUrl(
           "url;password=weak"
-        ) shouldBe ("url", None, Some("weak"))
+        ) shouldBe (("url", None, Some("weak")))
       }
 
       "not touch other properties" in {
         H2StorageBackend.extractUserPasswordAndRemoveFromUrl(
           "url;alpha=1;beta=2;gamma=3"
-        ) shouldBe ("url;alpha=1;beta=2;gamma=3", None, None)
+        ) shouldBe (("url;alpha=1;beta=2;gamma=3", None, None))
       }
 
       "match upper-case user and password keys" in {
         H2StorageBackend.extractUserPasswordAndRemoveFromUrl(
           "url;USER=sally;PASSWORD=supersafe"
-        ) shouldBe ("url", Some("sally"), Some("supersafe"))
+        ) shouldBe (("url", Some("sally"), Some("supersafe")))
       }
 
       "match mixed-case user and password keys" in {
         H2StorageBackend.extractUserPasswordAndRemoveFromUrl(
           "url;User=sally;Password=supersafe"
-        ) shouldBe ("url", Some("sally"), Some("supersafe"))
+        ) shouldBe (("url", Some("sally"), Some("supersafe")))
       }
     }
   }
