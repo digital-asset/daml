b4328b3dc3cf03309289ebac9bdc4f8cd2900661
nicu-da <nicu.reut@digitalasset.com>
2021-09-15 05:15:13 -0700

    ledger-api-test-tool - Add conformance test for parallel command deduplication using CommandSubmissionService [KVL-1099] (#10869)
    * Extract deduplication "features" into a configuration to be used around the tests.
    Better naming for assertions that support sync and async deduplication
    
    CHANGELOG_BEGIN
    
    CHANGELOG_END
    
    * Fix broken test and use consistency for tests
    
    * ledger-api-test-tool - Add conformance test for parallel command deduplication
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Add import for 2.12 compat
    
    * Add silencer plugin
    
    * Split parallel command deduplication scenario into it's own test suite
    
    * Add the parallel command deduplication test to append only ledgers
    
    * Run parallel command deduplication tests for append only ledgers
    
    * Apply suggestions from code review
    
    Co-authored-by: fabiotudone-da <fabio.tudone@digitalasset.com>
    
    * Code review renames
    
    * Add compat import
    
    * Run the test concurrently

---
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/AppendOnlyCommandDeduplicationParallelIT.scala
@@ -0,0 +1,91 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.api.testtool.suites
+
+import java.util.UUID
+
+import com.daml.grpc.GrpcException
+import com.daml.ledger.api.testtool.infrastructure.Allocation.{
+  Participant,
+  Participants,
+  SingleParty,
+  allocate,
+}
+import com.daml.ledger.api.testtool.infrastructure.Assertions.fail
+import com.daml.ledger.api.testtool.infrastructure.LedgerTestSuite
+import com.daml.ledger.api.testtool.infrastructure.ProtobufConverters._
+import com.daml.ledger.api.testtool.infrastructure.participant.ParticipantTestContext
+import com.daml.ledger.api.v1.command_submission_service.SubmitRequest
+import com.daml.ledger.api.v1.commands.Commands.DeduplicationPeriod
+import com.daml.ledger.client.binding.Primitive.Party
+import com.daml.ledger.test.model.Test.DummyWithAnnotation
+import io.grpc.Status
+import io.grpc.Status.Code
+
+import scala.collection.compat._
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
+/** Should be enabled for ledgers that fill the submission ID in the completions,
+  * as we need to use the submission id to find completions for parallel submissions
+  */
+class AppendOnlyCommandDeduplicationParallelIT extends LedgerTestSuite {
+
+  test(
+    s"DeduplicateParallelSubmissions",
+    "Commands submitted at the same, in parallel, should be deduplicated",
+    allocate(SingleParty),
+  )(implicit ec => { case Participants(Participant(ledger, party)) =>
+    val deduplicationDuration = 3.seconds
+    val numberOfParallelRequests = 10
+    lazy val request = ledger
+      .submitRequest(party, DummyWithAnnotation(party, "Duplicate").create.command)
+      .update(
+        _.commands.deduplicationPeriod := DeduplicationPeriod.DeduplicationDuration(
+          deduplicationDuration.asProtobuf
+        )
+      )
+    Future
+      .traverse(Seq.fill(numberOfParallelRequests)(request))(request => {
+        submitRequestAndGetStatusCode(ledger)(request, party)
+      })
+      .map(_.groupBy(identity).view.mapValues(_.size).toMap)
+      .map(responses => {
+        val expectedDuplicateResponses = numberOfParallelRequests - 1
+        val okResponses = responses.getOrElse(Code.OK, 0)
+        val alreadyExistsResponses = responses.getOrElse(Code.ALREADY_EXISTS, 0)
+        // Participant-based command de-duplication can currently also reject duplicates via a SQL exception
+        val internalResponses = responses.getOrElse(Code.INTERNAL, 0)
+        // Canton can return ABORTED for duplicate submissions
+        val abortedResponses = responses.getOrElse(Code.ABORTED, 0)
+        val duplicateResponses =
+          alreadyExistsResponses + internalResponses + abortedResponses
+        assert(
+          okResponses == 1 && duplicateResponses == numberOfParallelRequests - 1,
+          s"Expected $expectedDuplicateResponses duplicate responses and one accepted, got $responses",
+        )
+      })
+  })
+
+  protected def submitRequestAndGetStatusCode(
+      ledger: ParticipantTestContext
+  )(request: SubmitRequest, parties: Party*)(implicit ec: ExecutionContext): Future[Code] = {
+    val submissionId = UUID.randomUUID().toString
+    val requestWithSubmissionId = request.update(_.commands.submissionId := submissionId)
+    ledger
+      .submit(requestWithSubmissionId)
+      .flatMap(_ => ledger.findCompletion(parties: _*)(_.submissionId == submissionId))
+      .map {
+        case Some(completion) =>
+          completion.getStatus.code
+        case None => fail(s"Did not find completion for request with submission id $submissionId")
+      }
+      .recover {
+        case GrpcException(status, _) =>
+          status.getCode.value()
+        case otherException => fail("Not a GRPC exception", otherException)
+      }
+      .map(codeValue => Status.fromCodeValue(codeValue).getCode)
+  }
+}
