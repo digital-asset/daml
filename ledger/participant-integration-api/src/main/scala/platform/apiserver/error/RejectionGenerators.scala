bf7fac461350cefedb2fc959af7c8413bfe7f322
Tudor Voicu <tudor.voicu@digitalasset.com>
2021-09-15 15:51:41 +0200

    Adapted RejectionGenerator * Added //ledger/error:error to artifacts

--- ledger/participant-integration-api/src/main/scala/platform/apiserver/error/RejectionGenerators.scala
+++ ledger/participant-integration-api/src/main/scala/platform/apiserver/error/RejectionGenerators.scala
@@ -1,10 +1,15 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.platform.apiserver.error
 
-import com.daml.ledger.api.domain
+import com.daml.error.BaseError
 import com.daml.ledger.participant.state
 import com.daml.lf.engine.Error.{Interpretation, Package, Preprocessing, Validation}
 import com.daml.lf.engine.{Error => LfError}
 import com.daml.lf.interpretation.{Error => LfInterpretationError}
+import com.daml.logging.{ContextualizedLogger, LoggingContext}
+import com.daml.platform.store.ErrorCause
 import io.grpc.Status.Code
 import io.grpc.StatusRuntimeException
 
@@ -12,32 +17,42 @@ import scala.util.{Failure, Success, Try}
 
 object RejectionGenerators {
 
-  private[sync] def toGrpc(reject: BaseError): StatusRuntimeException = {
-    reject.asGrpcErrorFromContext(this.loggingContext)
-  }
+  def toGrpc(reject: BaseError, correlationId: Option[String] = None)(implicit
+      logger: ContextualizedLogger,
+      loggingContext: LoggingContext,
+  ): StatusRuntimeException =
+    reject.asGrpcErrorFromContext(correlationId, logger)(loggingContext)
 
-  override def duplicateCommand(commands: domain.Commands): StatusRuntimeException = {
-    toGrpc(LedgerApiErrors.CommandPreparation.DuplicateCommand.Reject()(this.loggingContext))
-  }
+  def duplicateCommand(implicit
+      logger: ContextualizedLogger,
+      loggingContext: LoggingContext,
+  ): StatusRuntimeException =
+    toGrpc(LedgerApiErrors.CommandPreparation.DuplicateCommand.Reject())
 
-  override def commandExecutorError(commands: domain.Commands,
-                                    cause: ErrorCauseExport): Option[StatusRuntimeException] = {
+  def commandExecutorError(cause: ErrorCauseExport)(implicit
+      logger: ContextualizedLogger,
+      loggingContext: LoggingContext,
+  ): Option[StatusRuntimeException] = {
 
     def processPackageError(err: LfError.Package.Error): BaseError = err match {
       case e: Package.Internal => LedgerApiErrors.InternalError.PackageInternal(e)
       case Package.Validation(validationError) =>
-        LedgerApiErrors.Package.PackageValidationFailed.Reject(validationError)
+        LedgerApiErrors.Package.PackageValidationFailed.Reject(validationError.pretty)
       case Package.MissingPackage(packageId, context) =>
         LedgerApiErrors.Package.MissingPackage.Reject(packageId, context)
       case Package.AllowedLanguageVersion(packageId, languageVersion, allowedLanguageVersions) =>
-        LedgerApiErrors.Package.AllowedLanguageVersions.Error(packageId, languageVersion, allowedLanguageVersions)
+        LedgerApiErrors.Package.AllowedLanguageVersions.Error(
+          packageId,
+          languageVersion,
+          allowedLanguageVersions,
+        )
       case e: Package.SelfConsistency =>
         LedgerApiErrors.InternalError.PackageSelfConsistency(e)
     }
 
     def processPreprocessingError(err: LfError.Preprocessing.Error): BaseError = err match {
       case e: Preprocessing.Internal => LedgerApiErrors.InternalError.Preprocessing(e)
-      case e                         => LedgerApiErrors.PreprocessingErrors.PreprocessingFailed.Reject(e)
+      case e => LedgerApiErrors.PreprocessingErrors.PreprocessingFailed.Reject(e)
     }
 
     def processValidationError(err: LfError.Validation.Error): BaseError = err match {
@@ -45,46 +60,54 @@ object RejectionGenerators {
       case e: Validation.ReplayMismatch => LedgerApiErrors.InternalError.Validation(e)
     }
 
-    def processDamlException(err: com.daml.lf.interpretation.Error, renderedMessage: String): BaseError = {
+    def processDamlException(
+        err: com.daml.lf.interpretation.Error,
+        renderedMessage: String,
+    ): BaseError = {
       // detailMessage is only suitable for server side debugging but not for the user, so don't pass except on internal errors
 
       err match {
         case LfInterpretationError.ContractNotFound(cid) =>
-          LedgerApiErrors.InterpreterErrors.LookupErrors.ContractNotFound.Reject(renderedMessage, cid)
+          LedgerApiErrors.InterpreterErrors.LookupErrors.ContractNotFound
+            .Reject(renderedMessage, cid)
         case LfInterpretationError.ContractKeyNotFound(key) =>
-          LedgerApiErrors.InterpreterErrors.LookupErrors.ContractKeyNotFound.Reject(renderedMessage, key)
-        case e: LfInterpretationError.FailedAuthorization =>
+          LedgerApiErrors.InterpreterErrors.LookupErrors.ContractKeyNotFound
+            .Reject(renderedMessage, key)
+        case _: LfInterpretationError.FailedAuthorization =>
           LedgerApiErrors.InterpreterErrors.AuthorizationError.Reject(renderedMessage)
         case e: LfInterpretationError.ContractNotActive =>
           LedgerApiErrors.InterpreterErrors.ContractNotActive.Reject(renderedMessage, e)
-        case e: LfInterpretationError.LocalContractKeyNotVisible =>
+        case _: LfInterpretationError.LocalContractKeyNotVisible =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
         case LfInterpretationError.DuplicateContractKey(key) =>
           LedgerApiErrors.InterpreterErrors.DuplicateContractKey.Reject(renderedMessage, key)
-        case e: LfInterpretationError.UnhandledException =>
+        case _: LfInterpretationError.UnhandledException =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
-        case e: LfInterpretationError.UserError =>
+        case _: LfInterpretationError.UserError =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
-        case e: LfInterpretationError.TemplatePreconditionViolated =>
+        case _: LfInterpretationError.TemplatePreconditionViolated =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
-        case e: LfInterpretationError.CreateEmptyContractKeyMaintainers =>
+        case _: LfInterpretationError.CreateEmptyContractKeyMaintainers =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
-        case e: LfInterpretationError.FetchEmptyContractKeyMaintainers =>
+        case _: LfInterpretationError.FetchEmptyContractKeyMaintainers =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
-        case e: LfInterpretationError.WronglyTypedContract =>
+        case _: LfInterpretationError.WronglyTypedContract =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
         case LfInterpretationError.NonComparableValues =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
-        case e: LfInterpretationError.ContractIdInContractKey =>
+        case _: LfInterpretationError.ContractIdInContractKey =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
         case LfInterpretationError.ValueExceedsMaxNesting =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
-        case e: LfInterpretationError.ContractIdComparability =>
+        case _: LfInterpretationError.ContractIdComparability =>
           LedgerApiErrors.InterpreterErrors.GenericInterpretationError.Error(renderedMessage)
       }
     }
 
-    def processInterpretationError(err: LfError.Interpretation.Error, detailMessage: Option[String]): BaseError =
+    def processInterpretationError(
+        err: LfError.Interpretation.Error,
+        detailMessage: Option[String],
+    ): BaseError =
       err match {
         case Interpretation.Internal(location, message) =>
           LedgerApiErrors.InternalError.Interpretation(location, message, detailMessage)
@@ -93,13 +116,15 @@ object RejectionGenerators {
 
     def processLfError(error: LfError) = {
       val transformed = error match {
-        case LfError.Package(packageError)          => processPackageError(packageError)
+        case LfError.Package(packageError) => processPackageError(packageError)
         case LfError.Preprocessing(processingError) => processPreprocessingError(processingError)
         case LfError.Interpretation(interpretationError, detailMessage) =>
           processInterpretationError(interpretationError, detailMessage)
         case LfError.Validation(validationError) => processValidationError(validationError)
         case e
-          if e.message.contains("requires authorizers") => // Keeping this around as a string match as daml is not yet generating LfError.InterpreterErrors.Validation
+            if e.message.contains(
+              "requires authorizers"
+            ) => // Keeping this around as a string match as daml is not yet generating LfError.InterpreterErrors.Validation
           LedgerApiErrors.InterpreterErrors.AuthorizationError.Reject(e.message)
       }
       toGrpc(transformed)
@@ -113,18 +138,20 @@ object RejectionGenerators {
     Some(rej)
   }
 
-  override def submissionResult(commands: domain.Commands,
-                                result: Try[state.v2.SubmissionResult]): Option[Try[Unit]] = {
+  def submissionResult(result: Try[state.v2.SubmissionResult]): Option[Try[Unit]] = {
     result match {
-      case Success(state.v2.SubmissionResult.Acknowledged)                    => None
-      case Success(grpcError @ state.v2.SubmissionResult.SynchronousError(_)) => Some(Failure(grpcError.exception))
-      case Failure(_)                                                         => None
+      case Success(state.v2.SubmissionResult.Acknowledged) => None
+      case Success(grpcError @ state.v2.SubmissionResult.SynchronousError(_)) =>
+        Some(Failure(grpcError.exception))
+      case Failure(_) => None
     }
   }
 
-  override def validationFailure(reject: StatusRuntimeException): StatusRuntimeException = {
-    implicit val errorContext: ErrorLoggingContext = this.loggingContext(TraceContext.empty)
-    val description                                = reject.getStatus.getDescription
+  def validationFailure(reject: StatusRuntimeException)(implicit
+      logger: ContextualizedLogger,
+      loggingContext: LoggingContext,
+  ): StatusRuntimeException = {
+    val description = reject.getStatus.getDescription
     reject.getStatus.getCode match {
       case Code.INVALID_ARGUMENT =>
         if (description.startsWith("Missing field:")) {
@@ -134,14 +161,25 @@ object RejectionGenerators {
         } else if (description.startsWith("Invalid field:")) {
           toGrpc(LedgerApiErrors.CommandValidation.InvalidField.Reject(description))
         } else {
-          noTracingLogger.warn(s"Unknown invalid argument rejection: ${reject.getStatus}")
+          logger.warn(s"Unknown invalid argument rejection: ${reject.getStatus}")
           reject
         }
       case Code.NOT_FOUND if description.startsWith("Ledger ID") =>
         toGrpc(LedgerApiErrors.CommandValidation.LedgerIdMismatch.Reject(description))
-      case x =>
-        noTracingLogger.warn(s"Unknown rejection: ${reject.getStatus}")
+      case _ =>
+        logger.warn(s"Unknown rejection: ${reject.getStatus}")
         reject
+    }
+  }
+
+  sealed trait ErrorCauseExport
+  object ErrorCauseExport {
+    final case class DamlLf(error: LfError) extends ErrorCauseExport
+    final case class LedgerTime(retries: Int, explain: String) extends ErrorCauseExport
+
+    private[platform] def fromErrorCause(err: ErrorCause): ErrorCauseExport = err match {
+      case ErrorCause.DamlLf(error) => DamlLf(error)
+      case x: ErrorCause.LedgerTime => LedgerTime(x.retries, x.explain)
     }
   }
 }
