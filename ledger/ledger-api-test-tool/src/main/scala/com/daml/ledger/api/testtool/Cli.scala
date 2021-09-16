51174497e0c3c3229c111abab29b92ba5a52ee69
Pawel Batko <pawel.batko@digitalasset.com>
2021-09-16 15:06:26 +0200

    Revert changes in Cli.scala

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/Cli.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/Cli.scala
@@ -3,22 +3,24 @@
 
 package com.daml.ledger.api.testtool
 
+import java.io.File
+import java.nio.file.{Path, Paths}
+
 import com.daml.buildinfo.BuildInfo
 import com.daml.ledger.api.testtool.infrastructure.PartyAllocationConfiguration
 import com.daml.ledger.api.testtool.tests.Tests
+import com.daml.ledger.api.tls.TlsConfiguration
 import scopt.{OptionParser, Read}
 
-import java.io.File
-import java.nio.file.{Path, Paths}
 import scala.collection.compat.immutable.LazyList
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.util.Try
 
 object Cli {
 
-  private val Name = "ledger-api-test-tool"
-
-  private def reportUsageOfDeprecatedOption[B](option: String) = { (_: Any, config: B) =>
+  private def reportUsageOfDeprecatedOption[B](
+                                                option: String
+                                              ) = { (_: Any, config: B) =>
     System.err.println(
       s"WARNING: $option has been deprecated and will be removed in a future version"
     )
@@ -41,24 +43,43 @@ object Cli {
       case n: Int => (s.slice(0, n), s.slice(n + 1, s.length))
     }
 
+  private val Name = "ledger-api-test-tool"
+
+  private val pemConfig = (path: String, config: Config) =>
+    config.copy(
+      tlsConfig = config.tlsConfig.fold(
+        Some(TlsConfiguration(enabled = true, None, Some(new File(path)), None))
+      )(c => Some(c.copy(keyFile = Some(new File(path)))))
+    )
+
+  private val crtConfig = (path: String, config: Config) =>
+    config.copy(
+      tlsConfig = config.tlsConfig.fold(
+        Some(TlsConfiguration(enabled = true, Some(new File(path)), None, None))
+      )(c => Some(c.copy(keyCertChainFile = Some(new File(path)))))
+    )
+
+  private val cacrtConfig = (path: String, config: Config) =>
+    config.copy(
+      tlsConfig = config.tlsConfig.fold(
+        Some(TlsConfiguration(enabled = true, None, None, Some(new File(path))))
+      )(c => Some(c.copy(trustCertCollectionFile = Some(new File(path)))))
+    )
+
   private[this] implicit val pathRead: Read[Path] = Read.reads(Paths.get(_))
 
   private val argParser: OptionParser[Config] = new scopt.OptionParser[Config](Name) {
     private def invalidPerformanceTestName[A](name: String): Either[String, Unit] =
       failure(s"$name is not a valid performance test name. Use `--list` to see valid names.")
 
-    head(
-      """The Ledger API Test Tool is a command line tool for testing the correctness of
-            |ledger implementations based on Daml and Ledger API.""".stripMargin
-    )
+    head("""The Ledger API Test Tool is a command line tool for testing the correctness of
+           |ledger implementations based on Daml and Ledger API.""".stripMargin)
 
     arg[(String, Int)]("[endpoints...]")(endpointRead)
-      .action((address, config) =>
-        config.copy(participantsEndpoints = config.participantsEndpoints :+ address)
-      )
+      .action((address, config) => config.copy(participants = config.participants :+ address))
       .unbounded()
       .optional()
-      .text("Addresses of the participants to test, specified as `<host>:<port>`.")
+      .text("""Addresses of the participants to test, specified as `<host>:<port>`.""")
 
     opt[Int]("max-connection-attempts")
       .action((maxConnectionAttempts, config) =>
@@ -77,36 +98,27 @@ object Cli {
     opt[String]("pem")
       .optional()
       .text("TLS: The pem file to be used as the private key. Applied to all endpoints.")
-      .action { (path: String, config: Config) =>
-        config.withTlsConfig(_.copy(keyFile = Some(new File(path))))
-      }
+      .action(pemConfig)
 
     opt[String]("crt")
       .optional()
       .text(
-        """TLS: The crt file to be used as the cert chain.
-              |Required if any other TLS parameters are set. Applied to all endpoints.""".stripMargin
+        "TLS: The crt file to be used as the cert chain. Required if any other TLS parameters are set. Applied to all endpoints."
       )
-      .action { (path: String, config: Config) =>
-        config.withTlsConfig(_.copy(keyCertChainFile = Some(new File(path))))
-      }
+      .action(crtConfig)
 
     opt[String]("cacrt")
       .optional()
       .text("TLS: The crt file to be used as the trusted root CA. Applied to all endpoints.")
-      .action { (path: String, config: Config) =>
-        config.withTlsConfig(_.copy(trustCertCollectionFile = Some(new File(path))))
-      }
+      .action(cacrtConfig)
 
     opt[Double](name = "timeout-scale-factor")
       .optional()
       .action((v, c) => c.copy(timeoutScaleFactor = v))
-      .text(
-        """Scale factor for timeouts used in all test suites. Useful to tune timeouts
+      .text("""Scale factor for timeouts used in all test suites. Useful to tune timeouts
               |depending on the environment and the Ledger implementation under test.
               |Defaults to 1.0. Use numbers higher than 1.0 to make test timeouts more lax,
-              |use numbers lower than 1.0 to make test timeouts more strict.""".stripMargin
-      )
+              |use numbers lower than 1.0 to make test timeouts more strict.""".stripMargin)
 
     opt[String](name = "load-scale-factor")
       .optional()
@@ -126,12 +138,10 @@ object Cli {
 
     opt[Unit]("must-fail")
       .action((_, c) => c.copy(mustFail = true))
-      .text(
-        """Reverse success status logic of the tool. Use this flag if you expect one or
+      .text("""Reverse success status logic of the tool. Use this flag if you expect one or
               |more or the scenario tests to fail. If enabled, the tool will succeed when at
               |least one test fails, and it will fail when all tests succeed. Defaults to
-              |false.""".stripMargin
-      )
+              |false.""".stripMargin)
 
     opt[Unit]('x', "extract")
       .action((_, c) => c.copy(extract = true))
@@ -144,38 +154,21 @@ object Cli {
       .action((ex, c) => c.copy(excluded = c.excluded ++ ex))
       .unbounded()
       .text(
-        """A comma-separated list of exclusion prefixes. Tests whose name start with
-              |any of the given prefixes will be skipped. Can be specified multiple times,
-              |i.e. `--exclude=a,b` is the same as `--exclude=a --exclude=b`.""".stripMargin
+        """A comma-separated list of exclusion prefixes. Tests whose name start with any of the given prefixes will be skipped. Can be specified multiple times, i.e. `--exclude=a,b` is the same as `--exclude=a --exclude=b`."""
       )
 
     opt[Seq[String]]("include")
       .action((inc, c) => c.copy(included = c.included ++ inc))
       .unbounded()
       .text(
-        """A comma-separated list of inclusion prefixes. If not specified,
-              |all default tests are included. If specified, only tests that match at least one
-              |of the given inclusion prefixes (and none of the given exclusion prefixes) will be run.
-              |Can be specified multiple times, i.e. `--include=a,b` is the same as `--include=a --include=b`.
-              |Mutually exclusive with `--additional`.""".stripMargin
-      )
-
-    opt[Seq[String]]("additional")
-      .action((additional, c) => c.copy(additional = c.additional ++ additional))
-      .hidden()
-      .unbounded()
-      .text(
-        """A comma-separated list of additional prefixes. If specified, also tests that match at least one
-              |of the given inclusion prefixes (and none of the given exclusion prefixes) will be run.
-              |Can be specified multiple times, i.e. `--additional=a,b` is the same as `--additional=a --additional=b`.
-              |Mutually exclusive with `--include`.""".stripMargin
+        """A comma-separated list of inclusion prefixes. If not specified, all default tests are included. If specified, only tests that match at least one of the given inclusion prefixes (and none of the given exclusion prefixes) will be run. Can be specified multiple times, i.e. `--include=a,b` is the same as `--include=a --include=b`."""
       )
 
     opt[Seq[String]]("perf-tests")
       .validate(_.find(!Tests.PerformanceTestsKeys(_)).fold(success)(invalidPerformanceTestName))
       .action((inc, c) => c.copy(performanceTests = c.performanceTests ++ inc))
       .unbounded()
-      .text("A comma-separated list of performance tests that should be run.")
+      .text("""A comma-separated list of performance tests that should be run.""")
 
     opt[Path]("perf-tests-report")
       .action((inc, c) => c.copy(performanceTestsReport = Some(inc)))
@@ -191,35 +184,29 @@ object Cli {
 
     opt[Unit]("shuffle-participants")
       .action((_, c) => c.copy(shuffleParticipants = true))
-      .text(
-        """Shuffle the list of participants used in a test.
-              |By default participants are used in the order they're given.""".stripMargin
-      )
+      .text("""Shuffle the list of participants used in a test.
+              |By default participants are used in the order they're given.""".stripMargin)
 
     opt[Unit]("no-wait-for-parties")
       .action((_, c) => c.copy(partyAllocation = PartyAllocationConfiguration.ClosedWorld))
-      .text("Do not wait for parties to be allocated on all participants.")
+      .text("""Do not wait for parties to be allocated on all participants.""")
       .hidden()
 
     opt[Unit]("open-world")
       .action((_, c) => c.copy(partyAllocation = PartyAllocationConfiguration.OpenWorld))
-      .text(
-        """Do not allocate parties explicitly.
-              |Instead, expect the ledger to allocate parties dynamically.
-              |Party names must be their hints.""".stripMargin
-      )
+      .text("""|Do not allocate parties explicitly.
+               |Instead, expect the ledger to allocate parties dynamically.
+               |Party names must be their hints.""".stripMargin)
 
     opt[Unit]("list")
       .action((_, c) => c.copy(listTestSuites = true))
       .text(
-        """Lists all available test suites that can be used in the include and exclude options.
-              |Test names always start with their suite name, so using the suite name as a prefix
-              |matches all tests in a given suite.""".stripMargin
+        """Lists all available test suites that can be used in the include and exclude options. Test names always start with their suite name, so using the suite name as a prefix matches all tests in a given suite."""
       )
 
     opt[Unit]("list-all")
       .action((_, c) => c.copy(listTests = true))
-      .text("Lists all available tests that can be used in the include and exclude options.")
+      .text("""Lists all available tests that can be used in the include and exclude options.""")
 
     opt[Unit]("version")
       .optional()
@@ -235,8 +222,7 @@ object Cli {
       .optional()
       .action((x, c) => c.copy(ledgerClockGranularity = x))
       .text(
-        """Specify the largest interval that you will see between clock ticks
-              |on the ledger under test. The default is \"1s\" (1 second).""".stripMargin
+        "Specify the largest interval that you will see between clock ticks on the ledger under test. The default is \"1s\" (1 second)."
       )
 
     opt[Unit]("skip-dar-upload")
@@ -244,14 +230,8 @@ object Cli {
       .action((_, c) => c.copy(uploadDars = false))
       .text("Skip DARs upload into ledger before running tests")
 
-    checkConfig(c =>
-      if (c.included.nonEmpty && c.additional.nonEmpty)
-        failure("`--include` and `--additional` are mutually exclusive")
-      else
-        success
-    )
-
     help("help").text("Prints this usage text")
+
   }
 
   def parse(args: Array[String]): Option[Config] =
