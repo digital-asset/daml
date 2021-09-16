7ced1ca8ee3741a23061711a4fd6d6a7e30a81e3
Pawel Batko <pawel.batko@digitalasset.com>
2021-09-16 15:01:25 +0200

    Streamline TLS1.3 IT

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/TLSOnePointThreeIT.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/TLSOnePointThreeIT.scala
@@ -4,90 +4,88 @@
 package com.daml.ledger.api.testtool.suites
 
 import com.daml.ledger.api.testtool.infrastructure.Allocation.{NoParties, allocate}
-import com.daml.ledger.api.testtool.infrastructure.{Endpoint, LedgerTestSuite}
 import com.daml.ledger.api.testtool.infrastructure.participant.ParticipantTestContext
+import com.daml.ledger.api.testtool.infrastructure.{Endpoint, LedgerTestSuite}
+import com.daml.ledger.api.tls.TlsVersion
 import com.daml.ledger.api.tls.TlsVersion.TlsVersion
-import com.daml.ledger.api.tls.{TlsConfiguration, TlsVersion}
 import com.daml.ledger.api.v1.ledger_identity_service.LedgerIdentityServiceGrpc.LedgerIdentityServiceBlockingStub
 import com.daml.ledger.api.v1.ledger_identity_service.{
   GetLedgerIdentityRequest,
   LedgerIdentityServiceGrpc,
 }
 import com.daml.ledger.resources.{ResourceContext, ResourceOwner}
-import io.grpc.netty.{NegotiationType, NettyChannelBuilder}
-import io.grpc.{Channel, StatusRuntimeException}
+import io.grpc.StatusRuntimeException
+import io.grpc.netty.NettyChannelBuilder
 
-import java.util.concurrent._
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
 
+/** Verifies that the given participant server correctly handles TLSv1.3 only mode.
+  *
+  * It works by creating and exercising a series of client service stubs, each over different TLS version.
+  * Only TLSv1.3 connection is expected to succeed.
+  * Connections over lower TLS versions are expected to fail.
+  */
 final class TLSOnePointThreeIT extends LedgerTestSuite {
 
-  testTlsConnection(clientTls = TlsVersion.V1_3, assertConnectionSuccessful = true)
-  testTlsConnection(clientTls = TlsVersion.V1_2, assertConnectionSuccessful = false)
-  testTlsConnection(clientTls = TlsVersion.V1_1, assertConnectionSuccessful = false)
-  testTlsConnection(clientTls = TlsVersion.V1, assertConnectionSuccessful = false)
+  testTlsConnection(clientTlsVersion = TlsVersion.V1_3, assertConnectionOk = true)
+  testTlsConnection(clientTlsVersion = TlsVersion.V1_2, assertConnectionOk = false)
+  testTlsConnection(clientTlsVersion = TlsVersion.V1_1, assertConnectionOk = false)
+  testTlsConnection(clientTlsVersion = TlsVersion.V1, assertConnectionOk = false)
 
-  def testTlsConnection(clientTls: TlsVersion, assertConnectionSuccessful: Boolean): Unit = {
-    val (what, assertion) =
-      if (assertConnectionSuccessful)
+  def testTlsConnection(clientTlsVersion: TlsVersion, assertConnectionOk: Boolean): Unit = {
+    val (what, assertionOnServerResponse) =
+      if (assertConnectionOk)
         ("accept", assertSuccessfulConnection)
       else
-        ("reject", assertFailedlConnection)
+        ("reject", assertFailedConnection)
 
     testGivenAllParticipants(
-      s"ConnectionOnTLSv13FromClientOn${clientTls.version.replace(".", "")}",
-      s"A ledger API server  should ${what} a ${clientTls} connection",
+      s"ConnectionOnTLSv13FromClientOn${clientTlsVersion.version.replace(".", "")}",
+      s"A ledger API server should ${what} a ${clientTlsVersion} connection",
       allocate(NoParties),
     ) { implicit ec => (testContexts: Seq[ParticipantTestContext]) =>
       { case _ =>
-        assume(testContexts.nonEmpty, "At least one context expected!")
+        // preconditions
+        assume(testContexts.nonEmpty, "Missing an expected participant test context!")
         val firstTextContext = testContexts.head
         assume(
           firstTextContext.clientTlsConfiguration.isDefined,
-          "This test case requires TLS configuration!",
+          "Missing required TLS configuration!",
         )
-        assume(
-          firstTextContext.ledgerEndpoint.isInstanceOf[Endpoint.Remote]
-        )
-        val Endpoint.Remote(ledgerHostname, ledgerPort) = firstTextContext.ledgerEndpoint
         val tlsConfiguration = firstTextContext.clientTlsConfiguration.get
-
-        val concurrency = Concurrency(
-          corePoolSize = 2,
-          maxPoolSize = 2,
-          keepAliveTime = 1000,
-          maxQueueLength = 1000,
+        assume(tlsConfiguration.enabled, "TLS configuration is disabled but expected be enabled!")
+        assume(
+          firstTextContext.ledgerEndpoint.isInstanceOf[Endpoint.Remote],
+          "Expected a remote (i.e. with a hostname and port) ledger endpoint!",
         )
+        val Endpoint.Remote(ledgerHostname, ledgerPort) =
+          firstTextContext.ledgerEndpoint.asInstanceOf[Endpoint.Remote]
 
-        // TODO PBATKO
-        val ledger = Ledger(
-          hostname = ledgerHostname,
-          port = ledgerPort,
-        )
-
-        val resources: ResourceOwner[LedgerIdentityServiceBlockingStub] = for {
-          executorService <- threadPoolExecutorOwner(concurrency)
-          channel <- channelOwner(
-            ledger,
-            tlsConfiguration,
-            executorService,
-            clientTlsVersion = clientTls,
+        // given
+        val sslContext = tlsConfiguration
+          .client(enabledProtocols = Seq(clientTlsVersion))
+          .getOrElse(throw new IllegalStateException("Missing SslContext!"))
+        val serviceStubOwner: ResourceOwner[LedgerIdentityServiceBlockingStub] = for {
+          channel <- ResourceOwner.forChannel(
+            builder = NettyChannelBuilder
+              .forAddress(ledgerHostname, ledgerPort)
+              .useTransportSecurity()
+              .sslContext(sslContext),
+            shutdownTimeout = 2.seconds,
           )
-          identityService = LedgerIdentityServiceGrpc.blockingStub(channel)
-        } yield identityService
+        } yield LedgerIdentityServiceGrpc.blockingStub(channel)
 
         // when
-        val actual: Future[String] = resources.use {
-          (identityService: LedgerIdentityServiceBlockingStub) =>
-            val response = identityService.getLedgerIdentity(new GetLedgerIdentityRequest())
-            Future.successful(response.ledgerId)
+        val response: Future[String] = serviceStubOwner.use { identityService =>
+          val response = identityService.getLedgerIdentity(new GetLedgerIdentityRequest())
+          Future.successful(response.ledgerId)
         }(ResourceContext(ec))
 
         // then
-        actual.transform[Unit] {
-          assertion
+        response.transform[Unit] {
+          assertionOnServerResponse
         }
       }
     }
@@ -98,20 +96,20 @@ final class TLSOnePointThreeIT extends LedgerTestSuite {
       Try[Unit] {
         assert(
           assertion = ledgerId ne null,
-          message = s"Expected not null ledger id!",
+          message = s"Expected a not null ledger id!",
         )
       }
     case Failure(exception) =>
-      throw new AssertionError(s"Failed to get a successful server response!", exception)
+      throw new AssertionError(s"Failed to receive a successful server response!", exception)
   }
 
-  private lazy val assertFailedlConnection: Try[String] => Try[Unit] = {
+  private lazy val assertFailedConnection: Try[String] => Try[Unit] = {
     case Success(ledgerId) =>
       Try[Unit] {
         assert(
           assertion = false,
           message =
-            s"Connection succeeded (and returned ledgerId: ${ledgerId} why it should have failed!",
+            s"Connection succeeded and returned ledgerId: ${ledgerId} but expected connection failure!",
         )
       }
     case Failure(_: StatusRuntimeException) => Success[Unit](())
@@ -119,67 +117,9 @@ final class TLSOnePointThreeIT extends LedgerTestSuite {
       Try[Unit] {
         assert(
           assertion = false,
-          message = s"Unexexpected failure: ${other}",
+          message = s"Unexpected failure: ${other}",
         )
       }
-  }
-
-  // TODO BATKO: below code copied from com.daml.ledger.api.benchtool.LedgerApiBenchTool. There's likely room to simplify
-
-  case class Concurrency(
-      corePoolSize: Int,
-      maxPoolSize: Int,
-      keepAliveTime: Long,
-      maxQueueLength: Int,
-  )
-
-  case class Ledger(
-      hostname: String,
-      port: Int,
-  )
-
-  def threadPoolExecutorOwner(
-      config: Concurrency
-  ): ResourceOwner[ThreadPoolExecutor] =
-    ResourceOwner.forExecutorService(() =>
-      // TODO PBATKO: What are good defaults? Do we even need a thread pool?
-      new ThreadPoolExecutor(
-        config.corePoolSize,
-        config.maxPoolSize,
-        config.keepAliveTime,
-        TimeUnit.SECONDS,
-        if (config.maxQueueLength == 0) new SynchronousQueue[Runnable]()
-        else new ArrayBlockingQueue[Runnable](config.maxQueueLength),
-      )
-    )
-
-  def channelOwner(
-      ledger: Ledger,
-      tls: TlsConfiguration,
-      executor: Executor,
-      clientTlsVersion: TlsVersion,
-  ): ResourceOwner[Channel] = {
-    val MessageChannelSizeBytes: Int = 32 * 1024 * 1024 // 32 MiB
-    val ShutdownTimeout: FiniteDuration = 5.seconds
-
-    val channelBuilder = NettyChannelBuilder
-      .forAddress(ledger.hostname, ledger.port)
-      .executor(executor)
-      .maxInboundMessageSize(MessageChannelSizeBytes)
-      .usePlaintext()
-
-    if (tls.enabled) {
-      tls
-        .client(enabledProtocols = Seq(clientTlsVersion))
-        .map { sslContext =>
-          channelBuilder
-            .useTransportSecurity()
-            .sslContext(sslContext)
-            .negotiationType(NegotiationType.TLS)
-        }
-    }
-
-    ResourceOwner.forChannel(channelBuilder, ShutdownTimeout)
   }
 
 }
