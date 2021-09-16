910bc5a0c3cf80ebeff4258a9f25530b5654b0bc
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2021-09-16 13:43:57 +0200

    Logging delay of submitted commands
    This would have helped track down the timezone issue more quickly.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/participant-integration-api/src/main/scala/platform/apiserver/services/ApiSubmissionService.scala
+++ ledger/participant-integration-api/src/main/scala/platform/apiserver/services/ApiSubmissionService.scala
@@ -258,6 +258,9 @@ private[apiserver] final class ApiSubmissionService private[services] (
             if (submissionDelay.isNegative)
               submitTransaction(transactionInfo)
             else {
+              logger.debug(
+                s"Delaying submission of ${transactionInfo.submitterInfo.commandId} by $submissionDelay"
+              )
               metrics.daml.commands.delayedSubmissions.mark()
               val scalaDelay = scala.concurrent.duration.Duration.fromNanos(submissionDelay.toNanos)
               Delayed.Future.by(scalaDelay)(submitTransaction(transactionInfo))
