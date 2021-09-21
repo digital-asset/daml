ed1ba019ed31787de804f172d9698681b87627d7
Fabio Tudone <fabio.tudone@digitalasset.com>
2021-09-21 15:17:01 +0200

    Simplify by using only the main work tree

--- ci/check-protobuf-against-stable.sh
+++ ci/check-protobuf-against-stable.sh
@@ -19,35 +19,19 @@ latest_stable() {
   git tag ${gitTagSearchSpace} | grep -v "snapshot" | sort -V | tail -1
 }
 
-cleanup_tmp_files() {
-  rm -rf "${TMP_STABLE_PROTOS_DIR}"
-}
-
-checkout_stable_protos() {
-  trap cleanup_tmp_files EXIT
-  TMP_STABLE_PROTOS_DIR="$(mktemp -d -t ci-stable-protos-XXXXXXXXXX)"
-  local archive;
-  archive="${TMP_STABLE_PROTOS_DIR}/archive.tar.gz"
-  git archive --output="${archive}" "${LATEST_STABLE}" daml-lf ledger-api ledger
-  tar xzf "${archive}" -C "${TMP_STABLE_PROTOS_DIR}"
-}
-
-readonly BUF_IMAGE="buf-stable-protos-image.bin"
-
-create_stable_protos_buf_image() {
-  cp "${PROJECT_ROOT}/buf.yaml" "${PROJECT_ROOT}/buf.lock" "${TMP_STABLE_PROTOS_DIR}"
-  cd "${TMP_STABLE_PROTOS_DIR}"
-  buf build -o "${BUF_IMAGE}"
-}
-
-check_against_stable_protos_buf_image() {
-  cd "${PROJECT_ROOT}"
-  buf breaking --against "${TMP_STABLE_PROTOS_DIR}/${BUF_IMAGE}"
-}
-
 LATEST_STABLE="$(latest_stable)"
 echo "Checking protos against '${LATEST_STABLE}' (i.e., the most recent stable tag up to '${CURRENT_BRANCH}')"
 
-checkout_stable_protos
-create_stable_protos_buf_image
+check_against_stable_protos_buf_image() {
+  local buf_image_dir
+  buf_image_dir="$(mktemp -d)"
+  local buf_image
+  buf_image="${buf_image_dir}/buf.bin"
+  trap 'rm -rf ${buf_image_dir}' EXIT
+  git checkout "${LATEST_STABLE}"
+  buf build -o "${buf_image}"
+  git checkout "${CURRENT_BRANCH}"
+  buf breaking --against "${buf_image}"
+}
+
 check_against_stable_protos_buf_image
