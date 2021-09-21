aa7fc3ba5d52de68d98de361c823b483a74a489f
Fabio Tudone <fabio.tudone@digitalasset.com>
2021-09-21 17:55:45 +0200

    Support release branches and PRs targeting them

--- ci/check-protobuf-against-stable.sh
+++ ci/check-protobuf-against-stable.sh
@@ -8,11 +8,19 @@ eval "$(dev-env/bin/dade assist)"
 readonly BUF_IMAGE_TMPDIR="$(mktemp -d)"
 trap 'rm -rf ${BUF_IMAGE_TMPDIR}' EXIT
 
-readonly LATEST_STABLE_TAG="$(git tag --no-merged | grep -v "snapshot" | sort -V | tail -1)"
-echo "Checking protobuf against tag '${LATEST_STABLE_TAG}' (i.e., the most recent stable tag)"
+readonly CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+if [[ "${CURRENT_BRANCH}" == "main" || "${SYSTEM_PULLREQUEST_TARGETBRANCH}" == "main" ]]; then
+  GIT_TAG_SCOPE="--no-merged" # Release tags are not reachable from `main` because release branches predate them
+else
+  GIT_TAG_SCOPE="--merged"    # The relevant most recent release tag is always reachable from its release branch
+fi
+
+readonly LATEST_STABLE_TAG="$(git tag "${GIT_TAG_SCOPE}" | grep -v "snapshot" | sort -V | tail -1)"
+echo "Checking protobuf against tag '${LATEST_STABLE_TAG}' (i.e., the most recent stable tag w.r.t. )"
 git checkout "${LATEST_STABLE_TAG}"
 
 readonly BUF_IMAGE="${BUF_IMAGE_TMPDIR}/buf.bin"
 buf build -o "${BUF_IMAGE}"
-git checkout main
+git checkout "${CURRENT_BRANCH}"
 buf breaking --against "${BUF_IMAGE}"
