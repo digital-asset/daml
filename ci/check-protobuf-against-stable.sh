3701da2115c4e1c4d105bffe9049fe9973d09ca0
fabiotudone-da <fabio.tudone@digitalasset.com>
2021-09-21 19:05:17 +0200

    Fix comment formatting
    Co-authored-by: Gary Verhaegen <gary.verhaegen@digitalasset.com>

--- ci/check-protobuf-against-stable.sh
+++ ci/check-protobuf-against-stable.sh
@@ -12,14 +12,17 @@ readonly CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 echo "The target branch is '${SYSTEM_PULLREQUEST_TARGETBRANCH}'."
 
-# For `main` and PRs targeting `main`, we simply check against the most recent stable tag.
+# For `main` and PRs targeting `main`, we simply check against the most recent
+# stable tag.
 #
-# For release branches and PRs targeting them, we should really check against all the most recent stable tags
-# reachable from either the current branch or from previous release branches (say, against both `1.17.1` and
-# `1.16.2` created after the `release/1.17.x` branch).
+# For release branches and PRs targeting them, we should really check against
+# all the most recent stable tags reachable from either the current branch or
+# from previous release branches (say, against both `1.17.1` and `1.16.2`
+# created after the `release/1.17.x` branch).
 #
-# Instead, we approximate by checking only against the most recent stable tag reachable from the current branch,
-# under the assumption that if a lesser release branch contains a protobuf change, then it will also be present in
+# Instead, we approximate by checking only against the most recent stable tag
+# reachable from the current branch, under the assumption that if a lesser
+# release branch contains a protobuf change, then it will also be present in
 # higher ones either through a shared commit or a back-port from `main`.
 readonly RELEASE_BRANCH_REGEX="^release/.*"
 GIT_TAG_SCOPE=""
