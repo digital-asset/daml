ee20aaf7c02656a82cd472b2f34282810cfcccaa
Fabio Tudone <fabio.tudone@digitalasset.com>
2021-09-21 15:28:09 +0200

    Simplify further as the check will be only run from `main`

--- ci/check-protobuf-against-stable.sh
+++ ci/check-protobuf-against-stable.sh
@@ -3,35 +3,14 @@
 # SPDX-License-Identifier: Apache-2.0
 set -euo pipefail
 
-readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-readonly PROJECT_ROOT="${SCRIPT_DIR}/.."
+BUF_IMAGE_TMPDIR="$(mktemp -d)"
+trap 'rm -rf ${BUF_IMAGE_TMPDIR}' EXIT
 
-cd "${PROJECT_ROOT}"
-readonly CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+LATEST_STABLE_TAG="$(git tag --no-merged | grep -v "snapshot" | sort -V | tail -1)"
+echo "Checking protobuf against tag '${LATEST_STABLE_TAG}' (i.e., the most recent stable tag)"
+git checkout "${LATEST_STABLE_TAG}"
 
-latest_stable() {
-  local gitTagSearchSpace
-  if [[ "${CURRENT_BRANCH}" == "main" ]]; then
-    gitTagSearchSpace="--no-merged"
-  else
-    gitTagSearchSpace="--merged"
-  fi
-  git tag ${gitTagSearchSpace} | grep -v "snapshot" | sort -V | tail -1
-}
-
-LATEST_STABLE="$(latest_stable)"
-echo "Checking protos against '${LATEST_STABLE}' (i.e., the most recent stable tag up to '${CURRENT_BRANCH}')"
-
-check_against_stable_protos_buf_image() {
-  local buf_image_dir
-  buf_image_dir="$(mktemp -d)"
-  local buf_image
-  buf_image="${buf_image_dir}/buf.bin"
-  trap 'rm -rf ${buf_image_dir}' EXIT
-  git checkout "${LATEST_STABLE}"
-  buf build -o "${buf_image}"
-  git checkout "${CURRENT_BRANCH}"
-  buf breaking --against "${buf_image}"
-}
-
-check_against_stable_protos_buf_image
+BUF_IMAGE="${BUF_IMAGE_TMPDIR}/buf.bin"
+buf build -o "${BUF_IMAGE}"
+git checkout main
+buf breaking --against "${BUF_IMAGE}"
