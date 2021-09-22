5c3a000dbb6e40dcaf5cf789f7704f31a726fada
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2021-09-22 13:26:17 +0200

    add tenant id

--- ci/copy-windows-release-artifacts.sh
+++ ci/copy-windows-release-artifacts.sh
@@ -22,6 +22,7 @@ fi
 /C/Users/u/.dotnet/tools/azuresigntool.exe sign \
   --azure-key-vault-url "$AZURE_KEY_VAULT_URL" \
   --azure-key-vault-client-id "$AZURE_CLIENT_ID" \
+  --azure-key-vault-tenant-id "$AZURE_TENANT_ID" \
   --azure-key-vault-client-secret "$AZURE_CLIENT_SECRET" \
   --azure-key-vault-certificate "$AZURE_KEY_VAULT_CERTIFICATE" \
   --description "Daml SDK installer" \
