b37184ca1a0ef8c8d49cc2f34a031655e042e6d4
Robin Krom <robin@digitalasset.com>
2021-09-28 22:08:36 +0200

    daml-ledger: better errors for non-json responses.
    This fixes #10985.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- daml-assistant/daml-helper/src/DA/Daml/Helper/Ledger.hs
+++ daml-assistant/daml-helper/src/DA/Daml/Helper/Ledger.hs
@@ -584,8 +584,24 @@ instance A.ToJSON AllocatePartyRequest
 httpJsonRequest :: A.FromJSON a => LedgerArgs -> Method -> Path -> (Request -> Request) -> IO a
 httpJsonRequest args method path modify = do
   req <- makeRequest args method path modify
-  resp <- runHttp httpJSON req
-  pure $ result resp
+  respOrErr <- runHttp httpJSONEither req
+  case respOrErr of
+    Right resp -> pure $ result resp
+    Left err ->
+      case err of
+        JSONParseException _req resp parseErr -> handleError resp parseErr
+        JSONConversionException _req resp convErr -> handleError resp convErr
+  where
+    handleError resp err = do
+      let contentType = getResponseHeader "Content-Type" resp
+      if BSC.pack "application/json" `elem` contentType
+        then fail $ "Response could not be parsed: " <> show err
+        else fail $
+             unlines
+               [ "The service returned an error response: "
+               , show $ getResponseStatus resp
+               , show $ getResponseBody resp
+               ]
 
 httpBsRequest :: LedgerArgs -> Method -> Path -> (Request -> Request) -> IO BS.ByteString
 httpBsRequest args method path modify = do
