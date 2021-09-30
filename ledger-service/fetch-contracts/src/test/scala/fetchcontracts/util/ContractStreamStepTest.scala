2a07d111c33f5cbfc83ee2b99095ecd7bdc5253a
Stephen Compall <stephen.compall@daml.com>
2021-09-30 19:53:46 -0400

    fix packages

--- ledger-service/fetch-contracts/src/test/scala/fetchcontracts/util/ContractStreamStepTest.scala
+++ ledger-service/fetch-contracts/src/test/scala/fetchcontracts/util/ContractStreamStepTest.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.http
+package com.daml.fetchcontracts
 package util
 
 import com.daml.scalatest.FlatSpecCheckLaws
--- ledger-service/fetch-contracts/src/test/scala/fetchcontracts/util/InsertDeleteStepTest.scala
+++ ledger-service/fetch-contracts/src/test/scala/fetchcontracts/util/InsertDeleteStepTest.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.http.util
+package com.daml.fetchcontracts.util
 
 import com.daml.scalatest.FlatSpecCheckLaws
 
