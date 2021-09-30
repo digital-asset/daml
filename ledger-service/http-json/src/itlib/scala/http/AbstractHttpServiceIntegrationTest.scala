1ed7adef3e2e4713bbc7bb372ebac30cb4722094
Akshay <akshay.shirahatti@digitalasset.com>
2021-09-30 14:42:59 +0100

    Reduce size of contracts for archiving test

--- ledger-service/http-json/src/itlib/scala/http/AbstractHttpServiceIntegrationTest.scala
+++ ledger-service/http-json/src/itlib/scala/http/AbstractHttpServiceIntegrationTest.scala
@@ -1735,7 +1735,8 @@ abstract class AbstractHttpServiceIntegrationTest
     StartSettings.DefaultMaxInboundMessageSize * 10
   ) { (uri, encoder, _, _, _) =>
     val (alice, headers) = getUniquePartyAndAuthHeaders("Alice")
-    val numContracts: Long = 10000
+    //The numContracts size should test for https://github.com/digital-asset/daml/issues/10339
+    val numContracts: Long = 2000
     val helperId = domain.TemplateId(None, "Account", "Helper")
     val payload = v.Record(
       fields = List(v.RecordField("owner", Some(v.Value(v.Value.Sum.Party(alice.unwrap)))))
