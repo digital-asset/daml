d98e9768ce7a1184d1519525a216f66df41d6d5c
Andreas Herrmann <andreas.herrmann@tweag.io>
2021-10-04 14:59:42 +0200

    Add table.prefix in trigger service migrations

--- triggers/service/src/main/resources/com/daml/lf/engine/trigger/db/migration/postgres/V3__Add_application_id.sql
+++ triggers/service/src/main/resources/com/daml/lf/engine/trigger/db/migration/postgres/V3__Add_application_id.sql
@@ -2,6 +2,6 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 -- Add application_id to running trigger table defaulting to trigger_instance
-alter table running_triggers add column application_id text;
-update running_triggers set application_id = trigger_instance;
-alter table running_triggers alter column application_id set not null;
+alter table ${table.prefix}running_triggers add column application_id text;
+update ${table.prefix}running_triggers set application_id = trigger_instance;
+alter table ${table.prefix}running_triggers alter column application_id set not null;
