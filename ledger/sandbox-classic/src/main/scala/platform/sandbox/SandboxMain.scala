cfcdc13c6e05283d13d74fb9a7e980d294a078ea
Robert Autenrieth <31539813+rautenrieth-da@users.noreply.github.com>
2021-10-06 00:04:18 +0200

    Add deprecation output for sandbox classic (#11119)
    changelog_begin
    changelog_end

--- ledger/sandbox-classic/src/main/scala/platform/sandbox/SandboxMain.scala
+++ ledger/sandbox-classic/src/main/scala/platform/sandbox/SandboxMain.scala
@@ -5,15 +5,28 @@ package com.daml.platform.sandbox
 
 import com.daml.ledger.resources.ResourceContext
 import com.daml.cliopts.GlobalLogLevel
+import com.daml.logging.ContextualizedLogger
+import com.daml.logging.LoggingContext.newLoggingContext
 import com.daml.platform.sandbox.config.SandboxConfig
 import com.daml.resources.ProgramResource
 
 object SandboxMain {
 
+  private val logger = ContextualizedLogger.get(this.getClass)
+
   def main(args: Array[String]): Unit =
     new ProgramResource({
       val config: SandboxConfig = Cli.parse(args).getOrElse(sys.exit(1))
       config.logLevel.foreach(GlobalLogLevel.set("Sandbox"))
+
+      // Point users to non-deprecated ledgers
+      config.jdbcUrl.foreach(_ =>
+        newLoggingContext { implicit loggingContext =>
+          logger.info(
+            "Sandbox classic with persistence is deprecated. Use the Daml Driver for PostgreSQL if you need persistence."
+          )
+        }
+      )
       SandboxServer.owner(Name, config)
     }).run(ResourceContext.apply)
 
