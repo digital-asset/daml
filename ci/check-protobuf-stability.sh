5abedac9ab92b2c0c532a4c74ccc430f3cfd1f8d
nicu-da <nicu.reut@digitalasset.com>
2021-10-08 02:18:36 -0700

    Check the commit message to skip the compatibility check [KVL-1111] (#11162)
    * Check the commit message to skip the compatibility check
    
    CHANGELOG_BEGIN
    
    CHANGELOG_END
    
    * Use git trailers to skip the check conditionally

--- ci/check-protobuf-stability.sh
+++ ci/check-protobuf-stability.sh
@@ -14,6 +14,7 @@ eval "$(dev-env/bin/dade assist)"
 TARGET="${SYSTEM_PULLREQUEST_TARGETBRANCH:-main}"
 echo "The target branch is '${TARGET}'."
 
+readonly BREAKING_PROTOBUF_CHANGE_TRAILER_KEY="Breaks-protobuf"
 # LF protobufs are checked against local snapshots.
 function check_lf_protos() {
 
@@ -57,6 +58,21 @@ function check_non_lf_protos() {
 
 }
 
+is_check_skipped() {
+  for sha in $(git rev-list origin/"$TARGET"..); do
+    breaks_proto_trailer_value=$(git show -s --format="%(trailers:key=${BREAKING_PROTOBUF_CHANGE_TRAILER_KEY},valueonly)" "$sha" | xargs)
+    if [[ $breaks_proto_trailer_value == "true" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
+check_protos() {
+  check_lf_protos
+  check_non_lf_protos
+}
+
 case "${1:---stable}" in
 -h | --help)
   cat <<USAGE
@@ -66,6 +82,12 @@ Options:
   -h, --help: shows this help
   --stable:     check against the latest stable tag (default)
   --target:     check against the tip of the target branch
+      When checking against a target branch, the check can be skipped by adding the following trailer:
+          $BREAKING_PROTOBUF_CHANGE_TRAILER_KEY: true
+      Example commit message:
+         This commit breaks protobufs when checking against the target branch.
+
+         $BREAKING_PROTOBUF_CHANGE_TRAILER_KEY: true
 USAGE
   exit
   ;;
@@ -97,6 +119,7 @@ USAGE
     BUF_CONFIG_UPDATED=true
   fi
   BUF_GIT_TARGET_TO_CHECK=".git#tag=${LATEST_STABLE_TAG}"
+  check_protos
   ;;
 --target)
   # Check against the tip of the target branch.
@@ -107,12 +130,15 @@ USAGE
   # The files are always split for versions > 1.17, and there is no way of opening a PR against a target <= 1.17 which includes this check
   BUF_CONFIG_UPDATED=true
   BUF_GIT_TARGET_TO_CHECK=".git#branch=origin/${TARGET}"
+  # The target check can be skipped by including the following trailer `Breaks-Proto: true` into the commit message
+  if is_check_skipped; then
+    echo "Skipping check for protobuf compatibility"
+  else
+    check_protos
+  fi
   ;;
 *)
   echo "unknown argument $1" >&2
   exit 1
   ;;
 esac
-
-check_lf_protos
-check_non_lf_protos
