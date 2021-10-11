0a11a56b9457fff4aa14ac7b3443e934fb4f14ac
Nick Chapman <nick.chapman@digitalasset.com>
2021-10-11 15:52:01 +0100

    test speedy compilation: identity function

--- daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/SpeedyCompilerSpec.scala
+++ daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/SpeedyCompilerSpec.scala
@@ -4,6 +4,7 @@
 package com.daml.lf
 package speedy
 
+import com.daml.lf.data.Ref._
 import com.daml.lf.language.Ast._
 import com.daml.lf.language.PackageInterface
 import com.daml.lf.speedy.Compiler.Config
@@ -22,12 +23,24 @@ class SpeedyCompilerSpec extends AnyFreeSpec with Matchers with Inside {
     def config: Config = Config.Default
     def compiler: Compiler = new Compiler(interface, config)
 
-    "expression 42" in {
+    def compile(expr: Expr): SExpr = compiler.unsafeCompile(expr)
+
+    "42" in {
 
       def before: Expr = EPrimLit(PLInt64(42))
-      def after: SExpr = compiler.unsafeCompile(before)
       def expected: SExpr = SEValue(SInt64(42))
-      after shouldBe expected
+      compile(before) shouldBe expected
+    }
+
+    def x: ExprVarName = Name.assertFromString("x")
+    def a: TypeVarName = Name.assertFromString("a")
+    def ty: Type = TVar(a)
+
+    "\\x -> x" in {
+
+      def before: Expr = EAbs(binder = (x, ty), body = EVar(x), None)
+      def expected: SExpr = SEMakeClo(Array(), 1, SELocA(0))
+      compile(before) shouldBe expected
     }
 
   }
