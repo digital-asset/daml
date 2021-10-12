bc2661483b91765d30c4bc7e91b182b9d227e735
Robin Krom <robin@digitalasset.com>
2021-10-12 19:19:29 +0200

    interfaces: test for triggers
    This adds a test checking whether we can exercise choices on interface
    contract IDs in triggers. We can only test this for daml-lf 1.dev.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

---
+++ triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/DevOnly.scala
@@ -0,0 +1,47 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.lf.engine.trigger.test
+
+import akka.stream.scaladsl.Flow
+import com.daml.lf.data.Ref._
+import com.daml.ledger.api.testing.utils.SuiteResourceManagementAroundAll
+import com.daml.ledger.api.v1.{value => LedgerApi}
+import org.scalatest._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+import com.daml.lf.engine.trigger.TriggerMsg
+
+class DevOnly
+    extends AsyncWordSpec
+    with AbstractTriggerTest
+    with Matchers
+    with Inside
+    with SuiteResourceManagementAroundAll
+    with TryValues {
+  self: Suite =>
+
+  this.getClass.getSimpleName can {
+    "InterfaceTest" should {
+      val triggerId = QualifiedName.assertFromString("Interface:test")
+      val tId = LedgerApi.Identifier(packageId, "Interface", "Asset")
+      "1 transfer" in {
+        for {
+          client <- ledgerClient()
+          party <- allocateParty(client)
+          runner = getRunner(client, triggerId, party)
+          (acs, offset) <- runner.queryACS()
+          // 1 for create of Asset
+          // 1 for completion
+          // 1 for exercise
+          // 1 for corresponding completion
+          _ <- runner.runWithACS(acs, offset, msgFlow = Flow[TriggerMsg].take(4))._2
+          acs <- queryACS(client, party)
+        } yield {
+          assert(acs(tId).length == 1)
+        }
+      }
+    }
+
+  }
+}
