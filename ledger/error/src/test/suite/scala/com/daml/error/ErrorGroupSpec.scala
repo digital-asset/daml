fdab93950dab88b7a73ecc8acf17be29edafa464
Pawel Batko <pawel.batko@digitalasset.com>
2021-10-14 20:49:46 +0200

    1

---
+++ ledger/error/src/test/suite/scala/com/daml/error/ErrorGroupSpec.scala
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.error
+
+import org.scalatest.BeforeAndAfter
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ErrorGroupSpec extends AnyFlatSpec with Matchers with BeforeAndAfter {
+
+  object ErrorGroupBar extends ErrorGroup()(ErrorClass.root())
+
+  object ErrorGroupsFoo {
+    private implicit val errorClass: ErrorClass = ErrorClass.root()
+
+    object ErrorGroupFoo1 extends ErrorGroup() {
+      object ErrorGroupFoo2 extends ErrorGroup() {
+        object ErrorGroupFoo3 extends ErrorGroup()
+      }
+    }
+  }
+
+  it should "resolve correct error group names" in {
+    ErrorGroupsFoo.ErrorGroupFoo1.ErrorGroupFoo2.ErrorGroupFoo3.errorClass shouldBe ErrorClass(
+      List("ErrorGroupFoo1", "ErrorGroupFoo2", "ErrorGroupFoo3")
+    )
+    ErrorGroupBar.errorClass shouldBe ErrorClass(List("ErrorGroupBar"))
+  }
+
+}
