3961aa170c0f994d4d66f9615b1e0c59e6da34ec
Samir Talwar <samir.talwar@digitalasset.com>
2021-10-18 10:47:11 +0200

    kvutils: Extract out versioned offset generators.

--- ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/VersionedOffsetBuilderSpec.scala
+++ ledger/participant-state/kvutils/src/test/suite/scala/com/daml/ledger/participant/state/kvutils/VersionedOffsetBuilderSpec.scala
@@ -3,8 +3,6 @@
 
 package com.daml.ledger.participant.state.kvutils
 
-import org.scalacheck.Arbitrary.arbitrary
-import org.scalacheck.Gen
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
@@ -15,35 +13,33 @@ class VersionedOffsetBuilderSpec
     with Matchers
     with ScalaCheckDrivenPropertyChecks {
 
-  import VersionedOffsetBuilderSpec._
+  import OffsetGen._
 
   "VersionedOffsetBuilder" should {
     "construct and extract" in {
-      forAll(arbitrary[Byte], genHighest, Gen.posNum[Int], Gen.posNum[Int]) {
-        (version, highest, middle, lowest) =>
-          val builder = new VersionedOffsetBuilder(version)
-          val offset = builder.of(highest, middle, lowest)
+      forAll(genVersion, genHighest, genMiddle, genLowest) { (version, highest, middle, lowest) =>
+        val builder = new VersionedOffsetBuilder(version)
+        val offset = builder.of(highest, middle, lowest)
 
-          val splitOffset = builder.split(offset)
-          splitOffset.version should be(version)
-          splitOffset.highest should be(highest)
-          splitOffset.middle should be(middle)
-          splitOffset.lowest should be(lowest)
+        val splitOffset = builder.split(offset)
+        splitOffset.version should be(version)
+        splitOffset.highest should be(highest)
+        splitOffset.middle should be(middle)
+        splitOffset.lowest should be(lowest)
       }
     }
 
     "extract the highest index" in {
-      forAll(arbitrary[Byte], genHighest, Gen.posNum[Int], Gen.posNum[Int]) {
-        (version, highest, middle, lowest) =>
-          val builder = new VersionedOffsetBuilder(version)
-          val offset = builder.of(highest, middle, lowest)
+      forAll(genVersion, genHighest, genMiddle, genLowest) { (version, highest, middle, lowest) =>
+        val builder = new VersionedOffsetBuilder(version)
+        val offset = builder.of(highest, middle, lowest)
 
-          builder.highestIndex(offset) should be(highest)
+        builder.highestIndex(offset) should be(highest)
       }
     }
 
     "fail on a highest that is out of range" in {
-      forAll(arbitrary[Byte], genOutOfRangeHighest, Gen.posNum[Int], Gen.posNum[Int]) {
+      forAll(genVersion, genOutOfRangeHighest, genMiddle, genLowest) {
         (version, highest, middle, lowest) =>
           val builder = new VersionedOffsetBuilder(version)
           (the[IllegalArgumentException] thrownBy builder.of(highest, middle, lowest)
@@ -52,7 +48,7 @@ class VersionedOffsetBuilderSpec
     }
 
     "fail on a negative middle index" in {
-      forAll(arbitrary[Byte], genHighest, Gen.negNum[Int], Gen.posNum[Int]) {
+      forAll(genVersion, genHighest, genOutOfRangeMiddle, genLowest) {
         (version, highest, middle, lowest) =>
           val builder = new VersionedOffsetBuilder(version)
           (the[IllegalArgumentException] thrownBy builder.of(highest, middle, lowest)
@@ -61,7 +57,7 @@ class VersionedOffsetBuilderSpec
     }
 
     "fail on a negative lowest index" in {
-      forAll(arbitrary[Byte], genHighest, Gen.posNum[Int], Gen.negNum[Int]) {
+      forAll(genVersion, genHighest, genMiddle, genOutOfRangeLowest) {
         (version, highest, middle, lowest) =>
           val builder = new VersionedOffsetBuilder(version)
           (the[IllegalArgumentException] thrownBy builder.of(highest, middle, lowest)
@@ -70,50 +66,37 @@ class VersionedOffsetBuilderSpec
     }
 
     "fail on a wrong version" in {
-      forAll(genHighest, Gen.posNum[Int], Gen.posNum[Int], genDifferentVersions) {
-        (highest, middle, lowest, versions) =>
-          val offset = new VersionedOffsetBuilder(versions._1).of(highest, middle, lowest)
-          val offsetBuilder = new VersionedOffsetBuilder(versions._2)
+      forAll(genDifferentVersions, genHighest, genMiddle, genLowest) {
+        case ((versionA, versionB), highest, middle, lowest) =>
+          val offset = new VersionedOffsetBuilder(versionA).of(highest, middle, lowest)
+          val offsetBuilder = new VersionedOffsetBuilder(versionB)
           val testedMethods =
             List(offsetBuilder.version(_), offsetBuilder.highestIndex(_), offsetBuilder.split(_))
 
           testedMethods.foreach { method =>
             (the[IllegalArgumentException] thrownBy method(offset)
-              should have message s"requirement failed: wrong version ${versions._1}, should be ${versions._2}")
+              should have message s"requirement failed: wrong version $versionA, should be $versionB")
           }
       }
     }
 
     "test the version of the offset, returning `true` on a match" in {
-      forAll(genHighest, Gen.posNum[Int], Gen.posNum[Int], arbitrary[Byte]) {
-        (highest, middle, lowest, version) =>
-          val offsetBuilder = new VersionedOffsetBuilder(version)
-          val offset = offsetBuilder.of(highest, middle, lowest)
+      forAll(genVersion, genHighest, genMiddle, genLowest) { (version, highest, middle, lowest) =>
+        val offsetBuilder = new VersionedOffsetBuilder(version)
+        val offset = offsetBuilder.of(highest, middle, lowest)
 
-          offsetBuilder.matchesVersionOf(offset) should be(true)
+        offsetBuilder.matchesVersionOf(offset) should be(true)
       }
     }
 
     "test the version of the offset, returning `false` on a mismatch" in {
-      forAll(genHighest, Gen.posNum[Int], Gen.posNum[Int], genDifferentVersions) {
-        (highest, middle, lowest, versions) =>
-          val offset = new VersionedOffsetBuilder(versions._1).of(highest, middle, lowest)
-          val offsetBuilder = new VersionedOffsetBuilder(versions._2)
+      forAll(genDifferentVersions, genHighest, genMiddle, genLowest) {
+        case ((versionA, versionB), highest, middle, lowest) =>
+          val offset = new VersionedOffsetBuilder(versionA).of(highest, middle, lowest)
+          val offsetBuilder = new VersionedOffsetBuilder(versionB)
 
           offsetBuilder.matchesVersionOf(offset) should be(false)
       }
     }
   }
-}
-
-object VersionedOffsetBuilderSpec {
-  private val genHighest = Gen.chooseNum(0L, VersionedOffsetBuilder.MaxHighest)
-
-  private val genOutOfRangeHighest =
-    Gen.oneOf(Gen.negNum[Long], Gen.chooseNum(VersionedOffsetBuilder.MaxHighest + 1, Long.MaxValue))
-
-  private val genDifferentVersions = for {
-    version1 <- arbitrary[Byte]
-    version2 <- arbitrary[Byte].suchThat(_ != version1)
-  } yield (version1, version2)
 }
