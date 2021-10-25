eece94bca979fc82176e3c9442faf00702160e90
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-10-25 22:44:39 +0200

    Multi-observer

--- ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/RandomnessProvider.scala
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/RandomnessProvider.scala
@@ -4,8 +4,9 @@
 package com.daml.ledger.api.benchtool.submission
 
 trait RandomnessProvider {
-  def randomDouble(): Double // 0.0 <= x <= 1.0
+  def randomDouble(): Double // 0.0 <= randomDouble() <= 1.0
   def randomBytes(n: Int): Array[Byte]
+  def randomNatural(n: Int): Int // 0 <= randomNatural(n) < n
 }
 
 object RandomnessProvider {
@@ -13,5 +14,6 @@ object RandomnessProvider {
     private val r = new scala.util.Random(System.currentTimeMillis())
     override def randomDouble(): Double = r.nextDouble()
     override def randomBytes(n: Int): Array[Byte] = r.nextBytes(n)
+    override def randomNatural(n: Int): Int = r.nextInt(n)
   }
 }
