8d5cab5f16330b4f01fe76698092a4923f2e24d4
Remy <remy.haemmerle@daml.com>
2021-10-25 08:54:42 +0200

    LF: Simplify seeds generation in scenario runnner (#11353)
    We just need the new seeds to be:
    - fresh during a run,
    - deterministic across two runs.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- daml-lf/repl/src/main/scala/com/digitalasset/daml/lf/repl/testing/Main.scala
+++ daml-lf/repl/src/main/scala/com/digitalasset/daml/lf/repl/testing/Main.scala
@@ -206,7 +206,7 @@ object Repl {
           compiledPackages,
           expr,
         )
-      (machine, ScenarioRunner(machine, seed).run())
+      (machine, new ScenarioRunner(machine, seed).run())
     }
   }
 
