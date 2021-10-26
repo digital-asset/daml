b787031a37a80d89adb63e0a6a1f6548fed64c10
Brian Healey <brian.healey@digitalasset.com>
2021-10-26 10:16:18 -0400

    Grant create job permission
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/participant-integration-api/src/main/resources/db/migration/oracle-appendonly/V2_1__Json_less_frequent_indexes.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/oracle-appendonly/V2_1__Json_less_frequent_indexes.sql
@@ -1,11 +1,3 @@
-DROP INDEX participant_events_create_tree_event_witnesses_idx;
-DROP INDEX participant_events_create_flat_event_witnesses_idx;
-DROP INDEX participant_events_consuming_exercise_flat_event_witnesses_idx;
-DROP INDEX participant_events_consuming_exercise_tree_event_witnesses_idx;
-DROP INDEX participant_events_non_consuming_exercise_flat_event_witness_idx;
-DROP INDEX participant_events_non_consuming_exercise_tree_event_witness_idx;
-DROP INDEX participant_events_divulgence_tree_event_witnesses_idx;
-
 CREATE SEARCH INDEX participant_events_create_tree_event_witnesses_idx ON participant_events_create (tree_event_witnesses)
     FOR JSON
     PARAMETERS('DATAGUIDE OFF SEARCH_ON TEXT SYNC (EVERY "FREQ=SECONDLY; INTERVAL=1")');
