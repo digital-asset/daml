4a34b6850083bc6ad8647c0ddf8c3d87453dadf8
fabiotudone-da <fabio.tudone@digitalasset.com>
2021-10-26 23:17:18 +0200

    KV: port V2 errors to self-service errors framework [KVL-1143] (#11326)
    * KV: port V2 errors to self-service errors framework
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Fix ConversionsSpec compiling
    
    * Use a valid ID for ValidationFailure
    
    * Fix error details in Conversions
    
    * Relax and simplify checks in KeyValueConsumptionSpec
    
    * Fix formatting
    
    * Fix definite answer in deduplication v2
    
    * Try and make Scala 2.12 happy
    
    * Fix typos in comments
    
    * Use NOT_FOUND for unknown parties and provide them as resources
    
    * Race (group) -> SubmissionRaces
    
    * Don't deprecate resource exhausted (ABORTED) error
    
    * Rename KVCompletionErrors group into KVErrors (ResourceExhausted can be synchronous)
    
    * Remove unneeded braces
    
    Co-authored-by: Hubert Slojewski <hubert.slojewski@digitalasset.com>
    
    * Move toJsonString to where it's used
    
    * Deprecate InvalidLedgerTime
    
    * Embed deprecation comments into annotation
    
    * Change deprecation messages so that they make more sense in the docs
    
    * Move deprecated errors to a dedicated group as proposed by @tudor-da
    
    Co-authored-by: Hubert Slojewski <hubert.slojewski@digitalasset.com>

---
+++ ledger/participant-state/kvutils/src/main/scala/com/daml/ledger/participant/state/kvutils/errors/KVLoggingTransactionErrorImpl.scala
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.participant.state.kvutils.errors
+
+import com.daml.error.definitions.LoggingTransactionErrorImpl
+import com.daml.error.{ContextualizedErrorLogger, ErrorCode}
+import com.daml.grpc.GrpcStatus
+import com.google.rpc.status.Status
+
+class KVLoggingTransactionErrorImpl(
+    cause: String,
+    throwable: Option[Throwable] = None,
+    definiteAnswer: Boolean = false,
+)(implicit
+    code: ErrorCode,
+    loggingContext: ContextualizedErrorLogger,
+) extends LoggingTransactionErrorImpl(cause, throwable, definiteAnswer) {
+  final def asStatus: Status = GrpcStatus.toProto(asGrpcStatusFromContext)
+}
