d9c7031fc377d7f7ebd88d6df0e44789b288cdbb
Kamil Bozek <72492440+kamil-da@users.noreply.github.com>
2021-10-26 17:19:03 +0200

    ACS testing - payload support [DPP-661] (#11308)
    * Generating random payload of configurable size in ledger-api-bench-tool command submission
    
    CHANGELOG_BEGIN
    - [Integration Kit] - ledger-api-bench-tool can generate test contracts with configurable payload size.
    CHANGELOG_END
    
    * Fix for Scala 2.12
    
    * Multi-template support for command submission in the ledger-api-bench-tool [DPP-659] (#11365)
    
    * Added multi-template support for command submission in the ledger-api-bench-tool
    
    CHANGELOG_BEGIN
    - [Integration Kit] - Added multi-template support for command submission in the ledger-api-bench-tool
    CHANGELOG_END
    
    * Simplified distribution calculation
    
    * Improved throttling - set max 100 in-flight commands

--- ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/generating/ContractSetDescriptor.scala
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package com.daml.ledger.api.benchtool.generating
-
-final case class ContractSetDescriptor(
-    numberOfInstances: Int
-)
---
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/Distribution.scala
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.api.benchtool.submission
+
+class Distribution(weights: List[Int]) {
+  assert(weights.nonEmpty, "Weights list must not be empty.")
+  assert(!weights.exists(_ < 1), "Weights must be strictly positive.")
+
+  def index(randomDouble: Double): Int = {
+    assert(randomDouble <= 1.0, "Given random double must be <= 1.0.")
+    // Consider changing implementation to use binary search when using on large lists.
+    distribution.indexWhere(_ >= randomDouble)
+  }
+
+  private lazy val totalWeight: Long = weights.map(_.toLong).sum
+  private lazy val distribution: List[Double] =
+    weights.scanLeft(0)((sum, weight) => sum + weight).map(_.toDouble / totalWeight).tail
+
+}
