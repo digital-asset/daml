b677200eb495d40a2b86c58aaee639f9fcbda454
Kamil Bozek <72492440+kamil-da@users.noreply.github.com>
2021-10-26 14:34:04 +0200

    Multi-template support for command submission in the ledger-api-bench-tool [DPP-659] (#11365)
    * Added multi-template support for command submission in the ledger-api-bench-tool
    
    CHANGELOG_BEGIN
    - [Integration Kit] - Added multi-template support for command submission in the ledger-api-bench-tool
    CHANGELOG_END
    
    * Simplified distribution calculation
    
    * Improved throttling - set max 100 in-flight commands

--- ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/CommandSubmitter.scala
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/CommandSubmitter.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.ledger.api.benchtool.generating
+package com.daml.ledger.api.benchtool.submission
 
 import akka.actor.ActorSystem
 import akka.stream.Materializer
@@ -9,21 +9,19 @@ import akka.stream.scaladsl.Source
 import com.daml.ledger.api.benchtool.infrastructure.TestDars
 import com.daml.ledger.api.benchtool.services.LedgerApiServices
 import com.daml.ledger.api.benchtool.util.SimpleFileReader
-import com.daml.ledger.api.v1.commands.Commands
+import com.daml.ledger.api.v1.commands.{Command, Commands}
 import com.daml.ledger.client.binding.Primitive
 import com.daml.ledger.client.binding.Primitive.Party
 import com.daml.ledger.resources.{ResourceContext, ResourceOwner}
-import com.daml.ledger.test.model.Foo.Foo1
 import org.slf4j.LoggerFactory
 import scalaz.syntax.tag._
 
 import java.io.File
-import java.nio.charset.StandardCharsets
-import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Random, Success, Try}
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
-case class ContractProducer(services: LedgerApiServices) {
+case class CommandSubmitter(services: LedgerApiServices) {
   private val logger = LoggerFactory.getLogger(getClass)
 
   private val identifierSuffix = f"${System.nanoTime}%x"
@@ -31,78 +29,64 @@ case class ContractProducer(services: LedgerApiServices) {
   private val workflowId = s"$applicationId-$identifierSuffix"
   private def commandId(index: Int) = s"command-$index-$identifierSuffix"
 
-  def create(
+  def submitCommands(
       descriptorFile: File
-  )(implicit ec: ExecutionContext): Future[Unit] = {
-    logger.info("Generating contracts...")
-    for {
+  )(implicit ec: ExecutionContext): Future[Unit] =
+    (for {
+      _ <- Future.successful(logger.info("Generating contracts..."))
       descriptor <- Future.fromTry(parseDescriptor(descriptorFile))
       party <- allocateParty()
       _ <- uploadTestDars()
       _ <- createContracts(descriptor = descriptor, party = party)
-    } yield {
-      logger.info("Contracts produced successfully.")
+    } yield logger.info("Contracts produced successfully.")).recoverWith { case NonFatal(ex) =>
+      logger.error(s"Command submission failed. Details: ${ex.getLocalizedMessage}", ex)
+      Future.failed(CommandSubmitter.CommandSubmitterError(ex.getLocalizedMessage))
     }
 
-  }
-
-  private def parseDescriptor(descriptorFile: File): Try[ContractSetDescriptor] = {
+  private def parseDescriptor(descriptorFile: File): Try[ContractSetDescriptor] =
     SimpleFileReader.readFile(descriptorFile)(DescriptorParser.parse).flatMap {
       case Left(err: DescriptorParser.DescriptorParserError) =>
         val message = s"Descriptor parsing error. Details: ${err.details}"
         logger.error(message)
-        Failure(new RuntimeException(message))
+        Failure(CommandSubmitter.CommandSubmitterError(message))
       case Right(descriptor) =>
         logger.info(s"Descriptor parsed: $descriptor")
         Success(descriptor)
     }
-  }
 
-  private def allocateParty()(implicit ec: ExecutionContext): Future[Primitive.Party] = {
-    val party0Hint = s"party-0-$identifierSuffix"
-    services.partyManagementService.allocateParty(party0Hint)
-  }
+  private def allocateParty()(implicit ec: ExecutionContext): Future[Primitive.Party] =
+    services.partyManagementService.allocateParty(
+      hint = s"party-0-$identifierSuffix"
+    )
 
-  private def uploadTestDars()(implicit ec: ExecutionContext): Future[Unit] = {
-    def uploadDar(dar: TestDars.DarFile, submissionId: String): Future[Unit] = {
-      logger.info(s"Uploading dar: ${dar.name}")
-      services.packageManagementService.uploadDar(
-        bytes = dar.bytes,
-        submissionId = submissionId,
-      )
-    }
+  private def uploadDar(dar: TestDars.DarFile, submissionId: String)(implicit
+      ec: ExecutionContext
+  ): Future[Unit] =
+    services.packageManagementService.uploadDar(
+      bytes = dar.bytes,
+      submissionId = submissionId,
+    )
 
+  private def uploadTestDars()(implicit ec: ExecutionContext): Future[Unit] =
     for {
       dars <- Future.fromTry(TestDars.readAll())
       _ <- Future.sequence(dars.zipWithIndex.map { case (dar, index) =>
         uploadDar(dar, s"submission-dars-$index-$identifierSuffix")
       })
     } yield ()
-  }
 
-  private def randomPayload(sizeBytes: Int, random: Random): String = {
-    val arr = Array.ofDim[Byte](sizeBytes)
-    random.nextBytes(arr)
-    new String(arr, StandardCharsets.UTF_8)
-  }
-
-  private def createContract(index: Int, party: Party, payload: String)(implicit
+  private def createContract(id: String, party: Party, createCommand: Command)(implicit
       ec: ExecutionContext
-  ): Future[Int] = {
-    val createCommand = Foo1(
-      signatory = party,
-      observers = List(party),
-      payload = payload,
-    ).create.command
+  ): Future[Unit] = {
     val commands = new Commands(
       ledgerId = services.ledgerId,
       applicationId = applicationId,
-      commandId = commandId(index),
+      commandId = id,
       party = party.unwrap,
       commands = List(createCommand),
       workflowId = workflowId,
     )
-    services.commandService.submitAndWait(commands).map(_ => index)
+    services.commandService.submitAndWait(commands).map(_ => ())
   }
 
   private def createContracts(descriptor: ContractSetDescriptor, party: Party)(implicit
@@ -115,23 +99,28 @@ case class ContractProducer(services: LedgerApiServices) {
         )
       }
 
+    val generator = new CommandGenerator(RandomnessProvider.Default, descriptor, party)
+
     implicit val resourceContext: ResourceContext = ResourceContext(ec)
-    val random = new Random(System.currentTimeMillis())
     materializerOwner()
       .use { implicit materializer =>
         Source
           .fromIterator(() => (1 to descriptor.numberOfInstances).iterator)
-          .throttle(
-            elements = 100,
-            per = 1.second,
-          )
-          .mapAsync(8)(index =>
-            createContract(
-              index = index,
-              party = party,
-              payload = randomPayload(descriptor.payloadSizeBytes, random),
-            )
-          )
+          .mapAsync(100) { index =>
+            generator.next() match {
+              case Right(command) =>
+                createContract(
+                  id = commandId(index),
+                  party = party,
+                  createCommand = command(party),
+                ).map(_ => index)
+              case Left(error) =>
+                Future.failed {
+                  logger.error(s"Command generation failed. Details: $error")
+                  CommandSubmitter.CommandSubmitterError(error)
+                }
+            }
+          }
           .runForeach(logProgress)
       }
       .map(_ => ())
@@ -143,4 +132,8 @@ case class ContractProducer(services: LedgerApiServices) {
       materializer <- ResourceOwner.forMaterializer(() => Materializer(actorSystem))
     } yield materializer
   }
+}
+
+object CommandSubmitter {
+  case class CommandSubmitterError(msg: String) extends RuntimeException(msg)
 }
---
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/RandomnessProvider.scala
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.api.benchtool.submission
+
+trait RandomnessProvider {
+  def randomDouble(): Double // 0.0 <= x <= 1.0
+  def randomBytes(n: Int): Array[Byte]
+}
+
+object RandomnessProvider {
+  object Default extends RandomnessProvider {
+    private val r = new scala.util.Random(System.currentTimeMillis())
+    override def randomDouble(): Double = r.nextDouble()
+    override def randomBytes(n: Int): Array[Byte] = {
+      val arr = Array.ofDim[Byte](n)
+      r.nextBytes(arr)
+      arr
+    }
+  }
+}
