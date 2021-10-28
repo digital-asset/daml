22e9e6b56e46368f784b4089d5def7a14d402a2f
Kamil Bozek <72492440+kamil-da@users.noreply.github.com>
2021-10-28 13:54:28 +0200

    Multi-observer command submission in the ledger-api-bench-tool [DPP-660] (#11413)
    * Multi-observer command submission in the ledger-api-bench-tool
    
    CHANGELOG_BEGIN
    - [Integration Kit] - ledger-api-bench-tool can generate synthetic contracts with multiple observers
    CHANGELOG_END

--- ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/CommandGenerator.scala
+++ ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/submission/CommandGenerator.scala
@@ -4,44 +4,72 @@
 package com.daml.ledger.api.benchtool.submission
 
 import com.daml.ledger.api.v1.commands.Command
-import com.daml.ledger.client.binding.Primitive.Party
+import com.daml.ledger.client.binding.Primitive
 import com.daml.ledger.test.model.Foo._
 
 import java.nio.charset.StandardCharsets
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
 final class CommandGenerator(
     randomnessProvider: RandomnessProvider,
     descriptor: ContractSetDescriptor,
-    party: Party,
+    observers: List[Primitive.Party],
 ) {
   private val distribution = new Distribution(descriptor.instanceDistribution.map(_.weight))
-  private lazy val descriptionMapping: Map[Int, ContractSetDescriptor.ContractDescription] =
+  private val descriptionMapping: Map[Int, ContractSetDescriptor.ContractDescription] =
     descriptor.instanceDistribution.zipWithIndex
       .map(_.swap)
       .toMap
+  private val observersWithIndices: List[(Primitive.Party, Int)] = observers.zipWithIndex
 
-  def next(): Either[String, Party => Command] = {
-    val description = descriptionMapping(distribution.index(randomnessProvider.randomDouble()))
-    createContractCommand(
-      template = description.template,
-      observers = List(party),
-      payload = randomPayload(description.payloadSizeBytes),
-    )
-  }
+  def next(): Try[Primitive.Party => Command] =
+    (for {
+      (description, observers) <- Try((pickDescription(), pickObservers()))
+      payload <- Try(randomPayload(description.payloadSizeBytes))
+      command <- createContractCommand(
+        template = description.template,
+        observers = observers,
+        payload = payload,
+      )
+    } yield command).recoverWith { case NonFatal(ex) =>
+      Failure(
+        CommandGenerator.CommandGeneratorError(
+          msg = s"Command generation failed. Details: ${ex.getLocalizedMessage}",
+          cause = ex,
+        )
+      )
+    }
+
+  private def pickDescription(): ContractSetDescriptor.ContractDescription =
+    descriptionMapping(distribution.index(randomnessProvider.randomDouble()))
+
+  private def pickObservers(): List[Primitive.Party] =
+    observersWithIndices
+      .filter { case (_, index) => isObserverUsed(index) }
+      .map(_._1)
+
+  private def isObserverUsed(i: Int): Boolean =
+    randomnessProvider.randomNatural(math.pow(10.0, i.toDouble).toInt) == 0
 
   private def createContractCommand(
       template: String,
-      observers: List[Party],
+      observers: List[Primitive.Party],
       payload: String,
-  ): Either[String, Party => Command] = {
+  ): Try[Primitive.Party => Command] =
     template match {
-      case "Foo1" => Right(Foo1(_, observers, payload).create.command)
-      case "Foo2" => Right(Foo2(_, observers, payload).create.command)
-      case "Foo3" => Right(Foo3(_, observers, payload).create.command)
-      case invalid => Left(s"Invalid template: $invalid")
+      case "Foo1" => Success(Foo1(_, observers, payload).create.command)
+      case "Foo2" => Success(Foo2(_, observers, payload).create.command)
+      case "Foo3" => Success(Foo3(_, observers, payload).create.command)
+      case invalid => Failure(new RuntimeException(s"Invalid template: $invalid"))
     }
-  }
 
   private def randomPayload(sizeBytes: Int): String =
     new String(randomnessProvider.randomBytes(sizeBytes), StandardCharsets.UTF_8)
+
+}
+
+object CommandGenerator {
+  case class CommandGeneratorError(msg: String, cause: Throwable)
+      extends RuntimeException(msg, cause)
 }
