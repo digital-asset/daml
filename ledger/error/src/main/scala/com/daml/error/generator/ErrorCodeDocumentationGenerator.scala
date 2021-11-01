e91ced54f3f828a1b2fa6e059d4da0b363ef36fe
fabiotudone-da <fabio.tudone@digitalasset.com>
2021-11-01 11:13:06 +0100

    KV: generate self-service error docs [KVL-1148] (#11431)
    * Differentiate committer deduplication error code
    
    * Let error docs generator tolerate `deprecated`
    
    * Generate error documentation for KV
    
    CHANGELOG_BEGIN
    - [Integration Kit] Document KV-specific self-service errors
    CHANGELOG_END
    
    * Cleanup ErrorCodeDocumentationGenerator
    
    * Add test case for deprecated errors
    
    * Improve ErrorCodeDocumentationGenerator.getAnnotations
    
    * Fix test
    
    * Factor annotationTypeName
    
    * Fix formatting
    
    * Use case class for annotations and deal with missing ones in convertToDocItem
    
    * Improve cause wording for KV duplicate command self-service error
    
    * Simplify error message
    
    Co-authored-by: tudor-da <tudor.voicu@digitalasset.com>
    
    Co-authored-by: tudor-da <tudor.voicu@digitalasset.com>

--- ledger/error/src/main/scala/com/daml/error/generator/ErrorCodeDocumentationGenerator.scala
+++ ledger/error/src/main/scala/com/daml/error/generator/ErrorCodeDocumentationGenerator.scala
@@ -4,12 +4,16 @@
 package com.daml.error.generator
 
 import java.lang.reflect.Modifier
+
 import com.daml.error.ErrorCode
 import com.daml.error.generator.ErrorCodeDocumentationGenerator.{
+  acceptedTypeNames,
+  deprecatedTypeName,
   explanationTypeName,
   resolutionTypeName,
 }
 import com.daml.error.{Explanation, Resolution}
+
 import org.reflections.Reflections
 
 import scala.reflect.runtime.{universe => ru}
@@ -35,7 +39,7 @@ case class ErrorCodeDocumentationGenerator(prefix: String = "com.daml") {
     errorCodes.map(convertToDocItem).sortBy(_.code)
   }
 
-  private def getErrorCodeInstances =
+  private def getErrorCodeInstances: Seq[ErrorCode] =
     new Reflections(prefix)
       .getSubTypesOf(classOf[ErrorCode])
       .asScala
@@ -47,51 +51,89 @@ case class ErrorCodeDocumentationGenerator(prefix: String = "com.daml") {
       .toSeq
 
   private def convertToDocItem(error: ErrorCode): DocItem = {
-    val (expl, res) = getErrorNameAndAnnotations(error)
+    val ErrorDocumentationAnnotations(explanation, resolution) =
+      getErrorDocumentationAnnotations(error)
+
     DocItem(
       className = error.getClass.getName,
       category = error.category.getClass.getSimpleName.replace("$", ""),
       hierarchicalGrouping = error.parent.docNames.filter(_ != ""),
       conveyance = error.errorConveyanceDocString.getOrElse(""),
       code = error.id,
-      explanation = expl,
-      resolution = res,
+      explanation = explanation.getOrElse(Explanation("")),
+      resolution = resolution.getOrElse(Resolution("")),
     )
   }
 
-  private def getErrorNameAndAnnotations(error: ErrorCode): (Explanation, Resolution) = {
+  private case class ErrorDocumentationAnnotations(
+      explanation: Option[Explanation],
+      resolution: Option[Resolution],
+  )
+
+  private def getErrorDocumentationAnnotations(error: ErrorCode): ErrorDocumentationAnnotations = {
     val mirror = ru.runtimeMirror(getClass.getClassLoader)
     val mirroredType = mirror.reflect(error)
     val annotations: Seq[ru.Annotation] = mirroredType.symbol.annotations
     getAnnotations(annotations)
   }
 
-  private def getAnnotations(annotations: Seq[ru.Annotation]): (Explanation, Resolution) = {
-    annotations match {
-      case Nil => (Explanation(""), Resolution(""))
+  private case class GetAnnotationsState(
+      explanation: Option[Explanation],
+      resolution: Option[Resolution],
+  )
 
-      case v :: Nil if isExplanationAnnotation(v) =>
-        (Explanation(parseValueOfAnnotation(v.tree)), Resolution(""))
+  private def getAnnotations(
+      annotations: Seq[ru.Annotation]
+  ): ErrorDocumentationAnnotations = {
 
-      case v :: Nil if isResolutionAnnotation(v) =>
-        (Explanation(""), Resolution(parseValueOfAnnotation(v.tree)))
+    def update(
+        state: GetAnnotationsState,
+        updatedExplanation: Option[String] = None,
+        updatedResolution: Option[String] = None,
+    ): GetAnnotationsState = {
 
-      case v :: v2 :: Nil if isExplanationAnnotation(v) && isResolutionAnnotation(v2) =>
-        (Explanation(parseValueOfAnnotation(v.tree)), Resolution(parseValueOfAnnotation(v2.tree)))
+      def updateString(
+          existing: Option[String],
+          updated: Option[String],
+          designation: String,
+      ): Option[String] =
+        updated.fold(ifEmpty = existing) { value: String =>
+          if (existing.isDefined)
+            sys.error(s"Multiple $designation annotations detected")
+          else
+            Some(value)
+        }
 
-      case v :: v2 :: Nil if isResolutionAnnotation(v) && isExplanationAnnotation(v2) =>
-        (Explanation(parseValueOfAnnotation(v2.tree)), Resolution(parseValueOfAnnotation(v.tree)))
+      val existingExplanation = state.explanation
+      val updatedExplanationString =
+        updateString(existingExplanation.map(_.explanation), updatedExplanation, "explanation")
+      val existingResolution = state.resolution
+      val updatedResolutionString =
+        updateString(existingResolution.map(_.resolution), updatedResolution, "resolution")
+      GetAnnotationsState(
+        updatedExplanationString.map(Explanation),
+        updatedResolutionString.map(Resolution),
+      )
+    }
 
-      case _ =>
+    val doc = annotations.foldLeft(GetAnnotationsState(None, None)) { case (state, annotation) =>
+      if (isAnnotation(annotation, deprecatedTypeName))
+        state
+      else if (isAnnotation(annotation, explanationTypeName))
+        update(state, updatedExplanation = Some(parseAnnotationValue(annotation.tree)))
+      else if (isAnnotation(annotation, resolutionTypeName))
+        update(state, updatedResolution = Some(parseAnnotationValue(annotation.tree)))
+      else
         sys.error(
-          s"Error code has an unexpected amount (${annotations.size}) or type of annotations. " +
-            s"Did you rename the error code annotations `${classOf[Explanation].getTypeName}` or `${classOf[Resolution].getTypeName}`?"
+          s"Unexpected annotation detected (${annotations.map(annotationTypeName)} but the only supported ones are $acceptedTypeNames)."
         )
     }
+
+    ErrorDocumentationAnnotations(doc.explanation, doc.resolution)
   }
 
   @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-  private def parseValueOfAnnotation(tree: ru.Tree): String = {
+  private def parseAnnotationValue(tree: ru.Tree): String = {
     try {
       // get second (index starts at 0) child of tree as it contains the first value of the annotation
       Seq(1).map(tree.children(_).asInstanceOf[ru.Literal].value.value.asInstanceOf[String]) match {
@@ -107,14 +149,18 @@ case class ErrorCodeDocumentationGenerator(prefix: String = "com.daml") {
     }
   }
 
-  private def isResolutionAnnotation(annotation: ru.Annotation): Boolean =
-    annotation.tree.tpe.toString == resolutionTypeName
+  private def isAnnotation(annotation: ru.Annotation, typeName: String): Boolean =
+    annotationTypeName(annotation) == typeName
 
-  private def isExplanationAnnotation(annotation: ru.Annotation): Boolean =
-    annotation.tree.tpe.toString == explanationTypeName
+  private def annotationTypeName(annotation: ru.Annotation) =
+    annotation.tree.tpe.toString
 }
 
 private object ErrorCodeDocumentationGenerator {
+
+  private val deprecatedTypeName = classOf[deprecated].getTypeName.replace("scala.", "")
   private val explanationTypeName = classOf[Explanation].getTypeName.replace("$", ".")
   private val resolutionTypeName = classOf[Resolution].getTypeName.replace("$", ".")
+
+  private val acceptedTypeNames = Set(deprecatedTypeName, explanationTypeName, resolutionTypeName)
 }
