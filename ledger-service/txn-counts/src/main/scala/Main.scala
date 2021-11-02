c0252e6c613989745c2df7585c89427757bb0309
Stephen Compall <stephen.compall@daml.com>
2021-11-02 16:23:04 -0400

    stub transaction counts utility

---
+++ ledger-service/txn-counts/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.txncounts
+
+object Main {
+  def it() = {}
+}
