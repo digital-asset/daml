dd1b0347eca16afb811dcb175086aa73d57aeacb
Gerolf Seitz <gerolf.seitz@daml.com>
2021-11-09 10:03:57 +0100

    Restart the submission interpretation in case of a race [DPP-737] (#11579)
    Command interpretation happens in two stages:
    1. Daml interpretation
    2. Determine a suitable ledger effective time
    
    The race can happen in the following situation:
    In 1) a contract key K is resolved to contract C1.
    Between 1) and 2), a transaction is stored on the participant that
    archives C1, but creates C2 with the same contract key K.
    In 2) the ledger api server tries to lookup the ledger effective time
    for all input contracts to the transaction. If it doesn't find one of
    the input contracts at all, it can conclude that the transaction
    wouldn't be accepted by the ledger anyway.
    
    The behavior before this patch was to simply abort command
    interpretation and return a rather cryptic error to the user.
    With this patch, the ledger api server restarts the command
    interpretation.
    If the "missing contract" was an explicit input to the
    command (e.g. as the contract for the exercise or as an argument to an
    exercise), then this command will be rejected because the contract is
    now archived.
    If the contract ID was determined via a contract key lookup, then
    restarting the interpretation will result in either a negative lookup or
    a different contract ID for the new contract under this contract key.
    
    CHANGELOG_BEGIN
    [Ledger API] Retry the interpretation of a command in case of a race
    with other transactions. This fix drastically reduces the likelihood of the error
    "Could not find a suitable ledger time after 0 retries".
    CHANGELOG_END

--- ledger/participant-integration-api/src/test/suite/scala/platform/store/cache/MutableCacheBackedContractStoreSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/platform/store/cache/MutableCacheBackedContractStoreSpec.scala
@@ -20,13 +20,12 @@ import com.daml.lf.transaction.test.TransactionBuilder
 import com.daml.lf.value.Value.{ContractInstance, ValueRecord, ValueText}
 import com.daml.logging.LoggingContext
 import com.daml.metrics.Metrics
+import com.daml.platform.apiserver.execution.MissingContracts
 import com.daml.platform.store.EventSequentialId
 import com.daml.platform.store.appendonlydao.events.ContractStateEvent
 import com.daml.platform.store.cache.ContractKeyStateValue.{Assigned, Unassigned}
 import com.daml.platform.store.cache.ContractStateValue.{Active, Archived}
 import com.daml.platform.store.cache.MutableCacheBackedContractStore.{
-  ContractNotFound,
-  EmptyContractIds,
   EventSequentialId,
   SignalNewLedgerHead,
   SubscribeToContractStateEvents,
@@ -350,7 +349,7 @@ class MutableCacheBackedContractStoreSpec
         _ = store.cacheIndex.set(unusedOffset, 2L)
         // populate the cache
         _ <- store.lookupActiveContract(Set(bob), cId_5)
-        assertion <- recoverToSucceededIf[ContractNotFound](
+        assertion <- recoverToSucceededIf[MissingContracts](
           store.lookupMaximumLedgerTime(Set(cId_1, cId_5))
         )
       } yield assertion
@@ -360,17 +359,10 @@ class MutableCacheBackedContractStoreSpec
       for {
         store <- contractStore(cachesSize = 0L).asFuture
         _ = store.cacheIndex.set(unusedOffset, 2L)
-        assertion <- recoverToSucceededIf[IllegalArgumentException](
+        assertion <- recoverToSucceededIf[MissingContracts](
           store.lookupMaximumLedgerTime(Set(cId_1, cId_5))
         )
       } yield assertion
-    }
-
-    "fail if the requested contract id set is empty" in {
-      for {
-        store <- contractStore(cachesSize = 0L).asFuture
-        _ <- recoverToSucceededIf[EmptyContractIds](store.lookupMaximumLedgerTime(Set.empty))
-      } yield succeed
     }
   }
 
@@ -496,14 +488,8 @@ object MutableCacheBackedContractStoreSpec {
     ): Future[Option[Timestamp]] = ids match {
       case setIds if setIds == Set(cId_4) =>
         Future.successful(Some(t4))
-      case set if set.isEmpty =>
-        Future.failed(EmptyContractIds())
       case _ =>
-        Future.failed(
-          new IllegalArgumentException(
-            s"The following contracts have not been found: ${ids.map(_.coid).mkString(", ")}"
-          )
-        )
+        Future.failed(MissingContracts(ids))
     }
 
     override def lookupActiveContractAndLoadArgument(
