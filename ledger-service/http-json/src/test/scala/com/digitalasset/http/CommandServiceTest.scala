8300b6fa5baafef21049805636549fa241a66c2e
Stephen Compall <stephen.compall@daml.com>
2021-11-09 21:26:18 -0500

    test that command service uses meta readAs, actAs

---
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/CommandServiceTest.scala
@@ -0,0 +1,128 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.http
+
+import com.daml.ledger.api.{v1 => lav1}
+import lav1.command_service.{
+  SubmitAndWaitForTransactionResponse,
+  SubmitAndWaitRequest,
+  SubmitAndWaitForTransactionTreeResponse,
+}
+import lav1.transaction.{Transaction, TransactionTree}
+import com.daml.http.util.{Logging => HLogging}
+import com.daml.logging.LoggingContextOf
+import LoggingContextOf.{label, newLoggingContext}
+import HttpServiceTestFixture.jwtForParties
+
+import org.scalatest.Inside
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+import scalaz.{\/-, NonEmptyList}
+import scalaz.syntax.foldable._
+import scalaz.syntax.tag._
+
+import java.util.concurrent.CopyOnWriteArrayList
+import scala.{collection => sc}
+import scala.concurrent.{Future, ExecutionContext => EC}
+import scala.jdk.CollectionConverters._
+
+@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+class CommandServiceTest extends AsyncWordSpec with Matchers with Inside {
+  import CommandServiceTest._
+
+  "create" should {
+    "let CommandMeta parties override JWT" in {
+      val (cs, txns, trees) = simpleCommand()
+      val specialActAs = NonEmptyList("bar")
+      val specialReadAs = List("quux")
+      def create(meta: Option[domain.CommandMeta]) =
+        domain.CreateCommand(tplId, lav1.value.Record(), meta)
+      for {
+        normal <- cs.create(multiPartyJwt, multiPartyJwp, create(None))
+        overridden <- cs.create(
+          multiPartyJwt,
+          multiPartyJwp,
+          create(
+            Some(
+              domain.CommandMeta(
+                None,
+                actAs = Some(domain.Party subst specialActAs),
+                readAs = Some(domain.Party subst specialReadAs),
+              )
+            )
+          ),
+        )
+      } yield {
+        normal shouldBe a[\/-[_]]
+        overridden shouldBe a[\/-[_]]
+        inside(txns) {
+          case sc.Seq(
+                SubmitAndWaitRequest(Some(normalC)),
+                SubmitAndWaitRequest(Some(overriddenC)),
+              ) =>
+            normalC.actAs should ===(multiPartyJwp.actAs)
+            normalC.readAs should ===(multiPartyJwp.readAs)
+            overriddenC.actAs should ===(specialActAs.toList)
+            overriddenC.readAs should ===(specialReadAs)
+        }
+        trees shouldBe empty
+      }
+    }
+  }
+}
+
+object CommandServiceTest {
+  private val multiPartyJwp = domain.JwtWritePayload(
+    domain.LedgerId("what"),
+    domain.ApplicationId("myapp"),
+    submitter = domain.Party subst NonEmptyList("foo", "bar"),
+    readAs = domain.Party subst List("baz", "quux"),
+  )
+  private val multiPartyJwt = jwtForParties(
+    actAs = domain.Party unsubst multiPartyJwp.submitter.toList,
+    readAs = domain.Party unsubst multiPartyJwp.readAs,
+    ledgerId = multiPartyJwp.ledgerId.unwrap,
+  )
+  private val tplId = domain.TemplateId("Foo", "Bar", "Baz")
+
+  implicit private val ignoredLoggingContext
+      : LoggingContextOf[HLogging.InstanceUUID with HLogging.RequestID] =
+    newLoggingContext(label[HLogging.InstanceUUID with HLogging.RequestID])(identity)
+
+  @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+  private def simpleCommand()(implicit
+      ec: EC
+  ): (CommandService, sc.Seq[SubmitAndWaitRequest], sc.Seq[SubmitAndWaitRequest]) = {
+    val txns = new CopyOnWriteArrayList[SubmitAndWaitRequest]()
+    val trees = new CopyOnWriteArrayList[SubmitAndWaitRequest]()
+    (
+      new CommandService(
+        submitAndWaitForTransaction = (_, req) =>
+          Future {
+            txns.add(req)
+            import lav1.event.{CreatedEvent, Event}, Event.Event.Created
+            val creation = Event(
+              Created(
+                CreatedEvent(
+                  templateId = Some(
+                    lav1.value
+                      .Identifier(tplId.packageId, tplId.moduleName, tplId.entityName)
+                  ),
+                  createArguments = Some(lav1.value.Record()),
+                )
+              )
+            )
+            \/-(SubmitAndWaitForTransactionResponse(Some(Transaction(events = Seq(creation)))))
+          },
+        submitAndWaitForTransactionTree = (_, req) =>
+          Future {
+            trees.add(req)
+            \/-(SubmitAndWaitForTransactionTreeResponse(Some(TransactionTree())))
+          },
+      ),
+      txns.asScala,
+      trees.asScala,
+    )
+  }
+}
