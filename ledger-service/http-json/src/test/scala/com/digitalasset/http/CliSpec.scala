1a1eed3cc8893f7800295266c05699fe15b76459
Akshay <akshay.shirahatti@digitalasset.com>
2021-11-10 10:05:32 +0000

    add constants for test defaults

--- ledger-service/http-json/src/test/scala/com/digitalasset/http/CliSpec.scala
+++ ledger-service/http-json/src/test/scala/com/digitalasset/http/CliSpec.scala
@@ -8,7 +8,15 @@ import org.scalatest.matchers.should.Matchers
 import com.daml.dbutils
 import com.daml.http.dbbackend.{DbStartupMode, JdbcConfig}
 
+object CliSpec {
+  private val poolSize = 10
+  private val minIdle = 4
+  private val connectionTimeout = 5000L
+  private val idleTimeout = 1000L
+  private val tablePrefix = "foo"
+}
 final class CliSpec extends AnyFreeSpec with Matchers {
+  import CliSpec._
 
   private def configParser(
       parameters: Seq[String],
@@ -22,17 +30,17 @@ final class CliSpec extends AnyFreeSpec with Matchers {
       "jdbc:postgresql://localhost:5432/test?&ssl=true",
       "postgres",
       "password",
-      poolSize = 10,
-      minIdle = 4,
-      connectionTimeout = 5000,
-      idleTimeout = 1000,
-      tablePrefix = "foo",
+      poolSize,
+      minIdle,
+      connectionTimeout,
+      idleTimeout,
+      tablePrefix,
     ),
     dbStartupMode = DbStartupMode.StartOnly,
   )
   val jdbcConfigString =
     "driver=org.postgresql.Driver,url=jdbc:postgresql://localhost:5432/test?&ssl=true,user=postgres,password=password," +
-      "poolSize=10,minIdle=4,connectionTimeout=5000,idleTimeout=1000,tablePrefix=foo"
+      s"poolSize=$poolSize,minIdle=$minIdle,connectionTimeout=$connectionTimeout,idleTimeout=$idleTimeout,tablePrefix=$tablePrefix"
 
   val sharedOptions =
     Seq("--ledger-host", "localhost", "--ledger-port", "6865", "--http-port", "7500")
@@ -112,8 +120,8 @@ final class CliSpec extends AnyFreeSpec with Matchers {
 
     "DbStartupMode" - {
       val jdbcConfigShared =
-        "driver=org.postgresql.Driver,url=jdbc:postgresql://localhost:5432/test?&ssl=true,user=postgres," +
-          "password=password,poolSize=10,minIdle=4,connectionTimeout=5000,idleTimeout=1000,tablePrefix=foo"
+        "driver=org.postgresql.Driver,url=jdbc:postgresql://localhost:5432/test?&ssl=true,user=postgres,password=password," +
+          s"poolSize=$poolSize,minIdle=$minIdle,connectionTimeout=$connectionTimeout,idleTimeout=$idleTimeout,tablePrefix=$tablePrefix"
 
       "should get the CreateOnly startup mode from the string" in {
         val jdbcConfigString = s"$jdbcConfigShared,start-mode=create-only"
