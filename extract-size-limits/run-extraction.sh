aaef8b42096fdc8c77618889ee4bccadc649ec89
Simon Meier <simon@digitalasset.com>
2021-11-10 17:19:00 +0100

    Final readme and adjustments.

--- extract-size-limits/run-extraction.sh
+++ extract-size-limits/run-extraction.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+# Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# One-off script to extract maximal sizes from events and packages stored in an IndexDB of a Daml participant node.
+#
+# Usage:
+#   1. Adjust the DBNAME and PSQL invocations below to point to your DB and use your authentication method.
+#   2. call the script using 'bash run-extraction.sh'
+#   3. Inspect the resulting .csv files. The extraction queries are written for different IndexDB versions, which is
+#      why not all .csv files will contain any content. At least one of the 'event_V<xxx>.csv' files should contain data.
+#   4. Share the packages .tar.gz file with other stakeholders wrt understanding size limits of your workflow.
 
-# set -e
 
-DBNAME='connect_1_0_1'
+DBNAME='chess_db'
 PSQL="sudo -u postgres psql -d$DBNAME"
 
 echo "-- starting extraction --"
@@ -21,5 +31,6 @@ done
 TAR="db-limits-extraction-$(date -u -I).tar.gz"
 
 echo "-- extraction complete --"
-echo "Storing the extraction results in '$TAR'"
+echo "See 'csv/$DBNAME' for the resulting .csv files. "
+echo "Packaging them into '$TAR' for easy sharing."
 tar -czf "$TAR" csv/ sql/
