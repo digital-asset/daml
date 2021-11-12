d3bb03659104ba58c8caee2695d38c506b5cd158
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-11-12 07:18:12 +0100

    Fix sandbox database config in script & trigger tests (#11655)
    * Fix sandbox database config in script & trigger tests
    
    Setting jdbcUrl does absolutely nothing because it later gets
    overwritten again.
    
    changelog_begin
    changelog_end
    
    * .
    
    changelog_begin
    changelog_end

--- triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/AbstractTriggerTest.scala
+++ triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/AbstractTriggerTest.scala
@@ -27,8 +27,8 @@ import com.daml.lf.data.Ref._
 import com.daml.lf.speedy.SValue
 import com.daml.lf.speedy.SValue._
 import com.daml.platform.sandboxnext.SandboxNextFixture
+import com.daml.platform.sandbox.SandboxBackend
 import com.daml.platform.sandbox.services.TestCommands
-import com.daml.testing.postgresql.PostgresAroundAll
 import org.scalatest._
 import scalaz.syntax.tag._
 
@@ -36,7 +36,10 @@ import scala.collection.compat._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
-trait AbstractTriggerTest extends SandboxNextFixture with PostgresAroundAll with TestCommands {
+trait AbstractTriggerTest
+    extends SandboxNextFixture
+    with SandboxBackend.Postgresql
+    with TestCommands {
   self: Suite =>
 
   protected def toHighLevelResult(s: SValue) = s match {
@@ -77,10 +80,6 @@ trait AbstractTriggerTest extends SandboxNextFixture with PostgresAroundAll with
   override protected def darFile =
     Try(BazelRunfiles.requiredResource("triggers/tests/acs.dar"))
       .getOrElse(BazelRunfiles.requiredResource("triggers/tests/acs-1.dev.dar"))
-
-  override protected def config = super.config.copy(
-    jdbcUrl = Some(postgresDatabase.url)
-  )
 
   protected val dar = DarDecoder.assertReadArchiveFromFile(darFile)
   protected val compiledPackages =
