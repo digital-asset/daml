05f604d672284a2a95bf162e18824d246c17f39d
tudor-da <tudor.voicu@digitalasset.com>
2021-11-16 15:25:53 +0100

    [Self-service error codes] Enrich migration guide [DPP-755] (#11711)
    * Adapted migration guide to include changes from https://github.com/digital-asset/daml/pull/11686
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Enriched migration guide with Sandbox-classic changed gRPC error codes
    
    * Added migration guide for error codes changed in KV committer
    
    * Extract the KV and Sandbox-classic migration guides
    
    * Hierarchical order of error codes docs
    
    * Added back full grpc error code description to columns
    * Using "gRPC status code" instead of "gRPC error code"
    * Addressed review comments from Hubert
    
    * Apply suggestions from code review
    
    Co-authored-by: Miklos <57664299+miklos-da@users.noreply.github.com>
    
    * Addressed review comments
    * Use 'Error codes' instead of 'Self-service error codes'
    
    Co-authored-by: Miklos <57664299+miklos-da@users.noreply.github.com>

--- ledger/error/src/main/scala/com/daml/error/definitions/LedgerApiErrors.scala
+++ ledger/error/src/main/scala/com/daml/error/definitions/LedgerApiErrors.scala
@@ -711,7 +711,7 @@ object LedgerApiErrors extends LedgerApiErrorGroup {
           id = "DUPLICATE_CONTRACT_KEY",
           ErrorCategory.InvalidGivenCurrentSystemStateResourceExists,
         ) {
-      case class InterpretationReject(
+      case class RejectWithContractKeyArg(
           override val cause: String,
           _key: GlobalKey,
       )(implicit
@@ -726,7 +726,7 @@ object LedgerApiErrors extends LedgerApiErrorGroup {
         )
       }
 
-      case class LedgerReject(override val cause: String)(implicit
+      case class Reject(override val cause: String)(implicit
           loggingContext: ContextualizedErrorLogger
       ) extends LoggingTransactionErrorImpl(cause = cause)
     }
@@ -848,10 +848,15 @@ object LedgerApiErrors extends LedgerApiErrorGroup {
           id = "SUBMITTER_CANNOT_ACT_VIA_PARTICIPANT",
           ErrorCategory.InsufficientPermission,
         ) {
-      case class Reject(
+      case class RejectWithSubmitterAndParticipantId(
           details: String,
-          submitter: String = "N/A",
-          participantId: String = "N/A",
+          submitter: String,
+          participantId: String,
+      )(implicit loggingContext: ContextualizedErrorLogger)
+          extends LoggingTransactionErrorImpl(cause = s"Inconsistent: $details")
+
+      case class Reject(
+          details: String
       )(implicit loggingContext: ContextualizedErrorLogger)
           extends LoggingTransactionErrorImpl(cause = s"Inconsistent: $details")
     }
