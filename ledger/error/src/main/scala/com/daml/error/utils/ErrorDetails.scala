ed9bb709f2b4a5fb82a9b2c5d89a4ec3f2137144
Tudor Voicu <tudor.voicu@digitalasset.com>
2021-11-23 16:29:49 +0100

    Fix tests

--- ledger/error/src/main/scala/com/daml/error/utils/ErrorDetails.scala
+++ ledger/error/src/main/scala/com/daml/error/utils/ErrorDetails.scala
@@ -15,30 +15,27 @@ object ErrorDetails {
   sealed trait ErrorDetail extends Product with Serializable
 
   final case class ResourceInfoDetail(name: String, typ: String) extends ErrorDetail
-  final case class ErrorInfoDetail(reason: String, metadata: Map[String, String] = Map.empty)
+  final case class ErrorInfoDetail(reason: String, metadata: Map[String, String])
       extends ErrorDetail
   final case class RetryInfoDetail(retryDelayInSeconds: Long) extends ErrorDetail
   final case class RequestInfoDetail(requestId: String) extends ErrorDetail
 
-  def from(anys: Seq[protobuf.Any]): Seq[ErrorDetail] = anys.toList.flatMap {
+  def from(anys: Seq[protobuf.Any]): Seq[ErrorDetail] = anys.toList.map {
     case any if any.is(classOf[ResourceInfo]) =>
       val v = any.unpack(classOf[ResourceInfo])
-      List(ResourceInfoDetail(v.getResourceType, v.getResourceName))
+      ResourceInfoDetail(v.getResourceType, v.getResourceName)
 
     case any if any.is(classOf[ErrorInfo]) =>
       val v = any.unpack(classOf[ErrorInfo])
-      val reason = v.getReason
-      if (reason.nonEmpty || v.getMetadataCount > 0)
-        List(ErrorInfoDetail(reason, v.getMetadataMap.asScala.toMap))
-      else Nil
+      ErrorInfoDetail(v.getReason, v.getMetadataMap.asScala.toMap)
 
     case any if any.is(classOf[RetryInfo]) =>
       val v = any.unpack(classOf[RetryInfo])
-      List(RetryInfoDetail(v.getRetryDelay.getSeconds))
+      RetryInfoDetail(v.getRetryDelay.getSeconds)
 
     case any if any.is(classOf[RequestInfo]) =>
       val v = any.unpack(classOf[RequestInfo])
-      List(RequestInfoDetail(v.getRequestId))
+      RequestInfoDetail(v.getRequestId)
 
     case any => throw new IllegalStateException(s"Could not unpack value of: |$any|")
   }
