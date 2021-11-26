d0eba0b082519bdc57e12c96e29ff00470722d3f
Nicu Reut <nicu.reut@digitalasset.com>
2021-11-26 14:39:08 +0100

    Run formatting

--- ledger/participant-state/kvutils/src/test/lib/scala/com/daml/ledger/TestLoggers.scala
+++ ledger/participant-state/kvutils/src/test/lib/scala/com/daml/ledger/TestLoggers.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger
 
 import com.daml.error.{ContextualizedErrorLogger, DamlContextualizedErrorLogger}
