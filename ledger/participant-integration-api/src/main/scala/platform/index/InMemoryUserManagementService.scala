65cb9c1256d7cc7eb1434afef35a06fb6742f5b1
Adriaan Moors <adriaan.moors@digitalasset.com>
2021-12-05 18:26:49 +0100

    userinfo state mgmt more like a concurrent hashmap
    encapsulate synchronization
    synchronize on a private object, not the service (deadlocks)

--- ledger/participant-integration-api/src/main/scala/platform/index/InMemoryUserManagementService.scala
+++ ledger/participant-integration-api/src/main/scala/platform/index/InMemoryUserManagementService.scala
@@ -8,71 +8,86 @@ import com.daml.ledger.participant.state.index.v2.UserManagementService
 import com.daml.ledger.participant.state.index.v2.UserManagementService._
 
 import scala.concurrent.Future
+import scala.collection.mutable
 
 class InMemoryUserManagementService extends UserManagementService {
   import InMemoryUserManagementService._
-
-  @volatile private var state: Map[String, UserInfo] = Map(AdminUser.toStateEntry)
-
-  override def createUser(user: User, rights: Set[UserRight]): Future[Result[Unit]] = synchronized {
-    state.get(user.id) match {
-      case Some(_) => Future.successful(Left(UserExists(user.id)))
-      case None =>
-        state = state + UserInfo(user, rights).toStateEntry
-        Future.successful(Right(()))
+  
+  override def createUser(user: User, rights: Set[UserRight]): Future[Result[Unit]] = Future.successful(
+    putIfAbsent(UserInfo(user, rights)) match {
+      case Some(_) => Left(UserExists(user.id))
+      case None => Right(())
     }
-  }
+  )
 
-  override def getUser(id: String): Future[Result[User]] =
-    Future.successful(
-      state.get(id) match {
-        case Some(userInfo) => Right(userInfo.user)
-        case None => Left(UserNotFound(id))
-      }
-    )
+  override def getUser(id: String): Future[Result[User]] = Future.successful(
+    lookup(id) match {
+      case Some(userInfo) => Right(userInfo.user)
+      case None => Left(UserNotFound(id))
+    }
+  )
 
-  override def deleteUser(id: String): Future[Result[Unit]] = synchronized {
-    Future.successful(state.get(id) match {
-      case Some(_) =>
-        state = state - id
-        Right(())
+  override def deleteUser(id: String): Future[Result[Unit]] = Future.successful(
+    dropExisting(id) match {
+      case Some(_) => Right(())
+      case None => Left(UserNotFound(id))
+    }
+  )
+
+  override def grantRights(id: String, rights: Set[UserRight]): Future[Result[Set[UserRight]]] = Future.successful(
+    lookup(id) match {
+      case Some(userInfo) =>
+        val newRights = rights
+          .filterNot(userInfo.rights)
+        replaceInfo(userInfo, userInfo.copy(rights = userInfo.rights ++ newRights))
+        Right(newRights)
       case None =>
         Left(UserNotFound(id))
-    })
+    }
+  )
+
+  override def revokeRights(id: String, rights: Set[UserRight]): Future[Result[Set[UserRight]]] = Future.successful(
+    lookup(id) match {
+      case Some(userInfo) =>
+        val newRevokeRights = rights
+          .filter(userInfo.rights)
+        replaceInfo(userInfo, userInfo.copy(rights = userInfo.rights -- newRevokeRights))
+        Right(newRevokeRights)
+      case None =>
+        Left(UserNotFound(id))
+    }
+  )
+
+  override def listUserRights(id: String): Future[Result[Set[UserRight]]] = Future.successful(
+    lookup(id) match {
+      case Some(userInfo) => Right(userInfo.rights)
+      case None => Left(UserNotFound(id))
+    }
+  )
+
+  // Underlying map to keep track of UserInfo state, modeled like a concurrent hashmap
+  // structured so we can use a ConcurrentHashMap for real implementations where performance is key
+  private val state: mutable.Map[String, UserInfo] = mutable.Map(AdminUser.toStateEntry)
+  private def lookup(id: String) = state.synchronized {
+    state.get(id)
+  }
+  private def dropExisting(id: String) = state.synchronized {
+    state.get(id).map(_ => state -= id)
+  }
+  private def putIfAbsent(info: UserInfo) = state.synchronized {
+    val old = state.get(info.user.id)
+
+    if (old.isEmpty) state.update(info.user.id, info)
+
+    old
   }
 
-  override def grantRights(id: String, rights: Set[UserRight]): Future[Result[Set[UserRight]]] =
-    synchronized {
-      state.get(id) match {
-        case Some(userInfo) =>
-          val newRights = rights
-            .filterNot(userInfo.rights)
-          state = state + userInfo.copy(rights = userInfo.rights ++ newRights).toStateEntry
-          Future.successful(Right(newRights))
-        case None =>
-          Future.successful(Left(UserNotFound(id)))
-      }
+  private def replaceInfo(oldInfo: UserInfo, newInfo: UserInfo) = state.synchronized {
+    state.get(oldInfo.user.id) match {
+      case Some(`oldInfo`) => state.update(newInfo.user.id, newInfo); true
+      case _ => false
     }
-
-  override def revokeRights(id: String, rights: Set[UserRight]): Future[Result[Set[UserRight]]] =
-    synchronized {
-      state.get(id) match {
-        case Some(userInfo) =>
-          val newRevokeRights = rights
-            .filter(userInfo.rights)
-          state = state + userInfo.copy(rights = userInfo.rights -- newRevokeRights).toStateEntry
-          Future.successful(Right(newRevokeRights))
-        case None =>
-          Future.successful(Left(UserNotFound(id)))
-      }
-    }
-
-  override def listUserRights(id: String): Future[Result[Set[UserRight]]] =
-    state.get(id) match {
-      case Some(userInfo) => Future.successful(Right(userInfo.rights))
-      case None => Future.successful(Left(UserNotFound(id)))
-    }
-
+  }
 }
 
 object InMemoryUserManagementService {
