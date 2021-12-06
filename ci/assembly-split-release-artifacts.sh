886b98afdfb7d8ca1ebbb5451045cb01f8d0ceb6
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-12-06 12:57:14 +0100

    Split release process (PoC)
    This PR adds the necessary infrastructure for the new split release
    process. Releases are still triggered via the LATEST file but you can
    choose between the old and the new release process by adding
    SPLIT-RELEASE at the end of the line.
    
    As a first step this publishes all artifacts we currently publish to
    Github releases to our GCP bucket.
    
    changelog_begin
    changelog_end

---
+++ ci/assembly-split-release-artifacts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+set -euo pipefail
+
+RELEASE_TAG=$1
+SOURCE_DIR=$2
+OUTPUT_DIR=$3
+
+mkdir -p $OUTPUT_DIR/github
+
+for file in $SOURCE_DIR/github/*; do
+    # Copy as a placeholder for potential tweaks we might want to do here.
+    cp $file $OUTPUT_DIR/github
+done
