bfd83f74c0782f7d67c035352e574c515d89b34a
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-12-08 10:20:27 +0100

    Drop UserId domain datatype
    changelog_begin
    changelog_end

--- ledger/ledger-api-domain/src/main/scala/com/digitalasset/ledger/api/domain.scala
+++ ledger/ledger-api-domain/src/main/scala/com/digitalasset/ledger/api/domain.scala
@@ -299,13 +299,6 @@ object domain {
   type ApplicationId = Ref.ApplicationId @@ ApplicationIdTag
   val ApplicationId: Tag.TagOf[ApplicationIdTag] = Tag.of[ApplicationIdTag]
 
-  /** Identifiers for participant node users interacting with the Ledger API.
-    *
-    * These are a subset of [[ApplicationId]]. See [[Ref.UserId]] for details.
-    */
-  type UserId = Ref.UserId @@ ApplicationIdTag
-  val UserId: Tag.TagOf[ApplicationIdTag] = Tag.of[ApplicationIdTag]
-
   sealed trait SubmissionIdTag
 
   type SubmissionId = Ref.SubmissionId @@ SubmissionIdTag
@@ -403,7 +396,7 @@ object domain {
   }
 
   final case class User(
-      id: UserId,
+      id: Ref.UserId,
       primaryParty: Option[Ref.Party],
   )
 
