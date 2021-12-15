9d00861b4ef05dcded5d68c82b45d58accbd8395
Samir Talwar <samir.talwar@digitalasset.com>
2021-12-15 21:42:06 +0100

    resources-akka: Wait for the bounded source queue to finish. (#12142)
    * resources-akka: Wait for the bounded source queue to finish.
    
    Otherwise, we may get submissions after dependencies have shut down.
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * resources-akka: Simplify the interface and use clearer type param names.

--- libs-scala/resources-akka/src/main/scala/com/digitalasset/resources/akka/AkkaResourceOwnerFactories.scala
+++ libs-scala/resources-akka/src/main/scala/com/digitalasset/resources/akka/AkkaResourceOwnerFactories.scala
@@ -4,9 +4,11 @@
 package com.daml.resources.akka
 
 import akka.actor.{ActorSystem, Cancellable}
-import akka.stream.{BoundedSourceQueue, Materializer}
 import akka.stream.scaladsl.RunnableGraph
+import akka.stream.{BoundedSourceQueue, Materializer}
 import com.daml.resources.{AbstractResourceOwner, HasExecutionContext}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 trait AkkaResourceOwnerFactories[Context] {
   protected implicit val hasExecutionContext: HasExecutionContext[Context]
@@ -28,8 +30,14 @@ trait AkkaResourceOwnerFactories[Context] {
   def forCancellable[C <: Cancellable](acquire: () => C): AbstractResourceOwner[Context, C] =
     new CancellableResourceOwner(acquire)
 
-  def forBoundedSourceQueue[T](
-      queueGraph: RunnableGraph[BoundedSourceQueue[T]]
-  )(implicit materializer: Materializer): AbstractResourceOwner[Context, BoundedSourceQueue[T]] =
-    new BoundedSourceQueueResourceOwner[T, Context](queueGraph)
+  def forBoundedSourceQueue[T, U](
+      queueGraph: RunnableGraph[(BoundedSourceQueue[T], Future[U])]
+  )(implicit
+      materializer: Materializer
+  ): AbstractResourceOwner[Context, (BoundedSourceQueue[T], Future[U])] =
+    new BoundedSourceQueueResourceOwner[(BoundedSourceQueue[T], Future[U]), T, Context](
+      queueGraph = queueGraph,
+      toSourceQueue = _._1,
+      toDone = _._2.map(_ => ())(ExecutionContext.parasitic),
+    )
 }
