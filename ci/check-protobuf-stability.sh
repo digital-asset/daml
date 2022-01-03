5ba013e7cbf888c3ed1de7fb26158ce5a67e6e17
Hubert Slojewski <hubert.slojewski@digitalasset.com>
2022-01-03 10:25:15 +0100

    Fix error handling
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ci/check-protobuf-stability.sh
+++ ci/check-protobuf-stability.sh
@@ -104,7 +104,7 @@ USAGE
     readonly VERSION="${TARGET#release/}"
     readonly VERSION_PREFIX="${VERSION%.x}"
     readonly STABLE_TAGS=($(git tag | grep "v.*" | grep -v "snapshot" | sort -V))
-    LATEST_STABLE_TAG="$(for TAG in ${STABLE_TAGS[@]}; do version_lte "${TAG#v}" "${VERSION_PREFIX}.999" && echo "$TAG"; done | tail -1)"
+    LATEST_STABLE_TAG="$(for TAG in "${STABLE_TAGS[@]}"; do version_lte "${TAG#v}" "${VERSION_PREFIX}.999" && echo "$TAG" || :; done | tail -1)"
   else
     LATEST_STABLE_TAG="$(git tag | grep "v.*" | grep -v "snapshot" | sort -V | tail -1)"
   fi
