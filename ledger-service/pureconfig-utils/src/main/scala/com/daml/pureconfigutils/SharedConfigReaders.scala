8516fe5e0813790bc8265dc67f75ff7f160b851b
Akshay <akshay.shirahatti@digitalasset.com>
2022-01-07 15:27:34 +0000

    changes to the JwtVerifierBase config reader and equivalent test

--- ledger-service/pureconfig-utils/src/main/scala/com/daml/pureconfigutils/SharedConfigReaders.scala
+++ ledger-service/pureconfig-utils/src/main/scala/com/daml/pureconfigutils/SharedConfigReaders.scala
@@ -6,13 +6,21 @@ package com.daml.pureconfigutils
 import akka.http.scaladsl.model.Uri
 import com.auth0.jwt.algorithms.Algorithm
 import com.daml.dbutils.JdbcConfig
-import com.daml.jwt.{ECDSAVerifier, HMAC256Verifier, JwksVerifier, JwtVerifierBase, RSA256Verifier}
+import com.daml.jwt.{
+  ECDSAVerifier,
+  HMAC256Verifier,
+  JwksVerifier,
+  JwtVerifier,
+  JwtVerifierBase,
+  RSA256Verifier,
+}
 import com.daml.ledger.api.tls.TlsConfiguration
 import com.daml.metrics.MetricsReporter
 import com.daml.platform.services.time.TimeProviderType
-import pureconfig.error.{CannotConvert, FailureReason}
-import pureconfig.{ConfigReader, ConvertHelpers}
+import pureconfig.error.{CannotConvert, ConvertFailure, FailureReason}
+import pureconfig.{ConfigObjectCursor, ConfigReader, ConvertHelpers}
 import pureconfig.generic.semiauto.deriveReader
+import scalaz.{-\/, \/, \/-}
 
 import java.nio.file.Path
 import java.io.File
@@ -35,6 +43,48 @@ final case class LedgerApiConfig(
 )
 final case class MetricsConfig(reporter: MetricsReporter, reportingInterval: FiniteDuration)
 
+object TokenVerifierConfig {
+  val knownTokenVerifiers =
+    List("hs256-unsafe", "rs256-crt", "es256-crt", "es512-crt", "rs256-jwks")
+
+  def extractByType(
+      typeStr: String,
+      valueStr: String,
+      objectCursor: ConfigObjectCursor,
+  ): ConfigReader.Result[JwtVerifierBase] = {
+    def convertFailure(msg: String) = {
+      ConfigReader.Result.fail(
+        ConvertFailure(
+          CannotConvert(typeStr, "JwtVerifier", msg),
+          objectCursor,
+        )
+      )
+    }
+    if (!knownTokenVerifiers.contains(typeStr)) {
+      convertFailure(s"value not one of ${knownTokenVerifiers.drop(1).mkString(", ")}")
+    } else {
+      val result: JwtVerifier.Error \/ JwtVerifierBase = typeStr match {
+        case "hs256-unsafe" =>
+          HMAC256Verifier(valueStr)
+        case "rs256-crt" =>
+          RSA256Verifier
+            .fromCrtFile(valueStr)
+        case "es256-crt" =>
+          ECDSAVerifier
+            .fromCrtFile(valueStr, Algorithm.ECDSA256(_, null))
+        case "es512-crt" =>
+          ECDSAVerifier
+            .fromCrtFile(valueStr, Algorithm.ECDSA512(_, null))
+        case "rs256-jwks" =>
+          \/.attempt(JwksVerifier(valueStr))(e => JwtVerifier.Error(Symbol("RS256"), e.getMessage))
+      }
+      result match {
+        case -\/(err) => convertFailure(s"Failed to create $typeStr verifier: ${err}")
+        case \/-(res) => Right(res)
+      }
+    }
+  }
+}
 object SharedConfigReaders {
 
   def catchConvertError[A, B](f: String => Either[String, B])(implicit
@@ -42,29 +92,16 @@ object SharedConfigReaders {
   ): String => Either[FailureReason, B] =
     s => f(s).left.map(CannotConvert(s, B.toString, _))
 
-  implicit val tokenVerifierReader: ConfigReader[JwtVerifierBase] =
-    ConfigReader.forProduct2[JwtVerifierBase, String, String]("type", "uri") {
-      case (t: String, p: String) =>
-        // hs256-unsafe, rs256-crt, es256-crt, es512-crt, rs256-jwks
-        t match {
-          case "hs256-unsafe" =>
-            HMAC256Verifier(p)
-              .valueOr(err => sys.error(s"Failed to create HMAC256 verifier: $err"))
-          case "rs256-crt" =>
-            RSA256Verifier
-              .fromCrtFile(p)
-              .valueOr(err => sys.error(s"Failed to create RSA256 verifier: $err"))
-          case "es256-crt" =>
-            ECDSAVerifier
-              .fromCrtFile(p, Algorithm.ECDSA256(_, null))
-              .valueOr(err => sys.error(s"Failed to create ECDSA256 verifier: $err"))
-          case "es512-crt" =>
-            ECDSAVerifier
-              .fromCrtFile(p, Algorithm.ECDSA512(_, null))
-              .valueOr(err => sys.error(s"Failed to create ECDSA512 verifier: $err"))
-          case "rs256-jwks" =>
-            JwksVerifier(p)
-        }
+  implicit val tokenVerifierCfgRead: ConfigReader[JwtVerifierBase] =
+    ConfigReader.fromCursor { cur =>
+      for {
+        objCur <- cur.asObjectCursor
+        typeCur <- objCur.atKey("type")
+        typeStr <- typeCur.asString
+        valueCur <- objCur.atKey("uri")
+        valueStr <- valueCur.asString
+        ident <- TokenVerifierConfig.extractByType(typeStr, valueStr, objCur)
+      } yield ident
     }
 
   implicit val uriCfgReader: ConfigReader[Uri] =
