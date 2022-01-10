db909b2e8a2ce4a45217f83cda70240167116a26
Marton Nagy <marton.nagy@digitalasset.com>
2022-01-10 17:10:59 +0100

    Switch to H2 BINARY LARGE OBJECT to work around a limitation with BYTEA

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/h2/H2Field.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/h2/H2Field.scala
@@ -3,6 +3,8 @@
 
 package com.daml.platform.store.backend.h2
 
+import java.io.{ByteArrayInputStream, InputStream}
+
 import com.daml.platform.store.backend.common.Field
 import com.daml.platform.store.interning.StringInterning
 
@@ -16,4 +18,16 @@ private[h2] case class IntArrayOptional[FROM](
 ) extends Field[FROM, Option[Iterable[Int]], Array[java.lang.Integer]] {
   override def convert: Option[Iterable[Int]] => Array[java.lang.Integer] =
     _.map(_.view.map(Int.box).toArray).orNull
+}
+
+private[h2] case class H2Bytea[FROM](extract: StringInterning => FROM => Array[Byte])
+    extends Field[FROM, Array[Byte], InputStream] {
+  override def convert: Array[Byte] => InputStream = new ByteArrayInputStream(_)
+}
+
+private[h2] case class H2ByteaOptional[FROM](
+    extract: StringInterning => FROM => Option[Array[Byte]]
+) extends Field[FROM, Option[Array[Byte]], InputStream] {
+  override def convert: Option[Array[Byte]] => InputStream =
+    _.map(new ByteArrayInputStream(_)).orNull
 }
