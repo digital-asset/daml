864b12f0da8f1693e9b5ca7c2d1edf675bac1e15
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-01-11 14:40:49 +0100

    adjust ledger api test tool
    changelog_begin
    changelog_end

--- ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/CommandServiceIT.scala
+++ ledger/ledger-api-test-tool/src/main/scala/com/daml/ledger/api/testtool/suites/CommandServiceIT.scala
@@ -350,7 +350,7 @@ final class CommandServiceIT extends LedgerTestSuite {
         LedgerApiErrors.CommandExecution.Interpreter.GenericInterpretationError,
         Some(
           Pattern.compile(
-            "Interpretation error: Error: (User abort: Assertion failed.?|Unhandled exception: [0-9a-zA-Z\\.:]*@[0-9a-f]*\\{ message = \"Assertion failed\" \\}\\. [Dd]etails(: |=)Last location: \\[[^\\]]*\\], partial transaction: root node)"
+            "Interpretation error: Error: (User abort: Assertion failed.?|Unhandled (Daml )?exception: [0-9a-zA-Z\\.:]*@[0-9a-f]*\\{ message = \"Assertion failed\" \\}\\. [Dd]etails(: |=)Last location: \\[[^\\]]*\\], partial transaction: root node)"
           )
         ),
         checkDefiniteAnswerMetadata = true,
