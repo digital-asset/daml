864ef10363dce4dfa09c59a1db9ca84f299ef4c1
Tudor Voicu <tudor.voicu@digitalasset.com>
2022-01-11 09:48:14 +0100

    TagWithLedgerEndSpec
    changelog_begin
    changelog_end

---
+++ ledger/sandbox-on-x/src/test/suite/scala/com/daml/ledger/sandbox/bridge/validate/TagWithLedgerEndSpec.scala
@@ -0,0 +1,53 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.sandbox.bridge.validate
+
+import com.codahale.metrics.MetricRegistry
+import com.daml.ledger.api.domain
+import com.daml.ledger.offset.Offset
+import com.daml.ledger.participant.state.index.v2.IndexService
+import com.daml.ledger.sandbox.bridge.{BridgeMetrics, PreparedSubmission}
+import com.daml.ledger.sandbox.domain.Submission
+import com.daml.lf.data.Ref
+import com.daml.logging.LoggingContext
+import com.daml.metrics.Metrics
+import org.mockito.MockitoSugar
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.chaining._
+
+class TagWithLedgerEndSpec extends AnyFlatSpec with Matchers with MockitoSugar {
+  behavior of classOf[TagWithLedgerEndImpl].getSimpleName
+
+  private implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+  private implicit val loggingContext: LoggingContext = LoggingContext.ForTesting
+
+  "tagWithLedgerEnd" should "tag the incoming submissions with the index service ledger end" in {
+    val indexServiceMock = mock[IndexService]
+    val tagWithLedgerEnd = new TagWithLedgerEndImpl(
+      indexServiceMock,
+      bridgeMetrics = new BridgeMetrics(new Metrics(new MetricRegistry)),
+    )
+    val offsetString = Ref.HexString.assertFromString("ab")
+    val expectedOffset = Offset.fromHexString(offsetString)
+    val indexServiceProvidedOffset = domain.LedgerOffset.Absolute(offsetString)
+
+    val somePreparedSubmission =
+      mock[PreparedSubmission]
+        .tap { preparedSubmission =>
+          val someSubmission = mock[Submission].tap { submission =>
+            when(submission.loggingContext).thenReturn(loggingContext)
+          }
+          when(preparedSubmission.submission).thenReturn(someSubmission)
+        }
+
+    when(indexServiceMock.currentLedgerEnd())
+      .thenReturn(Future.successful(indexServiceProvidedOffset))
+
+    tagWithLedgerEnd(Right(somePreparedSubmission))
+      .map(_ shouldBe Right(expectedOffset -> somePreparedSubmission))
+  }
+}
