c94dea40ed420c220ab7881fb1da77bfcb6a099f
Tudor Voicu <tudor.voicu@digitalasset.com>
2022-01-12 19:38:57 +0100

    [Sandbox-on-X unit test] SequencerStateSpec implementation * Simplification of dequeue in SequencerState
    changelog_begin
    changelog_end

--- ledger/sandbox-on-x/src/main/scala/com/daml/ledger/sandbox/bridge/validate/SequencerState.scala
+++ ledger/sandbox-on-x/src/main/scala/com/daml/ledger/sandbox/bridge/validate/SequencerState.scala
@@ -14,68 +14,67 @@ import scala.util.chaining._
 
 /** State of the sequencing conflict checking stage
   *
-  * @param sequencerQueue A queue ordered by each transaction's `noConflictUpTo`
+  * @param sequencerQueue A queue ordered by each transaction's offset.
   * @param keyState Mapping of all key updates occurred over the span of the `sequencerQueue` coupled
   *                 with the transaction offset that last updated them.
-  * @param consumedContractsState Map of all consumed contracts over the span of the `sequencerQueue`..
+  * @param consumedContractsState Map of all consumed contracts over the span of the `sequencerQueue`.
   */
 case class SequencerState(
-    sequencerQueue: SequencerQueue = Vector.empty,
-    keyState: Map[Key, (Option[ContractId], LastUpdatedAt)] = Map.empty,
-    consumedContractsState: Set[ContractId] = Set.empty,
-)(bridgeMetrics: BridgeMetrics) {
+    private[validate] val sequencerQueue: SequencerQueue = Vector.empty,
+    private[validate] val keyState: Map[Key, (Option[ContractId], LastUpdatedAt)] = Map.empty,
+    private[validate] val consumedContractsState: Set[ContractId] = Set.empty,
+)(implicit bridgeMetrics: BridgeMetrics) {
 
   def enqueue(
       offset: Offset,
       updatedKeys: Map[Key, Option[ContractId]],
       consumedContracts: Set[ContractId],
   ): SequencerState =
-    if (sequencerQueue.lastOption.exists(_._1 > offset))
+    if (sequencerQueue.lastOption.exists(_._1 >= offset))
       throw new RuntimeException(
-        s"Offset to be enqueued ($offset) is smaller than the last enqueued offset (${sequencerQueue.last._1})"
+        s"Offset to be enqueued ($offset) is not higher than the last enqueued offset (${sequencerQueue.last._1})"
       )
     else
       SequencerState(
         sequencerQueue = sequencerQueue :+ (offset -> (updatedKeys, consumedContracts)),
         keyState = keyState ++ updatedKeys.view.mapValues(_ -> offset),
         consumedContractsState = consumedContractsState ++ consumedContracts,
-      )(bridgeMetrics)
-        .tap(newState => {
-          bridgeMetrics.SequencerState.sequencerQueueLength
-            .update(newState.sequencerQueue.length)
-          bridgeMetrics.SequencerState.keyStateSize.update(newState.keyState.size)
-          bridgeMetrics.SequencerState.consumedContractsStateSize
-            .update(newState.consumedContractsState.size)
-        })
+      ).tap(updateMetrics)
 
-  def dequeue(upToOffset: Offset): SequencerState = {
-    val pruneAfter = sequencerQueue.view.map(_._1).search(upToOffset) match {
+  def dequeue(noConflictUpTo: Offset): SequencerState = {
+    val pruneBefore = sequencerQueue.view.map(_._1).search(noConflictUpTo) match {
       case Searching.Found(foundIndex) => foundIndex + 1
       case Searching.InsertionPoint(insertionPoint) => insertionPoint
     }
 
-    val (evictedEntries, prunedQueue) = sequencerQueue.splitAt(pruneAfter)
+    val (evictedQueueEntries, prunedQueue) = sequencerQueue.splitAt(pruneBefore)
 
-    val prunedKeyState =
-      evictedEntries.iterator
-        .flatMap { case (onConflictUpTo, (updatedKeys, _)) =>
-          updatedKeys.iterator.map { case (key, _) => onConflictUpTo -> key }
+    val keyStateEntriesToBeEvicted =
+      evictedQueueEntries.iterator
+        .flatMap { case (_, (updatedKeys, _)) => updatedKeys.keySet }
+        .filter { key =>
+          val (_, lastUpdatedAt) = keyState(key)
+          lastUpdatedAt <= noConflictUpTo
         }
-        .foldLeft(keyState) { case (updatedKeyState, (noConflictUpTo, key)) =>
-          updatedKeyState.get(key).fold(throw new RuntimeException("Should not be missing")) {
-            case (_, offset) if noConflictUpTo == offset => updatedKeyState - key
-            case _ => updatedKeyState
-          }
-        }
+        .toSet
+
+    val prunedKeyState = keyState -- keyStateEntriesToBeEvicted
 
     val prunedConsumedContractsState =
-      consumedContractsState.diff(evictedEntries.iterator.flatMap(_._2._2).toSet)
+      consumedContractsState.diff(evictedQueueEntries.iterator.flatMap(_._2._2).toSet)
 
     SequencerState(
       sequencerQueue = prunedQueue,
       keyState = prunedKeyState,
       consumedContractsState = prunedConsumedContractsState,
-    )(bridgeMetrics)
+    )
+  }
+
+  private def updateMetrics(newState: SequencerState): Unit = {
+    val stateMetrics = bridgeMetrics.SequencerState
+    stateMetrics.sequencerQueueLength.update(newState.sequencerQueue.length)
+    stateMetrics.keyStateSize.update(newState.keyState.size)
+    stateMetrics.consumedContractsStateSize.update(newState.consumedContractsState.size)
   }
 }
 
