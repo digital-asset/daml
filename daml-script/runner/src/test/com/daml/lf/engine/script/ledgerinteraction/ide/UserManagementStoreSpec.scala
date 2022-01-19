35ef23c8d619b3324e481d01610988a164c0eb30
Nick Chapman <nick.chapman@digitalasset.com>
2022-01-19 09:49:57 +0000

    meh... done over by scala's poor excuse for a type system

--- daml-script/runner/src/test/com/daml/lf/engine/script/ledgerinteraction/ide/UserManagementStoreSpec.scala
+++ daml-script/runner/src/test/com/daml/lf/engine/script/ledgerinteraction/ide/UserManagementStoreSpec.scala
@@ -25,60 +25,60 @@ final class InMemoryUserManagementStoreSpec extends AnyFreeSpec with Matchers {
   "in-memory user management" - {
     "allow creating a fresh user" in {
       val mgmt = new UserManagementStore()
-      mgmt.createUser(User("user1", None), Set.empty) shouldBe Right(())
-      mgmt.createUser(User("user2", None), Set.empty) shouldBe Right(())
+      mgmt.createUser(User("user1", None), Set.empty) shouldBe Some(())
+      mgmt.createUser(User("user2", None), Set.empty) shouldBe Some(())
     }
 
     "disallow re-creating an existing user" in {
       val mgmt = new UserManagementStore()
       val user = User("user1", None)
-      mgmt.createUser(user, Set.empty) shouldBe Right(())
-      mgmt.createUser(user, Set.empty) shouldBe Left(())
+      mgmt.createUser(user, Set.empty) shouldBe Some(())
+      mgmt.createUser(user, Set.empty) shouldBe None
     }
 
     "find a freshly created user" in {
       val mgmt = new UserManagementStore()
       val user = User("user1", None)
-      mgmt.createUser(user, Set.empty) shouldBe Right(())
-      mgmt.getUser("user1") shouldBe Right(user)
+      mgmt.createUser(user, Set.empty) shouldBe Some(())
+      mgmt.getUser("user1") shouldBe Some(user)
     }
 
     "not find a non-existent user" in {
       val mgmt = new UserManagementStore()
-      mgmt.getUser("user1") shouldBe Left(())
+      mgmt.getUser("user1") shouldBe None
     }
     "not find a deleted user" in {
       val mgmt = new UserManagementStore()
       val user = User("user1", None)
-      mgmt.createUser(user, Set.empty) shouldBe Right(())
-      mgmt.getUser("user1") shouldBe Right(user)
-      mgmt.deleteUser("user1") shouldBe Right(())
-      mgmt.getUser("user1") shouldBe Left(())
+      mgmt.createUser(user, Set.empty) shouldBe Some(())
+      mgmt.getUser("user1") shouldBe Some(user)
+      mgmt.deleteUser("user1") shouldBe Some(())
+      mgmt.getUser("user1") shouldBe None
     }
     "allow recreating a deleted user" in {
       val mgmt = new UserManagementStore()
       val user = User("user1", None)
-      mgmt.createUser(user, Set.empty) shouldBe Right(())
-      mgmt.deleteUser(user.id) shouldBe Right(())
-      mgmt.createUser(user, Set.empty) shouldBe Right(())
+      mgmt.createUser(user, Set.empty) shouldBe Some(())
+      mgmt.deleteUser(user.id) shouldBe Some(())
+      mgmt.createUser(user, Set.empty) shouldBe Some(())
     }
     "fail to delete a non-existent user" in {
       val mgmt = new UserManagementStore()
-      mgmt.deleteUser("user1") shouldBe Left(())
+      mgmt.deleteUser("user1") shouldBe None
     }
     "list created users" in {
       val mgmt = new UserManagementStore()
-      mgmt.createUser(User("user1", None), Set.empty) shouldBe Right(())
-      mgmt.createUser(User("user2", None), Set.empty) shouldBe Right(())
-      mgmt.listUsers() shouldBe Right(Seq(User("user1", None), User("user2", None)))
+      mgmt.createUser(User("user1", None), Set.empty) shouldBe Some(())
+      mgmt.createUser(User("user2", None), Set.empty) shouldBe Some(())
+      mgmt.listUsers() shouldBe Seq(User("user1", None), User("user2", None))
     }
     "not list deleted users" in {
       val mgmt = new UserManagementStore()
-      mgmt.createUser(User("user1", None), Set.empty) shouldBe Right(())
-      mgmt.createUser(User("user2", None), Set.empty) shouldBe Right(())
-      mgmt.listUsers() shouldBe Right(Seq(User("user1", None), User("user2", None)))
-      mgmt.deleteUser("user1") shouldBe Right(())
-      mgmt.listUsers() shouldBe Right(Seq(User("user2", None)))
+      mgmt.createUser(User("user1", None), Set.empty) shouldBe Some(())
+      mgmt.createUser(User("user2", None), Set.empty) shouldBe Some(())
+      mgmt.listUsers() shouldBe Seq(User("user1", None), User("user2", None))
+      mgmt.deleteUser("user1") shouldBe Some(())
+      mgmt.listUsers() shouldBe Seq(User("user2", None))
     }
   }
 
@@ -90,56 +90,56 @@ final class InMemoryUserManagementStoreSpec extends AnyFreeSpec with Matchers {
     import UserRight._
     "listUserRights should find the rights of a freshly created user" in {
       val mgmt = new UserManagementStore()
-      mgmt.createUser(User("user1", None), Set.empty) shouldBe Right(())
-      mgmt.listUserRights("user1") shouldBe Right(Set.empty)
+      mgmt.createUser(User("user1", None), Set.empty) shouldBe Some(())
+      mgmt.listUserRights("user1") shouldBe Some(Set.empty)
       mgmt.createUser(
         User("user2", None),
         Set(ParticipantAdmin, CanActAs("party1"), CanReadAs("party2")),
-      ) shouldBe Right(())
-      mgmt.listUserRights("user2") shouldBe Right(
+      ) shouldBe Some(())
+      mgmt.listUserRights("user2") shouldBe Some(
         Set(ParticipantAdmin, CanActAs("party1"), CanReadAs("party2"))
       )
     }
     "listUserRights should fail on non-existent user" in {
       val mgmt = new UserManagementStore()
-      mgmt.listUserRights("user1") shouldBe Left(())
+      mgmt.listUserRights("user1") shouldBe None
     }
     "grantUserRights should add new rights" in {
       val mgmt = new UserManagementStore()
-      mgmt.createUser(User("user1", None), Set.empty) shouldBe Right(())
-      mgmt.grantRights("user1", Set(ParticipantAdmin)) shouldBe Right(Set(ParticipantAdmin))
-      mgmt.grantRights("user1", Set(ParticipantAdmin)) shouldBe Right(Set.empty)
-      mgmt.grantRights("user1", Set(CanActAs("party1"), CanReadAs("party2"))) shouldBe Right(
+      mgmt.createUser(User("user1", None), Set.empty) shouldBe Some(())
+      mgmt.grantRights("user1", Set(ParticipantAdmin)) shouldBe Some(Set(ParticipantAdmin))
+      mgmt.grantRights("user1", Set(ParticipantAdmin)) shouldBe Some(Set.empty)
+      mgmt.grantRights("user1", Set(CanActAs("party1"), CanReadAs("party2"))) shouldBe Some(
         Set(CanActAs("party1"), CanReadAs("party2"))
       )
-      mgmt.listUserRights("user1") shouldBe Right(
+      mgmt.listUserRights("user1") shouldBe Some(
         Set(ParticipantAdmin, CanActAs("party1"), CanReadAs("party2"))
       )
     }
     "grantRights should fail on non-existent user" in {
       val mgmt = new UserManagementStore()
-      mgmt.grantRights("user1", Set.empty) shouldBe Left(())
+      mgmt.grantRights("user1", Set.empty) shouldBe None
     }
     "revokeRights should revoke rights" in {
       val mgmt = new UserManagementStore()
       mgmt.createUser(
         User("user1", None),
         Set(ParticipantAdmin, CanActAs("party1"), CanReadAs("party2")),
-      ) shouldBe Right(())
-      mgmt.listUserRights("user1") shouldBe Right(
+      ) shouldBe Some(())
+      mgmt.listUserRights("user1") shouldBe Some(
         Set(ParticipantAdmin, CanActAs("party1"), CanReadAs("party2"))
       )
-      mgmt.revokeRights("user1", Set(ParticipantAdmin)) shouldBe Right(Set(ParticipantAdmin))
-      mgmt.revokeRights("user1", Set(ParticipantAdmin)) shouldBe Right(Set.empty)
-      mgmt.listUserRights("user1") shouldBe Right(Set(CanActAs("party1"), CanReadAs("party2")))
-      mgmt.revokeRights("user1", Set(CanActAs("party1"), CanReadAs("party2"))) shouldBe Right(
+      mgmt.revokeRights("user1", Set(ParticipantAdmin)) shouldBe Some(Set(ParticipantAdmin))
+      mgmt.revokeRights("user1", Set(ParticipantAdmin)) shouldBe Some(Set.empty)
+      mgmt.listUserRights("user1") shouldBe Some(Set(CanActAs("party1"), CanReadAs("party2")))
+      mgmt.revokeRights("user1", Set(CanActAs("party1"), CanReadAs("party2"))) shouldBe Some(
         Set(CanActAs("party1"), CanReadAs("party2"))
       )
-      mgmt.listUserRights("user1") shouldBe Right(Set.empty)
+      mgmt.listUserRights("user1") shouldBe Some(Set.empty)
     }
     "revokeRights should fail on non-existent user" in {
       val mgmt = new UserManagementStore()
-      mgmt.revokeRights("user1", Set.empty) shouldBe Left(())
+      mgmt.revokeRights("user1", Set.empty) shouldBe None
     }
   }
 }
