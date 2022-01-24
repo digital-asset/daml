c706bcdc47c15df42ef886162d03f1e94b4d0ead
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-01-24 21:00:39 +0100

    .
    changelog_begin
    changelog_end

--- debug-ghci/Lib.hs
+++ debug-ghci/Lib.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Lib where
 
-foreign import ccall "grpc_completion_queue_next42"
+foreign import ccall "grpc_completion_queue_next"
     sendMessageTimeout :: IO ()
--- debug-ghci/Main.hs
+++ debug-ghci/Main.hs
@@ -1,4 +1,6 @@
 module Main where
 
+import Lib
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = sendMessageTimeout
