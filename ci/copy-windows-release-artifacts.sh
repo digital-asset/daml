a1b05ec042101ea2a347de8affbd486d5e272343
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-01-25 15:51:56 +0100

    split: do not push exe
    In the split release process, the executable installer has to be created
    later in the pipeline, once we have the Canton parts available.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ci/copy-windows-release-artifacts.sh
+++ ci/copy-windows-release-artifacts.sh
@@ -5,34 +5,38 @@ set -eou pipefail
 
 RELEASE_TAG=$1
 OUTPUT_DIR=$2
+SPLIT_RELEASE=${3:-false}
 
 mkdir -p $OUTPUT_DIR/github
 mkdir -p $OUTPUT_DIR/artifactory
 mkdir -p $OUTPUT_DIR/split-release
-INSTALLER="$OUTPUT_DIR/github/daml-sdk-$RELEASE_TAG-windows.exe"
-EE_INSTALLER="$OUTPUT_DIR/artifactory/daml-sdk-$RELEASE_TAG-windows-ee.exe"
-mv "bazel-bin/release/windows-installer/daml-sdk-installer-ce.exe" "$INSTALLER"
-mv "bazel-bin/release/windows-installer/daml-sdk-installer-ee.exe" "$EE_INSTALLER"
-chmod +wx "$INSTALLER"
-chmod +wx "$EE_INSTALLER"
 
-if ! [ -f /C/Users/u/.dotnet/tools/azuresigntool.exe ]; then
-    "/C/Program Files/dotnet/dotnet.exe" tool install --global AzureSignTool --version 3.0.0
+if [ "$SPLIT_RELEASE" = "false" ]; then
+    INSTALLER="$OUTPUT_DIR/github/daml-sdk-$RELEASE_TAG-windows.exe"
+    EE_INSTALLER="$OUTPUT_DIR/artifactory/daml-sdk-$RELEASE_TAG-windows-ee.exe"
+    mv "bazel-bin/release/windows-installer/daml-sdk-installer-ce.exe" "$INSTALLER"
+    mv "bazel-bin/release/windows-installer/daml-sdk-installer-ee.exe" "$EE_INSTALLER"
+    chmod +wx "$INSTALLER"
+    chmod +wx "$EE_INSTALLER"
+
+    if ! [ -f /C/Users/u/.dotnet/tools/azuresigntool.exe ]; then
+        "/C/Program Files/dotnet/dotnet.exe" tool install --global AzureSignTool --version 3.0.0
+    fi
+
+    /C/Users/u/.dotnet/tools/azuresigntool.exe sign \
+      --azure-key-vault-url "$AZURE_KEY_VAULT_URL" \
+      --azure-key-vault-client-id "$AZURE_CLIENT_ID" \
+      --azure-key-vault-tenant-id "$AZURE_TENANT_ID" \
+      --azure-key-vault-client-secret "$AZURE_CLIENT_SECRET" \
+      --azure-key-vault-certificate "$AZURE_KEY_VAULT_CERTIFICATE" \
+      --description "Daml SDK installer" \
+      --description-url "https://daml.com" \
+      --timestamp-rfc3161 "http://timestamp.digicert.com" \
+      --file-digest sha384 \
+      --verbose \
+      "$INSTALLER" \
+      "$EE_INSTALLER"
 fi
-
-/C/Users/u/.dotnet/tools/azuresigntool.exe sign \
-  --azure-key-vault-url "$AZURE_KEY_VAULT_URL" \
-  --azure-key-vault-client-id "$AZURE_CLIENT_ID" \
-  --azure-key-vault-tenant-id "$AZURE_TENANT_ID" \
-  --azure-key-vault-client-secret "$AZURE_CLIENT_SECRET" \
-  --azure-key-vault-certificate "$AZURE_KEY_VAULT_CERTIFICATE" \
-  --description "Daml SDK installer" \
-  --description-url "https://daml.com" \
-  --timestamp-rfc3161 "http://timestamp.digicert.com" \
-  --file-digest sha384 \
-  --verbose \
-  "$INSTALLER" \
-  "$EE_INSTALLER"
 
 TARBALL=daml-sdk-$RELEASE_TAG-windows.tar.gz
 EE_TARBALL=daml-sdk-$RELEASE_TAG-windows-ee.tar.gz
