92938d5e764436e077aef7dc708b28597be3f24c
tudor-da <tudor.voicu@digitalasset.com>
2022-01-31 18:04:58 +0100

    Moved com.daml.platform.sandbox to com.daml.ledger.sandbox in SoX (#12675)
    * Moved com.daml.platform.sandbox to com.daml.ledger.sandbox in SoX
    
    changelog_begin
    changelog_end
    
    * Reordered imports

--- ledger/sandbox-on-x/src/test/lib/scala/com/daml/platform/sandbox/fixture/SandboxFixture.scala
+++ ledger/sandbox-on-x/src/test/lib/scala/com/daml/platform/sandbox/fixture/SandboxFixture.scala
@@ -5,14 +5,15 @@ package com.daml.platform.sandbox.fixture
 
 import com.daml.ledger.api.testing.utils.{OwnedResource, Resource, SuiteResource}
 import com.daml.ledger.resources.{ResourceContext, ResourceOwner}
+import com.daml.ledger.sandbox.SandboxServer
 import com.daml.platform.apiserver.services.GrpcClientResource
 import com.daml.platform.sandbox.config.SandboxConfig
-import com.daml.platform.sandbox.{AbstractSandboxFixture, SandboxServer}
+import com.daml.platform.sandbox.AbstractSandboxFixture
 import com.daml.ports.Port
 import io.grpc.Channel
 import org.scalatest.Suite
-import scala.concurrent.duration._
 
+import scala.concurrent.duration._
 import java.time.Duration
 
 trait SandboxFixture extends AbstractSandboxFixture with SuiteResource[(SandboxServer, Channel)] {
