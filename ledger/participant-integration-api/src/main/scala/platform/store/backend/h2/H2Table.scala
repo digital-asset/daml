ce0cb9ecf66e42db662ef70b74893c6074d1d257
Marton Nagy <marton.nagy@digitalasset.com>
2022-02-03 14:56:19 +0100

    Fixing idempotent insert for Oracle (and H2) [DPP-562]
    The idempotent insert clauses are racy for Oracle and H2, this is
    only used for inserting packages idempotently.
    
    * Fixes H2 by manually retrying maximum 10 times the execution in question
    * Fixes Oracle by applying a different approach not requireing retrying:
    using IGNORE_ROW_ON_DUPKEY_INDEX
    * Adding unit tests which ensures racy idempotent insert is not an issue
    (this was failing originally)
    * Adding withConnections helper to StorageBackendSpec
    
    changelog_begin
    changelog_end

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/h2/H2Table.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/h2/H2Table.scala
@@ -7,6 +7,9 @@ import java.sql.Connection
 
 import com.daml.platform.store.backend.common.{BaseTable, Field, Table}
 
+import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
+
 private[h2] object H2Table {
   private def idempotentBatchedInsertBase[FROM](
       insertStatement: String,
@@ -33,10 +36,19 @@ private[h2] object H2Table {
                 }
                 preparedStatement.addBatch()
               }
-              preparedStatement.executeBatch()
+              retry(10) {
+                preparedStatement.executeBatch()
+              }
               preparedStatement.close()
             }
     }
+
+  @tailrec
+  private def retry[T](maxRetry: Int)(body: => T): T = Try(body) match {
+    case Success(result) => result
+    case Failure(t) if maxRetry <= 0 => throw t
+    case Failure(_) => retry(maxRetry - 1)(body)
+  }
 
   private def idempotentBatchedInsertStatement(
       tableName: String,
