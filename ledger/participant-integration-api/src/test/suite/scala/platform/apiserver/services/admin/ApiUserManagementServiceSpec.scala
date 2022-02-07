1865ccc309146b69d01c4c51aa337fe89b6f8cdf
pbatko-da <pawel.batko@digitalasset.com>
2022-02-07 09:54:56 +0100

    [User management] Return empty users' next page token early [DPP-840] (#12773)
    If UserManagementStore retrieved fewer users than requested we can assume this is the last page and thus set the next_page_token to the empty string.
    
    changelog_begin
    changelog_end

--- ledger/participant-integration-api/src/test/suite/scala/platform/apiserver/services/admin/ApiUserManagementServiceSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/platform/apiserver/services/admin/ApiUserManagementServiceSpec.scala
@@ -8,8 +8,6 @@ import java.util.Base64
 
 import com.daml.error.definitions.LedgerApiErrors
 import com.daml.error.{DamlContextualizedErrorLogger, ErrorsAssertions}
-import com.daml.ledger.api.domain.User
-import com.daml.ledger.participant.state.index.v2.UserManagementStore.UsersPage
 import com.daml.lf.data.Ref
 import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
@@ -25,19 +23,15 @@ class ApiUserManagementServiceSpec
 
   it should "test users page token encoding and decoding" in {
     val id2 = Ref.UserId.assertFromString("user2")
-    val page = UsersPage(
-      users = Seq(User(Ref.UserId.assertFromString("user1"), None), User(id2, None))
-    )
-    val actualNextPageToken = ApiUserManagementService.encodeNextPageToken(page)
+    val actualNextPageToken = ApiUserManagementService.encodeNextPageToken(Some(id2))
     actualNextPageToken shouldBe "dXNlcjI="
     ApiUserManagementService.decodePageToken(actualNextPageToken)(errorLogger) shouldBe Right(
       Some(id2)
     )
   }
 
-  it should "test users page token encoding and decoding for an empty page" in {
-    val page = UsersPage(users = Seq.empty)
-    val actualNextPageToken = ApiUserManagementService.encodeNextPageToken(page)
+  it should "test users empty page token encoding and decoding" in {
+    val actualNextPageToken = ApiUserManagementService.encodeNextPageToken(None)
     actualNextPageToken shouldBe ("")
     ApiUserManagementService.decodePageToken(actualNextPageToken)(errorLogger) shouldBe Right(None)
   }
