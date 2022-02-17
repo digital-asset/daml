441bfe3eb91354fd23c1cab3c192ece69c8104e7
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-02-17 10:04:44 +0100

    .
    changelog_begin
    changelog_end

--- language-support/codegen-common/src/main/scala/com/digitalasset/daml/lf/codegen/conf/CodegenConfigReader.scala
+++ language-support/codegen-common/src/main/scala/com/digitalasset/daml/lf/codegen/conf/CodegenConfigReader.scala
@@ -160,16 +160,16 @@ object CodegenConfigReader {
       // TODO (MK) https://github.com/digital-asset/daml/issues/9934
       // For now we only allow name-version pairs to match the compiler. Once the compiler
       // accepts package ids we can allow for those here as well.
-      final def apply(key: String): Option[PackageReference] = {
-        val dashIndex = key.lastIndexWhere(_ == '-')
-        if (dashIndex < 0) {
-          None
-        } else {
-          for {
-            name <- PackageName.fromString(key.slice(0, dashIndex)).toOption
-            version <- PackageVersion.fromString(key.slice(dashIndex + 1, key.length)).toOption
-          } yield PackageReference.NameVersion(name, version)
+      final def apply(key: String): Option[PackageReference] =
+        key.splitAt(key.lastIndexOf('-')) match {
+          case (rawPackageName, rawPackageVersion)
+              if rawPackageName.nonEmpty && rawPackageVersion.nonEmpty =>
+            for {
+              name <- PackageName.fromString(rawPackageName).toOption
+              version <- PackageVersion.fromString(rawPackageVersion.tail).toOption
+            } yield PackageReference.NameVersion(name, version)
+          case _ =>
+            None
         }
-      }
     }
 }
