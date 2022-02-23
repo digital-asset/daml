44eaeb70aacbc39a95ba7ba3bfddc7a938401ca7
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-02-23 15:34:42 +0100

    .
    changelog_begin
    changelog_end

--- build.sh
+++ build.sh
@@ -34,6 +34,9 @@ nix show-config
 
 nix upgrade-nix
 
+unset NIX_CONF_DIR
+export NIX_USER_CONF_FILES="${DADE_DEVENV_DIR}/etc/nix.conf:${NIX_USER_CONF_FILES:-}:${XDG_CONFIG_HOME:-${HOME}/.config}/nix/nix.conf"
+
 sha256sum /nix/store/hys4dykcgmr9i2wvaxq5af5jzvqaz68a-ghc-native-bignum-9.0.2/lib/ghc-9.0.2/base-4.15.1.0/libHSbase-4.15.1.0-ghc9.0.2.dylib
 
 nix-store --delete --ignore-liveness /nix/store/hys4dykcgmr9i2wvaxq5af5jzvqaz68a-ghc-native-bignum-9.0.2
