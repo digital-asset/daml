eaed19d08221bb565d4bfac88f5f385b6c73bd3d
Stephen Compall <stephen.compall@daml.com>
2022-02-24 14:37:55 -0500

    point to #13076 instead

--- triggers/service/src/test/scala/com/digitalasset/daml/lf/engine/trigger/TriggerServiceTest.scala
+++ triggers/service/src/test/scala/com/digitalasset/daml/lf/engine/trigger/TriggerServiceTest.scala
@@ -99,7 +99,7 @@ trait AbstractTriggerServiceTest
 
     /** Like `in`, but disables tests that would require the oauth test server
       * to grant claims for the user tokens it manufactures; see
-      * https://github.com/digital-asset/daml/issues/12831#issuecomment-1048176312
+      * https://github.com/digital-asset/daml/issues/13076
       */
     def inClaims(testFn: => Future[Assertion])(implicit pos: source.Position) =
       AbstractTriggerServiceTest.this.inClaims(self, testFn)
