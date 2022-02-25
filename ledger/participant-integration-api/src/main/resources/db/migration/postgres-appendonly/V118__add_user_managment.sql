ab4c346aeef8513a8b2320080fc95edc872cfb78
Pawel Batko <pawel.batko@digitalasset.com>
2022-02-25 16:52:12 +0100

    MN review

--- ledger/participant-integration-api/src/main/resources/db/migration/postgres-appendonly/V118__add_user_managment.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/postgres-appendonly/V118__add_user_managment.sql
@@ -6,14 +6,14 @@
 CREATE TABLE participant_users (
     internal_id         SERIAL          PRIMARY KEY,
     user_id             VARCHAR(256)    NOT NULL UNIQUE COLLATE "C",
-    primary_party       VARCHAR(512)    COLLATE "C",
+    primary_party       VARCHAR(512),
     created_at          BIGINT          NOT NULL
 );
 
 CREATE TABLE participant_user_rights (
     user_internal_id    INTEGER         NOT NULL REFERENCES participant_users (internal_id) ON DELETE CASCADE,
     user_right          INTEGER         NOT NULL,
-    for_party           VARCHAR(512)    COLLATE "C",
+    for_party           VARCHAR(512),
     granted_at          BIGINT          NOT NULL,
     UNIQUE (user_internal_id, user_right, for_party)
 );
