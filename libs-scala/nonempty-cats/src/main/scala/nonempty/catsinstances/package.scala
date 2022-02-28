29d49f495fba2d110dadea80e6f0c5904c3b2d6a
Stephen Compall <stephen.compall@daml.com>
2022-02-28 15:23:39 -0500

    new nonempty-cats library for instances

---
+++ libs-scala/nonempty-cats/src/main/scala/nonempty/catsinstances/package.scala
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.scalautil.nonempty
+
+import cats.Traverse
+
+package object catsinstances extends catsinstances.impl.CatsInstancesLow {
+  implicit def `cats nonempty traverse`[F[_]](implicit F: Traverse[F]): Traverse[NonEmptyF[F, *]] =
+    NonEmpty substF F
+}
