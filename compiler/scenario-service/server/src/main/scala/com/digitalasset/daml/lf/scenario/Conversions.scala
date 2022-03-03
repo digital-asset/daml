b3f1f658123e7e19b06dd57d992d5b6548c38a00
Remy Haemmerle <Remy.Haemmerle@daml.com>
2022-03-03 16:16:19 +0100

    fmt

--- compiler/scenario-service/server/src/main/scala/com/digitalasset/daml/lf/scenario/Conversions.scala
+++ compiler/scenario-service/server/src/main/scala/com/digitalasset/daml/lf/scenario/Conversions.scala
@@ -98,8 +98,6 @@ final class Conversions(
             interpretationError match {
               case UnhandledException(_, value) =>
                 builder.setUnhandledException(convertValue(value))
-              case UncatchableException(_, value, _) =>
-                builder.setUnhandledException(convertValue(value))
               case UserError(msg) =>
                 builder.setUserError(msg)
               case ContractNotFound(cid) =>
