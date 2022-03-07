5ec8ecf22abe27cd7477fdf38a094ff862bffb31
Stephen Compall <stephen.compall@daml.com>
2022-03-07 14:24:01 -0500

    cats Foldable, Reducible instances
    Co-authored-by: Andreas Lochbihler <36634420+andreaslochbihler-da@users.noreply.github.com>

--- libs-scala/nonempty-cats/src/main/scala/nonempty/catsinstances/impl/CatsInstancesLow.scala
+++ libs-scala/nonempty-cats/src/main/scala/nonempty/catsinstances/impl/CatsInstancesLow.scala
@@ -4,4 +4,35 @@
 package com.daml.scalautil.nonempty
 package catsinstances.impl
 
-abstract class CatsInstancesLow
+import cats.{Eval, Foldable, Reducible, Traverse}
+
+abstract class CatsInstancesLow extends CatsInstancesLow1 {
+  implicit def `cats nonempty foldable`[F[_]](implicit F: Foldable[F]): Foldable[NonEmptyF[F, *]] =
+    NonEmpty substF F
+}
+
+abstract class CatsInstancesLow1 {
+  implicit def reducibleNonEmpty[F[_]](implicit F: Foldable[F]): Reducible[NonEmptyF[F, *]] =
+    new Reducible[NonEmptyF[F, *]] {
+      private def errOnEmpty[A](x: NonEmptyF[F, A]): Nothing =
+        throw new IllegalArgumentException(
+          s"empty structure coerced to non-empty: $x: ${x.getClass.getSimpleName}"
+        )
+
+      override def reduceLeftTo[A, B](fa: NonEmptyF[F, A])(f: A => B)(g: (B, A) => B): B =
+        F.reduceLeftToOption(fa)(f)(g).getOrElse(errOnEmpty(fa))
+
+      override def reduceRightTo[A, B](fa: NonEmptyF[F, A])(f: A => B)(
+          g: (A, Eval[B]) => Eval[B]
+      ): Eval[B] =
+        F.reduceRightToOption(fa)(f)(g).map(_.getOrElse(errOnEmpty(fa)))
+
+      override def foldLeft[A, B](fa: NonEmptyF[F, A], b: B)(f: (B, A) => B): B =
+        F.foldLeft(fa, b)(f)
+
+      override def foldRight[A, B](fa: NonEmptyF[F, A], lb: Eval[B])(
+          f: (A, Eval[B]) => Eval[B]
+      ): Eval[B] =
+        F.foldRight(fa, lb)(f)
+    }
+}
