3e8b4242d90c002f873ab17479b802ce24db8a55
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-03-16 12:11:02 +0100

    generator improvements

--- ledger/ledger-security-evidence/tool/src/scala/com/daml/security/evidence/generator/Main.scala
+++ ledger/ledger-security-evidence/tool/src/scala/com/daml/security/evidence/generator/Main.scala
@@ -4,83 +4,86 @@
 package com.daml.security.evidence.generator
 
 import better.files.File
+import com.daml.ledger.api.testtool.infrastructure.LedgerTestSuite
 import com.daml.ledger.api.testtool.suites
-import com.daml.security.evidence.generator.TestEntry.SecurityTestEntry
-import com.daml.security.evidence.generator.TestEntry.ReliabilityTestEntry
-import io.circe.syntax._
+import com.daml.security.evidence.generator.TestEntry.{ReliabilityTestEntry, SecurityTestEntry}
 import com.daml.security.evidence.tag.Reliability.{ReliabilityTest, ReliabilityTestSuite}
 import com.daml.security.evidence.tag.Security.{SecurityTest, SecurityTestSuite}
+import io.circe.Encoder
 import io.circe.generic.auto._
+import io.circe.syntax._
 import org.scalatest.Suite
 import com.daml.security.evidence.scalatest.JsonCodec.SecurityJson._
 import com.daml.security.evidence.scalatest.JsonCodec.ReliabilityJson._
 import org.scalatest.daml.ScalaTestAdapter
 
+import scala.reflect.ClassTag
+
 object Main {
 
-  private def loadIntelliJClasspath(): Option[String] =
-    Some(System.getProperty("java.class.path")).filter(!_.contains("sbt-launch.jar"))
+  private def loadClasspath(): Option[String] = Some(System.getProperty("java.class.path"))
+
+  private def writeEvidenceToFile[TE: Encoder](fileName: String, entries: List[TE]): Unit = {
+    println(s"Writing inventory to $fileName...")
+    val path = File(fileName)
+      .write(entries.asJson.spaces2)
+      .path
+      .toAbsolutePath
+      .toString
+    println(s"Wrote to $path")
+  }
+
+  private def collectTestEvidence[TT: ClassTag, TS: ClassTag, TE](
+      ledgerApiTests: List[LedgerTestSuite],
+      scalaTestSuites: List[Suite],
+      testEntry: (String, String, TT, Boolean, Option[TS]) => TE,
+  ): List[TE] = {
+    val scalaTestEntries =
+      ScalaTestGeneratorSupport.testEntries[TT, TS, TE](
+        scalaTestSuites,
+        testEntry,
+      )
+
+    val ledgerApiTestEntries = LedgerApiTestGeneratorSupport
+      .testEntries[TT, TS, TE](
+        ledgerApiTests,
+        testEntry,
+      )
+
+    scalaTestEntries ++ ledgerApiTestEntries
+  }
 
   def main(args: Array[String]): Unit = {
-    val ledgerApiTests = (suites.v1_8.default(0L) ++ suites.v1_8.optional()).toList
+    val ledgerApiTests: List[LedgerTestSuite] =
+      (suites.v1_8.default(0L) ++ suites.v1_8.optional()).toList
 
-    val cp: Seq[String] = loadIntelliJClasspath()
-      .getOrElse(
-        sys.error("Currently I only support this to be run in Intellij")
-      )
-      .split(":")
-      .toSeq
-    println("cp: " + cp.mkString(","))
-    val runpathList = cp.toList
+    val runpathList: List[String] = loadClasspath()
+      .map(_.split(":").toList)
+      .getOrElse(List.empty)
 
     val testSuites: List[Suite] = ScalaTestAdapter.loadTestSuites(runpathList)
 
     println()
 
     println("Writing security tests inventory..")
-    val securityTestsFilePath = File("security-tests.json")
-      .write(
-        ScalaTestGeneratorSupport
-          .testEntries[SecurityTest, SecurityTestSuite, SecurityTestEntry](
-            testSuites,
-            SecurityTestEntry,
-          )
-          .asJson
-          .spaces2
-      )
-      .path
-      .toAbsolutePath
-      .toString
-    println(s"Wrote to $securityTestsFilePath")
 
-    println("Writing reliability tests inventory..")
-    val reliabilityTestsFilePath = File("reliability-tests.json")
-      .write(
-        ScalaTestGeneratorSupport
-          .testEntries[ReliabilityTest, ReliabilityTestSuite, ReliabilityTestEntry](
-            testSuites,
-            ReliabilityTestEntry,
-          )
-          .asJson
-          .spaces2
+    val securityTestEntries =
+      collectTestEvidence[SecurityTest, SecurityTestSuite, SecurityTestEntry](
+        ledgerApiTests,
+        testSuites,
+        SecurityTestEntry,
       )
-      .path
-      .toAbsolutePath
-      .toString
-    println(s"Wrote to $reliabilityTestsFilePath")
 
-    println("Writing Ledger Api tests inventory..")
-    val ledgerApiTestsFilePath = File("ledger-api-tests.json")
-      .write(
-        LedgerApiTestGeneratorSupport.testEntries[SecurityTest, SecurityTestSuite, SecurityTestEntry](
-          ledgerApiTests,
-          SecurityTestEntry,
-        ).asJson.spaces2
+    val reliabilityTestEntries =
+      collectTestEvidence[ReliabilityTest, ReliabilityTestSuite, ReliabilityTestEntry](
+        ledgerApiTests,
+        testSuites,
+        ReliabilityTestEntry,
       )
-      .path
-      .toAbsolutePath
-      .toString
-    println(s"Wrote to $ledgerApiTestsFilePath")
+
+    writeEvidenceToFile("security-tests.json", securityTestEntries)
+
+    writeEvidenceToFile("reliability-tests.json", reliabilityTestEntries)
 
     sys.exit()
   }
