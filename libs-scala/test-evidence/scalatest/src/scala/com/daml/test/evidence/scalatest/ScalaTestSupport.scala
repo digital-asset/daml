e38e750b9ed7b9f88c37b725370b55130f4dc60f
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-03-17 14:11:11 +0100

    Test library for test evidence to annotate scala and ledger api tests. [DPP-959]
    changelog_begin
    changelog_end

---
+++ libs-scala/test-evidence/scalatest/src/scala/com/daml/test/evidence/scalatest/ScalaTestSupport.scala
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.test.evidence.scalatest
+
+import com.daml.test.evidence.tag.EvidenceTag
+import org.scalatest.Tag
+import scala.language.implicitConversions
+
+object ScalaTestSupport {
+  class TagContainer(testTag: EvidenceTag) extends Tag("TestEvidence") {
+    override val name: String = JsonCodec.encodeEvidenceTag(testTag).noSpaces
+  }
+
+  object Implicits {
+    implicit def tagToContainer(tag: EvidenceTag): Tag = new TagContainer(tag)
+  }
+}
