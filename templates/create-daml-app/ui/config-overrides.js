43786109d4b83bab179b72e58b24be5641626656
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-03-17 13:37:07 +0100

    fix some webpack config issues

--- templates/create-daml-app/ui/config-overrides.js
+++ templates/create-daml-app/ui/config-overrides.js
@@ -9,7 +9,7 @@ module.exports = function override(config, env) {
         //https: require.resolve('https-browserify'),
         //os: require.resolve('os-browserify/browser'),
         //buffer: require.resolve('buffer'),
-        //stream: require.resolve('stream-browserify'),
+        stream: require.resolve('stream-browserify'),
     };
     config.plugins.push(
         new webpack.ProvidePlugin({
