06108e30e3805d081d850a77eb4ad22c801fdf1f
Stephen Compall <stephen.compall@daml.com>
2022-03-18 09:02:36 -0400

    test exercise by inherited choice and interface ID

--- ledger-service/http-json/src/it/scala/http/HttpServiceIntegrationTest.scala
+++ ledger-service/http-json/src/it/scala/http/HttpServiceIntegrationTest.scala
@@ -8,17 +8,30 @@ import java.nio.file.Files
 
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, StatusCodes, Uri}
+import com.daml.bazeltools.BazelRunfiles.requiredResource
 import com.daml.http.dbbackend.JdbcConfig
+import com.daml.ledger.api.v1.{value => v}
+import com.daml.lf.data.Ref
+import com.daml.lf.value.test.TypedValueGenerators.{RNil, ValueAddend => VA}
 import com.daml.scalautil.Statement.discard
 import com.daml.http.util.TestUtil.writeToFile
 import org.scalacheck.Gen
 import org.scalatest.{Assertion, BeforeAndAfterAll}
+import shapeless.record.{Record => ShRecord}
+import spray.json.JsValue
 
 import scala.concurrent.Future
+
+object HttpServiceIntegrationTest {
+
+  // private val iiouDar = requiredResource("ledger-service/http-json/IIou.dar")
+  private val ciouDar = requiredResource("ledger-service/http-json/CIou.dar")
+}
 
 abstract class HttpServiceIntegrationTest
     extends AbstractHttpServiceIntegrationTestTokenIndependent
     with BeforeAndAfterAll {
+  import HttpServiceIntegrationTest._
 
   private val staticContent: String = "static"
 
@@ -65,6 +78,88 @@ abstract class HttpServiceIntegrationTest
             body shouldBe expectedDummyContent
           }
         }: Future[Assertion]
+  }
+
+  "pick up new package's inherited interfaces" in withHttpService { (uri, encoder, _, _) =>
+    import json.JsonProtocol._
+    def createIouAndExerciseTransfer(
+        initialTplId: domain.TemplateId.OptionalPkg,
+        exerciseBy: domain.TemplateId.OptionalPkg,
+    ) = for {
+      aliceH <- getUniquePartyAndAuthHeaders(uri)("Alice")
+      (alice, aliceHeaders) = aliceH
+      createTest <- postCreateCommand(
+        iouCommand(alice, initialTplId),
+        encoder,
+        uri,
+        aliceHeaders,
+      )
+      testIIouID = {
+        discard { createTest._1 should ===(StatusCodes.OK) }
+        createTest._2.convertTo[domain.OkResponse[domain.ActiveContract[JsValue]]].result.contractId
+      }
+      bobH <- getUniquePartyAndAuthHeaders(uri)("Bob")
+      (bob, bobHeaders) = bobH
+      exerciseTest <- postJsonRequest(
+        uri withPath Uri.Path("/v1/exercise"),
+        encodeExercise(encoder)(
+          iouTransfer(testIIouID, bob, exerciseBy)
+        ),
+        aliceHeaders,
+      )
+    } yield exerciseTest._1 should ===(StatusCodes.OK)
+
+    for {
+      // _ <- uploadPackage(uri)(iiouDar)
+      _ <- uploadPackage(uri)(ciouDar)
+      // first, use IIou only
+      _ <- createIouAndExerciseTransfer(
+        initialTplId = domain.TemplateId(None, "IIou", "TestIIou"),
+        // whether we can exercise by interface-ID
+        exerciseBy = domain.TemplateId(None, "IIou", "IIou"),
+      )
+      // next, use CIou
+      _ <- createIouAndExerciseTransfer(
+        initialTplId = domain.TemplateId(None, "CIou", "CIou"),
+        // whether we can exercise inherited by concrete template ID
+        exerciseBy = domain.TemplateId(None, "CIou", "CIou"),
+      )
+    } yield succeed
+  }
+
+  private[this] val (_, ciouVA) = {
+    import shapeless.syntax.singleton._
+    val iouT = Symbol("issuer") ->> VA.party ::
+      Symbol("owner") ->> VA.party ::
+      Symbol("amount") ->> VA.text ::
+      RNil
+    VA.record(Ref.Identifier assertFromString "none:Iou:Iou", iouT)
+  }
+
+  private[this] def iouCommand(party: domain.Party, templateId: domain.TemplateId.OptionalPkg) = {
+    val issuer = Ref.Party assertFromString domain.Party.unwrap(party)
+    val iouT = argToApi(ciouVA)(
+      ShRecord(
+        issuer = issuer,
+        owner = issuer,
+        amount = "42",
+      )
+    )
+    domain.CreateCommand(templateId, iouT, None)
+  }
+
+  private[this] def iouTransfer(
+      cid: domain.ContractId,
+      to: domain.Party,
+      templateId: domain.TemplateId.OptionalPkg,
+  ) = {
+    val payload = recordFromFields(ShRecord(to = v.Value.Sum.Party(domain.Party unwrap to)))
+    domain.ExerciseCommand(
+      domain.EnrichedContractId(Some(templateId), cid),
+      domain.Choice("Transfer"),
+      v.Value(v.Value.Sum.Record(payload)),
+      None,
+    )
   }
 }
 
