e2004eb5ee656cb7599824d60108f601bb483ea0
Stephen Compall <stephen.compall@daml.com>
2022-03-21 10:30:59 -0400

    move nonempty code to new nonempty library

---
+++ libs-scala/nonempty/src/main/scala/scalautil/nonempty/package.scala
@@ -0,0 +1,19 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.scalautil
+
+import com.daml.{nonempty => ne}
+
+package object nonempty {
+  /*
+  @deprecated("use com.daml.nonempty.NonEmpty instead", since = "2.1.0")
+  val NonEmpty: ne.NonEmpty.type = ne.NonEmpty
+
+  @deprecated("use com.daml.nonempty.NonEmpty instead", since = "2.1.0")
+  type NonEmpty[+A] = ne.NonEmpty[A]
+
+  @deprecated("use com.daml.nonempty.NonEmptyF instead", since = "2.1.0")
+  type NonEmptyF[F[_], A] = ne.NonEmptyF[F, A]
+   */
+}
