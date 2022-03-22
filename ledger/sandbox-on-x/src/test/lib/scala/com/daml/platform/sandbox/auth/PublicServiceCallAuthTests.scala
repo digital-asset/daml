4eb70c466372d624a58e99f97bef2098bfd5ff82
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-03-22 13:12:53 +0100

    Runtime dependency of test evidence generator on sandbox-on-x tests [DPP-961]
    changelog_begin
    changelog_end

--- ledger/sandbox-on-x/src/test/lib/scala/com/daml/platform/sandbox/auth/PublicServiceCallAuthTests.scala
+++ ledger/sandbox-on-x/src/test/lib/scala/com/daml/platform/sandbox/auth/PublicServiceCallAuthTests.scala
@@ -5,9 +5,8 @@ package com.daml.platform.sandbox.auth
 
 import java.time.Duration
 import java.util.UUID
-
 import com.daml.ledger.api.auth.AuthServiceJWTPayload
-
+import com.daml.test.evidence.scalatest.ScalaTestSupport.Implicits._
 import scala.concurrent.Future
 
 trait PublicServiceCallAuthTests extends SecuredServiceCallAuthTests {
@@ -15,78 +14,124 @@ trait PublicServiceCallAuthTests extends SecuredServiceCallAuthTests {
   protected def serviceCallWithPayload(payload: AuthServiceJWTPayload): Future[Any] =
     serviceCallWithToken(Some(toHeader(payload)))
 
-  it should "deny calls with an expired read-only token" in {
+  it should "deny calls with an expired read-only token" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit an expired read-only token")
+  ) in {
     expectUnauthenticated(serviceCallWithToken(canReadAsRandomPartyExpired))
   }
-  it should "allow calls with explicitly non-expired read-only token" in {
+
+  it should "allow calls with explicitly non-expired read-only token" taggedAs securityAsset
+    .setHappyCase("Connect with token expiring tomorrow") in {
     expectSuccess(serviceCallWithToken(canReadAsRandomPartyExpiresTomorrow))
   }
-  it should "allow calls with read-only token without expiration" in {
+
+  it should "allow calls with read-only token without expiration" taggedAs securityAsset
+    .setHappyCase("Connect with token without expiration") in {
     expectSuccess(serviceCallWithToken(canReadAsRandomParty))
   }
 
-  it should "allow calls with 'participant_admin' user token" in {
+  it should "allow calls with 'participant_admin' user token" taggedAs securityAsset.setHappyCase(
+    "Connect with `participant_admin` token"
+  ) in {
     expectSuccess(serviceCallWithToken(canReadAsAdminStandardJWT))
   }
-  it should "allow calls with non-expired 'participant_admin' user token" in {
+
+  it should "allow calls with non-expired 'participant_admin' user token" taggedAs securityAsset.setHappyCase(
+    "Connect with non-expired `participant_admin` user token"
+  ) in {
     val payload = standardToken("participant_admin", Some(Duration.ofDays(1)))
     expectSuccess(serviceCallWithPayload(payload))
   }
-  it should "deny calls with expired 'participant_admin' user token" in {
+
+  it should "deny calls with expired 'participant_admin' user token" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit an expired 'participant_admin' user token")
+  ) in {
     val payload =
       standardToken("participant_admin", Some(Duration.ofDays(-1)))
     expectUnauthenticated(serviceCallWithPayload(payload))
   }
-  it should "allow calls with 'participant_admin' user token for this participant node" in {
+
+  it should "allow calls with 'participant_admin' user token for this participant node" taggedAs securityAsset.setHappyCase(
+    "Connect with 'participant_admin' user token for this participant node"
+  ) in {
     val payload =
       standardToken(userId = "participant_admin", participantId = Some("sandbox-participant"))
     expectSuccess(serviceCallWithPayload(payload))
   }
-  it should "deny calls with 'participant_admin' user token for another participant node" in {
+
+  it should "deny calls with 'participant_admin' user token for another participant node" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit 'participant_admin' user token for another participant node")
+  ) in {
     val payload =
       standardToken(userId = "participant_admin", participantId = Some("other-participant-id"))
     expectPermissionDenied(serviceCallWithPayload(payload))
   }
-  it should "allow calls with freshly created user" in {
+
+  it should "allow calls with freshly created user" taggedAs securityAsset.setHappyCase(
+    "allow calls with freshly created user"
+  ) in {
     expectSuccess(
       createUserByAdmin(UUID.randomUUID().toString)
         .flatMap { case (_, token) => serviceCallWithToken(token) }
     )
   }
-  it should "deny calls with non-expired 'unknown_user' user token" in {
+  it should "deny calls with non-expired 'unknown_user' user token" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit non-expired 'unknown_user' user token")
+  ) in {
     expectPermissionDenied(serviceCallWithToken(canReadAsUnknownUserStandardJWT))
   }
 
-  it should "deny calls with an expired read/write token" in {
+  it should "deny calls with an expired read/write token" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit expired read/write token")
+  ) in {
     expectUnauthenticated(serviceCallWithToken(canActAsRandomPartyExpired))
   }
-  it should "allow calls with explicitly non-expired read/write token" in {
+  it should "allow calls with explicitly non-expired read/write token" taggedAs securityAsset.setHappyCase(
+    "allow calls with explicitly non-expired read/write token"
+  ) in {
     expectSuccess(serviceCallWithToken(canActAsRandomPartyExpiresTomorrow))
   }
-  it should "allow calls with read/write token without expiration" in {
+  it should "allow calls with read/write token without expiration" taggedAs securityAsset.setHappyCase(
+    "allow calls with read/write token without expiration"
+  ) in {
     expectSuccess(serviceCallWithToken(canActAsRandomParty))
   }
 
-  it should "deny calls with an expired admin token" in {
+  it should "deny calls with an expired admin token" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit expired admin token")
+  ) in {
     expectUnauthenticated(serviceCallWithToken(canReadAsAdminExpired))
   }
-  it should "allow calls with explicitly non-expired admin token" in {
+  it should "allow calls with explicitly non-expired admin token" taggedAs securityAsset.setHappyCase(
+    "allow calls with explicitly non-expired admin token"
+  ) in {
     expectSuccess(serviceCallWithToken(canReadAsAdminExpiresTomorrow))
   }
-  it should "allow calls with admin token without expiration" in {
+
+  it should "allow calls with admin token without expiration" taggedAs securityAsset.setHappyCase(
+    "allow calls with admin token without expiration"
+  ) in {
     expectSuccess(serviceCallWithToken(canReadAsAdmin))
   }
 
-  it should "allow calls with the correct ledger ID" in {
+  it should "allow calls with the correct ledger ID" taggedAs securityAsset.setHappyCase(
+    "allow calls with the correct ledger ID"
+  ) in {
     expectSuccess(serviceCallWithToken(canReadAsRandomPartyActualLedgerId))
   }
-  it should "deny calls with a random ledger ID" in {
+  it should "deny calls with a random ledger ID" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit random ledger ID")
+  ) in {
     expectPermissionDenied(serviceCallWithToken(canReadAsRandomPartyRandomLedgerId))
   }
-  it should "allow calls with the correct participant ID" in {
+  it should "allow calls with the correct participant ID" taggedAs securityAsset.setHappyCase(
+    "allow calls with the correct participant ID"
+  ) in {
     expectSuccess(serviceCallWithToken(canReadAsRandomPartyActualParticipantId))
   }
-  it should "deny calls with a random participant ID" in {
+  it should "deny calls with a random participant ID" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit calls with a random participant ID")
+  ) in {
     expectPermissionDenied(serviceCallWithToken(canReadAsRandomPartyRandomParticipantId))
   }
 }
--- ledger/sandbox-on-x/src/test/lib/scala/com/daml/platform/sandbox/auth/SecuredServiceCallAuthTests.scala
+++ ledger/sandbox-on-x/src/test/lib/scala/com/daml/platform/sandbox/auth/SecuredServiceCallAuthTests.scala
@@ -3,10 +3,25 @@
 
 package com.daml.platform.sandbox.auth
 
+import com.daml.test.evidence.tag.Security.SecurityTest.Property.Authorization
+import com.daml.test.evidence.tag.Security.{Attack, SecurityTest}
+import com.daml.test.evidence.scalatest.ScalaTestSupport.Implicits._
+
 trait SecuredServiceCallAuthTests extends ServiceCallAuthTests {
   behavior of serviceCallName
 
-  it should "deny unauthenticated calls" in {
+  val securityAsset: SecurityTest =
+    SecurityTest(property = Authorization, asset = serviceCallName)
+
+  def attack(threat: String): Attack = Attack(
+    actor = s"A user that can reach $serviceCallName",
+    threat = threat,
+    mitigation = s"Refuse to connect the user to $serviceCallName",
+  )
+
+  it should "deny unauthenticated calls" taggedAs securityAsset.setAttack(
+    attack(threat = "Exploit a missing token")
+  ) in {
     expectUnauthenticated(serviceCallWithToken(None))
   }
 }
