1fd1e6a9dddf77db8227ba66d564792ddcea1921
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-03-23 16:49:29 +0100

    Test evidence support for Ledger API tests [DPP-965]
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger/test-evidence/src/main/scala/com/daml/test/evidence/generator/Main.scala
+++ ledger/test-evidence/src/main/scala/com/daml/test/evidence/generator/Main.scala
@@ -4,6 +4,8 @@
 package com.daml.test.evidence.generator
 
 import better.files.File
+import com.daml.ledger.api.testtool.suites
+import com.daml.ledger.api.testtool.infrastructure.LedgerTestSuite
 import com.daml.test.evidence.generator.TestEntry.{ReliabilityTestEntry, SecurityTestEntry}
 import com.daml.test.evidence.tag.Reliability.{ReliabilityTest, ReliabilityTestSuite}
 import com.daml.test.evidence.tag.Security.{SecurityTest, SecurityTestSuite}
@@ -33,15 +35,23 @@ object Main {
 
   private def collectTestEvidence[TT: ClassTag, TS: ClassTag, TE](
       scalaTestSuites: List[Suite],
+      ledgerApiSuites: List[LedgerTestSuite],
       testEntry: (String, String, TT, Boolean, Option[TS]) => TE,
   ): List[TE] =
     ScalaTestGeneratorSupport
       .testEntries[TT, TS, TE](scalaTestSuites, testEntry)
+      .concat(LedgerApiTestGeneratorSupport.testEntries(ledgerApiSuites, testEntry))
 
   def main(args: Array[String]): Unit = {
     val runpathList: List[String] = loadClasspath()
       .map(_.split(":").toList)
       .getOrElse(List.empty)
+
+    val ledgerApiTests = List()
+      .concat(suites.v1_8.default(timeoutScaleFactor = 0L))
+      .concat(suites.v1_8.optional())
+      .concat(suites.v1_14.default(timeoutScaleFactor = 0L))
+      .concat(suites.v1_14.optional())
 
     val testSuites: List[Suite] = ScalaTestAdapter.loadTestSuites(runpathList)
 
@@ -50,12 +60,14 @@ object Main {
     val securityTestEntries =
       collectTestEvidence[SecurityTest, SecurityTestSuite, SecurityTestEntry](
         testSuites,
+        ledgerApiTests,
         SecurityTestEntry,
       )
 
     val reliabilityTestEntries =
       collectTestEvidence[ReliabilityTest, ReliabilityTestSuite, ReliabilityTestEntry](
         testSuites,
+        ledgerApiTests,
         ReliabilityTestEntry,
       )
 
