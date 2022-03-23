587d9abfc3d5d15b72c013c66a561d264c14b3dc
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2022-03-23 09:03:35 +0100

    Standardize package for JVM codegen (#13370)
    The Scala codegen code lives under `com.daml.codegen` but the common
    library and the Java codegen both live under `com.daml.lf.codegen`.
    
    In order to share code while preserving package private access of
    certain facilities (i.e. `DependencyGraph`) the package needs to
    be standardized. I chose to go the Java way mostly to reduce noise
    on work currently on its way on the Java codegen (#13366).
    
    Contributes towards https://github.com/digital-asset/daml/issues/13324
    
    changelog_begin
    changelog_end

---
+++ language-support/codegen-common/src/test/scala/com/digitalasset/daml/lf/codegen/dependencygraph/DependencyGraphSpec.scala
@@ -0,0 +1,48 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.lf.codegen.dependencygraph
+
+import com.daml.lf.codegen.Util
+import com.daml.lf.data.Ref
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import scalaz.syntax.std.map._
+
+final class DependencyGraphSpec extends AnyWordSpec with Matchers {
+
+  "orderedDependencies" should {
+    "include contract keys" in {
+      val ei = Util.filterTemplatesBy(Seq("HasKey".r))(DependencyGraphSpec.envInterfaceWithKey)
+      DependencyGraph.orderedDependencies(ei).deps map (_._1) should ===(
+        Vector("a:b:It", "a:b:HasKey") map Ref.Identifier.assertFromString
+      )
+    }
+  }
+
+}
+
+object DependencyGraphSpec {
+
+  import com.daml.lf.iface._
+  import com.daml.lf.data.ImmArray.ImmArraySeq
+
+  private[this] val fooRec = Record(ImmArraySeq.empty)
+  private val envInterfaceWithKey = EnvironmentInterface(
+    Map.empty,
+    Map(
+      "a:b:HasKey" -> InterfaceType.Template(
+        fooRec,
+        DefTemplate(
+          Map.empty,
+          Map.empty,
+          Some(TypeCon(TypeConName(Ref.Identifier assertFromString "a:b:It"), ImmArraySeq.empty)),
+        ),
+      ),
+      "a:b:NoKey" -> InterfaceType.Template(fooRec, DefTemplate(Map.empty, Map.empty, None)),
+      "a:b:It" -> InterfaceType.Normal(DefDataType(ImmArraySeq.empty, fooRec)),
+    ) mapKeys Ref.Identifier.assertFromString,
+    astInterfaces = Map.empty,
+  )
+
+}
