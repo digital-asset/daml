c87920497c6c4ffc9fddabeafaed7010706b7263
davidrichards-da <89472028+davidrichards-da@users.noreply.github.com>
2022-03-24 10:35:41 -0400

    Adding section to error codes page and updating descriptions
    CHANGELOG_BEGIN
    CHANGELOG_END
    Added a section in the error codes page and improved description to errors

--- ledger/error/src/main/scala/com/daml/error/ErrorCategory.scala
+++ ledger/error/src/main/scala/com/daml/error/ErrorCategory.scala
@@ -134,7 +134,8 @@ object ErrorCategory {
 
   /** Some internal error
     */
-  @Description("Request processing failed due to a violation of system internal invariants.")
+  @Description("Request processing failed due to a violation of system internal invariants.  This error is 
+                |exposed on the API with grpc-status INTERNAL without any details due to security reasons")
   @RetryStrategy("Retry after operator intervention.")
   @Resolution(
     """Expectation: this is due to a bug in the implementation or data corruption in the systems databases.
@@ -154,7 +155,8 @@ object ErrorCategory {
   /** Malicious or faulty behaviour detected
     */
   @Description("""Request processing failed due to unrecoverable data loss or corruption
-                 |(e.g. detected via checksums)""")
+                 |(e.g. detected via checksums). This error is exposed on the API with grpc-status INTERNAL 
+                 |without any details due to security reasons""")
   @RetryStrategy("Retry after operator intervention.")
   @Resolution(
     """Expectation: this can be a severe issue that requires operator attention or intervention, and
@@ -173,7 +175,8 @@ object ErrorCategory {
 
   /** Client is not authenticated properly
     */
-  @Description("""The request does not have valid authentication credentials for the operation.""")
+  @Description("""The request does not have valid authentication credentials for the operation.
+                  | This error is exposed on the API with grpc-status INTERNAL without any details due to security reasons""")
   @RetryStrategy("""Retry after application operator intervention.""")
   @Resolution(
     """Expectation: this is an application bug, application misconfiguration or ledger-level
@@ -192,7 +195,8 @@ object ErrorCategory {
 
   /** Client does not have appropriate permissions
     */
-  @Description("""The caller does not have permission to execute the specified operation.""")
+  @Description("""The caller does not have permission to execute the specified operation.
+                  | This error is exposed on the API with grpc-status INTERNAL without any details due to security reasons""")
   @RetryStrategy("""Retry after application operator intervention.""")
   @Resolution(
     """Expectation: this is an application bug or application misconfiguration. Resolution requires
@@ -334,7 +338,8 @@ object ErrorCategory {
       with ErrorCategory
 
   @Description(
-    """This error category is used to signal that an unimplemented code-path has been triggered by a client or participant operator request."""
+    """This error category is used to signal that an unimplemented code-path has been triggered by a client or participant operator request.
+                  | This error is exposed on the API with grpc-status INTERNAL without any details due to security reasons"""
   )
   @RetryStrategy("""Errors in this category are non-retryable.""")
   @Resolution(
