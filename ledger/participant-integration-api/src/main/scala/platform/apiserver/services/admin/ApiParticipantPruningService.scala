11df87ae4e4ece956617b20e0282b9e3c78409ec
pbatko-da <pawel.batko@digitalasset.com>
2022-03-28 16:56:31 +0200

    [Backport] WriteParticipantPruningService propagates rich error codes on prune failure [DPP-861] (#13430)
    * WriteParticipantPruningService propagates rich error codes on prune failure [DPP-861] (#13238)
    
    changelog_begin
    changelog_end

--- ledger/participant-integration-api/src/main/scala/platform/apiserver/services/admin/ApiParticipantPruningService.scala
+++ ledger/participant-integration-api/src/main/scala/platform/apiserver/services/admin/ApiParticipantPruningService.scala
@@ -112,7 +112,7 @@ final class ApiParticipantPruningService private (
       .prune(pruneUpTo, submissionId, pruneAllDivulgedContracts)
       .asScala
       .flatMap {
-        case NotPruned(status) => Future.failed(status.asRuntimeException())
+        case NotPruned(status) => Future.failed(errorFactories.grpcError(status))
         case ParticipantPruned =>
           logger.info(s"Pruned participant ledger up to ${pruneUpTo.toApiString} inclusively.")
           Future.successful(())
