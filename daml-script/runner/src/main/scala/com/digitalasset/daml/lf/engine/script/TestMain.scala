8215b60b0f7067fad31c42a3e994249254b18f7c
Remy Haemmerle <Remy.Haemmerle@daml.com>
2022-03-29 20:06:15 +0200

    Allow LF dev for daml script test
    CHANGELOG_BEGIN
    CHANGELOG_END

--- daml-script/runner/src/main/scala/com/digitalasset/daml/lf/engine/script/TestMain.scala
+++ daml-script/runner/src/main/scala/com/digitalasset/daml/lf/engine/script/TestMain.scala
@@ -78,7 +78,7 @@ object TestMain extends StrictLogging {
     }
 
     val darMap = dar.all.toMap
-    val compiledPackages = PureCompiledPackages.assertBuild(darMap)
+    val compiledPackages = PureCompiledPackages.assertBuild(darMap, Runner.compilerConfig)
     val testScripts: Map[Ref.Identifier, Script.Action] = dar.main._2.modules.flatMap {
       case (moduleName, module) =>
         module.definitions.collect(Function.unlift { case (name, _) =>
