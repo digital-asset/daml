252a93e88a961a2e1a6f8a8b933278ec9efcb950
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-04-08 15:26:52 +0200

    Security Test Evidence for JwksVerifier
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger-service/jwt/src/test/scala/com/digitalasset/jwt/JwksSpec.scala
+++ ledger-service/jwt/src/test/scala/com/digitalasset/jwt/JwksSpec.scala
@@ -3,10 +3,14 @@
 
 package com.daml.jwt
 
-import com.daml.jwt.domain.DecodedJwt
+import com.daml.jwt.domain.{DecodedJwt, Jwt}
+import com.daml.test.evidence.scalatest.ScalaTestSupport.Implicits._
+import com.daml.test.evidence.tag.Security.SecurityTest.Property.Authenticity
+import com.daml.test.evidence.tag.Security.{Attack, SecurityTest}
 import com.daml.testing.SimpleHttpServer
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import scalaz.\/
 import scalaz.syntax.show._
 
 import java.security.KeyPairGenerator
@@ -14,72 +18,85 @@ import java.security.interfaces.{RSAPrivateKey, RSAPublicKey}
 
 class JwksSpec extends AnyFlatSpec with Matchers {
 
-  private def generateToken(keyId: String, privateKey: RSAPrivateKey) = {
+  val securityAsset: SecurityTest =
+    SecurityTest(property = Authenticity, asset = "JWKS-configured Resource")
+
+  def attack(threat: String): Attack = Attack(
+    actor = s"JWKS-configured Resource User",
+    threat = threat,
+    mitigation = s"Refuse to verify authenticity of the token",
+  )
+
+  private def generateToken(keyId: String, privateKey: RSAPrivateKey): JwtSigner.Error \/ Jwt = {
     val jwtPayload = s"""{"test": "JwksSpec"}"""
     val jwtHeader = s"""{"alg": "RS256", "typ": "JWT", "kid": "$keyId"}"""
     JwtSigner.RSA256.sign(DecodedJwt(jwtHeader, jwtPayload), privateKey)
   }
 
-  it should "correctly verify JWT tokens using a JWKS server" in {
+  it should "successfully verify against provided correct key by JWKS server" taggedAs securityAsset
+    .setHappyCase(
+      "Successfully verify against provided correct key by JWKS server"
+    ) in new JwksSpec.Scope {
+    val token = generateToken("test-key-1", privateKey1)
+      .fold(e => fail("Failed to generate signed token: " + e.shows), x => x)
+    val result = verifier.verify(token)
+
+    assert(
+      result.isRight,
+      s"The correctly signed token should successfully verify, but the result was ${result.leftMap(e => e.shows)}",
+    )
+  }
+
+  it should "raise an error by verifying a token with an unknown key id" taggedAs securityAsset
+    .setAttack(attack(threat = "Exploit an unknown key-id")) in new JwksSpec.Scope {
+    val token = generateToken("test-key-unknown", privateKey1)
+      .fold(e => fail("Failed to generate signed token: " + e.shows), x => x)
+    val result = verifier.verify(token)
+
+    assert(result.isLeft, s"The token with an unknown key ID should not successfully verify")
+  }
+
+  it should "raise an error by verifying a token with wrong public key" taggedAs securityAsset
+    .setAttack(
+      attack(threat = "Exploit a known key-id, but not the one used for encryption")
+    ) in new JwksSpec.Scope {
+    val token = generateToken("test-key-1", privateKey2)
+      .fold(e => fail("Failed to generate signed token: " + e.shows), x => x)
+    val result = verifier.verify(token)
+
+    assert(
+      result.isLeft,
+      s"The token with a mismatching public key should not successfully verify",
+    )
+  }
+}
+
+object JwksSpec {
+  trait Scope {
     // Generate some RSA key pairs
-    val keySize = 2048
-    val kpg = KeyPairGenerator.getInstance("RSA")
+    private val keySize = 2048
+    private val kpg = KeyPairGenerator.getInstance("RSA")
     kpg.initialize(keySize)
 
-    val keyPair1 = kpg.generateKeyPair()
-    val publicKey1 = keyPair1.getPublic.asInstanceOf[RSAPublicKey]
+    private val keyPair1 = kpg.generateKeyPair()
+    private val publicKey1 = keyPair1.getPublic.asInstanceOf[RSAPublicKey]
     val privateKey1 = keyPair1.getPrivate.asInstanceOf[RSAPrivateKey]
 
-    val keyPair2 = kpg.generateKeyPair()
-    val publicKey2 = keyPair2.getPublic.asInstanceOf[RSAPublicKey]
+    private val keyPair2 = kpg.generateKeyPair()
+    private val publicKey2 = keyPair2.getPublic.asInstanceOf[RSAPublicKey]
     val privateKey2 = keyPair2.getPrivate.asInstanceOf[RSAPrivateKey]
 
     // Start a JWKS server and create a verifier using the JWKS server
-    val jwks = KeyUtils.generateJwks(
+    private val jwks = KeyUtils.generateJwks(
       Map(
         "test-key-1" -> publicKey1,
         "test-key-2" -> publicKey2,
       )
     )
 
-    val server = SimpleHttpServer.start(jwks)
-    val url = SimpleHttpServer.responseUrl(server)
+    private val server = SimpleHttpServer.start(jwks)
+    private val url = SimpleHttpServer.responseUrl(server)
 
     val verifier = JwksVerifier(url)
-
-    // Test 1: Success
-    val token1 = generateToken("test-key-1", privateKey1)
-      .fold(e => fail("Failed to generate signed token: " + e.shows), x => x)
-    val result1 = verifier.verify(token1)
-
-    assert(
-      result1.isRight,
-      s"The correctly signed token should successfully verify, but the result was ${result1
-        .leftMap(e => e.shows)}",
-    )
-
-    // Test 2: Failure - unknown key ID
-    val token2 = generateToken("test-key-unknown", privateKey1)
-      .fold(e => fail("Failed to generate signed token: " + e.shows), x => x)
-    val result2 = verifier.verify(token2)
-
-    assert(
-      result2.isLeft,
-      s"The token with an unknown key ID should not successfully verify",
-    )
-
-    // Test 3: Failure - wrong public key
-    val token3 = generateToken("test-key-1", privateKey2)
-      .fold(e => fail("Failed to generate signed token: " + e.shows), x => x)
-    val result3 = verifier.verify(token3)
-
-    assert(
-      result3.isLeft,
-      s"The token with a mismatching public key should not successfully verify",
-    )
-
-    SimpleHttpServer.stop(server)
-
-    ()
   }
 }
