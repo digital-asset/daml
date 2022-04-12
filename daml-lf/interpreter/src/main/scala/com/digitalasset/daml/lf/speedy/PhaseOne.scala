5179add354b17d57d7a83fb84c31d18d56765cc7
Robin Krom <robin@digitalasset.com>
2022-04-12 12:03:09 +0200

    Update daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/PhaseOne.scala
    Co-authored-by: Sofia Faro <sofia.faro@digitalasset.com>

--- daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/PhaseOne.scala
+++ daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/PhaseOne.scala
@@ -349,7 +349,7 @@ private[lf] final class PhaseOne(
         compileExp(env, exp) { exp =>
           Return(SBFromRequiredInterface(requiringIfaceId)(exp))
         }
-      case EUnsafeFromRequiredInterface(requiredIfaceId @ _, requiringIfaceId, cidExp, ifaceExp) =>
+      case EUnsafeFromRequiredInterface(requiredIfaceId, requiringIfaceId, cidExp, ifaceExp) =>
         compileExp(env, cidExp) { cidExp =>
           compileExp(env, ifaceExp) { ifaceExp =>
             Return(
