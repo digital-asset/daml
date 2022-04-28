7cb42dc7beb72b2b8ecdd4c97a3851304bd6676f
Nicu Reut <nicu.reut@digitalasset.com>
2022-04-28 09:38:19 +0200

    Formatting

--- ledger/participant-state-kv-errors/src/main/scala/com/daml/ledger/error/definitions/kv/KvErrors.scala
+++ ledger/participant-state-kv-errors/src/main/scala/com/daml/ledger/error/definitions/kv/KvErrors.scala
@@ -122,7 +122,7 @@ object KvErrors extends ErrorGroup()(ErrorGroups.rootErrorClass) {
           ErrorCategory.ContentionOnSharedResources, // It may succeed at a later time
         ) {
       case class Reject(
-          override val cause: String,
+          override val cause: String
       )(implicit loggingContext: ContextualizedErrorLogger)
           extends KVLoggingTransactionErrorImpl(cause)
     }
