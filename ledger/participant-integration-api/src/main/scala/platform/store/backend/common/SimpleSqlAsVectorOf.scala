0fe0f86ee28444256acbf2b76d0a9f9726a60d5f
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2022-04-28 09:55:38 +0200

    Restrict visibility

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/common/SimpleSqlAsVectorOf.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/common/SimpleSqlAsVectorOf.scala
@@ -9,7 +9,7 @@ import anorm.{Cursor, Row, RowParser, SimpleSql}
 
 import scala.util.{Failure, Success, Try}
 
-private[platform] object SimpleSqlAsVectorOf {
+private[backend] object SimpleSqlAsVectorOf {
 
   implicit final class `SimpleSql ops`(val sql: SimpleSql[Row]) extends AnyVal {
 
