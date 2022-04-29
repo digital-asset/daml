9f9e32bc26eccca3cc436703b322ee6f685b2d42
Pawel Batko <pawel.batko@digitalasset.com>
2022-04-29 22:31:54 +0200

    change file name
    changelog_begin
    changelog_end

--- ledger/participant-integration-api/src/main/resources/db/migration/postgres-appendonly/V124__recreate_events_create_create_key_index.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/postgres-appendonly/V124__recreate_events_create_create_key_index.sql
@@ -3,4 +3,5 @@
 
 DROP INDEX participant_events_create_create_key_hash_idx;
 
+-- recreate the index ordering by event_sequential_id first
 CREATE INDEX participant_events_create_create_key_hash_idx ON participant_events_create USING btree (event_sequential_id, create_key_hash);
