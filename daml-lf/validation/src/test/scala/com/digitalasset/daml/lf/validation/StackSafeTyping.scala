7505bc35b5f1107563df0acc7ff4750ba7563219
Nick Chapman <nick.chapman@digitalasset.com>
2022-04-29 12:34:41 +0100

    ensure all test examples are well-typed

--- daml-lf/validation/src/test/scala/com/digitalasset/daml/lf/validation/StackSafeTyping.scala
+++ daml-lf/validation/src/test/scala/com/digitalasset/daml/lf/validation/StackSafeTyping.scala
@@ -5,6 +5,7 @@ package com.daml.lf.validation
 
 import com.daml.lf.data.ImmArray
 import com.daml.lf.data.Ref._
+import com.daml.lf.data.Struct
 import com.daml.lf.language.Ast._
 import com.daml.lf.language.LanguageVersion
 import com.daml.lf.language.PackageInterface
@@ -19,13 +20,170 @@ class StackSafeTyping extends AnyFreeSpec with Matchers with TableDrivenProperty
 
   "typing (stack-safety)" - {
 
-    val langVersion: LanguageVersion = LanguageVersion.default
-    val signatures: PartialFunction[PackageId, PackageSignature] = Map.empty
-    val interface = new PackageInterface(signatures)
-    val ctx: Context = Context.None
-    val env = Typing.Env(langVersion, interface, ctx)
+    // types
 
-    def typecheck(expr: Expr): Option[ValidationError] = { // This is the code under test
+    def textT: Type = TBuiltin(BTText)
+    def intT: Type = TBuiltin(BTInt64)
+    def partyT: Type = TBuiltin(BTParty)
+    def timestampT: Type = TBuiltin(BTTimestamp)
+    def unitT: Type = TBuiltin(BTUnit)
+    def listT: Type = TApp(TBuiltin(BTList), theType)
+    def scenT: Type = scenario(theType)
+    def updateT: Type = TApp(TBuiltin(BTUpdate), theType)
+    def scenario(ty: Type): Type = TApp(TBuiltin(BTScenario), ty)
+    def optional(ty: Type): Type = TApp(TBuiltin(BTOptional), ty)
+    def arrow(ty1: Type, ty2: Type): Type = TApp(TApp(TBuiltin(BTArrow), ty1), ty2)
+    def structT: Type = TStruct(
+      Struct.assertFromSeq(List((field, theType), (field2, theType)))
+    )
+
+    // make an expression of any given type...
+    def mk(ty: Type): Expr = {
+      EApp(ETyApp(EBuiltin(BError), ty), EPrimLit(PLText("message")))
+    }
+
+    def theType: Type = unitT
+    def theExp: Expr = mk(theType)
+
+    // embed an expression of the fixed type, whilst constructing an expression of any given type
+    def embed(exp: Expr, ty: Type): Expr = {
+      EApp(mk(arrow(theType, ty)), exp)
+    }
+    // consume an expression of any given type, whilst constructing an expression of the fixed type
+    def consume(ty: Type, exp: Expr): Expr = {
+      EApp(mk(arrow(ty, theType)), exp)
+    }
+
+    def var1: ExprVarName = Name.assertFromString("x1")
+    def var2: ExprVarName = Name.assertFromString("x2")
+    def tyvar: TypeVarName = Name.assertFromString("T")
+    def field: FieldName = Name.assertFromString("field")
+    def field2: FieldName = Name.assertFromString("field2")
+
+    // Construct one level of source-expression at various 'recursion-points'...
+
+    // The constructed expressions *must* be well-typed, or else the type-checking
+    // algorithm will terminate early, and hence stack-safety is not tested.
+
+    def app1 = (x: Expr) => EApp(embed(x, arrow(theType, theType)), theExp)
+    def app2 = (x: Expr) => EApp(mk(arrow(theType, theType)), x)
+    def tyApp = (x: Expr) => ETyApp(embed(x, TForall((tyvar, KStar), theType)), theType)
+    def esome = (x: Expr) => consume(optional(theType), ESome(theType, x))
+    def eabs = (x: Expr) => EAbs((var1, theType), x, None)
+    def etyabs = (x: Expr) => ETyAbs((tyvar, KStar), x)
+    def struct1 = (x: Expr) => EStructCon(ImmArray((field, x), (field2, theExp)))
+    def struct2 = (x: Expr) => EStructCon(ImmArray((field, theExp), (field2, x)))
+    def consH = (x: Expr) => consume(listT, ECons(theType, ImmArray(x), mk(listT)))
+    def consT = (x: Expr) => consume(listT, ECons(theType, ImmArray(theExp), embed(x, listT)))
+    def scenPure = (x: Expr) => consume(scenT, EScenario(ScenarioPure(theType, x)))
+    def scenBlock1 = (x: Expr) =>
+      consume(
+        scenT,
+        EScenario(ScenarioBlock(ImmArray(Binding(None, theType, embed(x, scenT))), mk(scenT))),
+      )
+    def scenBlock2 = (x: Expr) =>
+      consume(
+        scenT,
+        EScenario(ScenarioBlock(ImmArray(Binding(None, theType, mk(scenT))), embed(x, scenT))),
+      )
+    def scenCommit1 =
+      (x: Expr) => consume(scenT, EScenario(ScenarioCommit(embed(x, partyT), mk(updateT), theType)))
+    def scenCommit2 =
+      (x: Expr) => consume(scenT, EScenario(ScenarioCommit(mk(partyT), embed(x, updateT), theType)))
+    def scenMustFail1 = (x: Expr) =>
+      consume(scenT, EScenario(ScenarioMustFailAt(embed(x, partyT), mk(updateT), theType)))
+    def scenMustFail2 = (x: Expr) =>
+      consume(scenT, EScenario(ScenarioMustFailAt(mk(partyT), embed(x, updateT), theType)))
+    def scenPass =
+      (x: Expr) => consume(scenario(timestampT), EScenario(ScenarioPass(embed(x, intT))))
+    def scenParty =
+      (x: Expr) => consume(scenario(partyT), EScenario(ScenarioGetParty(embed(x, textT))))
+    def scenEmbed = (x: Expr) =>
+      consume(scenario(intT), EScenario(ScenarioEmbedExpr(intT, embed(x, scenario(intT)))))
+    def upure = (x: Expr) => consume(updateT, EUpdate(UpdatePure(theType, x)))
+    def ublock1 = (x: Expr) =>
+      consume(
+        updateT,
+        EUpdate(
+          UpdateBlock(
+            ImmArray(Binding(None, unitT, embed(x, updateT)), Binding(None, unitT, mk(updateT))),
+            mk(updateT),
+          )
+        ),
+      )
+    def ublock2 = (x: Expr) =>
+      consume(
+        updateT,
+        EUpdate(
+          UpdateBlock(
+            ImmArray(Binding(None, unitT, mk(updateT)), Binding(None, unitT, embed(x, updateT))),
+            mk(updateT),
+          )
+        ),
+      )
+    def ublock3 = (x: Expr) =>
+      consume(
+        updateT,
+        EUpdate(
+          UpdateBlock(
+            ImmArray(Binding(None, unitT, mk(updateT)), Binding(None, unitT, mk(updateT))),
+            embed(x, updateT),
+          )
+        ),
+      )
+    def uembed = (x: Expr) => consume(updateT, EUpdate(UpdateEmbedExpr(unitT, embed(x, updateT))))
+    def utrycatch1 = (x: Expr) =>
+      consume(
+        updateT,
+        EUpdate(UpdateTryCatch(unitT, embed(x, updateT), var1, mk(optional(updateT)))),
+      )
+    def utrycatch2 = (x: Expr) =>
+      consume(
+        updateT,
+        EUpdate(UpdateTryCatch(unitT, mk(updateT), var1, embed(x, optional(updateT)))),
+      )
+    def structUpd1 = (x: Expr) => consume(structT, EStructUpd(field, embed(x, structT), mk(unitT)))
+    def structUpd2 = (x: Expr) => consume(structT, EStructUpd(field, mk(structT), x))
+    def caseScrut = (x: Expr) => {
+      ECase(
+        embed(x, listT),
+        ImmArray(
+          CaseAlt(CPNil, theExp),
+          CaseAlt(CPCons(var1, var2), theExp),
+        ),
+      )
+    }
+    def caseAlt1 = (x: Expr) => {
+      ECase(
+        mk(listT),
+        ImmArray(
+          CaseAlt(CPNil, x),
+          CaseAlt(CPCons(var1, var2), theExp),
+        ),
+      )
+    }
+    def caseAlt2 = (x: Expr) => {
+      ECase(
+        mk(listT),
+        ImmArray(
+          CaseAlt(CPNil, theExp),
+          CaseAlt(CPCons(var1, var2), x),
+        ),
+      )
+    }
+    def let1 = (x: Expr) => ELet(Binding(None, theType, x), theExp)
+    def let2 = (x: Expr) => ELet(Binding(None, theType, theExp), x)
+
+    // We don't test any recursion points for which the compiler requires access to a package
+    // signature containing info for the type constructor.
+
+    // This is the code under test...
+    def typecheck(expr: Expr): Option[ValidationError] = {
+      val langVersion: LanguageVersion = LanguageVersion.default
+      val signatures: PartialFunction[PackageId, PackageSignature] = Map.empty
+      val interface = new PackageInterface(signatures)
+      val ctx: Context = Context.None
+      val env = Typing.Env(langVersion, interface, ctx)
       try {
         val _: Type = env.typeOf(expr)
         None
@@ -41,7 +199,7 @@ class StackSafeTyping extends AnyFreeSpec with Matchers with TableDrivenProperty
     def runTest[T](check: Expr => T)(depth: Int, cons: Expr => Expr): T = {
       // Make an expression by iterating the 'cons' function, 'depth' times
       @tailrec def loop(x: Expr, n: Int): Expr = if (n == 0) x else loop(cons(x), n - 1)
-      val source: Expr = loop(exp, depth)
+      val source: Expr = loop(theExp, depth)
       check(source)
     }
 
@@ -78,138 +236,34 @@ class StackSafeTyping extends AnyFreeSpec with Matchers with TableDrivenProperty
         ("ublock1", ublock1),
         ("ublock2", ublock2),
         ("ublock3", ublock3),
-        ("ucreate", ucreate),
-        ("ucreateI", ucreateI),
-        ("ufetch", ufetch),
-        ("ufetchI", ufetchI),
-        ("uexercise1", uexercise1),
-        ("uexercise2", uexercise2),
-        ("uexerciseI1", uexerciseI1),
-        ("uexerciseI2", uexerciseI2),
-        ("uexerciseI3", uexerciseI3),
-        ("uexbykey1", uexbykey1),
-        ("uexbykey2", uexbykey2),
-        ("ufetchbykey", ufetchbykey),
-        ("ulookupbykey", ulookupbykey),
         ("uembed", uembed),
         ("utrycatch1", utrycatch1),
         ("utrycatch2", utrycatch2),
         ("structUpd1", structUpd1),
         ("structUpd2", structUpd2),
-        ("recCon1", recCon1),
-        ("recCon2", recCon2),
         ("caseScrut", caseScrut),
         ("caseAlt1", caseAlt1),
         ("caseAlt2", caseAlt2),
         ("let1", let1),
         ("let2", let2),
-        ("eabs_esome", eabs_esome),
-        ("etyabs_esome", etyabs_esome),
-        ("app1_esome", app1_esome),
-        ("app2_esome", app2_esome),
-        ("tyApp_esome", tyApp_esome),
-        ("let1_esome", let1_esome),
-        ("let2_esome", let2_esome),
       )
     }
 
     {
       val depth = 100 // Small enough to not cause stack-overflow, even for stack-unsafe code
+
+      // TODO https://github.com/digital-asset/daml/issues/13351
+      //
+      // testCases should not fail even when depth is LARGE, say 10k or 100k
+
       s"typing, (SMALL) depth = $depth" - {
         forEvery(testCases) { (name: String, recursionPoint: Expr => Expr) =>
           name in {
-            runTest(typecheck)(depth, recursionPoint)
+            // we enfrce that all the test examples are well-typed
+            runTest(typecheck)(depth, recursionPoint) shouldBe None
           }
         }
       }
     }
-
   }
-
-  // TODO: avoid duplication with code in PhaseOneTest.scala
-
-  // Construct one level of source-expression at various 'recursion-points'.
-  private def app1 = (x: Expr) => EApp(x, exp)
-  private def app2 = (x: Expr) => EApp(exp, x)
-  private def tyApp = (x: Expr) => ETyApp(x, ty)
-  private def esome = (x: Expr) => ESome(ty, x)
-  private def eabs = (x: Expr) => EAbs(binder, x, None)
-  private def etyabs = (x: Expr) => ETyAbs(tvBinder, x)
-  private def struct1 = (x: Expr) => EStructCon(ImmArray((field, x), (field2, exp)))
-  private def struct2 = (x: Expr) => EStructCon(ImmArray((field, exp), (field2, x)))
-  private def structUpd1 = (x: Expr) => EStructUpd(field, x, exp)
-  private def structUpd2 = (x: Expr) => EStructUpd(field, exp, x)
-  private def consH = (x: Expr) => ECons(ty, ImmArray(x), exp)
-  private def consT = (x: Expr) => ECons(ty, ImmArray(exp), x)
-  private def scenPure = (x: Expr) => EScenario(ScenarioPure(ty, x))
-  private def scenBlock1 = (x: Expr) =>
-    EScenario(ScenarioBlock(ImmArray(Binding(None, ty, x)), exp))
-  private def scenBlock2 = (x: Expr) =>
-    EScenario(ScenarioBlock(ImmArray(Binding(None, ty, exp)), x))
-  private def scenCommit1 = (x: Expr) => EScenario(ScenarioCommit(x, exp, ty))
-  private def scenCommit2 = (x: Expr) => EScenario(ScenarioCommit(exp, x, ty))
-  private def scenMustFail1 = (x: Expr) => EScenario(ScenarioMustFailAt(x, exp, ty))
-  private def scenMustFail2 = (x: Expr) => EScenario(ScenarioMustFailAt(exp, x, ty))
-  private def scenPass = (x: Expr) => EScenario(ScenarioPass(x))
-  private def scenParty = (x: Expr) => EScenario(ScenarioGetParty(x))
-  private def scenEmbed = (x: Expr) => EScenario(ScenarioEmbedExpr(ty, x))
-  private def upure = (x: Expr) => EUpdate(UpdatePure(ty, x))
-  private def ublock1 = (x: Expr) =>
-    EUpdate(UpdateBlock(ImmArray(Binding(None, ty, x), Binding(None, ty, exp)), exp))
-  private def ublock2 = (x: Expr) =>
-    EUpdate(UpdateBlock(ImmArray(Binding(None, ty, exp), Binding(None, ty, x)), exp))
-  private def ublock3 = (x: Expr) =>
-    EUpdate(UpdateBlock(ImmArray(Binding(None, ty, exp), Binding(None, ty, exp)), x))
-  private def ucreate = (x: Expr) => EUpdate(UpdateCreate(tcon, x))
-  private def ucreateI = (x: Expr) => EUpdate(UpdateCreateInterface(tcon, x))
-  private def ufetch = (x: Expr) => EUpdate(UpdateFetch(tcon, x))
-  private def ufetchI = (x: Expr) => EUpdate(UpdateFetchInterface(tcon, x))
-  private def uexercise1 = (x: Expr) => EUpdate(UpdateExercise(tcon, choice, x, exp))
-  private def uexercise2 = (x: Expr) => EUpdate(UpdateExercise(tcon, choice, exp, x))
-  private def uexerciseI1 = (x: Expr) => EUpdate(UpdateExerciseInterface(tcon, choice, x, exp, exp))
-  private def uexerciseI2 = (x: Expr) => EUpdate(UpdateExerciseInterface(tcon, choice, exp, x, exp))
-  private def uexerciseI3 = (x: Expr) => EUpdate(UpdateExerciseInterface(tcon, choice, exp, exp, x))
-  private def uexbykey1 = (x: Expr) => EUpdate(UpdateExerciseByKey(tcon, choice, x, exp))
-  private def uexbykey2 = (x: Expr) => EUpdate(UpdateExerciseByKey(tcon, choice, exp, x))
-  private def ufetchbykey = (x: Expr) => EUpdate(UpdateFetchByKey(RetrieveByKey(tcon, x)))
-  private def ulookupbykey = (x: Expr) => EUpdate(UpdateLookupByKey(RetrieveByKey(tcon, x)))
-  private def uembed = (x: Expr) => EUpdate(UpdateEmbedExpr(ty, x))
-  private def utrycatch1 = (x: Expr) => EUpdate(UpdateTryCatch(ty, x, varname, exp))
-  private def utrycatch2 = (x: Expr) => EUpdate(UpdateTryCatch(ty, exp, varname, x))
-
-  private def recCon1 = (x: Expr) => ERecCon(tapp, ImmArray((field, x), (field2, exp)))
-  private def recCon2 = (x: Expr) => ERecCon(tapp, ImmArray((field, exp), (field2, x)))
-
-  // We dont test the recursion points ERecProj and ERecUpd, because the compiler requires
-  // access to a package signature containing info for the type constructor.
-
-  private def caseScrut = (x: Expr) => ECase(x, ImmArray())
-  private def caseAlt1 = (x: Expr) => ECase(exp, ImmArray(CaseAlt(CPNil, x), alt))
-  private def caseAlt2 = (x: Expr) => ECase(exp, ImmArray(alt, CaseAlt(CPNil, x)))
-  private def let1 = (x: Expr) => ELet(Binding(None, ty, x), exp)
-  private def let2 = (x: Expr) => ELet(Binding(None, ty, exp), x)
-
-  // Alterating construction for applicaton & abstraction with some
-  private def eabs_esome = (x: Expr) => eabs(esome(x))
-  private def etyabs_esome = (x: Expr) => etyabs(esome(x))
-  private def app1_esome = (x: Expr) => app1(esome(x))
-  private def app2_esome = (x: Expr) => app2(esome(x))
-  private def tyApp_esome = (x: Expr) => tyApp(esome(x))
-  private def let1_esome = (x: Expr) => let1(esome(x))
-  private def let2_esome = (x: Expr) => let2(esome(x))
-
-  // Leaves for various types
-  private def alt: CaseAlt = CaseAlt(CPNil, exp)
-  private def exp: Expr = EPrimLit(PLText("exp"))
-  private def ty: Type = TVar(Name.assertFromString("ty"))
-  private def binder: (ExprVarName, Type) = (varname, ty)
-  private def tvBinder: (TypeVarName, Kind) = (tvar, KStar)
-  private def varname: ExprVarName = Name.assertFromString("x")
-  private def tvar: TypeVarName = Name.assertFromString("T")
-  private def tcon: TypeConName = Identifier.assertFromString("P:M:tcon")
-  private def choice: ChoiceName = Name.assertFromString("choice")
-  private def tapp: TypeConApp = TypeConApp(tcon, ImmArray.empty)
-  private def field: FieldName = Name.assertFromString("field")
-  private def field2: FieldName = Name.assertFromString("field2")
-
 }
