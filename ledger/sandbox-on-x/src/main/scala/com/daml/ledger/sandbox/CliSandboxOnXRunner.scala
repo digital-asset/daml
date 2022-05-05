541bc046309000265aeaaa30fec80cd9030d97bf
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-05-05 15:52:06 +0200

    Dropping config provider

--- ledger/sandbox-on-x/src/main/scala/com/daml/ledger/sandbox/CliSandboxOnXRunner.scala
+++ ledger/sandbox-on-x/src/main/scala/com/daml/ledger/sandbox/CliSandboxOnXRunner.scala
@@ -6,25 +6,57 @@ package com.daml.ledger.sandbox
 import com.daml.ledger.resources.{Resource, ResourceContext, ResourceOwner}
 import com.daml.ledger.runner.common._
 import com.daml.ledger.sandbox.SandboxOnXRunner.run
+import scopt.OptionParser
+
+import java.time.Duration
 
 object CliSandboxOnXRunner {
   val RunnerName = "sandbox-on-x"
 
+  def bridgeConfigParser(parser: OptionParser[CliConfig[BridgeConfig]]): Unit = {
+    parser
+      .opt[Int]("bridge-submission-buffer-size")
+      .text("Submission buffer size. Defaults to 500.")
+      .action((p, c) => c.copy(extra = c.extra.copy(submissionBufferSize = p)))
+
+    parser
+      .opt[Unit]("disable-conflict-checking")
+      .hidden()
+      .text("Disable ledger-side submission conflict checking.")
+      .action((_, c) => c.copy(extra = c.extra.copy(conflictCheckingEnabled = false)))
+
+    parser
+      .opt[Boolean](name = "implicit-party-allocation")
+      .optional()
+      .action((x, c) => c.copy(implicitPartyAllocation = x))
+      .text(
+        s"When referring to a party that doesn't yet exist on the ledger, the participant will implicitly allocate that party."
+          + s" You can optionally disable this behavior to bring participant into line with other ledgers."
+      )
+
+    parser.checkConfig(c =>
+      Either.cond(
+        c.maxDeduplicationDuration.forall(_.compareTo(Duration.ofHours(1L)) <= 0),
+        (),
+        "Maximum supported deduplication duration is one hour",
+      )
+    )
+    ()
+  }
+
   def owner(
       args: collection.Seq[String],
       manipulateConfig: CliConfig[BridgeConfig] => CliConfig[BridgeConfig] = identity,
-  ): ResourceOwner[Unit] = {
-    val configProvider = new BridgeCliConfigProvider
+  ): ResourceOwner[Unit] =
     CliConfig
       .owner(
         RunnerName,
-        configProvider.extraConfigParser,
-        configProvider.defaultExtraConfig,
+        bridgeConfigParser,
+        BridgeConfig.Default,
         args,
       )
       .map(manipulateConfig)
       .flatMap(owner)
-  }
 
   def owner(originalConfig: CliConfig[BridgeConfig]): ResourceOwner[Unit] =
     new ResourceOwner[Unit] {
