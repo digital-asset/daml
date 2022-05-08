643e35690090ade9b43ff3c147a47cfe5330066d
Marcin Ziolek <marcin.ziolek@digitalasset.com>
2022-05-08 15:13:01 +0200

    add tests for large maps and lists

--- ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_8/ValueLimitsIT.scala
+++ ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_8/ValueLimitsIT.scala
@@ -6,9 +6,12 @@ package com.daml.ledger.api.testtool.suites.v1_8
 import com.daml.ledger.api.testtool.infrastructure.Allocation._
 import com.daml.ledger.api.testtool.infrastructure.Assertions._
 import com.daml.ledger.api.testtool.infrastructure.LedgerTestSuite
-import com.daml.ledger.test.model.Test.DummyWithAnnotation
+import com.daml.ledger.client.binding.Primitive.Party
+import com.daml.ledger.test.model.Test.{DummyWithAnnotation, WithList, WithMap}
+import com.daml.timer.Delayed
 
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 import scala.util.Random
 
 final class ValueLimitsIT extends LedgerTestSuite {
@@ -37,6 +40,64 @@ final class ValueLimitsIT extends LedgerTestSuite {
       contracts <- ledger.activeContracts(parties.head)
     } yield {
       assertSingleton("Single create contract expected", contracts)
+      ()
+    }
+  })
+
+  test(
+    "VLLargeMapInContract",
+    "Create a contract with a field containing large map",
+    allocate(SingleParty),
+  )(implicit ec => { case Participants(Participant(ledger, alice)) =>
+    val elements = (1 to 10000).map(e => (f"element_$e%08d", alice)).toMap
+    for {
+      contract <- ledger.create(alice, WithMap(alice, elements))
+      _ <- ledger.exercise(alice, contract.exerciseWithMap_Noop)
+    } yield {
+      ()
+    }
+  })
+
+  test(
+    "VLLargeMapInChoice",
+    "Exercise a choice with a large map",
+    allocate(SingleParty),
+  )(implicit ec => { case Participants(Participant(ledger, alice)) =>
+    val elements = (1 to 10000).map(e => (f"element_$e%08d", alice)).toMap
+    for {
+      contract <- ledger.create(alice, WithMap(alice, Map.empty[String, Party]))
+      _ <- ledger.exercise(alice, contract.exerciseWithMap_Expand(_, elements))
+    } yield {
+      ()
+    }
+  })
+
+  test(
+    "VLLargeListInContract",
+    "Create a contract with a field containing large list",
+    allocate(SingleParty),
+  )(implicit ec => { case Participants(Participant(ledger, alice)) =>
+    val elements = (1 to 10000).map(e => f"element_$e%08d")
+    for {
+      //contract <- ledger.create(alice, WithList(alice, elements))
+      _ <- Future.traverse(1 to 10000)(d => Delayed.by((d*10).millis)(()).flatMap(_ => ledger.create(alice, WithList(alice, elements))))
+      //_ <- Future.traverse(1 to 10000)(d => Delayed.by(d.millis)(()).flatMap(_ => ledger.exercise(alice, contract.exerciseWithList_Noop)))
+      //_ <- ledger.exercise(alice, contract.exerciseWithList_Noop)
+    } yield {
+      ()
+    }
+  })
+
+  test(
+    "VLLargeListInChoice",
+    "Exercise a choice with a large list",
+    allocate(SingleParty),
+  )(implicit ec => { case Participants(Participant(ledger, alice)) =>
+    val elements = (1 to 10000).map(e => f"element_$e%08d")
+    for {
+      contract <- ledger.create(alice, WithList(alice, List.empty[String]))
+      _ <- ledger.exercise(alice, contract.exerciseWithList_Expand(_, elements))
+    } yield {
       ()
     }
   })
