be275bb5eca8328b017f506ba72eb4efde9cc9cc
Nicu Reut <nicu.reut@digitalasset.com>
2022-05-10 10:18:51 +0200

    Upload dars sequentially for every participant defined [kvl-1369] (#13834)

---
+++ ledger/ledger-api-tests/infrastructure/src/main/scala/com/daml/ledger/api/testtool/infrastructure/future/FutureUtil.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.ledger.api.testtool.infrastructure.future
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object FutureUtil {
+
+  def sequential[T, R](elements: Seq[T])(f: T => Future[R])(implicit
+      ec: ExecutionContext
+  ): Future[Seq[R]] =
+    elements.foldLeft(Future.successful(Seq.empty[R])) { case (results, newElement) =>
+      results.flatMap(resultsSoFar => f(newElement).map(resultsSoFar :+ _))
+    }
+
+}
