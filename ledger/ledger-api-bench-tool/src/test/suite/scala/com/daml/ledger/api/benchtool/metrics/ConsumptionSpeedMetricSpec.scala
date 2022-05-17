66d625d6c2c076c61db72a24f13629dd59296304
Pawel Batko <pawel.batko@digitalasset.com>
2022-05-17 09:40:34 +0200

    Benchtool: Move observers to a test-lib target [DPP-979]
    changelog_begin
    changelog_end

--- ledger/ledger-api-bench-tool/src/test/suite/scala/com/daml/ledger/api/benchtool/metrics/ConsumptionSpeedMetricSpec.scala
+++ ledger/ledger-api-bench-tool/src/test/suite/scala/com/daml/ledger/api/benchtool/metrics/ConsumptionSpeedMetricSpec.scala
@@ -1,9 +1,8 @@
 // Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.ledger.api.benchtool
+package com.daml.ledger.api.benchtool.metrics
 
-import com.daml.ledger.api.benchtool.metrics.ConsumptionSpeedMetric
 import com.daml.ledger.api.benchtool.metrics.ConsumptionSpeedMetric._
 import com.google.protobuf.timestamp.Timestamp
 import org.scalatest.matchers.should.Matchers
