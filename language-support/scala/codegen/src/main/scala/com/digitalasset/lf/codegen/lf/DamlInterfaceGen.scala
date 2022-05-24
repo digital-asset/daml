e1a64e21f1cbfaf609a1ab31a51eeb8a4e84dae9
Stephen Compall <stephen.compall@daml.com>
2022-05-24 15:56:01 -0400

    starting interface output based on prior patterns

--- language-support/scala/codegen/src/main/scala/com/digitalasset/lf/codegen/lf/DamlInterfaceGen.scala
+++ language-support/scala/codegen/src/main/scala/com/digitalasset/lf/codegen/lf/DamlInterfaceGen.scala
@@ -1,3 +1,21 @@
-package com.daml.lf.codegen.lf object DamlInterfaceGen {
+package com.daml.lf.codegen.lf
 
+import java.io.File
+
+import com.daml.lf.codegen.lf.LFUtil
+import com.daml.lf.data.ImmArray.ImmArraySeq
+import com.daml.lf.data.Ref.{Identifier, QualifiedName}
+import com.daml.lf.iface
+import com.typesafe.scalalogging.Logger
+import scalaz.syntax.std.option._
+
+import scala.reflect.runtime.universe._
+
+object DamlInterfaceGen {
+  def generate(
+      util: LFUtil,
+      templateId: Identifier,
+      interfaceSignature: iface.DefInterface.FWT,
+      companionMembers: Iterable[Tree],
+  ): (File, Set[Tree], Iterable[Tree]) = {}
 }
