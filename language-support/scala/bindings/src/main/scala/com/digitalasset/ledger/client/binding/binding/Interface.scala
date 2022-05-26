7b5295be10dfd44c5c06cdc70dc2e2fad5db0134
Stephen Compall <stephen.compall@daml.com>
2022-05-26 10:53:50 -0400

    unsafeToTemplate for interface contract IDs

--- language-support/scala/bindings/src/main/scala/com/digitalasset/ledger/client/binding/binding/Interface.scala
+++ language-support/scala/bindings/src/main/scala/com/digitalasset/ledger/client/binding/binding/Interface.scala
@@ -3,6 +3,10 @@
 
 package com.daml.ledger.client.binding
 
+import com.daml.ledger.api.refinements.ApiTypes
+
+import scala.annotation.nowarn
+
 /** Common superclass of interface marker types.  There are no instances of
   * subclasses of this class; it is strictly a marker type to aid in implicit
   * resolution, and only occurs within contract IDs.
@@ -10,5 +14,15 @@ package com.daml.ledger.client.binding
 abstract class Interface extends VoidValueRef
 
 object Interface {
-  // TODO #13924 unsafeToTemplate extension method for cids
+  import Primitive.ContractId, ContractId.subst
+
+  implicit final class `interface ContractId syntax`[I](private val self: ContractId[I])
+      extends AnyVal {
+    @nowarn("cat=unused&msg=parameter value ev in method")
+    def unsafeToTemplate[T](implicit ev: Template.Implements[T, I]): ContractId[T] = {
+      type K[C] = C => ApiTypes.ContractId
+      type K2[C] = ContractId[I] => C
+      subst[K2, T](subst[K, I](identity))(self)
+    }
+  }
 }
