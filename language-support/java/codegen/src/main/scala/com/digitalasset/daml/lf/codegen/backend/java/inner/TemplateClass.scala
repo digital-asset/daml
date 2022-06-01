5f3f2161260108b9d22b508d701c44d5ae534152
Stephen Compall <stephen.compall@daml.com>
2022-06-01 13:04:35 -0400

    more Scalaesque docstring

--- language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/TemplateClass.scala
+++ language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/TemplateClass.scala
@@ -145,10 +145,11 @@ private[inner] object TemplateClass extends StrictLogging {
             .generateToValueConverter(key, CodeBlock.of("key"), newNameGenerator, packagePrefixes),
         )
         .addJavadoc(
-          "Set up an {@link $T};$Winvoke an {@code exercise} method on the result of$W" +
-            "this to finish creating the command, or convert to an interface first$W" +
-            "with {@code toInterface}$W" +
-            "to invoke an interface {@code exercise} method.",
+          """Set up an {@link $T};$Winvoke an {@code exercise} method on the result of
+            |this to finish creating the command, or convert to an interface first
+            |with {@code toInterface}
+            |to invoke an interface {@code exercise} method.""".stripMargin
+            .replaceAll("\n", "\\$W"),
           classOf[javaapi.data.ExerciseByKeyCommand],
         )
         .build()
