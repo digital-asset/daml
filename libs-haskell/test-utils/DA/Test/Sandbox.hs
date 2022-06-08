1ac367876574c2a230cf2bf72993de70ce048196
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-06-08 14:42:43 +0200

    Removal of Sandbox Classic in Haskell code.
    CHANGELOG_BEGIN
    CHANGELOG_END

--- libs-haskell/test-utils/DA/Test/Sandbox.hs
+++ libs-haskell/test-utils/DA/Test/Sandbox.hs
@@ -21,6 +21,7 @@ import qualified Data.Aeson as Aeson
 import qualified Data.Map as Map
 import qualified Data.Text as T
 import qualified Data.Vector as Vector
+import qualified Data.Maybe as Maybe
 import System.FilePath
 import System.IO.Extra
 import System.Process
@@ -57,7 +58,7 @@ defaultSandboxConf = SandboxConfig
 
 getSandboxProc :: SandboxConfig -> FilePath -> IO CreateProcess
 getSandboxProc SandboxConfig{..} portFile = do
-    sandbox <- locateRunfiles (mainWorkspace </> "ledger" </> "sandbox-classic" </> exe "sandbox-classic-binary")
+    sandbox <- locateRunfiles (mainWorkspace </> "ledger" </> "sandbox-on-x" </> exe "app")
     tlsArgs <- if enableTls
         then do
             certDir <- locateRunfiles (mainWorkspace </> "ledger" </> "test-common" </> "test-certificates")
@@ -68,17 +69,16 @@ getSandboxProc SandboxConfig{..} portFile = do
                 ]
         else pure []
     pure $ proc sandbox $ concat
-        [ [ "--port=0", "--port-file", portFile ]
+        [ [ "--participant=participant-id=example,port=0,port-file=" <> portFile ]
         , tlsArgs
-        , [ timeArg ]
+        , Maybe.maybeToList timeArg
         , [ "--client-auth=" <> clientAuthArg auth | Just auth <- [mbClientAuth] ]
         , [ "--auth-jwt-hs256-unsafe=" <> secret | Just secret <- [mbSharedSecret] ]
-        , [ "--ledgerid=" <> ledgerId | Just ledgerId <- [mbLedgerId] ]
-        , dars
+        , [ "--ledger-id=" <> ledgerId | Just ledgerId <- [mbLedgerId] ]
         ]
   where timeArg = case timeMode of
-            WallClock -> "--wall-clock-time"
-            Static -> "--static-time"
+            WallClock -> Nothing
+            Static ->  Just "--static-time"
         clientAuthArg auth = case auth of
             None ->  "none"
             Optional -> "optional"
