2bcca0172e97461f6d5a732c4704abafa930e16d
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-06-09 16:03:54 +0200

    deploy unpinned u2 machine

--- infra/vsts_agent_ubuntu_20_04.tf
+++ infra/vsts_agent_ubuntu_20_04.tf
@@ -7,11 +7,13 @@ locals {
       name      = "ci-u1",
       disk_size = 200,
       size      = 30,
+      image     = "ubuntu-os-cloud/ubuntu-2004-focal-v20220606",
     },
     {
       name      = "ci-u2",
       disk_size = 400,
-      size      = 0,
+      size      = 1,
+      image     = "ubuntu-os-cloud/ubuntu-2004-lts",
     },
   ]
 }
@@ -64,7 +66,7 @@ resource "google_compute_instance_template" "vsts-agent-ubuntu_20_04" {
     disk_type    = "pd-ssd"
     #TODO: when the image gets fixed, go back to auto-upgrading
     #source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
-    source_image = "ubuntu-os-cloud/ubuntu-2004-focal-v20220606"
+    source_image = local.ubuntu[count.index].image
   }
 
   lifecycle {
