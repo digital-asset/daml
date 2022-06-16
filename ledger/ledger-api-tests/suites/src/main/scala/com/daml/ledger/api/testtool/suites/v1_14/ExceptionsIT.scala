44a14d06c94cfb30bd07be4c388d1c3cf1273273
nickchapman-da <49153372+nickchapman-da@users.noreply.github.com>
2022-06-16 15:58:19 +0100

    Regression test for rollback create bug (#14197)
    * Regression test for issue 14148
    
    changelog_begin
    changelog_end
    
    * update compatability

--- ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_14/ExceptionsIT.scala
+++ ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_14/ExceptionsIT.scala
@@ -420,4 +420,25 @@ final class ExceptionsIT extends LedgerTestSuite {
         _ <- ledger.exercise(party, t.exerciseRollbackGlobalArchivedCreate(_, withKey))
       } yield ()
   })
+
+  test(
+    "ExRollbackCreate",
+    "Archiving a contract created within a rolled-back try-catch block, fails",
+    allocate(SingleParty),
+  )(implicit ec => { case Participants(Participant(ledger, party)) =>
+    for {
+      t <- ledger.create(party, ExceptionTester(party))
+      failure <- ledger
+        .exercise(party, t.exerciseRollbackCreateBecomesInactive(_))
+        .mustFail("contract is inactive")
+    } yield {
+      assertGrpcError(
+        failure,
+        LedgerApiErrors.ConsistencyErrors.ContractNotFound,
+        Some("Contract could not be found"),
+        checkDefiniteAnswerMetadata = true,
+      )
+    }
+  })
+
 }
