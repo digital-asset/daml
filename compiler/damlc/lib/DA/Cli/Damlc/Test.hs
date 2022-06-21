5315dac3080419fa5803a84b57b881ca6fd19643
Carl Pulley <carl.pulley@digitalasset.com>
2022-06-21 17:17:06 +0100

    Refactored Test.hs: pulled out activeContracts into a utility function

--- compiler/damlc/lib/DA/Cli/Damlc/Test.hs
+++ compiler/damlc/lib/DA/Cli/Damlc/Test.hs
@@ -260,11 +260,14 @@ prettyErr lfVersion err = case err of
     SSC.ExceptionError e -> DA.Pretty.string $ show e
 
 
+activeContracts :: SS.ScenarioResult -> S.Set TL.Text
+activeContracts result =
+    S.fromList (V.toList (SS.scenarioResultActiveContracts result))
+
 prettyResult :: SS.ScenarioResult -> DA.Pretty.Doc Pretty.SyntaxClass
 prettyResult result =
     let nTx = length (SS.scenarioResultScenarioSteps result)
-        activeContracts = S.fromList (V.toList (SS.scenarioResultActiveContracts result))
-        nActive = length $ filter (SS.isActive activeContracts) (V.toList (SS.scenarioResultNodes result))
+        nActive = length $ filter (SS.isActive (activeContracts result)) (V.toList (SS.scenarioResultNodes result))
     in DA.Pretty.typeDoc_ "ok, "
     <> DA.Pretty.int nActive <> DA.Pretty.typeDoc_ " active contracts, "
     <> DA.Pretty.int nTx <> DA.Pretty.typeDoc_ " transactions."
