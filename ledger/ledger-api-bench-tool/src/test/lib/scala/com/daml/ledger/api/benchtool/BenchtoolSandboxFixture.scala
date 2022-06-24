7cd677e36761d802ed19793721de1be2fff017bc
pbatko-da <pawel.batko@digitalasset.com>
2022-06-24 11:33:05 +0200

    [ETQ-Testing] Prepare Benchtool for extending it to support huge party filters [DPP-1079] (#14246)
    This a preparation before the adding support for huge party filters.
    
    Some of the changes:
    1. Scan for existing parties even if when there is not submission step configured so that we can enrich stream configs fully.
    NOTE: ConfigEnricher will now never fail on missing party.
    2. Extract party selection for contract creation to a separate class.
    3. Expose `commandGenerationParallelism` and a couple of randomness providers to allow for reproducible party allotment to contracts - useful in tests.
    4. Extract `benchtoolFixture` to reduce boilerplate in IT tests
    
    
    changelog_begin
    changelog_end

--- ledger/ledger-api-bench-tool/src/test/lib/scala/com/daml/ledger/api/benchtool/BenchtoolSandboxFixture.scala
+++ ledger/ledger-api-bench-tool/src/test/lib/scala/com/daml/ledger/api/benchtool/BenchtoolSandboxFixture.scala
@@ -5,10 +5,16 @@ package com.daml.ledger.api.benchtool
 
 import java.io.File
 
+import com.codahale.metrics.MetricRegistry
 import com.daml.bazeltools.BazelRunfiles.rlocation
+import com.daml.ledger.api.benchtool.metrics.MetricsManager.NoOpMetricsManager
+import com.daml.ledger.api.benchtool.services.LedgerApiServices
+import com.daml.ledger.api.benchtool.submission.{CommandSubmitter, Names, PartyAllocating}
 import com.daml.ledger.test.BenchtoolTestDar
 import com.daml.platform.sandbox.fixture.SandboxFixture
 import org.scalatest.Suite
+
+import scala.concurrent.{ExecutionContext, Future}
 
 trait BenchtoolSandboxFixture extends SandboxFixture {
   self: Suite =>
@@ -16,5 +22,34 @@ trait BenchtoolSandboxFixture extends SandboxFixture {
   override protected def packageFiles: List[File] = List(
     new File(rlocation(BenchtoolTestDar.path))
   )
+
+  def benchtoolFixture()(implicit
+      ec: ExecutionContext
+  ): Future[(LedgerApiServices, Names, CommandSubmitter)] = {
+    for {
+      ledgerApiServicesF <- LedgerApiServices.forChannel(
+        channel = channel,
+        authorizationHelper = None,
+      )
+      apiServices: LedgerApiServices = ledgerApiServicesF("someUser")
+      names = new Names()
+      submitter = CommandSubmitter(
+        names = names,
+        benchtoolUserServices = apiServices,
+        adminServices = apiServices,
+        metricRegistry = new MetricRegistry,
+        metricsManager = NoOpMetricsManager(),
+        waitForSubmission = true,
+        partyAllocating = new PartyAllocating(
+          names = names,
+          adminServices = apiServices,
+        ),
+      )
+    } yield (
+      apiServices,
+      names,
+      submitter,
+    )
+  }
 
 }
