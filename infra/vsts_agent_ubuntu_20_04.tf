b3d9aa57702743325f94a55d5ab33fbba29579d6
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-07-18 17:35:28 +0200

    oops; update Docker install script

--- infra/vsts_agent_ubuntu_20_04.tf
+++ infra/vsts_agent_ubuntu_20_04.tf
@@ -8,7 +8,19 @@ locals {
       disk_size = 200,
       size      = 30,
       docker    = <<EOF
-apt-get install -qy docker-ce docker-ce-cli containerd.io
+# BEGIN Installing Docker per https://docs.docker.com/engine/install/ubuntu/
+apt-get -y install apt-transport-https \
+                   ca-certificates \
+                   curl \
+                   gnupg \
+                   lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+docker run --rm hello-world
+# END Installing Docker
 EOF
     },
     {
