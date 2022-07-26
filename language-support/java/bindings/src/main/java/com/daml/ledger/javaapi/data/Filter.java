1092c66ccbf947be45ad4676bc45bc28a18f9bb3
Stephen Compall <stephen.compall@daml.com>
2022-07-26 15:11:57 -0400

    declare InterfaceFilter counterpart

--- language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/Filter.java
+++ language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/Filter.java
@@ -4,6 +4,7 @@
 package com.daml.ledger.javaapi.data;
 
 import com.daml.ledger.api.v1.TransactionFilterOuterClass;
+import java.util.Objects;
 
 public abstract class Filter {
 
@@ -16,4 +17,30 @@ public abstract class Filter {
   }
 
   public abstract TransactionFilterOuterClass.Filters toProto();
+
+  public static final class Interface {
+    public final boolean includeInterfaceViews;
+
+    public Interface(boolean includeInterfaceViews) {
+      this.includeInterfaceViews = includeInterfaceViews;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Interface that = (Interface) o;
+      return includeInterfaceViews == that.includeInterfaceViews;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(includeInterfaceViews);
+    }
+
+    @Override
+    public String toString() {
+      return "Interface{" + "includeInterfaceViews=" + includeInterfaceViews + '}';
+    }
+  }
 }
