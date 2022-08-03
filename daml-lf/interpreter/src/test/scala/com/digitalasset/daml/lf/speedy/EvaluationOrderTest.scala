84b822317b7cb3fa527ce6269ee4af21fe62e5af
Carl Pulley <carl.pulley@digitalasset.com>
2022-08-03 15:54:52 +0100

    Added in LF testig

--- daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/EvaluationOrderTest.scala
+++ daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/EvaluationOrderTest.scala
@@ -4,7 +4,8 @@
 package com.daml.lf
 package speedy
 
-import com.daml.lf.data.{FrontStack, ImmArray, Ref}
+import com.daml.lf.command.ContractMetadata
+import com.daml.lf.data.{FrontStack, ImmArray, Ref, Time}
 import com.daml.lf.data.Ref.{Location, Party}
 import com.daml.lf.interpretation.{Error => IE}
 import com.daml.lf.language.Ast._
@@ -337,6 +338,21 @@ class EvaluationOrderTest extends AnyFreeSpec with Matchers with Inside {
       "agreement",
     ),
   )
+
+  private[this] def buildDisclosedContract(signatory: Party): Versioned[DisclosedContract] =
+    Versioned(
+      TransactionVersion.minExplicitDisclosure,
+      DisclosedContract(
+        Dummy,
+        SContractId(cId),
+        SRecord(
+          Dummy,
+          ImmArray(Ref.Name.assertFromString("signatory")),
+          ArrayList(SParty(signatory)),
+        ),
+        ContractMetadata(Time.Timestamp.now(), None, ImmArray.Empty),
+      ),
+    )
 
   private[this] val visibleContract = buildContract(bob)
   private[this] val nonVisibleContract = buildContract(alice)
@@ -2145,6 +2161,27 @@ class EvaluationOrderTest extends AnyFreeSpec with Matchers with Inside {
 
           inside(res) { case Success(Left(SErrorDamlException(IE.FailedAuthorization(_, _)))) =>
             msgs shouldBe Seq("starts test")
+          }
+        }
+      }
+
+      "a disclosed contract" - {
+
+        // TEST_EVIDENCE: Semantics: Evaluation order of fetch of a wrongly typed disclosed contract
+        "wrongly typed contract" in {
+          val (result, events) = evalUpdateApp(
+            pkgs,
+            e"""\(sig : Party) (fetchingParty: Party) (cId1: ContractId M:Dummy) ->
+             let cId2: ContractId M:T = COERCE_CONTRACT_ID @M:Dummy @M:T cId1
+             in Test:fetch_by_id fetchingParty cId2""",
+            Array(SParty(alice), SParty(alice), SContractId(cId)),
+            Set(alice),
+            disclosedContracts = ImmArray(buildDisclosedContract(alice)),
+          )
+
+          inside(result) {
+            case Success(Left(SErrorDamlException(IE.WronglyTypedContract(`cId`, T, Dummy)))) =>
+              events shouldBe Seq("starts test")
           }
         }
       }
