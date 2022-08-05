538f3a7270a24286405653977ce2bf25df1c6497
Stephen Compall <stephen.compall@daml.com>
2022-08-05 15:43:22 -0400

    missing copyright header
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ledger-service/fetch-contracts/src/main/scala/fetchcontracts/domain/ContractTypeId.scala
+++ ledger-service/fetch-contracts/src/main/scala/fetchcontracts/domain/ContractTypeId.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.fetchcontracts.domain
 
 import com.daml.ledger.api.{v1 => lav1}
