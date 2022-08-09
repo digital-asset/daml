500c94971a3ff9e9aa8a05f39ad00f906f053756
Nick Chapman <nick.chapman@digitalasset.com>
2022-08-09 13:00:14 +0100

    prefer Control to throw
    changelog_begin
    changelog_end
    
    kill: SEDamlException

--- daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Speedy.scala
+++ daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Speedy.scala
@@ -551,6 +551,8 @@ private[lf] object Speedy {
                   val ctx = onLedger.ptx.finish
                   SResultFinal(value, Some(ctx))
               }
+            case Control.Error(ie) =>
+              SResultError(SErrorDamlException(ie))
           }
         }
         loop()
@@ -1107,6 +1109,7 @@ private[lf] object Speedy {
     final case class Value(v: SValue) extends Control
     final case class Question(res: SResult) extends Control
     final case class Complete(res: SValue) extends Control
+    final case class Error(err: interpretation.Error) extends Control
   }
 
   /** Kont, or continuation. Describes the next step for the machine
@@ -1616,7 +1619,7 @@ private[lf] object Speedy {
         machine.kontStack.clear()
         machine.env.clear()
         machine.envBase = 0
-        throw SErrorDamlException(
+        Control.Error(
           IError.UnhandledException(excep.ty, excep.value.toUnnormalizedValue)
         )
     }
