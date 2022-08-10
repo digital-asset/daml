2be1a2d4dcd9ad3c052b4b1474df871169f34bbe
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-08-10 14:54:18 +0200

    .

--- build.sh
+++ build.sh
@@ -38,12 +38,13 @@ ls -al
 echo ---------- uname -m -----------
 uname -m
 echo ---------- if -----------
-$(if [ "macos-m1" == "$1" ]; then echo "--host-platform=//:ci-m1 --platforms=//:ci-m1"; fi)
+echo "\$1=$1"
+$(if [ "_macos-m1" == "$1" ]; then echo "--host-platform=//:ci-m1 --platforms=//:ci-m1"; fi)
 echo ---------------------
 
 # Bazel test only builds targets that are dependencies of a test suite so do a full build first.
 bazel build //... \
-  $(if [ "macos-m1" == "$1" ]; then echo "--host-platform=//:ci-m1 --platforms=//:ci-m1"; fi) \
+  $(if [ "_macos-m1" == "$1" ]; then echo "--host-platform=//:ci-m1 --platforms=//:ci-m1"; fi) \
   --toolchain_resolution_debug \
   --build_tag_filters "$tag_filter" \
   --profile build-profile.json \
