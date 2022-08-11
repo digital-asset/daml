aab2725e8a40f78290ad5ce7931207cefd40ced6
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-08-11 16:47:40 +0200

    remove use of custom platform

--- build.sh
+++ build.sh
@@ -57,7 +57,6 @@ arch -arm64 bazel clean --expunge
 
 # Bazel test only builds targets that are dependencies of a test suite so do a full build first.
 arch -arm64 bazel build //ci/... \
-  $(if [ "_macos-m1" == "$1" ]; then echo "--host_platform=//:ci-m1 --platforms=//:ci-m1"; fi) \
   --toolchain_resolution_debug \
   --build_tag_filters "$tag_filter" \
   --profile build-profile.json \
