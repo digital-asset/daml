4282ae396b06d06ead229f2dd763c66bb62d997f
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2022-08-16 17:41:15 +0100

    Move v1_15 ledger-api-tests into own suite

--- ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_15.scala
+++ ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_15.scala
@@ -6,10 +6,10 @@ package com.daml.ledger.api.testtool.suites
 import com.daml.ledger.api.testtool.infrastructure.LedgerTestSuite
 import com.daml.ledger.api.tls.TlsConfiguration
 
-package object v1_dev {
+package object v1_15 {
   def default(timeoutScaleFactor: Double): Vector[LedgerTestSuite] =
-    v1_15.default(timeoutScaleFactor)
+    v1_14.default(timeoutScaleFactor) :+ new InterfaceIT
 
   def optional(tlsConfig: Option[TlsConfiguration]): Vector[LedgerTestSuite] =
-    v1_15.optional(tlsConfig)
+    v1_14.optional(tlsConfig)
 }
