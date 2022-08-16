26b48133b4796306b2f24e8b9e253f6bea86fc04
Carl Pulley <106966370+carlpulley-da@users.noreply.github.com>
2022-08-16 14:15:47 +0100

    Test expected behaviour when superfluous (i.e. unused) contracts are disclosed in commands (#14617)
    * Resolves #14350
    
    CHANGELOG_BEGIN
    
    * Engine/speedy-level tests for explicit disclosure (#14227): Test expected behaviour when superfluous (i.e. unused) contracts are disclosed in commands.
    
    CHANGELOG_END

--- daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/Transaction.scala
+++ daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/Transaction.scala
@@ -701,7 +701,7 @@ object Transaction {
     * @param nodeSeeds        : An association list that maps to each ID of create and exercise
     *                         nodes its seeds.
     * @param globalKeyMapping : input key mapping inferred by interpretation
-    * @param disclosures      : contracts explicitly disclosed to this transaction
+    * @param disclosures      : contracts passed via explicit disclosure that have been used in this transaction
     */
   final case class Metadata(
       submissionSeed: Option[crypto.Hash],
