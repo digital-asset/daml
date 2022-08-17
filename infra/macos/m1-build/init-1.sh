8e44f3b8bf71a2cb4a8e7cf33d875403f2de7101
Edward Newman <edward@digitalasset.com>
2022-08-17 07:57:22 -0400

    M1 build setup using Packer & Tart (#14635)
    * M1 build setup using Packer
    
    * Add change log
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * Update infra/macos/m1-build/init-2.sh
    
    Co-authored-by: Gary Verhaegen <gary.verhaegen@digitalasset.com>
    
    Co-authored-by: Gary Verhaegen <gary.verhaegen@digitalasset.com>

---
+++ infra/macos/m1-build/init-1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+echo "nix" > "$1/private/etc/synthetic.conf"
+
+# Increase shared memory allocation for PostGresQL on MacOS
+cat > "$1/private/etc/sysctl.conf" <<END
+kern.sysv.shmmax=16777216
+kern.sysv.shmmin=1
+kern.sysv.shmmni=128
+kern.sysv.shmseg=32
+kern.sysv.shmall=4096
+END
---
+++ infra/macos/m1-build/init-3.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+# Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+
+# Note: this script is ran as root on VM creation as per the Vagrantfile.
+
+set -euo pipefail
+
+if [ "$1" = "unset" ] || [ "$2" = "unset" ]; then
+    echo "Please set the VSTS_TOKEN and GUEST_NAME env vars before running \`vagrant up\`." >&2
+    exit 1
+fi
+
+LOGFILE=/Users/admin/run.log
+touch $LOGFILE
+chmod a+w $LOGFILE
+
+log () {
+    echo $(/bin/date -u +%Y-%m-%dT%H:%M:%S%z) [$SECONDS] $1 >> $LOGFILE
+}
+
+log "Starting init script."
+
+su -l vsts <<AGENT_SETUP
+set -euo pipefail
+
+export PATH="/opt/homebrew/bin:\$PATH"
+
+VSTS_ACCOUNT=digitalasset
+VSTS_POOL=macOS-pool
+VSTS_TOKEN=$1
+
+mkdir -p ~/agent
+cd ~/agent
+echo 'assignment=m1-builds' > .capabilities
+
+echo Determining matching VSTS agent...
+VSTS_AGENT_RESPONSE=\$(curl -sSfL \
+  -u "user:\$VSTS_TOKEN" \
+  -H 'Accept:application/json;api-version=3.0-preview' \
+  "https://\$VSTS_ACCOUNT.visualstudio.com/_apis/distributedtask/packages/agent?platform=osx-x64")
+
+VSTS_AGENT_URL=\$(echo "\$VSTS_AGENT_RESPONSE" \
+  | jq -r '.value | map([.version.major,.version.minor,.version.patch,.downloadUrl]) | sort | .[length-1] | .[3]')
+
+if [ -z "\$VSTS_AGENT_URL" -o "\$VSTS_AGENT_URL" == "null" ]; then
+  echo 1>&2 error: could not determine a matching VSTS agent - check that account \\'\$VSTS_ACCOUNT\\' is correct and the token is valid for that account
+  exit 1
+fi
+
+echo Downloading and installing VSTS agent...
+curl -sSfL "\$VSTS_AGENT_URL" | tar -xz --no-same-owner
+
+set +u
+source ./env.sh
+set -u
+
+./config.sh \
+  --acceptTeeEula \
+  --agent "$2" \
+  --auth PAT \
+  --pool "\$VSTS_POOL" \
+  --replace \
+  --token "\$VSTS_TOKEN" \
+  --unattended \
+  --url "https://\$VSTS_ACCOUNT.visualstudio.com"
+AGENT_SETUP
+
+## Remount Nix partition
+hdiutil attach /System/Volumes/Data/Nix.dmg.sparseimage -mountpoint /nix
+
+su -l vsts <<END
+hdiutil attach /var/tmp/bazel_cache.dmg.sparseimage -mountpoint /var/tmp/_bazel_vsts
+hdiutil attach /var/tmp/disk_cache.dmg.sparseimage -mountpoint /Users/vsts/.bazel-cache
+END
+
+## Hardening
+chown -R root:wheel /Users/vsts/agent/{*.sh,bin,externals}
+
+log "Done installing VSTS agent."
+
+# run the fake local webserver, taken from the docker image
+web-server() {
+  while true; do
+    printf 'HTTP/1.1 302 Found\r\nLocation: https://%s.visualstudio.com/_admin/_AgentPool\r\n\r\n' "digitalasset" | /opt/homebrew/bin/nc -l -p 80 > /dev/null
+  done
+}
+web-server &
+
+log "Started web server."
+
+# Start the VSTS agent
+log "Starting agent..."
+su -l vsts <<END
+cd /Users/vsts/agent
+./run.sh >> $LOGFILE &
+END
