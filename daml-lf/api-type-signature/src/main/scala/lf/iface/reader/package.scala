b09d7b8b12bd57e50e2491ea7d912803ac0992fb
Stephen Compall <stephen.compall@daml.com>
2022-08-22 15:01:43 -0400

    rename InterfaceReader to SignatureReader in deprecation note

--- daml-lf/api-type-signature/src/main/scala/lf/iface/reader/package.scala
+++ daml-lf/api-type-signature/src/main/scala/lf/iface/reader/package.scala
@@ -14,7 +14,7 @@ package object reader {
   type Errors[K, A] = tsr.Errors[K, A]
   // @deprecated("moved to typesig.reader.Errors", since = "2.4.0")
   final val Errors = tsr.Errors
-  // @deprecated("moved to typesig.reader.InterfaceReader", since = "2.4.0")
+  // @deprecated("renamed to typesig.reader.SignatureReader", since = "2.4.0")
   final val InterfaceReader = tsr.SignatureReader
   // @deprecated("moved to typesig.reader.InterfaceReaderMain", since = "2.4.0")
   final val InterfaceReaderMain = tsr.InterfaceReaderMain
