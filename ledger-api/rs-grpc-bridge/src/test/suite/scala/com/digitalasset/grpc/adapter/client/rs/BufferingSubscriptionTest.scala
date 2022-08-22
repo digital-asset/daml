3bf74a29b3df4a26eff66c72c5efcaa74071c160
Nicu Reut <nicu.reut@digitalasset.com>
2022-08-22 15:55:31 +0200

    Add test for scenario where elements are received

--- ledger-api/rs-grpc-bridge/src/test/suite/scala/com/digitalasset/grpc/adapter/client/rs/BufferingSubscriptionTest.scala
+++ ledger-api/rs-grpc-bridge/src/test/suite/scala/com/digitalasset/grpc/adapter/client/rs/BufferingSubscriptionTest.scala
@@ -4,7 +4,10 @@
 package com.daml.grpc.adapter.client.rs
 
 import com.daml.grpc.adapter.SingleThreadExecutionSequencerPool
-import com.daml.grpc.adapter.client.rs.BufferingSubscriptionTest.MockClientCallStreamObserver
+import com.daml.grpc.adapter.client.rs.BufferingSubscriptionTest.{
+  MockClientCallStreamObserver,
+  NoOpSubscriber,
+}
 import io.grpc.stub.ClientCallStreamObserver
 import org.reactivestreams.{Subscriber, Subscription}
 import org.scalatest.concurrent.Eventually.eventually
@@ -16,21 +19,20 @@ class BufferingSubscriptionTest extends AnyWordSpecLike with Matchers {
   private val sequencePool = new SingleThreadExecutionSequencerPool("test")
 
   "Buffering subscription" should {
+    "propagate cancellation when elements are sent" in {
+      val observer = new MockClientCallStreamObserver()
+      val subscription = newBufferingSubscription(observer)
+      subscription.onNextElement(observer)
+      subscription.cancel()
+      eventually {
+        subscription.isCancelled should be(true)
+        observer.cancelled should be(true)
+      }
+    }
 
     "propagate cancellation when no elements were sent" in {
       val observer = new MockClientCallStreamObserver()
-      val subscription = new BufferingSubscription[String](
-        (_: Subscriber[_ >: String]) => (),
-        () => observer,
-        sequencePool.getExecutionSequencer,
-        new Subscriber[String] {
-          override def onSubscribe(s: Subscription): Unit = ???
-          override def onNext(t: String): Unit = ???
-          override def onError(t: Throwable): Unit = ???
-          override def onComplete(): Unit = ???
-        },
-        "test",
-      )
+      val subscription = newBufferingSubscription(observer)
       subscription.cancel()
       eventually {
         subscription.isCancelled should be(true)
@@ -38,9 +40,26 @@ class BufferingSubscriptionTest extends AnyWordSpecLike with Matchers {
       }
     }
   }
+  private def newBufferingSubscription(
+      observer: MockClientCallStreamObserver
+  ) = {
+    new BufferingSubscription[String](
+      (_: Subscriber[_ >: String]) => (),
+      () => observer,
+      sequencePool.getExecutionSequencer,
+      NoOpSubscriber,
+      "test",
+    )
+  }
 }
 
 object BufferingSubscriptionTest {
+  object NoOpSubscriber extends Subscriber[String] {
+    override def onSubscribe(s: Subscription): Unit = ???
+    override def onNext(t: String): Unit = ???
+    override def onError(t: Throwable): Unit = ???
+    override def onComplete(): Unit = ???
+  }
   class MockClientCallStreamObserver extends ClientCallStreamObserver[String] {
     @volatile
     var cancelled = false
