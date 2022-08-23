115d364cdcef7e187e3c822a69f21078c4bd83f4
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2022-08-23 12:50:04 +0100

    Move InterfaceSubscriptionsIT test back into 1.dev suite

--- ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_dev.scala
+++ ledger/ledger-api-tests/suites/src/main/scala/com/daml/ledger/api/testtool/suites/v1_dev.scala
@@ -8,7 +8,9 @@ import com.daml.ledger.api.tls.TlsConfiguration
 
 package object v1_dev {
   def default(timeoutScaleFactor: Double): Vector[LedgerTestSuite] =
-    v1_15.default(timeoutScaleFactor)
+    v1_15.default(timeoutScaleFactor) ++ Vector(
+      new InterfaceSubscriptionsIT,
+    )
 
   def optional(tlsConfig: Option[TlsConfiguration]): Vector[LedgerTestSuite] =
     v1_15.optional(tlsConfig)
