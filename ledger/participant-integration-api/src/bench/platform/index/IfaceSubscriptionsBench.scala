f1100331e474dce25680bf4e96bbe3b57293a992
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-08-25 14:20:07 +0200

    copyright

--- ledger/participant-integration-api/src/bench/platform/index/IfaceSubscriptionsBench.scala
+++ ledger/participant-integration-api/src/bench/platform/index/IfaceSubscriptionsBench.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.platform.index
 
 import com.daml.ledger.api.domain.{Filters, InclusiveFilters, InterfaceFilter, TransactionFilter}
