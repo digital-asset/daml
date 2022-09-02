008ece97d1c7351d3067283745e67042e5e7f6a2
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-09-02 16:15:07 +0200

    fix copyright

--- ledger/participant-integration-api/src/test/suite/scala/platform/store/dao/events/EventsTableFlatEventsRangeQueriesSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/platform/store/dao/events/EventsTableFlatEventsRangeQueriesSpec.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.platform.store.dao.events
 
 import com.daml.lf.data.Ref
