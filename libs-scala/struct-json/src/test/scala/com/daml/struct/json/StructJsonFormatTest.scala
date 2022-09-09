f623e13655cfb5fb771c699fa41cef724e305642
Simon Maxen <simon.maxen@digitalasset.com>
2022-09-09 18:16:18 +0100

    Add libs-scala/struct-json
    changelog_begin
    changelog_end

--- libs-scala/struct-json/src/test/scala/com/daml/struct/json/StructJsonFormatTest.scala
+++ libs-scala/struct-json/src/test/scala/com/daml/struct/json/StructJsonFormatTest.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.http.json
+package com.daml.struct.json
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
