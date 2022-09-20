4f5c7c08f25125fc106370266e0786350c2db3dc
Stephen Compall <stephen.compall@daml.com>
2022-09-20 10:57:42 +0200

    fix QueryStoreDependent reference

--- ledger-service/http-json-oracle/src/itlib/scala/http/HttpServiceWithOracleIntTest.scala
+++ ledger-service/http-json-oracle/src/itlib/scala/http/HttpServiceWithOracleIntTest.scala
@@ -4,7 +4,7 @@
 package com.daml.http
 
 abstract class HttpServiceWithOracleIntTest(override val disableContractPayloadIndexing: Boolean)
-    extends QueryStoreDependentTokenIndependentTests
+    extends AbstractHttpServiceIntegrationTestTokenIndependent
     with HttpServiceOracleInt {
 
   override final def testLargeQueries = disableContractPayloadIndexing
