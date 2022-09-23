f7b939839af6bc823de92e8b36630582d2e237cf
Chun Lok Ling <chunlok.ling@digitalasset.com>
2022-09-23 14:59:02 +0100

    format

--- language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/ContractDecoder.java
+++ language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/ContractDecoder.java
@@ -1,8 +1,10 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.ledger.javaapi.data.codegen;
 
 import com.daml.ledger.javaapi.data.CreatedEvent;
 import com.daml.ledger.javaapi.data.Identifier;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
@@ -10,24 +12,34 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class ContractDecoder {
-    private final Map<Identifier, ? extends ContractCompanion<? extends Contract<?, ?>, ?, ?>> companions;
+  private final Map<Identifier, ? extends ContractCompanion<? extends Contract<?, ?>, ?, ?>>
+      companions;
 
-    public ContractDecoder(Iterable<? extends ContractCompanion<? extends Contract<?, ?>, ?, ?>> companions) {
-        this.companions = StreamSupport.stream(companions.spliterator(), false)
-                .collect(Collectors.toMap(c -> c.TEMPLATE_ID, Function.identity()));
-    }
+  public ContractDecoder(
+      Iterable<? extends ContractCompanion<? extends Contract<?, ?>, ?, ?>> companions) {
+    this.companions =
+        StreamSupport.stream(companions.spliterator(), false)
+            .collect(Collectors.toMap(c -> c.TEMPLATE_ID, Function.identity()));
+  }
 
-    public Contract<?, ?> fromCreatedEvent(CreatedEvent event) throws IllegalArgumentException {
-        Identifier templateId = event.getTemplateId();
-        ContractCompanion<? extends Contract<?, ?>, ?, ?> companion = getContractCompanion(templateId).orElseThrow(() -> new IllegalArgumentException("No template found for identifier " + templateId));
-        return companion.fromCreatedEvent(event);
-    }
+  public Contract<?, ?> fromCreatedEvent(CreatedEvent event) throws IllegalArgumentException {
+    Identifier templateId = event.getTemplateId();
+    ContractCompanion<? extends Contract<?, ?>, ?, ?> companion =
+        getContractCompanion(templateId)
+            .orElseThrow(
+                () ->
+                    new IllegalArgumentException("No template found for identifier " + templateId));
+    return companion.fromCreatedEvent(event);
+  }
 
-    public Optional<? extends ContractCompanion<? extends Contract<?, ?>, ?, ?>> getContractCompanion(Identifier templateId) {
-        return Optional.ofNullable(companions.get(templateId));
-    }
+  public Optional<? extends ContractCompanion<? extends Contract<?, ?>, ?, ?>> getContractCompanion(
+      Identifier templateId) {
+    return Optional.ofNullable(companions.get(templateId));
+  }
 
-    public Optional<Function<CreatedEvent, com.daml.ledger.javaapi.data.Contract>> getDecoder(Identifier templateId) {
-        return Optional.ofNullable(companions.get(templateId)).map(companion -> companion::fromCreatedEvent);
-    }
+  public Optional<Function<CreatedEvent, com.daml.ledger.javaapi.data.Contract>> getDecoder(
+      Identifier templateId) {
+    return Optional.ofNullable(companions.get(templateId))
+        .map(companion -> companion::fromCreatedEvent);
+  }
 }
