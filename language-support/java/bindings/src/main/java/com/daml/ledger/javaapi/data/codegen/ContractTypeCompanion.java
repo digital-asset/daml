bdc9c4772365db1b6e88d43975eb2d7a323e3585
Fayi Femi-Balogun <fayimora.femibalogun@digitalasset.com>
2022-09-29 16:40:30 +0100

    #13766 Add marker type to ContractTypeCompanion

--- language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/ContractTypeCompanion.java
+++ language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/ContractTypeCompanion.java
@@ -8,13 +8,13 @@ import com.daml.ledger.javaapi.data.Identifier;
 import java.util.List;
 
 /** The commonality between {@link ContractCompanion} and {@link InterfaceCompanion}. */
-public abstract class ContractTypeCompanion {
+public abstract class ContractTypeCompanion<Marker> {
   /** The full template ID of the template or interface that defined this companion. */
   public final Identifier TEMPLATE_ID;
   //TODO: Need to figure out the right type params here
-  private final List<ChoiceMetadata<?, ?, ?>> choices;
+  private final List<ChoiceMetadata<Marker, ?, ?>> choices;
 
-  protected ContractTypeCompanion(Identifier templateId, List<ChoiceMetadata<?, ?, ?>> choices) {
+  protected ContractTypeCompanion(Identifier templateId, List<ChoiceMetadata<Marker, ?, ?>> choices) {
     TEMPLATE_ID = templateId;
     this.choices = choices;
   }
