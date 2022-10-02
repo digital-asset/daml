db043b4c9e0bfaaf4518b1d9dcae67c8075431bb
Chun Lok Ling <chunlok.ling@digitalasset.com>
2022-10-02 15:41:50 +0100

    test

--- language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/ContractClass.scala
+++ language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/ContractClass.scala
@@ -18,7 +18,7 @@ object ContractClass {
   def builder(
       templateClassName: ClassName,
       key: Option[Type],
-      packagePrefixes: Map[PackageId, Test],
+      packagePrefixes: Map[PackageId, String],
   ) = Builder.create(templateClassName, key, packagePrefixes)
 
   case class Builder private (
