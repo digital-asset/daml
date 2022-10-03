27b7727f21f58dcc03e5af61e10047e2933282e9
Stephen Compall <stephen.compall@daml.com>
2022-10-03 17:36:07 -0400

    some javadoc for DefinedDataType

--- language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/DefinedDataType.java
+++ language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/DefinedDataType.java
@@ -5,6 +5,18 @@ package com.daml.ledger.javaapi.data.codegen;
 
 import com.daml.ledger.javaapi.data.Value;
 
+/**
+ * Either one of these:
+ *
+ * <ol>
+ *   <li>what {@link DamlRecord} describes,
+ *   <li>a variant without type parameters, or
+ *   <li>any Daml enum.
+ * </ol>
+ *
+ * @param <T> A "self type", some subclass of this interface that {@code T} implements.
+ */
 public interface DefinedDataType<T> {
+  /** Produce the encoded form. */
   Value toValue();
 }
