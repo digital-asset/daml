debf73a7e4bcf53c4f405f4fc3bec0a84951c892
Stephen Compall <stephen.compall@daml.com>
2022-10-04 08:51:13 -0400

    finish empty tparam doc

--- language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/DefinedDataType.java
+++ language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/codegen/DefinedDataType.java
@@ -6,13 +6,16 @@ package com.daml.ledger.javaapi.data.codegen;
 import com.daml.ledger.javaapi.data.Value;
 
 /**
- * Either one of these:
+ * The codegen-decoded form of any of these:
  *
  * <ol>
  *   <li>what {@link DamlRecord} describes,
- *   <li>a variant without type parameters, or
- *   <li>any Daml enum.
+ *   <li>a {@link Variant} without type parameters, or
+ *   <li>any {@link DamlEnum}.
  * </ol>
+ *
+ * <p>Its encoded counterpart is {@link com.daml.ledger.javaapi.data.Value}, which can be produced
+ * with {@link #toValue}.
  *
  * @param <T> A "self type", some subclass of this interface that {@code T} implements.
  */
