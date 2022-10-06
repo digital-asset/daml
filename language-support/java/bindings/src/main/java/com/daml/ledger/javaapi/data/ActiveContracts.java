783383c68e013a217652a814397c6e7df978f0e0
Chun Lok Ling <chunlok.ling@digitalasset.com>
2022-10-06 11:56:45 +0100

    Ct

--- language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/ActiveContracts.java
+++ language-support/java/bindings/src/main/java/com/daml/ledger/javaapi/data/ActiveContracts.java
@@ -56,7 +56,7 @@ public class ActiveContracts<Ct> implements WorkflowEvent {
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
-    ActiveContracts<Ct> that = (ActiveContracts<Ct>) o;
+    ActiveContracts<?> that = (ActiveContracts<?>) o;
     return Objects.equals(offset, that.offset)
         && Objects.equals(activeContracts, that.activeContracts)
         && Objects.equals(workflowId, that.workflowId);
