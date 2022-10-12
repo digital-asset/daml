e9a5fe379991aaeb9fe94e8781c6e8ad057cb3de
Andreas Triantafyllos <andreas.triantafyllos@digitalasset.com>
2022-10-12 13:10:09 +0200

    Fixed InstrumentedExecutorServiceForDocs.

--- ledger/metrics/src/main/scala/com/daml/metrics/InstrumentedExecutorServiceForDocs.scala
+++ ledger/metrics/src/main/scala/com/daml/metrics/InstrumentedExecutorServiceForDocs.scala
@@ -15,16 +15,7 @@ class InstrumentedExecutorServiceForDocs(name: MetricName) {
                     |https://www.javadoc.io/doc/io.dropwizard.metrics/metrics-core/latest/com/codahale/metrics/InstrumentedExecutorService.html""",
     qualification = Debug,
   )
-  val submitted: Meter = Meter(name, null)
-
-  @MetricDoc.Tag(
-    summary = "The number of tasks completed in an instrumented executor.",
-    description = """Thread pools within the ledger api server are instrumented using the
-                    |dropwizard's InstrumentedExecutorService:
-                    |https://www.javadoc.io/doc/io.dropwizard.metrics/metrics-core/latest/com/codahale/metrics/InstrumentedExecutorService.html""",
-    qualification = Debug,
-  )
-  val completed: Meter = Meter(name, null)
+  val submitted: Meter = Meter(name :+ "submitted", null)
 
   @MetricDoc.Tag(
     summary = "The number of tasks running in an instrumented executor.",
@@ -33,7 +24,16 @@ class InstrumentedExecutorServiceForDocs(name: MetricName) {
                     |https://www.javadoc.io/doc/io.dropwizard.metrics/metrics-core/latest/com/codahale/metrics/InstrumentedExecutorService.html""",
     qualification = Debug,
   )
-  val running: Counter = Counter(name, null)
+  val running: Counter = Counter(name :+ "running", null)
+
+  @MetricDoc.Tag(
+    summary = "The number of tasks completed in an instrumented executor.",
+    description = """Thread pools within the ledger api server are instrumented using the
+                    |dropwizard's InstrumentedExecutorService:
+                    |https://www.javadoc.io/doc/io.dropwizard.metrics/metrics-core/latest/com/codahale/metrics/InstrumentedExecutorService.html""",
+    qualification = Debug,
+  )
+  val completed: Meter = Meter(name :+ "completed", null)
 
   @MetricDoc.Tag(
     summary = "The time that a task is idle in an instrumented executor.",
@@ -42,7 +42,7 @@ class InstrumentedExecutorServiceForDocs(name: MetricName) {
                     |https://www.javadoc.io/doc/io.dropwizard.metrics/metrics-core/latest/com/codahale/metrics/InstrumentedExecutorService.html""",
     qualification = Debug,
   )
-  val idle: Timer = Timer(name, null)
+  val idle: Timer = Timer(name :+ "idle", null)
 
   @MetricDoc.Tag(
     summary = "The duration of a task is running in an instrumented executor.",
@@ -51,5 +51,5 @@ class InstrumentedExecutorServiceForDocs(name: MetricName) {
                     |https://www.javadoc.io/doc/io.dropwizard.metrics/metrics-core/latest/com/codahale/metrics/InstrumentedExecutorService.html""",
     qualification = Debug,
   )
-  val duration: Timer = Timer(name, null)
+  val duration: Timer = Timer(name :+ "duration", null)
 }
