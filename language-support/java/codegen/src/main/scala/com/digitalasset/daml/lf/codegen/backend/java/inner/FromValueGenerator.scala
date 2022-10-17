8f8487af10a91f801707d185966ffcde09903be2
Stephen Compall <stephen.compall@daml.com>
2022-10-17 12:54:01 -0400

    remove accessor references from the decision branches of extractor
    CHANGELOG_BEGIN
    CHANGELOG_END

--- language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/FromValueGenerator.scala
+++ language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/FromValueGenerator.scala
@@ -206,21 +206,17 @@ private[inner] object FromValueGenerator extends StrictLogging {
     )
 
   // If [[typeName]] is defined in the primitive extractors map, create an extractor for it
-  private def primitive(
+  private[this] def primitive(
       damlType: PrimType,
       apiType: TypeName,
       field: String,
-      accessor: CodeBlock,
-  ): Option[CodeBlock] =
+  ): Option[Extractor] =
     extractors
       .get(damlType)
       .map { extractor =>
         logger.debug(s"Generating primitive extractor for $field of type $apiType")
-        CodeBlock.of(
-          "$T.$L.decode($L)",
-          classOf[PrimitiveValueDecoders],
-          extractor,
-          accessor,
+        Extractor.Decoder(
+          CodeBlock.of("$T.$L", classOf[PrimitiveValueDecoders], extractor)
         )
       }
 
@@ -229,6 +225,19 @@ private[inner] object FromValueGenerator extends StrictLogging {
       ".orElseThrow(() -> new IllegalArgumentException($S))",
       s"Expected $field to be of type $typeName",
     )
+
+  private[this] sealed abstract class Extractor extends Product with Serializable {
+    import Extractor._
+    def extract(accessor: CodeBlock): CodeBlock = this match {
+      case Decoder(decoder) => CodeBlock.of("$L$Z.decode($L)", decoder, accessor)
+      case Lambda(decodeAccessor) => decodeAccessor(accessor)
+    }
+  }
+
+  private[this] object Extractor {
+    final case class Lambda(decodeAccessor: CodeBlock => CodeBlock) extends Extractor
+    final case class Decoder(decoder: CodeBlock) extends Extractor
+  }
 
   /** Generates extractor for types that are not immediately covered by primitive extractors
     * @param damlType The type of the field being accessed
@@ -249,25 +258,28 @@ private[inner] object FromValueGenerator extends StrictLogging {
     lazy val javaType = toJavaTypeName(damlType, packagePrefixes)
     logger.debug(s"Generating composite extractor for $field of type $javaType")
 
-    def oneTypeArgPrim(primFun: String, param: Type): CodeBlock = {
+    import Extractor._
+
+    def oneTypeArgPrim(primFun: String, param: Type): Extractor = {
       val singleArg = args.next()
-      CodeBlock.of(
-        "$T.$L($L ->$>$W$L$<)$Z.decode($L)",
-        classOf[PrimitiveValueDecoders],
-        primFun,
-        singleArg,
-        extractor(param, singleArg, CodeBlock.of("$L", singleArg), args, packagePrefixes),
-        accessor,
+      Decoder(
+        CodeBlock.of(
+          "$T.$L($L ->$>$W$L$<)",
+          classOf[PrimitiveValueDecoders],
+          primFun,
+          singleArg,
+          extractor(param, singleArg, CodeBlock.of("$L", singleArg), args, packagePrefixes),
+        )
       )
     }
 
-    damlType match {
+    val suspendedResult = damlType match {
       // Case #1: the type is actually a type parameter: we assume the calling code defines a
       // suitably named function that takes the result of accessing the underlying data (as
       // defined by the accessor
       // TODO: review aforementioned assumption
       case TypeVar(tvName) =>
-        CodeBlock.of("fromValue$L.decode($L)", JavaEscaper.escapeString(tvName), accessor)
+        Decoder(CodeBlock.of("fromValue$L", JavaEscaper.escapeString(tvName)))
 
       case TypePrim(PrimTypeList, ImmArraySeq(param)) =>
         oneTypeArgPrim("fromList", param)
@@ -276,44 +288,48 @@ private[inner] object FromValueGenerator extends StrictLogging {
         oneTypeArgPrim("fromOptional", param)
 
       case TypePrim(PrimTypeContractId, ImmArraySeq(TypeVar(name))) =>
-        CodeBlock.of(
-          "$T.fromContractId(fromValue$L)$Z.decode($L)",
-          classOf[PrimitiveValueDecoders],
-          JavaEscaper.escapeString(name),
-          accessor,
+        Decoder(
+          CodeBlock.of(
+            "$T.fromContractId(fromValue$L)",
+            classOf[PrimitiveValueDecoders],
+            JavaEscaper.escapeString(name),
+          )
         )
       case TypePrim(PrimTypeContractId, ImmArraySeq(_)) =>
-        CodeBlock.of(
-          "new $T($L.asContractId()$L.getValue())",
-          javaType,
-          accessor,
-          orElseThrow(apiType, field),
+        Lambda(accessor =>
+          CodeBlock.of(
+            "new $T($L.asContractId()$L.getValue())",
+            javaType,
+            accessor,
+            orElseThrow(apiType, field),
+          )
         )
       case TypePrim(PrimTypeTextMap, ImmArraySeq(param)) =>
         oneTypeArgPrim("fromTextMap", param)
 
       case TypePrim(PrimTypeGenMap, ImmArraySeq(keyType, valueType)) =>
         val entryArg = args.next()
-        CodeBlock.of(
-          "$T.fromGenMap($L ->$>$W$L$<,$W$L ->$>$W$L$<)$Z.decode($L)",
-          classOf[PrimitiveValueDecoders],
-          entryArg,
-          extractor(keyType, entryArg, CodeBlock.of("$L", entryArg), args, packagePrefixes),
-          entryArg,
-          extractor(valueType, entryArg, CodeBlock.of("$L", entryArg), args, packagePrefixes),
-          accessor,
+        Decoder(
+          CodeBlock.of(
+            "$T.fromGenMap($L ->$>$W$L$<,$W$L ->$>$W$L$<)",
+            classOf[PrimitiveValueDecoders],
+            entryArg,
+            extractor(keyType, entryArg, CodeBlock.of("$L", entryArg), args, packagePrefixes),
+            entryArg,
+            extractor(valueType, entryArg, CodeBlock.of("$L", entryArg), args, packagePrefixes),
+          )
         )
 
       case TypeNumeric(_) =>
-        CodeBlock.of("$T.fromNumeric.decode($L)", classOf[PrimitiveValueDecoders], accessor)
+        Decoder(CodeBlock.of("$T.fromNumeric", classOf[PrimitiveValueDecoders]))
 
       case TypePrim(prim, _) =>
-        primitive(prim, apiType, field, accessor).getOrElse(
+        primitive(prim, apiType, field).getOrElse(
           sys.error(s"Unhandled primitive type $prim")
         )
 
       case TypeCon(_, ImmArraySeq()) =>
-        CodeBlock.of("$T.valueDecoder().decode($L)", javaType, accessor)
+        Decoder(CodeBlock.of("$T.valueDecoder()", javaType))
 
       case TypeCon(_, typeParameters) =>
         val (targs, valueDecoders) = typeParameters.map {
@@ -337,12 +353,15 @@ private[inner] object FromValueGenerator extends StrictLogging {
         }.unzip
 
         val targsCode = CodeBlock.join(targs.map(CodeBlock.of("$L", _)).asJava, ", ")
-        CodeBlock
-          .builder()
-          .add(CodeBlock.of("$T.<$L>valueDecoder(", javaType.rawType, targsCode))
-          .add(CodeBlock.join(valueDecoders.asJava, ", "))
-          .add(CodeBlock.of(").decode($L)", accessor))
-          .build()
+        Decoder(
+          CodeBlock
+            .builder()
+            .add(CodeBlock.of("$T.<$L>valueDecoder(", javaType.rawType, targsCode))
+            .add(CodeBlock.join(valueDecoders.asJava, ", "))
+            .add(CodeBlock.of(")"))
+            .build()
+        )
     }
+    suspendedResult extract accessor
   }
 }
