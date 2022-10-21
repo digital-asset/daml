aabdbadcc4ed58b9a01f65ddaa7c670731dcf1fd
Carl Pulley <carl.pulley@digitalasset.com>
2022-10-21 12:23:07 +0100

    Testing patchup

--- triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/DevOnly.scala
+++ triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/DevOnly.scala
@@ -7,12 +7,14 @@ import akka.stream.scaladsl.Flow
 import com.daml.lf.data.Ref._
 import com.daml.ledger.api.testing.utils.SuiteResourceManagementAroundAll
 import com.daml.ledger.api.v1.commands.CreateCommand
+import com.daml.ledger.api.v1.event.{CreatedEvent, Event}
+import com.daml.ledger.api.v1.event.Event.Event.Created
 import com.daml.ledger.api.v1.value.{Identifier, Record, RecordField, Value}
 import com.daml.ledger.api.v1.{value => LedgerApi}
 import org.scalatest._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
-import com.daml.lf.engine.trigger.TriggerMsg
+import com.daml.lf.engine.trigger.{TransactionMsg, TriggerMsg}
 
 class DevOnly
     extends AsyncWordSpec
@@ -23,11 +25,13 @@ class DevOnly
     with TryValues {
   self: Suite =>
 
+  import DevOnly._
+
   this.getClass.getSimpleName can {
     "InterfaceTest" should {
       val triggerId = QualifiedName.assertFromString("Interface:test")
       val tId = LedgerApi.Identifier(packageId, "Interface", "Asset")
-      "1 transfer" in {
+      "1 transfer" ignore {
         for {
           client <- ledgerClient()
           party <- allocateParty(client)
@@ -51,6 +55,8 @@ class DevOnly
 
       "succeed with all templates and interfaces registered" in {
         val triggerId = QualifiedName.assertFromString("InterfaceTriggers:globalTrigger")
+
+        var triggerMessages = Seq.empty[TriggerMsg]
 
         for {
           client <- ledgerClient()
@@ -96,11 +102,25 @@ class DevOnly
           queryResult <- runner.queryACS()
           (acs, _) = queryResult
 
-          // 1 for create of template A
-          // 1 for create of template B
-          _ <- runner.runWithACS(acs, offset, msgFlow = Flow[TriggerMsg].take(2))._2
+          // 1 for ledger create command completion
+          // 1 for ledger create command completion
+          // 1 for transactional create of template A
+          // 1 for transactional create of template B
+          _ <- runner
+            .runWithACS(
+              acs,
+              offset,
+              msgFlow = Flow[TriggerMsg]
+                .map { msg => triggerMessages = triggerMessages :+ msg; msg }
+                .take(4),
+            )
+            ._2
           acs <- queryACS(client, party)
         } yield {
+          triggerMessages.size shouldBe 4
+          triggerMessages(2) shouldHaveCreateArgumentsFor templateA
+          triggerMessages(3) shouldHaveCreateArgumentsFor templateB
+
           acs(templateA) should have length 1
           acs(templateB) should have length 1
         }
@@ -108,6 +128,8 @@ class DevOnly
 
       "succeed with interface registration and implementing template not registered" in {
         val triggerId = QualifiedName.assertFromString("InterfaceTriggers:triggerWithRegistration")
+
+        var triggerMessages = Seq.empty[TriggerMsg]
 
         for {
           client <- ledgerClient()
@@ -159,17 +181,54 @@ class DevOnly
           queryResult <- runner.queryACS()
           (acs, _) = queryResult
 
-          // 1 for create of template A
-          // 1 for create of template B, via interface I
+          // 1 for ledger create command completion
+          // 1 for ledger create command completion
+          // 1 for transactional create of template A
+          // 1 for transactional create of template B, via interface I
           _ <- runner
             .runWithACS(
               acs,
               offset,
-              msgFlow = Flow[TriggerMsg].map { msg => println(s"DEBUGGY: $msg"); msg }.take(2),
+              msgFlow = Flow[TriggerMsg]
+                .map { msg => triggerMessages = triggerMessages :+ msg; msg }
+                .take(4),
             )
             ._2
-        } yield succeed
+        } yield {
+          triggerMessages.size shouldBe 4
+          triggerMessages(2) shouldHaveCreateArgumentsFor templateA
+          triggerMessages(3) shouldHaveNoCreateArgumentsFor templateB
+        }
       }
     }
+  }
+}
+
+object DevOnly extends Matchers with Inside {
+
+  implicit class TriggerMsgTestHelper(msg: TriggerMsg) {
+    def shouldHaveNoCreateArgumentsFor(templateId: Identifier): Assertion =
+      inside(msg) { case TransactionMsg(transaction) =>
+        transaction.events.size shouldBe 1
+        inside(transaction.events.head) {
+          case Event(
+                Created(CreatedEvent(_, _, Some(`templateId`), _, None, _, Seq(_), _, _, _, _, _))
+              ) =>
+            succeed
+        }
+      }
+
+    def shouldHaveCreateArgumentsFor(templateId: Identifier): Assertion =
+      inside(msg) { case TransactionMsg(transaction) =>
+        transaction.events.size shouldBe 1
+        inside(transaction.events.head) {
+          case Event(
+                Created(
+                  CreatedEvent(_, _, Some(`templateId`), _, Some(_), _, Seq(_), _, _, _, _, _)
+                )
+              ) =>
+            succeed
+        }
+      }
   }
 }
