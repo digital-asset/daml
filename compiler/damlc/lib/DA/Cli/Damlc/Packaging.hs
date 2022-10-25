da8064de642a5114dcd598db501d91bf5dfdf57c
dylant-da <106664681+dylant-da@users.noreply.github.com>
2022-10-25 12:49:32 +0100

    Add AnyView with HasFromAnyView instances (#15280)
    * Implement AnyView as a stable package / primitive
    
    * Point to updated GHC
    
    * Export AnyView datatype, move typeclasses, fix convertPrim
    
    * Adjust AnyView w/ InterfaceTypeRep, implement {to,from}AnyView
    
    * fix lint, satisfy changelog
    
    CHANGELOG_BEGIN
    CHANGELOG_END
    
    * fix daml-desugar
    
    * Has{From,To}AnyView instances in daml-docs
    
    * Add DA.Internal.Interface.AnyView to LibraryModules for stdlib
    
    * Fix hash for DA.Internal.Interface.AnyView
    
    * Prefix AnyView with CurrentSdk, add to golden for daml-script test
    
    * Remove HasToAnyView
    
    * Split AnyView into AnyView and AnyView.Types
    
    * fix hash
    
    * Remove unused StandaloneDeriving
    
    * Add HasField instances (requires relevant workers)
    
    * Accept desugaring test changes for removal of HasToAnyView
    
    * Update daml-doc for removal/modification of HasToAnyView/HasFromAnyView
    
    * Update StablePackage hash for DA.Internal.Interface.AnyView.Types

--- compiler/damlc/lib/DA/Cli/Damlc/Packaging.hs
+++ compiler/damlc/lib/DA/Cli/Damlc/Packaging.hs
@@ -503,6 +503,7 @@ baseImports =
           , "DA.Validation.Types"
           , "DA.Date.Types"
           , "DA.Time.Types"
+          , "DA.Internal.Interface.AnyView.Types"
           ]
        )
     ]
