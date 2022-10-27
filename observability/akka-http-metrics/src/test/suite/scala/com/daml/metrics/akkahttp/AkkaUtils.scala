e1322bda68680270c296fca7a5a0457489e60ef9
Luc Bourlier <luc.bourlier@digitalasset.com>
2022-10-27 16:28:21 +0200

    Missing copyright header

--- observability/akka-http-metrics/src/test/suite/scala/com/daml/metrics/akkahttp/AkkaUtils.scala
+++ observability/akka-http-metrics/src/test/suite/scala/com/daml/metrics/akkahttp/AkkaUtils.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.metrics.akkahttp
 
 import akka.stream.scaladsl.Source
@@ -24,3 +27,4 @@ object AkkaUtils {
   }
 
 }
+
--- observability/akka-http-metrics/src/test/suite/scala/com/daml/metrics/akkahttp/OpenTelemetryUtils.scala
+++ observability/akka-http-metrics/src/test/suite/scala/com/daml/metrics/akkahttp/OpenTelemetryUtils.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.metrics.akkahttp
 
 import scala.annotation.tailrec
@@ -114,3 +117,4 @@ abstract class TestMetricsBase {
     }
 
   }
+
