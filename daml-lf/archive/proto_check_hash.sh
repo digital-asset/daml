34b76de06524902b7258d2e278a5f2c47ba6024e
Remy Haemmerle <Remy.Haemmerle@daml.com>
2022-11-14 15:05:56 +0100

    update proto_immutability_check

--- daml-lf/archive/proto_check_hash.sh
+++ daml-lf/archive/proto_check_hash.sh
@@ -8,7 +8,7 @@ set -e
 declare -a checkSums=(
  "500eefd480e9af6940adf12e7ec4c2cf4975d4cb9b25096c15edb0d57d364de8  daml-lf/archive/src/stable/protobuf/com/daml/daml_lf_1_14/daml_lf_1.proto"
  "22e549209116e91d8073a255e6d868be60515824c321015cc424f0b83634f199  daml-lf/archive/src/stable/protobuf/com/daml/daml_lf_1_14/daml_lf.proto"
- "b7e6ebf85f99d506c1b9b08ccf9543ac24c3d39ce8ba4583b0d8c1b97e979032  daml-lf/archive/src/stable/protobuf/com/daml/daml_lf_1_15/daml_lf_1.proto"
+ "891475286a476c94b2973bad04886f594811e769968ea1b927fac0e80c179e83  daml-lf/archive/src/stable/protobuf/com/daml/daml_lf_1_15/daml_lf_1.proto"
  "5e6e33e885e80384fcfde6ac5072b7d6a2e2582430a6a449f96cb06d48a0edbf  daml-lf/archive/src/stable/protobuf/com/daml/daml_lf_1_15/daml_lf.proto"
 )
 
