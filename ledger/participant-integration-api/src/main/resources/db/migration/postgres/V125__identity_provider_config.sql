f93812f02d5578fc2b3f409ad66a6b02dfad375b
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-11-15 09:36:05 +0100

    db changes

--- ledger/participant-integration-api/src/main/resources/db/migration/postgres/V125__identity_provider_config.sql
+++ ledger/participant-integration-api/src/main/resources/db/migration/postgres/V125__identity_provider_config.sql
@@ -3,3 +3,11 @@ ALTER TABLE participant_users
 
 ALTER TABLE participant_party_records
     ADD COLUMN identity_provider_id VARCHAR DEFAULT NULL;
+
+CREATE TABLE participant_identity_provider_config
+(
+    identity_provider_id VARCHAR PRIMARY KEY NOT NULL,
+    issuer               VARCHAR             NOT NULL UNIQUE,
+    jwks_url             VARCHAR             NOT NULL,
+    is_deactivated       BOOLEAN             NOT NULL
+);
