bef0f15d709e0650a59680a94c21bb2057241cc3
Remy <remy.haemmerle@daml.com>
2022-11-17 23:44:18 +0100

    [LF] make qualified choice name a LF concept (#15599)

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/UpdateToDbDto.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/UpdateToDbDto.scala
@@ -13,13 +13,11 @@ import com.daml.lf.engine.Blinding
 import com.daml.lf.ledger.EventId
 import com.daml.lf.transaction.Transaction.ChildrenRecursion
 import com.daml.platform.index.index.StatusDetails
-import com.daml.platform.store.ChoiceCoder
 import com.daml.platform.store.dao.JdbcLedgerDao
 import com.daml.platform.store.dao.events._
 import com.daml.platform._
 
 import java.util.UUID
-import scala.annotation.nowarn
 
 object UpdateToDbDto {
 
@@ -217,9 +215,7 @@ object UpdateToDbDto {
                     blinding.disclosure.getOrElse(nodeId, Set.empty).map(_.toString),
                   create_key_value = createKeyValue
                     .map(compressionStrategy.createKeyValueCompression.compress),
-                  exercise_choice = Some(
-                    ChoiceCoder.encode(exercise.interfaceId, exercise.choiceId)
-                  ): @nowarn("msg=deprecated"),
+                  exercise_choice = Some(exercise.qualifiedChoideName.toString),
                   exercise_argument = Some(exerciseArgument)
                     .map(compressionStrategy.exerciseArgumentCompression.compress),
                   exercise_result = exerciseResult
