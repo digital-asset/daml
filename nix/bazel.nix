e106c308905fe0781eb28f950b790d5b675d06f0
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-11-25 17:14:53 +0100

    .

--- nix/bazel.nix
+++ nix/bazel.nix
@@ -146,6 +146,7 @@ let shared = rec {
   # rules_nodejs expects nodejs in a subdirectory of a repository rule.
   # We use a linkFarm to fulfill this requirement.
   nodejsNested = pkgs.linkFarm "nodejs" [ { name = "node_nix"; path = pkgs.nodejs-16_x; }];
+  nodejs14Nested = pkgs.linkFarm "nodejs" [ { name = "node_nix"; path = pkgs.nodejs-14_x; }];
 
   sass = pkgs.sass;
 
