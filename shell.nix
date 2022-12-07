ca1453ee8cb9f3f382cefd2533b519fd6e37ba59
Claudio Bley <103681498+cbley-da@users.noreply.github.com>
2022-12-07 14:09:50 +0100

    Add pre-commit config and install hook in dev-env (#15637)
    * Add `pre-commit` tool to dev-env
    
    * Add pre-commit configuration
    
    Fixes #15522
    
    * Install pre-commit hooks inside the nix shell
    
    * Limit buildifier hook to files to be comitted
    
    The `:buildifier-fix` tool always runs on all Bazel files (by calling `find`),
    which is not useful when using pre-commit.
    
    * Install pre-commit hook only when `DADE_NO_PRE_COMMIT` is unset
    
    Since direnv does not execute the shellHook, install in `.envrc` too.

--- shell.nix
+++ shell.nix
@@ -3,4 +3,9 @@
 }:
 pkgs.mkShell {
   buildInputs = pkgs.lib.attrsets.mapAttrsToList (name: value: value) default.toolAttrs;
+
+  shellHook = ''
+    # install pre-commit hook (opt-out by setting `DADE_NO_PRE_COMMIT`)
+    [ -v DADE_NO_PRE_COMMIT ] || pre-commit install
+  '';
 }
