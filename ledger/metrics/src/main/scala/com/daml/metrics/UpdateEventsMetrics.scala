f5cadc387ec61df5a9da4440a66b683155bc97a4
Nicu Reut <nicu.reut@digitalasset.com>
2022-12-13 12:45:28 +0100

    Add tests for metered events

--- ledger/metrics/src/main/scala/com/daml/metrics/UpdateEventsMetrics.scala
+++ ledger/metrics/src/main/scala/com/daml/metrics/UpdateEventsMetrics.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.daml.metrics
 
 import com.daml.metrics.api.MetricDoc.MetricQualification.Debug
