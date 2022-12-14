a7a958a337f9ffdb9f8c7ccbe07dfb23f9efbdc6
Sergey Kisel <98825453+skisel-da@users.noreply.github.com>
2022-12-14 11:05:18 +0100

    Simplification of errors within JWT, unit test (#15881)

---
+++ libs-scala/jwt/src/main/scala/com/digitalasset/jwt/Error.scala
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.jwt
+
+import scalaz.Show
+
+final case class Error(what: Symbol, message: String)
+
+object Error {
+  implicit val showInstance: Show[Error] =
+    Show.shows(e => s"JwtVerifier.Error: ${e.what}, ${e.message}")
+}
