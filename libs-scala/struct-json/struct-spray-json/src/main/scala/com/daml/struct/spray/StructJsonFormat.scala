d76603d137204faba5e87b625221d9756491147b
Simon Maxen <56595114+simonmaxen-da@users.noreply.github.com>
2022-12-16 10:46:32 +0000

    Metering clarifications for Daml Hub [DPP-1314] (#15898)
    * Add metering report JSON schema
    
    * Add metering report JSON schema
    * Remove metering-report-schema from proto exclusion list
    * Add header to metering report validator
    * Rename struct-json to struct-spray-json
    * Add struct-circe-json
    
    * Update with release comments

--- libs-scala/struct-json/struct-spray-json/src/main/scala/com/daml/struct/spray/StructJsonFormat.scala
+++ libs-scala/struct-json/struct-spray-json/src/main/scala/com/daml/struct/spray/StructJsonFormat.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.daml.struct.json
+package com.daml.struct.spray
 
 import com.google.protobuf.struct.Struct
 import spray.json.{
