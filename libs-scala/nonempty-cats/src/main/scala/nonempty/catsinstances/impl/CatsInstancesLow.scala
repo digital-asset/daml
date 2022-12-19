0cfed4a8348725e3b442883f3f20397992d4cfca
Stephen Compall <stephen.compall@daml.com>
2022-12-19 18:55:34 -0500

    make cats Reducible reusable

--- libs-scala/nonempty-cats/src/main/scala/nonempty/catsinstances/impl/CatsInstancesLow.scala
+++ libs-scala/nonempty-cats/src/main/scala/nonempty/catsinstances/impl/CatsInstancesLow.scala
@@ -16,28 +16,34 @@ abstract class CatsInstancesLow extends CatsInstancesLow1 {
 
 abstract class CatsInstancesLow1 {
   implicit def `cats nonempty reducible`[F[_]](implicit
-      F: Foldable[F]
+      F0: Foldable[F]
   ): Reducible[NonEmptyF[F, *]] =
-    new Reducible[NonEmptyF[F, *]] {
-      private def errOnEmpty[A](x: NonEmptyF[F, A]): Nothing =
-        throw new IllegalArgumentException(
-          s"empty structure coerced to non-empty: $x: ${x.getClass.getSimpleName}"
-        )
-
-      override def reduceLeftTo[A, B](fa: NonEmptyF[F, A])(f: A => B)(g: (B, A) => B): B =
-        F.reduceLeftToOption(fa)(f)(g).getOrElse(errOnEmpty(fa))
-
-      override def reduceRightTo[A, B](fa: NonEmptyF[F, A])(f: A => B)(
-          g: (A, Eval[B]) => Eval[B]
-      ): Eval[B] =
-        F.reduceRightToOption(fa)(f)(g).map(_.getOrElse(errOnEmpty(fa)))
-
-      override def foldLeft[A, B](fa: NonEmptyF[F, A], b: B)(f: (B, A) => B): B =
-        F.foldLeft(fa, b)(f)
-
-      override def foldRight[A, B](fa: NonEmptyF[F, A], lb: Eval[B])(
-          f: (A, Eval[B]) => Eval[B]
-      ): Eval[B] =
-        F.foldRight(fa, lb)(f)
+    new NonEmptyReducibleFromEmpty[F] {
+      override val F = F0
     }
+}
+
+private[impl] sealed trait NonEmptyReducibleFromEmpty[F[_]] extends Reducible[NonEmptyF[F, *]] {
+  protected[this] val F: Foldable[F]
+
+  private def errOnEmpty[A](x: NonEmptyF[F, A]): Nothing =
+    throw new IllegalArgumentException(
+      s"empty structure coerced to non-empty: $x: ${x.getClass.getSimpleName}"
+    )
+
+  override def reduceLeftTo[A, B](fa: NonEmptyF[F, A])(f: A => B)(g: (B, A) => B): B =
+    F.reduceLeftToOption(fa)(f)(g).getOrElse(errOnEmpty(fa))
+
+  override def reduceRightTo[A, B](fa: NonEmptyF[F, A])(f: A => B)(
+      g: (A, Eval[B]) => Eval[B]
+  ): Eval[B] =
+    F.reduceRightToOption(fa)(f)(g).map(_.getOrElse(errOnEmpty(fa)))
+
+  override def foldLeft[A, B](fa: NonEmptyF[F, A], b: B)(f: (B, A) => B): B =
+    F.foldLeft(fa, b)(f)
+
+  override def foldRight[A, B](fa: NonEmptyF[F, A], lb: Eval[B])(
+      f: (A, Eval[B]) => Eval[B]
+  ): Eval[B] =
+    F.foldRight(fa, lb)(f)
 }
