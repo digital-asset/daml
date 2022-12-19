723b3e9784b931fc3bfd9a132112b83990f30874
Chun Lok Ling <chunlok.ling@digitalasset.com>
2022-12-19 14:14:01 +0000

    address comments

--- language-support/java/json/src/main/scala/com/daml/lf/codegen/json/JsonCodec.scala
+++ language-support/java/json/src/main/scala/com/daml/lf/codegen/json/JsonCodec.scala
@@ -4,16 +4,21 @@
 package com.daml.lf.codegen.json
 
 import com.daml.ledger.javaapi.data.Value
+import com.daml.ledger.javaapi.data.codegen.DefinedDataType
 import com.daml.lf.value.json.ApiCodecCompressed
 import com.daml.lf.value.json.ApiCodecCompressed.JsonImplicits._
 import spray.json.JsValue
 
-final class JsonCodec private(encodeDecimalAsString: Boolean, encodeInt64AsString: Boolean) {
+final class JsonCodec private (encodeDecimalAsString: Boolean, encodeInt64AsString: Boolean) {
   private val apiCodec = new ApiCodecCompressed(encodeDecimalAsString, encodeInt64AsString)
 
   /** Converts `javaapi.data.Value` to `JsValue`. */
   def toJsValue(value: Value): JsValue =
     apiCodec.apiValueToJsValue(ValueConversion.toLfValue(value))
+
+  /** Converts `javaapi.data.codegen.DefinedDataType` to `JsValue`. */
+  def toJsValue[T](definedDataType: DefinedDataType[T]): JsValue =
+    apiCodec.apiValueToJsValue(ValueConversion.toLfValue(definedDataType.toValue))
 }
 
 object JsonCodec {
