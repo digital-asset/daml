c176edb5104f097dbda4b0c8f2fd78500a9416f8
Carl Pulley <carl.pulley@digitalasset.com>
2023-01-05 09:40:17 +0000

    Attempt at using size=large

--- triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/AbstractFuncTests.scala
+++ triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/test/AbstractFuncTests.scala
@@ -41,7 +41,7 @@ abstract class AbstractFuncTests
 
   this.getClass.getSimpleName can {
     "Failure testing" should {
-      val contractPairings = 200
+      val contractPairings = 400
 
       s"with $contractPairings contract pairings and always failing submissions" should {
         def command(template: String, owner: String, i: Int): CreateCommand =
