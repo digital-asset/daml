94327f5de29f64794651ea124b1df8d3d93060f7
Sergey Kisel <98825453+skisel-da@users.noreply.github.com>
2023-01-16 19:47:12 +0100

    Expose `isLocal` as is within Default IDP. (#16074)

--- ledger/participant-integration-api/src/test/suite/scala/db/migration/translation/apiserver/services/admin/ApiPartyManagementServiceSpec.scala
+++ ledger/participant-integration-api/src/test/suite/scala/db/migration/translation/apiserver/services/admin/ApiPartyManagementServiceSpec.scala
@@ -15,31 +15,70 @@ import com.daml.ledger.participant.state.index.v2.{
   IndexerPartyDetails,
   PartyEntry,
 }
+import com.daml.ledger.api.v1.admin.party_management_service.{PartyDetails => ProtoPartyDetails}
 import com.daml.ledger.participant.state.{v2 => state}
 import com.daml.lf.data.Ref
 import com.daml.logging.LoggingContext
+import com.daml.platform.apiserver.services.admin.ApiPartyManagementService.blindAndConvertToProto
 import com.daml.platform.apiserver.services.admin.ApiPartyManagementServiceSpec._
 import com.daml.platform.localstore.api.{PartyRecord, PartyRecordStore}
 import com.daml.telemetry.TelemetrySpecBase._
 import com.daml.telemetry.{TelemetryContext, TelemetrySpecBase}
 import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AsyncWordSpec
+import org.scalatest.wordspec.AnyWordSpec
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.Duration
 
 class ApiPartyManagementServiceSpec
-    extends AsyncWordSpec
+    extends AnyWordSpec
     with TelemetrySpecBase
     with MockitoSugar
     with Matchers
+    with ScalaFutures
     with ArgumentMatchersSugar
+    with IntegrationPatience
     with AkkaBeforeAndAfterAll {
 
   private implicit val loggingContext: LoggingContext = LoggingContext.ForTesting
+  private implicit val ec = ExecutionContext.global
 
   "ApiPartyManagementService" should {
+    def blind(
+        idpId: IdentityProviderId,
+        partyDetails: IndexerPartyDetails,
+        partyRecord: Option[PartyRecord],
+    ): ProtoPartyDetails =
+      blindAndConvertToProto(idpId)((partyDetails, partyRecord))
+
+    "translate basic input to the output" in new TestScope {
+      blind(IdentityProviderId.Default, partyDetails, Some(partyRecord)) shouldBe protoPartyDetails
+    }
+
+    "blind identity_provider_id for non default IDP" in new TestScope {
+      blind(IdentityProviderId("idp_1"), partyDetails, Some(partyRecord)) shouldBe protoPartyDetails
+        .copy(isLocal = false)
+    }
+
+    "blind identity_provider_id if record is for non default IDP" in new TestScope {
+      blind(
+        IdentityProviderId.Default,
+        partyDetails,
+        Some(partyRecord.copy(identityProviderId = IdentityProviderId("idp_1"))),
+      ) shouldBe protoPartyDetails.copy(identityProviderId = "")
+    }
+
+    "not blind `isLocal` if local record does not exist" in new TestScope {
+      blind(IdentityProviderId.Default, partyDetails, None) shouldBe protoPartyDetails
+    }
+
+    "blind `isLocal` if local record does not exist for non default IDP" in new TestScope {
+      blind(IdentityProviderId("idp_1"), partyDetails, None) shouldBe protoPartyDetails
+        .copy(isLocal = false)
+    }
+
     "propagate trace context" in {
       val mockIndexTransactionsService = mock[IndexTransactionsService]
       val mockPartyRecordStore = mock[PartyRecordStore]
@@ -90,15 +129,34 @@ class ApiPartyManagementServiceSpec
           scope.close()
           span.end()
         }
-        .map { _ =>
-          spanExporter.finishedSpanAttributes should contain(anApplicationIdSpanAttribute)
-          succeed
-        }
+        .futureValue
+
+      spanExporter.finishedSpanAttributes should contain(anApplicationIdSpanAttribute)
     }
   }
 }
 
 object ApiPartyManagementServiceSpec {
+  trait TestScope {
+    val partyDetails: IndexerPartyDetails = IndexerPartyDetails(
+      party = Ref.Party.assertFromString("Bob"),
+      displayName = Some("Bob Martin"),
+      isLocal = true,
+    )
+    val partyRecord: PartyRecord = PartyRecord(
+      party = Ref.Party.assertFromString("Bob"),
+      ObjectMeta.empty,
+      IdentityProviderId.Default,
+    )
+    val protoPartyDetails: ProtoPartyDetails = ProtoPartyDetails(
+      party = "Bob",
+      displayName = "Bob Martin",
+      localMetadata = Some(new com.daml.ledger.api.v1.admin.object_meta.ObjectMeta()),
+      isLocal = true,
+      identityProviderId = "",
+    )
+  }
+
   private object TestWritePartyService extends state.WritePartyService {
     override def allocateParty(
         hint: Option[Ref.Party],
