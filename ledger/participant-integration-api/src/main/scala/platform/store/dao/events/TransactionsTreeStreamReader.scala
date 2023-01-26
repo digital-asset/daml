90c68f2c9d87c4ab5e8c18f8085e80b6f97f1f44
Pawel Batko <pawel.batko@digitalasset.com>
2023-01-26 12:05:32 +0100

    Payload para := 16, async buffer := 16

--- ledger/participant-integration-api/src/main/scala/platform/store/dao/events/TransactionsTreeStreamReader.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/dao/events/TransactionsTreeStreamReader.scala
@@ -153,7 +153,7 @@ class TransactionsTreeStreamReader(
       ids.async
         .addAttributes(
           Attributes
-            .inputBuffer(initial = maxParallelPayloadQueries, max = maxParallelPayloadQueries)
+            .inputBuffer(initial = 16, max = 16)
         )
         .mapAsync(maxParallelPayloadQueries)(ids =>
           payloadQueriesLimiter.execute {
