afb9b5588c532bc883c7da438c5f9ab278b28c62
Carl Pulley <carl.pulley@digitalasset.com>
2023-02-21 07:44:44 +0000

    Wrap single messages in singleton lists for batching

--- triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/TriggerRuleSimulationLib.scala
+++ triggers/tests/src/test/scala/com/digitalasset/daml/lf/engine/trigger/TriggerRuleSimulationLib.scala
@@ -8,12 +8,13 @@ import akka.stream.{FlowShape, KillSwitches, Materializer, SourceShape}
 import akka.stream.scaladsl.{Flow, GraphDSL, Keep, Sink, Source}
 import com.daml.ledger.api.v1.command_submission_service.SubmitRequest
 import com.daml.ledger.api.v1.event.CreatedEvent
+import com.daml.lf.data.FrontStack
 import com.daml.lf.data.Time.Timestamp
 import com.daml.lf.engine.trigger.Runner.{TriggerContext, TriggerContextualFlow}
 import com.daml.lf.engine.trigger.ToLoggingContext._
 import com.daml.lf.engine.trigger.UnfoldState.{flatMapConcatNodeOps, toSourceOps}
 import com.daml.lf.speedy.SExpr.SEValue
-import com.daml.lf.speedy.SValue.SUnit
+import com.daml.lf.speedy.SValue.{SList, SUnit}
 import com.daml.lf.speedy.{SValue, Speedy}
 import com.daml.logging.LoggingContextOf
 import com.daml.util.Ctx
@@ -69,7 +70,9 @@ class TriggerRuleSimulationLib[UserState](runner: Runner) {
     val updateStateGraph = GraphDSL.createGraph(Sink.last[SValue]) { implicit gb => saveLastState =>
       import GraphDSL.Implicits._
 
-      val msgIn = gb add TriggerContextualFlow[SValue]
+      val msgIn = gb add TriggerContextualFlow[SValue].map(ctx =>
+        ctx.copy(value = SList(FrontStack(ctx.value)))
+      )
       val stateOut = gb add Source.single(state)
       val rule = gb add runner.runRuleOnMsgs(lambdaKillSwitch)
       val killSwitch = gb add lambdaKillSwitch.flow[TriggerContext[SValue]]
