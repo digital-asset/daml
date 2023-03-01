219fca58e2eb73ac5ff18a035a85515680942994
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-03-01 17:17:06 +0100

    change ci-u1 to match

--- infra/vsts_agent_ubuntu_20_04.tf
+++ infra/vsts_agent_ubuntu_20_04.tf
@@ -5,13 +5,12 @@ locals {
   ubuntu = [
     {
       name      = "ci-u1",
-      disk_size = 200,
+      disk_size = 400,
       size      = 30,
       extra     = <<EOF
-## Currently broken, temporarily disabling as this prevents machine startup
 # Taken from https://cloud.google.com/logging/docs/agent/logging/installation
-#curl -sSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-#curl -sSL https://dl.google.com/cloudagents/add-logging-agent-repo.sh | bash -s -- --also-install
+curl -sSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -sSL https://dl.google.com/cloudagents/add-logging-agent-repo.sh | bash -s -- --also-install
 EOF
     },
     {
