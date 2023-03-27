b419a085cebe5ffda0665127555ad91253a12039
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-03-27 14:39:43 +0200

    create basic network

---
+++ infra/ubuntu_azure.tf
@@ -0,0 +1,22 @@
+# Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+resource "azurerm_virtual_network" "ubuntu" {
+  name                = "ubuntu"
+  location            = azurerm_resource_group.daml-ci.location
+  resource_group_name = azurerm_resource_group.daml-ci.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.ubuntu.id
+  }
+}
+
+resource "azurerm_network_security_group" "ubuntu" {
+  name                = "ubuntu"
+  location            = azurerm_resource_group.daml-ci.location
+  resource_group_name = azurerm_resource_group.daml-ci.name
+}
