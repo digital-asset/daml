2bcb575297afa7aecdb57dda0e73707cb868d01f
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-03-27 14:49:40 +0200

    add security group

--- infra/ubuntu_azure.tf
+++ infra/ubuntu_azure.tf
@@ -19,4 +19,16 @@ resource "azurerm_network_security_group" "ubuntu" {
   name                = "ubuntu"
   location            = azurerm_resource_group.daml-ci.location
   resource_group_name = azurerm_resource_group.daml-ci.name
+
+  security_rule {
+    name                       = "deny-inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
