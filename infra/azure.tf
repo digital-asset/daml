af51d6601cebd2e064f4e3368530522b0a85d866
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-05-04 19:59:47 +0200

    infra: multiple IPs for NAT gateway (#16803)

--- infra/azure.tf
+++ infra/azure.tf
@@ -25,17 +25,16 @@ resource "azurerm_nat_gateway" "nat" {
   resource_group_name = azurerm_resource_group.daml-ci.name
 }
 
-resource "azurerm_public_ip" "nat" {
-  name                = "nat"
+resource "azurerm_public_ip_prefix" "nat" {
+  name                = "nat-ip-prefix"
   location            = azurerm_resource_group.daml-ci.location
   resource_group_name = azurerm_resource_group.daml-ci.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  prefix_length       = 28
 }
 
-resource "azurerm_nat_gateway_public_ip_association" "nat" {
-  nat_gateway_id       = azurerm_nat_gateway.nat.id
-  public_ip_address_id = azurerm_public_ip.nat.id
+resource "azurerm_nat_gateway_public_ip_prefix_association" "nat" {
+  nat_gateway_id      = azurerm_nat_gateway.nat.id
+  public_ip_prefix_id = azurerm_public_ip_prefix.nat.id
 }
 
 resource "azurerm_subnet_nat_gateway_association" "nat" {
