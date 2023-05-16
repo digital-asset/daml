c5ca9e4cf0c36b0397bffaa0ab0504a77a7d9f49
Remy <remy.haemmerle@daml.com>
2023-05-16 10:09:13 +0200

    Drop Ledger Api Bench Tool (#16857)

--- ledger/ledger-api-bench-tool/src/main/scala/com/daml/ledger/api/benchtool/metrics/ServiceLevelObjective.scala
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package com.daml.ledger.api.benchtool.metrics
-
-trait ServiceLevelObjective[MetricValueType <: MetricValue] {
-  def isViolatedBy(metricValue: MetricValueType): Boolean
-}
