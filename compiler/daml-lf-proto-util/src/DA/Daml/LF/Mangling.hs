0f5561ea36b344b6d6e47ed620868c74631150bd
Krisztian Pinter <krisztian.pinter@digitalasset.com>
2023-05-23 15:32:51 +0200

    fix

--- compiler/daml-lf-proto-util/src/DA/Daml/LF/Mangling.hs
+++ compiler/daml-lf-proto-util/src/DA/Daml/LF/Mangling.hs
@@ -1,7 +1,7 @@
 -- Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE MultiWayIf, NondecreasingIndentation #-}
 module DA.Daml.LF.Mangling
     ( UnmangledIdentifier(..)
     , mangleIdentifier
