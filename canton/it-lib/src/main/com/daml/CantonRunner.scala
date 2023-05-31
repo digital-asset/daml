7453bdd65d40cf7b24c4be13c063a81bdd7c66cf
Nick Chapman <nick.chapman@digitalasset.com>
2023-05-31 11:21:38 +0100

    add 16151 tag

--- canton/it-lib/src/main/com/daml/CantonRunner.scala
+++ canton/it-lib/src/main/com/daml/CantonRunner.scala
@@ -172,7 +172,8 @@ object CantonRunner {
               config.adminToken,
               ApiTypes.ApplicationId("CantonRunner"),
             )
-            // TODO: better to sequence the dar-loading in case of dependencies
+            // TODO https://github.com/digital-asset/daml/issues/16151
+            // better to sequence the dar-loading in case of dependencies
             _ <- Future.traverse(darFiles) { file =>
               client.packageManagementClient.uploadDarFile(
                 ByteString.copyFrom(Files.readAllBytes(file))
