9efd53ba96466d86497d3a3ba56b2a5882afc03b
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2023-08-01 18:50:59 +0100

    remove reliance on NameMap in del/existing/new logic

--- compiler/daml-lf-tools/src/DA/Daml/LF/TypeChecker/Upgrade.hs
+++ compiler/daml-lf-tools/src/DA/Daml/LF/TypeChecker/Upgrade.hs
@@ -11,6 +11,7 @@ import           DA.Daml.LF.TypeChecker.Env
 import           DA.Daml.LF.TypeChecker.Error
 import qualified Data.NameMap as NM
 import qualified Data.HashMap.Strict as HMS
+import Data.Hashable
 
 import Control.Monad
 import Data.Data
@@ -23,7 +24,7 @@ data Upgrading a = Upgrading
     { past :: a
     , present :: a
     }
-    deriving (Eq, Data, Generic, NFData, Show, Functor)
+    deriving (Eq, Data, Generic, NFData, Show)
 
 instance Functor Upgrading where
     fmap f Upgrading{..} = Upgrading (f past) (f present)
@@ -42,28 +43,33 @@ checkUpgrade version package =
 
 checkUpgradeM :: MonadGamma m => Upgrading LF.Package -> m ()
 checkUpgradeM package = do
-    upgradedModules <- checkDeleted packageModules package
+    (upgradedModules, _new) <- checkDeleted (const EUpgradeMissing) $ NM.toHashMap . packageModules <$> package
     forM_ upgradedModules checkModule
 
-extractDeleted :: NM.Named a => (b -> NM.NameMap a) -> Upgrading b -> HMS.HashMap (NM.Name a) a
-extractDeleted extract Upgrading{..} =
-    NM.toHashMap (extract past) `HMS.difference` NM.toHashMap (extract present)
+extractDelExistNew
+    :: (Eq k, Hashable k)
+    => Upgrading (HMS.HashMap k a)
+    -> (HMS.HashMap k a, HMS.HashMap k (Upgrading a), HMS.HashMap k a)
+extractDelExistNew Upgrading{..} =
+    ( past `HMS.difference` present
+    , HMS.intersectionWith Upgrading past present
+    , present `HMS.difference` past
+    )
 
-extractExisting :: NM.Named a => (b -> NM.NameMap a) -> Upgrading b -> HMS.HashMap (NM.Name a) (Upgrading a)
-extractExisting extract Upgrading{..} =
-    HMS.intersectionWith Upgrading (NM.toHashMap (extract past)) (NM.toHashMap (extract present))
-
-checkDeleted :: (NM.Named a, MonadGamma m) => (b -> NM.NameMap a) -> Upgrading b -> m (HMS.HashMap (NM.Name a) (Upgrading a))
-checkDeleted extract upgrade = do
-    let deleted = extractDeleted extract upgrade
-    let existing = extractExisting extract upgrade
-    if HMS.null deleted
-       then pure existing
-       else throwWithContext EUpgradeMissing
+checkDeleted
+    :: (Eq k, Hashable k, MonadGamma m)
+    => (a -> Error)
+    -> Upgrading (HMS.HashMap k a)
+    -> m (HMS.HashMap k (Upgrading a), HMS.HashMap k a)
+checkDeleted handle upgrade = do
+    let (deleted, existing, new) = extractDelExistNew upgrade
+    case HMS.toList deleted of
+      ((_, head):_) -> throwWithContext $ handle head
+      _ -> pure (existing, new)
 
 checkModule :: MonadGamma m => Upgrading LF.Module -> m ()
 checkModule module_ = do
-    existingDefDataTypes <- checkDeleted moduleDataTypes module_
+    (existingDefDataTypes, _new) <- checkDeleted (const EUpgradeMissing) $ NM.toHashMap . moduleDataTypes <$> module_
     forM_ existingDefDataTypes checkDefDataType
 
 checkDefDataType :: MonadGamma m => Upgrading LF.DefDataType -> m ()
@@ -77,21 +83,14 @@ checkDefDataType datatype = do
 
 checkFields :: MonadGamma m => Upgrading [(FieldName, Type)] -> m ()
 checkFields fields = do
-    let pastMap = HMS.fromList (past fields)
-    let presentMap = HMS.fromList (present fields)
-    let deletedFields = pastMap `HMS.difference` presentMap
-    let matchingFields = HMS.intersectionWith Upgrading pastMap presentMap
-    let newFields = presentMap `HMS.difference` pastMap
-
-    -- If a field from the upgraded package is missing
-    when (not (HMS.null deletedFields)) $ throwWithContext EUpgradeRecordFieldsMissing
+    (existing, new) <- checkDeleted (const EUpgradeRecordFieldsMissing) $ HMS.fromList <$> fields
 
     -- If a field from the upgraded package has had its type changed
     let matchingFieldDifferentType Upgrading{..} = past /= present
-    when (any matchingFieldDifferentType matchingFields) $ throwWithContext EUpgradeRecordFieldsExistingChanged
+    when (any matchingFieldDifferentType existing) $ throwWithContext EUpgradeRecordFieldsExistingChanged
 
     -- If a new field has a non-optional type
     let newFieldOptionalType (TOptional _) = True
         newFieldOptionalType _ = False
-    when (not (all newFieldOptionalType newFields)) $ throwWithContext EUpgradeRecordFieldsNewNonOptional
+    when (not (all newFieldOptionalType new)) $ throwWithContext EUpgradeRecordFieldsNewNonOptional
 
