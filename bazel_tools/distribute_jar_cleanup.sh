97349be3f4f771bf9a59ce0c9adaeb1ed041de50
Samuel Williams <samuel.williams@digitalasset.com>
2023-08-22 12:55:14 +0100

    Change NOTICES to NOTICES.txt for consistency

--- bazel_tools/distribute_jar_cleanup.sh
+++ bazel_tools/distribute_jar_cleanup.sh
@@ -5,7 +5,7 @@
 
 SRC=$1
 OUT=$2
-NOTICE=$3
+NOTICES=$3
 LICENSE=$4
 LICENSE_EE=$5
 IS_EE=$6
@@ -13,15 +13,15 @@ IS_EE=$6
 cp $SRC $OUT
 chmod +w $OUT
 zip -dq $OUT "/LICENSE*" "/META-INF/LICENSE*" "/NOTICE*" "/META-INF/NOTICE*"
-zip -uq $OUT $NOTICE
 
 # both licenses have the wrong file name/directory, so we copy to a temp dir
 TMP_LICENSE_DIR=$(mktemp -d)
+cp $NOTICES $TMP_LICENSE_DIR/NOTICES.txt
 if [[ "$IS_EE" -eq 1 ]]; then
   cp $LICENSE_EE $TMP_LICENSE_DIR/LICENSE.txt
 else
   cp $LICENSE $TMP_LICENSE_DIR/LICENSE.txt
 fi
-zip -ujq $OUT $TMP_LICENSE_DIR/LICENSE.txt
+zip -ujq $OUT $TMP_LICENSE_DIR/LICENSE.txt $TMP_LICENSE_DIR/NOTICES.txt
 rm -rf $TMP_LICENSE_DIR
 
