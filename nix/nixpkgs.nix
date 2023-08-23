b1703f47d295c36b2b5f0796e578691ff7b1a66d
Moisés Ackerman <6054733+akrmn@users.noreply.github.com>
2023-08-23 17:51:23 +0200

    Upgrade to Bazel 5.2.0

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -28,12 +28,12 @@ let
       };
     });
 
-    bazel_4 = pkgs.bazel_4.overrideAttrs(oldAttrs: {
+    bazel_5 = pkgs.bazel_5.overrideAttrs(oldAttrs: {
       patches = oldAttrs.patches ++ [
         # Bazel is too aggressive in treating arguments starting with @ as
         # response files. This fix has been upstreamed and is available from Bazel 5.1.0
         # https://github.com/bazelbuild/bazel/commit/48b60d22bca0158d194b78481ff86b0ac251243f
-        ./bazel-cc-wrapper-response-file.patch
+        # ./bazel-cc-wrapper-response-file.patch
         # This has been upstreamed but it's only available from Bazel 7.0.0-pre.20230215.2
         # (maybe we can get it backported?)
         # https://github.com/bazelbuild/bazel/commit/6115d94cd05864fe5c6e5f774e9482b3b4976976
