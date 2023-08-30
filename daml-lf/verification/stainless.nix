20330329ac7d3df5bc5dc87e446ffe37980b4067
Carl Pulley <106966370+carlpulley-da@users.noreply.github.com>
2023-08-30 15:00:31 +0100

    nix changes to allow PDF documentation to be built and latex cruft to be ignored (#17315)

--- daml-lf/verification/stainless.nix
+++ daml-lf/verification/stainless.nix
@@ -1,6 +1,7 @@
 let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
+  pdflatex_setup = pkgs.texlive.combine { inherit (pkgs.texlive) scheme-small parskip cleveref etoolbox; };
 in rec {
   stainlessEnv = stdenv.mkDerivation rec {
     name = "stainless-env";
@@ -12,6 +13,7 @@ in rec {
       sbt
       openjdk17
       z3
+      pdflatex_setup
     ];
   };
 }
