15be5507a104ce8d0b05f7c53df150adf2357a34
Paul Brauner <paul.brauner@digitalasset.com>
2023-09-06 08:41:14 +0200

    rephrasing comments

--- compiler/damlc/daml-desugar/test/DA/Daml/Desugar/Tests.hs
+++ compiler/damlc/daml-desugar/test/DA/Daml/Desugar/Tests.hs
@@ -35,7 +35,7 @@ runDamlDesugar input = desugar opts input
   where
     opts = (defaultOptions Nothing)
       { optScenarioService = EnableScenarioService False
-      -- The desugarer is unaffected by the version of LF so we randomly test it with 2.dev.
+      -- The desugarer is unaffected by the version of LF so we arbitrarily test it with 2.dev.
       , optDamlLfVersion = version2_dev
       }
 
