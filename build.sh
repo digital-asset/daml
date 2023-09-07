5d5507a58f1e66652991dec0bea34f16f6174e0f
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-09-07 13:52:23 +0200

    hopefully shorter builds

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ else
 fi
 
 # Bazel test only builds targets that are dependencies of a test suite so do a full build first.
-$bazel build //... \
+$bazel build //navigator/... \
   --build_tag_filters "${tag_filter:1}" \
   --profile build-profile.json \
   --experimental_profile_include_target_label \
@@ -79,7 +79,7 @@ stop_postgresql # in case it's running from a previous build
 start_postgresql
 
 # Run the tests.
-$bazel test //... \
+$bazel test //navigator/... \
   --build_tag_filters "${tag_filter:1}" \
   --test_tag_filters "${tag_filter:1}" \
   --test_env "POSTGRESQL_HOST=${POSTGRESQL_HOST}" \
