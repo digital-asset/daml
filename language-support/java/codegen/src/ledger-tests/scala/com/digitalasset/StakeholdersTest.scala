1bee52e40c889d4cbf4cc95b664d0b354cb6c82c
Moisés Ackerman <6054733+akrmn@users.noreply.github.com>
2023-09-08 12:30:21 +0200

    Remove compiler support for `controller..can` choice syntax (#17362)
    * Update GHC_REV
    
    * Remove 'controller..can' syntax in java codegen StakeholdersTest
    
    * Remove 'controller..can' syntax in ChoiceSyntaxes daml-test-file
    
    * Remove daml-test-files {,No}WarnControllerCan
    
    * Remove 'controller..can' syntax in SemanticsEvalOrder{,RightToLeft} daml-test-files
    
    * Update {Structure,Reference}.daml code snippets for docs.daml.com
    
    * Remove 'controller..can' syntax in TestGrammar.daml
    
    * changelog entry for 'controller..can' removal
    
    * Update UNRELEASED.md
    
    Co-authored-by: carrielaben-da <91496516+carrielaben-da@users.noreply.github.com>

--- language-support/java/codegen/src/ledger-tests/scala/com/digitalasset/StakeholdersTest.scala
+++ language-support/java/codegen/src/ledger-tests/scala/com/digitalasset/StakeholdersTest.scala
@@ -9,7 +9,7 @@ import io.grpc.Channel
 import org.scalatest.Assertion
 import org.scalatest.flatspec.AsyncFlatSpecLike
 import org.scalatest.matchers.should.Matchers
-import stakeholders.{ExplicitObservers, ImplicitObservers, MixedObservers, OnlySignatories}
+import stakeholders.{ExplicitObservers, OnlySignatories}
 
 import scala.concurrent.Future
 
@@ -26,20 +26,16 @@ trait StakeholdersTest
       testCode: (
           String,
           OnlySignatories,
-          ImplicitObservers,
           ExplicitObservers,
-          MixedObservers,
           Channel,
       ) => Assertion
   ): Future[Assertion] = {
     for {
       List(alice, bob, charlie) <- Future.sequence(List.fill(3)(allocateParty))
       onlySignatories = new OnlySignatories(alice)
-      implicitObservers = new ImplicitObservers(alice, bob)
       explicitObservers = new ExplicitObservers(alice, bob)
-      mixedObservers = new MixedObservers(alice, bob, charlie)
       result <- withClient(
-        testCode(alice, onlySignatories, implicitObservers, explicitObservers, mixedObservers, _)
+        testCode(alice, onlySignatories, explicitObservers, _)
       )
     } yield result
   }
@@ -47,7 +43,7 @@ trait StakeholdersTest
   behavior of "Stakeholders"
 
   they should "be exposed correctly for contracts without observers" in withUniqueParties {
-    (alice, onlySignatories, _, _, _, client) =>
+    (alice, onlySignatories, _, client) =>
       sendCmd(client, alice, onlySignatories.create())
 
       val contract :: _ =
@@ -57,19 +53,8 @@ trait StakeholdersTest
       contract.observers shouldBe empty
   }
 
-  they should "be exposed correctly for contracts with only implicit observers" in withUniqueParties {
-    (alice, _, implicitObservers, _, _, client) =>
-      sendCmd(client, alice, implicitObservers.create())
-
-      val contract :: _ =
-        readActiveContracts(ImplicitObservers.Contract.fromCreatedEvent)(client, alice)
-
-      contract.signatories should contain only implicitObservers.owner
-      contract.observers should contain only implicitObservers.thirdParty
-  }
-
   they should "be exposed correctly for contracts with only explicit observers" in withUniqueParties {
-    (alice, _, _, explicitObservers, _, client) =>
+    (alice, _, explicitObservers, client) =>
       sendCmd(client, alice, explicitObservers.create())
 
       val contract :: _ =
@@ -78,16 +63,4 @@ trait StakeholdersTest
       contract.signatories should contain only explicitObservers.owner
       contract.observers should contain only explicitObservers.thirdParty
   }
-
-  they should "be exposed correctly for contracts with both implicit and explicit observers" in withUniqueParties {
-    (alice, _, _, _, mixedObservers, client) =>
-      sendCmd(client, alice, mixedObservers.create())
-
-      val contract :: _ =
-        readActiveContracts(MixedObservers.Contract.fromCreatedEvent)(client, alice)
-
-      contract.signatories should contain only mixedObservers.owner
-      contract.observers should contain.only(mixedObservers.thirdParty1, mixedObservers.thirdParty2)
-  }
-
 }
