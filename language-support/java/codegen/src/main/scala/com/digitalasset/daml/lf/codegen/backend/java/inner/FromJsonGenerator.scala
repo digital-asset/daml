7a063cc5016a9dfefc0176574d7ca08ad069bb8e
= <=>
2023-09-18 16:09:38 +1000

    support arbitrary nesting of optionals

--- language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/FromJsonGenerator.scala
+++ language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/FromJsonGenerator.scala
@@ -245,15 +245,14 @@ private[inner] object FromJsonGenerator extends StrictLogging {
         CodeBlock.of("$T.contractId($T::new)", decodeClass, contractIdType)
       case TypePrim(PrimTypeList, typeParams) =>
         CodeBlock.of("$T.list($L)", decodeClass, typeReaders(typeParams))
-      case TypePrim(PrimTypeOptional, Seq(TypePrim(PrimTypeOptional, typeParams))) =>
-        CodeBlock.of(
-          "$T.optionalNested($T.optional($L))",
-          decodeClass,
-          decodeClass,
-          typeReaders(typeParams),
-        )
-      case TypePrim(PrimTypeOptional, typeParams) =>
-        CodeBlock.of("$T.optional($L)", decodeClass, typeReaders(typeParams))
+      case TypePrim(PrimTypeOptional, Seq(typeParam)) =>
+        def buildNestedOptionals(b: CodeBlock.Builder, typ: Type): CodeBlock.Builder = typ match {
+          case TypePrim(PrimTypeOptional, Seq(innerType)) =>
+            buildNestedOptionals(b.add("$T.optionalNested(", decodeClass), innerType).add(")")
+          case _ =>
+            b.add("$T.optional($L)", decodeClass, typeReaders(Seq(typ)))
+        }
+        buildNestedOptionals(CodeBlock.builder(), typeParam).build()
       case TypePrim(PrimTypeTextMap, typeParams) =>
         CodeBlock.of("$T.textMap($L)", decodeClass, typeReaders(typeParams))
       case TypePrim(PrimTypeGenMap, typeParams) =>
