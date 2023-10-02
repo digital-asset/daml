6366f8ce0f2ebbaa1527325c002140c2362b70b6
Andreas Triantafyllos <andreas.triantafyllos@digitalasset.com>
2023-10-02 13:05:52 +0200

    remove ledger-api-errors that were only used in ErrorFactoriesSpec.

--- ledger/ledger-api-errors/src/main/scala/com/daml/error/definitions/LedgerApiErrors.scala
+++ ledger/ledger-api-errors/src/main/scala/com/daml/error/definitions/LedgerApiErrors.scala
@@ -7,7 +7,6 @@ import com.daml.error._
 import com.daml.error.definitions.ErrorGroups.ParticipantErrorGroup.LedgerApiErrorGroup
 import com.daml.lf.engine.Error.Validation.ReplayMismatch
 import com.daml.lf.engine.{Error => LfError}
-import org.slf4j.event.Level
 
 @Explanation(
   "Errors raised by or forwarded by the Ledger API."
@@ -22,30 +21,6 @@ object LedgerApiErrors extends LedgerApiErrorGroup {
   val WriteServiceRejections: groups.WriteServiceRejections.type = groups.WriteServiceRejections
 
   val EarliestOffsetMetadataKey = "earliest_offset"
-
-  @Explanation(
-    """This error occurs when a participant rejects a command due to excessive load.
-      |Load can be caused by the following factors:
-      |1. when commands are submitted to the participant through its Ledger API,
-      |2. when the participant receives requests from other participants through a connected domain."""
-  )
-  @Resolution(
-    """Wait a bit and retry, preferably with some backoff factor.
-      |If possible, ask other participants to send fewer requests; the domain operator can enforce this by imposing a rate limit."""
-  )
-  object ParticipantBackpressure
-      extends ErrorCode(
-        id = "PARTICIPANT_BACKPRESSURE",
-        ErrorCategory.ContentionOnSharedResources,
-      ) {
-    override def logLevel: Level = Level.INFO
-
-    case class Rejection(reason: String)(implicit errorLogger: ContextualizedErrorLogger)
-        extends DamlErrorWithDefiniteAnswer(
-          cause = s"The participant is overloaded: $reason",
-          extraContext = Map("reason" -> reason),
-        )
-  }
 
   @Explanation("""This error occurs if there was an unexpected error in the Ledger API.""")
   @Resolution("Contact support.")
