bc5e8a3a58c52371b9587d13d5e4ad4fce73c762
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-10-03 17:16:27 +0200

    remove some unused dade tools

--- dev-env/profile_bash.sh
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# DADE shell profile for bash
-
-export DADE_REPO_ROOT="$(cd $(dirname "${BASH_SOURCE[0]}")/.. ; pwd)"
-source /dev/stdin <<< "$(${DADE_REPO_ROOT}/dev-env/bin/dade assist)"
--- dev-env/profile_zsh.sh
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# DADE shell profile compatible with zsh.
-
-source <("${0:A:h}"/bin/dade assist)
