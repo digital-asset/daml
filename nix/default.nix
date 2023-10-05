86d8e29879f2c7c59fda08fa58761335a108dbf7
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-10-05 18:18:49 +0200

    remove selectBin

--- nix/default.nix
+++ nix/default.nix
@@ -10,20 +10,6 @@ let
     config.allowUnfree = true;
     config.allowBroken = true;
   };
-  selectBin = pkg:
-    if pkg == null then
-      null
-    else if builtins.hasAttr "bin" pkg then
-      pkg.bin
-    else if builtins.hasAttr "outputs" pkg then
-      builtins.getAttr (builtins.elemAt pkg.outputs 0) pkg
-    else
-      pkg;
-in rec {
-  inherit pkgs;
-
-  toolAttrs = rec {
-    semver = pkgs.callPackage ./tools/semver-tool {};
-  };
-  tools = pkgs.lib.mapAttrs (_: pkg: selectBin pkg) toolAttrs;
+in {
+  semver = pkgs.callPackage ./tools/semver-tool {};
 }
