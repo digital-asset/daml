52c2704a0d512185b8fad580e2fa67258c4ba89c
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-10-05 18:16:32 +0200

    inline nixpkgs/default.src.json

--- nix/default.nix
+++ nix/default.nix
@@ -1,6 +1,9 @@
 let
   system = builtins.currentSystem;
-  src = import ./nixpkgs;
+  src = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/9a82a9b5248919805a2400266ebd881d5783df2a.tar.gz";
+    sha256 = "142x1zq3cjadgmvfv0paydlq268pfinllqpq2vl0vxwdiq2nr9iz";
+  };
   pkgs = import src {
     inherit system;
 
