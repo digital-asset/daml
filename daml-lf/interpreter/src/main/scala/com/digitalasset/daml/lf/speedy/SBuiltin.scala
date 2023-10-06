digital-asset/daml

commit 4b60e35a5d7fc31362381edcbe04a3dcea7a0e02

[engine] Restructure contract caching (#17171)

daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Speedy.scala
@@ -121,7 +121,7 @@ private[lf] object Speedy {
     }
   }
 
-  final case class CachedContract(
+  final case class ContractInfo(
       version: TxVersion,
       templateId: Ref.TypeConName,
       value: SValue,
@@ -237,18 +237,18 @@ private[lf] object Speedy {
     /* Flag to trace usage of get_time builtins */
     private[this] var dependsOnTime: Boolean = false
     // global contract discriminators, that are discriminators from contract created in previous transactions
-    private[this] var cachedContracts_ : Map[V.ContractId, CachedContract] = Map.empty
+
     private[this] var numInputContracts: Int = 0
 
-    private[this] var disclosedContracts_ = Map.empty[V.ContractId, CachedContract]
-    private[speedy] def disclosedContracts: Map[V.ContractId, CachedContract] = disclosedContracts_
+    private[this] var disclosedContracts_ = Map.empty[V.ContractId, ContractInfo]
+    private[speedy] def disclosedContracts: Map[V.ContractId, ContractInfo] = disclosedContracts_
 
     private[this] var disclosedContractKeys_ = Map.empty[GlobalKey, V.ContractId]
     private[speedy] def disclosedContractKeys: Map[GlobalKey, V.ContractId] = disclosedContractKeys_
 
     private[speedy] def addDisclosedContracts(
         contractId: V.ContractId,
-        contract: CachedContract,
+        contract: ContractInfo,
     ): Unit = {
       disclosedContracts_ = disclosedContracts.updated(contractId, contract)
       contract.keyOpt.foreach(key =>
@@ -265,9 +265,6 @@ private[lf] object Speedy {
     def getDependsOnTime: Boolean =
       dependsOnTime
 
-    private[speedy] def cachedContracts: Map[V.ContractId, CachedContract] =
-      cachedContracts_
-
     val visibleToStakeholders: Set[Party] => SVisibleToStakeholders =
       if (validating) { _ => SVisibleToStakeholders.Visible }
       else {
@@ -276,11 +273,50 @@ private[lf] object Speedy {
 
     def incompleteTransaction: IncompleteTx = ptx.finishIncomplete
     def nodesToString: String = ptx.nodesToString
+
+    /** Local Contract Store:
+      *      Maps contract-id to type+svalue, for LOCALLY-CREATED contracts.
+      *      - Consulted (getIfLocalContract) by fetchAny (SBuiltin).
+      *      - Updated   (storeLocalContract) by SBUCreate.
+      */
+    private[speedy] var localContractStore: Map[V.ContractId, (TypeConName, SValue)] = Map.empty
+    private[speedy] def getIfLocalContract(coid: V.ContractId): Option[(TypeConName, SValue)] = {
+      localContractStore.get(coid)
+    }
+    private[speedy] def storeLocalContract(
+        coid: V.ContractId,
+        templateId: TypeConName,
+        templateArg: SValue,
+    ): Unit = {
+      localContractStore = localContractStore + (coid -> (templateId, templateArg))
+    }
+
+    /** Contract Info Cache:
+      *      Maps contract-id to contract-info, for EVERY referenced contract-id.
+      *      - Consulted (lookupContractInfoCache) by getContractInfo (SBuiltin).
+      *      - Updated   (insertContractInfoCache) by getContractInfo + SBUCreate.
+      */
+    // TODO: https://github.com/digital-asset/daml/issues/17082
+    // - Must be template-id aware when we support ResultNeedUpgradeVerification
+    private[speedy] var contractInfoCache: Map[V.ContractId, ContractInfo] = Map.empty
+    private[speedy] def lookupContractInfoCache(coid: V.ContractId): Option[ContractInfo] = {
+      contractInfoCache.get(coid)
+    }
+    private[speedy] def insertContractInfoCache(
+        coid: V.ContractId,
+        contract: ContractInfo,
+    ): Unit = {
+      contractInfoCache = contractInfoCache + (coid -> contract)
+    }
+
     private[speedy] def isLocalContract(contractId: V.ContractId): Boolean = {
       ptx.contractState.locallyCreated.contains(contractId)
     }
 
-    private[speedy] def updateCachedContracts(cid: V.ContractId, contract: CachedContract): Unit = {
+    private[speedy] def enforceLimitSignatoriesAndObservers(
+        cid: V.ContractId,
+        contract: ContractInfo,
+    ): Unit = {
       enforceLimit(
         NameOf.qualifiedNameOfCurrentFunc,
         contract.signatories.size,
@@ -307,10 +343,9 @@ private[lf] object Speedy {
             _,
           ),
       )
-      cachedContracts_ = cachedContracts_.updated(cid, contract)
     }
 
-    private[speedy] def addGlobalContract(coid: V.ContractId, contract: CachedContract): Unit = {
+    private[speedy] def enforceLimitAddInputContract(): Unit = {
       numInputContracts += 1
       enforceLimit(
         NameOf.qualifiedNameOfCurrentFunc,
@@ -318,7 +353,6 @@ private[lf] object Speedy {
         limits.transactionInputContracts,
         IError.Dev.Limit.TransactionInputContracts,
       )
-      updateCachedContracts(coid, contract)
     }
 
     private[speedy] def enforceChoiceControllersLimit(
@@ -363,13 +397,21 @@ private[lf] object Speedy {
         IError.Dev.Limit.ChoiceAuthorizers(cid, templateId, choiceName, arg, authorizers, _),
       )
 
+    // Track which disclosed contracts are used, so we can report events to the ledger
+    private[this] var usedDiclosedContracts: Set[V.ContractId] = Set.empty
+    private[speedy] def markDisclosedcontractAsUsed(coid: V.ContractId): Unit = {
+      usedDiclosedContracts = usedDiclosedContracts + coid
+    }
+
     // The set of create events for the disclosed contracts that are used by the generated transaction.
-    def disclosedCreateEvents: ImmArray[Node.Create] =
+    def disclosedCreateEvents: ImmArray[Node.Create] = {
       disclosedContracts.iterator
         .collect {
-          case (coid, contract) if cachedContracts_.isDefinedAt(coid) => contract.toCreateNode(coid)
+          case (coid, contract) if usedDiclosedContracts.contains(coid) =>
+            contract.toCreateNode(coid)
         }
         .to(ImmArray)
+    }
 
     @throws[IllegalArgumentException]
     def zipSameLength[X, Y](xs: ImmArray[X], ys: ImmArray[Y]): ImmArray[(X, Y)] = {
@@ -393,7 +435,7 @@ private[lf] object Speedy {
 
     def checkContractVisibility(
         cid: V.ContractId,
-        contract: CachedContract,
+        contract: ContractInfo,
     ): Unit = {
       // For disclosed contracts, we do not perform visibility checking
       if (!isDisclosedContract(cid)) {
@@ -424,29 +466,20 @@ private[lf] object Speedy {
         gkey: GlobalKey,
         coid: V.ContractId,
         handleKeyFound: V.ContractId => Control.Value,
+        contract: ContractInfo,
     ): Control.Value = {
       // For local and disclosed contracts, we do not perform visibility checking
       if (isLocalContract(coid) || isDisclosedContract(coid)) {
         handleKeyFound(coid)
       } else {
-        cachedContracts.get(coid) match {
-          case Some(cachedContract) =>
-            val stakeholders = cachedContract.signatories union cachedContract.observers
-            visibleToStakeholders(stakeholders) match {
-              case SVisibleToStakeholders.Visible =>
-                handleKeyFound(coid)
-
-              case SVisibleToStakeholders.NotVisible(actAs, readAs) =>
-                throw SErrorDamlException(
-                  interpretation.Error
-                    .ContractKeyNotVisible(coid, gkey, actAs, readAs, stakeholders)
-                )
-            }
-
-          case None =>
-            throw SErrorCrash(
-              NameOf.qualifiedNameOfCurrentFunc,
-              s"contract ${coid.coid} not in cachedContracts",
+        val stakeholders = contract.signatories union contract.observers
+        visibleToStakeholders(stakeholders) match {
+          case SVisibleToStakeholders.Visible =>
+            handleKeyFound(coid)
+          case SVisibleToStakeholders.NotVisible(actAs, readAs) =>
+            throw SErrorDamlException(
+              interpretation.Error
+                .ContractKeyNotVisible(coid, gkey, actAs, readAs, stakeholders)
             )
         }
       }
@@ -790,8 +823,8 @@ private[lf] object Speedy {
       track.reset()
     }
 
-    final def setControl(x: Control[Q]): Unit = {
-      control = x
+    final def setControl[Q2](x: Control[Q2]): Unit = {
+      control = x.asInstanceOf[Control[Q]] // appease unhelpful typing
     }
 
     /** Run a machine until we get a result: either a final-value or a request for data, with a callback */
@@ -1375,8 +1408,9 @@ private[lf] object Speedy {
 
   /** Kont, or continuation. Describes the next step for the machine
     * after an expression has been evaluated into a 'SValue'.
+    * Not sealed, so we can define Kont variants in SBuiltin.scala
     */
-  private[speedy] sealed abstract class Kont {
+  private[speedy] abstract class Kont {
 
     /** Execute the continuation. */
     def execute[Q](machine: Machine[Q], v: SValue): Control[Q]
@@ -1575,8 +1609,9 @@ private[lf] object Speedy {
       case SValue.SContractId(_) | SValue.SDate(_) | SValue.SNumeric(_) | SValue.SInt64(_) |
           SValue.SParty(_) | SValue.SText(_) | SValue.STimestamp(_) | SValue.SStruct(_, _) |
           SValue.SMap(_, _) | _: SValue.SRecordRep | SValue.SAny(_, _) | SValue.STypeRep(_) |
-          SValue.SBigNumeric(_) | _: SValue.SPAP | SValue.SToken =>
+          SValue.SBigNumeric(_) | _: SValue.SPAP | SValue.SToken => {
         throw SErrorCrash(NameOf.qualifiedNameOfCurrentFunc, "Match on non-matchable value")
+      }
     }
 
     val e = altOpt
@@ -1753,25 +1788,6 @@ private[lf] object Speedy {
       defn.setCached(sv)
       Control.Value(sv)
     }
-  }
-
-  private[speedy] final case class KCacheContract(cid: V.ContractId) extends Kont {
-    override def execute[Q](machine: Machine[Q], sv: SValue): Control[Q] =
-      machine.asUpdateMachine(productPrefix) { machine =>
-        val cached = SBuiltin.extractCachedContract(machine.tmplId2TxVersion, sv)
-        machine.checkContractVisibility(cid, cached)
-        machine.addGlobalContract(cid, cached)
-        Control.Value(cached.any)
-      }
-  }
-
-  private[speedy] final case class KCheckKeyVisibility(
-      gKey: GlobalKey,
-      cid: V.ContractId,
-      handleKeyFound: V.ContractId => Control.Value,
-  ) extends Kont {
-    override def execute[Q](machine: Machine[Q], sv: SValue): Control[Q] =
-      machine.asUpdateMachine(productPrefix)(_.checkKeyVisibility(gKey, cid, handleKeyFound))
   }
 
   /** KCloseExercise. Marks an open-exercise which needs to be closed. Either:

daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/Engine.scala
@@ -304,6 +304,7 @@ class Engine(val config: EngineConfig = Engine.StableConfig) {
       submissionTime: Time.Timestamp,
       seeding: speedy.InitialSeeding,
   )(implicit loggingContext: LoggingContext): Result[(SubmittedTransaction, Tx.Metadata)] = {
+
     val machine = UpdateMachine(
       compiledPackages = compiledPackages,
       submissionTime = submissionTime,
@@ -384,8 +385,14 @@ class Engine(val config: EngineConfig = Engine.StableConfig) {
           handleError(err)
       }
 
+    // This cache is a pure optimization to avoid repeated calls to Result.needcontract
+    type CoinstCache = Map[ContractId, VersionedContractInstance]
+
+    // for use in non tail contexts
+    def loopOuter(cache: CoinstCache): Result[(SubmittedTransaction, Tx.Metadata)] = loop(cache)
+
     @scala.annotation.tailrec
-    def loop(): Result[(SubmittedTransaction, Tx.Metadata)] =
+    def loop(cache: CoinstCache): Result[(SubmittedTransaction, Tx.Metadata)] = {
       machine.run() match {
 
         case SResultQuestion(question) =>
@@ -397,7 +404,7 @@ class Engine(val config: EngineConfig = Engine.StableConfig) {
                 { granted: Boolean =>
                   if (granted) {
                     callback()
-                    interpretLoop(machine, time)
+                    loopOuter(cache)
                   } else {
                     ResultError(
                       Error.Interpretation.DamlException(
@@ -413,11 +420,11 @@ class Engine(val config: EngineConfig = Engine.StableConfig) {
               // TODO https://github.com/digital-asset/daml/issues/16154 (dynamic-exercise)
               // For now this just continues with the input package id
               callback(pid0)
-              loop()
+              loop(cache)
 
             case Question.Update.NeedTime(callback) =>
               callback(time)
-              loop()
+              loop(cache)
 
             case Question.Update.NeedPackage(pkgId, context, callback) =>
               Result.needPackage(
@@ -426,32 +433,38 @@ class Engine(val config: EngineConfig = Engine.StableConfig) {
                 { pkg: Package =>
                   compiledPackages.addPackage(pkgId, pkg).flatMap { _ =>
                     callback(compiledPackages)
-                    interpretLoop(machine, time)
+                    loopOuter(cache)
                   }
                 },
               )
 
-            case Question.Update.NeedContract(contractId, _, callback) =>
-              Result.needContract(
-                contractId,
-                { coinst: VersionedContractInstance =>
+            case Question.Update.NeedContract(coid, _, callback) =>
+              cache.get(coid) match {
+                case Some(coinst) => // cache hit
                   callback(coinst.unversioned)
-                  interpretLoop(machine, time)
-                },
-              )
+                  loop(cache)
+                case None =>
+                  Result.needContract(
+                    coid,
+                    { coinst: VersionedContractInstance =>
+                      callback(coinst.unversioned)
+                      loopOuter(cache.updated(coid, coinst))
+                    },
+                  )
+              }
 
             case Question.Update.NeedKey(gk, _, callback) =>
               ResultNeedKey(
                 gk,
                 { coid: Option[ContractId] =>
                   discard[Boolean](callback(coid))
-                  interpretLoop(machine, time)
+                  loopOuter(cache)
                 },
               )
           }
 
         case SResultInterruption =>
-          ResultInterruption(() => interpretLoop(machine, time))
+          ResultInterruption(() => loopOuter(cache))
 
         case _: SResultFinal =>
           finish
@@ -459,8 +472,9 @@ class Engine(val config: EngineConfig = Engine.StableConfig) {
         case SResultError(err) =>
           handleError(err, detailMsg)
       }
+    }
 
-    loop()
+    loop(cache = Map.empty)
   }
 
   def clearPackages(): Unit = compiledPackages.clear()

commit 5482a4564ee17328801a25ccf7dd6fe86bb5105c

[Speedy] fix non-loaded package when translating interface payload
Bug was introduced by #17171, i.e. after last release

daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/SBuiltin.scala
@@ -2197,7 +2197,7 @@ private[lf] object SBuiltin {
           f(SValue.SAnyContract(templateId, templateArg))
         }
 
-      case None => {
+      case None =>
         machine.disclosedContracts.get(coid) match {
           case Some(contract) =>
             ensureContractActive(machine, coid, contract.templateId) {
@@ -2215,40 +2215,43 @@ private[lf] object SBuiltin {
                 case None =>
                   (false, srcTemplateId) // upgrading not enabled; import at source type
               }
+              machine.ensurePackageIsLoaded(
+                destTemplateId.packageId,
+                language.Reference.Template(destTemplateId),
+              ) { () =>
+                importValue(machine, destTemplateId, coinstArg) { templateArg =>
+                  getContractInfo(machine, coid, destTemplateId, templateArg, keyOpt) { contract =>
+                    ensureContractActive(machine, coid, contract.templateId) {
 
-              importValue(machine, destTemplateId, coinstArg) { templateArg =>
-                getContractInfo(machine, coid, destTemplateId, templateArg, keyOpt) { contract =>
-                  ensureContractActive(machine, coid, contract.templateId) {
+                      machine.checkContractVisibility(coid, contract)
+                      machine.enforceLimitAddInputContract()
+                      machine.enforceLimitSignatoriesAndObservers(coid, contract)
 
-                    machine.checkContractVisibility(coid, contract)
-                    machine.enforceLimitAddInputContract()
-                    machine.enforceLimitSignatoriesAndObservers(coid, contract)
+                      val src: TypeConName = srcTemplateId
+                      val dest: TypeConName = destTemplateId
 
-                    val src: TypeConName = srcTemplateId
-                    val dest: TypeConName = destTemplateId
+                      // In Validation mode, we always call validateContractInfo
+                      // In Submission mode, we only call validateContractInfo when src != dest
+                      val needValidationCall: Boolean =
+                        if (machine.validating) {
+                          upgradingIsEnabled
+                        } else {
+                          upgradingIsEnabled && (src != dest)
+                        }
+                      if (needValidationCall) {
 
-                    // In Validation mode, we always call validateContractInfo
-                    // In Submission mode, we only call validateContractInfo when src != dest
-                    val needValidationCall: Boolean =
-                      if (machine.validating) {
-                        upgradingIsEnabled
+                        validateContractInfo(machine, coid, contract) { () =>
+                          f(contract.any)
+                        }
                       } else {
-                        upgradingIsEnabled && (src != dest)
-                      }
-                    if (needValidationCall) {
-
-                      validateContractInfo(machine, coid, contract) { () =>
                         f(contract.any)
                       }
-                    } else {
-                      f(contract.any)
                     }
                   }
                 }
               }
             }
         }
-      }
     }
   }
 

daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Speedy.scala
@@ -333,6 +333,32 @@ private[lf] object Speedy {
       ptx.contractState.locallyCreated.contains(contractId)
     }
 
+    private[speedy] def needPackage(
+        packageId: PackageId,
+        ref: language.Reference,
+        k: () => Control[Question.Update],
+    ): Control[Question.Update] =
+      Control.Question(
+        Question.Update.NeedPackage(
+          packageId,
+          ref,
+          { packages =>
+            this.compiledPackages = packages
+            // To avoid infinite loop in case the packages are not updated properly by the caller
+            assert(compiledPackages.packageIds.contains(packageId))
+            setControl(k())
+          },
+        )
+      )
+
+    private[speedy] def ensurePackageIsLoaded(packageId: PackageId, ref: => language.Reference)(
+        k: () => Control[Question.Update]
+    ): Control[Question.Update] =
+      if (compiledPackages.packageIds.contains(packageId))
+        k()
+      else
+        needPackage(packageId, ref, k)
+
     private[speedy] def enforceLimitSignatoriesAndObservers(
         cid: V.ContractId,
         contract: ContractInfo,
@@ -914,18 +940,11 @@ private[lf] object Speedy {
                   s"definition $ref not found even after caller provided new set of packages",
                 )
               else {
-                asUpdateMachine(NameOf.qualifiedNameOfCurrentFunc)(_ =>
-                  Control.Question(
-                    Question.Update.NeedPackage(
-                      ref.packageId,
-                      language.Reference.Package(ref.packageId),
-                      callback = { packages =>
-                        this.compiledPackages = packages
-                        // To avoid infinite loop in case the packages are not updated properly by the caller
-                        assert(compiledPackages.packageIds.contains(ref.packageId))
-                        setControl(Control.Expression(eval))
-                      },
-                    )
+                asUpdateMachine(NameOf.qualifiedNameOfCurrentFunc)(
+                  _.needPackage(
+                    ref.packageId,
+                    language.Reference.Package(ref.packageId),
+                    () => Control.Expression(eval),
                   )
                 )
               }