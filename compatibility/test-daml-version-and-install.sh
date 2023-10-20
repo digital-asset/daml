f6a0a236b62ad5ffda4bcf0ce1fc1e4aa6d0d7ca
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2023-10-20 15:02:36 +0100

    Use runfiles to locate daml in test-daml-version-and-install.sh

--- compatibility/test-daml-version-and-install.sh
+++ compatibility/test-daml-version-and-install.sh
@@ -2,9 +2,20 @@
 # Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
 set -euo pipefail
+
 # Add daml to path
-export PATH="$(echo external/daml-sdk-*):$PATH"
+export PATH="$(rlocation daml-sdk-$1):$PATH"
 which daml
 
 # Set daml cache
