bb614a6240d6cf3b57cff83873e0920105a310d9
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2023-10-26 14:24:02 +0000

    Minimal upgrade support in TS codegen

--- language-support/ts/daml-types/index.ts
+++ language-support/ts/daml-types/index.ts
@@ -552,7 +552,7 @@ class OptionalWorker<T> implements Serializable<Optional<T>> {
         }
       };
     }
-    this.decoder = jtv.oneOf(jtv.constant(null), this.innerDecoder);
+    this.decoder = jtv.optional(jtv.oneOf(jtv.constant(null), this.innerDecoder)).map(r => r === undefined ? null : r);
   }
 }
 
