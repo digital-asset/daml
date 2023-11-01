3b7c93919436a04a13b0b1d0da531caaab34e097
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-11-01 11:13:07 +0100

    Fixes issue with Ledger class in app-dev/bindings-ts/daml-ledger/ (#17711)
    Co-authored-by: Dylan Thinnes <dylan.thinnes@digitalasset.com>

--- language-support/ts/daml-ledger/index.ts
+++ language-support/ts/daml-ledger/index.ts
@@ -901,7 +901,7 @@ class QueryStreamsManager {
 /**
  * An object of type `Ledger` represents a handle to a Daml ledger.
  */
-class Ledger {
+export class Ledger {
   private readonly token: string;
   private readonly httpBaseUrl: string;
   private readonly wsBaseUrl: string;
