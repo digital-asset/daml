9c817e560e82c526b983029880d4964c2d6ddfe7
Paul Brauner <paul.brauner@digitalasset.com>
2023-11-13 13:24:35 +0100

    disable v2 integration tests

--- daml-script/test/src/com/digitalasset/daml/lf/engine/script/test/DevIT.scala
+++ daml-script/test/src/com/digitalasset/daml/lf/engine/script/test/DevIT.scala
@@ -19,7 +19,10 @@ import org.scalatest.wordspec.AsyncWordSpec
 // TODO(#17366): Once daml3-script diverges from script, V1DevIT and V2DevIT may not be able to
 //     share the same code anymore.
 class DevITV1 extends DevIT(V1)
-class DevITV2 extends DevIT(V2)
+
+// TODO(https://github.com/digital-asset/daml/issues/17812): re-enable this test and control its run
+//  at the bazel target level.
+//class DevITV2 extends DevIT(V2)
 
 class DevIT(override val majorLanguageVersion: LanguageMajorVersion)
     extends AsyncWordSpec
