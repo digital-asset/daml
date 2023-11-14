cee7987446b5a3424937efa0bb324d49c745f563
= <=>
2023-11-14 12:45:37 +1100

    more logging and a minimal package to assist in diagnosis.
    Run with `bazel run simple-https:simple`. This only copies across the way we build the
    HttpsConnectionContext, and is otherwise pretty basic and we still see the same symptoms
    i.e. ClientHello goes in, no ServerHello comes back.

---
+++ simple-https/src/main/scala/simple/Main.scala
@@ -0,0 +1,60 @@
+// Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * Copyright (C) 2009-2023 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package simple
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.server.{Directives, Route}
+import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
+import com.daml.bazeltools.BazelRunfiles
+import com.daml.ledger.api.tls.TlsConfiguration
+import io.netty.handler.ssl.ClientAuth
+import java.nio.file.Paths
+
+object Main extends Directives {
+
+  def main(args: Array[String]): Unit = {
+    val _ = System.setProperty("javax.net.debug", "all")
+
+    val tlsConfig = TlsConfiguration(
+      enabled = true,
+      certChainFile = Some(testCerts("server.crt")),
+      privateKeyFile = Some(testCerts("server.pem")),
+      trustCollectionFile = Some(testCerts("ca.crt")),
+      clientAuth = ClientAuth.NONE,
+      enableCertRevocationChecking = false,
+      minimumServerProtocolVersion = None,
+    )
+    val https: HttpsConnectionContext = httpsConnectionContext(tlsConfig)
+
+    val routes: Route = get { complete("Hello world!") }
+
+    implicit val system: ActorSystem = ActorSystem()
+    val _ = Http().newServerAt("127.0.0.1", 1235).enableHttps(https).bind(routes)
+  }
+
+  // copied from ledger-service/http-json/src/main/scala/com/digitalasset/http/HttpService.scala
+  private def httpsConnectionContext(config: TlsConfiguration): HttpsConnectionContext = {
+    import io.netty.buffer.ByteBufAllocator
+    val sslContext =
+      config.server
+        .getOrElse(
+          throw new IllegalArgumentException(s"$config could not be built as a server ssl context")
+        )
+    ConnectionContext.httpsServer(() => {
+      println("now creating the engine")
+      val engine: javax.net.ssl.SSLEngine = sslContext.newEngine(ByteBufAllocator.DEFAULT)
+      engine.setUseClientMode(false)
+      engine.setWantClientAuth(false)
+      println(s"created the engine: $engine")
+      engine
+    })
+  }
+
+  private def testCerts(basename: String) =
+    BazelRunfiles.rlocation(Paths.get(s"test-common/test-certificates/${basename}")).toFile
+}
