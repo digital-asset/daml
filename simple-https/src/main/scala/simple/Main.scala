766f0dde432a940d18b31928457d2d6984209db1
= <=>
2023-11-15 15:23:09 +1100

    Add example of sslcontext based on a keystore, which does work.
    The issue is that currently we do our config based on the certificate, private key, and ca file seperately, rather than via a keystore + password.
    
    Perhaps for now we could change the HTTPS config to be based on keystore + password, and then figure out how to support the other mode in due course.

--- simple-https/src/main/scala/simple/Main.scala
+++ simple-https/src/main/scala/simple/Main.scala
@@ -20,7 +20,7 @@ object Main extends Directives {
   def main(args: Array[String]): Unit = {
     val _ = System.setProperty("javax.net.debug", "all")
 
-    val tlsConfig = TlsConfiguration(
+    val _ = TlsConfiguration(
       enabled = true,
       certChainFile = Some(testCerts("server.crt")),
       privateKeyFile = Some(testCerts("server.pem")),
@@ -29,22 +29,48 @@ object Main extends Directives {
       enableCertRevocationChecking = false,
       minimumServerProtocolVersion = None,
     )
-    val https: HttpsConnectionContext = httpsConnectionContext(tlsConfig)
+    val https: HttpsConnectionContext = doesWork // doesNotWorkHttpsConnectionContext(tlsConfig)
 
-    val routes: Route = get { complete("Hello world!") }
+    val routes: Route = get { complete("Hello world!\n") }
 
     implicit val system: ActorSystem = ActorSystem()
     val _ = Http().newServerAt("127.0.0.1", 1235).enableHttps(https).bind(routes)
   }
 
+  def doesWork = {
+    import java.io.FileInputStream
+    import java.security.{KeyStore, SecureRandom}
+    import javax.net.ssl.{SSLContext, KeyManagerFactory, TrustManagerFactory}
+
+    val keyStoreFile = testCerts("server.p12")
+    val password = "".toCharArray // The password used to create the keystore file above
+
+    val keyStore = KeyStore.getInstance("PKCS12")
+    keyStore.load(new FileInputStream(keyStoreFile), password)
+
+    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(keyStore, password)
+
+    val trustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+    trustManagerFactory.init(keyStore)
+
+    val context = SSLContext.getInstance("TLS")
+    context.init(
+      keyManagerFactory.getKeyManagers,
+      trustManagerFactory.getTrustManagers,
+      new SecureRandom,
+    )
+
+    ConnectionContext.httpsServer(context)
+  }
+
   // copied from ledger-service/http-json/src/main/scala/com/digitalasset/http/HttpService.scala
-  private def httpsConnectionContext(config: TlsConfiguration): HttpsConnectionContext = {
+  def doesNotWorkHttpsConnectionContext(config: TlsConfiguration): HttpsConnectionContext = {
     import io.netty.buffer.ByteBufAllocator
-    val sslContext =
-      config.server
-        .getOrElse(
-          throw new IllegalArgumentException(s"$config could not be built as a server ssl context")
-        )
+    val sslContext = config.server
+      .getOrElse(
+        throw new IllegalArgumentException(s"$config could not be built as a server ssl context")
+      )
     ConnectionContext.httpsServer(() => {
       println("now creating the engine")
       val engine: javax.net.ssl.SSLEngine = sslContext.newEngine(ByteBufAllocator.DEFAULT)
