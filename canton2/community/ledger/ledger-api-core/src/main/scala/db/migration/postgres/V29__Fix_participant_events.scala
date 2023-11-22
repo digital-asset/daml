d0ac8449ce74ebecc948f585d407baf5b5f95524
Paul Brauner <paul.brauner@digitalasset.com>
2023-11-22 10:33:10 +0100

    rename to canton-3x and only keep the bazel rules

--- canton2/community/ledger/ledger-api-core/src/main/scala/db/migration/postgres/V29__Fix_participant_events.scala
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package com.digitalasset.canton.platform.db.migration.postgres
-
-import com.digitalasset.canton.platform.db.migration.EmptyJavaMigration
-
-/** This migration has been effectively deleted by replacing it with a no-op migration */
-private[migration] class V29__Fix_participant_events extends EmptyJavaMigration
--- canton2/community/ledger/ledger-json-api/src/main/scala/com/digitalasset/canton/cliopts/Http.scala
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package com.digitalasset.canton.cliopts
-
-object Http {
-  val defaultAddress: String = java.net.InetAddress.getLoopbackAddress.getHostAddress
-}
