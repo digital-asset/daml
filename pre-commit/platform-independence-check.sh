a8c027c018fe193e76211a94d5a733603ff875aa
Moisés Ackerman <6054733+akrmn@users.noreply.github.com>
2023-11-30 17:00:24 +0100

    move platform-independence-checks warning to .envrc

--- pre-commit/platform-independence-check.sh
+++ pre-commit/platform-independence-check.sh
@@ -4,11 +4,6 @@
 
 set -euo pipefail
 
-if [ -z "${RUN_PLATFORM_INDEPENDENCE_CHECK:-}" ]; then
-  echo "Warning: platform-independence-check will be skipped."
-  echo "To silence this warning, set 'RUN_PLATFORM_INDEPENDENCE_CHECK' by "
-  echo "running the following line:"
-  echo "    echo export RUN_PLATFORM_INDEPENDENCE_CHECK=no >> $(pwd)/.envrc.private"
-elif [ "$RUN_PLATFORM_INDEPENDENCE_CHECK" = "yes" ]; then
+if [ "$RUN_PLATFORM_INDEPENDENCE_CHECK" = "yes" ]; then
   bazel test //compiler/damlc/tests:platform-independence-dar-hash-file-matches
 fi
