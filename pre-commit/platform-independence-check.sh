002e0564c742f5d1fd57f8e05e14421f9c607663
Moisés Ackerman <6054733+akrmn@users.noreply.github.com>
2023-11-30 16:01:34 +0100

    platform-independence-dar-check pre-commit hook runs depending on env var

---
+++ pre-commit/platform-independence-check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+if [ "$RUN_PLATFORM_INDEPENDENCE_CHECK" = "yes" ]; then
+  bazel test //compiler/damlc/tests:platform-independence-dar-hash-file-matches
+fi
