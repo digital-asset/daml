f21e4b68bb42497320a24f0c4bc51e4e6836866b
Paul Brauner <paul.brauner@digitalasset.com>
2023-12-04 17:20:09 +0100

    build canton 3.x in build.yml

--- ci/build-canton-3x.sh
+++ ci/build-canton-3x.sh
@@ -15,9 +15,11 @@ else
   repo_url="https://$GITHUB_TOKEN@github.com/DACH-NY/canton"
 fi
 
-git clone --depth 1 --branch main $repo_url $tmp
-head=$(git -C $tmp rev-parse HEAD)
-echo "cloned at revision $head"
+commitish=${1:-$(cat canton-3x/canton-3x-sha)}
+
+git clone $repo_url $tmp
+git -C $tmp reset --hard $commitish
+echo "cloned at revision $commitish"
 
 daml_common_staging_src="$tmp/daml-common-staging"
 community_src="$tmp/community"
