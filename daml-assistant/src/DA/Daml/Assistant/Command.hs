3eb66e746ccd8abf4ba1bbd0ddb89f6f4fa71c18
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2024-01-08 10:25:17 +0000

    Document split release flag, document tarball in TARGET

--- daml-assistant/src/DA/Daml/Assistant/Command.hs
+++ daml-assistant/src/DA/Daml/Assistant/Command.hs
@@ -111,7 +111,7 @@ versionParser = VersionOptions
 
 installParser :: Parser InstallOptions
 installParser = InstallOptions
-    <$> optional (RawInstallTarget <$> argument str (metavar "TARGET" <> completeWith ["latest"] <> help "The SDK version to install. Use 'latest' to download and install the latest stable SDK version available. Run 'daml install' to see the full set of options."))
+    <$> optional (RawInstallTarget <$> argument str (metavar "TARGET" <> completeWith ["latest"] <> help "The SDK version to install, or a tarball to install from directly. Use 'latest' to download and install the latest stable SDK version available. Run 'daml install' to see the full set of options."))
     <*> flagYesNoAuto "snapshots" False "Pick up snapshot versions with daml install latest." idm
     <*> (InstallAssistant <$> flagYesNoAuto' "install-assistant" "Install associated Daml assistant version. Can be set to \"yes\" (always installs), \"no\" (never installs), or \"auto\" (installs if newer). Default is \"auto\"." idm)
     <*> iflag ActivateInstall "activate" hidden "Activate installed version of daml"
@@ -121,7 +121,7 @@ installParser = InstallOptions
     <*> fmap BashCompletions (flagYesNoAuto' "bash-completions" "Install bash completions for Daml assistant. Default is yes for linux and mac, no for windows." idm)
     <*> fmap ZshCompletions (flagYesNoAuto' "zsh-completions" "Install Zsh completions for Daml assistant. Default is yes for linux and mac, no for windows." idm)
     <*> fmap InstallWithInternalVersion (flagYesNoAuto "install-with-internal-version" False "Allow installing from a tarball that has no associated release by using the tarball's SDK version as its release version." internal)
-    <*> fmap InstallWithCustomVersion (optionOnce (Just <$> str) (long "install-with-custom-version" <> value Nothing))
+    <*> fmap InstallWithCustomVersion (optionOnce (Just <$> str) (long "install-with-custom-version" <> value Nothing <> help "Install a tarball with a custom release version. Useful for environments without internet access, where Daml cannot look up the release version from Github."))
     where
         iflag p name opts desc = fmap p (switch (long name <> help desc <> opts))
 
