cc7730e07d0677ad997f81055a7b1745968215e3
Marcin Ziolek <marcin.ziolek@digitalasset.com>
2024-01-11 19:37:37 +0100

    remove all apart from assistant

--- fmt.sh
+++ fmt.sh
@@ -175,7 +175,7 @@ if ! diff .bazelrc compatibility/.bazelrc >/dev/null; then
     diff -u .bazelrc compatibility/.bazelrc
 fi
 
-# check akka is not used as dependency except in navigator_maven and deprecated_maven
+# check akka is not used as dependency except in deprecated_maven
 for f in $(ls *_install*.json | egrep -v "deprecated"); do
   if grep -q akka $f;  then
     echo $f contains a dependency to akka
