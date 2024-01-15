e03c821a2654ae17176472cba3c8ba2a0db11b44
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2024-01-15 13:09:53 +0100

    [2.x] update copyright

--- navigator/frontend/modernizr.js
+++ navigator/frontend/modernizr.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 // Variation of https://webpack.js.org/loaders/val-loader/#modernizr
