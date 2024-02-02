5485cc405cf971b7965e3f272eee658f07d8bf7f
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2024-02-02 11:31:40 +0100

    remove infra folder
    Moved to DACH-NY/daml-ci.

--- infra/bracin/nix/nixpkgs.nix
+++
@@ -1,7 +0,0 @@
-let
-  spec = builtins.fromJSON (builtins.readFile ./src.json);
-in
-  import (builtins.fetchTarball {
-    url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
-    sha256 = spec.sha256;
-  }) {}
