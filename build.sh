7e36c507bd7abc6f58b85a811cd3ee2ec726cba4
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2024-02-06 14:52:09 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -91,10 +91,6 @@ $bazel build //... \
 
 # Set up a shared PostgreSQL instance.
 export POSTGRESQL_ROOT_DIR="${POSTGRESQL_TMP_ROOT_DIR:-$DIR/.tmp-pg}/daml/postgresql"
-if [ -e "$POSTGRESQL_ROOT_DIR" ]; then
-  echo "'$POSTGRESQL_ROOT_DIR' exists, please choose another one by setting $POSTGRESQL_TMP_ROOT_DIR."
-  exit 1
-fi
 export POSTGRESQL_DATA_DIR="${POSTGRESQL_ROOT_DIR}/data"
 export POSTGRESQL_LOG_FILE="${POSTGRESQL_ROOT_DIR}/postgresql.log"
 export POSTGRESQL_HOST='localhost'
