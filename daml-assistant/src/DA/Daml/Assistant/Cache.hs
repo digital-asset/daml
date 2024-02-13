74a9d98a1f4e9993412a61cf87cbaa73c88d1f5b
dylant-da <106664681+dylant-da@users.noreply.github.com>
2024-02-13 12:28:32 +0000

    Support split release artifactory, improve missing connection errors (#18271)
    * Add resolution via artifactory, handle HTTP errors
    
    * Replace origin with wrapErr in resolveReleaseVersionUnsafe
    
    * Handle broken connections (e.g. no internet)
    
    * drop comment, fix foldMap id to fold
    
    * remove debugging writeFile calls
    
    run-all-tests: true
    
    * Rename partial field damlPath to damlPathUnsafe and add safe function
    
    * support multiple possible installation locations
    
    * update alternatives
    
    * Wrap CouldNotResolveReleaseVersion into AssistantError in unsafe resolve

--- daml-assistant/src/DA/Daml/Assistant/Cache.hs
+++ daml-assistant/src/DA/Daml/Assistant/Cache.hs
@@ -6,6 +6,7 @@ module DA.Daml.Assistant.Cache
     ( cacheAvailableSdkVersions
     , CacheAge (..)
     , UseCache (..)
+    , damlPath
     , cacheWith
     , loadFromCacheWith
     , saveToCacheWith
@@ -53,17 +54,21 @@ data UseCache
   = UseCache
       { overrideTimeout :: Maybe CacheTimeout
       , cachePath :: CachePath
-      , damlPath :: DamlPath
+      , damlPathUnsafe :: DamlPath
       }
   | DontUseCache
   deriving (Show, Eq)
+
+damlPath :: UseCache -> Maybe DamlPath
+damlPath DontUseCache = Nothing
+damlPath UseCache { damlPathUnsafe } = Just damlPathUnsafe
 
 cacheAvailableSdkVersions
     :: UseCache
     -> (Maybe [ReleaseVersion] -> IO [ReleaseVersion])
     -> IO ([ReleaseVersion], CacheAge)
 cacheAvailableSdkVersions DontUseCache getVersions = (, Fresh) <$> getVersions Nothing
-cacheAvailableSdkVersions UseCache { overrideTimeout, cachePath, damlPath } getVersions = do
+cacheAvailableSdkVersions UseCache { overrideTimeout, cachePath, damlPathUnsafe = damlPath } getVersions = do
     damlConfigE <- tryConfig $ readDamlConfig damlPath
     let configUpdateCheckM = join $ eitherToMaybe (queryDamlConfig ["update-check"] =<< damlConfigE)
         (neverRefresh, timeout)
