be9e524a322e7118f2b4e3dde09b9b2acc695185
Remy Haemmerle <Remy.Haemmerle@daml.com>
2024-02-27 12:32:36 +0100

    fix middleman tets

--- compiler/damlc/tests/src/DA/Test/DamlcIntegration.hs
+++ compiler/damlc/tests/src/DA/Test/DamlcIntegration.hs
@@ -310,7 +310,7 @@ getIntegrationTests registerTODO scenarioService (packageDbPath, packageFlags) =
             , getCantSkipPreprocessorTestFiles
             ]
 
-    let outdir = "compiler/damlc/output"
+    let outdir = "/tmp/compiler/damlc/output"
     createDirectoryIfMissing True outdir
 
     -- initialise the compiler service
