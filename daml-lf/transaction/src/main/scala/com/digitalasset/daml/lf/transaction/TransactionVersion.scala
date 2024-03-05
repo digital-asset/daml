d795a74845f6d8fbe56a87ce9054ee73c323c45c
Paul Brauner <paul.brauner@digitalasset.com>
2024-03-05 17:17:48 +0100

    remove minSharedKeys

--- daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/TransactionVersion.scala
+++ daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/TransactionVersion.scala
@@ -46,7 +46,6 @@ object TransactionVersion {
   // TODO(https://github.com/digital-asset/daml/issues/18240) move this feature flag to VDev.
   private[lf] val minContractKeys = V31
 
-  private[lf] val minSharedKeys = V31
   private[lf] val minChoiceAuthorizers = VDev
 
   private[lf] val assignNodeVersion: LanguageVersion => TransactionVersion = {
