b51d09056f7e95107619f2de19ac98cc304895c3
Remy Haemmerle <Remy.Haemmerle@daml.com>
2024-03-18 17:44:21 +0100

    fix hash

--- daml-lf/archive/proto_check_hash.sh
+++ daml-lf/archive/proto_check_hash.sh
@@ -8,7 +8,7 @@ set -e
 declare -a checkSums=(
  "1c44a789914665751f62951a1cc7b7ccba9da2c66021017b48d4fac3c7024409
   daml-lf/archive/src/stable/protobuf/com/daml/daml_lf_2_1/daml_lf2.proto"
- "a03b1b1d0ab81d43bf72b5a1276d342437d1a11965affa55558aa472cdfae74a
+ "3c21928962d31911efde0d607016d0fa5582e640fb6cef9900397c699f0f8afe
   daml-lf/archive/src/stable/protobuf/com/daml/daml_lf_2_1/daml_lf.proto"
 )
 
