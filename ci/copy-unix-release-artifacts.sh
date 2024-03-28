6701813aff42a51285d4a9016c2c31fdee920eb4
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2024-03-28 19:45:16 +0100

    [release] faster releases - experiment (#18812)
    When we build a release, it is always a "past" commit - typically, one
    that has already been tested twice: once when the corresponding PR was
    run, and then again as a "main"-branch commit.
    
    Release branches don't run, but their protection rules enforce linear
    merges.
    
    Either way, we know we're building a _good_ commit, and, assuming our
    builds and tests are hermetic, testing that commit again when we make a
    release is a pure waste of time and CPU resources.
    
    The other case, where we make an ad-hoc release from a branch that has
    not been merged, has a similar issue: we do not necessarily want to run
    the full test suite, because part of the reason we need that commit may
    be that it doesn't succeed as is.
    
    Based on that observation, I wondered what might be the minimal set of
    things we actually need to build when making a release. This PR is an
    experiment in trying to find that out.

--- ci/copy-unix-release-artifacts.sh
+++ ci/copy-unix-release-artifacts.sh
@@ -20,7 +20,6 @@ mkdir -p $OUTPUT_DIR/split-release
 
 TARBALL=daml-sdk-$RELEASE_TAG-$NAME.tar.gz
 EE_TARBALL=daml-sdk-$RELEASE_TAG-$NAME-ee.tar.gz
-bazel build //release:sdk-release-tarball-ce //release:sdk-release-tarball-ee
 cp bazel-bin/release/sdk-release-tarball-ce.tar.gz $OUTPUT_DIR/github/$TARBALL
 # Used for the non-split release process.
 cp bazel-bin/release/sdk-release-tarball-ee.tar.gz $OUTPUT_DIR/artifactory/$EE_TARBALL
@@ -28,54 +27,40 @@ cp bazel-bin/release/sdk-release-tarball-ee.tar.gz $OUTPUT_DIR/artifactory/$EE_T
 cp bazel-bin/release/sdk-release-tarball-ee.tar.gz $OUTPUT_DIR/split-release/$EE_TARBALL
 
 
-bazel build //compiler/damlc:damlc-dist
 cp bazel-bin/compiler/damlc/damlc-dist.tar.gz $OUTPUT_DIR/split-release/damlc-$RELEASE_TAG-$NAME.tar.gz
 
 # Platform independent artifacts are only built on Linux.
 if [[ "$NAME" == "linux-intel" ]]; then
-    bazel build //release:protobufs
     PROTOS_ZIP=protobufs-$RELEASE_TAG.zip
     cp bazel-bin/release/protobufs.zip $OUTPUT_DIR/github/$PROTOS_ZIP
 
     JSON_API=http-json-$RELEASE_TAG.jar
     JSON_API_EE=http-json-$RELEASE_TAG-ee.jar
-    bazel build //ledger-service/http-json:http-json-binary_deploy.jar
     cp bazel-bin/ledger-service/http-json/http-json-binary_deploy.jar $OUTPUT_DIR/github/$JSON_API
-    bazel build //ledger-service/http-json:http-json-binary-ee_deploy.jar
     cp bazel-bin/ledger-service/http-json/http-json-binary-ee_deploy.jar $OUTPUT_DIR/artifactory/$JSON_API_EE
 
     TRIGGER_SERVICE=trigger-service-$RELEASE_TAG.jar
     TRIGGER_SERVICE_EE=trigger-service-$RELEASE_TAG-ee.jar
-    bazel build //triggers/service:trigger-service-binary-ce_deploy.jar
     cp bazel-bin/triggers/service/trigger-service-binary-ce_deploy.jar $OUTPUT_DIR/github/$TRIGGER_SERVICE
-    bazel build //triggers/service:trigger-service-binary-ee_deploy.jar
     cp bazel-bin/triggers/service/trigger-service-binary-ee_deploy.jar $OUTPUT_DIR/artifactory/$TRIGGER_SERVICE_EE
 
     OAUTH2_MIDDLEWARE=oauth2-middleware-$RELEASE_TAG.jar
-    bazel build //triggers/service/auth:oauth2-middleware-binary_deploy.jar
     cp bazel-bin/triggers/service/auth/oauth2-middleware-binary_deploy.jar $OUTPUT_DIR/github/$OAUTH2_MIDDLEWARE
 
 
     TRIGGER=daml-trigger-runner-$RELEASE_TAG.jar
-    bazel build //triggers/runner:trigger-runner_deploy.jar
     cp bazel-bin/triggers/runner/trigger-runner_deploy.jar $OUTPUT_DIR/artifactory/$TRIGGER
 
     SCRIPT=daml-script-$RELEASE_TAG.jar
-    bazel build //daml-script/runner:daml-script-binary_deploy.jar
     cp bazel-bin/daml-script/runner/daml-script-binary_deploy.jar $OUTPUT_DIR/artifactory/$SCRIPT
 
     NON_REPUDIATION=non-repudiation-$RELEASE_TAG-ee.jar
-    bazel build //runtime-components/non-repudiation-app:non-repudiation-app_deploy.jar
     cp bazel-bin/runtime-components/non-repudiation-app/non-repudiation-app_deploy.jar $OUTPUT_DIR/artifactory/$NON_REPUDIATION
 
     NON_REPUDIATION_CORE_JAR=non-repudiation-core-$RELEASE_TAG.jar
     NON_REPUDIATION_CORE_POM=non-repudiation-core-$RELEASE_TAG.pom
     NON_REPUDIATION_CORE_SRC=non-repudiation-core-$RELEASE_TAG-sources.jar
     NON_REPUDIATION_CORE_DOC=non-repudiation-core-$RELEASE_TAG-javadoc.jar
-    bazel build \
-          //runtime-components/non-repudiation-core/... \
-          //runtime-components/non-repudiation-core:non-repudiation-core_javadoc \
-          //runtime-components/non-repudiation-core:libnon-repudiation-core-src.jar
     cp bazel-bin/runtime-components/non-repudiation-core/libnon-repudiation-core.jar $OUTPUT_DIR/artifactory/$NON_REPUDIATION_CORE_JAR
     cp bazel-bin/runtime-components/non-repudiation-core/non-repudiation-core_pom.xml $OUTPUT_DIR/artifactory/$NON_REPUDIATION_CORE_POM
     cp bazel-bin/runtime-components/non-repudiation-core/libnon-repudiation-core-src.jar $OUTPUT_DIR/artifactory/$NON_REPUDIATION_CORE_SRC
@@ -86,32 +71,24 @@ if [[ "$NAME" == "linux-intel" ]]; then
     NON_REPUDIATION_CLIENT_POM=non-repudiation-client-$RELEASE_TAG.pom
     NON_REPUDIATION_CLIENT_SRC=non-repudiation-client-$RELEASE_TAG-sources.jar
     NON_REPUDIATION_CLIENT_DOC=non-repudiation-client-$RELEASE_TAG-javadoc.jar
-    bazel build \
-          //runtime-components/non-repudiation-client/... \
-          //runtime-components/non-repudiation-client:non-repudiation-client_javadoc \
-          //runtime-components/non-repudiation-client:libnon-repudiation-client-src.jar
     cp bazel-bin/runtime-components/non-repudiation-client/libnon-repudiation-client.jar $OUTPUT_DIR/artifactory/$NON_REPUDIATION_CLIENT_JAR
     cp bazel-bin/runtime-components/non-repudiation-client/non-repudiation-client_pom.xml $OUTPUT_DIR/artifactory/$NON_REPUDIATION_CLIENT_POM
     cp bazel-bin/runtime-components/non-repudiation-client/libnon-repudiation-client-src.jar $OUTPUT_DIR/artifactory/$NON_REPUDIATION_CLIENT_SRC
     cp bazel-bin/runtime-components/non-repudiation-client/non-repudiation-client_javadoc.jar $OUTPUT_DIR/artifactory/$NON_REPUDIATION_CLIENT_DOC
 
     mkdir -p $OUTPUT_DIR/split-release/daml-libs/daml-script
-    bazel build //daml-script/daml:daml-script-dars
     cp bazel-bin/daml-script/daml/*.dar $OUTPUT_DIR/split-release/daml-libs/daml-script/
 
     mkdir -p $OUTPUT_DIR/split-release/daml-libs/daml-trigger
-    bazel build //triggers/daml:daml-trigger-dars
     cp bazel-bin/triggers/daml/*.dar $OUTPUT_DIR/split-release/daml-libs/daml-trigger/
 
     mkdir -p $OUTPUT_DIR/split-release/docs
 
-    bazel build //docs:sphinx-source-tree //docs:pdf-fonts-tar //docs:non-sphinx-html-docs //docs:sphinx-source-tree-deps
     cp bazel-bin/docs/sphinx-source-tree.tar.gz $OUTPUT_DIR/split-release/docs/sphinx-source-tree-$RELEASE_TAG.tar.gz
     cp bazel-bin/docs/sphinx-source-tree-deps.tar.gz $OUTPUT_DIR/split-release/docs/sphinx-source-tree-deps-$RELEASE_TAG.tar.gz
     cp bazel-bin/docs/pdf-fonts-tar.tar.gz $OUTPUT_DIR/split-release/docs/pdf-fonts-$RELEASE_TAG.tar.gz
     cp bazel-bin/docs/non-sphinx-html-docs.tar.gz $OUTPUT_DIR/split-release/docs/non-sphinx-html-docs-$RELEASE_TAG.tar.gz
 
-    bazel build //test-evidence:generate-security-test-evidence-files
     cp bazel-bin/test-evidence/daml-security-test-evidence.csv $OUTPUT_DIR/github/daml-security-test-evidence-$RELEASE_TAG.csv
     cp bazel-bin/test-evidence/daml-security-test-evidence.json $OUTPUT_DIR/github/daml-security-test-evidence-$RELEASE_TAG.json
 fi
