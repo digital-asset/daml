6f81c3207e31d0e164738b6b4c94735cade34d6f
Azure Pipelines Daml Build <support@digitalasset.com>
2024-04-09 15:52:23 +0000

    update canton to 20240409.11938.0.v11cf2e73/2.9.0-snapshot.20240409.11938.0.v11cf2e73 in main-2.x
    tell-slack: canton

--- sdk/canton/daml-common-staging/daml-errors/src/main/scala/com/daml/error/SerializableErrorComponents.scala
+++ sdk/canton/daml-common-staging/daml-errors/src/main/scala/com/daml/error/SerializableErrorComponents.scala
@@ -3,7 +3,11 @@
 
 package com.daml.error
 
-import com.daml.error.NonSecuritySensitiveErrorCodeComponents.{stringsPackedSize, truncateDetails}
+import com.daml.error.NonSecuritySensitiveErrorCodeComponents.{
+  MaxCauseLogLength,
+  stringsPackedSize,
+  truncateDetails,
+}
 import com.daml.error.SerializableErrorCodeComponents.*
 import com.daml.error.utils.ErrorDetails
 import io.grpc.Status.Code
@@ -141,7 +145,7 @@ private[error] final case class NonSecuritySensitiveErrorCodeComponents(
   /** Truncates and serializes the self-service error components into a [[com.google.rpc.Status]].
     *
     * Truncation happens for both the error message and error details aiming to ensure that
-    * the maximum message size ([[ErrorCode.MaxCauseLogLength]]) and maximum total Status serialization size ([[ErrorCode.MaxErrorContentBytes]]) are respected.
+    * the maximum message size ([[NonSecuritySensitiveErrorCodeComponents.MaxCauseLogLength]]) and maximum total Status serialization size ([[ErrorCode.MaxErrorContentBytes]]) are respected.
     */
   def toStatusProto(maxSizeBytes: Int): com.google.rpc.Status = {
     val grpcStatusCode = validatedGrpcErrorCode(errorCode.category.grpcCode)
@@ -155,7 +159,7 @@ private[error] final case class NonSecuritySensitiveErrorCodeComponents(
       correlationId.orElse(traceId).map(ErrorDetails.RequestInfoDetail(_).toRpcAny)
     val errorCodeId = errorCode.id
 
-    val validatedMessage = errorCode.toMsg(cause, correlationId)
+    val validatedMessage = errorCode.toMsg(cause, correlationId, limit = Some(MaxCauseLogLength))
     val mandatoryDetailsEncodedSize =
       GrpcCodeBytes + MaximumPerTagOverheadBytes + // Grpc code size + its byte tag overhead
         stringsPackedSize(
@@ -206,6 +210,10 @@ private[error] final case class NonSecuritySensitiveErrorCodeComponents(
 }
 
 private[error] object NonSecuritySensitiveErrorCodeComponents {
+
+  /** The maximum size (in characters) of the self-service error description, truncated for transport as part of a Status */
+  val MaxCauseLogLength = 512
+
   private[error] def truncateDetails(
       context: Map[String, String],
       errResources: Seq[(ErrorResource, String)],
