a4d2dea198be2233b8c295d1b763ac5e67800b4a
Paul Brauner <paul.brauner@digitalasset.com>
2024-04-10 11:56:10 +0200

    compute projections from the IDE ledger

--- sdk/daml-lf/model-test-lib/src/main/scala/com/digitalasset/daml/lf/model/test/ToProjection.scala
+++ sdk/daml-lf/model-test-lib/src/main/scala/com/digitalasset/daml/lf/model/test/ToProjection.scala
@@ -7,9 +7,12 @@ package test
 
 import com.daml.ledger.javaapi
 import com.daml.lf.data.Ref
+import com.daml.lf.ledger.EventId
 import com.daml.lf.model.test.Projections._
+import com.daml.lf.scenario.ScenarioLedger
 import com.daml.lf.value.{Value => V}
 
+import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 
 object ToProjection {
@@ -21,57 +24,142 @@ object ToProjection {
       contractIds: ContractIdReverseMapping,
   ) {
 
-    def convertFromTransactionTrees(
+    def fromTransactionTrees(
         trees: List[javaapi.data.TransactionTree]
     ): Projection =
-      trees.map(convertFromTransactionTree)
+      trees.map(fromTransactionTree)
 
-    def convertFromTransactionTree(
+    private def fromTransactionTree(
         tree: javaapi.data.TransactionTree
     ): Commands = {
       Commands(
         tree.getRootEventIds.asScala.toList
-          .map(convertFromEventId(tree.getEventsById.asScala.toMap, _))
+          .map(fromEventId(tree.getEventsById.asScala.toMap, _))
       )
     }
 
-    def convertFromEventId(
+    private def fromEventId(
         eventsById: Map[String, javaapi.data.TreeEvent],
         eventId: String,
     ): Action = {
       eventsById(eventId) match {
         case create: javaapi.data.CreatedEvent =>
           Create(
-            contractId = convertFromContractId(create.getContractId),
-            signatories = convertFromPartyIds(create.getSignatories),
+            contractId = fromContractId(create.getContractId),
+            signatories = fromPartyIds(create.getSignatories),
           )
         case exercise: javaapi.data.ExercisedEvent =>
           Exercise(
             kind = if (exercise.isConsuming) Consuming else NonConsuming,
-            contractId = convertFromContractId(exercise.getContractId),
-            controllers = convertFromPartyIds(exercise.getActingParties),
+            contractId = fromContractId(exercise.getContractId),
+            controllers = fromPartyIds(exercise.getActingParties),
             subTransaction = exercise.getChildEventIds.asScala.toList
-              .map(convertFromEventId(eventsById, _)),
+              .map(fromEventId(eventsById, _)),
           )
         case event =>
           throw new IllegalArgumentException(s"Unsupported event type: $event")
       }
     }
 
-    def convertFromContractId(contractId: String): ContractId =
+    private def fromContractId(contractId: String): ContractId =
       contractIds(V.ContractId.assertFromString(contractId))
 
-    def convertFromPartyId(partyId: String): PartyId =
+    private def fromPartyId(partyId: String): PartyId =
       partyIds(Ref.Party.assertFromString(partyId))
 
-    def convertFromPartyIds(partyIds: java.lang.Iterable[String]): PartySet =
-      partyIds.asScala.map(convertFromPartyId).toSet
+    private def fromPartyIds(partyIds: java.lang.Iterable[String]): PartySet =
+      partyIds.asScala.map(fromPartyId).toSet
   }
 
-  def convertFromCanton(
+  private[ToProjection] class FromScenarioLedger(
+      partyIds: PartyIdReverseMapping,
+      contractIds: ContractIdReverseMapping,
+      party: Ref.Party,
+  ) {
+
+    import com.daml.lf.scenario.{ScenarioLedger => SL}
+    import com.daml.lf.transaction.{Node, NodeId}
+
+    def fromScenarioLedger(scenarioLedger: ScenarioLedger): Projection =
+      scenarioLedger.scenarioSteps.values
+        .collect { case commit: SL.Commit =>
+          fromTransactionRoots(
+            scenarioLedger.ledgerData.nodeInfos,
+            commit.txId,
+            commit.richTransaction.transaction.roots.toList,
+          )
+        }
+        .flatten
+        .toList
+
+    private def fromTransactionRoots(
+        nodeInfos: SL.LedgerNodeInfos,
+        txId: ScenarioLedger.TransactionId,
+        roots: List[NodeId],
+    ): Option[Commands] = {
+      val newRoots = mutable.Buffer.empty[Action]
+      val _ =
+        roots.foreach(fromTransactionNode(nodeInfos, txId, newRoots, _, parentIncluded = false))
+      Option.when(newRoots.nonEmpty)(Commands(newRoots.toList))
+    }
+
+    private def fromTransactionNode(
+        nodeInfos: ScenarioLedger.LedgerNodeInfos,
+        txId: ScenarioLedger.TransactionId,
+        newRoots: mutable.Buffer[Action],
+        nodeId: NodeId,
+        parentIncluded: Boolean,
+    ): Option[Action] = {
+      val eventId = EventId(txId.id, nodeId)
+      val nodeInfo = nodeInfos(eventId)
+      val included = nodeInfo.disclosures.contains(party)
+      val res = nodeInfo.node match {
+        case create: Node.Create =>
+          Option.when(included) {
+            Create(
+              contractId = contractIds(create.coid),
+              signatories = fromParties(create.signatories),
+            )
+          }
+        case exercise: Node.Exercise =>
+          // Run unconditionally in order to collect the new roots
+          val subTransaction = exercise.children.toList.flatMap(
+            fromTransactionNode(nodeInfos, txId, newRoots, _, included)
+          )
+          Option.when(included) {
+            Exercise(
+              kind = if (exercise.consuming) Consuming else NonConsuming,
+              contractId = contractIds(exercise.targetCoid),
+              controllers = fromParties(exercise.actingParties),
+              subTransaction = subTransaction,
+            )
+          }
+        case _ => None
+      }
+      res.foreach(action =>
+        if (!parentIncluded) {
+          newRoots += action
+        }
+      )
+      res
+    }
+
+    private def fromParties(parties: Set[Ref.Party]): PartySet =
+      parties.map(partyIds)
+  }
+
+  def convertFromCantonProjection(
       partyIds: PartyIdReverseMapping,
       contractIds: ContractIdReverseMapping,
       trees: List[javaapi.data.TransactionTree],
   ): Projection =
-    new FromCanton(partyIds, contractIds).convertFromTransactionTrees(trees)
+    new FromCanton(partyIds, contractIds).fromTransactionTrees(trees)
+
+  def projectFromScenarioLedger(
+      partyIds: PartyIdReverseMapping,
+      contractIds: ContractIdReverseMapping,
+      scenarioLedger: ScenarioLedger,
+      party: Ref.Party,
+  ): Projection =
+    new FromScenarioLedger(partyIds, contractIds, party).fromScenarioLedger(scenarioLedger)
 }
