5d4fdc16096af589ec603cfd475ba100aabfd20a
Paul Brauner <paul.brauner@digitalasset.com>
2024-04-16 12:12:57 +0200

    add symbolic solver

---
+++ sdk/daml-lf/model-test-lib/src/main/scala/com/digitalasset/daml/lf/model/test/Symbolic.scala
@@ -0,0 +1,44 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.lf
+package model
+package test
+
+import com.microsoft.z3._
+
+object Symbolic {
+  type ContractIdSort = IntSort
+  type PartySort = IntSort
+  type PartySetSort = SetSort[IntSort]
+
+  type PartySet = ArrayExpr[PartySort, BoolSort]
+  type PartyId = IntExpr
+  type ContractId = IntExpr
+
+  sealed trait ExerciseKind
+  case object Consuming extends ExerciseKind
+  case object NonConsuming extends ExerciseKind
+
+  sealed trait Action
+  final case class Create(
+      contractId: ContractId,
+      signatories: PartySet,
+      observers: PartySet,
+  ) extends Action
+  final case class Exercise(
+      kind: ExerciseKind,
+      contractId: ContractId,
+      controllers: PartySet,
+      choiceObservers: PartySet,
+      subTransaction: Transaction,
+  ) extends Action
+  final case class Fetch(contractId: ContractId) extends Action
+  final case class Rollback(subTransaction: Transaction) extends Action
+
+  type Transaction = List[Action]
+
+  final case class Commands(actAs: PartySet, actions: Transaction)
+
+  type Ledger = List[Commands]
+}
