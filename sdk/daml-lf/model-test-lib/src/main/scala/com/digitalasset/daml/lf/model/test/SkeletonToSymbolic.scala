c00d6665ec2509708a2bbc0903709d233bf89813
Paul Brauner <paul.brauner@digitalasset.com>
2024-04-18 17:26:36 +0200

    use z3 to also validate concrete ledgers

---
+++ sdk/daml-lf/model-test-lib/src/main/scala/com/digitalasset/daml/lf/model/test/SkeletonToSymbolic.scala
@@ -0,0 +1,77 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.lf
+package model
+package test
+
+import com.daml.lf.model.test.{Skeletons => Skel, Symbolic => Sym}
+import com.microsoft.z3.Context
+
+import scala.annotation.nowarn
+
+object SkeletonToSymbolic {
+  def toSymbolic(ctx: Context, skeleton: Skel.Scenario): Sym.Scenario =
+    new Converter(ctx).toSymbolic(skeleton)
+
+  private class Converter(ctx: Context) {
+
+    private val partySetSort: Sym.PartySetSort = ctx.mkSetSort(ctx.mkIntSort())
+    private val contractIdSort: Sym.ContractIdSort = ctx.mkIntSort()
+    private val participantIdSort: Sym.ParticipantIdSort = ctx.mkIntSort()
+
+    private def mkFreshParticipantId(): Sym.ParticipantId =
+      ctx.mkFreshConst("p", participantIdSort).asInstanceOf[Sym.ContractId]
+
+    private def mkFreshContractId(): Sym.ContractId =
+      ctx.mkFreshConst("c", contractIdSort).asInstanceOf[Sym.ContractId]
+
+    private def mkFreshPartySet(name: String): Sym.PartySet =
+      ctx.mkFreshConst(name, partySetSort).asInstanceOf[Sym.PartySet]
+
+    private def toSymbolic(commands: Skel.Commands): Sym.Commands =
+      Sym.Commands(mkFreshParticipantId(), mkFreshPartySet("a"), commands.actions.map(toSymbolic))
+
+    private def toSymbolic(kind: Skel.ExerciseKind): Sym.ExerciseKind = {
+      kind match {
+        case Skel.Consuming => Sym.Consuming
+        case Skel.NonConsuming => Sym.NonConsuming
+      }
+    }
+
+    private def toSymbolic(action: Skel.Action): Sym.Action = action match {
+      case Skel.Create() =>
+        Sym.Create(
+          mkFreshContractId(),
+          mkFreshPartySet("s"),
+          mkFreshPartySet("o"),
+        )
+      case Skel.Exercise(kind, subTransaction) =>
+        Sym.Exercise(
+          toSymbolic(kind),
+          mkFreshContractId(),
+          mkFreshPartySet("k"),
+          mkFreshPartySet("q"),
+          subTransaction.map(toSymbolic),
+        )
+      case Skel.Fetch() =>
+        Sym.Fetch(mkFreshContractId())
+      case Skel.Rollback(subTransaction) =>
+        Sym.Rollback(subTransaction.map(toSymbolic))
+    }
+
+    def toSymbolic(ledger: Skel.Ledger): Sym.Ledger =
+      ledger.map(toSymbolic)
+
+    @nowarn("cat=unused")
+    def toSymbolic(participant: Skel.Participant): Sym.Participant =
+      Sym.Participant(mkFreshParticipantId(), mkFreshPartySet("ps"))
+
+    def toSymbolic(topology: Skel.Topology): Sym.Topology =
+      topology.map(toSymbolic)
+
+    def toSymbolic(scenario: Skel.Scenario): Sym.Scenario =
+      Sym.Scenario(toSymbolic(scenario.topology), toSymbolic(scenario.ledger))
+  }
+
+}
