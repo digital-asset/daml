05e887d21f56b31f5b26789de221a46af2ea472c
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2024-04-19 11:48:17 +0200

    [2.7] re-enable Windows testing (#19037)
    If anyone's been wondering why we've had so few Windows failures lately,
    this is why.

--- sdk/build.ps1
+++ sdk/build.ps1
@@ -71,19 +71,17 @@ bazel build //... `
 
 bazel shutdown
 
-if ($env:SKIP_TESTS -ceq "False") {
-    # Generate mapping from shortened scala-test names on Windows to long names on Linux and MacOS.
-    ./ci/remap-scala-test-short-names.ps1 `
-      | Out-File -Encoding UTF8 -NoNewline scala-test-suite-name-map.json
+# Generate mapping from shortened scala-test names on Windows to long names on Linux and MacOS.
+./ci/remap-scala-test-short-names.ps1 `
+  | Out-File -Encoding UTF8 -NoNewline scala-test-suite-name-map.json
 
-    $tag_filter = "-dev-canton-test"
+$tag_filter = "-dev-canton-test"
 
-    bazel test //... `
-      `-`-build_tag_filters "$tag_filter,-canton-ee" `
-      `-`-test_tag_filters "$tag_filter,-canton-ee" `
-      `-`-profile test-profile.json `
-      `-`-experimental_profile_include_target_label `
-      `-`-build_event_json_file test-events.json `
-      `-`-build_event_publish_all_actions `
-      `-`-experimental_execution_log_file ${ARTIFACT_DIRS}/logs/test_execution_windows_$env:SYSTEM_JOBATTEMPT.log `
-}
+bazel test //... `
+  `-`-build_tag_filters "$tag_filter,-canton-ee" `
+  `-`-test_tag_filters "$tag_filter,-canton-ee" `
+  `-`-profile test-profile.json `
+  `-`-experimental_profile_include_target_label `
+  `-`-build_event_json_file test-events.json `
+  `-`-build_event_publish_all_actions `
+  `-`-experimental_execution_log_file ${ARTIFACT_DIRS}/logs/test_execution_windows_$env:SYSTEM_JOBATTEMPT.log `
