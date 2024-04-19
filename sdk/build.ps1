0bc6047ee98a8f5c2e6166779d00cc5a57a4e636
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2024-04-19 10:39:12 +0200

    [2.3] re-enable Windows testing (#19038)
    If anyone's been wondering why we've had so few Windows failures lately,
    this is why.

--- sdk/build.ps1
+++ sdk/build.ps1
@@ -68,15 +68,13 @@ bazel build //... `
 
 bazel shutdown
 
-if ($env:SKIP_TESTS -ceq "False") {
-    # Generate mapping from shortened scala-test names on Windows to long names on Linux and MacOS.
-    ./ci/remap-scala-test-short-names.ps1 `
-      | Out-File -Encoding UTF8 -NoNewline scala-test-suite-name-map.json
+# Generate mapping from shortened scala-test names on Windows to long names on Linux and MacOS.
+./ci/remap-scala-test-short-names.ps1 `
+  | Out-File -Encoding UTF8 -NoNewline scala-test-suite-name-map.json
 
-    bazel test //... `
-      `-`-profile test-profile.json `
-      `-`-experimental_profile_include_target_label `
-      `-`-build_event_json_file test-events.json `
-      `-`-build_event_publish_all_actions `
-      `-`-experimental_execution_log_file ${ARTIFACT_DIRS}/logs/test_execution_windows.log
-}
+bazel test //... `
+  `-`-profile test-profile.json `
+  `-`-experimental_profile_include_target_label `
+  `-`-build_event_json_file test-events.json `
+  `-`-build_event_publish_all_actions `
+  `-`-experimental_execution_log_file ${ARTIFACT_DIRS}/logs/test_execution_windows.log
