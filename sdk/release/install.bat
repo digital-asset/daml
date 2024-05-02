169cdebc9d2a70e11d5b685c153cd1aeec4ba9b4
Moisés Ackerman <moises.ackerman@digitalasset.com>
2024-05-02 16:11:22 +0200

    install.bat only passes --install-with-custom-version if daml_version.txt exists

--- sdk/release/install.bat
+++ sdk/release/install.bat
@@ -1,7 +1,13 @@
 :: Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 :: SPDX-License-Identifier: Apache-2.0
 
-@SETLOCAL
-@SET /P DAML_VERSION=<daml_version.txt
-%~dp0daml\daml install %~dp0 --install-assistant=yes --install-with-custom-version=%DAML_VERSION%
+@SETLOCAL EnableDelayedExpansion
+
+@IF EXIST %~dp0daml_version.txt (
+    SET /P DAML_VERSION=<%~dp0daml_version.txt
+    SET CUSTOM_DAML_VERSION=--install-with-custom-version=!DAML_VERSION!
+)
+
+%~dp0daml\daml install %~dp0 --install-assistant=yes %CUSTOM_DAML_VERSION%
+
 @ENDLOCAL
