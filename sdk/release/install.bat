150ef21d5c24a6d63979fd7bde43cda5cea77c0e
Moisés Ackerman <moises.ackerman@digitalasset.com>
2024-05-02 14:28:26 +0200

    install.{bat,sh} pass --install-with-custom-version with contents of daml_version.txt

--- sdk/release/install.bat
+++ sdk/release/install.bat
@@ -1,5 +1,7 @@
 :: Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 :: SPDX-License-Identifier: Apache-2.0
 
-%~dp0daml\daml install %~dp0 --install-assistant=yes
-
+@SETLOCAL
+@SET /P DAML_VERSION=<daml_version.txt
+%~dp0daml\daml install %~dp0 --install-assistant=yes --install-with-custom-version=%DAML_VERSION%
+@ENDLOCAL
--- sdk/release/install.sh
+++ sdk/release/install.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-"$DIR/daml/daml" install "$DIR" $@
+"$DIR/daml/daml" install "$DIR" --install-with-custom-version=$(cat daml_version.txt) $@
