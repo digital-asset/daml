e7bef5518307a87ca4c31671b7d8fecedad7af16
Remy Haemmerle <Remy.Haemmerle@daml.com>
2024-05-02 13:52:50 +0200

    wrongly committed

--- sdk/daml-lf/encoder/src/validate.sh
+++ sdk/daml-lf/encoder/src/validate.sh
@@ -30,5 +30,4 @@ DAML_LF_REPL=$(rlocation "$TEST_WORKSPACE/$1")
 FILE=$(rlocation "$TEST_WORKSPACE/$2")
 shift 2
 
-echo $DAML_LF_REPL $* validate "$FILE" >> /tmp/titi
 $DAML_LF_REPL $* validate "$FILE"
