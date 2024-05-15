digital-asset/daml

commit 581021aaf6e47ebc51f54242165515f057a6fdfe

JSON API Upgrades: ETX-500 require fully qualified template ids (#19149)
- update all instances of ContractTypeId.OptionalPkg to ContractTypeId.RequiredPkg
- removed ContractTypeId.OptionalPkg and ContractTypeId.NoPkg type aliases entirely
- update tests to use fully qualified template ids, i.e. including a package reference
- update integration tests which do json-api queries to include a package reference in template ids

We still need to distinguish package name vs package id at the point where we resolve contract type ids, so we keep the logic that we used on missing package id, and do the same thing in the case of a package name. This logic deals with the fact that new packages can get uploaded in the background, and so we need to be able to treat a package name as a dynamically late-bound mapping to specific package.

sdk/ledger-service/http-json-testing/src/main/scala/com/daml/http/HttpServiceTestFixture.scala
@@ -66,6 +66,9 @@ object HttpServiceTestFixture extends LazyLogging with Assertions with Inside {
 
   private val doNotReloadPackages = FiniteDuration(100, DAYS)
 
+  // This may need to be updated if the Account.daml is updated.
+  val staticPkgIdAccount = "b3c9564bb7334bfd0f82099893eba518afc3b68a4d5c66cb2835498252db93e9"
+
   def withHttpService[A](
       testName: String,
       ledgerPort: Port,
@@ -416,7 +419,7 @@ object HttpServiceTestFixture extends LazyLogging with Assertions with Inside {
     postJsonStringRequest(uri, json.prettyPrint, headers)
 
   def postCreateCommand(
-      cmd: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg],
+      cmd: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg],
       encoder: DomainJsonEncoder,
       uri: Uri,
       headers: List[HttpHeader],
@@ -432,7 +435,7 @@ object HttpServiceTestFixture extends LazyLogging with Assertions with Inside {
   }
 
   def postArchiveCommand(
-      templateId: domain.ContractTypeId.OptionalPkg,
+      templateId: domain.ContractTypeId.RequiredPkg,
       contractId: domain.ContractId,
       encoder: DomainJsonEncoder,
       uri: Uri,
@@ -484,8 +487,8 @@ object HttpServiceTestFixture extends LazyLogging with Assertions with Inside {
       owner: domain.Party,
       number: String,
       time: v.Value.Sum.Timestamp = TimestampConversion.roundInstantToMicros(Instant.now),
-  ): domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg] = {
-    val templateId = domain.ContractTypeId.Template(None, "Account", "Account")
+  ): domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg] = {
+    val templateId = domain.ContractTypeId.Template(staticPkgIdAccount, "Account", "Account")
     val timeValue = v.Value(time)
     val enabledVariantValue =
       v.Value(v.Value.Sum.Variant(v.Variant(None, "Enabled", Some(timeValue))))
@@ -504,8 +507,8 @@ object HttpServiceTestFixture extends LazyLogging with Assertions with Inside {
       owners: Seq[domain.Party],
       number: String,
       time: v.Value.Sum.Timestamp = TimestampConversion.roundInstantToMicros(Instant.now),
-  ): domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg] = {
-    val templateId = domain.ContractTypeId.Template(None, "Account", "SharedAccount")
+  ): domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg] = {
+    val templateId = domain.ContractTypeId.Template(staticPkgIdAccount, "Account", "SharedAccount")
     val timeValue = v.Value(time)
     val ownersEnc = v.Value(
       v.Value.Sum.List(v.List(domain.Party.unsubst(owners).map(o => v.Value(v.Value.Sum.Party(o)))))

sdk/ledger-service/http-json/src/itlib/scala/http/AbstractHttpServiceIntegrationTestFuns.scala
@@ -65,6 +65,28 @@ object AbstractHttpServiceIntegrationTestFuns {
   private[http] val fooV1Dar = requiredResource("ledger-service/http-json/FooV1.dar")
   private[http] val fooV2Dar = requiredResource("ledger-service/http-json/FooV2.dar")
 
+  private[this] def packageIdOfDar(darFile: java.io.File): String = {
+    import com.daml.lf.{archive, typesig}
+    val dar = archive.UniversalArchiveReader.assertReadFile(darFile)
+    typesig.PackageSignature.read(dar.main)._2.packageId
+  }
+
+  val pkgIdAccount = {
+    val pkgId = packageIdOfDar(dar2)
+    assert(
+      pkgId == HttpServiceTestFixture.staticPkgIdAccount,
+      s"""Please update HttpServiceTestFixture.staticPkgIdAccount to "$pkgId"""",
+    )
+    pkgId
+  }
+
+  lazy val pkgIdCiou = packageIdOfDar(ciouDar)
+  lazy val pkgIdModelTests = packageIdOfDar(dar1)
+  lazy val pkgIdRiou = packageIdOfDar(riouDar)
+  lazy val pkgIdUser = packageIdOfDar(userDar)
+  lazy val pkgIdFooV1 = packageIdOfDar(fooV1Dar)
+  lazy val pkgIdFooV2 = packageIdOfDar(fooV2Dar)
+
   def sha256(source: Source[ByteString, Any])(implicit mat: Materializer): Try[String] = Try {
     import com.google.common.io.BaseEncoding
 
@@ -333,7 +355,7 @@ trait AbstractHttpServiceIntegrationTestFuns
   }
 
   protected def postCreateCommand(
-      cmd: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg],
+      cmd: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg],
       fixture: UriFixture with EncoderFixture,
       headers: List[HttpHeader],
   ): Future[domain.SyncResponse[domain.ActiveContract.ResolvedCtTyId[JsValue]]] =
@@ -342,7 +364,7 @@ trait AbstractHttpServiceIntegrationTestFuns
       .parseResponse[domain.ActiveContract.ResolvedCtTyId[JsValue]]
 
   protected def postCreateCommand(
-      cmd: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg],
+      cmd: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg],
       fixture: UriFixture with EncoderFixture,
   ): Future[domain.SyncResponse[domain.ActiveContract.ResolvedCtTyId[JsValue]]] =
     fixture.headersWithAuth.flatMap(postCreateCommand(cmd, fixture, _))
@@ -355,7 +377,7 @@ trait AbstractHttpServiceIntegrationTestFuns
     }
 
   protected def postArchiveCommand(
-      templateId: domain.ContractTypeId.OptionalPkg,
+      templateId: domain.ContractTypeId.RequiredPkg,
       contractId: domain.ContractId,
       fixture: UriFixture with EncoderFixture,
       headers: List[HttpHeader],
@@ -369,7 +391,7 @@ trait AbstractHttpServiceIntegrationTestFuns
     )
 
   protected def postArchiveCommand(
-      templateId: domain.ContractTypeId.OptionalPkg,
+      templateId: domain.ContractTypeId.RequiredPkg,
       contractId: domain.ContractId,
       fixture: UriFixture with EncoderFixture,
   ): Future[(StatusCode, JsValue)] =
@@ -380,7 +402,7 @@ trait AbstractHttpServiceIntegrationTestFuns
   protected def lookupContractAndAssert(
       contractLocator: domain.ContractLocator[JsValue],
       contractId: ContractId,
-      create: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg],
+      create: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg],
       fixture: UriFixture with EncoderFixture,
       headers: List[HttpHeader],
   ): Future[Assertion] =
@@ -398,11 +420,6 @@ trait AbstractHttpServiceIntegrationTestFuns
   }
 
   protected def removeRecordId(a: v.Record): v.Record = a.copy(recordId = None)
-
-  protected def removePackageId(
-      tmplId: domain.ContractTypeId.RequiredPkg
-  ): domain.ContractTypeId.OptionalPkg =
-    tmplId.copy(packageId = None)
 
   import com.daml.lf.data.{Numeric => LfNumeric}
   import shapeless.HList
@@ -439,47 +456,59 @@ trait AbstractHttpServiceIntegrationTestFuns
       amount = VA.numeric(Scale assertFromInt 10),
       observers = VA.list(VAx.partyDomain),
     )
-    VA.record(Ref.Identifier assertFromString "none:Iou:Iou", iouT)
+    VA.record(Ref.Identifier assertFromString "ignored:Iou:Iou", iouT)
   }
 
   protected[this] object TpId {
     import domain.{ContractTypeId => CtId}
-    import CtId.Template.{OptionalPkg => TId}
-    import CtId.Interface.{OptionalPkg => IId}
+    import CtId.Template.{RequiredPkg => TId}
+    import CtId.Interface.{RequiredPkg => IId}
 
     object Iou {
-      val Iou: TId = CtId.Template(None, "Iou", "Iou")
-      val IouTransfer: TId = CtId.Template(None, "Iou", "IouTransfer")
+      val Dummy: TId = CtId.Template(pkgIdModelTests, "Iou", "Dummy")
+      val Iou: TId = CtId.Template(pkgIdModelTests, "Iou", "Iou")
+      val IouTransfer: TId = CtId.Template(pkgIdModelTests, "Iou", "IouTransfer")
     }
     object Test {
-      val MultiPartyContract: TId = CtId.Template(None, "Test", "MultiPartyContract")
+      val MultiPartyContract: TId = CtId.Template(pkgIdModelTests, "Test", "MultiPartyContract")
     }
     object IAccount {
-      val IAccount: IId = CtId.Interface(None, "IAccount", "IAccount")
+      val IAccount: IId = CtId.Interface(pkgIdAccount, "IAccount", "IAccount")
     }
     object Account {
-      val Account: TId = CtId.Template(None, "Account", "Account")
-      val PubSub: TId = CtId.Template(None, "Account", "PubSub")
-      val KeyedByVariantAndRecord: TId = CtId.Template(None, "Account", "KeyedByVariantAndRecord")
-      val LongFieldNames: TId = CtId.Template(None, "Account", "LongFieldNames")
+      val Account: TId = CtId.Template(pkgIdAccount, "Account", "Account")
+      val Helper: TId = CtId.Template(pkgIdAccount, "Account", "Helper")
+      val KeyedByDecimal: TId = CtId.Template(pkgIdAccount, "Account", "KeyedByDecimal")
+      val KeyedByVariantAndRecord: TId =
+        CtId.Template(pkgIdAccount, "Account", "KeyedByVariantAndRecord")
+      val LongFieldNames: TId = CtId.Template(pkgIdAccount, "Account", "LongFieldNames")
+      val PubSub: TId = CtId.Template(pkgIdAccount, "Account", "PubSub")
+      val SharedAccount: TId = CtId.Template(pkgIdAccount, "Account", "SharedAccount")
     }
     object Disclosure {
-      val AnotherToDisclose: TId = CtId.Template(None, "Disclosure", "AnotherToDisclose")
-      val ToDisclose: TId = CtId.Template(None, "Disclosure", "ToDisclose")
-      val HasGarbage: IId = CtId.Interface(None, "Disclosure", "HasGarbage")
-      val Viewport: TId = CtId.Template(None, "Disclosure", "Viewport")
+      val AnotherToDisclose: TId = CtId.Template(pkgIdAccount, "Disclosure", "AnotherToDisclose")
+      val HasGarbage: IId = CtId.Interface(pkgIdAccount, "Disclosure", "HasGarbage")
+      val ToDisclose: TId = CtId.Template(pkgIdAccount, "Disclosure", "ToDisclose")
+      val Viewport: TId = CtId.Template(pkgIdAccount, "Disclosure", "Viewport")
     }
     object User {
-      val User: TId = CtId.Template(None, "User", "User")
+      val User: TId = CtId.Template(pkgIdUser, "User", "User")
+    }
+    object CIou {
+      val CIou: TId = CtId.Template(pkgIdCiou, "CIou", "CIou")
     }
     object IIou {
-      val IIou: IId = CtId.Interface(None, "IIou", "IIou")
+      val IIou: IId = CtId.Interface(pkgIdCiou, "IIou", "IIou")
+      val TestIIou: TId = CtId.Template(pkgIdCiou, "IIou", "TestIIou")
     }
     object RIou {
-      val RIou: IId = CtId.Interface(None, "RIou", "RIou")
+      val RIou: IId = CtId.Interface(pkgIdRiou, "RIou", "RIou")
     }
     object RIIou {
-      val RIIou: IId = CtId.Interface(None, "RIIou", "RIIou")
+      val RIIou: IId = CtId.Interface(pkgIdCiou, "RIIou", "RIIou")
+    }
+    object Transferrable {
+      val Transferrable: IId = CtId.Interface(pkgIdCiou, "Transferrable", "Transferrable")
     }
 
     def unsafeCoerce[Like[T] <: CtId[T], T](ctId: CtId[T])(implicit
@@ -494,7 +523,7 @@ trait AbstractHttpServiceIntegrationTestFuns
       currency: String = "USD",
       observers: Vector[domain.Party] = Vector.empty,
       meta: Option[domain.CommandMeta.NoDisclosed] = None,
-  ): domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg] = {
+  ): domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg] = {
     val arg = argToApi(iouVA)(
       ShRecord(
         issuer = party,
@@ -510,12 +539,12 @@ trait AbstractHttpServiceIntegrationTestFuns
 
   private[this] val (_, ciouVA) = {
     val iouT = ShRecord(issuer = VAx.partyDomain, owner = VAx.partyDomain, amount = VA.text)
-    VA.record(Ref.Identifier assertFromString "none:Iou:Iou", iouT)
+    VA.record(Ref.Identifier assertFromString "ignored:Iou:Iou", iouT)
   }
 
   protected def iouCommand(
       issuer: domain.Party,
-      templateId: domain.ContractTypeId.Template.OptionalPkg,
+      templateId: domain.ContractTypeId.Template.RequiredPkg,
   ) = {
     val iouT = argToApi(ciouVA)(
       ShRecord(
@@ -593,8 +622,8 @@ trait AbstractHttpServiceIntegrationTestFuns
   ): domain.CreateAndExerciseCommand[
     v.Record,
     v.Value,
-    domain.ContractTypeId.Template.OptionalPkg,
-    domain.ContractTypeId.OptionalPkg,
+    domain.ContractTypeId.Template.RequiredPkg,
+    domain.ContractTypeId.RequiredPkg,
   ] = {
     val targetParty = Ref.Party assertFromString target.unwrap
     val payload = argToApi(iouVA)(
@@ -705,7 +734,7 @@ trait AbstractHttpServiceIntegrationTestFuns
   }
 
   protected def encodeExercise(encoder: DomainJsonEncoder)(
-      exercise: domain.ExerciseCommand.OptionalPkg[v.Value, domain.ContractLocator[v.Value]]
+      exercise: domain.ExerciseCommand.RequiredPkg[v.Value, domain.ContractLocator[v.Value]]
   ): JsValue =
     encoder.encodeExerciseCommand(exercise).getOrElse(fail(s"Cannot encode: $exercise"))
 
@@ -741,7 +770,7 @@ trait AbstractHttpServiceIntegrationTestFuns
   protected def assertActiveContract(uri: Uri)(
       decoder: DomainJsonDecoder,
       actual: domain.ActiveContract.ResolvedCtTyId[JsValue],
-      create: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg],
+      create: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg],
       exercise: domain.ExerciseCommand[Any, v.Value, _],
       ledgerId: LedgerId,
   ): Future[Assertion] = {
@@ -777,13 +806,13 @@ trait AbstractHttpServiceIntegrationTestFuns
   protected def assertActiveContract(
       activeContract: domain.ActiveContract.ResolvedCtTyId[JsValue]
   )(
-      command: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg],
+      command: domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg],
       encoder: DomainJsonEncoder,
   ): Assertion = {
 
     import encoder.implicits._
 
-    val expected: domain.CreateCommand[JsValue, domain.ContractTypeId.Template.OptionalPkg] =
+    val expected: domain.CreateCommand[JsValue, domain.ContractTypeId.Template.RequiredPkg] =
       command
         .traversePayload(SprayJson.encode[v.Record](_))
         .getOrElse(fail(s"Failed to encode command: $command"))
@@ -797,15 +826,6 @@ trait AbstractHttpServiceIntegrationTestFuns
       jsPayload: JsValue
   ): Assertion = {
     (activeContract.payload: JsValue) shouldBe (jsPayload)
-  }
-
-  protected def assertTemplateId(
-      actual: domain.ContractTypeId.RequiredPkg,
-      expected: domain.ContractTypeId.OptionalPkg,
-  ): Future[Assertion] = Future {
-    expected.packageId.foreach(x => actual.packageId shouldBe x)
-    actual.moduleName shouldBe expected.moduleName
-    actual.entityName shouldBe expected.entityName
   }
 
   protected def getAllPackageIds(fixture: UriFixture): Future[domain.OkResponse[List[String]]] =
@@ -839,7 +859,7 @@ trait AbstractHttpServiceIntegrationTestFuns
     val payload =
       s"""
          |{
-         |  "templateId": "Iou:Iou",
+         |  "templateId": "${TpId.Iou.Iou.fqn}",
          |  "payload": {
          |    "observers": [],
          |    "issuer": $partyJson,
@@ -876,7 +896,7 @@ trait AbstractHttpServiceIntegrationTestFuns
   }
 
   protected def searchExpectOk(
-      commands: List[domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg]],
+      commands: List[domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg]],
       query: JsObject,
       fixture: UriFixture with EncoderFixture,
       headers: List[HttpHeader],
@@ -885,14 +905,14 @@ trait AbstractHttpServiceIntegrationTestFuns
   }
 
   protected def searchExpectOk(
-      commands: List[domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg]],
+      commands: List[domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg]],
       query: JsObject,
       fixture: UriFixture with EncoderFixture,
   ): Future[List[domain.ActiveContract.ResolvedCtTyId[JsValue]]] =
     fixture.headersWithAuth.flatMap(searchExpectOk(commands, query, fixture, _))
 
   protected def search(
-      commands: List[domain.CreateCommand[v.Record, domain.ContractTypeId.Template.OptionalPkg]],
+      commands: List[domain.CreateCommand[v.Record, domain.ContractTypeId.Template.RequiredPkg]],
       query: JsObject,
       fixture: UriFixture with EncoderFixture,
       headers: List[HttpHeader],
@@ -987,7 +1007,7 @@ trait AbstractHttpServiceIntegrationTestFuns
       _ = kill.shutdown()
     } yield (betweenOffset, afterOffset)
 
-    val query = """[{"templateIds": ["Iou:Iou"]}]"""
+    val query = s"""[{"templateIds": ["${TpId.Iou.Iou.fqn}"]}]"""
     for {
       jwt <- jwtForParties(uri)(List(party), List(), "participant0")
       (kill, source) =
@@ -1000,7 +1020,7 @@ trait AbstractHttpServiceIntegrationTestFuns
 
   protected def assertExerciseResponseArchivedContract(
       exerciseResponse: domain.ExerciseResponse[JsValue],
-      exercise: domain.ExerciseCommand.OptionalPkg[v.Value, domain.EnrichedContractId],
+      exercise: domain.ExerciseCommand.RequiredPkg[v.Value, domain.EnrichedContractId],
   ): Assertion =
     inside(exerciseResponse) { case domain.ExerciseResponse(exerciseResult, List(contract1), _) =>
       exerciseResult shouldBe JsObject()

sdk/daml-assistant/integration-tests/src/DA/Daml/Assistant/IntegrationTests.hs
@@ -88,6 +88,7 @@ withDamlServiceIn path command args act = withDevNull $ \devNull -> do
 data DamlStartResource = DamlStartResource
     { projDir :: FilePath
     , tmpDir :: FilePath
+    , packageRef :: String
     , alice :: String
     , aliceHeaders :: RequestHeaders
     , startStdin :: Handle
@@ -161,10 +162,13 @@ damlStart tmpDir _disableUpgradeValidation = do
         (authorizationHeaders "Alice") -- dummy party here, not important
     scriptOutput <- readFileUTF8 (projDir </> scriptOutputFile)
     let alice = (read scriptOutput :: String)
+    -- TODO(raphael-speyer-da): Use a package name once supported by canton out of the box.
+    let packageRef = "6310b9aa3d506211b592dd657afb167d63637453f31eae986fad5aa1b6d61401"
     pure $
         DamlStartResource
             { projDir = projDir
             , tmpDir = tmpDir
+            , packageRef = packageRef
             , sandboxPort = ledger ports
             , jsonApiPort = jsonApiPort
             , startStdin = startStdin
@@ -334,7 +338,7 @@ damlStartTests getDamlStart =
         let subtest :: forall t. String -> IO t -> IO t
             subtest m p = step m >> p
         subtest "sandbox and json-api come up" $ do
-            DamlStartResource {jsonApiPort, alice, aliceHeaders} <- getDamlStart
+            DamlStartResource {jsonApiPort, alice, aliceHeaders, packageRef} <- getDamlStart
             manager <- newManager defaultManagerSettings
             initialRequest <-
                 parseRequest $ "http://localhost:" <> show jsonApiPort <> "/v1/create"
@@ -346,7 +350,7 @@ damlStartTests getDamlStart =
                             RequestBodyLBS $
                             Aeson.encode $
                             Aeson.object
-                                [ "templateId" Aeson..= Aeson.String "Main:T"
+                                [ "templateId" Aeson..= (packageRef ++ ":Main:T")
                                 , "payload" Aeson..= [alice]
                                 ]
                         }
@@ -363,7 +367,7 @@ damlStartTests getDamlStart =
             callCommandSilentIn projDir $ unwords
                 ["daml", "ledger", "allocate-party", "--port", show sandboxPort, "Bob"]
         subtest "Run init-script" $ do
-            DamlStartResource {jsonApiPort, aliceHeaders} <- getDamlStart
+            DamlStartResource {jsonApiPort, aliceHeaders, packageRef} <- getDamlStart
             initialRequest <- parseRequest $ "http://localhost:" <> show jsonApiPort <> "/v1/query"
             let queryRequest = initialRequest
                     { method = "POST"
@@ -371,7 +375,7 @@ damlStartTests getDamlStart =
                     , requestBody =
                         RequestBodyLBS $
                         Aeson.encode $
-                        Aeson.object ["templateIds" Aeson..= [Aeson.String "Main:T"]]
+                        Aeson.object ["templateIds" Aeson..= [packageRef ++ ":Main:T"]]
                     }
             manager <- newManager defaultManagerSettings
             queryResponse <- httpLbs queryRequest manager
@@ -458,7 +462,7 @@ damlStartTestsWithoutValidation :: SdkVersioned => IO DamlStartResource -> TestT
 damlStartTestsWithoutValidation getDamlStart =
     -- We use testCaseSteps to make sure each of these tests runs in sequence, not in parallel.
     testCase "daml start without upgrade validation - hot reload" $ do
-        DamlStartResource {projDir, jsonApiPort, startStdin, stdoutChan, alice, aliceHeaders} <- getDamlStart
+        DamlStartResource {projDir, jsonApiPort, startStdin, stdoutChan, alice, aliceHeaders, packageRef} <- getDamlStart
         stdoutReadChan <- atomically $ dupTChan stdoutChan
         writeFileUTF8 (projDir </> "daml/Main.daml") $
             unlines
@@ -478,6 +482,7 @@ damlStartTestsWithoutValidation getDamlStart =
         untilM_ (pure ()) $ do
             line <- atomically $ readTChan stdoutReadChan
             pure ("Rebuild complete" `isInfixOf` line)
+        let newPackageRef = "8caa3f3b63b66d0338e0bb8b931c5c57644a4362b3bd82c482bd37d7438abcc8"
         initialRequest <-
             parseRequest $ "http://localhost:" <> show jsonApiPort <> "/v1/query"
         manager <- newManager defaultManagerSettings
@@ -488,7 +493,7 @@ damlStartTestsWithoutValidation getDamlStart =
                     , requestBody =
                         RequestBodyLBS $
                         Aeson.encode $
-                        Aeson.object ["templateIds" Aeson..= [Aeson.String "Main:T"]]
+                        Aeson.object ["templateIds" Aeson..= [packageRef ++ ":Main:T"]]
                     }
         let queryRequestS =
                 initialRequest
@@ -497,7 +502,7 @@ damlStartTestsWithoutValidation getDamlStart =
                     , requestBody =
                         RequestBodyLBS $
                         Aeson.encode $
-                        Aeson.object ["templateIds" Aeson..= [Aeson.String "Main:S"]]
+                        Aeson.object ["templateIds" Aeson..= [newPackageRef ++ ":Main:S"]]
                     }
         queryResponseT <- httpLbs queryRequestT manager
         queryResponseS <- httpLbs queryRequestS manager

sdk/language-support/ts/codegen/tests/BUILD.bazel
@@ -23,6 +23,7 @@ daml_compile(
     name = "hidden",
     srcs = ["daml/Hidden.daml"],
     data_dependencies = ["//language-support/ts/codegen/tests:build-and-lint.dar"],
+    project_name = "hidden",
     target = "1.dev",
 )
 

sdk/test-evidence/BUILD.bazel
@@ -3,6 +3,7 @@
 
 load("@os_info//:os_info.bzl", "is_linux_intel")
 load("//bazel_tools:scala.bzl", "da_scala_binary", "da_scala_library")
+load("//daml-lf/language:daml-lf.bzl", "lf_version_configuration")
 load(":test_evidence.bzl", "test_evidence_binary", "write_scalatest_runpath")
 
 write_scalatest_runpath(
@@ -37,6 +38,9 @@ write_scalatest_runpath(
     test_evidence_binary(
         name = "generator",
         testonly = True,  # needed in order to depend on testonly targets
+        data = [
+            "//ledger-service/http-json:integration-tests-ce",  # For the dars files in data deps
+        ],
         main_class = "com.daml.test.evidence.generator.Main",
         tests = [
             "//ledger-service/http-json:failure-tests",

sdk/language-support/ts/codegen/tests/ts/build-and-lint-test/src/__tests__/test.ts
@@ -917,7 +917,7 @@ describe("interfaces", () => {
     // meaningful we *must not* codegen or load Hidden
     type NotVisibleInTs = { owner: Party };
     const NotVisibleInTs: Pick<Template<{ owner: Party }>, "templateId"> = {
-      templateId: "Hidden:NotVisibleInTs",
+      templateId: "#hidden:Hidden:NotVisibleInTs",
     };
     const initialPayload: NotVisibleInTs = { owner: ALICE_PARTY };
 

sdk/ledger-service/fetch-contracts/src/main/scala/fetchcontracts/domain/ContractTypeId.scala
@@ -61,6 +61,8 @@ sealed abstract class ContractTypeId[+PkgId]
     import scala.util.hashing.{MurmurHash3 => H}
     H.productHash(this, H.productSeed, ignorePrefix = true)
   }
+
+  def fqn: String = s"${packageId.toString}:${moduleName}:${entityName}"
 }
 
 object ResolvedQuery {
@@ -240,9 +242,7 @@ object ContractTypeId extends ContractTypeIdLike[ContractTypeId] {
 
 /** A contract type ID companion. */
 sealed abstract class ContractTypeIdLike[CtId[T] <: ContractTypeId[T]] {
-  type OptionalPkg = CtId[Option[String]]
   type RequiredPkg = CtId[String]
-  type NoPkg = CtId[Unit]
   type Resolved = ContractTypeId.ResolvedOf[CtId]
 
   // treat the companion like a typeclass instance

commit 8e4e0378786f796e647a41ba73348783b110ab75

[2.9] restore ability to make releases
Releases were broken by [#19149] because package IDs include the SDK
version in what they hash.

[#19149]: https://github.com/digital-asset/daml/pull/19149

sdk/ledger-service/http-json-testing/BUILD.bazel
@@ -39,6 +39,9 @@ load("//ledger-service/utils:scalaopts.bzl", "hj_scalacopts")
         ],
         deps = [
             "//bazel_tools/runfiles:scala_runfiles",
+            "//daml-lf/api-type-signature",
+            "//daml-lf/archive:daml_lf_archive_reader",
+            "//daml-lf/archive:daml_lf_dev_archive_proto_java",
             "//daml-lf/transaction",
             "//language-support/scala/bindings-pekko",
             "//ledger-service/fetch-contracts",

sdk/ledger-service/http-json-testing/src/main/scala/com/daml/http/HttpServiceTestFixture.scala
@@ -11,6 +11,7 @@ import org.apache.pekko.http.scaladsl.model.headers.{Authorization, OAuth2Bearer
 import org.apache.pekko.stream.Materializer
 import com.daml.api.util.TimestampConversion
 import com.daml.bazeltools.BazelRunfiles.rlocation
+import com.daml.bazeltools.BazelRunfiles.requiredResource
 import com.daml.grpc.adapter.ExecutionSequencerFactory
 import com.daml.http.HttpService.doLoad
 import com.daml.http.dbbackend.{ContractDao, JdbcConfig}
@@ -66,8 +67,13 @@ object HttpServiceTestFixture extends LazyLogging with Assertions with Inside {
 
   private val doNotReloadPackages = FiniteDuration(100, DAYS)
 
-  // This may need to be updated if the Account.daml is updated.
-  val staticPkgIdAccount = "b3c9564bb7334bfd0f82099893eba518afc3b68a4d5c66cb2835498252db93e9"
+  // TODO(paulbrauner-da): Use a package name once supported by canton out of the box.
+  lazy val staticPkgIdAccount = {
+    import com.daml.lf.{archive, typesig}
+    val darFile = requiredResource("ledger-service/http-json/Account.dar")
+    val dar = archive.UniversalArchiveReader.assertReadFile(darFile)
+    typesig.PackageSignature.read(dar.main)._2.packageId
+  }
 
   def withHttpService[A](
       testName: String,

sdk/ledger-service/http-json/src/itlib/scala/http/AbstractHttpServiceIntegrationTestFuns.scala
@@ -71,21 +71,13 @@ object AbstractHttpServiceIntegrationTestFuns {
     typesig.PackageSignature.read(dar.main)._2.packageId
   }
 
-  val pkgIdAccount = {
-    val pkgId = packageIdOfDar(dar2)
-    assert(
-      pkgId == HttpServiceTestFixture.staticPkgIdAccount,
-      s"""Please update HttpServiceTestFixture.staticPkgIdAccount to "$pkgId"""",
-    )
-    pkgId
-  }
-
   lazy val pkgIdCiou = packageIdOfDar(ciouDar)
   lazy val pkgIdModelTests = packageIdOfDar(dar1)
   lazy val pkgIdRiou = packageIdOfDar(riouDar)
   lazy val pkgIdUser = packageIdOfDar(userDar)
   lazy val pkgIdFooV1 = packageIdOfDar(fooV1Dar)
   lazy val pkgIdFooV2 = packageIdOfDar(fooV2Dar)
+  lazy val pkgIdAccount = packageIdOfDar(dar2)
 
   def sha256(source: Source[ByteString, Any])(implicit mat: Materializer): Try[String] = Try {
     import com.google.common.io.BaseEncoding

sdk/daml-assistant/integration-tests/src/DA/Daml/Assistant/IntegrationTests.hs
@@ -496,6 +496,7 @@ damlStartTestsWithoutValidation getDamlStart =
         untilM_ (pure ()) $ do
             line <- atomically $ readTChan stdoutReadChan
             pure ("Rebuild complete" `isInfixOf` line)
+        -- TODO(paulbrauner-da): Use a package name once supported by canton out of the box.
         newPackageRef <- extractPackageRefFromDar projDir
         initialRequest <-
             parseRequest $ "http://localhost:" <> show jsonApiPort <> "/v1/query"