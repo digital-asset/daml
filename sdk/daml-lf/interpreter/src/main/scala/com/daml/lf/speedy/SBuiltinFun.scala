18c9dce94102d149e6b1365021596a5c54cb6a84
Remy <remy.haemmerle@daml.com>
2024-05-16 09:16:16 +0200

    [Speedy] recompute contract metadata when using different pkg versions  (#19206)
    This is the last piece to fix #19161
    
    Co-authored-by: Paul Brauner <141240651+paulbrauner-da@users.noreply.github.com>

--- sdk/daml-lf/interpreter/src/main/scala/com/daml/lf/speedy/SBuiltinFun.scala
+++ sdk/daml-lf/interpreter/src/main/scala/com/daml/lf/speedy/SBuiltinFun.scala
@@ -2293,8 +2293,10 @@ private[lf] object SBuiltinFun {
       templateArg: SValue,
       keyOpt: SValue,
   )(f: ContractInfo => Control[Question.Update]): Control[Question.Update] = {
-    machine.lookupContractInfoCache(coid) match {
+    machine.contractInfoCache.get((coid, templateId.packageId)) match {
       case Some(contract) =>
+        // sanity check
+        assert(contract.templateId == templateId)
         f(contract)
       case None =>
         computeContractInfo(machine, templateId, templateArg, keyOpt) { contract =>
