7ffc096fa857700fe510c41f299c7ce06f5bbb4f
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2024-05-16 17:45:00 +0200

    [3.1] nix: remove unused xmlstarlet (#19219)

--- sdk/nix/default.nix
+++ sdk/nix/default.nix
@@ -177,7 +177,6 @@ in rec {
     sha1sum = pkgs.coreutils;
     sha256sum = pkgs.coreutils;
     xargs = pkgs.findutils;
-    xmlstarlet = pkgs.xmlstarlet;
 
     # Cryptography tooling
     gnupg = pkgs.gnupg;
