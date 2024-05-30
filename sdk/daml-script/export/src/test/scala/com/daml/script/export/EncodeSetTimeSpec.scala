d55280574e3e03b869e9f255cba321f320144933
Carl Pulley <carl.pulley@digitalasset.com>
2024-05-30 09:37:55 +0100

    Another CI test fix

--- sdk/daml-script/export/src/test/scala/com/daml/script/export/EncodeSetTimeSpec.scala
+++ sdk/daml-script/export/src/test/scala/com/daml/script/export/EncodeSetTimeSpec.scala
@@ -7,10 +7,13 @@ import com.daml.lf.data.Time.Timestamp
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.time.Instant
+
 class EncodeSetTimeSpec extends AnyFreeSpec with Matchers {
   import Encode._
   "encodeSetTime" in {
-    encodeSetTime(Timestamp.assertFromString("1990-11-09T04:30:23.123456Z")).render(80) shouldBe
+    encodeSetTime(Timestamp.assertFromInstant(Instant.parse("1990-11-09T04:30:23.123456Z")))
+      .render(80) shouldBe
       """setTime (DA.Time.time (DA.Date.date 1990 DA.Date.Nov 9) 4 30 23)"""
   }
 }
