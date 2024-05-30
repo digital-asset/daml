4a44b327218b6b36551aace89ce9bd59600c06f4
Samuel Williams <samuel.williams@digitalasset.com>
2024-05-30 11:55:13 +0100

    Tweaks

--- sdk/daml-script/runner/src/test-lib/scala/com/daml/lf/engine/script/RunnerTestBase.scala
+++ sdk/daml-script/runner/src/test-lib/scala/com/daml/lf/engine/script/RunnerTestBase.scala
@@ -13,23 +13,6 @@ import scala.sys.process._
 
 trait RunnerTestBase {
   self: Suite =>
-  // protected val jwt: Path =
-  //   BazelRunfiles.rlocation(Paths.get("daml-script/runner/src/test/resources/json-access.jwt"))
-  // protected val inputFile: String =
-  //   BazelRunfiles.rlocation("daml-script/runner/src/test/resources/input.json")
-
-  // // Defines the size of `dars`
-  // // Should always match test_dar_count in the BUILD file
-  // val DAR_COUNT = 5
-
-  // // We use a different DAR for each test that asserts upload behaviour to avoid clashes
-  // val dars: Seq[Path] = (1 to DAR_COUNT).map(n =>
-  //   BazelRunfiles.rlocation(Paths.get(s"daml-script/runner/test-script$n.dar"))
-  // )
-
-  // // DAR containing failingScript and successfulScript
-  // val failingDar: Path =
-  //   BazelRunfiles.rlocation(Paths.get("daml-script/runner/failing-test-script.dar"))
 
   private implicit val ec: ExecutionContext = ExecutionContext.global
 
