ca2d8c1a825ed58f1df414c87a39195eb120f12a
Remy Haemmerle <Remy.Haemmerle@daml.com>
2024-06-17 15:48:12 +0200

    [LF] make LF 1.16 preview
    this partially revert #19230

--- sdk/daml-lf/language/src/main/scala/com/digitalasset/daml/lf/language/LanguageVersion.scala
+++ sdk/daml-lf/language/src/main/scala/com/digitalasset/daml/lf/language/LanguageVersion.scala
@@ -94,7 +94,7 @@ object LanguageVersion {
 
   // All the stable versions.
   val StableVersions: VersionRange[LanguageVersion] =
-    VersionRange(min = v1_6, max = v1_16)
+    VersionRange(min = v1_6, max = v1_15)
 
   // All versions compatible with legacy contract ID scheme.
   val LegacyVersions: VersionRange[LanguageVersion] =
@@ -103,7 +103,7 @@ object LanguageVersion {
   // All the stable and preview versions
   // Equals `Stable` if no preview version is available
   val EarlyAccessVersions: VersionRange[LanguageVersion] =
-    StableVersions
+    StableVersions.copy(max = v1_16)
 
   // All the versions
   def AllVersions(majorLanguageVersion: LanguageMajorVersion): VersionRange[LanguageVersion] = {
