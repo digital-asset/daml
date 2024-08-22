8159762bb52bb3edb71a3a83d6a277504ba7f668
Gerolf Seitz <gerolf.seitz@daml.com>
2024-08-22 11:48:00 +0200

    fix compile error

--- sdk/canton/community/base/src/main/scala/com/digitalasset/canton/data/ActionDescription.scala
+++ sdk/canton/community/base/src/main/scala/com/digitalasset/canton/data/ActionDescription.scala
@@ -203,6 +203,7 @@ object ActionDescription extends HasProtocolVersionedCompanion[ActionDescription
             _key,
             byKey,
             version,
+            _isInterface,
           ) =>
         for {
           _ <- Either.cond(
