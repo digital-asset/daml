e7077d92e6e633cd5ccc36ef695241b26a05c3db
= <=>
2024-08-27 17:26:04 +0200

    never use package names to address interfaces

--- sdk/language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/TemplateClass.scala
+++ sdk/language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/backend/java/inner/TemplateClass.scala
@@ -503,7 +503,7 @@ private[inner] object TemplateClass extends StrictLogging {
   private def generateTemplateIdFields(typeWithContext: TypeWithContext): Seq[FieldSpec] =
     ClassGenUtils.generateTemplateIdFields(
       typeWithContext.packageId,
-      typeWithContext.interface.metadata.name,
+      None, // TODO(raphael-speyer-da): Some(typeWithContext.interface.metadata.name)
       typeWithContext.modulesLineage.map(_._1).toImmArray.iterator.mkString("."),
       typeWithContext.name,
     )
