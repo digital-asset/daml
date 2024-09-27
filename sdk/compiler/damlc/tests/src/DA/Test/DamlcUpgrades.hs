421a1a6c48f7d1ecbc82f0df3df1fd3b60570ecb
dylant-da <106664681+dylant-da@users.noreply.github.com>
2024-09-27 11:44:15 +0100

    Check for alpha equivalence in data types (#20016)

--- sdk/compiler/damlc/tests/src/DA/Test/DamlcUpgrades.hs
+++ sdk/compiler/damlc/tests/src/DA/Test/DamlcUpgrades.hs
@@ -618,6 +618,27 @@ tests damlc =
                   "my-package"
                   "0.0.2"
                   version1_dev
+            , test
+                  "FailWhenParamCountChanges"
+                  (FailWithError "\ESC\\[0;91merror type checking data type Main.MyStruct:\n  EUpgradeDifferentParamsCount")
+                  versionDefault
+                  NoDependencies
+                  False
+                  True
+            , test
+                  "FailWhenParamKindChanges"
+                  (FailWithError "\ESC\\[0;91merror type checking data type Main.MyStruct:\n  EUpgradeDifferentParamsKinds")
+                  versionDefault
+                  NoDependencies
+                  False
+                  True
+            , test
+                  "SucceedWhenParamNameChanges"
+                  Succeed
+                  versionDefault
+                  NoDependencies
+                  False
+                  True
             ]
        )
   where
