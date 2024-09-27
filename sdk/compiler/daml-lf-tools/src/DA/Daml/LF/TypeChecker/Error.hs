e6207ce4211c8ef6bea5ed6ec21f0e2b69348cd2
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2024-09-27 11:49:03 +0100

    Add two tests for Phantom type, fix error messages

--- sdk/compiler/daml-lf-tools/src/DA/Daml/LF/TypeChecker/Error.hs
+++ sdk/compiler/daml-lf-tools/src/DA/Daml/LF/TypeChecker/Error.hs
@@ -689,8 +689,8 @@ instance Pretty UnwarnableError where
       where
       pprintDep (pkgId, meta) = pPrint pkgId <> "(" <> pPrint (packageName meta) <> ", " <> pPrint (packageVersion meta) <> ")"
     EUpgradeMultiplePackagesWithSameNameAndVersion name version ids -> "Multiple packages with name " <> pPrint name <> " and version " <> pPrint (show version) <> ": " <> hcat (L.intersperse ", " (map pPrint ids))
-    EUpgradeDifferentParamsCount origin -> "EUpgradeDifferentParamsCount " <> pPrint origin
-    EUpgradeDifferentParamsKinds origin -> "EUpgradeDifferentParamsKinds " <> pPrint origin
+    EUpgradeDifferentParamsCount origin -> "The upgraded " <> pPrint origin <> " has changed the number of type variables it has."
+    EUpgradeDifferentParamsKinds origin -> "The upgraded " <> pPrint origin <> " has changed the kind of one of its type variables."
 
 instance Pretty UpgradedRecordOrigin where
   pPrint = \case
--- sdk/compiler/damlc/tests/src/DA/Test/DamlcUpgrades.hs
+++ sdk/compiler/damlc/tests/src/DA/Test/DamlcUpgrades.hs
@@ -595,20 +595,27 @@ tests damlc =
                   LF.version2_dev
             , test
                   "FailWhenParamCountChanges"
-                  (FailWithError "\ESC\\[0;91merror type checking data type Main.MyStruct:\n  EUpgradeDifferentParamsCount")
+                  (FailWithError "\ESC\\[0;91merror type checking data type Main.MyStruct:\n  The upgraded data type MyStruct has changed the number of type variables it has.")
                   versionDefault
                   NoDependencies
                   False
                   True
             , test
                   "FailWhenParamKindChanges"
-                  (FailWithError "\ESC\\[0;91merror type checking data type Main.MyStruct:\n  EUpgradeDifferentParamsKinds")
+                  (FailWithError "\ESC\\[0;91merror type checking data type Main.MyStruct:\n  The upgraded data type MyStruct has changed the kind of one of its type variables.")
                   versionDefault
                   NoDependencies
                   False
                   True
             , test
                   "SucceedWhenParamNameChanges"
+                  Succeed
+                  versionDefault
+                  NoDependencies
+                  False
+                  True
+            , test
+                  "SucceedWhenPhantomParamBecomesUsed"
                   Succeed
                   versionDefault
                   NoDependencies
