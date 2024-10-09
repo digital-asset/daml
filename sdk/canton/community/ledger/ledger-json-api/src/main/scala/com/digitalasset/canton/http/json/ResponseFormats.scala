bb309a78e269d9447714a0998f8dff0a67f35882
Remy Haemmerle <Remy.Haemmerle@daml.com>
2024-10-09 17:56:38 +0200

    fix

--- sdk/canton/community/ledger/ledger-json-api/src/main/scala/com/digitalasset/canton/http/json/ResponseFormats.scala
+++ sdk/canton/community/ledger/ledger-json-api/src/main/scala/com/digitalasset/canton/http/json/ResponseFormats.scala
@@ -50,7 +50,9 @@ import spray.json._
       jsVals ~> partition.in
 
       // second consume all successes
-      partition.out1.zipWithIndex.map(a => formatOneElement(a._1, a._2)) ~> concat.in(1)
+      partition.out1
+        .zip(Source.fromIterator(() => Iterator.iterate(0L)(_ + 1)))
+        .map(a => formatOneElement(a._1, a._2)) ~> concat.in(1)
 
       // then consume all failures and produce the status and optional errors
       partition.out0.fold(Vector.empty[E])((b, a) => b :+ a).map {
