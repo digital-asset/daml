0dc30f8b2d6e58036efee6b38ae63a1c5515ee4d
Remy <remy.haemmerle@daml.com>
2024-10-10 22:07:15 +0200

    reapply canton change from (#20069)

--- sdk/canton/community/base/src/main/scala/com/digitalasset/canton/data/ActionDescription.scala
+++ sdk/canton/community/base/src/main/scala/com/digitalasset/canton/data/ActionDescription.scala
@@ -164,39 +164,21 @@ object ActionDescription extends HasProtocolVersionedCompanion[ActionDescription
           protocolVersionRepresentativeFor(protocolVersion)
         )
 
-      case LfNodeExercises(
-            inputContract,
-            _packageName,
-            templateId,
-            interfaceId,
-            choice,
-            _consuming,
-            actors,
-            chosenValue,
-            _stakeholders,
-            _signatories,
-            _choiceObservers,
-            _choiceAuthorizers,
-            _children,
-            exerciseResult,
-            _key,
-            byKey,
-            version,
-          ) =>
+      case exe: LfNodeExercises =>
         for {
           seed <- seedO.toRight(InvalidActionDescription("No seed for an Exercise node given"))
           actionDescription <- ExerciseActionDescription.create(
-            inputContract,
-            Option.when(protocolVersion >= ProtocolVersion.v5)(templateId),
-            choice,
-            interfaceId,
+            exe.targetCoid,
+            Option.when(protocolVersion >= ProtocolVersion.v5)(exe.templateId),
+            exe.choiceId,
+            exe.interfaceId,
             packagePreference,
-            chosenValue,
-            actors,
-            byKey,
+            exe.chosenValue,
+            exe.actingParties,
+            exe.byKey,
             seed,
-            version,
-            failed = exerciseResult.isEmpty, // absence of exercise result indicates failure
+            exe.version,
+            failed = exe.exerciseResult.isEmpty, // absence of exercise result indicates failure
             protocolVersionRepresentativeFor(protocolVersion),
           )
         } yield actionDescription
