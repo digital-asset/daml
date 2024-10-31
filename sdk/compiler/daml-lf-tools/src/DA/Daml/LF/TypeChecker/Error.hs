c68d9278641075ca737a216c6c72e9318685b615
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2024-10-31 10:32:52 +0000

    Add -Wno-error and -Wwarn

--- sdk/compiler/daml-lf-tools/src/DA/Daml/LF/TypeChecker/Error.hs
+++ sdk/compiler/daml-lf-tools/src/DA/Daml/LF/TypeChecker/Error.hs
@@ -278,7 +278,9 @@ data DamlWarningFlag
 parseDamlWarningFlag :: String -> Either String DamlWarningFlag
 parseDamlWarningFlag = \case
   ('e':'r':'r':'o':'r':'=':name) -> RawDamlWarningFlag name AsError <$> parseNameE name
+  ('n':'o':'-':'e':'r':'r':'o':'r':'=':name) -> RawDamlWarningFlag name AsWarning <$> parseNameE name
   ('n':'o':'-':name) -> RawDamlWarningFlag name Hidden <$> parseNameE name
+  ('w':'a':'r':'n':'=':name) -> RawDamlWarningFlag name AsWarning <$> parseNameE name
   name -> RawDamlWarningFlag name AsWarning <$> parseNameE name
   where
   namesToFilters = [(upgradeInterfacesName, upgradeInterfacesFilter)]
