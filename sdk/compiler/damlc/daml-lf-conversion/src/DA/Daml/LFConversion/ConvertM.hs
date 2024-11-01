ec0995ec595d8412fae6946f3ad962da8a629185
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2024-11-01 13:14:03 +0000

    Add StandaloneError/StandaloneWarning/ErrorOrWarning for LFConversion

--- sdk/compiler/damlc/daml-lf-conversion/src/DA/Daml/LFConversion/ConvertM.hs
+++ sdk/compiler/damlc/daml-lf-conversion/src/DA/Daml/LFConversion/ConvertM.hs
@@ -2,7 +2,6 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 module DA.Daml.LFConversion.ConvertM (
-    ConversionError(..),
     ConversionEnv(..),
     ConvertM(..),
     runConvertM,
@@ -11,9 +10,14 @@ module DA.Daml.LFConversion.ConvertM (
     resetFreshVarCounters,
     conversionWarning,
     conversionError,
+    conversionDiagnostic,
     unsupported,
     unknown,
-    unhandled
+    unhandled,
+    StandaloneWarning(..),
+    StandaloneError(..),
+    ErrorOrWarning(..),
+    InvalidInterfaceError(..)
   ) where
 
 import           DA.Daml.UtilLF
@@ -32,14 +36,9 @@ import qualified Data.Map.Strict as MS
 import qualified Data.Text.Extended as T
 import           "ghc-lib" GHC
 import           "ghc-lib" GhcPlugins as GHC hiding ((<>), notNull)
+import           DA.Pretty (renderPretty)
 
-data ConversionError
-  = ConversionError
-     { errorFilePath :: !NormalizedFilePath
-     , errorRange :: !(Maybe Range)
-     , errorMessage :: !String
-     }
-  deriving Show
+import "ghc-lib" TyCoRep
 
 data ConversionEnv = ConversionEnv
   { convModuleFilePath :: !NormalizedFilePath
@@ -51,11 +50,113 @@ data ConversionState = ConversionState
     , warnings :: [FileDiagnostic]
     }
 
+data ErrorOrWarning
+  = LargeTuple Int
+
+data Warning
+  = StandaloneWarning StandaloneWarning
+  | ErrorOrWarningAsWarning ErrorOrWarning
+
+data Error
+  = StandaloneError StandaloneError
+  | ErrorOrWarningAsError ErrorOrWarning
+
+data StandaloneWarning
+  = PolymorphicTopLevelScript GHC.Var GHC.Type GHC.TyVar
+
+data StandaloneError
+  = Unhandled String Constr String
+  | Unsupported String String
+  | OnlySupportedOnDev String
+  | UnknownPackage GHC.UnitId (MS.Map GHC.UnitId DalfPackage)
+  | ScenariosNoLongerSupported GHC.Var GHC.Type
+  | UnknownPrimitive String LF.Type
+  | RawError String
+  | NoViewFoundForInterface LF.TypeConName
+  | InvalidInterface GHC.TyCon GHC.TyCon InvalidInterfaceError
+  | CannotRequireNonInterface GHC.TyCon
+
+data InvalidInterfaceError
+  = NotATemplate GHC.TyCon
+  | NotAnInterface GHC.TyCon
+  | DoesNotMatchEnclosingTemplateDeclaration
+  | NoViewDefined
+  | MoreThanOneViewDefined
+
+ppError (StandaloneError err) = ppStandaloneError err
+ppError (ErrorOrWarningAsError errOrWarn) = ppErrorOrWarning errOrWarn
+
+ppWarning (StandaloneWarning warn) = ppStandaloneWarning warn
+ppWarning (ErrorOrWarningAsWarning errOrWarn) = ppErrorOrWarning errOrWarn
+
+ppStandaloneError :: StandaloneError -> String
+ppStandaloneError = \case
+  Unsupported typ x ->
+    "Failure to process Daml program, this feature is not currently supported.\n" ++
+    typ ++ "\n" ++
+    prettyPrint x
+  Unhandled typ constr x ->
+    "Failure to process Daml program, this feature is not currently supported.\n" ++
+    (typ ++ " with " ++ lower (show constr)) ++ "\n" ++
+    x
+  OnlySupportedOnDev feature ->
+    feature <> " only available with --target=1.dev"
+  UnknownPackage unitId pkgMap ->
+    "Unknown package: " ++ GHC.unitIdString unitId
+    ++ "\n" ++  "Loaded packages are:" ++ prettyPrint (MS.keys pkgMap)
+  ScenariosNoLongerSupported name ty ->
+    unlines
+      [ "Scenarios are no longer supported."
+      , "Instead, consider using Daml Script (https://docs.daml.com/daml-script/index.html)."
+      , "When compiling " <> prettyPrint name <> " : " <> prettyPrint ty <> "."
+      ]
+  UnknownPrimitive x ty ->
+    "Unknown primitive " ++ show x ++ " at type " ++ renderPretty ty
+  RawError msg -> msg
+  NoViewFoundForInterface intName ->
+    "No view found for interface " <> renderPretty intName
+  InvalidInterface iface tpl err ->
+    unwords
+      [ "Invalid 'interface instance"
+      , prettyPrint iface
+      , "for"
+      , prettyPrint tpl <> "':"
+      , ppInvalidInterfaceError err
+      ]
+  CannotRequireNonInterface tyCon ->
+    "cannot require '" ++ prettyPrint tyCon ++ "' because it is not an interface"
+
+ppInvalidInterfaceError (NotAnInterface iface) = "'" <> prettyPrint iface <> "' is not an interface"
+ppInvalidInterfaceError (NotATemplate tpl) = "'" <> prettyPrint tpl <> "' is not a template"
+ppInvalidInterfaceError DoesNotMatchEnclosingTemplateDeclaration = unwords
+  [ "The template of this interface instance does not match the"
+  , "enclosing template declaration."
+  ]
+ppInvalidInterfaceError NoViewDefined = "no view implementation defined"
+ppInvalidInterfaceError MoreThanOneViewDefined = "more than one view implementation defined"
+
+ppStandaloneWarning :: StandaloneWarning -> String
+ppStandaloneWarning (PolymorphicTopLevelScript name ty tyVar) =
+  unlines
+    [ "This method is implicitly polymorphic. Top level polymorphic scripts will not be run as tests."
+    , "If this is intentional, please write an explicit type signature with the `forall'"
+    , "If not, either provide a specialised type signature, such as:"
+    , showSDocUnsafe $ quotes $ ppr name <+> ":" <+> pprType (substUnit tyVar ty)
+    , "Or apply the unit type to the top level `script' call:"
+    , showSDocUnsafe $ quotes $ ppr name <+> "= script @() do ..."
+    ]
+  where
+  substUnit :: GHC.TyVar -> GHC.Type -> GHC.Type
+  substUnit tyVar ty = TyCoRep.substTy (setTvSubstEnv emptyTCvSubst $ mkVarEnv [(tyVar, TyConApp unitTyCon [])]) ty
+
+ppErrorOrWarning :: ErrorOrWarning -> String
+ppErrorOrWarning (LargeTuple n) = "Used tuple of size " <> show n <> ". Daml only has Show, Eq, Ord instances for tuples of size <= 5."
+
 newtype ConvertM a = ConvertM (ReaderT ConversionEnv (StateT ConversionState (Except FileDiagnostic)) a)
   deriving (Functor, Applicative, Monad, MonadError FileDiagnostic, MonadState ConversionState, MonadReader ConversionEnv)
 
 instance MonadFail ConvertM where
-    fail = conversionError
+    fail = conversionError . RawError
 
 -- The left case is for the single error thrown, the right case for a list of
 -- non-fatal warnings
@@ -83,27 +184,33 @@ resetFreshVarCounters = modify' (\st -> st{freshTmVarCounter = 0})
 ---------------------------------------------------------------------
 -- FAILURE REPORTING
 
-conversionError :: String -> ConvertM e
-conversionError msg = do
+conversionError :: StandaloneError -> ConvertM e
+conversionError = conversionErrorRaw . StandaloneError
+
+conversionErrorRaw :: Error -> ConvertM e
+conversionErrorRaw err = do
   ConversionEnv{..} <- ask
   throwError $ (convModuleFilePath,ShowDiag,) Diagnostic
       { _range = maybe noRange sourceLocToRange convRange
       , _severity = Just DsError
       , _source = Just "Core to Daml-LF"
-      , _message = T.pack msg
+      , _message = T.pack (ppError err)
       , _code = Nothing
       , _relatedInformation = Nothing
       , _tags = Nothing
       }
 
-conversionWarning :: String -> ConvertM ()
-conversionWarning msg = do
+conversionWarning :: StandaloneWarning -> ConvertM ()
+conversionWarning = conversionWarningRaw . StandaloneWarning
+
+conversionWarningRaw :: Warning -> ConvertM ()
+conversionWarningRaw msg = do
   ConversionEnv{..} <- ask
   let diagnostic = Diagnostic
         { _range = maybe noRange sourceLocToRange convRange
         , _severity = Just DsWarning
         , _source = Just "Core to Daml-LF"
-        , _message = T.pack msg
+        , _message = T.pack (ppWarning msg)
         , _code = Nothing
         , _relatedInformation = Nothing
         , _tags = Nothing
@@ -111,21 +218,25 @@ conversionWarning msg = do
       fileDiagnostic = (convModuleFilePath, ShowDiag, diagnostic)
   modify $ \s -> s { warnings = fileDiagnostic : warnings s }
 
+class IsErrorOrWarning e where
+  conversionDiagnostic :: e -> ConvertM ()
+
+instance IsErrorOrWarning StandaloneError where
+  conversionDiagnostic = conversionError
+
+instance IsErrorOrWarning StandaloneWarning where
+  conversionDiagnostic = conversionWarning
+
+instance IsErrorOrWarning ErrorOrWarning where
+  conversionDiagnostic errOrWarn = conversionWarningRaw (ErrorOrWarningAsWarning errOrWarn)
+
 unsupported :: (HasCallStack, Outputable a) => String -> a -> ConvertM e
-unsupported typ x = conversionError errMsg
-    where
-         errMsg =
-             "Failure to process Daml program, this feature is not currently supported.\n" ++
-             typ ++ "\n" ++
-             prettyPrint x
+unsupported typ x = conversionError (Unsupported typ (prettyPrint x))
 
 unknown :: HasCallStack => GHC.UnitId -> MS.Map GHC.UnitId DalfPackage -> ConvertM e
-unknown unitId pkgMap = conversionError errMsg
-    where errMsg =
-              "Unknown package: " ++ GHC.unitIdString unitId
-              ++ "\n" ++  "Loaded packages are:" ++ prettyPrint (MS.keys pkgMap)
+unknown unitId pkgMap = conversionError (UnknownPackage unitId pkgMap)
 
 unhandled :: (HasCallStack, Data a, Outputable a) => String -> a -> ConvertM e
-unhandled typ x = unsupported (typ ++ " with " ++ lower (show (toConstr x))) x
+unhandled typ x = conversionError (Unhandled typ (toConstr x) (prettyPrint x))
 
 
