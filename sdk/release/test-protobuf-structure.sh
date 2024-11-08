f36458ac4b9bb9952f98e7c0d550bf91c2c718f2
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2024-11-08 12:14:27 +0000

    Update test-protobuf-structure.sh

--- sdk/release/test-protobuf-structure.sh
+++ sdk/release/test-protobuf-structure.sh
@@ -42,8 +42,9 @@ com/daml/ledger/api/v2/command_submission_service.proto
 com/daml/ledger/api/v2/command_service.proto
 com/daml/ledger/api/v2/state_service.proto
 com/daml/ledger/api/v2/experimental_features.proto
-com/daml/ledger/api/v2/interactive_submission_data.proto
-com/daml/ledger/api/v2/interactive_submission_service.proto
+com/daml/ledger/api/v2/interactive/interactive_submission_common_data.proto
+com/daml/ledger/api/v2/interactive/interactive_submission_service.proto
+com/daml/ledger/api/v2/interactive/transaction/v1/interactive_submission_data.proto
 com/daml/ledger/api/v2/completion.proto
 com/daml/ledger/api/v2/package_service.proto
 com/daml/ledger/api/v2/commands.proto
