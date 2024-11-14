97f9dd918eae927ef9b8bd5bc7cc5dd7df53d199
Tudor Voicu <tudor.voicu@digitalasset.com>
2024-11-14 14:57:50 +0100

    subgroupOf

--- sdk/daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/PackageRequirements.scala
+++ sdk/daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/PackageRequirements.scala
@@ -5,19 +5,23 @@ import com.daml.lf.data.Ref.PackageId
 
 case class PackageRequirements(knownOnly: Set[PackageId], vetted: Set[PackageId]) {
   lazy val isEmpty: Boolean = knownOnly.isEmpty && vetted.isEmpty
+
+  def subgroupOf(other: PackageRequirements): Boolean =
+    knownOnly.subsetOf(other.knownOnly) && vetted.subsetOf(other.vetted)
 }
 
 object PackageRequirements {
 
   val empty: PackageRequirements = PackageRequirements(Set.empty, Set.empty)
 
-  implicit val packagesUsedSemigroup: Semigroup[PackageRequirements] = Semigroup.instance { (x, y) =>
-    val vetted = x.vetted ++ y.vetted
-    val known = (x.knownOnly ++ y.knownOnly) -- vetted
-    PackageRequirements(
-      knownOnly = known,
-      vetted = vetted,
-    )
+  implicit val packagesUsedSemigroup: Semigroup[PackageRequirements] = Semigroup.instance {
+    (x, y) =>
+      val vetted = x.vetted ++ y.vetted
+      val known = (x.knownOnly ++ y.knownOnly) -- vetted
+      PackageRequirements(
+        knownOnly = known,
+        vetted = vetted,
+      )
   }
 
   def knownOnly(singleton: PackageId): PackageRequirements =
