5f7d05902f8f1b55c7d21a93dc4ea8474ea560c3
Andreas Lochbihler <andreas.lochbihler@digitalasset.com>
2024-12-03 18:00:18 +0100

    add ResultPrefetch

--- sdk/daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/Engine.scala
+++ sdk/daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/Engine.scala
@@ -5,6 +5,7 @@ package com.daml.lf
 package engine
 
 import com.daml.lf.command._
+import com.daml.lf.crypto.Hash.KeyPackageName
 import com.daml.lf.data._
 import com.daml.lf.data.Ref.{Identifier, PackageId, ParticipantId, Party}
 import com.daml.lf.language.Ast._
@@ -13,17 +14,18 @@ import com.daml.lf.speedy.SExpr.{SEApp, SExpr}
 import com.daml.lf.speedy.Speedy.{Machine, PureMachine, UpdateMachine}
 import com.daml.lf.speedy.SResult._
 import com.daml.lf.transaction.{
+  GlobalKey,
   Node,
   SubmittedTransaction,
   Versioned,
   VersionedTransaction,
   Transaction => Tx,
+  TransactionVersion => TxVersion,
 }
 
 import java.nio.file.Files
 import com.daml.lf.value.Value
 import com.daml.lf.value.Value.ContractId
-
 import com.daml.lf.language.{
   LanguageMajorVersion,
   LanguageVersion,
@@ -31,6 +33,7 @@ import com.daml.lf.language.{
   PackageInterface,
   StablePackages,
 }
+import com.daml.lf.speedy.SError.{SErrorCrash, SErrorDamlException}
 import com.daml.lf.speedy.Speedy.Machine.newTraceLog
 import com.daml.lf.validation.Validation
 import com.daml.logging.LoggingContext
@@ -152,6 +155,7 @@ class Engine(val config: EngineConfig = Engine.StableConfig, allowLF2: Boolean =
     for {
       pkgResolution <- preprocessor.buildPackageResolution(packageMap, packagePreference)
       processedCmds <- preprocessor.preprocessApiCommands(pkgResolution, cmds.commands)
+      _ <- prefetchKeys(processedCmds)
       processedDiscs <- preprocessor.preprocessDisclosedContracts(disclosures)
       result <-
         interpretCommands(
@@ -168,6 +172,44 @@ class Engine(val config: EngineConfig = Engine.StableConfig, allowLF2: Boolean =
         )
       (tx, meta) = result
     } yield tx -> meta.copy(submissionSeed = Some(submissionSeed))
+  }
+
+  private def prefetchKeys(commands: ImmArray[speedy.Command]): Result[Unit] = {
+    val keys = commands.iterator.collect {
+      case speedy.Command.ExerciseByKey(templateId, contractKey, _, _) =>
+        // Extracted from com.daml.lf.speedy.SBuiltin.extractKey
+
+        // Copied from com.daml.lf.speedy.Speedy.Machine.tmplId2TxVersion
+        val packageTxVersion = TxVersion.assignNodeVersion(
+          compiledPackages.pkgInterface.packageLanguageVersion(templateId.packageId)
+        )
+        // Copied from com.daml.lf.speedy.Speedy.Machine.tmplId2PackageName
+        val pkgName = {
+          import Ordering.Implicits._
+          if (packageTxVersion < TxVersion.minUpgrade)
+            None
+          else
+            compiledPackages.pkgInterface.signatures(templateId.packageId).metadata match {
+              case Some(value) => Some(value.name)
+              case None =>
+                val version =
+                  compiledPackages.pkgInterface.packageLanguageVersion(templateId.packageId)
+                throw SErrorCrash(
+                  NameOf.qualifiedNameOfCurrentFunc,
+                  s"unexpected ${version.pretty} package without metadata",
+                )
+            }
+        }
+        val lfValue = contractKey.toNormalizedValue(packageTxVersion)
+        import com.daml.lf.interpretation.{Error => IE}
+        val gkey = GlobalKey
+          .build(templateId, lfValue, KeyPackageName(pkgName, packageTxVersion))
+          .getOrElse(
+            throw SErrorDamlException(IE.ContractIdInContractKey(contractKey.toUnnormalizedValue))
+          )
+        gkey
+    }
+    ResultPrefetch(keys.toSeq, () => Result.unit)
   }
 
   /** Behaves like `submit`, but it takes a single `ReplayCommand` (instead of `ApiCommands`) as input.
