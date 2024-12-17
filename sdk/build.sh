8b5e20fe439007253a90b2101e2faaac0aeb0123
Paul Brauner <paul.brauner@digitalasset.com>
2024-12-17 12:13:59 +0100

    try 500 timwes

--- sdk/build.sh
+++ sdk/build.sh
@@ -117,7 +117,7 @@ start_postgresql
 
 echo "Running bazel test with the following tag filters: ${tag_filter}"
 
-$bazel test //compiler/damlc/tests:data-dependencies-lf1 --runs_per_test=100
+$bazel test //compiler/damlc/tests:data-dependencies-lf1 --runs_per_test=500
 
 # Make sure that Bazel query works.
 $bazel query 'deps(//...)' >/dev/null
