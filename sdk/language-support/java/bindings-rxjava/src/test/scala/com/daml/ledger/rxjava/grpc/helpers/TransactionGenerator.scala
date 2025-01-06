a5d6e6c87dbb7b3520fe1cda1c2962569dd8b303
Samuel Williams <samuel.williams@digitalasset.com>
2025-01-06 16:58:54 +0000

    Use offset from tx rather than event.

--- sdk/language-support/java/bindings-rxjava/src/test/scala/com/daml/ledger/rxjava/grpc/helpers/TransactionGenerator.scala
+++ sdk/language-support/java/bindings-rxjava/src/test/scala/com/daml/ledger/rxjava/grpc/helpers/TransactionGenerator.scala
@@ -434,10 +434,8 @@ object TransactionGenerator {
     (arbitraryLedgerContent, _) <- ledgerContentTreeGen
     (queriedLedgerContent, queriedTransaction) <- transactionTreeGen.suchThat(_._1.events.nonEmpty)
     ledgerContent = arbitraryLedgerContent :+ queriedLedgerContent
-    eventOffsets = queriedLedgerContent.events.map(UpdateServiceImpl.eventOffset)
-    eventOffsetList <- Gen.pick(1, eventOffsets)
-    eventOffset = eventOffsetList.head
-  } yield (ledgerContent, eventOffset, queriedTransaction)
+    txOffset = queriedTransaction.getOffset
+  } yield (ledgerContent, txOffset, queriedTransaction)
 
   val ledgerContentWithTransactionIdGen: Gen[(List[LedgerItem], String, TransactionTree)] =
     for {
