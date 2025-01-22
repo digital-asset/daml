9281a1f3f55d42bfa983e1bdc05a0a12b352f25b
Paul Brauner <paul.brauner@digitalasset.com>
2025-01-22 14:31:36 +0100

    list failed test targets post build

---
+++ sdk/dummy_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+
+echo "abc"
+echo "def"
+exit 1
