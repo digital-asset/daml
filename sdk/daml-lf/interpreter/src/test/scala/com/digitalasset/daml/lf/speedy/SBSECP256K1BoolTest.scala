2f221c3dbf05ed691829c13bfbde7f4ca5186431
Carl Pulley <carl.pulley@digitalasset.com>
2025-01-27 15:42:40 +0000

    Merge conflict fixes

--- sdk/daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/SBSECP256K1BoolTest.scala
+++ sdk/daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/SBSECP256K1BoolTest.scala
@@ -4,27 +4,21 @@
 package com.digitalasset.daml.lf
 package speedy
 
-import com.digitalasset.daml.lf.data.{Bytes, Ref}
+import com.digitalasset.daml.lf.data.cctp.MessageSignatureUtil
+import com.digitalasset.daml.lf.data.Bytes
 import com.digitalasset.daml.lf.speedy.SBuiltinFun.SBSECP256K1Bool
 import org.bouncycastle.jce.provider.BouncyCastleProvider
-import org.scalatest.BeforeAndAfterAll
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-import java.security.{KeyPairGenerator, SecureRandom, Security}
-import java.security.spec.ECGenParameterSpec
+import java.security.Security
 
-class SBSECP256K1BoolTest extends AnyFreeSpec with Matchers with BeforeAndAfterAll {
-
-  override def beforeAll(): Unit = {
-    val _ = Security.insertProviderAt(new BouncyCastleProvider, 1)
-  }
+class SBSECP256K1BoolTest extends AnyFreeSpec with Matchers {
+  Security.addProvider(new BouncyCastleProvider)
 
   "PublicKeys are correctly built from hex encoded public key strings" in {
-    val keyPairGen = KeyPairGenerator.getInstance("EC")
-    keyPairGen.initialize(new ECGenParameterSpec("secp256k1"), new SecureRandom())
-    val actualPublicKey = keyPairGen.generateKeyPair().getPublic
-    val hexEncodedPublicKey = Ref.HexString.encode(Bytes.fromByteArray(actualPublicKey.getEncoded))
+    val actualPublicKey = MessageSignatureUtil.generateKeyPair.getPublic
+    val hexEncodedPublicKey = Bytes.fromByteArray(actualPublicKey.getEncoded).toHexString
 
     SBSECP256K1Bool.extractPublicKey(hexEncodedPublicKey) shouldBe actualPublicKey
   }
