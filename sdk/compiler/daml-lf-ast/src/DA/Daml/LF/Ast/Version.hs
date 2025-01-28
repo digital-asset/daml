ad6a8556ac373401dc000117b2fed4e426537640
Carl Pulley <carl.pulley@digitalasset.com>
2025-01-28 10:43:29 +0000

    Bug fic: hide CCTP behind CPP flags

--- sdk/compiler/daml-lf-ast/src/DA/Daml/LF/Ast/Version.hs
+++ sdk/compiler/daml-lf-ast/src/DA/Daml/LF/Ast/Version.hs
@@ -194,6 +194,13 @@ featureContractKeys = Feature
     , featureCppFlag = Just "DAML_CONTRACT_KEYS"
     }
 
+featureCCTP :: Feature
+featureCCTP = Feature
+    { featureName = "Daml CCTP primitives"
+    , featureVersionReq = devOnly
+    , featureCppFlag = Just "DAML_CCTP"
+    }
+
 featureExperimental :: Feature
 featureExperimental = Feature
     { featureName = "Daml Experimental"
@@ -226,6 +233,7 @@ allFeatures =
     , featureUnstable
     , featureExperimental
     , featureDynamicExercise
+    , featureCCTP
     ]
 
 -- | A map from feature CPP flags to features.
