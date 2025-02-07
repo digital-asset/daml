cbcf1e8c3a1234fd24727fc63ac967739c275e08
Carl Pulley <106966370+carlpulley-da@users.noreply.github.com>
2025-02-07 11:24:37 +0000

    Structured errors for CCTP builtins (#20719)
    CCTP builtin structured error reporting

--- sdk/compiler/script-service/server/src/main/scala/com/digitalasset/daml/lf/script/Conversions.scala
+++ sdk/compiler/script-service/server/src/main/scala/com/digitalasset/daml/lf/script/Conversions.scala
@@ -252,6 +252,11 @@ final class Conversions(
                         .setExpected(convertIdentifier(expected))
                         .addAllAccepted(accepted.map(convertIdentifier(_)).asJava)
                     )
+                  case _: Dev.CCTP =>
+                    // TODO: https://github.com/DACH-NY/canton-network-utilities/issues/3017: add structured error reporting to daml-script
+                    proto.ScriptError.CCTPError.newBuilder.setMessage(
+                      speedy.Pretty.prettyDamlException(interpretationError).render(80)
+                    )
                   case _: Dev.Upgrade =>
                     proto.ScriptError.UpgradeError.newBuilder.setMessage(
                       speedy.Pretty.prettyDamlException(interpretationError).render(80)
