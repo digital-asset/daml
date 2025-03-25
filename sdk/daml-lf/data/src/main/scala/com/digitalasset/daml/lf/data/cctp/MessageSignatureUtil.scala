c1fdd6af24f9d49d23a24883036135691ad151a2
Carl Pulley <carl.pulley@digitalasset.com>
2025-03-25 18:33:09 +0000

    Use a deterministic secure random for signing

--- sdk/daml-lf/data/src/main/scala/com/digitalasset/daml/lf/data/cctp/MessageSignatureUtil.scala
+++ sdk/daml-lf/data/src/main/scala/com/digitalasset/daml/lf/data/cctp/MessageSignatureUtil.scala
@@ -7,16 +7,21 @@ package cctp
 import com.daml.crypto.MessageSignaturePrototypeUtil
 
 import java.security.spec.ECGenParameterSpec
-import java.security.{KeyPair, KeyPairGenerator, PrivateKey}
+import java.security.{KeyPair, KeyPairGenerator, PrivateKey, SecureRandom}
 
 // The following utility methods should only be used within a testing context.
 // They have been moved into a main project scope so that daml-script runners may access this code.
 object MessageSignatureUtil {
-  def sign(message: Ref.HexString, privateKey: PrivateKey): Ref.HexString = {
+  def sign(
+      message: Ref.HexString,
+      privateKey: PrivateKey,
+      randomSrc: SecureRandom = new SecureRandom(),
+  ): Ref.HexString = {
     val signature =
       MessageSignaturePrototypeUtil.Secp256k1.sign(
         Bytes.fromHexString(message).toByteArray,
         privateKey,
+        randomSrc,
       )
 
     Ref.HexString.encode(Bytes.fromByteArray(signature))
