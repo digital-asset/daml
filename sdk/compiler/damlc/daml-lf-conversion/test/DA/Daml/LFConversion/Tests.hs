07e007865ed399a192cb0bd25fd28c901f74a57d
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2025-04-15 12:57:28 +0100

    Fix warningFlagParserLFConversion -> warningFlagParser

--- sdk/compiler/damlc/daml-lf-conversion/test/DA/Daml/LFConversion/Tests.hs
+++ sdk/compiler/damlc/daml-lf-conversion/test/DA/Daml/LFConversion/Tests.hs
@@ -318,6 +318,6 @@ bignumericTests = testGroup "BigNumeric"
         let dummyEnv = ConversionEnv
               { convModuleFilePath = toNormalizedFilePath' ""
               , convRange = Nothing
-              , convWarningFlags = mkWarningFlags warningFlagParserLFConversion []
+              , convWarningFlags = mkWarningFlags warningFlagParser []
               }
         in runConvertM dummyEnv $ convertRationalBigNumeric (numerator r) (denominator r)
