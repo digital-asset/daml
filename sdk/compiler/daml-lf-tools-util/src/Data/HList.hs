f5c7b3aa58ec2493fd0d68685873590c8ee328a4
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2025-04-15 16:02:19 +0100

    Fix lint & deps

--- sdk/compiler/daml-lf-tools-util/src/Data/HList.hs
+++ sdk/compiler/daml-lf-tools-util/src/Data/HList.hs
@@ -1,6 +1,7 @@
 -- Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
