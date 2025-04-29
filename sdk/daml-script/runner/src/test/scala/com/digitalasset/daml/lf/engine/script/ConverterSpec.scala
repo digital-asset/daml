a874ade745d217c6e55224f3f20b0c6ec23e4339
Carl Pulley <carl.pulley@digitalasset.com>
2025-04-29 11:42:08 +0100

    daml-script converter tests

--- sdk/daml-script/runner/src/test/scala/com/digitalasset/daml/lf/engine/script/ConverterSpec.scala
+++ sdk/daml-script/runner/src/test/scala/com/digitalasset/daml/lf/engine/script/ConverterSpec.scala
@@ -1,15 +1,23 @@
 // Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.digitalasset.daml.lf.engine.script
+package com.digitalasset
+package daml.lf
+package engine.script
 
-import org.scalatest.freespec.AsyncFreeSpec
+import com.digitalasset.daml.lf.data.{ImmArray, Ref, Time}
+import com.digitalasset.daml.lf.engine.preprocessing.ValueTranslator
 import com.digitalasset.daml.lf.engine.script.v2.Converter
+import com.digitalasset.daml.lf.engine.script.v2.ledgerinteraction.ScriptLedgerClient.TransactionTree
+import com.digitalasset.daml.lf.language.PackageInterface
+import com.digitalasset.daml.lf.speedy.SValue._
+import org.scalatest.{Assertion, Inside}
+import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.util.Random
 
-final class ConverterTest extends AsyncFreeSpec with Matchers {
+final class ConverterSpec extends AsyncFreeSpec with Matchers with Inside {
   "toPartyIdHint" - {
     "with a non-empty hint" - {
       val hint = "foo"
@@ -55,6 +63,81 @@ final class ConverterTest extends AsyncFreeSpec with Matchers {
         val first = Converter.toPartyIdHint("", "", random)
         val second = Converter.toPartyIdHint("", "", random)
         first should not be second
+      }
+    }
+  }
+
+  "translateTransactionTree" - {
+    "with an empty time bound" in {
+      val timeBoundaries = None
+
+      assertTimeBoundaries(timeBoundaries)
+    }
+
+    "with a non-empty time bound" - {
+      val t0 = Time.Timestamp.now()
+      val t1 = t0.addMicros(1)
+
+      "Timestamp.min == lower bound < upper bound" in {
+        val timeBoundaries = Some(Time.Range(Time.Timestamp.MinValue, t0))
+
+        assertTimeBoundaries(timeBoundaries)
+      }
+
+      "lower bound < upper bound == Timestamp.max" in {
+        val timeBoundaries = Some(Time.Range(t0, Time.Timestamp.MaxValue))
+
+        assertTimeBoundaries(timeBoundaries)
+      }
+
+      "Timestamp.min == lower bound < upper bound == Timestamp.max" in {
+        val timeBoundaries = Some(Time.Range(Time.Timestamp.MinValue, Time.Timestamp.MaxValue))
+
+        assertTimeBoundaries(timeBoundaries)
+      }
+
+      "lower bound == upper bound" in {
+        val timeBoundaries = Some(Time.Range(t0, t0))
+
+        assertTimeBoundaries(timeBoundaries)
+      }
+
+      "lower bound < upper bound" in {
+        val timeBoundaries = Some(Time.Range(t0, t1))
+
+        assertTimeBoundaries(timeBoundaries)
+      }
+    }
+
+    def assertTimeBoundaries(timeBoundaries: Option[Time.Range]): Assertion = {
+      val timeBoundariesName = Ref.Name.assertFromString("timeBoundaries")
+      val svalue = Converter.translateTransactionTree(
+        { case _: (Ref.Identifier, Option[Ref.Identifier], Ref.ChoiceName) =>
+          Left("Unimplemented")
+        },
+        new ValueTranslator(
+          new PackageInterface(PartialFunction.empty),
+          requireV1ContractIdSuffix = false,
+        ),
+        ScriptIds(Ref.PackageId.assertFromString("ledgerTimePackageId"), isLegacy = false),
+        TransactionTree(List.empty, timeBoundaries),
+      )
+
+      inside(svalue) { case Right(SRecord(_, ImmArray(_, `timeBoundariesName`), boundaryValues)) =>
+        inside(boundaryValues.get(1)) {
+          case SOptional(None) =>
+            timeBoundaries should be(None)
+
+          case SOptional(Some(SRecord(_, ImmArray(minName, maxName), rangeValues))) =>
+            minName should be(Ref.Name.assertFromString("min"))
+            maxName should be(Ref.Name.assertFromString("max"))
+
+            inside((rangeValues.get(0), rangeValues.get(1))) {
+              case (STimestamp(min), STimestamp(max)) =>
+                timeBoundaries should be(Some(Time.Range(min, max)))
+            }
+        }
+
       }
     }
   }
