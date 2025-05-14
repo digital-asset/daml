487fe13d15460ab414f8a9e4309953fce758fbeb
Samuel Williams <samuel.williams@digitalasset.com>
2025-05-14 16:29:40 +0100

    Try to fix compatibility

--- sdk/compatibility/bazel_tools/create_daml_app_test.sh
+++ sdk/compatibility/bazel_tools/create_daml_app_test.sh
@@ -41,6 +41,7 @@ TEST_TS="$(rlocation "$TEST_WORKSPACE/${15}")"
 CODEGEN_OUTPUT="$(canonicalize_rlocation "${16}")"
 export DAR_PATH="$(canonicalize_rlocation "${17}")"
 
+export PATH=$NODE:$PATH
 "$RUNNER" \
   --daml "$DAML" \
   --daml-types "$DAML_TYPES" \
