38c76a00187ef0fe0853d089d165c952edb5a4ce
Paul Brauner <paul.brauner@digitalasset.com>
2025-05-20 16:22:02 +0200

    add copyright header to ps1 file

--- sdk/ci/create-dotnetrc.ps1
+++ sdk/ci/create-dotnetrc.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 Set-StrictMode -Version Latest
 
 $githubToken = $env:GITHUB_TOKEN
