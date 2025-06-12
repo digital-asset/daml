ea1b0ee7b937946b868ca41958a17b41d73d891c
Roger Bosman <roger.bosman@digitalasset.com>
2025-06-12 17:04:52 +0200

    Encode support for interning kinds
    WIP: into encodeKind'
    
    Added Encoding
    
    Now also encode package
    
    Extracted interning logic to InternedMap
    
    WIP Added the stateful interning
    
    WIP removed non-lens map
    
    WIP Refactored to internedKindsMap
    
    Move to module
    
    Add InteredMap

---
+++ sdk/compiler/daml-lf-proto-encode/src/DA/Daml/LF/Proto3/InternedMap.hs
@@ -0,0 +1,58 @@
+-- Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+
+module DA.Daml.LF.Proto3.InternedMap (InternedMap, internState, toVec, empty) where
+
+import           Control.Lens (Lens', (.=), use)
+import           Control.Monad.State.Strict
+
+import qualified Data.List           as L
+import qualified Data.Map.Strict     as Map
+import qualified Data.Vector         as V
+
+data InternedMap val key where
+  InternedMap :: (Ord val, Ord key, Num key, Bounded key) => !(Map.Map val key) -> !key -> InternedMap val key
+
+empty :: (Ord val, Ord key, Num key, Bounded key) => InternedMap val key
+empty = InternedMap Map.empty 0
+
+toVec :: InternedMap val key -> V.Vector val
+toVec (InternedMap mp n) =
+  let vec = V.fromList $ map fst $ L.sortOn snd $ Map.toList mp
+  in
+    if (fromIntegral $ V.length vec) == n
+    then vec
+    else error "internedKinds of incorrect length"
+
+extend :: val -> InternedMap val key -> (InternedMap val key, key)
+extend x (InternedMap mp n) =
+  if n == maxBound
+  then error "Interning table grew too large"
+  else (InternedMap (Map.insert x n mp) (n + 1), n)
+
+-- API to be exposed
+_internBlind :: val -> InternedMap val key -> (InternedMap val key, key)
+_internBlind = extend
+
+-- API to be exposed
+internShare :: val -> InternedMap val key -> (InternedMap val key, key)
+internShare x im@(InternedMap mp _) =
+  case x `Map.lookup` mp of
+    Just n -> (im, n)
+    Nothing -> extend x im
+
+_intern :: val -> InternedMap val key -> (InternedMap val key, key)
+_intern = internShare
+
+internState :: val -> Lens' s (InternedMap val key) -> State s key
+internState x lns = do
+  im@(InternedMap mp _) <- use lns
+  case x `Map.lookup` mp of
+    Just n -> return n
+    Nothing -> do
+      let (mp', n') = extend x im
+      lns .= mp'
+      return n'
