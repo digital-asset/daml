ace3d6af682417257bbaa475dc4d31bf87406cb8
Volodymyr Marchenko <volodymyr.marchenko@digitalasset.com>
2025-06-13 16:28:22 +0300

    Hotfix for Implementation publishing windows artifacts to OCI with unifi tool #21342
    Signed-off-by: Volodymyr Marchenko <volodymyr.marchenko@digitalasset.com>

--- ci/copy-windows-release-artifacts.sh
+++ ci/copy-windows-release-artifacts.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 set -euo pipefail
-
+DEBUG="${DEBUG:-}"
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${DIR}"/../sdk || exit 1
 
