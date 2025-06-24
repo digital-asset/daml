82b454fb05f11f598a9848c41aac4a518efa94c3
Roger Bosman <roger.bosman@digitalasset.com>
2025-06-24 12:53:07 +0200

    Renamed test files to be more descriptive

--- sdk/compiler/daml-lf-proto-encode/test/DA/Daml/LF/Proto3/EncodeTest.hs
+++ sdk/compiler/daml-lf-proto-encode/test/DA/Daml/LF/Proto3/EncodeTest.hs
@@ -1,8 +1,8 @@
 -- Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-module DA.Daml.LF.Proto3.Test (
-        module DA.Daml.LF.Proto3.Test
+module DA.Daml.LF.Proto3.EncodeTest (
+        module DA.Daml.LF.Proto3.EncodeTest
 ) where
 
 
