bd4d73d66a7422ff1cc81287294cd50432d8ea91
Samuel Williams <samuel.williams@digitalasset.com>
2025-07-23 09:54:33 +0100

    Update language client (port of #21154) (#21247) (#21585)
    Fix the #21253 bug wich turns out to be with the language-client VS library.
    
    - To fix it, we need to update  language-client VS library from 7.0.0 to > 8.0.0
    -language-client VS library > 8.0.0 requires nodejs 18
    - Updating nodejs to 18 requires many things, including updating typescript
    - Updating typescript requires updating rules_nodejs
    
    Co-authored-by: Samuel Williams samuel.williams@digitalasset.com
    Co-authored-by: Dylan Thinnes dylan.thinnes@digitalasset.com
    
    Co-authored-by: Remy <remy.haemmerle@daml.com>

--- sdk/language-support/ts/codegen/tests/ts/build-and-lint-test/src/__tests__/test.ts
+++ sdk/language-support/ts/codegen/tests/ts/build-and-lint-test/src/__tests__/test.ts
@@ -11,6 +11,7 @@ import Ledger, {
   Stream,
   PartyInfo,
   UserRightHelper,
+  LedgerError,
 } from "@daml/ledger";
 import {
   Choice,
@@ -73,6 +74,8 @@ const SANDBOX_PORT_FILE = "sandbox.port";
 let jsonApiPort: number | undefined = undefined;
 const JSON_API_PORT_FILE = "json-api.port";
 const COMPLETION_FILE = "completion_marker";
+// Using `127.0.0.1` instead of `localhost`, as `localhost` fails to connect on MacOS in node 18
+// See this comment: https://github.com/node-fetch/node-fetch/issues/1624#issuecomment-1235826631
 const httpBaseUrl: () => string = () => `http://127.0.0.1:${jsonApiPort}/`;
 
 let sandboxProcess: ChildProcess | undefined = undefined;
@@ -1266,8 +1269,10 @@ test("stream close behaviour", async () => {
   const url = "ws" + httpBaseUrl().slice(4) + "v1/stream/query";
   const events: string[] = [];
   const ws = new WebSocket(url, ["jwt.token." + ALICE_TOKEN, "daml.ws.auth"]);
-  await new Promise(resolve => ws.addEventListener("open", () => resolve()));
-  const forCloseEvent = new Promise(resolve =>
+  await new Promise<void>(resolve =>
+    ws.addEventListener("open", () => resolve()),
+  );
+  const forCloseEvent = new Promise<void>(resolve =>
     ws.addEventListener("close", () => {
       events.push("close");
       resolve();
@@ -1372,7 +1377,8 @@ test("package API", async () => {
     try {
       await p;
       expect(true).toBe(false);
-    } catch (exc) {
+    } catch (e) {
+      const exc = e as LedgerError;
       expect([400, 404]).toContain(exc.status);
       expect(exc.errors.length).toBe(1);
     }
@@ -1386,7 +1392,7 @@ test("package API", async () => {
   );
   expect(packagesBefore.length > 1).toBe(true);
 
-  const nonSense = Uint8Array.from([1, 2, 3, 4]);
+  const nonSense = Buffer.from(Uint8Array.from([1, 2, 3, 4]));
 
   await expectFail(ledger.uploadDarFile(nonSense));
 
