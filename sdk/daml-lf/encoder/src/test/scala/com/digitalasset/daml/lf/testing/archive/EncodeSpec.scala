bcd47b6a2751456563f99a9b679f66ddd24f7806
Remy Haemmerle <Remy.Haemmerle@daml.com>
2025-08-06 13:18:40 +0200

    typo

--- sdk/daml-lf/encoder/src/test/scala/com/digitalasset/daml/lf/testing/archive/EncodeSpec.scala
+++ sdk/daml-lf/encoder/src/test/scala/com/digitalasset/daml/lf/testing/archive/EncodeSpec.scala
@@ -172,7 +172,7 @@ ${onlyIf(languageVersion >= LanguageVersion.Features.contractKeys)("""
            val identity: forall (a: *). a -> a = /\ (a: *). \(x: a) -> x;
            val anExercise: (ContractId Mod:Person) -> Update Unit = \(cId: ContractId Mod:Person) ->
              exercise @Mod:Person Sleep (Mod:identity @(ContractId Mod:Person) cId) ();
-             |
+
 ${onlyIf(languageVersion >= LanguageVersion.Features.contractKeys)(s"""
            val aFecthByKey: Party -> Update ($tuple2TyCon (ContractId Mod:Person) Mod:Person) = \\(party: Party) ->
              fetch_by_key @Mod:Person party;
