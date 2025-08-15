9350a366bc58b3e716d5dacfbb8c56b8e05c279d
Simon Maxen <simon.maxen@digitalasset.com>
2025-08-15 11:32:06 +0100

    Update ResultNeedContract callbabck

--- sdk/canton/community/ledger/ledger-api-core/src/main/scala/com/digitalasset/canton/platform/apiserver/execution/StoreBackedCommandInterpreter.scala
+++ sdk/canton/community/ledger/ledger-api-core/src/main/scala/com/digitalasset/canton/platform/apiserver/execution/StoreBackedCommandInterpreter.scala
@@ -288,7 +288,7 @@ final class StoreBackedCommandInterpreter(
               resolveStep(
                 Tracked.value(
                   metrics.execution.engineRunning,
-                  trackSyncExecution(interpretationTimeNanos)(resume(fatInstanceOpt)),
+                  trackSyncExecution(interpretationTimeNanos)(resume(ResultNeedContract.wrapLegacyResponse(fatInstanceOpt))),
                 )
               )
             }
