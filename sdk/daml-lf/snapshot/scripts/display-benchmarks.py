c57603badc47793f489dd663c4d6a1adb37d150b
Carl Pulley <106966370+carlpulley-da@users.noreply.github.com>
2025-08-19 13:03:37 +0100

    Test for generating generic snapshot files (#21651)

---
+++ sdk/daml-lf/snapshot/scripts/display-benchmarks.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import json
+import os
+import pandas as pd
+import plotly.express as px
+
+def get_jmh_data(base_dir):
+    result = []
+    for dar_file in os.listdir(base_dir):
+        for script in os.listdir("{}/{}".format(base_dir, dar_file)):
+            for jmh_data in os.listdir("{}/{}/{}".format(base_dir, dar_file, script)):
+                if jmh_data.endswith(".json"):
+                    with open("{}/{}/{}/{}".format(base_dir, dar_file, script, jmh_data)) as fd:
+                        data = json.load(fd)
+                        fd.close()
+                        if len(data) > 0:
+                            entry = {
+                                "dar-file": dar_file,
+                                "script": script,
+                                "choice": data[0]["params"]["choiceName"],
+                                "benchmark-data": data[0]["primaryMetric"],
+                            }
+                            result.append(entry)
+    return result
+
+def get_graph_data(base_data, update_data):
+    result = {"type": [], "choice": [], "score": [], "min-score": [], "max-score": [], "score-unit": [], "dar#script": []}
+    for entry in base_data:
+        result["type"].append("base")
+        result["choice"].append(entry["choice"])
+        result["score"].append(entry["benchmark-data"]["score"])
+        result["min-score"].append(entry["benchmark-data"]["scoreConfidence"][0])
+        result["max-score"].append(entry["benchmark-data"]["scoreConfidence"][1])
+        result["score-unit"].append(entry["benchmark-data"]["scoreUnit"])
+        result["dar#script"].append("{}#{}".format(entry["dar-file"], entry["script"]))
+    for entry in update_data:
+        result["type"].append("update")
+        result["choice"].append(entry["choice"])
+        result["score"].append(entry["benchmark-data"]["score"])
+        result["min-score"].append(entry["benchmark-data"]["scoreConfidence"][0])
+        result["max-score"].append(entry["benchmark-data"]["scoreConfidence"][1])
+        result["score-unit"].append(entry["benchmark-data"]["scoreUnit"])
+        result["dar#script"].append("{}#{}".format(entry["dar-file"], entry["script"]))
+    return pd.DataFrame(result)
+
+def display_graph(graph_data):
+    return px.bar(graph_data, x="choice", y="score", color="type", barmode="group", facet_row="dar#script", facet_col="choice")
+
+def main():
+    parser = argparse.ArgumentParser(description="Display JMH Benchmarking Data for Snapshot Files")
+    parser.add_argument("base_dir", type=str, help="Directory holding baseline snapshot data")
+    parser.add_argument("update_dir", type=str, help="Directory holding updated snapshot data")
+    args = parser.parse_args()
+    base_data = get_jmh_data(args.base_dir)
+    update_data = get_jmh_data(args.update_dir)
+    graph_data = get_graph_data(base_data, update_data)
+    fig = display_graph(graph_data)
+    fig.show()
+
+if __name__ == "__main__":
+    main()
+
