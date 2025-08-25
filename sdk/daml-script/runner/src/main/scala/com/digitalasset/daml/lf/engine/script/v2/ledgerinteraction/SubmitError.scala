afbc3566461f6079040f150d4bcf6752a03db777
Paul Brauner <paul.brauner@digitalasset.com>
2025-08-25 19:08:27 +0200

    fix daml script upgrade tests

--- sdk/daml-script/runner/src/main/scala/com/digitalasset/daml/lf/engine/script/v2/ledgerinteraction/SubmitError.scala
+++ sdk/daml-script/runner/src/main/scala/com/digitalasset/daml/lf/engine/script/v2/ledgerinteraction/SubmitError.scala
@@ -612,6 +612,8 @@ object SubmitError {
           SEnum(devErrorTypeIdentifier, Name.assertFromString("ChoiceGuardFailed"), 0)
         case "WronglyTypedContractSoft" =>
           SEnum(devErrorTypeIdentifier, Name.assertFromString("WronglyTypedContractSoft"), 1)
+        case "TranslationError" =>
+          SEnum(devErrorTypeIdentifier, Name.assertFromString("TranslationError"), 2)
         case _ => SEnum(devErrorTypeIdentifier, Name.assertFromString("UnknownNewFeature"), 3)
       }
       SubmitErrorConverters(env).damlScriptError(
