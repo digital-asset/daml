c07b7ce5c367d94269cd39def278f7a460b103a1
Carl Pulley <carl.pulley@digitalasset.com>
2025-08-29 11:50:06 +0100

    Add step and tx counts to simple benchmarks

--- sdk/daml-lf/interpreter/bench/src/scala/com/digitalasset/daml/lf/speedy/Bench.scala
+++ sdk/daml-lf/interpreter/bench/src/scala/com/digitalasset/daml/lf/speedy/Bench.scala
@@ -155,14 +155,36 @@ class Bench {
   }
 
   @Benchmark
-  def bench(): SValue = {
+  def bench(counters: Bench.EventCounter): SValue = {
+    counters.reset()
     machine.setExpressionToEvaluate(sexpr)
     machine.run() match {
       case SResult.SResultFinal(v) =>
+        counters.update(machine.metrics)
         v
       case otherwise =>
         throw new UnknownError(otherwise.toString)
     }
   }
 
+}
+
+object Bench {
+  @State(Scope.Thread)
+  @AuxCounters(AuxCounters.Type.EVENTS)
+  class EventCounter {
+    var stepCount: Long = 0
+    var transactionNodeCount: Long = 0
+
+    def reset(): Unit = {
+      stepCount = 0
+      transactionNodeCount = 0
+    }
+
+    def update(metrics: Speedy.Metrics): Unit = {
+      val (stepBatchCount, stepsCounted) = metrics.totalStepCount
+      stepCount += stepBatchCount * metrics.batchSize + stepsCounted
+      transactionNodeCount += metrics.transactionNodeCount
+    }
+  }
 }
