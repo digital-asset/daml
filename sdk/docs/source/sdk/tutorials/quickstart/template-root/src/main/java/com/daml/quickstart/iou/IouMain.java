245f803bff60bf8d72344b72021647d58679f47e
Gergely Homanyi <gergely.homanyi@digitalasset.com>
2025-08-29 16:50:56 +0200

    fix

--- sdk/docs/source/sdk/tutorials/quickstart/template-root/src/main/java/com/daml/quickstart/iou/IouMain.java
+++ sdk/docs/source/sdk/tutorials/quickstart/template-root/src/main/java/com/daml/quickstart/iou/IouMain.java
@@ -5,14 +5,20 @@ package com.daml.quickstart.iou;
 
 import static java.util.UUID.randomUUID;
 
+import com.daml.ledger.api.v2.*;
 import com.daml.ledger.javaapi.data.*;
+import com.daml.ledger.javaapi.data.codegen.Choice;
+import com.daml.ledger.javaapi.data.codegen.Created;
+import com.daml.ledger.javaapi.data.codegen.Exercised;
 import com.daml.ledger.javaapi.data.codegen.Update;
 import com.daml.quickstart.model.iou.Iou;
+import com.google.common.base.Function;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
-import io.reactivex.disposables.Disposable;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
@@ -44,29 +50,37 @@ public class IouMain {
     BiMap<Long, Iou.ContractId> idMap = Maps.synchronizedBiMap(HashBiMap.create());
 
     // Connect to gRPC services
-    Channel channel = NettyChannelBuilder
-            .forAddress(ledgerhost, ledgerport)
+    Channel channel =
+        ManagedChannelBuilder.forAddress(ledgerhost, ledgerport)
             .maxInboundMessageSize(10485760)
             .usePlaintext()
             .build();
 
-    StateServiceGrpc.StateServiceBlockingStub stateServiceBlockingStub = StateServiceGrpc.newBlockingStub(channel);
+    StateServiceGrpc.StateServiceBlockingStub stateServiceBlockingStub =
+        StateServiceGrpc.newBlockingStub(channel);
     long ledgerEnd =
-            stateServiceBlockingStub.getLedgerEnd(StateServiceOuterClass.GetLedgerEndRequest.newBuilder().build()).getOffset();
+        stateServiceBlockingStub
+            .getLedgerEnd(StateServiceOuterClass.GetLedgerEndRequest.newBuilder().build())
+            .getOffset();
 
     var contractFilter = Iou.contractFilter();
     var eventFormat = contractFilter.eventFormat(partyFilter);
     GetActiveContractsRequest request = new GetActiveContractsRequest(eventFormat, ledgerEnd);
     Iterator<StateServiceOuterClass.GetActiveContractsResponse> activeContracts =
-            stateServiceBlockingStub.getActiveContracts(request.toProto());
+        stateServiceBlockingStub.getActiveContracts(request.toProto());
 
-    activeContracts.forEachRemaining(r -> {
-      GetActiveContractsResponse response = GetActiveContractsResponse.fromProto(r);
-      long id = idCounter.getAndIncrement();
-      var contract = contractFilter.toContract(response.getContractEntry().get().getCreatedEvent());
-      contracts.put(id, contract.data);
-      idMap.put(id, contract.id);
-    });
+    activeContracts.forEachRemaining(
+        r -> {
+          GetActiveContractsResponse response = GetActiveContractsResponse.fromProto(r);
+          long id = idCounter.getAndIncrement();
+          var contract =
+              contractFilter.toContract(response.getContractEntry().get().getCreatedEvent());
+          contracts.put(id, contract.data);
+          idMap.put(id, contract.id);
+        });
+
+    CommandServiceGrpc.CommandServiceBlockingStub commandService =
+        CommandServiceGrpc.newBlockingStub(channel);
 
     Gson g = new Gson();
     Spark.port(restport);
@@ -80,7 +94,8 @@ public class IouMain {
         (req, res) -> {
           Iou iou = g.fromJson(req.body(), Iou.class);
           var iouCreate = iou.create();
-          var createdContractId = submitCreate(commandService, party, iouCreate, Iou.ContractId::new);
+          var createdContractId =
+              submitCreate(commandService, party, iouCreate, Iou.ContractId::new);
           return "Iou creation submitted: " + createdContractId;
         },
         g::toJson);
@@ -105,34 +120,44 @@ public class IouMain {
       }
   }
 
-  private static <T> Created<T> submitCreate(CommandServiceGrpc.CommandServiceBlockingStub commandService,
-                                             String party,
-                                             Update<Created<T>> update, Function<String, T> convertContractId) {
-    var updateSubmission = UpdateSubmission.create(APP_ID, randomUUID().toString(), update).withActAs(party);
+  private static <T> Created<T> submitCreate(
+      CommandServiceGrpc.CommandServiceBlockingStub commandService,
+      String party,
+      Update<Created<T>> update,
+      Function<String, T> convertContractId) {
+    var updateSubmission =
+        UpdateSubmission.create(APP_ID, randomUUID().toString(), update).withActAs(party);
     var request = new SubmitAndWaitForTransactionRequest(updateSubmission.toCommandsSubmission());
     SubmitAndWaitForTransactionResponse response =
-            SubmitAndWaitForTransactionResponse.fromProto(commandService.submitAndWaitForTransaction(request.toProto()));
+        SubmitAndWaitForTransactionResponse.fromProto(
+            commandService.submitAndWaitForTransaction(request.toProto()));
     Event event = EventUtils.singleCreatedEvent(response.getTransaction().getEvents());
     return Created.fromEvent(convertContractId, (CreatedEvent) event);
   }
 
-  private static <T> Exercised<T> submitExercise(CommandServiceGrpc.CommandServiceBlockingStub commandService,
-                                                 String party, Update<Exercised<T>> update,
-                                                 Choice<?, ?, T> choice) {
-    var updateSubmission = UpdateSubmission.create(APP_ID, randomUUID().toString(), update).withActAs(party);
+  private static <T> Exercised<T> submitExercise(
+      CommandServiceGrpc.CommandServiceBlockingStub commandService,
+      String party,
+      Update<Exercised<T>> update,
+      Choice<?, ?, T> choice) {
+    var updateSubmission =
+        UpdateSubmission.create(APP_ID, randomUUID().toString(), update).withActAs(party);
 
     var contractFilter = Iou.contractFilter();
     var eventFormar = contractFilter.eventFormat(Optional.of(Collections.singleton(party)));
     var format = new TransactionFormat(eventFormar, TransactionShape.LEDGER_EFFECTS);
-    var request = new SubmitAndWaitForTransactionRequest(updateSubmission.toCommandsSubmission(), format);
+    var request =
+        new SubmitAndWaitForTransactionRequest(updateSubmission.toCommandsSubmission(), format);
     SubmitAndWaitForTransactionResponse response =
-            SubmitAndWaitForTransactionResponse.fromProto(commandService.submitAndWaitForTransaction(request.toProto()));
+        SubmitAndWaitForTransactionResponse.fromProto(
+            commandService.submitAndWaitForTransaction(request.toProto()));
     Transaction transaction = response.getTransaction();
-    Optional<Event> exercisedOpt = transaction.getEvents().stream().filter(e -> e instanceof ExercisedEvent).findFirst();
+    Optional<Event> exercisedOpt =
+        transaction.getEvents().stream().filter(e -> e instanceof ExercisedEvent).findFirst();
     if (exercisedOpt.isEmpty()) {
       throw new RuntimeException("No exercised event found in transaction");
     }
-    ExercisedEvent event = (ExercisedEvent)exercisedOpt.get();
+    ExercisedEvent event = (ExercisedEvent) exercisedOpt.get();
     return Exercised.fromEvent(choice.returnTypeDecoder, event);
   }
 }
