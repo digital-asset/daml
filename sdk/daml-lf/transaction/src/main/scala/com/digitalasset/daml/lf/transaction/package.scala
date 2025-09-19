5930b1e295084f72426dc2072e8592d8c513d1a8
Remy Haemmerle <Remy.Haemmerle@daml.com>
2025-09-19 13:52:28 +0200

    [Engine] generic proto validator that reject message for unknown fields

--- sdk/daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/package.scala
+++ sdk/daml-lf/transaction/src/main/scala/com/digitalasset/daml/lf/transaction/package.scala
@@ -3,6 +3,10 @@
 
 package com.digitalasset.daml.lf
 
+import com.daml.SafeProto
+import com.digitalasset.daml.lf.value.ValueCoder
+import com.google.protobuf
+
 import scala.collection.BuildFrom
 
 package object transaction {
@@ -33,4 +37,15 @@ package object transaction {
   type CommittedTransaction = CommittedTransaction.T
 
   type VersionedGlobalKey = Versioned[GlobalKey]
+
+  private[lf] def ensuresNoUnknownFields(message: protobuf.Message) =
+    SafeProto.ensureNoUnknownFields(message).left.map(ValueCoder.DecodeError)
+
+  private[lf] def ensuresNoUnknownFieldsThenDecode[M <: protobuf.Message, A](
+      msg: M
+  )(decode: M => Either[ValueCoder.DecodeError, A]): Either[ValueCoder.DecodeError, A] =
+    for {
+      _ <- ensuresNoUnknownFields(msg)
+      a <- decode(msg)
+    } yield a
 }
