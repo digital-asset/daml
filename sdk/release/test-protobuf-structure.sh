47992a17d421279e9a5499a09c63363db0d3f9a9
Remy Haemmerle <Remy.Haemmerle@daml.com>
2025-10-07 11:37:44 +0200

    fix

--- sdk/release/test-protobuf-structure.sh
+++ sdk/release/test-protobuf-structure.sh
@@ -33,8 +33,6 @@ SORT=$3
 export LANG=C
 
 $DIFF -u <(unzip -Z1 $PROTOS | $SORT | sed 's|^[^/]*/||g') <((cat <<EOF
-com/daml/daml_lf_2_1/daml_lf.proto
-com/daml/daml_lf_2_1/daml_lf2.proto
 com/daml/ledger/api/v2/trace_context.proto
 com/daml/ledger/api/v2/testing/time_service.proto
 com/daml/ledger/api/v2/topology_transaction.proto
