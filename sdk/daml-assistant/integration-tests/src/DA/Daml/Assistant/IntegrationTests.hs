8c9349e03d342fb14659bc13fb4debf596fa6a86
Paul Brauner <141240651+paulbrauner-da@users.noreply.github.com>
2025-10-09 10:40:27 +0200

    Try and fix flaky behavior in tests calling 'canton sandbox' (#22068)
    Canton now starts the JSON API by default, so if we don't pick a free
    port for it, it might create collisions between tests.

--- sdk/daml-assistant/integration-tests/src/DA/Daml/Assistant/IntegrationTests.hs
+++ sdk/daml-assistant/integration-tests/src/DA/Daml/Assistant/IntegrationTests.hs
@@ -627,6 +627,7 @@ cantonTests = testGroup "daml sandbox"
         sequencerPublicApiPort <- getFreePort
         sequencerAdminApiPort <- getFreePort
         mediatorAdminApiPort <- getFreePort
+        jsonApiPort <- getFreePort
         step "Staring Canton sandbox"
         let portFile = dir </> "canton-portfile.json"
         withDamlServiceIn (dir </> "skeleton") "sandbox"
@@ -635,6 +636,7 @@ cantonTests = testGroup "daml sandbox"
             , "--sequencer-public-port", show sequencerPublicApiPort
             , "--sequencer-admin-port", show sequencerAdminApiPort
             , "--mediator-admin-port", show mediatorAdminApiPort
+            , "--json-api-port", show jsonApiPort
             , "--canton-port-file", portFile
             ] $ \ ph -> do
             -- wait for port file to be written
