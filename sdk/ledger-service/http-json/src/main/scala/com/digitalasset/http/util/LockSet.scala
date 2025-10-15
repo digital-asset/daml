30dc62fe4ec4c23bf1eff6abf1f2dd27983e42cc
Raphael Speyer <raphael.speyer@digitalasset.com>
2025-10-15 10:09:54 +0200

    update to work properly with args that are futures

--- sdk/ledger-service/http-json/src/main/scala/com/digitalasset/http/util/LockSet.scala
+++ sdk/ledger-service/http-json/src/main/scala/com/digitalasset/http/util/LockSet.scala
@@ -2,29 +2,34 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package com.daml.http.util
+
 import scala.collection.concurrent.TrieMap
-import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.Semaphore
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.{ExecutionContext, Future}
 
-/**
- * Utility for sharing a set of locks to be shared across threads. Each lock is identified by a key.
- *
- * Assumes that in practice there is a bound on the number of keys used, which can fit in memory.
- */
+/** Utility for sharing a set of locks across threads. Each lock is identified by a key.
+  *
+  * Assumes that in practice there is a bound on the number of keys used, which can fit in memory.
+  */
 class LockSet[K]()(implicit ord: Ordering[K]) {
-  val locks = new TrieMap[K, ReentrantLock]
+  val locks = new TrieMap[K, Semaphore]
 
-  def withLocksOn[A](keys: Iterable[K])(block: => A): A = {
-    val acquired = new ListBuffer[ReentrantLock]()
-    try {
-      keys.toSet.toSeq.sorted.foreach { key => 
-        val lock = locks.getOrElseUpdate(key, new ReentrantLock())
-        lock.lock() 
+  def withLocksOn[A](
+      keys: Iterable[K]
+  )(block: => Future[A])(implicit ec: ExecutionContext): Future[A] = {
+    val acquired = new ListBuffer[Semaphore]()
+    Future {
+      keys.toSet.toSeq.sorted.foreach { key =>
+        val lock = locks.getOrElseUpdate(key, new Semaphore(1))
+        lock.acquire()
         acquired += lock
       }
-      block
-    } finally {
-      acquired.reverse.foreach(_.unlock())
     }
+      .flatMap { _ => block }
+      .transform { result =>
+        acquired.reverse.foreach(_.release())
+        result
+      }
   }
 }
