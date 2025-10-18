80a188764545c6f7c1d4780359b11db640c856d8
Remy Haemmerle <Remy.Haemmerle@daml.com>
2025-10-18 17:41:22 +0200

    fix

--- sdk/daml-lf/archive/src/test/scala/com/digitalasset/daml/lf/archive/UniversalArchiveReaderSpec.scala
+++ sdk/daml-lf/archive/src/test/scala/com/digitalasset/daml/lf/archive/UniversalArchiveReaderSpec.scala
@@ -10,6 +10,7 @@ import org.scalatest.TryValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.flatspec.AnyFlatSpec
 
+@scala.annotation.nowarn("msg=deprecated")
 class UniversalArchiveReaderSpec extends AnyFlatSpec with Matchers with TryValues {
 
   private val darFile = new File(rlocation("daml-lf/archive/DarReaderTest.dar"))
