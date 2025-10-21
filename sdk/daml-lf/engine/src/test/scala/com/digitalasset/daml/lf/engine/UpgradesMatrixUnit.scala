be9b53abe170f029ce471e47f712f8b0e23bad97
Paul Brauner <141240651+paulbrauner-da@users.noreply.github.com>
2025-10-21 17:32:47 +0200

    [fwdport] Run matrix tests on both 2.2 and 2.dev (#22195)
    This is a forward port of #22165
    
    When the target language version passed to UpdateMatrix does not support
    keys, then don't generate template with keys or choices that use by_key
    operations. Also disable the key-related use cases.
    
    Run the upgrade matrix test on both v2.2 and v2.dev.

--- sdk/daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/UpgradesMatrixUnit.scala
+++ sdk/daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/UpgradesMatrixUnit.scala
@@ -24,8 +24,8 @@ import scala.concurrent.Future
 
 // Split the Upgrade unit tests over two suites, which seems to be the sweet
 // spot (~25s instead of ~35s runtime)
-class UpgradesMatrixUnit0 extends UpgradesMatrixUnit(2, 0)
-class UpgradesMatrixUnit1 extends UpgradesMatrixUnit(2, 1)
+class UpgradesMatrixUnit0 extends UpgradesMatrixUnit(UpgradesMatrixCasesV2MaxStable, 1, 0)
+class UpgradesMatrixUnit1 extends UpgradesMatrixUnit(UpgradesMatrixCasesV2Dev, 1, 0)
 
 /** A test suite to run the UpgradesMatrix matrix directly in the engine
   *
@@ -33,10 +33,10 @@ class UpgradesMatrixUnit1 extends UpgradesMatrixUnit(2, 1)
   * (~5000s) because it does not need to spin up Canton, so we can use this for
   * sanity checking before running UpgradesMatrixIT.
   */
-abstract class UpgradesMatrixUnit(n: Int, k: Int)
+abstract class UpgradesMatrixUnit(upgradesMatrixCases: UpgradesMatrixCases, n: Int, k: Int)
     extends UpgradesMatrix[Error, (SubmittedTransaction, Transaction.Metadata)](
       UpgradesMatrix.IdeLedger,
-      UpgradesMatrixCasesV2MaxStable,
+      upgradesMatrixCases,
       Some((n, k)),
     )
     with ParallelTestExecution {
@@ -104,7 +104,7 @@ abstract class UpgradesMatrixUnit(n: Int, k: Int)
       ),
       signatories = immutable.TreeSet(setupData.alice),
       stakeholders = immutable.TreeSet(setupData.alice),
-      contractKeyWithMaintainers = Some(testHelper.globalContractKeyWithMaintainers(setupData)),
+      contractKeyWithMaintainers = testHelper.globalContractKeyWithMaintainers(setupData),
       createdAt = CreationTime.CreatedAt(Time.Timestamp.Epoch),
       authenticationData = Bytes.assertFromString("00"),
     )
@@ -129,12 +129,14 @@ abstract class UpgradesMatrixUnit(n: Int, k: Int)
     }
     val lookupContractByKey = contractOrigin match {
       case UpgradesMatrixCases.Global | UpgradesMatrixCases.Disclosed =>
-        val keyMap = Map(
-          testHelper
-            .globalContractKeyWithMaintainers(setupData)
-            .globalKey -> setupData.globalContractId
-        )
-        ((kwm: GlobalKeyWithMaintainers) => keyMap.get(kwm.globalKey)).unlift
+        (
+            (kwm: GlobalKeyWithMaintainers) =>
+              testHelper
+                .globalContractKeyWithMaintainers(setupData)
+                .flatMap(helperKey =>
+                  Option.when(helperKey.globalKey == kwm.globalKey)(setupData.globalContractId)
+                )
+        ).unlift
       case _ => PartialFunction.empty
     }
 
