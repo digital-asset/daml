9ab4da59ebff5db283d1de275fb5b0d573693e96
Raphael Speyer <raphael.speyer@digitalasset.com>
2025-10-22 11:59:51 +0200

    JSON API: add lock acquisition timeout to `LockSet` (#22180)
    Also an `opName` for better logging.
    
    Adding config and passing it in from `ContractsService` is a separate
    PR.

--- sdk/ledger-service/http-json/src/main/scala/com/digitalasset/http/util/LockSet.scala
+++ sdk/ledger-service/http-json/src/main/scala/com/digitalasset/http/util/LockSet.scala
@@ -4,10 +4,11 @@
 package com.daml.http.util
 
 import com.daml.logging.{ContextualizedLogger, LoggingContext}
-import java.util.concurrent.{Executors, Semaphore}
+import java.util.concurrent.{Executors, Semaphore, TimeoutException, TimeUnit}
 import scala.collection.concurrent.TrieMap
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ExecutionContext, Future, blocking}
+import scala.concurrent.duration.{Deadline, FiniteDuration, MINUTES}
 
 /** Utility for sharing a set of locks across threads. Each lock is identified by a key.
   *
@@ -20,39 +21,63 @@ class LockSet[K](logger: ContextualizedLogger)(implicit ord: Ordering[K]) {
     ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 
   def withLocksOn[A](
-      keysItr: Iterable[K]
+      keysItr: Iterable[K],
+      acquireTimeout: FiniteDuration = FiniteDuration(1, MINUTES),
+      opName: String = "",
   )(block: => Future[A])(implicit callerEc: ExecutionContext, lc: LoggingContext): Future[A] = {
     val keys = keysItr.toSet.toSeq.sorted
     val acquired = new ListBuffer[K]()
 
     // Use our own EC for this, to avoid thread starvation on the callers EC.
     val acquireAllLocks = Future {
-      logger.debug(s"Attempting to acquire locks for keys: $keys")
-      keys.foreach { key =>
-        acquireLock(key)
-        acquired += key
-      }
-      logger.debug(s"Acquired locks for keys: $keys")
+      val deadline = acquireTimeout.fromNow // Timeout starts ticking when the future is scheduled.
+      logger.debug(
+        s"$opName: Attempting to acquire locks for keys: $keys, by ${formatDeadline(deadline)}"
+      )
+      keys.foreach { key => if (acquireLock(opName, key, deadline)) acquired += key }
+      logger.debug(s"$opName: Acquired locks for keys: $acquired")
+      acquired.size == keys.size
     }(acquisitionEc)
 
     acquireAllLocks
-      .flatMap { _ => block }
+      .flatMap { success =>
+        if (success) block
+        else Future.failed(new TimeoutException(s"Acquiring locks on $keys within $acquireTimeout"))
+      }
       .transform { result =>
-        acquired.reverse.foreach(releaseLock(_))
-        logger.debug(s"Released locks for keys: $keys")
+        acquired.reverse.foreach(releaseLock(opName, _))
+        logger.debug(s"$opName: Released locks for keys: $acquired")
         result
       }(callerEc)
   }
 
-  private def acquireLock(key: K)(implicit lc: LoggingContext): Unit = {
-    val lock = locks.getOrElseUpdate(key, new Semaphore(1))
-    logger.debug(s"Attempting to acquire lock for $key")
-    blocking { lock.acquire() }
-    logger.debug(s"Acquired lock for $key")
+  private def acquireLock(opName: String, key: K, deadline: Deadline)(implicit
+      lc: LoggingContext
+  ): Boolean =
+    if (deadline.isOverdue()) {
+      logger.debug(s"$opName: Not attempting to acquire lock for $key, as deadline has passed")
+      false
+    } else {
+      val lock = locks.getOrElseUpdate(key, new Semaphore(1))
+      logger.debug(s"$opName: Attempting to acquire lock for $key")
+      val acquired = blocking { lock.tryAcquire(deadline.timeLeft.toMillis, TimeUnit.MILLISECONDS) }
+      if (acquired) logger.debug(s"$opName: Acquired lock for $key")
+      else logger.debug(s"$opName: Failed to acquire lock for $key before deadline")
+      acquired
+    }
+
+  private def releaseLock(opName: String, key: K)(implicit lc: LoggingContext): Unit = {
+    locks(key).release() // Lock for this key was added by acquireLock
+    logger.debug(s"$opName: Released lock for $key")
   }
 
-  private def releaseLock(key: K)(implicit lc: LoggingContext): Unit = {
-    locks(key).release() // Lock for this key was added by acquireLock
-    logger.debug(s"Released lock for $key")
+  private def formatDeadline(deadline: Deadline): String = {
+    import java.time.{Instant, Duration => JavaDuration}
+    import java.time.temporal.ChronoUnit
+    Instant
+      .now()
+      .plus(JavaDuration.ofNanos(deadline.timeLeft.toNanos))
+      .truncatedTo(ChronoUnit.MILLIS) // Nanos would be over-precise.
+      .toString()
   }
 }
