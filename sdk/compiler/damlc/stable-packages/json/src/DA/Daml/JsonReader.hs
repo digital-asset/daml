c32b30dd2cb6e40880b0fb8535c4cd218537e880
Roger Bosman <roger.bosman@digitalasset.com>
2025-10-23 17:29:01 +0200

    Initial implementation
    Initial impl

---
+++ sdk/compiler/damlc/stable-packages/json/src/DA/Daml/JsonReader.hs
@@ -0,0 +1,41 @@
+-- Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+{- HLINT ignore "Avoid restricted extensions" -}
+{-# LANGUAGE CPP             #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module DA.Daml.JsonReader (
+  --   allStableIds
+  -- , stablePackagesUntilVersion
+  module DA.Daml.JsonReader  --note to reviewer: if we still export entire module I simply forgot to delete after debug
+  ) where
+
+import qualified Data.Aeson           as Aeson
+import           Data.Bifunctor
+import           Data.ByteString      (ByteString)
+import           Data.FileEmbed       (embedFile)
+import qualified Data.Text            as T
+
+import           DA.Daml.LF.Ast
+import qualified DA.Daml.LF.Ast.Range as R
+
+jsonData :: ByteString
+jsonData = $(embedFile JSON_FILE_PATH)
+
+decoded :: Either String [(Version, T.Text)]
+decoded = Aeson.eitherDecodeStrict jsonData
+
+-- | Use VersionReq instead of Version here, to anticipate stable packges with a
+-- max version
+entries :: [(VersionReq, PackageId)]
+entries = case decoded of
+  Left errMsg
+    -> error $ "Failed to decode: " ++ errMsg
+  Right parsedEntries -> map (bimap R.From PackageId) parsedEntries
+
+allStableIds :: [PackageId]
+allStableIds = map snd entries
+
+stablePackagesUntilVersion :: Version -> [PackageId]
+stablePackagesUntilVersion v = map snd $ filter (R.elem v . fst) entries
---
+++ sdk/compiler/damlc/stable-packages/json/src/DA/Daml/JsonWriter.hs
@@ -0,0 +1,25 @@
+-- Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+module Main(main) where
+
+import qualified Data.Aeson           as Aeson
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Text            as T
+
+import           DA.Daml.LF.Ast
+import           DA.Daml.StablePackages
+
+entries :: [(Version, T.Text)]
+entries = map toEntry allV2StablePackagesTuples
+  where
+    toEntry (id, pkg) = (packageLfVersion pkg, unPackageId id)
+
+outputPath :: FilePath
+outputPath = "compiler/damlc/stable-packages/json/stable-packages.json"
+
+main :: IO ()
+main = do
+  putStrLn $ "Generating fresh json data for: " ++ outputPath
+  LBS.writeFile outputPath (Aeson.encode entries)
+  putStrLn "Successfully wrote dummy stable-packages.json."
