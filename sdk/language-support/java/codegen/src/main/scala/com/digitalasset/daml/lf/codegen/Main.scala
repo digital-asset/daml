ee4639ea41c04d1d96b1469e4a4a91eed7569d12
Adrien Piquerez <adrien.piquerez@digitalasset.com>
2025-10-24 15:03:57 +0200

    minor improvements

--- sdk/language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/Main.scala
+++ sdk/language-support/java/codegen/src/main/scala/com/digitalasset/daml/lf/codegen/Main.scala
@@ -10,7 +10,7 @@ import scala.util.control.NonFatal
 
 object StandaloneMain extends StrictLogging {
 
-  @deprecated("Use codegen font-end: com.daml.codegen.CodegenMain.main", "0.13.23")
+  @deprecated("Use codegen front-end: com.daml.codegen.CodegenMain.main", "0.13.23")
   def main(args: Array[String]): Unit =
     try {
       Main.main(args)
@@ -22,7 +22,7 @@ object StandaloneMain extends StrictLogging {
 }
 
 object Main {
-  @deprecated("Use codegen font-end: com.daml.codegen.CodegenMain.main", "0.13.23")
+  @deprecated("Use codegen front-end: com.daml.codegen.CodegenMain.main", "0.13.23")
   def main(args: Array[String]): Unit =
     Conf.parse(args) match {
       case Some(conf) => CodeGenRunner.run(conf)
