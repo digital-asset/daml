b7fe7ad9cd44ab3c2464fd88986304688ab78147
Roger Bosman <roger.bosman@digitalasset.com>
2025-11-13 16:34:32 +0100

    fix throwable type

--- sdk/canton/community/ledger/ledger-common/src/main/scala/com/digitalasset/canton/ledger/error/PackageServiceErrors.scala
+++ sdk/canton/community/ledger/ledger-common/src/main/scala/com/digitalasset/canton/ledger/error/PackageServiceErrors.scala
@@ -129,12 +129,12 @@ object PackageServiceErrors extends PackageServiceErrorGroup {
     @Resolution("Inspect the error message and contact support.")
     object ParseError
         extends ErrorCode(id = "DAR_PARSE_ERROR", ErrorCategory.InvalidIndependentOfSystemState) {
-      final case class Error(reason: String, throwableO: Option[Throwable] = None)(implicit
+      final case class Error(reason: String, throwable: Option[Throwable] = None)(implicit
           val loggingContext: ErrorLoggingContext
       ) extends ContextualizedDamlError(
             cause = "Failed to parse the dar file content.",
             extraContext = Map("cause" -> reason),
-            throwableO = throwableO
+            throwableO = throwable
           )
     }
 
@@ -202,7 +202,7 @@ object PackageServiceErrors extends PackageServiceErrorGroup {
         case LfArchiveError.ZipBomb =>
           PackageServiceErrors.Reading.ZipBomb.Error(LfArchiveError.ZipBomb.getMessage)
         case e: LfArchiveError =>
-          PackageServiceErrors.Reading.ParseError.Error(e.msg, throwable0 = e)
+          PackageServiceErrors.Reading.ParseError.Error(e.msg, throwable = Some(e))
         case e =>
           PackageServiceErrors.InternalError.Unhandled(e)
       }
