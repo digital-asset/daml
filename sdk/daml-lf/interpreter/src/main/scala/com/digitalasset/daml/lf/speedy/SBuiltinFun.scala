1e40d9c3375e89d94d2dbb71fda3310bea5f5d0f
Carl Pulley <carl.pulley@digitalasset.com>
2025-12-09 16:01:23 +0000

    Address review comments

--- sdk/daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/SBuiltinFun.scala
+++ sdk/daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/SBuiltinFun.scala
@@ -762,10 +762,6 @@ private[lf] object SBuiltinFun {
 
         result.fold(Control.Error, Control.Value)
       } catch {
-        case _: NoSuchProviderException =>
-          crash("JCE Provider BouncyCastle not found")
-        case _: NoSuchAlgorithmException =>
-          crash("BouncyCastle provider fails to support SECP256K1")
         case _: IllegalArgumentException =>
           Control.Error(
             IE.Crypto(
@@ -793,6 +789,10 @@ private[lf] object SBuiltinFun {
               IE.Crypto.MalformedSignature(getSText(args, 0), exn.getMessage)
             )
           )
+        case _: NoSuchProviderException =>
+          crash("JCE Provider BouncyCastle not found")
+        case _: NoSuchAlgorithmException =>
+          crash("BouncyCastle provider fails to support SECP256K1")
       }
     }
 
