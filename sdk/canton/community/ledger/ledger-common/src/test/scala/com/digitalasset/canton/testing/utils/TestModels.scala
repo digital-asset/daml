2c63159ce3339eb9988c0faab997273acde081a8
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2026-01-07 14:40:49 +0000

    //daml-lf/encoder

--- sdk/canton/community/ledger/ledger-common/src/test/scala/com/digitalasset/canton/testing/utils/TestModels.scala
+++ sdk/canton/community/ledger/ledger-common/src/test/scala/com/digitalasset/canton/testing/utils/TestModels.scala
@@ -7,7 +7,7 @@ object TestModels {
   val com_daml_ledger_test_ModelTestDar_path = "model-tests-3.1.0.dar"
   val com_daml_ledger_test_SemanticTestDar_path = "semantic-tests-3.1.0.dar"
 
-  // TODO(#12303) Dar files generated in Daml SDK with `bazel build //daml-lf/encoder:testing-dar-<lf-version>`
+  // TODO(#12303) Dar files generated in Daml SDK with `bazel build //canton/community/daml-lf/encoder:testing-dar-<lf-version>`
   //              and copied from `bazel-bin/daml-lf/encoder/test-<lf-version>.dar`.
   def daml_lf_encoder_test_dar(lfVersion: String): String =
     s"test-models/daml-lf/encoder/test-$lfVersion.dar"
