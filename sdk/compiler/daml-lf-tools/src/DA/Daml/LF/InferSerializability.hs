ff72539479656062ef9e1fc494f896ffc91ff708
Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>
2026-01-27 17:04:46 +0100

    Revert some InferSerializability changes

--- sdk/compiler/daml-lf-tools/src/DA/Daml/LF/InferSerializability.hs
+++ sdk/compiler/daml-lf-tools/src/DA/Daml/LF/InferSerializability.hs
@@ -23,7 +23,7 @@ data Options = Options
   } deriving (Show)
 
 inferModule :: World -> Options -> Module -> Either String Module
-inferModule world0 opts@Options {..} mod0 =
+inferModule world0 Options {..} mod0 =
   case moduleName mod0 of
     -- Unstable parts of stdlib mustn't contain serializable types, because if they are 
     -- serializable, then the upgrading checks run on the datatypes and this causes problems. 
@@ -58,40 +58,14 @@ inferModule world0 opts@Options {..} mod0 =
       let eqs =
             [ (dataTypeCon dataType, serializable, deps)
             | dataType <- NM.toList dataTypes
-            , let (serializable, deps) = inferDataType
-                    world0 opts (CurrentModule modName interfaces) dataType
+            , let (serializable, deps) =
+                    case serializabilityConditionsDataType world0 (Just $ CurrentModule modName interfaces) dataType of
+                      Left _ -> (False, [])
+                      Right deps0 -> (True, HS.toList deps0)
             ]
-      -- TODO(jaspervdj): we probably can get rid of this leastFixedPointBy if
-      -- we're doing explicit serializability.  The issue is that this is
-      -- marking dependents as False, so they don't get checked at all.
-      -- We want to be explicit about things.
       case leastFixedPointBy (&&) eqs of
         Left name -> throwError ("Reference to unknown data type: " ++ show name)
         Right serializabilities -> do
           let updateDataType dataType =
-                let serializable = IsSerializable (HMS.lookupDefault False (dataTypeCon dataType) serializabilities) in
-                dataType {dataSerializable = serializable}
+                dataType{dataSerializable = IsSerializable (HMS.lookupDefault False (dataTypeCon dataType) serializabilities)}
           pure mod0{moduleDataTypes = NM.map updateDataType dataTypes}
-
-inferDataType
-    :: World -> Options -> CurrentModule -> DefDataType -> (Bool, [TypeConName])
-inferDataType world0 Options {..} currentModule dataType =
-  case serializabilityConditionsDataType world0 (Just currentModule) dataType of
-    -- NOTE(jaspervdj): All the ExplicitSerializable stuff below is dead,
-    -- revert when I create the PR.
-    --
-    -- NOTE(jaspervdj): ExplicitSerializable: the LFConversion will only set
-    -- IsSerializable if the datatype has an explicit Serializable instance.
-    --
-    -- If ExplicitSerializable is on and this type is not marked as
-    -- serializable, always return false.
-    _ | oExplicitSerializable && not (getIsSerializable (dataSerializable dataType)) ->
-        (False, [])
-    -- If ExplicitSerializable is on, and this datatype is has a Serializable
-    -- instance, but we get a Left, that means this datatype inherently isn't
-    -- serializable (e.g. it contains functions).  We could return an error
-    -- here; but currently we mark it as serializable and let the checker reject
-    -- it later.
-    Left _ | oExplicitSerializable && getIsSerializable (dataSerializable dataType) -> (True, [])
-    Left _ -> (False, [])
-    Right deps0 -> (True, HS.toList deps0)
