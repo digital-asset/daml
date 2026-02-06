499b7e291619a07081458df3f294d6c61b5ea4f7
Paul Brauner <141240651+paulbrauner-da@users.noreply.github.com>
2026-02-06 18:06:58 +0100

    Delete pretty much everything in sdk/canton (#22692)
    Also update refresh-canton.sh to no longer copy code from canton.
    
    I had to disable some codegen tests and benchmarks, which I marked with
    a TODO pointing to https://github.com/DACH-NY/canton/issues/30666. They
    will be re-enabled after the move.

--- sdk/canton/community/common/src/main/resources/db/migration/canton/postgres/dev/V999__dev.sql
+++
@@ -1,6 +0,0 @@
--- Copyright (c) 2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
--- SPDX-License-Identifier: Apache-2.0
-
--- This is a dummy column we are adding in order to test that adding dev version migrations
--- works properly. DO NOT MOVE THIS TO STABLE
-alter table common_node_id add column test_column int not null default 0;
--- sdk/canton/community/transcode/codec-json/src/main/scala/com/digitalasset/transcode/IncorrectVariantRepresentationException.scala
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package com.digitalasset.transcode
-
-class IncorrectVariantRepresentationException(
-    val message: String
-) extends Exception(message) {}
