09a6dd391e2ad9828c5d31d081807a339acaf731
Roger Bosman <roger.bosman@digitalasset.com>
2026-02-24 12:16:47 +0100

    drop unnecessary hashbang

--- sdk/daml-lf/generate_haskell_versions.py
+++ sdk/daml-lf/generate_haskell_versions.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-#!/usr/bin/env python3
-
 import json
 import sys
 import re
