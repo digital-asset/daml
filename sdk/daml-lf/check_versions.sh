eab93731b87f9df4e025c8b941cd5e0ab7ee6f7d
Roger Bosman <roger.bosman@digitalasset.com>
2026-03-10 10:57:38 +0100

    copyright comments

--- sdk/daml-lf/check_versions.sh
+++ sdk/daml-lf/check_versions.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
+
+# Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 set -euo pipefail
 
 GOLDEN_FILE="$1"
@@ -11,3 +15,4 @@ if ! diff -u "$GOLDEN_FILE" "$GENERATED_FILE"; then
   echo ""
   exit 1
 fi
+
--- sdk/daml-lf/update_versions.sh
+++ sdk/daml-lf/update_versions.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 set -euo pipefail
 
 if [ -z "${BUILD_WORKSPACE_DIRECTORY:-}" ]; then
@@ -11,3 +15,4 @@ DEST_PATH="$BUILD_WORKSPACE_DIRECTORY/$2"
 
 cp -f "$GENERATED_FILE" "$DEST_PATH"
 echo "✅ Successfully updated $2"
+
