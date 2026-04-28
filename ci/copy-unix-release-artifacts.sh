f7ddbd9a436c4ebf44438ca5c20a9efb370ed968
Samuel Williams <samuel.williams@digitalasset.com>
2026-04-28 11:26:44 +0100

    Drop assistant artifacts from split release

--- ci/copy-unix-release-artifacts.sh
+++ ci/copy-unix-release-artifacts.sh
@@ -23,26 +23,7 @@ else
   makedir="mkdir -p"
 fi
 
-for item in "github" "artifactory" "split-release" "oci/${RELEASE_TAG}/${NAME}"; do
-  ${makedir} "${OUTPUT_DIR}/$item"
-done
-
-SDK_CE_TARBALL="daml-sdk-${RELEASE_TAG}-${NAME}.tar.gz"
-echo "Copyring SDK tarball CE to ${OUTPUT_DIR}/github/${SDK_CE_TARBALL}"
-${copy} bazel-bin/release/sdk-release-tarball-ce.tar.gz "${OUTPUT_DIR}/github/${SDK_CE_TARBALL}"
-
-# Used for the non-split release process.
-SDK_EE_TARBALL="daml-sdk-${RELEASE_TAG}-${NAME}-ee.tar.gz"
-echo "Copying SDK tarball EE to ${OUTPUT_DIR}/artifactory/${SDK_EE_TARBALL}"
-${copy} bazel-bin/release/sdk-release-tarball-ee.tar.gz "${OUTPUT_DIR}/artifactory/${SDK_EE_TARBALL}"
-
-# Used for the split release process.
-echo "Copying SDK tarball EE to ${OUTPUT_DIR}/split-release/${SDK_EE_TARBALL}"
-${copy} bazel-bin/release/sdk-release-tarball-ee.tar.gz "${OUTPUT_DIR}/split-release/${SDK_EE_TARBALL}"
-
-DAMLC="damlc-${RELEASE_TAG}-${NAME}.tar.gz"
-echo "Copying damlc to ${OUTPUT_DIR}/split-release/${DAMLC}"
-${copy} bazel-bin/compiler/damlc/damlc-dist.tar.gz "${OUTPUT_DIR}/split-release/${DAMLC}"
+${makedir} "${OUTPUT_DIR}/oci/${RELEASE_TAG}/${NAME}"
 
 # OCI uploads
 function copy_oci {
@@ -57,7 +38,7 @@ function copy_oci {
 copy_oci damlc bazel-bin/compiler/damlc/damlc-oci.tar.gz
 copy_oci daml-script bazel-bin/daml-script/runner/daml-script-oci.tar.gz
 copy_oci codegen bazel-bin/language-support/codegen-main/codegen-oci.tar.gz
-copy_oci daml-new bazel-bin/daml-assistant/daml-helper/daml-new-oci.tar.gz
+copy_oci daml-new bazel-bin/daml-assistant/daml-new/daml-new-oci.tar.gz
 copy_oci upgrade-check bazel-bin/daml-assistant/upgrade-check-main/upgrade-check-oci.tar.gz
 
 # Platform independent artifacts are only built on Linux.
