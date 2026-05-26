340f0ee4eb80d473b98eba8c483ebfa0da40788f
wojciechormaniec-da <wojciech.ormaniec@digitalasset.com>
2026-05-27 01:51:24 +0200

    feat: Add `zip` to avoid custom toolchain implementation

--- sdk/bazel_tools/distribute_jar_cleanup.sh
+++ sdk/bazel_tools/distribute_jar_cleanup.sh
@@ -3,25 +3,26 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-SRC=$1
-OUT=$2
-NOTICES=$3
-LICENSE=$4
-LICENSE_EE=$5
-IS_EE=$6
+ZIP_BIN=$1
+SRC=$2
+OUT=$3
+NOTICES=$4
+LICENSE=$5
+LICENSE_EE=$6
+IS_EE=$7
 
-cp $SRC $OUT
-chmod +w $OUT
-zip -dq $OUT "/LICENSE*" "/META-INF/LICENSE*" "/NOTICE*" "/META-INF/NOTICE*"
+cp "$SRC" "$OUT"
+chmod +w "$OUT"
+"$ZIP_BIN" -dq "$OUT" "/LICENSE*" "/META-INF/LICENSE*" "/NOTICE*" "/META-INF/NOTICE*"
 
 # both licenses have the wrong file name/directory, so we copy to a temp dir
 TMP_LICENSE_DIR=$(mktemp -d)
-cp $NOTICES $TMP_LICENSE_DIR/NOTICES.txt
+cp "$NOTICES" "$TMP_LICENSE_DIR/NOTICES.txt"
 if [[ "$IS_EE" -eq 1 ]]; then
-  cp $LICENSE_EE $TMP_LICENSE_DIR/LICENSE.txt
+  cp "$LICENSE_EE" "$TMP_LICENSE_DIR/LICENSE.txt"
 else
-  cp $LICENSE $TMP_LICENSE_DIR/LICENSE.txt
+  cp "$LICENSE" "$TMP_LICENSE_DIR/LICENSE.txt"
 fi
-zip -ujq $OUT $TMP_LICENSE_DIR/LICENSE.txt $TMP_LICENSE_DIR/NOTICES.txt
+"$ZIP_BIN" -ujq "$OUT" "$TMP_LICENSE_DIR/LICENSE.txt" "$TMP_LICENSE_DIR/NOTICES.txt"
 rm -rf $TMP_LICENSE_DIR
 
