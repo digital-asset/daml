af572c812ec434ec625075fb0cfde5f2e81620e8
Roger Bosman <roger.bosman@digitalasset.com>
2026-05-26 16:06:17 +0200

    add back pre-shuffle retrystrategy
    add back retrystrategy
    
    add back original pre shuffle files
    
    rm timer_utils

---
+++ sdk/timer-utils/src/main/scala/com/daml/timer/PromiseTask.scala
@@ -0,0 +1,32 @@
+// Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// taken from:
+// https://github.com/digital-asset/daml/blob/pre-shuffle/sdk/libs-scala/timer-utils/src/main/scala/com/daml/timer/PromiseTask.scala
+
+package com.daml.timer
+
+import java.util.TimerTask
+
+import scala.concurrent.{Future, Promise}
+import scala.util.Try
+import scala.util.control.NonFatal
+
+private class PromiseTask[A](value: => Future[A]) extends TimerTask with Promise[A] {
+
+  private val p = Promise[A]()
+
+  override def run(): Unit = {
+    p.completeWith {
+      try value
+      catch { case NonFatal(t) => Future.failed(t) }
+    }
+  }
+
+  override def future: Future[A] = p.future
+
+  override def isCompleted: Boolean = p.isCompleted
+
+  override def tryComplete(result: Try[A]): Boolean = p.tryComplete(result)
+
+}
