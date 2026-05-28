78cd1f625fe5a53cabfd39140f68b0969bc82037
Roger Bosman <roger.bosman@digitalasset.com>
2026-05-28 09:48:17 +0200

    bump codegentest to pv dev and devmode

--- sdk/language-support/java/codegen/src/ledger-tests/scala/com/daml/CodegenTest.scala
+++ sdk/language-support/java/codegen/src/ledger-tests/scala/com/daml/CodegenTest.scala
@@ -4,6 +4,7 @@
 package com.daml
 
 import org.scalatest.flatspec.AsyncFlatSpec
+import com.daml.integrationtest.CantonConfig
 import com.daml.integrationtest.CantonConfig.ProtocolVersion
 
 // We factorize the 3 test to spin Canton only once.
@@ -11,5 +12,10 @@ import com.daml.integrationtest.CantonConfig.ProtocolVersion
 class CodegenTest extends AsyncFlatSpec with LedgerTest with InterfacesTest with StakeholdersTest {
   // TODO(https://github.com/digital-asset/daml/issues/18457): split key test cases and revert to
   //  protocolVersion = "latest" for non-key test cases.
-  override protected lazy val protocolVersion = ProtocolVersion.Explicit("v35")
+  // TODO[23052]: be smarter about this ProtocolVersion when we unhardcode staging
+  override protected lazy val protocolVersion = ProtocolVersion.Dev
+
+  // TODO[23052]: be smarter about this dev mode when we unhardcode staging
+  override protected def cantonConfig(): CantonConfig =
+    super.cantonConfig().copy(enableLfDevVersionSupport = true)
 }
