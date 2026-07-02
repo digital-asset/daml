7939ce80b50249c7ad6a7f1939e5b6e810f40560
Samuel Williams <samuel.williams@digitalasset.com>
2026-07-02 12:42:08 +0100

    Further tweaks to `bump-dpm.sh`

--- sdk/bump-dpm.sh
+++ sdk/bump-dpm.sh
@@ -2,13 +2,19 @@
 # Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# Takes optional DPM version, if none provided, latest stable version will be used.
+# Only allows DPM versions from the stable registry, as this is what the bazel rule uses
+
+# Must be run from `sdk`
+
 DPM_VERSION=$1
+DPM_REGISTRY="europe-docker.pkg.dev/da-images/public"
 
 if [ -z "$DPM_VERSION" ]; then
   # dpm prints things to stdout when downloading, which is quite annoying
   # Run it once to cache the output, then ask again for the sanitised output
-  bazel run @dpm_binary//:dpm -- repo resolve-tags --registry europe-docker.pkg.dev/da-images/public dpm:latest > /dev/null
-  DPM_VERSION=$(bazel run @dpm_binary//:dpm -- repo resolve-tags --registry europe-docker.pkg.dev/da-images/public dpm:latest 2> /dev/null)
+  bazel run @dpm_binary//:dpm -- repo resolve-tags --registry $DPM_REGISTRY dpm:latest > /dev/null
+  DPM_VERSION=$(bazel run @dpm_binary//:dpm -- repo resolve-tags --registry $DPM_REGISTRY dpm:latest 2> /dev/null)
 fi
 
 echo "Bumping DPM to $DPM_VERSION"
@@ -17,7 +23,7 @@ platforms=(linux_arm64 darwin_amd64 darwin_arm64 linux_amd64 windows_amd64)
 
 shas=()
 for platform in "${platforms[@]}"; do
-  sha=$(bazel run @dpm_binary//:oras -- manifest fetch europe-docker.pkg.dev/da-images/public/components/dpm:$DPM_VERSION.$platform  2> /dev/null | jq -r '.layers[0].digest | split(":")[1]')
+  sha=$(bazel run @dpm_binary//:oras -- manifest fetch $DPM_REGISTRY/components/dpm:$DPM_VERSION.$platform  2> /dev/null | jq -r '.layers[0].digest | split(":")[1]')
   shas+=("\"$platform\": \"$sha\"")
 done
 
