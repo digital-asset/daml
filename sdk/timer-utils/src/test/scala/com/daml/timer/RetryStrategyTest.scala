a5e93b7c9a599e372c6fe9dda1fcc31d2a822e55
KENDAL <barbutcukendal42@gmail.com>
2026-08-13 12:37:18 +0300

    test(timer-utils): add unit test for RetryStrategy overflow and message formatting

---
+++ sdk/timer-utils/src/test/scala/com/daml/timer/RetryStrategyTest.scala
@@ -0,0 +1,34 @@
+// Copyright (c) 2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.timer
+
+import org.scalatest.wordspec.AsyncWordSpec
+import org.scalatest.matchers.should.Matchers
+import scala.concurrent.duration._
+
+class RetryStrategyTest extends AsyncWordSpec with Matchers {
+
+  "RetryStrategy.exponentialBackoff" should {
+    "not throw overflow exception when attempts count is large" in {
+      // Large attempt count (e.g. 1024) causes math.pow(2.0, 1024.0) to equal Double.PositiveInfinity.
+      // Without capping, multiplying FiniteDuration * Infinity throws an overflow exception.
+      noException should be thrownBy {
+        RetryStrategy.exponentialBackoff(attempts = 1024, firstWaitTime = 10.millis)
+      }
+    }
+
+    "format attempts count string cleanly without printing Option wrapper" in {
+      val strategy = RetryStrategy.exponentialBackoff(attempts = 1, firstWaitTime = 1.milli)
+      strategy { (_, _) =>
+        scala.concurrent.Future.failed(new RuntimeException("test error"))
+      }.failed.map {
+        case ex: RetryStrategy.TooManyAttemptsException =>
+          ex.getMessage should include("after 1 attempts")
+          ex.getMessage should not include "Some(1)"
+        case other =>
+          fail(s"Unexpected exception type: $other")
+      }
+    }
+  }
+}
