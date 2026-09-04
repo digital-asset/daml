b27d4ed2be8a3a5e38123dd6041e3d2abe1b4441
Samuel Williams <samuel.williams@digitalasset.com>
2026-09-04 11:44:01 +0100

    Fix newline comparison in stable package test for windows

--- sdk/daml-script/daml/daml-script-stable/test_daml_script_stable_packages.sh
+++ sdk/daml-script/daml/daml-script-stable/test_daml_script_stable_packages.sh
@@ -30,4 +30,4 @@ generated=$(rlocation "$TEST_WORKSPACE/$1")
 real=$(rlocation "$TEST_WORKSPACE/$2")
 diff="$3"
 
-$diff -u "$generated" "$real" || (>&2 echo "Stable packages have been updated without updating the stable-packages.bzl file. Please run \"bazel run //daml-script/daml/daml-script-stable:update-script-stable-packages\" to update it." && exit 1)
+$diff -u --strip-trailing-cr "$generated" "$real" || (>&2 echo "Stable packages have been updated without updating the stable-packages.bzl file. Please run \"bazel run //daml-script/daml/daml-script-stable:update-script-stable-packages\" to update it." && exit 1)
